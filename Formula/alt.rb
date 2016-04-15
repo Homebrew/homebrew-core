ef8eb9b5cb2c8d053915f91c16f32eae8af910ba
Andrew De Ponte <cyphactor@gmail.com>
2016-04-15 01:39:11 -0700

    alt v0.0.1 (new formula)
    Added the new formula for the alt command line tool so that it would be easier
    to install.

---
+++ Formula/alt.rb
@@ -0,0 +1,19 @@
+class Alt < Formula
+  desc "Command line utility to find alternate file"
+  homepage "https://github.com/cyphactor/alt"
+  url "https://github.com/cyphactor/alt/archive/v0.0.1.tar.gz"
+  version "0.0.1"
+  sha256 "9b12d298580774e285b49aabc0ef511eb46561cd16cf89088431f02dac2326ea"
+
+  bottle :unneeded
+
+  depends_on :ruby => "1.9"
+
+  def install
+    bin.install "alt"
+  end
+
+  test do
+    system "#{bin}/alt", "--version"
+  end
+end
