ea73feff8744c2a2b733a1fd353129a2a9385d59
Andrew De Ponte <cyphactor@gmail.com>
2016-04-15 01:56:39 -0700

    Remove version line as not needed

--- Formula/alt.rb
+++ Formula/alt.rb
@@ -2,7 +2,6 @@ class Alt < Formula
   desc "command-line utility to find alternate file"
   homepage "https://github.com/cyphactor/alt"
   url "https://github.com/cyphactor/alt/archive/v0.0.1.tar.gz"
-  version "0.0.1"
   sha256 "9b12d298580774e285b49aabc0ef511eb46561cd16cf89088431f02dac2326ea"
 
   bottle :unneeded
