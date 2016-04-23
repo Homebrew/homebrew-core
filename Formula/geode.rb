e77b5f7dec845ff588af11b8c818515f54e7f308
Anthony Baker <abaker@apache.org>
2016-04-23 09:56:57 -0700

    Update geode 1.0.0-incubating.M2 (new formula)
    - Modified java dependency to 1.8+
    - Ensure that gfsh is invoked with JAVA_HOME set to a 1.8+ jdk
    - Remove JAVA_HOME and DISCLAIMER text from caveats
    - Expand test to fire up a locator process using gfsh

--- Formula/geode.rb
+++ Formula/geode.rb
@@ -6,31 +6,23 @@ class Geode < Formula
   sha256 "8447912c6d893008dd03917722d01abff4853861ce3f34c2bc95b60c51675cea"
 
   bottle :unneeded
-  depends_on :java
+
+  depends_on :java => "1.8+"
 
   def install
     rm_f "bin/gfsh.bat"
     bash_completion.install "bin/gfsh-completion.bash" => "gfsh"
     libexec.install Dir["*"]
-    bin.write_exec_script libexec+"bin/gfsh"
-  end
-
-  def caveats; <<-EOS.undent
-    Apache Geode is an effort undergoing incubation at The Apache Software Foundation (ASF),
-    sponsored by the Apache Incubator. Incubation is required of all newly accepted projects until
-    a further review indicates that the infrastructure, communications, and decision making process
-    have stabilized in a manner consistent with other successful ASF projects. While incubation
-    status is not necessarily a reflection of the completeness or stability of the code, it does
-    indicate that the project has yet to be fully endorsed by the ASF.
-
-    You may need to set JAVA_HOME:
-      export JAVA_HOME="$(/usr/libexec/java_home)"
-    EOS
+    (bin/"gfsh").write_env_script libexec/"bin/gfsh", Language::Java.java_home_env("1.8+")
   end
 
   test do
     ENV.java_cache
-    output = shell_output("#{bin}/gfsh version")
-    assert_match /v#{version}/, output
+    begin
+      output = shell_output("#{bin}/gfsh start locator --dir #{testpath} --name=geode_locator_brew_test")
+      assert_match /Cluster configuration service is up and running/, output
+    ensure
+      quiet_system "pkill", "-9", "-f", "geode_locator_brew_test"
+    end
   end
 end
