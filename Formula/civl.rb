fc52fb078bdc92f3b94babdbac6470e7a794ff47
AlshammariA <a.turqi@hotmail.com>
2016-04-22 23:22:22 -0400

    Updated civl.rb
    At this point, I fixed what I was mentioned to do. For Test part, I need time to figure out how can I do it. I might update the formula each month due to the version updates. So I think I will be able to complete the updating test section by that time.

--- Formula/civl.rb
+++ Formula/civl.rb
@@ -1,9 +1,12 @@
-
 class Civl < Formula
   desc "The Concurrency Intermediate Verification Language"
   homepage "http://vsl.cis.udel.edu/civl/"
   url "http://vsl.cis.udel.edu/lib/sw/civl/1.7/r3157/release/CIVL-1.7_3157.tgz"
+  version "1.7-3157"
   sha256 "33fa5b4877fbf494b0a4b426c15d6a165fa3db48a9f97cbf391818d965d2dfb9"
+
+  bottle :unneeded
+
   depends_on :java => "1.8+"
   depends_on "z3"
   depends_on "cvc4"
@@ -11,11 +14,9 @@ class Civl < Formula
   def install
     libexec.install "lib/civl-1.7_3157.jar"
     bin.write_jar_script libexec/"civl-1.7_3157.jar", "civl"
-    share.install "doc"
-    share.install "examples"
-    share.install "licenses"
-    share.install "emacs"
+    share.install "doc", "examples", "licenses", "emacs"
   end
+
   test do
     system "#{bin}/civl"
   end
