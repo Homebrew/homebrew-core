9a1e66de8612803aa4ba7d3869a6b77189854451
Joseph Chiocchi <input.output@gmail.com>
2016-05-05 19:08:08 -0500

    fix url

--- Formula/awscli.rb
+++ Formula/awscli.rb
@@ -2,7 +2,7 @@ class Awscli < Formula
   desc "Official Amazon AWS command-line interface"
   homepage "https://aws.amazon.com/cli/"
   url "https://pypi.python.org/packages/66/38/4661426e4f9e3a2dc3e52bd86f1d6e2aba551c734f39a927b6f9dcc72623/awscli-1.10.26.tar.gz"
-  mirror "https://github.com/aws/aws-cli/archive/1.10.25.tar.gz"
+  mirror "https://github.com/aws/aws-cli/archive/1.10.26.tar.gz"
   sha256 "92075d988e1739e6646b7ea84e4aa28d27887629cadaccaa82e5892b2c2fce4e"
 
   bottle do
