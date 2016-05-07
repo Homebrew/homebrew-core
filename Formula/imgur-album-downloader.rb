048d7d874edb35e1259d36027aa7fe3af2bd80a3
Shyam Sunder <sgsunder1@gmail.com>
2016-05-07 12:35:40 -0400

    Update imgur-album-downloader.rb
    Made some of the suggested changes

--- Formula/imgur-album-downloader.rb
+++ Formula/imgur-album-downloader.rb
@@ -1,6 +1,6 @@
 class ImgurAlbumDownloader < Formula
   desc "Command Line Utility to Download Imgur Albums"
-  homepage "https://github.com/sgsunder/imgur-album-downloader"
+  homepage "https://github.com/alexgisby/imgur-album-downloader"
   url "https://github.com/alexgisby/imgur-album-downloader.git", :revision => "dbaee1e342c1b46c022fccd26c294e251ac04015"
   version "1"
 
@@ -9,8 +9,7 @@ class ImgurAlbumDownloader < Formula
   depends_on "python3"
 
   def install
-    mv "imguralbum.py", "imgur-album-downloader" # Rename Command Line Script
-    bin.install "imgur-album-downloader"
+    bin.install "imguralbum.py" => "imgur-album-downloader" # Rename Command Line Script
   end
 
   test do
