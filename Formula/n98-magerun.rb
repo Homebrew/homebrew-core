27e833fddb1e45a9525f99ad4d5418d2879bd342
Mattheo Geoffray <mattheo.geoffray@gmail.com>
2016-05-26 02:30:19 +0200

    n98-magerun: fix formula version

--- Formula/n98-magerun.rb
+++ Formula/n98-magerun.rb
@@ -2,7 +2,7 @@ class N98Magerun < Formula
   desc "The swiss army knife for Magento developers, sysadmins and devops."
   homepage "http://magerun.net/"
   url "https://files.magerun.net/n98-magerun.phar"
-  version "1.0.0"
+  version "1.97.22"
   sha256 "199dd0d416260c68e691580ffd3f7a448d60f616529aadadcfee584992d6253e"
 
   def install
