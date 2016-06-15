06f90e4b98fae463a0d65a125199a2a8b73e5926
darklore <zodiac.brave.story@gmail.com>
2016-06-16 08:01:53 +0900

    Make it obvious what is being downloaded

--- Formula/digdag.rb
+++ Formula/digdag.rb
@@ -1,7 +1,7 @@
 class Digdag < Formula
   desc "Workload Automation System"
   homepage "https://github.com/treasure-data/digdag"
-  url "https://dl.digdag.io/digdag-0.8.1"
+  url "https://dl.digdag.io/digdag-0.8.1.jar"
   sha256 "92e79f5eb26b8992c9d510206c71f64fe4765c0c51a4e4cb62279aa1a4231ec7"
 
   bottle :unneeded
@@ -9,9 +9,9 @@ class Digdag < Formula
   depends_on :java => "1.8+"
 
   def install
-    libexec.install "digdag-#{version}" => "digdag"
-    chmod 0755, (libexec/"digdag")
-    bin.install_symlink "#{libexec}/digdag" => "digdag"
+    libexec.install "digdag-#{version}.jar" => "digdag.jar"
+    chmod 0755, libexec/"digdag.jar"
+    bin.install_symlink "#{libexec}/digdag.jar" => "digdag"
   end
 
   test do
