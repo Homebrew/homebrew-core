5a12f987e9d0270a8201bc9eb4ee5e06652554cd
darklore <zodiac.brave.story@gmail.com>
2016-06-16 00:03:44 +0900

    digdag 0.8.1 (new formula)

---
+++ Formula/digdag.rb
@@ -0,0 +1,20 @@
+class Digdag < Formula
+  desc "Workload Automation System"
+  homepage "https://github.com/treasure-data/digdag"
+  url "https://dl.digdag.io/digdag-0.8.1"
+  sha256 "92e79f5eb26b8992c9d510206c71f64fe4765c0c51a4e4cb62279aa1a4231ec7"
+
+  bottle :unneeded
+
+  depends_on :java => "1.8+"
+
+  def install
+    libexec.install "digdag-#{version}" => "digdag"
+    chmod 0755, (libexec/"digdag")
+    bin.install_symlink (libexec/"digdag") => "digdag"
+  end
+
+  test do
+    assert_match(/#{version}/, shell_output("#{bin}/digdag --version"))
+  end
+end
