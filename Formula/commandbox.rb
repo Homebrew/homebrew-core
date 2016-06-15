b429697a502873b1d4e7b2068f77105c0209fe69
Jon Clausen <jon_clausen@silowebworks.com>
2016-06-15 00:10:51 -0500

    Update commandbox.rb

--- Formula/commandbox.rb
+++ Formula/commandbox.rb
@@ -1,11 +1,11 @@
 class Commandbox < Formula
   desc "CFML embedded server, package manager, and app scaffolding tools"
-  homepage "http://www.ortussolutions.com/products/commandbox"
-  url "http://downloads.ortussolutions.com/ortussolutions/commandbox/3.1.1/commandbox-bin-3.1.1.zip"
+  homepage "https://www.ortussolutions.com/products/commandbox"
+  url "https://downloads.ortussolutions.com/ortussolutions/commandbox/3.1.1/commandbox-bin-3.1.1.zip"
   sha256 "050bb737db3033a7ae8d47f2e5a973583963328c7ed1c3e1ab7b1191c609ac60"
 
   devel do
-    url "http://integration.stg.ortussolutions.com/artifacts/ortussolutions/commandbox/3.1.1/commandbox-bin-3.1.1.zip"
+    url "https://integration.stg.ortussolutions.com/artifacts/ortussolutions/commandbox/3.1.1/commandbox-bin-3.1.1.zip"
     sha256 "050bb737db3033a7ae8d47f2e5a973583963328c7ed1c3e1ab7b1191c609ac60"
   end
 
@@ -14,7 +14,7 @@ class Commandbox < Formula
   depends_on :java => "1.7+"
 
   resource "apidocs" do
-    url "http://downloads.ortussolutions.com/ortussolutions/commandbox/3.1.1/commandbox-apidocs-3.1.1.zip"
+    url "https://downloads.ortussolutions.com/ortussolutions/commandbox/3.1.1/commandbox-apidocs-3.1.1.zip"
     sha256 "0495a4055582b4278de73a6edaa6526b7acf13daafcbe7bc65c67f97031edd93"
   end
 
