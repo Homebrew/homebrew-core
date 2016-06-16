49e502626698bde5c035bbbd98c5859f5a34dbf7
darklore <zodiac.brave.story@gmail.com>
2016-06-16 18:33:17 +0900

    Fix for OS X 10.9

--- Formula/digdag.rb
+++ Formula/digdag.rb
@@ -9,6 +9,9 @@ class Digdag < Formula
   depends_on :java => "1.8+"
 
   def install
+    # Add prefix a shebang before install for OS X 10.9.
+    # See https://github.com/Homebrew/homebrew-core/pull/2012#issuecomment-226407948 for the detail.
+    inreplace "digdag-#{version}.jar", /\A/, "#!/bin/sh\n"
     libexec.install "digdag-#{version}.jar" => "digdag.jar"
     chmod 0755, libexec/"digdag.jar"
     bin.install_symlink "#{libexec}/digdag.jar" => "digdag"
