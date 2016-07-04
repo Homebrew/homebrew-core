c4de842f169dd95c2107fa0f128c527894745b68
Freddy Kelly <me@fredkelly.net>
2016-07-04 17:18:17 +0100

    use dyn mirror url

--- Formula/apache-spark.rb
+++ Formula/apache-spark.rb
@@ -1,7 +1,7 @@
 class ApacheSpark < Formula
   desc "Engine for large-scale data processing"
   homepage "https://spark.apache.org/"
-  url "https://www.apache.org/dist/spark/spark-1.6.2/spark-1.6.2-bin-hadoop2.6.tgz"
+  url "https://www.apache.org/dyn/closer.lua?path=spark/spark-1.6.2/spark-1.6.2-bin-hadoop2.6.tgz"
   version "1.6.2"
   sha256 "bddeccec0fb8ac9491cbb4e320467e9263bcc1caf9b45466164f8ae2d97de710"
   head "https://github.com/apache/spark.git"
