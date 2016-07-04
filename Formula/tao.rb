40810b7d486cbb3ad50afa844aa2dd861dc8d8b5
Johnny Willemsen <jwillemsen@remedy.nl>
2016-07-04 13:53:28 +0200

    Removed block     * Formula/tao.rb:

--- Formula/tao.rb
+++ Formula/tao.rb
@@ -4,13 +4,6 @@ class Tao < Formula
   url "http://download.dre.vanderbilt.edu/previous_versions/ACE+TAO-6.4.0.tar.bz2"
   sha256 "8177d4f16bf013f7a003a7d1f83de9fda646406754c1f4cc0b0964a4b839eb0f"
 
-  bottle do
-    cellar :any
-    sha256 "0b53c1e14820be00af7115e9afefa09a380803589597038d962bb8ab16b79331" => :el_capitan
-    sha256 "d3e7e45197f7d7643d675ff84057df769947f6c0a9185388d23b2a28375760f0" => :yosemite
-    sha256 "72f21357797c350fe547a7978f402e564cf328c3662a0e321d1ec293f4899139" => :mavericks
-  end
-
   def install
     # Figure out the names of the header and makefile for this version
     # of OSX and link those files to the standard names.
