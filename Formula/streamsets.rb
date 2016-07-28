b860c1fd4fc44c4e1599031896f715c1a5b1482d
cnapun <cnapun@gmail.com>
2016-07-28 16:45:27 -0400

    streamsets: add dependencies and use inreplace

--- Formula/streamsets.rb
+++ Formula/streamsets.rb
@@ -3,7 +3,11 @@ class Streamsets < Formula
   homepage "https://streamsets.com/"
   url "https://archives.streamsets.com/datacollector/1.5.1.0/tarball/streamsets-datacollector-all-1.5.1.0.tgz"
   sha256 "d848ffaec4cbadce0e7aa031fb30d1e7e13c84be9115ecd6c69643ff02400fc8"
+
   bottle :unneeded
+
+  depends_on :java => "1.7+"
+  depends_on "md5sha1sum"
 
   def install
     # rename old SDC_CONF if it exists
@@ -24,11 +28,12 @@ class Streamsets < Formula
     bin.env_script_all_files libexec/"bin/", :STREAMSETS_HOME => libexec
 
     # edit sdc-env.sh so that data is stored in ${HOMEBREW_PREFIX}/var and ${HOMEBREW_PREFIX}/etc, so that data will be saved after brew upgrade
-    system "sed 's|#export SDC_LOG=/var/log/sdc|export SDC_LOG=#{var}/log/sdc|g' #{libexec}/libexec/sdc-env.sh > #{libexec}/libexec/sdc-env.sh.tmp && mv #{libexec}/libexec/sdc-env.sh.tmp #{libexec}/libexec/sdc-env.sh"
-    system "sed 's|#export SDC_DATA=/var/lib/sdc|export SDC_DATA=#{var}/lib/sdc|g' #{libexec}/libexec/sdc-env.sh > #{libexec}/libexec/sdc-env.sh.tmp && mv #{libexec}/libexec/sdc-env.sh.tmp #{libexec}/libexec/sdc-env.sh"
-    system "sed 's|#export SDC_RESOURCES=/var/lib/sdc-resources|export SDC_RESOURCES=#{var}/lib/sdc-resources|g' #{libexec}/libexec/sdc-env.sh > #{libexec}/libexec/sdc-env.sh.tmp && mv #{libexec}/libexec/sdc-env.sh.tmp #{libexec}/libexec/sdc-env.sh"
-    system "sed 's|#export SDC_CONF=/etc/sdc|export SDC_CONF=#{etc}/sdc|g' #{libexec}/libexec/sdc-env.sh > #{libexec}/libexec/sdc-env.sh.tmp && mv #{libexec}/libexec/sdc-env.sh.tmp #{libexec}/libexec/sdc-env.sh"
-    # copy default config files to $SDC_CONF
+    inreplace "#{libexec}/libexec/sdc-env.sh" do |s|
+      s.gsub! "#export SDC_LOG=/var/log/sdc", "export SDC_LOG=#{var}/log/sdc"
+      s.gsub! "#export SDC_DATA=/var/lib/sdc", "export SDC_DATA=#{var}/lib/sdc"
+      s.gsub! "#export SDC_RESOURCES=/var/lib/sdc-resources", "export SDC_RESOURCES=#{var}/lib/sdc-resources"
+      s.gsub! "#export SDC_CONF=/etc/sdc", "export SDC_CONF=#{etc}/sdc"
+    end
     (etc/"sdc").install Dir["#{libexec}/etc/*"]
   end
 
