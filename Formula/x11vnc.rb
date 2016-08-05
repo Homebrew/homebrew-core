6820e238b7087b9eece0830c41f53ac9f490ff3e
dj-m <domingomoronta@gmail.com>
2016-08-05 09:27:02 -0700

    x11vnc: fix patch whitespace

--- Formula/x11vnc.rb
+++ Formula/x11vnc.rb
@@ -52,9 +52,9 @@ index d6b0bda..0b2cfa9 100644
 @@ -177,7 +177,7 @@ unsigned long get_pixel(char *color) {
 
   XImage *solid_root(char *color) {
-  #if NO_X11
-  -	    RAWFB_RET_VOID
-  +	    RAWFB_RET(NULL)
-        if (!color) {}
-        return NULL;
-  #else
+    #if NO_X11
+    -	    RAWFB_RET_VOID
+    +	    RAWFB_RET(NULL)
+    if (!color) {}
+      return NULL;
+    #else
