7c1374d1df3df347e3b22fa3385a515be061b61c
Yamashita, Yuu <peek824545201@gmail.com>
2016-08-15 16:33:23 +0900

    Add custom version comparator for pyenv's weird versioning scheme (yyuu/pyenv#520)
    * v1.0.0 should win against v20160727
    * v1.0.0 should win against v0.4.0-20140602

--- Formula/pyenv.rb
+++ Formula/pyenv.rb
@@ -1,9 +1,32 @@
 class Pyenv < Formula
+  class Version < ::Version
+    # versions with datetime (e.g. v0.4.0-20140110.1, v20140614) is obsolete.
+    # those versions should be considered as older than ordinally versions (e.g. 1.0.0)
+    DATETIME_VERSIONS = /\A(?:0\.[1-4]\.\d+-)?201[3-6]\d{4}(?:\.\d+)?\z/
+    def <=>(other)
+      if DATETIME_VERSIONS =~ self
+        if DATETIME_VERSIONS =~ other
+          super
+        else
+          -1
+        end
+      else
+        if DATETIME_VERSIONS =~ other
+          1
+        else
+          super
+        end
+      end
+    end
+  end
+
+  PYENV_VERSION = "20160726"
   desc "Python version management"
   homepage "https://github.com/yyuu/pyenv"
-  url "https://github.com/yyuu/pyenv/archive/v20160726.tar.gz"
+  url "https://github.com/yyuu/pyenv/archive/v#{PYENV_VERSION}.tar.gz"
   sha256 "297040445f25912bea030be91b7d46c65eedbdf60655c2aae6226e88ed9b1346"
   head "https://github.com/yyuu/pyenv.git"
+  version Pyenv::Version.new(PYENV_VERSION)
 
   bottle :unneeded
 
