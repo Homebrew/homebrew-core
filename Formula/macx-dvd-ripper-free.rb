a3ed4f4fb64ad138bb270c5cf34488647228745e
Fred Lackey <fred.lackey@gmail.com>
2016-09-05 14:41:35 -0400

    MacX DVD Ripper (Free Edition) v4.2.4
    Creation of new formula for MacX DVD Ripper (Free Edition)

---
+++ Formula/macx-dvd-ripper-free.rb
@@ -0,0 +1,39 @@
+# Documentation: https://github.com/Homebrew/brew/blob/master/share/doc/homebrew/Formula-Cookbook.md
+#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class MacxVideoConverterFree < Formula
+  desc "MacX DVD Ripper (Free Edition)"
+  homepage "http://www.macxdvd.com/mac-video-converter-free"
+  url "http://www.macxdvd.com/download/converter_ripper_org/macx-dvd-ripper-mac-free-edition.dmg"
+  version "4.2.4"
+  sha256 "62acd1ac0c226227a2752e7445ef4fedc1e81acd153e79602e5a8f8f08b46733"
+
+  # depends_on "cmake" => :build
+  depends_on :x11 # if your formula requires any X11/XQuartz components
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+
+    # Remove unrecognized options if warned by configure
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
+    system "make", "install" # if this fails, try separate make/make install steps
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! It's enough to just replace
+    # "false" with the main program this formula installs, but it'd be nice if you
+    # were more thorough. Run the test with `brew test macx`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
