907321f314d912e4f3a59b7564c17d472c1a7020
reorx <novoreorx@gmail.com>
2016-09-11 16:28:04 +0800

    update to 1.1.3; add bottle :unneeded

--- Formula/httpstat.rb
+++ Formula/httpstat.rb
@@ -1,10 +1,11 @@
 class Httpstat < Formula
   desc "Curl statistics made simple"
   homepage "https://github.com/reorx/httpstat"
-  url "https://github.com/reorx/httpstat/archive/v1.1.2.tar.gz"
-  sha256 "0468abebfbae9ff7ea35d577b60ff9af9db3ff67ffd81b11ac005c4e9602919b"
+  url "https://github.com/reorx/httpstat/archive/v1.1.3.tar.gz"
+  sha256 "8ba3a8f0550468a634dce45d45dc03375e5dcdedea4c8edc8c26a326d929af04"
 
   depends_on :python if MacOS.version <= :snow_leopard
+  bottle :unneeded
 
   def install
     bin.install "httpstat.py" => "httpstat"
