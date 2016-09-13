91562e42d566f0651e8ccd3534e26a695415da9c
Jack Maney <JLManey@dstsystems.com>
2016-09-13 15:00:08 -0500

    Updated version_scheme as advised by @DomT4

--- Formula/pyenv-virtualenv.rb
+++ Formula/pyenv-virtualenv.rb
@@ -3,6 +3,7 @@ class PyenvVirtualenv < Formula
   homepage "https://github.com/yyuu/pyenv-virtualenv"
   url "https://github.com/yyuu/pyenv-virtualenv/archive/v1.0.0.tar.gz"
   sha256 "3523129c24ca3ad2cf130885ef9801cb066b04eabc354ee2c38c486b8b726f15"
+  version_scheme 1
   head "https://github.com/yyuu/pyenv-virtualenv.git"
 
   bottle :unneeded
