d19527972e43f8041530ec131102f0e359c10bbf
Konstantinos Koukopoulos <kouk@transifex.com>
2016-10-09 17:17:38 +0300

    fix sha hash in Formula/ssh-agent-filter.rb

--- Formula/ssh-agent-filter.rb
+++ Formula/ssh-agent-filter.rb
@@ -12,7 +12,7 @@ class SshAgentFilter < Formula
     # upstream didn't want this because realpath isn't included in coreutils for Debian wheezy.
     # see https://github.com/tiwe-de/ssh-agent-filter/pull/5
     url "https://patch-diff.githubusercontent.com/raw/tiwe-de/ssh-agent-filter/pull/5.diff"
-    sha256 "85cc828a96735bdafcf29eb6291ca91bac846579bcef7308536e0c875d6c81d7"
+    sha256 "307ccd6e872a3f3b999d8a4368d152dfbd3a7e43b4cff0cacbc0390c78b2976b"
   end
 
   patch do
