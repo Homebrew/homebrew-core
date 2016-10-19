7ef602627cb5a5b597be219463073d644367cde8
Matthias Winkelmann <m@matthi.coffee>
2016-10-20 00:18:28 +0200

    changed to canonical repository at sourceforge and used recommended pipe_output assertion

--- Formula/loris.rb
+++ Formula/loris.rb
@@ -1,8 +1,9 @@
 class Loris < Formula
   desc "Audio analysis, manipulation and synthesis"
   homepage "http://www.cerlsoundgroup.org/Loris"
-  url "https://github.com/MatthiasWinkelmann/loris/archive/1.9.tar.gz"
-  sha256 "94b894c05b31a59d08fe23b8403e1df3dc8b876acffe21d38a393a27775da2ac"
+  url "https://downloads.sourceforge.net/code-snapshots/git/l/lo/loris/loris-code.git/loris-loris-code-27d14aef9e50573bccb3e11d9c6b5c051ea04862.zip"
+  version "1.9.0"
+  sha256 "916d37d805ac07d823a351fb9742b107f306cafcaf4242a685bac3f0406077f0"
 
   depends_on "fftw"
   depends_on "autoconf" => :build
@@ -16,6 +17,11 @@ class Loris < Formula
   end
 
   test do
-    system "echo 'testing testing 1 2 3' | #{bin}/loris-analyze 50 -o /dev/null -verbose"
+    simple_test = pipe_output("#{bin}/loris-analyze 50 -o /dev/null -verbose", "testing testing 1 2 3")
+    return if simple_test =~ /analysis complete/
+    raise "tried running\n\nloris-analyze 50 -o /dev/null
+-verbose | echo 'testing 1 2 3'\n\n\
+expected '[..]analysis complete[..]' but got:\n\n\
+#{simple_test}"
   end
 end
