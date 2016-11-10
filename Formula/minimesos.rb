1ed569530a9b2ef04887e417eb54f089af0455d6
Mike McQuaid <mike@mikemcquaid.com>
2016-11-10 17:13:27 +0000

    minimesos: move bottle :unneeded

--- Formula/minimesos.rb
+++ Formula/minimesos.rb
@@ -4,9 +4,9 @@ class Minimesos < Formula
   url "https://github.com/ContainerSolutions/minimesos/archive/0.10.2.tar.gz"
   sha256 "16b198078c9384426f8849b08e92e173816c4d6626331bc6569aa5de89dffbdd"
 
-  depends_on "docker"
-  
   bottle :unneeded
+
+  depends_on "docker"
 
   def install
     bin.install "bin/minimesos"
