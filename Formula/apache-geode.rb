528faf92250d706fdec47dd60d63ecae425b1cd5
Jared Stewart <jstewart@pivotal.io>
2016-11-11 15:51:52 -0800

    Fixed apache-geode forumla to not use Java 1.9

--- Formula/apache-geode.rb
+++ Formula/apache-geode.rb
@@ -7,13 +7,13 @@ class ApacheGeode < Formula
 
   bottle :unneeded
 
-  depends_on :java => "1.8+"
+  depends_on :java => "1.8"
 
   def install
     rm_f "bin/gfsh.bat"
     bash_completion.install "bin/gfsh-completion.bash" => "gfsh"
     libexec.install Dir["*"]
-    (bin/"gfsh").write_env_script libexec/"bin/gfsh", Language::Java.java_home_env("1.8+")
+    (bin/"gfsh").write_env_script libexec/"bin/gfsh", Language::Java.java_home_env("1.8")
   end
 
   test do
