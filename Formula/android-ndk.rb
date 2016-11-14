21c0b4f23dab47fe66d46afc8427762a754384e1
Emanuele Zattin <ez@realm.io>
2016-11-14 11:21:15 +0100

    Move version before chesksum

--- Formula/android-ndk.rb
+++ Formula/android-ndk.rb
@@ -2,8 +2,8 @@ class AndroidNdk < Formula
   desc "Android native-code language toolset"
   homepage "https://developer.android.com/ndk/index.html"
   url "https://dl.google.com/android/repository/android-ndk-r13b-darwin-x86_64.zip"
-  sha256 "eba299f1cdd9059e635d159dbe71678cea9f00efb3ef9b56083c7a09dff665bf"
   version "r13b"
+  sha256 "eba299f1cdd9059e635d159dbe71678cea9f00efb3ef9b56083c7a09dff665bf"
   version_scheme 1
 
   bottle :unneeded
