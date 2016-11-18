639d5d82eb157ec84656a4383666259c9efb1532
Kaito Udagawa <umireon@gmail.com>
2016-11-19 04:08:47 +0900

    slimerjs: fix the outdated dependencies and caveats

--- Formula/slimerjs.rb
+++ Formula/slimerjs.rb
@@ -1,22 +1,3 @@
-class FirefoxRequirement < Requirement
-  fatal true
-  cask "firefox"
-
-  def self.firefox_installation
-    paths = ["~/Applications/FirefoxNightly.app", "~/Applications/Firefox.app",
-             "/Applications/FirefoxNightly.app", "/Applications/Firefox.app",
-             "~/Applications/FirefoxDeveloperEdition.app",
-             "/Applications/FirefoxDeveloperEdition.app"]
-    paths.find { |p| File.exist? File.expand_path(p) }
-  end
-
-  satisfy(:build_env => false) { FirefoxRequirement.firefox_installation }
-
-  def message
-    "Firefox must be available."
-  end
-end
-
 class Slimerjs < Formula
   desc "Scriptable browser for Web developers"
   homepage "https://slimerjs.org/"
@@ -25,10 +6,6 @@ class Slimerjs < Formula
   head "https://github.com/laurentj/slimerjs.git"
 
   bottle :unneeded
-
-  # Min supported OS X version by Firefox is 10.6.
-  depends_on :macos => :leopard
-  depends_on FirefoxRequirement
 
   def install
     cd "src" do
@@ -39,28 +16,24 @@ class Slimerjs < Formula
     bin.install_symlink libexec/"slimerjs"
   end
 
-  def caveats
-    s = ""
+  def caveats; <<-EOS.undent
+    SlimerJS may need some configurations before using:
+      https://docs.slimerjs.org/current/installation.html
 
-    if (firefox_installation = FirefoxRequirement.firefox_installation)
-      s += <<-EOS.undent
-        You MUST provide an installation of Mozilla Firefox and set
-        the environment variable SLIMERJSLAUNCHER pointing to it, e.g.:
-
-        export SLIMERJSLAUNCHER=#{firefox_installation}/Contents/MacOS/firefox
-        EOS
-    end
-    s += <<-EOS.undent
-
-      Note: If you use SlimerJS with an unstable version of Mozilla Firefox (>38.*)
-      you may have to change the [Gecko]MaxVersion in #{libexec}/application.ini
+    The configuration file is placed in:
+      #{libexec}/application.ini
     EOS
-
-    s
   end
 
   test do
-    ENV.delete "SLIMERJSLAUNCHER"
-    assert_match "Set it with the path to Firefox", shell_output("#{bin}/slimerjs", 1)
+    if File.exist? "/Applications/Firefox.app/Contents/MacOS/firefox"
+      (testpath/"test.js").write <<-EOS.undent
+        console.log("hello");
+        slimer.exit();
+      EOS
+      assert_match "hello", shell_output("#{bin}/slimerjs test.js", 0)
+    else
+      assert_match "Set it with the path to Firefox", shell_output("#{bin}/slimerjs test.js", 1)
+    end
   end
 end
