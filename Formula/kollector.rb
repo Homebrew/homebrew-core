10eda00c89003b6a21a67c8ef630459c72533bcd
Golnaz Jahesh <gjahesh@bcgsc.ca>
2016-12-19 21:42:33 -0800

    kollector v1.0.0 (new Formula)-apply PR comments

--- Formula/kollector.rb
+++ Formula/kollector.rb
@@ -1,11 +1,8 @@
-
 class Kollector < Formula
   desc "Targeted de novo Gene Assembly"
   homepage "https://github.com/bcgsc/kollector"
   url "https://github.com/bcgsc/kollector/archive/V1.0.tar.gz"
-  version "1.0"
   sha256 "76dc3e03398ade0ae39b241943ce7dabcf65c09e3b30180726bfea574c462d15"
-  #tag "bioinformatics"
 
   depends_on "abyss" => :build
   depends_on "biobloomtools" => :build
@@ -14,7 +11,6 @@ class Kollector < Formula
   depends_on "bwa" => :build
   
   def install
-   
     bin.install "./bin/kollector.sh"
     bin.install "./bin/kollector-recruit.mk"
     bin.install "./bin/kollector-extract.sh"
@@ -23,7 +19,6 @@ class Kollector < Formula
     doc.install "LICENSE.txt"
     prefix.install "test"
     prefix.install "data"
-    
   end
 
   test do
