d686aba0ffc0e3e9d4de839a4a4c881e37986e14
Stefan Stölzle <stefan@stoelzle.me>
2017-01-10 14:11:34 +0100

    bfg 1.12.14

--- Formula/bfg.rb
+++ Formula/bfg.rb
@@ -2,15 +2,15 @@ class Bfg < Formula
   desc "Remove large files or passwords from Git history like git-filter-branch"
   homepage "https://rtyley.github.io/bfg-repo-cleaner/"
   url "https://search.maven.org/remotecontent?filepath=com/madgag/bfg/1.12.14/bfg-1.12.14.jar"
-  sha256 "59059fdb9220172be229b4d1e2eda68ab0c280da3f0897f0b2c1f224645f7b40"
+  sha256 "7a44d53ef626a4282c34cc363166e678c5d9e91e4c6815e8e069036edd9fab64"
 
   bottle :unneeded
 
   depends_on :java => "1.7+"
 
   def install
-    libexec.install "bfg-1.12.13.jar"
-    bin.write_jar_script libexec/"bfg-1.12.13.jar", "bfg"
+    libexec.install "bfg-1.12.14.jar"
+    bin.write_jar_script libexec/"bfg-1.12.14.jar", "bfg"
   end
 
   test do
