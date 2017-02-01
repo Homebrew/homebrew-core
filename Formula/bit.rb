a7e0741887374a12227b334df11f2ce51dd0fead
Amit <amit@cocycles.com>
2017-02-02 00:53:34 +0200

    bit version update + bug fixes

--- Formula/bit.rb
+++ Formula/bit.rb
@@ -2,8 +2,8 @@ require "language/node"
 class Bit < Formula
   desc "Distributed Code Component Manager"
   homepage "https://www.bitsrc.io"
-  url "https://bitsrc.jfrog.io/bitsrc/bit-brew/stable/bit/0.1.12/bit-0.1.12-brew.tar.gz"
-  sha256 "122ec737cca207bba1fa9ae6e3ea22acf53cecd56b542a2a5bacd380a88bdef9"
+  url "https://bitsrc.jfrog.io/bitsrc/bit-brew/stable/bit/0.1.19/bit-0.1.19-brew.tar.gz"
+  sha256 "70683e3539036bfd0d276002229c0d01d02ecd2f1d849022d5e56f4bbdf4454d"
 
   depends_on "node"
 
