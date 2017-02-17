4ee341a06c8d7ec4262bd3937d1add7e51d17397
Christoph Blecker <admin@toph.ca>
2017-02-17 15:18:35 -0800

    terraform 0.8.7

--- Formula/terraform.rb
+++ Formula/terraform.rb
@@ -3,8 +3,8 @@ require "language/go"
 class Terraform < Formula
   desc "Tool to build, change, and version infrastructure"
   homepage "https://www.terraform.io/"
-  url "https://github.com/hashicorp/terraform/archive/v0.8.6.tar.gz"
-  sha256 "b6a346ef9956712f71efcbec09e29d6dedb3153485b1730a09be4613057f2355"
+  url "https://github.com/hashicorp/terraform/archive/v0.8.7.tar.gz"
+  sha256 "f7dae93f57618a9ea01c7b1e948161914035cfee8683848501bf083d1611c837"
   head "https://github.com/hashicorp/terraform.git"
 
   bottle do
@@ -75,10 +75,12 @@ class Terraform < Formula
       ENV.deparallelize { system "make", "test", "vet" }
 
       # Generate release binary
+      # Upsteam issue for parallelization errors:
+      # https://github.com/hashicorp/terraform/issues/12064
       arch = MacOS.prefer_64_bit? ? "amd64" : "386"
       ENV["XC_OS"] = "darwin"
       ENV["XC_ARCH"] = arch
-      system "make", "bin"
+      ENV.deparallelize { system "make", "bin" }
 
       # Install release binary
       bin.install "pkg/darwin_#{arch}/terraform"
