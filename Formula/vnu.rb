6cad7bc28715ebbceb87639bc686fc0ea0da52dc
Tomasz Pajor <tomek@polishgeeks.com>
2017-02-26 22:01:52 +0100

    vnu: depends on java 1.8+

--- Formula/vnu.rb
+++ Formula/vnu.rb
@@ -2,10 +2,11 @@ class Vnu < Formula
   desc "Nu Markup Checker: command-line and server HTML validator"
   homepage "https://validator.github.io/validator/"
   url "https://github.com/validator/validator/releases/download/17.2.1/vnu.jar_17.2.1.zip"
-  version "20170206"
   sha256 "e859b26b9532b4d1a5cb5c3bc873c85dc7a64c05ef7cb3f98ef040690d95609b"
 
   bottle :unneeded
+  
+  depends_on :java => "1.8+"
 
   def install
     libexec.install "vnu.jar"
