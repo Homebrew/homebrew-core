f34b5eea4e8c443b436f1fa43ad138c03a9b187b
Jeremy Mailen <jeremy.mailen@gmail.com>
2017-03-01 10:00:39 -0800

    Remove redundant version

--- Formula/kotlin.rb
+++ Formula/kotlin.rb
@@ -2,7 +2,6 @@ class Kotlin < Formula
   desc "Statically typed programming language for the JVM"
   homepage "https://kotlinlang.org/"
   url "https://github.com/JetBrains/kotlin/releases/download/v1.1/kotlin-compiler-1.1.zip"
-  version "1.1"
   sha256 "aa44db28bf3ccdae8842b6b92bec5991eb430a80e580aafbc6a044678a2f359d"
 
   bottle :unneeded
