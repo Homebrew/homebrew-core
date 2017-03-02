8e8dcbe14855afa163ce8d1a6579d79f4d72e977
Matt Nelson <matt.nelson@cerner.com>
2017-03-02 14:57:51 -0600

    fixing audit version warning

--- Formula/maven.rb
+++ Formula/maven.rb
@@ -6,7 +6,6 @@ class Maven < Formula
     url "https://www.apache.org/dyn/closer.cgi?path=maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz"
     mirror "https://archive.apache.org/dist/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz"
     sha256 "6e3e9c949ab4695a204f74038717aa7b2689b1be94875899ac1b3fe42800ff82"
-    version "3.3.9"
   end
 
   devel do
