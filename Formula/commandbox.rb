2ab8bf440c6ed3027ded0a29b8d053dd6f87db9d
Brad Wood <brad@bradwood.com>
2017-03-10 00:08:09 -0600

    Update for HTTPS

--- Formula/commandbox.rb
+++ Formula/commandbox.rb
@@ -1,7 +1,7 @@
 class Commandbox < Formula
   desc "CFML embedded server, package manager, and app scaffolding tools"
   homepage "https://www.ortussolutions.com/products/commandbox"
-  url "http://downloads.ortussolutions.com/ortussolutions/commandbox/3.6.0/commandbox-bin-3.6.0.zip"
+  url "https://downloads.ortussolutions.com/ortussolutions/commandbox/3.6.0/commandbox-bin-3.6.0.zip"
   sha256 "85c4336b50385eaf0194f1ff55bd79659f36a516ab0f2434c054a474b48ee7bb"
 
   bottle :unneeded
@@ -9,7 +9,7 @@ class Commandbox < Formula
   depends_on :java => "1.7+"
 
   resource "apidocs" do
-    url "http://downloads.ortussolutions.com/ortussolutions/commandbox/3.6.0/commandbox-apidocs-3.6.0.zip"
+    url "https://downloads.ortussolutions.com/ortussolutions/commandbox/3.6.0/commandbox-apidocs-3.6.0.zip"
     sha256 "bfb52645ed4302c65c53c589793511c36102f6a43131796bb82596552f03d2a1"
   end
 
