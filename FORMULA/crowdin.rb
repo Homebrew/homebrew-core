16cdd684fc47f5918e93c59f0ab6a5cfa5e0ce6c
ihor-popyk <ihor.popyk@crowdin.com>
2017-03-13 16:23:05 +0200

    crowdin 2.0.11 (new formula)

---
+++ FORMULA/crowdin.rb
@@ -0,0 +1,16 @@
+class Crowdin < Formula
+  desc "Crowdin-CLI is a command-line tool that allows to manage your resources"
+  homepage "https://support.crowdin.com/cli-tool/"
+  url "https://crowdin.com/downloads/crowdin-cli-2.0.11.zip"
+  sha256 "027925176948b8973ba1a9fab4f0c097bf49f2df0c23be20d79049bd7af4fbc3"
+
+  def install
+    bin.install "crowdin-cli.jar"
+    system "alias", "crowdin=\"java -jar #{bin}/crowdin-cli.jar\" >> ~/.bashrc"
+    system "alias", " crowdin=\"java -jar #{bin}/crowdin-cli.jar\" >> ~/.bash_profile"
+  end
+
+  test do
+    system "java", "-jar", "#{bin}/crowdin-cli.jar"
+  end
+end
