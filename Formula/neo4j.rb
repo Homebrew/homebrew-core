a62c8031f47eb15d7f8edb47b0346bb5c420114d
FX Coudert <fxcoudert@gmail.com>
2017-03-28 22:14:14 +0200

    Fix whitespace

--- Formula/neo4j.rb
+++ Formula/neo4j.rb
@@ -1,11 +1,9 @@
 class Neo4j < Formula
   desc "Robust (fully ACID) transactional property graph database"
   homepage "https://neo4j.com/"
-
   url "https://neo4j.com/artifact.php?name=neo4j-community-3.1.3-unix.tar.gz"
   version "3.1.3"
   sha256 "f0d79b4a98672dc527b708113644b8961ba824668c354e61dc4d2a16d8484880"
-
 
   bottle :unneeded
 
