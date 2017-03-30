b79b66589619bdcf190cf3ee51245b0bcbd3bb29
FX Coudert <fxcoudert@gmail.com>
2017-03-30 11:10:21 +0200

    Update jvm-mon.rb

--- Formula/jvm-mon.rb
+++ Formula/jvm-mon.rb
@@ -4,6 +4,8 @@ class JvmMon < Formula
   url "https://github.com/ajermakovics/jvm-mon/releases/download/0.3/jvm-mon-0.3.tar.gz"
   sha256 "9b5dd3d280cb52b6e2a9a491451da2ee41c65c770002adadb61b02aa6690c940"
 
+  bottle :unneeded
+
   depends_on :java => "1.8+"
 
   def install
