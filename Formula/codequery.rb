8e731d21bb8480ccfaf379aef612b917880c5f85
Ruben <ruben2020-git@users.sourceforge.net>
2017-04-09 03:43:35 +0800

    Update codequery.rb

--- Formula/codequery.rb
+++ Formula/codequery.rb
@@ -3,16 +3,17 @@ class Codequery < Formula
   homepage "https://github.com/ruben2020/codequery"
   url "https://github.com/ruben2020/codequery/archive/v0.19.0.tar.gz"
   sha256 "c8fbb133ec281cdc58d81824a1d3b1761e5496fcfd37229e68060beb0e0c7fc9"
+  revision 1
 
   bottle do
     cellar :any
-    sha256 "0a69d079240d98269069d0c38ca1579685084d975cf6c3e49e92554e2562e0a7" => :sierra
-    sha256 "7b305f7a624567e92d16637863d6d8c16946e8df22ece7502d5bd187a5e8517e" => :el_capitan
-    sha256 "d21a56640656f08060dd3c9e8b1800808f314e90f003a7d51158deb6be5f2a6e" => :yosemite
+    sha256 "048cefd89ef9f6701045ed89515d294dbcf1dc97881cea0c2a0dea1608ccb961" => :sierra
+    sha256 "a1a531716d96263ac8f277e0353079773697358719db22cf634659fd9e04aff6" => :el_capitan
+    sha256 "88c61e418dd6e065737a64f87c4d9611a2d3eb0cfb69fd02deacd58d16f6b43c" => :yosemite
   end
 
   depends_on "cmake" => :build
-  depends_on "qt5"
+  depends_on "qt"
 
   def install
     args = std_cmake_args
