084799621f2a7b8e422ee129e6393a1c324af2f1
Alexander Hamann <neutric@users.noreply.github.com>
2017-04-28 18:24:10 +0200

    groovy: remove formula name from description

--- Formula/groovy.rb
+++ Formula/groovy.rb
@@ -1,5 +1,5 @@
 class Groovy < Formula
-  desc "Groovy: a Java-based scripting language"
+  desc "Java-based scripting language"
   homepage "http://www.groovy-lang.org"
   url "https://dl.bintray.com/groovy/maven/apache-groovy-binary-2.4.11.zip"
   sha256 "4479a3653c565b173fc0d0e5e514a06f1c7d6f93926cbe0c5d8d29e5e4a0347c"
