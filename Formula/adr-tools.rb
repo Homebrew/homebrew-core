d3d788bed8e7174b919500463782ae411aa8d5ac
FX Coudert <fxcoudert@gmail.com>
2017-05-02 13:35:41 +0200

    Update adr-tools.rb

--- Formula/adr-tools.rb
+++ Formula/adr-tools.rb
@@ -4,6 +4,8 @@ class AdrTools < Formula
   url "https://github.com/npryce/adr-tools/archive/2.1.0.tar.gz"
   sha256 "1ef028cfeaa1b262a5c62845aa8965be169705370983f9ff73b17ec77bf75f70"
 
+  bottle :unneeded
+
   def install
     config = buildpath/"src/adr-config"
 
