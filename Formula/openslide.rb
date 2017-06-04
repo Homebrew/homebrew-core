f5ec64c8edb49d95e2fc7d60689c1596a8af59bd
Adam Gradzki <adam@mm.st>
2017-06-03 19:37:49 -0500

    Openslide: HEAD support

--- Formula/openslide.rb
+++ Formula/openslide.rb
@@ -12,9 +12,17 @@ class Openslide < Formula
     sha256 "ee965b0a44f4deab55d24bc49b2f3e1e08d9f3da95d66d067a1503b3c1ea3801" => :yosemite
     sha256 "4d822c1a8160f6b54224bb26e74fc4639bef2f27e6a9fbee824c2364c7a3bc00" => :mavericks
   end
+  
+  head do
+    url "https://github.com/openslide/openslide.git"
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+  end
 
   depends_on "pkg-config" => :build
   depends_on "libpng"
+  depends_on "gnutls"
   depends_on "jpeg"
   depends_on "libxml2"
   depends_on "libtiff"
@@ -28,9 +36,18 @@ class Openslide < Formula
     sha256 "ed92d5a9f2e86df67640d6f92ce3e231419ce127131697fbbce42ad5e002c8a7"
   end
 
+
   def install
-    system "./configure", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}"
+    if build.head?
+      system "autoreconf", "-i"
+      system "./configure", "--without-makeinfo",
+                            "--disable-static",
+                            "--prefix=#{prefix}"
+    else
+      system "./configure", "--disable-dependency-tracking",
+                            "--disable-static",
+                            "--prefix=#{prefix}"
+    end
     system "make", "install"
   end
 
