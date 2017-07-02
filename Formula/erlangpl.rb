4f55634969f3e38d43bd7fd79d86465ebfe184d1
Tomasz Cichocinski <tomaszcichocinski@gmail.com>
2017-07-02 11:05:50 +0200

    Update erlangpl test

--- Formula/erlangpl.rb
+++ Formula/erlangpl.rb
@@ -4,13 +4,19 @@ class Erlangpl < Formula
   url "https://github.com/erlanglab/erlangpl/releases/download/0.7.0/erlangpl.tar.gz"
   sha256 "f67c85024300a958e4ee582b24b87bee25a3f4a2325d7190585611212f4f8191"
 
+  bottle :unneeded
+
   depends_on "erlang"
 
   def install
-    bin.install "erlangpl" => "erlangpl"
+    bin.install "erlangpl"
   end
 
   test do
-    system "#{bin}/erlangpl", "--version"
+    system "erl", "-noshell", "-eval", "crypto:start().", "-s", "init", "stop"
+    # Erlang Perf Lab required Erlang/Elixir node to be presetn event with only
+    # --version or --help commands so we're testing if erlang is present since
+    # we're depending on it
+    # system "#{bin}/erlangpl -n e@127.0.0.1 -c epl"
   end
 end
