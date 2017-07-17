590f7aa389a2d32d451f0d81ee2550b639d6e657
ilovezfs <ilovezfs@icloud.com>
2017-07-17 10:53:47 -0700

    Update apache-ctakes.rb

--- Formula/apache-ctakes.rb
+++ Formula/apache-ctakes.rb
@@ -9,32 +9,17 @@ class ApacheCtakes < Formula
   depends_on :java => "1.8+"
 
   def install
-    rm_f Dir["bin/*.bat", "bin/*.cmd", "bin/ctakes.profile", "bin/ctakes-ytex", "libexec/*.bat", "libexec/*.cmd"]
+    rm_f Dir["bin/*.bat", "bin/*.cmd", "bin/ctakes.profile", "bin/ctakes-ytex",
+             "libexec/*.bat", "libexec/*.cmd"]
+    pkgshare.install_symlink libexec/"resources/org/apache/ctakes/examples"
     libexec.install %w[bin config desc lib resources]
     bin.write_exec_script Dir["#{libexec}/bin/*"]
   end
 
   test do
-    (testpath/"notes.txt").write <<-EOS.undent
-      Dr. Nutritious
-
-      Medical Nutrition Therapy for Hyperlipidemia
-
-      Referral from: Julie Tester, RD, LD, CNSD
-      Phone contact: (555) 555-1212
-      Height: 144 cm   Current Weight: 45 kg   Date of current weight:
-      02-29-2001 Admit Weight:  53 kg   BMI: 18 kg/m2
-      Diet: General
-      Daily Calorie needs (kcals): 1500 calories, assessed as HB + 20%
-      for activity.
-      Daily Protein needs: 40 grams,  assessed as 1.0 g/kg.
-      Pt has been on a 3-day calorie count and has had an average
-      intake of 1100 calories.  She was instructed to drink 2-3 cans
-      of liquid supplement to help promote weight gain.  She agrees
-      with the plan and has my number for further assessment. May want
-      a Resting Metabolic Rate as well. She takes an aspirin a day for
-      knee pain.
-    EOS
-    system "#{bin}/runPiperFile.sh", "-p", "#{libexec}/resources/org/apache/ctakes/examples/pipeline/HelloWorld.piper", "-i", "#{testpath}/notes.txt"
+    piper = pkgshare/"examples/pipeline/HelloWorld.piper"
+    note = pkgshare/"examples/notes/dr_nutritious_1.txt"
+    output = shell_output("#{bin}/runPiperFile.sh -p #{piper} -i #{note}")
+    assert_match "mayo-pos.zip", output
   end
 end
