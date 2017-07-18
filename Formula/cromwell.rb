02457fc46d87ff613afaaa2712ba477b7727aeed
ilovezfs <ilovezfs@icloud.com>
2017-07-18 06:43:58 -0700

    Update cromwell.rb

--- Formula/cromwell.rb
+++ Formula/cromwell.rb
@@ -2,8 +2,9 @@ class Cromwell < Formula
   desc "Workflow Execution Engine using Workflow Description Language"
   homepage "https://github.com/broadinstitute/cromwell"
   url "https://github.com/broadinstitute/cromwell/releases/download/28/cromwell-28.jar"
+  version "28.1"
   sha256 "c9ce762df236588ded042ceaf099848c0c1685d34788442ee251615ff13b5190"
-  revision 1
+
   head do
     url "https://github.com/broadinstitute/cromwell.git"
     depends_on "sbt" => :build
