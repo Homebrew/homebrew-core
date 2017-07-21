a2ea8a986b23bde875eb1d7c7d5bfe68bf1214d3
Michelangelo Bucci <mikonapoli@users.noreply.github.com>
2017-07-21 17:08:35 +0100

    Remove redundant version

--- Formula/grakn.rb
+++ Formula/grakn.rb
@@ -2,7 +2,6 @@ class Grakn < Formula
   desc "The Database for AI"
   homepage "https://grakn.ai"
   url "https://github.com/graknlabs/grakn/releases/download/v0.15.0/grakn-dist-0.15.0.tar.gz"
-  version "0.15.0"
   sha256 "010294b7d8f59e685efed2024132e49b31563b052351a91e2bf1ff75269b7052"
 
   bottle :unneeded
