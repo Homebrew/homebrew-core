f7f068d14ba7924aa7641213562532125a5e7cb7
FX Coudert <fxcoudert@gmail.com>
2017-07-21 11:57:10 +0200

    Update pyenv-virtualenv.rb

--- Formula/pyenv-virtualenv.rb
+++ Formula/pyenv-virtualenv.rb
@@ -3,6 +3,7 @@ class PyenvVirtualenv < Formula
   homepage "https://github.com/yyuu/pyenv-virtualenv"
   url "https://github.com/yyuu/pyenv-virtualenv/archive/v1.1.0.tar.gz"
   sha256 "26c71a30cc876350c48d670a128bd49be662ffad87b00ac91f907afea3a21f9f"
+  version_scheme 1
   head "https://github.com/yyuu/pyenv-virtualenv.git"
 
   bottle :unneeded
