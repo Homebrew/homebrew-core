d3902fbc18e12236aedbecc339f080857a987282
Alex Epstein <epsteina@wit.edu>
2017-07-26 12:32:04 -0400

    Changed sed command in bash-snippets installer

--- Formula/bash-snippets.rb
+++ Formula/bash-snippets.rb
@@ -2,7 +2,7 @@ class BashSnippets < Formula
   desc "Collection of small bash scripts for heavy terminal users"
   homepage "https://github.com/alexanderepstein/Bash-Snippets"
   url "https://github.com/alexanderepstein/Bash-Snippets/archive/v1.15.1.tar.gz"
-  sha256 "5de784a2d81163329a4c931b3b8c739a89513a05690c1218816404c74270dea2"
+  sha256 "ecc771d8d8486229309bdcaef330492ce37fb827e6ce276f251380db4a63930b"
 
   bottle :unneeded
 
