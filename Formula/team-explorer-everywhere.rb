fdd62da1cb8e59b493bd85cf6ba43cce08ea1f90
Chris Long <christopherglong@mac.com>
2017-08-17 01:31:15 -0700

    team-explorer-everywhere: address feedback

--- Formula/team-explorer-everywhere.rb
+++ Formula/team-explorer-everywhere.rb
@@ -1,6 +1,6 @@
 class TeamExplorerEverywhere < Formula
   desc "Run version control commands against a TFS server"
-  homepage "http://go.microsoft.com/fwlink/?LinkId=242481"
+  homepage "https://go.microsoft.com/fwlink/?LinkId=242481"
   url "https://github.com/Microsoft/team-explorer-everywhere/releases/download/14.120.0/TEE-CLC-14.120.0.zip"
   sha256 "157389b282a00c0d2e33bfab61e88bb47acb7811facbc427b766ed19be1821ec"
 
@@ -9,10 +9,9 @@ class TeamExplorerEverywhere < Formula
   depends_on :java => "1.6+"
 
   def install
-    ENV["TF_CLC_HOME"] = libexec
     libexec.install Dir["#{buildpath}/*"]
     bin.install libexec/"tf"
-    bin.env_script_all_files(libexec, :TF_CLC_HOME => ENV["TF_CLC_HOME"])
+    bin.env_script_all_files(libexec, :TF_CLC_HOME => libexec)
   end
 
   test do
