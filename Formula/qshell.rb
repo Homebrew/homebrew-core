a93c67ef40e580b6641586a8f889ba482498550c
jemygraw <jemygraw@gmail.com>
2017-08-22 13:07:37 +0800

    qshell 2.1.3 (new formula)

--- Formula/qshell.rb
+++ Formula/qshell.rb
@@ -1,37 +1,14 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook.html
-#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-
 class Qshell < Formula
-  desc ""
-  homepage ""
+  desc "Qiniu Cloud Resource Management Tool "
+  homepage "https://github.com/qiniu/qshell"
   url "https://dn-devtools.qbox.me/qshell-2.1.3.tar.gz"
   sha256 "26cad79300c471f34f52def329635f5405f520c7b662b6488a942173e1e93342"
 
-  # depends_on "cmake" => :build
-
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-
-    # Remove unrecognized options if warned by configure
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
-    # system "cmake", ".", *std_cmake_args
-    system "make", "install" # if this fails, try separate make/make install steps
+    system "make", "install"
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test qshell`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
     system "false"
   end
 end
