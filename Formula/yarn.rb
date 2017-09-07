901369e758ef16ca062ed64859f98f6df3d3cb16
ilovezfs <ilovezfs@icloud.com>
2017-09-07 07:48:16 -0700

    Update yarn.rb

--- Formula/yarn.rb
+++ Formula/yarn.rb
@@ -3,12 +3,6 @@ class Yarn < Formula
   homepage "https://yarnpkg.com/"
   url "https://yarnpkg.com/downloads/1.0.1/yarn-v1.0.1.tar.gz"
   sha256 "6b00b5e0a7074a512d39d2d91ba6262dde911d452617939ca4be4a700dd77cf1"
-  revision 1
-
-  devel do
-    url "https://yarnpkg.com/downloads/1.0.1/yarn-v1.0.1.tar.gz"
-    sha256 "6b00b5e0a7074a512d39d2d91ba6262dde911d452617939ca4be4a700dd77cf1"
-  end
 
   bottle :unneeded
 
