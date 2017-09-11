e63f917d8dcc4f58d8c8208db5f45a79f1132f38
king6cong <king6cong@gmail.com>
2017-09-11 16:36:57 +0800

    sbt use github release link directly

--- Formula/sbt.rb
+++ Formula/sbt.rb
@@ -1,8 +1,8 @@
 class Sbt < Formula
   desc "Build tool for Scala projects"
   homepage "http://www.scala-sbt.org"
-  url "https://cocl.us/sbt-1.0.1.tgz"
-  sha256 "5b68996a890b4a91efd9d13b9aca5e2b09f78c254c2907b3d548a9a7a73b0e5a"
+  url "https://github.com/sbt/sbt/archive/v1.0.1.zip"
+  sha256 "48c03adba7827f1990866baec88f21a438ca5c584624f95f1befb77d7a31588f"
 
   bottle :unneeded
 
