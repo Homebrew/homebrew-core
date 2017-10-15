dfc8123dccff8d8453a02096772b5b6badad30b0
BePsvPT <freedom@csie.io>
2017-10-15 23:49:51 +0800

    nvm 0.33.5

--- Formula/nvm.rb
+++ Formula/nvm.rb
@@ -1,8 +1,8 @@
 class Nvm < Formula
   desc "Manage multiple Node.js versions"
   homepage "https://github.com/creationix/nvm"
-  url "https://github.com/creationix/nvm/archive/v0.33.4.tar.gz"
-  sha256 "0647c23217d73cd91f4d51a5dab1892c4dfec727f0f6e9b1389a6e8b97034182"
+  url "https://github.com/creationix/nvm/archive/v0.33.5.tar.gz"
+  sha256 "9c4d1bf6c127a9d98e9e6a8b351dc025cecd3ef9416b932b00bf6069be252133"
   head "https://github.com/creationix/nvm.git"
 
   bottle :unneeded
