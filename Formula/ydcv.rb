1cd0eafe0eb9308a340047c5eabdf53a303dbe22
ilovezfs <ilovezfs@icloud.com>
2017-10-30 03:42:02 -0700

    Update ydcv.rb

--- Formula/ydcv.rb
+++ Formula/ydcv.rb
@@ -4,8 +4,9 @@ class Ydcv < Formula
   url "https://github.com/felixonmars/ydcv/archive/0.4.tar.gz"
   sha256 "2d9f6309bbf2d35c0c34c5ee945cf40769cc8201e6f374fa2a4f2d4b827fbdbb"
 
-  depends_on :python if MacOS.version <= :snow_leopard
   bottle :unneeded
+
+  depends_on :python if MacOS.version <= :snow_leopard
 
   def install
     bin.install "ydcv.py" => "ydcv"
