e0e855335f03147b0a6d1645eb9a23223c941aa4
Filipe Peliz Pinto Teixeira <fppintoteixeira@gmail.com>
2017-11-03 15:02:18 +0000

    Manually update grakn to next release 0.17.0

--- Formula/grakn.rb
+++ Formula/grakn.rb
@@ -1,8 +1,8 @@
 class Grakn < Formula
   desc "The Database for AI"
   homepage "https://grakn.ai"
-  url "https://github.com/graknlabs/grakn/releases/download/v0.16.0/grakn-dist-0.16.0.tar.gz"
-  sha256 "1c7194f09cbf31949323b79b2fb8a5683381af5ecf37886fd45deb71585261ed"
+  url "https://github.com/graknlabs/grakn/releases/download/v0.17.0/grakn-dist-0.17.0.tar.gz"
+  sha256 "27d41b82ea6144af149ed09ab4631501bb2beaea7b94e28f4b1fc4cca0443434"
 
   bottle :unneeded
 
@@ -10,11 +10,11 @@ class Grakn < Formula
 
   def install
     libexec.install Dir["*"]
-    bin.install Dir["#{libexec}/bin/*"]
-    bin.env_script_all_files(libexec/"bin", :CASSANDRA_HOME => ENV["CASSANDRA_HOME"])
+    bin.install Dir["#{libexec}/*"]
+    bin.env_script_all_files(libexec/, :CASSANDRA_HOME => ENV["CASSANDRA_HOME"])
   end
 
   test do
-    assert_match /stopped/i, shell_output("#{bin}/grakn.sh status")
+    assert_match /stopped/i, shell_output("grakn server status")
   end
 end
