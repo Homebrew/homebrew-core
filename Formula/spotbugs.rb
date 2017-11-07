3d67272ae0bfc9c3b3939ffee92aa216deb80e3a
Stephen G <stephen@egroat.com>
2017-11-07 09:40:00 -0800

    Add compiled version

--- Formula/spotbugs.rb
+++ Formula/spotbugs.rb
@@ -1,17 +1,23 @@
 class Spotbugs < Formula
   desc "Tool for Java static analysis (FindBugs' successor)"
   homepage "https://spotbugs.github.io/"
-  url "https://github.com/spotbugs/spotbugs/archive/3.1.0.tar.gz"
-  sha256 "ac6169a551212756a05dd9c066ef9212653d8a99b754ef4dade6a7008f8ad781"
+  url "http://repo.maven.apache.org/maven2/com/github/spotbugs/spotbugs/3.1.0/spotbugs-3.1.0.tgz"
+  sha256 "c1eab4dc50cd9854c83fc4a90b4a9b9c63e5307bd0f0f7bb04720e17153b629c"
   head "https://github.com/spotbugs/spotbugs.git"
+
+  bottle :unneeded
 
   depends_on "gradle" => :build
   depends_on :java => "1.8+"
 
   def install
-    system "gradle", "build"
-    system "gradle", "installDist"
-    libexec.install Dir["spotbugs/build/install/spotbugs/*"]
+    if build.head?
+      system "gradle", "build"
+      system "gradle", "installDist"
+      libexec.install Dir["spotbugs/build/install/spotbugs/*"]
+    else
+      libexec.install Dir["*"]
+    end
     bin.install_symlink "#{libexec}/bin/spotbugs"
   end
 
