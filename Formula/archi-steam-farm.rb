e2daebd070ae4f6082bca79540ab3e1ec4aeb368
Aubin Paul <outlyer@users.noreply.github.com>
2017-12-16 12:59:41 -0500

    ArchiSteamFarm 3.0.5.5

--- Formula/archi-steam-farm.rb
+++ Formula/archi-steam-farm.rb
@@ -1,30 +1,32 @@
 class ArchiSteamFarm < Formula
-  desc "ASF is a C# application that allows you to farm steam cards"
+  desc "A C# (mono) application that allows you to farm steam cards using multiple steam accounts simultaneously."
   homepage "https://github.com/JustArchi/ArchiSteamFarm"
-  url "https://github.com/JustArchi/ArchiSteamFarm/releases/download/2.3.2.0/ASF.zip"
-  sha256 "1a9f50c3cf2eb00e5148bc21a209b0c7c275b6c36c8cae8b4d9b2469bee7ff33"
+  url "https://github.com/JustArchi/ArchiSteamFarm/releases/download/3.0.5.5/ASF-osx-x64.zip"
+  sha256 "4eab592ed6f7c389c071717a3aad1ba335c27edd9b7e8d06c9a34900b28c00ab"
+  version "3.0.5.5"
 
   bottle :unneeded
 
   depends_on "mono"
 
   def install
-    libexec.install "ASF.exe"
-    (bin/"asf").write <<~EOS
-      #!/bin/bash
-      mono #{libexec}/ASF.exe "$@"
-    EOS
+    libexec.install Dir["*"]
+    chmod 0755, Dir["#{libexec}/ArchiSteamFarm"]
 
-    etc.install "config" => "asf"
-    libexec.install_symlink etc/"asf" => "config"
+    etc.install "#{libexec}/config" => "archi-steam-farm"
+    libexec.install_symlink etc/"archi-steam-farm" => "config"
   end
 
   def caveats; <<~EOS
-    Config: #{etc}/asf/
+    Config: #{etc}/archi-steam-farm/
+
+    You can also generate a configuration file via the web-based configurator here: 
+    https://justarchi.github.io/ArchiSteamFarm/
+
     EOS
   end
 
   test do
-    assert_match "ASF V#{version}", shell_output("#{bin}/asf --client")
+    assert_match "ASF V#{version}", shell_output("#{bin}/ArchiSteamFarm --client")
   end
 end
