607bda0bb1b69a6c91e29586bd779ee8a6c6964d
FX Coudert <fxcoudert@gmail.com>
2017-12-18 12:08:43 +0100

    autopsy 4.5.0

--- Formula/autopsy.rb
+++ Formula/autopsy.rb
@@ -1,8 +1,8 @@
 class Autopsy < Formula
   desc "Graphical interface to Sleuth Kit investigation tools"
   homepage "https://www.sleuthkit.org/autopsy/index.php"
-  url "https://downloads.sourceforge.net/project/autopsy/autopsy/2.24/autopsy-2.24.tar.gz"
-  sha256 "ab787f519942783d43a561d12be0554587f11f22bc55ab79d34d8da703edc09e"
+  url "https://github.com/sleuthkit/autopsy/archive/autopsy-4.5.0.tar.gz"
+  sha256 "59ed36b056b5ce9507ee713f2be438689fab6282b298c083710582b9992adff7"
 
   bottle :unneeded
 
