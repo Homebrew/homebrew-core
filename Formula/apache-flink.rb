7fff92c338d570564a49d49c8aaa4b17d00c87cb
Juan Miguel Cejuela <i@juanmi.rocks>
2017-12-21 09:40:57 +0100

    update apache-flink 1.4.0
    * no hadoop undled
    * download url using mirror
    * Java support for java 8 only, specifically no java9

--- Formula/apache-flink.rb
+++ Formula/apache-flink.rb
@@ -1,19 +1,21 @@
 class ApacheFlink < Formula
   desc "Scalable batch and stream data processing"
   homepage "https://flink.apache.org/"
-  url "https://www.apache.org/dyn/closer.lua?path=flink/flink-1.4.0/flink-1.4.0-bin-hadoop28-scala_2.11.tgz"
+  url "https://www.apache.org/dyn/mirrors/mirrors.cgi?action=download&filename=flink/flink-1.4.0/flink-1.4.0-bin-scala_2.11.tgz"
   version "1.4.0"
-  sha256 "150af64309d6d29175fbcc7991dcae5c843a54fbfab4d9494b23d50febe11671"
+  sha256 "a6cee77e0719c42f0bd2d30e5a149a71ad50a708f163605c4570d418ba6c0035"
   head "https://github.com/apache/flink.git"
 
   bottle :unneeded
 
-  depends_on :java => "1.7+"
+  # Upstream support for Java9 comes in version 1.5: https://issues.apache.org/jira/browse/FLINK-8033
+  # Upstream support for Scala 2.12 comes in version 1.5: https://issues.apache.org/jira/browse/FLINK-7811
+  depends_on :java => "1.8"
 
   def install
     rm_f Dir["bin/*.bat"]
     libexec.install Dir["*"]
-    bin.write_exec_script Dir["#{libexec}/bin/flink"]
+    bin.write_exec_script Dir["#{libexec}/bin/flink"], Language::Java.java_home_env("1.8")
   end
 
   test do
