40ac2f50f3a80531c982bad5e4a1c205b5eed090
Mike McQuaid <mike@mikemcquaid.com>
2018-01-07 19:31:20 +0000

    libxslt: fix rubocop check.

--- Formula/libxslt.rb
+++ Formula/libxslt.rb
@@ -43,9 +43,10 @@ class Libxslt < Formula
     system "make", "install"
   end
 
-  def caveats; <<~EOS
-    To allow the nokogiri gem to link against this libxslt run:
-      gem install nokogiri -- --with-xslt-dir=#{opt_prefix}
+  def caveats
+    <<~EOS
+      To allow the nokogiri gem to link against this libxslt run:
+        gem install nokogiri -- --with-xslt-dir=#{opt_prefix}
     EOS
   end
 
