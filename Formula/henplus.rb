819f1362bfcc58fb2b5eb36afd8752385641b84b
Mike McQuaid <mike@mikemcquaid.com>
2018-01-07 19:31:16 +0000

    henplus: fix rubocop check.

--- Formula/henplus.rb
+++ Formula/henplus.rb
@@ -30,9 +30,10 @@ class Henplus < Formula
     system "ant", "install", "-Dprefix=#{prefix}"
   end
 
-  def caveats; <<~EOS
-    You may need to set JAVA_HOME:
-      export JAVA_HOME="$(/usr/libexec/java_home)"
+  def caveats
+    <<~EOS
+      You may need to set JAVA_HOME:
+        export JAVA_HOME="$(/usr/libexec/java_home)"
     EOS
   end
 
