bb5d320ccceb21aef826ee0f0e9702a7b8858a8f
Mike McQuaid <mike@mikemcquaid.com>
2018-01-07 19:31:18 +0000

    jack: fix rubocop check.

--- Formula/jack.rb
+++ Formula/jack.rb
@@ -36,27 +36,28 @@ class Jack < Formula
 
   plist_options :manual => "jackd -d coreaudio"
 
-  def plist; <<~EOS
-    <?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-    <plist version="1.0">
-    <dict>
-      <key>Label</key>
-      <string>#{plist_name}</string>
-      <key>WorkingDirectory</key>
-      <string>#{prefix}</string>
-      <key>ProgramArguments</key>
-      <array>
-        <string>#{opt_bin}/jackd</string>
-        <string>-d</string>
-        <string>coreaudio</string>
-      </array>
-      <key>RunAtLoad</key>
-      <true/>
-      <key>KeepAlive</key>
-      <true/>
-    </dict>
-    </plist>
+  def plist
+    <<~EOS
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+      <dict>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>WorkingDirectory</key>
+        <string>#{prefix}</string>
+        <key>ProgramArguments</key>
+        <array>
+          <string>#{opt_bin}/jackd</string>
+          <string>-d</string>
+          <string>coreaudio</string>
+        </array>
+        <key>RunAtLoad</key>
+        <true/>
+        <key>KeepAlive</key>
+        <true/>
+      </dict>
+      </plist>
     EOS
   end
 
