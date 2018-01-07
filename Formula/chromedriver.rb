43dbf2f4ab6fcc51956238ff21fa50920e2ea577
Mike McQuaid <mike@mikemcquaid.com>
2018-01-07 19:31:05 +0000

    chromedriver: fix rubocop check.

--- Formula/chromedriver.rb
+++ Formula/chromedriver.rb
@@ -13,29 +13,30 @@ class Chromedriver < Formula
 
   plist_options :manual => "chromedriver"
 
-  def plist; <<~EOS
-    <?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-    <plist version="1.0">
-    <dict>
-      <key>Label</key>
-      <string>homebrew.mxcl.chromedriver</string>
-      <key>RunAtLoad</key>
-      <true/>
-      <key>KeepAlive</key>
-      <false/>
-      <key>ProgramArguments</key>
-      <array>
-        <string>#{opt_bin}/chromedriver</string>
-      </array>
-      <key>ServiceDescription</key>
-      <string>Chrome Driver</string>
-      <key>StandardErrorPath</key>
-      <string>#{var}/log/chromedriver-error.log</string>
-      <key>StandardOutPath</key>
-      <string>#{var}/log/chromedriver-output.log</string>
-    </dict>
-    </plist>
+  def plist
+    <<~EOS
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+      <dict>
+        <key>Label</key>
+        <string>homebrew.mxcl.chromedriver</string>
+        <key>RunAtLoad</key>
+        <true/>
+        <key>KeepAlive</key>
+        <false/>
+        <key>ProgramArguments</key>
+        <array>
+          <string>#{opt_bin}/chromedriver</string>
+        </array>
+        <key>ServiceDescription</key>
+        <string>Chrome Driver</string>
+        <key>StandardErrorPath</key>
+        <string>#{var}/log/chromedriver-error.log</string>
+        <key>StandardOutPath</key>
+        <string>#{var}/log/chromedriver-output.log</string>
+      </dict>
+      </plist>
     EOS
   end
 
