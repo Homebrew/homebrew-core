fde0f77795ef457c7ed6b3a2a5369727ebc064dc
Mike McQuaid <mike@mikemcquaid.com>
2018-01-07 19:31:40 +0000

    scala: fix rubocop check.

--- Formula/scala.rb
+++ Formula/scala.rb
@@ -70,9 +70,10 @@ class Scala < Formula
     idea.install_symlink doc => "doc"
   end
 
-  def caveats; <<~EOS
-    To use with IntelliJ, set the Scala home to:
-      #{opt_prefix}/idea
+  def caveats
+    <<~EOS
+      To use with IntelliJ, set the Scala home to:
+        #{opt_prefix}/idea
     EOS
   end
 
