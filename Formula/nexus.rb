77f331d144acc20f88998a8e292b9e721843942e
Mike McQuaid <mike@mikemcquaid.com>
2018-01-07 19:31:27 +0000

    nexus: fix rubocop check.

--- Formula/nexus.rb
+++ Formula/nexus.rb
@@ -19,22 +19,23 @@ class Nexus < Formula
 
   plist_options :manual => "nexus start"
 
-  def plist; <<~EOS
-    <?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-    <plist version="1.0">
-      <dict>
-        <key>Label</key>
-        <string>com.sonatype.nexus</string>
-        <key>ProgramArguments</key>
-        <array>
-          <string>#{opt_bin}/nexus</string>
-          <string>start</string>
-        </array>
-        <key>RunAtLoad</key>
-      <true/>
-      </dict>
-    </plist>
+  def plist
+    <<~EOS
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+        <dict>
+          <key>Label</key>
+          <string>com.sonatype.nexus</string>
+          <key>ProgramArguments</key>
+          <array>
+            <string>#{opt_bin}/nexus</string>
+            <string>start</string>
+          </array>
+          <key>RunAtLoad</key>
+        <true/>
+        </dict>
+      </plist>
     EOS
   end
 
