1be9ea5416e2e04d1890578fae896d76a63eb452
Mike McQuaid <mike@mikemcquaid.com>
2018-01-07 19:31:41 +0000

    sickbeard: fix rubocop check.

--- Formula/sickbeard.rb
+++ Formula/sickbeard.rb
@@ -46,35 +46,37 @@ class Sickbeard < Formula
 
   plist_options :manual => "sickbeard"
 
-  def plist; <<~EOS
-    <?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-    <plist version="1.0">
-    <dict>
-      <key>Label</key>
-      <string>#{plist_name}</string>
-      <key>ProgramArguments</key>
-      <array>
-        <string>#{opt_bin}/sickbeard</string>
-        <string>-q</string>
-        <string>--nolaunch</string>
-        <string>-p</string>
-        <string>8081</string>
-      </array>
-      <key>RunAtLoad</key>
-      <true/>
-    </dict>
-    </plist>
+  def plist
+    <<~EOS
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+      <dict>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>ProgramArguments</key>
+        <array>
+          <string>#{opt_bin}/sickbeard</string>
+          <string>-q</string>
+          <string>--nolaunch</string>
+          <string>-p</string>
+          <string>8081</string>
+        </array>
+        <key>RunAtLoad</key>
+        <true/>
+      </dict>
+      </plist>
     EOS
   end
 
-  def startup_script; <<~EOS
-    #!/bin/bash
-    export PYTHONPATH="#{libexec}/lib/python2.7/site-packages:$PYTHONPATH"
-    python "#{libexec}/SickBeard.py"\
-           "--pidfile=#{var}/run/sickbeard.pid"\
-           "--datadir=#{etc}/sickbeard"\
-           "$@"
+  def startup_script
+    <<~EOS
+      #!/bin/bash
+      export PYTHONPATH="#{libexec}/lib/python2.7/site-packages:$PYTHONPATH"
+      python "#{libexec}/SickBeard.py"\
+             "--pidfile=#{var}/run/sickbeard.pid"\
+             "--datadir=#{etc}/sickbeard"\
+             "$@"
     EOS
   end
 end
