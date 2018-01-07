9dfdeba05b42a7a6a3de8b01f1d57937b7d0d630
Mike McQuaid <mike@mikemcquaid.com>
2018-01-07 19:31:33 +0000

    ploticus: fix rubocop check.

--- Formula/ploticus.rb
+++ Formula/ploticus.rb
@@ -28,8 +28,9 @@ class Ploticus < Formula
     pkgshare.install Dir["prefabs/*"]
   end
 
-  def caveats; <<~EOS
-    Ploticus prefabs have been installed to #{opt_pkgshare}
+  def caveats
+    <<~EOS
+      Ploticus prefabs have been installed to #{opt_pkgshare}
   EOS
   end
 
