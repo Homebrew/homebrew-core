3f41ebe1c06811d13fe88d64e25081831e22bc6a
Mike McQuaid <mike@mikemcquaid.com>
2018-01-07 19:31:28 +0000

    np2: fix rubocop check.

--- Formula/np2.rb
+++ Formula/np2.rb
@@ -42,8 +42,9 @@ class Np2 < Formula
     end
   end
 
-  def caveats; <<~EOS
-    A Japanese TTF file named `default.ttf` should be in the working directory.
+  def caveats
+    <<~EOS
+      A Japanese TTF file named `default.ttf` should be in the working directory.
     EOS
   end
 
