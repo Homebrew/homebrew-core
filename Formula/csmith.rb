e3e08fa3e323da20b529c5122531a2f7d13f38fe
Mike McQuaid <mike@mikemcquaid.com>
2018-01-07 19:31:07 +0000

    csmith: fix rubocop check.

--- Formula/csmith.rb
+++ Formula/csmith.rb
@@ -22,9 +22,10 @@ class Csmith < Formula
     (include/"csmith-#{version}/runtime").install Dir["runtime/*.h"]
   end
 
-  def caveats; <<~EOS
-    It is recommended that you set the environment variable 'CSMITH_PATH' to
-      #{include}/csmith-#{version}
+  def caveats
+    <<~EOS
+      It is recommended that you set the environment variable 'CSMITH_PATH' to
+        #{include}/csmith-#{version}
     EOS
   end
 
