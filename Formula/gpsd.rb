bbe273ca99fc63a3ddde96c27f35047447599090
Mike McQuaid <mike@mikemcquaid.com>
2018-01-07 19:31:14 +0000

    gpsd: fix rubocop check.

--- Formula/gpsd.rb
+++ Formula/gpsd.rb
@@ -19,17 +19,18 @@ class Gpsd < Formula
     scons "install"
   end
 
-  def caveats; <<~EOS
-    gpsd does not automatically detect GPS device addresses. Once started, you
-    need to force it to connect to your GPS:
-
-      GPSD_SOCKET="#{var}/gpsd.sock" #{sbin}/gpsdctl add /dev/tty.usbserial-XYZ
+  def caveats
+    <<~EOS
+      gpsd does not automatically detect GPS device addresses. Once started, you
+      need to force it to connect to your GPS:
+         GPSD_SOCKET="#{var}/gpsd.sock" #{sbin}/gpsdctl add /dev/tty.usbserial-XYZ
     EOS
   end
 
   plist_options :manual => "#{HOMEBREW_PREFIX}/sbin/gpsd -N -F #{HOMEBREW_PREFIX}/var/gpsd.sock /dev/tty.usbserial-XYZ"
 
-  def plist; <<-EOS.undent
+  def plist
+    <<-EOS.undent
     <?xml version="1.0" encoding="UTF-8"?>
     <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
     <plist version="1.0">
