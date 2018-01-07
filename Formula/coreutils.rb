13bc1ac6f8b4908200b61d0a2e1f3d73ae2da604
Mike McQuaid <mike@mikemcquaid.com>
2018-01-07 19:31:07 +0000

    coreutils: fix rubocop check.

--- Formula/coreutils.rb
+++ Formula/coreutils.rb
@@ -69,18 +69,15 @@ class Coreutils < Formula
     man1.install_symlink "grealpath.1" => "realpath.1"
   end
 
-  def caveats; <<~EOS
-    All commands have been installed with the prefix 'g'.
-
-    If you really need to use these commands with their normal names, you
-    can add a "gnubin" directory to your PATH from your bashrc like:
-
-        PATH="#{opt_libexec}/gnubin:$PATH"
-
-    Additionally, you can access their man pages with normal names if you add
-    the "gnuman" directory to your MANPATH from your bashrc as well:
-
-        MANPATH="#{opt_libexec}/gnuman:$MANPATH"
+  def caveats
+    <<~EOS
+      All commands have been installed with the prefix 'g'.
+       If you really need to use these commands with their normal names, you
+      can add a "gnubin" directory to your PATH from your bashrc like:
+           PATH="#{opt_libexec}/gnubin:$PATH"
+       Additionally, you can access their man pages with normal names if you add
+      the "gnuman" directory to your MANPATH from your bashrc as well:
+           MANPATH="#{opt_libexec}/gnuman:$MANPATH"
 
     EOS
   end
