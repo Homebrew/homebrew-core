8c8b0e1b60f61559633c9237741c0c147a2ca706
Mike McQuaid <mike@mikemcquaid.com>
2018-01-07 19:31:40 +0000

    selenium-server-standalone@2.45: fix rubocop check.

--- Formula/selenium-server-standalone@2.45.rb
+++ Formula/selenium-server-standalone@2.45.rb
@@ -13,33 +13,34 @@ class SeleniumServerStandaloneAT245 < Formula
 
   plist_options :manual => "selenium-server -p 4444"
 
-  def plist; <<~EOS
-    <?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-    <plist version="1.0">
-    <dict>
-      <key>Label</key>
-      <string>#{plist_name}</string>
-      <key>RunAtLoad</key>
-      <true/>
-      <key>KeepAlive</key>
-      <false/>
-      <key>ProgramArguments</key>
-      <array>
-        <string>/usr/bin/java</string>
-        <string>-jar</string>
-        <string>#{libexec}/selenium-server-standalone-2.45.0.jar</string>
-        <string>-port</string>
-        <string>4444</string>
-      </array>
-      <key>ServiceDescription</key>
-      <string>Selenium Server</string>
-      <key>StandardErrorPath</key>
-      <string>#{var}/log/selenium-error.log</string>
-      <key>StandardOutPath</key>
-      <string>#{var}/log/selenium-output.log</string>
-    </dict>
-    </plist>
+  def plist
+    <<~EOS
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+      <dict>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>RunAtLoad</key>
+        <true/>
+        <key>KeepAlive</key>
+        <false/>
+        <key>ProgramArguments</key>
+        <array>
+          <string>/usr/bin/java</string>
+          <string>-jar</string>
+          <string>#{libexec}/selenium-server-standalone-2.45.0.jar</string>
+          <string>-port</string>
+          <string>4444</string>
+        </array>
+        <key>ServiceDescription</key>
+        <string>Selenium Server</string>
+        <key>StandardErrorPath</key>
+        <string>#{var}/log/selenium-error.log</string>
+        <key>StandardOutPath</key>
+        <string>#{var}/log/selenium-output.log</string>
+      </dict>
+      </plist>
   EOS
   end
 
