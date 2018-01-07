e08141dab826b4a02265237280e9446baef061cd
Mike McQuaid <mike@mikemcquaid.com>
2018-01-07 19:31:09 +0000

    dvm: fix rubocop check.

--- Formula/dvm.rb
+++ Formula/dvm.rb
@@ -27,10 +27,11 @@ class Dvm < Formula
     end
   end
 
-  def caveats; <<~EOS
-    dvm is a shell function, and must be sourced before it can be used.
-    Add the following command to your bash profile:
-        [ -f #{opt_prefix}/dvm.sh ] && . #{opt_prefix}/dvm.sh
+  def caveats
+    <<~EOS
+      dvm is a shell function, and must be sourced before it can be used.
+      Add the following command to your bash profile:
+          [ -f #{opt_prefix}/dvm.sh ] && . #{opt_prefix}/dvm.sh
     EOS
   end
 
