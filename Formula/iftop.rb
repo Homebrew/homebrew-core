c407b767cad38d353a104a31a30c4862f2c35ccd
Mike McQuaid <mike@mikemcquaid.com>
2018-01-07 19:31:17 +0000

    iftop: fix rubocop check.

--- Formula/iftop.rb
+++ Formula/iftop.rb
@@ -22,9 +22,10 @@ class Iftop < Formula
     system "make", "install"
   end
 
-  def caveats; <<~EOS
-    iftop requires root privileges so you will need to run `sudo iftop`.
-    You should be certain that you trust any software you grant root privileges.
+  def caveats
+    <<~EOS
+      iftop requires root privileges so you will need to run `sudo iftop`.
+      You should be certain that you trust any software you grant root privileges.
     EOS
   end
 
