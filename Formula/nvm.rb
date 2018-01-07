f430d2ec1b860fdb4ea2f4207b8808076abecc3c
Mike McQuaid <mike@mikemcquaid.com>
2018-01-07 19:31:28 +0000

    nvm: fix rubocop check.

--- Formula/nvm.rb
+++ Formula/nvm.rb
@@ -12,26 +12,21 @@ class Nvm < Formula
     bash_completion.install "bash_completion" => "nvm"
   end
 
-  def caveats; <<~EOS
-    Please note that upstream has asked us to make explicit managing
-    nvm via Homebrew is unsupported by them and you should check any
-    problems against the standard nvm install method prior to reporting.
-
-    You should create NVM's working directory if it doesn't exist:
-
-      mkdir ~/.nvm
-
-    Add the following to #{shell_profile} or your desired shell
-    configuration file:
-
-      export NVM_DIR="$HOME/.nvm"
-      . "#{opt_prefix}/nvm.sh"
-
-    You can set $NVM_DIR to any location, but leaving it unchanged from
-    #{prefix} will destroy any nvm-installed Node installations
-    upon upgrade/reinstall.
-
-    Type `nvm help` for further information.
+  def caveats
+    <<~EOS
+      Please note that upstream has asked us to make explicit managing
+      nvm via Homebrew is unsupported by them and you should check any
+      problems against the standard nvm install method prior to reporting.
+       You should create NVM's working directory if it doesn't exist:
+         mkdir ~/.nvm
+       Add the following to #{shell_profile} or your desired shell
+      configuration file:
+         export NVM_DIR="$HOME/.nvm"
+        . "#{opt_prefix}/nvm.sh"
+       You can set $NVM_DIR to any location, but leaving it unchanged from
+      #{prefix} will destroy any nvm-installed Node installations
+      upon upgrade/reinstall.
+       Type `nvm help` for further information.
   EOS
   end
 
