4f9208de9848bb67edd27f221977cbdfd2fe6385
Mike McQuaid <mike@mikemcquaid.com>
2018-01-07 19:31:47 +0000

    vsftpd: fix rubocop check.

--- Formula/vsftpd.rb
+++ Formula/vsftpd.rb
@@ -60,22 +60,23 @@ class Vsftpd < Formula
 
   plist_options :startup => true, :manual => "sudo vsftpd"
 
-  def plist; <<~EOS
-    <?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-    <plist version="1.0">
-    <dict>
-      <key>Label</key>
-      <string>#{plist_name}</string>
-      <key>ProgramArguments</key>
-      <array>
-        <string>#{sbin}/vsftpd</string>
-        <string>#{etc}/vsftpd.conf</string>
-      </array>
-      <key>RunAtLoad</key>
-      <true/>
-    </dict>
-    </plist>
+  def plist
+    <<~EOS
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+      <dict>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>ProgramArguments</key>
+        <array>
+          <string>#{sbin}/vsftpd</string>
+          <string>#{etc}/vsftpd.conf</string>
+        </array>
+        <key>RunAtLoad</key>
+        <true/>
+      </dict>
+      </plist>
     EOS
   end
 
