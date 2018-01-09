dfc30fc4d7e914b987282ef18912cb9dbdb4328d
Vinyl Darkscratch <vinyldarkscratch@gmail.com>
2018-01-09 03:31:43 -0800

    pytouhou: fix Python 3 dependency

--- Formula/pytouhou.rb
+++ Formula/pytouhou.rb
@@ -15,7 +15,7 @@ class Pytouhou < Formula
 
   option "with-demo", "Install demo version of Touhou 6"
 
-  depends_on :python3
+  depends_on "python3"
   depends_on "pkg-config" => :build
   depends_on "libepoxy"
   depends_on "sdl2"
@@ -25,7 +25,7 @@ class Pytouhou < Formula
   depends_on "gtk+3" => :recommended
   if build.with? "gtk+3"
     depends_on "py3cairo" # FIXME: didn't get picked up by pygobject3 below
-    depends_on "pygobject3" => "with-python3"
+    depends_on "pygobject3"
   end
 
   resource "Cython" do
