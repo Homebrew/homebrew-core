30fe500543829152f044b4dcf141d877a13542bb
Utkarsh Tripathi <f2016894@pilani.bits-pilani.ac.in>
2018-01-13 21:32:50 +0530

    undo changes to scipy.rb

--- Formula/scipy.rb
+++ Formula/scipy.rb
@@ -14,7 +14,7 @@ class Scipy < Formula
   option "without-python", "Build without python2 support"
 
   depends_on "swig" => :build
-  depends_on "gcc"
+  depends_on :fortran
   depends_on "numpy"
   depends_on "python" => :recommended if MacOS.version <= :snow_leopard
   depends_on :python3 => :recommended
