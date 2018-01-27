3862878b36eac1beda42cefd6cdf99ffe828e066
Matthew Lyon <matthew@lyonheart.us>
2018-01-27 13:43:27 -0800

    update SHA for the .tar.gz version

--- Formula/bork.rb
+++ Formula/bork.rb
@@ -2,7 +2,7 @@ class Bork < Formula
   desc "Bash DSL for config management"
   homepage "https://github.com/mattly/bork"
   url "https://github.com/mattly/bork/archive/0.11.0.tar.gz"
-  sha256 "4d254f74fbf13846eff0a9d6ee1dc8d4b0df93faad46d847791fb7b7a56b62ab"
+  sha256 "5c9f445962a9b8a8faf7cbdec9599945630b1a21f0fa244ec9d536c5e6fc867a"
 
   head "https://github.com/mattly/bork.git"
   bottle :unneeded
