c1fee9a9af2023c85dd0c94f05ec4630a4d7a13a
Matthew Lyon <matthew@lyonheart.us>
2018-01-27 15:31:12 -0800

    Restore 'v' in version

--- Formula/bork.rb
+++ Formula/bork.rb
@@ -1,7 +1,7 @@
 class Bork < Formula
   desc "Bash DSL for config management"
   homepage "https://github.com/mattly/bork"
-  url "https://github.com/mattly/bork/archive/0.11.0.tar.gz"
+  url "https://github.com/mattly/bork/archive/v0.11.0.tar.gz"
   sha256 "5c9f445962a9b8a8faf7cbdec9599945630b1a21f0fa244ec9d536c5e6fc867a"
 
   head "https://github.com/mattly/bork.git"
