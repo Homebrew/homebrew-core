a7cbd9f7919599120ca6e49108f45dca3f3988ab
Lazarus Lazaridis <lazarus.lazaridis@gmail.com>
2018-03-14 15:19:33 +0200

    goto 1.2.2 (new formula)
    This formula provides the `goto` shell script.

---
+++ Formula/goto.rb
@@ -0,0 +1,17 @@
+class Goto < Formula
+  desc "Shell tool for navigation to aliased directories with auto-completion"
+  homepage "https://github.com/iridakos/goto"
+  url "https://github.com/iridakos/goto/archive/v1.2.2.tar.gz"
+  sha256 "611ef7b82f6798ef83db0473811740c7f1ff5a15bb4ff52a1e5841bac87ba7f5"
+
+  bottle :unneeded
+
+  def install
+    bash_completion.install "goto.sh" => "goto"
+  end
+
+  test do
+    assert_match "-F _complete_goto_bash",
+                 shell_output("source #{bash_completion}/goto && complete -p goto")
+  end
+end
