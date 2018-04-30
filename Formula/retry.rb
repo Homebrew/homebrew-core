773277f0650c8467c0461f5359452b689c90fbfa
Neville Kadwa <neville@kadwa.com>
2018-04-30 17:42:39 -0400

    audit fix

--- Formula/retry.rb
+++ Formula/retry.rb
@@ -4,9 +4,9 @@ class Retry < Formula
   url "https://github.com/kadwanev/retry/releases/download/1.0.1/retry-1.0.1.tar.gz"
   sha256 "3df0d341e7973be0752b468e5d84643559ecbeb40e959c0b2ffbc2fa42616e1c"
 
-  depends_on "gnu-getopt"
-
   bottle :unneeded
+
+  depends_on "gnu-getopt"
 
   def install
     inreplace "retry", "$IN_GETOPT_BIN", "#{HOMEBREW_PREFIX}/opt/gnu-getopt/bin/getopt"
