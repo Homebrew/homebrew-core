12ff66aab23299a9b0c43fd4903b377c2ae3c7c6
commitay <commitay@users.noreply.github.com>
2018-05-24 19:33:03 +1000

    new formula audit test

---
+++ Formula/minikube.rb
@@ -0,0 +1,17 @@
+class Minikube < Formula
+  desc "Run Kubernetes locally"
+  homepage "https://github.com/kubernetes/minikube"
+  url "https://github.com/kubernetes/minikube.git",
+      :tag => "v0.26.1",
+      :revision => "6ded2b647d1b1f62100c630bcfcc1363c631ce2d"
+  head "https://github.com/kubernetes/minikube.git"
+
+  depends_on "go" => :build
+  depends_on "go-bindata" => :build
+  depends_on "icu4c"
+  depends_on "kubernetes-cli" => :optional
+
+  def install
+    bin.install "out/minikube"
+  end
+end
