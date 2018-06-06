2e9290a11479cfbd6b23e78950dd3ddfdbbfe4ce
Miguel Mota <miguelmota2@gmail.com>
2018-06-06 11:18:21 -0700

    1.0.5

--- Formula/cointop.rb
+++ Formula/cointop.rb
@@ -1,8 +1,8 @@
 class Cointop < Formula
   desc "Interactive terminal based UI application for tracking cryptocurrencies"
   homepage "https://cointop.sh"
-  url "https://github.com/miguelmota/cointop/archive/1.0.4.tar.gz"
-  sha256 "e5c9e7ad969252498d5e1c0f6861420e3a9871df85ed7c9d4d584c1ab548e31d"
+  url "https://github.com/miguelmota/cointop/archive/1.0.5.tar.gz"
+  sha256 "2c780c3fe7fa7dd0d51ad0f0d1b1b7b7ee3f8365b9605ab73f21557e72077d78"
 
   depends_on "go" => :build
 
