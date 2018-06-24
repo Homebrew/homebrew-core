18cad52fa6f29a34983a6d46378b7d5507eeb347
ilovezfs <ilovezfs@icloud.com>
2018-06-24 16:12:18 +0000

    Update phpmyadmin.rb

--- Formula/phpmyadmin.rb
+++ Formula/phpmyadmin.rb
@@ -1,8 +1,8 @@
 class Phpmyadmin < Formula
   desc "Web interface for MySQL and MariaDB"
   homepage "https://www.phpmyadmin.net"
-  url "https://files.phpmyadmin.net/phpMyAdmin/4.8.1/phpMyAdmin-4.8.1-all-languages.tar.gz"
-  sha256 "4f7771e96a1637797e0dc710d36345d8327a612d8c08ac305018ef7185f2217d"
+  url "https://files.phpmyadmin.net/phpMyAdmin/4.8.2/phpMyAdmin-4.8.2-all-languages.tar.gz"
+  sha256 "12a2462116fa8e32866c04f48678ef077520a108f3350bcee701d0e39b865a89"
 
   bottle :unneeded
 
