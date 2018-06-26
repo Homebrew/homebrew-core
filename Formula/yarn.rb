a6f904aac4df702cfff11d94d5fd257f841bffa2
Joscha Feth <joscha@feth.com>
2018-06-26 11:54:02 +1000

    Yarn 1.8.0
    Release notes, see: https://github.com/yarnpkg/yarn/releases/tag/v1.8.0

--- Formula/yarn.rb
+++ Formula/yarn.rb
@@ -1,8 +1,8 @@
 class Yarn < Formula
   desc "JavaScript package manager"
   homepage "https://yarnpkg.com/"
-  url "https://yarnpkg.com/downloads/1.7.0/yarn-v1.7.0.tar.gz"
-  sha256 "e7720ee346b2bd7ec32b7e04517643c38648f5022c7981168321ba1636f2dca3"
+  url "https://yarnpkg.com/downloads/1.8.0/yarn-v1.8.0.tar.gz"
+  sha256 "3d8dc87cae99f7547b82026f818b3a14f0393cfa09337bb9adfb446d50a527a7"
 
   bottle :unneeded
 
