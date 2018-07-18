aec530896cba97b3e2a3dea7b23be0cf09f4956a
wrkusr <wrkusr@Ajays-MacBook-Pro.local>
2018-07-18 16:36:52 -0500

    Executed and updated the gradle@3.5.rb as per brew audit strict results

--- Formula/gradle@3.5.rb
+++ Formula/gradle@3.5.rb
@@ -5,19 +5,15 @@ class GradleAT35 < Formula
   sha256 "d84bf6b6113da081d0082bcb63bd8547824c6967fe68704d1e3a6fde822b7212"
 
   bottle :unneeded
-  
   option "with-all", "Installs Javadoc, examples, and source in addition to the binaries"
-
-
-
+  depends_on :java => "1.7+"
   def install
     rm_f Dir["bin/*.bat"]
     libexec.install %w[bin lib]
     libexec.install %w[docs media samples src] if build.with? "all"
     (bin/"gradle").write_env_script libexec/"bin/gradle", Language::Java.overridable_java_home_env
   end
-
   test do
-	assert_match version.to_s, shell_output("%w{bin}/gradle --version")
+    assert_match version.to_s, shell_output("%w{bin}/gradle --version")
   end
 end
