dfc743990bdd2215d83fa77e236c1dd40083e8d6
Fabian Schilf <f.schilf94@gmail.com>
2018-08-03 03:42:18 +0200

    add gitflow

---
+++ Formula/gitflow.rb
@@ -0,0 +1,11 @@
+class Gitflow < Formula
+    desc "CLI to create, list and remove Github repositories"
+    homepage "https://github.com/fabianschilf94/gitflow"
+    url "https://sourceforge.net/projects/gitflow/files/gitflow-1.0.0.tar.gz"
+    sha256 "337eca044c7653e876f96f8c23c184f817abf08f7d735661c9115eb4d9f8a7b6"
+    # depends_on "cmake" => :build
+  
+    def install
+      bin.install "dist/gitflow"
+    end
+  end
