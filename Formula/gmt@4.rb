0b44e618feb1c547c2359189b8647f9b85181a99
Dominyk Tiller <dominyktiller@gmail.com>
2018-08-24 02:55:21 +0100

    gmt@4: revision for hdf5

--- Formula/gmt@4.rb
+++ Formula/gmt@4.rb
@@ -5,6 +5,7 @@ class GmtAT4 < Formula
   mirror "https://fossies.org/linux/misc/GMT/gmt-4.5.18-src.tar.bz2"
   mirror "https://mirrors.ustc.edu.cn/gmt/gmt-4.5.18-src.tar.bz2"
   sha256 "27c30b516c317fed8e44efa84a0262f866521d80cfe76a61bf12952efb522b63"
+  revision 1
 
   bottle do
     sha256 "248b76386487b09b1a61088683386be7123a66f3e4212746358a557955ccfc41" => :high_sierra
