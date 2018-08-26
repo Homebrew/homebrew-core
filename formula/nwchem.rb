77a75154cc0094649393573181085bb519aeb5a6
edoapra <edoardo.apra@gmail.com>
2018-08-25 23:01:58 -0700

    added test

--- formula/nwchem.rb
+++ formula/nwchem.rb
@@ -1,11 +1,8 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Nwchem < Formula
   desc "NWChem: Open Source High-Performance Computational Chemistry"
   homepage "http://www.nwchem-sw.org"
-  url "https://github.com/nwchemgit/nwchem/releases/download/6.8.1-release/nwchem-6.8.1-release.revision-v6.8-133-ge032219-srconly.2018-06-14.tar.bz2"
-  sha256 "fd20f9ca1b410270a815e77e052ec23552f828526cd252709f798f589b2a6431"
+  url "https://github.com/nwchemgit/nwchem/releases/download/6.8.1-release/nwchem-6.8.1-release.revision-v6.8-133-ge032219-src.2018-06-14.tar.bz2"
+  sha256 "23ce8241a5977a93d8224f66433851c81a08ad58a4c551858ae031485b095ab7"
   version "6.8.1"
   depends_on "openblas"
   depends_on "gcc" # for gfortran
@@ -27,6 +24,16 @@ class Nwchem < Formula
    charmm_s #{share}/charmm_s/
    charmm_x #{share}/charmm_x/
     EOS
+      inreplace "util/util_nwchemrc.F", "/etc/nwchemrc", "#{etc}/nwchemrc"
+      inreplace "config/makefile.h" do |s|
+        s.gsub! /-mtune=native/, "-mtune=generic"
+        s.gsub! /-mfpmath=sse/, " "
+        s.gsub! /-msse3/, " "
+      end
+      system "make", "nwchem_config", "NWCHEM_MODULES=all python", "NWCHEM_TOP=#{Dir.pwd}/.."
+      system "make", "64_to_32", "NWCHEM_TOP=#{Dir.pwd}/.."
+      system "make", "NWCHEM_TARGET=MACX64", "USE_MPI=Y", "NWCHEM_TOP=#{Dir.pwd}/..", "BLASOPT=-L#{Formula["openblas"].opt_lib} -lopenblas", "BLAS_SIZE=4", "SCALAPACK=-L#{Formula["scalapack"].opt_prefix}/lib -lscalapack", "USE_64TO32=y", "PYTHONVERSION=2.7", "PYTHONHOME=/usr", "NWCHEM_LONG_PATHS=Y"
+       bin.install "../bin/MACX64/nwchem"
       (prefix/"share").mkdir
       share.install Dir["basis/libraries"]
       share.install Dir["nwpw/libraryps"]
@@ -34,24 +41,14 @@ class Nwchem < Formula
       rm Dir["#{share}/libraryps/*F"]
       rm Dir["#{share}/libraryps/*fh"]
       rm Dir["#{share}/libraryps/*ake*ile"]
-      inreplace "util/util_nwchemrc.F", "/etc/nwchemrc", "#{etc}/nwchemrc"
-      system "make", "nwchem_config", "NWCHEM_MODULES=nwdft python", "NWCHEM_TOP=#{Dir.pwd}/.."
-      system "make", "64_to_32", "NWCHEM_TOP=#{Dir.pwd}/.."
-      system "make", "NWCHEM_TARGET=MACX64", "USE_MPI=Y", "NWCHEM_TOP=#{Dir.pwd}/..", "BLASOPT=-L#{Formula["openblas"].opt_lib} -lopenblas", "BLAS_SIZE=4", "SCALAPACK=-L#{Formula["scalapack"].opt_prefix}/lib -lscalapack", "USE_64TO32=y", "PYTHONVERSION=2.7", "PYTHONHOME=/usr", "NWCHEM_LONG_PATHS=Y"
-       bin.install "../bin/MACX64/nwchem"
     end
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test nwchem`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
+    cd "QA" do
     # The installed folder is not in the path, so use the entire path to any
     # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    system "runtests.mpi.unix", "procs", "2", "dft_he2+", "prop_mep_gcube"
+    end
   end
 end
