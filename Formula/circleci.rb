6458fa4de9c285e3ab0cc12b2c5e01486f2b4b08
circleci <ops+deploy-bot@circleci.com>
2018-09-25 21:00:35 -0700

    circleci 0.1.2610

--- Formula/circleci.rb
+++ Formula/circleci.rb
@@ -3,8 +3,8 @@ class Circleci < Formula
   homepage "https://circleci.com/docs/2.0/local-cli/"
   # Updates should be pushed no more frequently than once per week.
   url "https://github.com/CircleCI-Public/circleci-cli.git",
-      :tag => "v0.1.2061",
-      :revision => "3eae4e7eaa2f09f44a8bf812a292de729d1b681d"
+      :tag => "v0.1.2610n53d93c3",
+      :revision => "53d93c3afc03864c5c98855facded89e84b1943b"
 
   bottle do
     cellar :any_skip_relocation
