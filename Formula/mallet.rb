ed450c26a2c74d259b521a1c6579c04a5e63ede4
Scott Enderle <scott.enderle@gmail.com>
2018-09-28 10:27:08 -0400

    mallet 2.0.8, separate arguments to system

--- Formula/mallet.rb
+++ Formula/mallet.rb
@@ -22,7 +22,7 @@ class Mallet < Formula
 
   test do
     resource("testdata").stage do
-      system "#{bin}/mallet import-file --input testing.tsv --keep-sequence"
+      system "#{bin}/mallet", "import-file", "--input", "testing.tsv", "--keep-sequence"
       out = shell_output("#{bin}/mallet train-topics --input text.vectors --show-topics-interval 0 --num-iterations 100 2>&1")
       assert_equal "seconds", out.split.last
     end
