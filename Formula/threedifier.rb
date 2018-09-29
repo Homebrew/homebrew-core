8d63b265c8302f37f689248d871787d6c2999bac
Ylannl <r.y.peters@tudelft.nl>
2018-09-29 14:18:18 +0200

    add blank line

--- Formula/threedifier.rb
+++ Formula/threedifier.rb
@@ -3,6 +3,7 @@ class Threedifier < Formula
   homepage "https://github.com/Homebrew/homebrew-core"
   url "https://github.com/tudelft3d/3dfier/archive/v1.0.3.tar.gz"
   sha256 "0216cdb4bbad4f6ec8a3867742129db2b365a9f92a6cb6744bb6266be4f466f5"
+  
   depends_on "cmake" => :build
   depends_on "boost"
   depends_on "cgal"
