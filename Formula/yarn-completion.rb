6485150334e5a774cb66a5ba1c102c1257ecc251
Derek Sifford <dereksifford@gmail.com>
2018-10-02 15:37:18 -0400

    fix formula formatting (i think)

--- Formula/yarn-completion.rb
+++ Formula/yarn-completion.rb
@@ -1,10 +1,9 @@
 class YarnCompletion < Formula
   desc "Bash completion for Yarn"
   homepage "https://github.com/dsifford/yarn-completion"
+  depends_on "bash-completion@2"
   url "https://github.com/dsifford/yarn-completion/archive/v0.9.0.tar.gz"
   sha256 "b28fb51d5417a78b9e18c947e87dafd602de620e80360741ff0183dcf1e9c701"
-
-  depends_on "bash-completion@2"
   
   bottle :unneeded
 
