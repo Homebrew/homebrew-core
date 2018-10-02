80201eae9d54d6440d4b5897d0b4117cdf6f212d
Jason Karns <jason@karns.name>
2018-10-02 16:19:33 -0400

    Fix audit issues

--- Formula/yarn-completion.rb
+++ Formula/yarn-completion.rb
@@ -1,10 +1,11 @@
 class YarnCompletion < Formula
   desc "Bash completion for Yarn"
   homepage "https://github.com/dsifford/yarn-completion"
-  depends_on "bash-completion@2"
   url "https://github.com/dsifford/yarn-completion/archive/v0.9.0.tar.gz"
   sha256 "b28fb51d5417a78b9e18c947e87dafd602de620e80360741ff0183dcf1e9c701"
   bottle :unneeded
+
+  depends_on "bash-completion@2"
 
   def install
     bash_completion.install "yarn-completion.bash" => "yarn"
