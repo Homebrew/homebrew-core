49443a68ff6d879f8b16542df322af97f772f915
Chongyu Zhu <i@lembacon.com>
2018-10-20 01:14:50 +0800

    Update ssh-copy-id.rb

--- Formula/ssh-copy-id.rb
+++ Formula/ssh-copy-id.rb
@@ -2,6 +2,7 @@ class SshCopyId < Formula
   desc "Add a public key to a remote machine's authorized_keys file"
   homepage "https://www.openssh.com/"
   url "https://ftp.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-7.9p1.tar.gz"
+  mirror "https://mirror.vdms.io/pub/OpenBSD/OpenSSH/portable/openssh-7.9p1.tar.gz"
   version "7.9p1"
   sha256 "6b4b3ba2253d84ed3771c8050728d597c91cfce898713beb7b64a305b6f11aad"
   head "https://github.com/openssh/openssh-portable.git"
