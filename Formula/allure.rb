03c5c3f0bd76e8bc82fdbda022959c5bd4dbabc6
Artem Eroshenko <eroshenkoam@me.com>
2018-11-05 15:01:09 +0300

    allure 2.8.0

--- Formula/allure.rb
+++ Formula/allure.rb
@@ -1,8 +1,8 @@
 class Allure < Formula
   desc "Flexible lightweight test report tool"
   homepage "https://github.com/allure-framework/allure2"
-  url "https://dl.bintray.com/qameta/generic/io/qameta/allure/allure/2.7.0/allure-2.7.0.zip"
-  sha256 "9408ff5c11abda7491431539b38a724689b2172a1fdd1b1dacca5f4ab77a402b"
+  url "https://dl.bintray.com/qameta/maven/io/qameta/allure/allure-commandline/2.8.0/allure-commandline-2.7.0.zip"
+  sha256 "e8decc12ea5a8faf687c47ebf042919c0990a8fa2979579dff7742f30deec15e"
 
   bottle :unneeded
 
