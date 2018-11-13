5e91f20f6d3fa5317bc6d2fa1df0aed9ae5b8d97
Marcel Beck <marcel@beck.im>
2018-11-13 23:39:17 +0100

    Tmuxinator: Remove head

--- Formula/tmuxinator.rb
+++ Formula/tmuxinator.rb
@@ -3,7 +3,6 @@ class Tmuxinator < Formula
   homepage "https://github.com/tmuxinator/tmuxinator"
   url "https://github.com/tmuxinator/tmuxinator/archive/v0.13.0.tar.gz"
   sha256 "48d2d83fa93a394fde310375c54425958934e8dc9e202902d5c4c7cb9146c151"
-  head "https://github.com/tmuxinator/tmuxinator.git"
 
   bottle :unneeded
 
