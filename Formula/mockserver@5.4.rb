4c690a36d4fd56122d31d2d741d4e36a4ae8109f
James Bloom <733179+jamesdbloom@users.noreply.github.com>
2018-11-18 12:28:30 +0000

    mockserver 5.4.1

---
+++ Formula/mockserver@5.4.rb
@@ -0,0 +1,45 @@
+class MockserverAT54 < Formula
+  desc "Mock HTTP server and proxy"
+  homepage "https://www.mock-server.com/"
+  url "https://oss.sonatype.org/content/repositories/releases/org/mock-server/mockserver-netty/5.4.1/mockserver-netty-5.4.1-brew-tar.tar"
+  sha256 "18e13b7b256c9c8a0ced0a8128ceca7b36b04b3512541698776f62a1ed784927"
+
+  bottle :unneeded
+
+  keg_only :versioned_formula
+
+  depends_on :java => "1.7+"
+
+  def install
+    libexec.install Dir["*"]
+    bin.install_symlink "#{libexec}/bin/run_mockserver.sh" => "mockserver"
+
+    lib.install_symlink "#{libexec}/lib" => "mockserver"
+
+    mockserver_log = var/"log/mockserver"
+    mockserver_log.mkpath
+
+    libexec.install_symlink mockserver_log => "log"
+  end
+
+  test do
+    require "socket"
+
+    server = TCPServer.new(0)
+    port = server.addr[1]
+    server.close
+
+    mockserver = fork do
+      exec "#{bin}/mockserver", "-serverPort", port.to_s
+    end
+
+    loop do
+      Utils.popen_read("curl", "-s", "http://localhost:#{port}/status", "-X", "PUT")
+      break if $CHILD_STATUS.exitstatus.zero?
+    end
+
+    system "curl", "-s", "http://localhost:#{port}/stop", "-X", "PUT"
+
+    Process.wait(mockserver)
+  end
+end
