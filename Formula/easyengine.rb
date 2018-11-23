65f520d239fd3ef8e4470b00c9e2037855949812
Mriyam Tamuli <mbtamuli@gmail.com>
2018-11-23 21:58:33 +0530

    Add more tests and remove caveats
    Signed-off-by: Mriyam Tamuli <mbtamuli@gmail.com>

--- Formula/easyengine.rb
+++ Formula/easyengine.rb
@@ -1,8 +1,8 @@
 class Easyengine < Formula
   desc "Command-line control panel to manage WordPress sites"
   homepage "https://easyengine.io/"
-  url "https://github.com/EasyEngine/easyengine/releases/download/v4.0.0/easyengine.phar"
-  sha256 "5d7f7ec95911883240717458024f0e7c69309f7bd3646353a0594e1b0900eaa6"
+  url "https://github.com/EasyEngine/easyengine/releases/download/v4.0.1/easyengine.phar"
+  sha256 "247f2a58f414cc003467538f1c60139b9fef6d41a520aeafc872e55058bad1db"
 
   bottle :unneeded
 
@@ -12,16 +12,15 @@ class Easyengine < Formula
     bin.install "easyengine.phar" => "ee"
   end
 
-  def caveats; <<~EOS
-    EasyEngine requires Docker to work.
-
-    You can install Docker using:
-
-      brew cask install docker
-  EOS
-  end
-
   test do
-    system bin/"ee cli version"
+    system bin/"ee config set locale hi_IN"
+    output = shell_output("#{bin}/ee config get locale")
+    assert_match "hi_IN", output
+
+    output = shell_output("#{bin}/ee cli version")
+    assert_match "EE 4.0.1", output
+
+    output = shell_output("#{bin}/ee cli info")
+    assert_match "Darwin", output
   end
 end
