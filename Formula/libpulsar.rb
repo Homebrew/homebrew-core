8df2b83c98672d172c1974ff253e162cee4a0f49
Matteo Merli <mmerli@apache.org>
2018-12-02 10:04:43 -0800

    Use regular "make install"

--- Formula/libpulsar.rb
+++ Formula/libpulsar.rb
@@ -8,6 +8,7 @@ class Libpulsar < Formula
   depends_on "boost-python" => :build
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
+  depends_on "python" => :build
 
   depends_on "boost"
   depends_on "jsoncpp"
@@ -17,16 +18,11 @@ class Libpulsar < Formula
   def install
     cd "pulsar-client-cpp" do
       system "cmake", ".", "-DBUILD_TESTS=OFF",
-          "-DPYTHON_INCLUDE_DIR=#{Formula["python"].include}",
           "-DBoost_INCLUDE_DIRS=#{Formula["boost"].include}",
           "-DProtobuf_INCLUDE_DIR=#{Formula["protobuf"].include}",
-          "-DProtobuf_LIBRARIES=#{Formula["protobuf"].lib}/libprotobuf.dylib"
-      system "make", "pulsarShared", "pulsarStatic"
-
-      include.install "include/pulsar"
-      lib.install "lib/libpulsar.#{version}.dylib"
-      lib.install "lib/libpulsar.dylib"
-      lib.install "lib/libpulsar.a"
+          "-DProtobuf_LIBRARIES=#{Formula["protobuf"].lib}/libprotobuf.dylib",
+          "-DCMAKE_INSTALL_PREFIX=#{prefix}"
+      system "make", "pulsarShared", "pulsarStatic", "install"
     end
   end
 
