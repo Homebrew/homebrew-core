82f9805dd63b6dd1e38cf5dd2ca6c8a3b0ad58fa
Malware Utkonos <utkonos@malwarolo.gy>
2018-12-19 19:53:07 -0500

    mongodb 4.0.5

--- Formula/mongodb.rb
+++ Formula/mongodb.rb
@@ -1,9 +1,8 @@
 class Mongodb < Formula
   desc "High-performance, schema-free, document-oriented database"
   homepage "https://www.mongodb.com/"
-  url "https://fastdl.mongodb.org/src/mongodb-src-r4.0.4.tar.gz"
-  sha256 "02baada1c5665c77c58e068ac6e9d0b11371bcd89e1467896765a5e452e6cce3"
-  revision 1
+  url "https://fastdl.mongodb.org/src/mongodb-src-r4.0.5.tar.gz"
+  sha256 "d967098fc91d105cdb0f400c8b837e5c2795c3638d7720392bc47afb1efe1c10"
 
   bottle do
     sha256 "22fcbb5b68564be444fa816f540e481628d9eb883d15a1188b0cb308e6e84bff" => :mojave
