d018d86faa8837326dd4d75d6dc460248febaf99
Avinash Mohan <avinash.mohan@genesys.com>
2018-12-21 15:09:43 +0530

    confluent-oss 5.1.0

--- Formula/confluent-oss.rb
+++ Formula/confluent-oss.rb
@@ -1,7 +1,7 @@
 class ConfluentOss < Formula
   desc "Developer-optimized distribution of Apache Kafka"
   homepage "https://www.confluent.io/product/confluent-open-source/"
-  url "https://packages.confluent.io/archive/5.1/confluent-oss-5.1.0-2.11.tar.gz"
+  url "https://packages.confluent.io/archive/5.1/confluent-5.1.0-2.11.tar.gz"
   version "5.1.0"
   
   sha256 "2b65f4ca70ffa155991530d6ea1f27b9a043b38ecf81ced85a22b527d60b0ea3"
