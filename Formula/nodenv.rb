8d151c2dcc2cbb3670422cc873cad1f015c40cbf
Jason Karns <jason@karns.name>
2018-12-23 13:26:39 -0500

    nodenv 1.2.0

--- Formula/nodenv.rb
+++ Formula/nodenv.rb
@@ -1,8 +1,8 @@
 class Nodenv < Formula
   desc "Manage multiple NodeJS versions"
   homepage "https://github.com/nodenv/nodenv"
-  url "https://github.com/nodenv/nodenv/archive/v1.1.2.tar.gz"
-  sha256 "e8cfc0816f75b8c31dfd31089c442d7aafbffe26adb3078587c9ea2048df3519"
+  url "https://github.com/nodenv/nodenv/archive/v1.2.0.tar.gz"
+  sha256 "a922ddaa9b3d7b3190a46c4db2c4550d608f752cf7a701a15957cef976508ab7"
   head "https://github.com/nodenv/nodenv.git"
 
   bottle :unneeded
