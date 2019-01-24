559a4248fbb36a09397e7e441cad70b54289d475
Chase Hutchins <syntaqx@gmail.com>
2019-01-24 06:23:44 -0700

    remove indefinite

--- Formula/serve.rb
+++ Formula/serve.rb
@@ -1,5 +1,5 @@
 class Serve < Formula
-  desc "A static http server anywhere you need one"
+  desc "Static http server anywhere you need one"
   homepage "https://github.com/syntaqx/serve"
   url "https://github.com/syntaqx/serve/releases/download/v0.2.0/serve_0.2.0_macos_x86_64.tar.gz"
   version "0.2.0"
