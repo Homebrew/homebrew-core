739790f4cd40475f4fcd12af06be36d8c3ba8184
Aayush Kapoor <xeoneux@icloud.com>
2019-01-24 11:10:00 +0530

    Test vcpkg.rb

--- Formula/vcpkg.rb
+++ Formula/vcpkg.rb
@@ -6,18 +6,19 @@ class Vcpkg < Formula
 
   bottle :unneeded
 
-  depends_on "ninja"
   depends_on "cmake"
   depends_on "gcc"
+  depends_on "ninja"
 
   def install
-  	ENV["CC"] = "/usr/local/bin/gcc-8"
-  	ENV["CXX"] = "/usr/local/bin/g++-8"
+    ENV["CC"] = "/usr/local/bin/gcc-8"
+    ENV["CXX"] = "/usr/local/bin/g++-8"
     system "./bootstrap-vcpkg.sh", "-useSystemBinaries"
+    cp_r "#{buildpath}/.", prefix
     bin.install "./vcpkg"
   end
 
   test do
-    system "true"
+    assert_match version.to_s, pipe_output("#{bin}/vcpkg version")
   end
 end
