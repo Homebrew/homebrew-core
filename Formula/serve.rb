23d081347fccf71deeb1a250770a6b1ad29d0674
Chase Hutchins <syntaqx@gmail.com>
2019-01-24 07:42:42 -0700

    installing serve from source

--- Formula/serve.rb
+++ Formula/serve.rb
@@ -1,15 +1,21 @@
 class Serve < Formula
   desc "Static http server anywhere you need one"
   homepage "https://github.com/syntaqx/serve"
-  url "https://github.com/syntaqx/serve/releases/download/v0.2.0/serve_0.2.0_macos_x86_64.tar.gz"
-  version "0.2.0"
-  sha256 "95951376c655c512578de440a2f455cfb1b3a8fc20dc34ac6e9474eba87846ba"
+  url "https://github.com/syntaqx/serve/archive/v0.2.0.tar.gz"
+  sha256 "ff4d33c2d7fc95546023f3710619098e4e3220255a2b09ec67ed6cbf57ea4d06"
+
+  depends_on "go" => :build
 
   def install
-    bin.install "serve"
+    ENV["GOPATH"] = HOMEBREW_CACHE/"go_cache"
+    (buildpath/"src/github.com/syntaqx/serve").install buildpath.children
+    cd "src/github.com/syntaqx/serve" do
+      system "go", "build", "-ldflags", "-X main.version=#{version}", "-o", bin/"serve", "./cmd/serve"
+      prefix.install_metafiles
+    end
   end
 
   test do
-    system "#{bin}/serve", "  version"
+    assert_match version.to_s, shell_output("#{bin}/serve version 2>&1")
   end
 end
