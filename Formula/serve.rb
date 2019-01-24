1bb2387f8ae95765c9fa1bcabba9f9c504c424dc
Chase Hutchins <syntaqx@gmail.com>
2019-01-24 06:10:19 -0700

    strict rules

---
+++ Formula/serve.rb
@@ -0,0 +1,15 @@
+class Serve < Formula
+    desc "A static http server anywhere you need one"
+    homepage "https://github.com/syntaqx/serve"
+    url "https://github.com/syntaqx/serve/releases/download/v0.2.0/serve_0.2.0_macos_x86_64.tar.gz"
+    version "0.2.0"
+    sha256 "95951376c655c512578de440a2f455cfb1b3a8fc20dc34ac6e9474eba87846ba"
+
+    def install
+      bin.install "serve"
+    end
+
+    test do
+      system "#{bin}/serve", "  version"
+    end
+  end
