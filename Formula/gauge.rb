4dfb4d836ecf857622fcb6b691622e95df84d940
gaugeci <gaugeci@gmail.com>
2019-01-25 03:46:54 -0500

    gauge 1.0.4

--- Formula/gauge.rb
+++ Formula/gauge.rb
@@ -1,8 +1,8 @@
 class Gauge < Formula
   desc "Test automation tool that supports executable documentation"
   homepage "https://getgauge.io"
-  url "https://github.com/getgauge/gauge/archive/v1.0.3.tar.gz"
-  sha256 "c0092f40940469853ef0810b6ae388b405b60c7a40dc5585af841652da40cee8"
+  url "https://github.com/getgauge/gauge/archive/v1.0.4.tar.gz"
+  sha256 "395408a3dc9c3db2b5c200b8722a13a60898c861633b99e6e250186adffd1370"
   head "https://github.com/getgauge/gauge.git"
 
   bottle do
