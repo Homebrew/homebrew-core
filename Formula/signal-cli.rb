9f45b83fa3e44c31f48e3fe6b549962e9b517b20
Andrew Erickson <aerickson@gmail.com>
2019-02-07 18:58:31 -0800

    making requested chages

--- Formula/signal-cli.rb
+++ Formula/signal-cli.rb
@@ -3,18 +3,24 @@ class SignalCli < Formula
   homepage "https://github.com/AsamK/signal-cli"
   url "https://github.com/AsamK/signal-cli/releases/download/v0.6.2/signal-cli-0.6.2.tar.gz"
   sha256 "d917507211b3419e271674df7f03c8189921e2e78a389479a612847ddc8f1614"
-  depends_on :java => "1.7+"
+
+  depends_on :java => "1.6+"
+  bottle :unneeded
 
   def install
-    # pattern taken from activemq
     libexec.install Dir["lib", "bin"]
     (bin/"signal-cli").write_env_script libexec/"bin/signal-cli", Language::Java.java_home_env("1.6+")
   end
 
   test do
+    # test 1: checks class loading is working and version is correct
+    output = shell_output("#{bin}/signal-cli --version")
+    assert_match "signal-cli #{version}", output
+
+    # test 2: ensure crypto is working
     begin
       io = IO.popen("#{bin}/signal-cli link", :err => [:child, :out])
-      sleep 6
+      sleep 2
     ensure
       Process.kill("SIGINT", io.pid)
       Process.wait(io.pid)
