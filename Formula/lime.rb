79c10c1e03a736472d9524ada022967c9e022e49
liam <liamschumm@icloud.com>
2019-02-19 06:38:29 -0600

    fix sha hash

--- Formula/lime.rb
+++ Formula/lime.rb
@@ -4,7 +4,7 @@ class Lime < Formula
   desc "Experimental, stack-based, purely functional programming language"
   homepage "https://limelang.xyz"
   url "https://gitlab.com/lschumm/lime/-/archive/1.0.5/lime-1.0.5.tar.gz"
-  sha256 "307fc9742e2fd94bc124e73e7bd659a0e219309bd545a25e6b8aa119a4661bd1"
+  sha256 "70ee5c0b364b1df5059c2eab54c40dae87c0f3ce26edb161058d9e625e993da0"
 
   def install
     virtualenv_install_with_resources
