0e7cd3fc9b2a207364e46e31014f930925c9736b
liam <liamschumm@icloud.com>
2019-02-19 16:45:33 -0600

    update to github .tar.gz link

--- Formula/lime.rb
+++ Formula/lime.rb
@@ -3,7 +3,7 @@ class Lime < Formula
 
   desc "Experimental, stack-based, purely functional programming language"
   homepage "https://limelang.xyz"
-  url "https://gitlab.com/lschumm/lime/-/archive/1.0.5/lime-1.0.5.tar.gz"
+  url "https://github.com/lschumm/lime/archive/1.0.5.tar.gz"
   sha256 "70ee5c0b364b1df5059c2eab54c40dae87c0f3ce26edb161058d9e625e993da0"
 
   def install
