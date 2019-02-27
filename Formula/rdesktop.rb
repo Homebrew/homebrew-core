331da7ba131fd553556fe5f6ec466c874914a4d5
FX Coudert <fxcoudert@gmail.com>
2019-02-27 19:50:09 +0100

    Update rdesktop.rb

--- Formula/rdesktop.rb
+++ Formula/rdesktop.rb
@@ -13,11 +13,6 @@ class Rdesktop < Formula
   depends_on "openssl"
   depends_on :x11
 
-  # Note: The patch below is meant to remove the reference to the
-  # undefined symbol SCARD_CTL_CODE.
-  # upstream bug report: https://sourceforge.net/p/rdesktop/bugs/352/
-  patch :DATA
-
   def install
     args = %W[
       --prefix=#{prefix}
@@ -36,26 +31,3 @@ class Rdesktop < Formula
     assert_match version.to_s, shell_output("#{bin}/rdesktop -help 2>&1", 64)
   end
 end
-
-__END__
-diff --git a/scard.c b/scard.c
-index caa0745..5521ee9 100644
---- a/scard.c
-+++ b/scard.c
-@@ -2152,7 +2152,6 @@ TS_SCardControl(STREAM in, STREAM out)
-	{
-		/* Translate to local encoding */
-		dwControlCode = (dwControlCode & 0x3ffc) >> 2;
--		dwControlCode = SCARD_CTL_CODE(dwControlCode);
-	}
-	else
-	{
-@@ -2198,7 +2197,7 @@ TS_SCardControl(STREAM in, STREAM out)
-	}
-
- #ifdef PCSCLITE_VERSION_NUMBER
--	if (dwControlCode == SCARD_CTL_CODE(3400))
-+	if (0)
-	{
-		int i;
-		SERVER_DWORD cc;
