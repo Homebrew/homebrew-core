5a3ba157828741e67b624853cca0f79d7c06d364
Robert van Engelen <ravengelen@gmail.com>
2019-03-06 10:39:31 -0500

    re-flex v1.1.1 - updated formula
    This update fixes an issue with missing header files after install.

--- Formula/re-flex.rb
+++ Formula/re-flex.rb
@@ -1,8 +1,8 @@
 class ReFlex < Formula
   desc "The regex-centric, fast and flexible scanner generator for C++"
   homepage "https://www.genivia.com/doc/reflex/html"
-  url "https://github.com/Genivia/RE-flex/archive/reflex-1.0.12.tar.gz"
-  sha256 "75a1761d9a6fd3fee6454e3b6ec954efbad76ece9b68633a07ada977af4cd300"
+  url "https://github.com/Genivia/RE-flex/archive/v1.1.1.tar.gz"
+  sha256 "1d095a39e50cbeaa717c50f860d05b79bd7ac506292e775a8896d74f20346dce"
 
   depends_on "boost"
 
