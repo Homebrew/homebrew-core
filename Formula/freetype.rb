2880f6b424d80bbdf76bb20e80baf63c1a9a5c52
Sean Molenaar <SMillerDev@users.noreply.github.com>
2019-03-21 11:03:42 +0100

    Add back the mirror

--- Formula/freetype.rb
+++ Formula/freetype.rb
@@ -2,6 +2,7 @@ class Freetype < Formula
   desc "Software library to render fonts"
   homepage "https://www.freetype.org/"
   url "https://downloads.sourceforge.net/project/freetype/freetype2/2.10.0/freetype-2.10.0.tar.bz2"
+  mirror "https://download.savannah.gnu.org/releases/freetype/freetype-2.10.0.tar.bz2"
   sha256 "fccc62928c65192fff6c98847233b28eb7ce05f12d2fea3f6cc90e8b4e5fbe06"
 
   bottle do
