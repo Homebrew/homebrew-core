be51d346b8316341f8d72785755ea930d7cf778f
Igor Kapkov <igasgeek@me.com>
2019-04-01 16:19:07 +1100

    Update libcoap.rb

--- Formula/libcoap.rb
+++ Formula/libcoap.rb
@@ -12,6 +12,7 @@ class Libcoap < Formula
     sha256 "a8b3fe01f85e8d9345dbe4ceddda24abd244f1b4055abcfef36e73b26e14ae86" => :el_capitan
   end
 
+  depends_on "asciidoc" => :build
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "doxygen" => :build
