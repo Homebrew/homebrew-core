25d548741623ab15ddc34743c7103478044dc928
Tyler Chong <tcchong@hawaii.edu>
2019-04-16 19:26:03 -1000

    hcre 1.0.0 (new formula)

--- Formula/hcre.rb
+++ Formula/hcre.rb
@@ -1,15 +1,18 @@
 class Hcre < Formula
-  desc "A brew implementation of Hashcat's rule engine"
+  desc "Brew tap of Hashcat's Rule Engine"
   homepage "https://github.com/Viltaria/homebrew-hcre"
   url "https://github.com/Viltaria/homebrew-hcre/raw/master/archive/hcre-1.0.0.tar.gz"
   sha256 "3bf42f2b407f3cb6540fba2b2168e0b585313d63e4ddd4d4665fa042003e0011"
-  version "1.0.0"
-
-  depends_on "gcc"
 
   bottle :unneeded
 
+  depends_on "gcc"
+
   def install
     bin.install "hcre"
+  end
+
+  test do
+    system "#{bin}/hcre"
   end
 end
