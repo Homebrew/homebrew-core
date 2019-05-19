9a4f6b97e684cbc2eae8cd7321e0e76b6981287b
Rui Chen <rchen@meetup.com>
2019-05-19 18:36:24 -0400

    haskell-stack 1.9.3.1

--- Formula/haskell-stack.rb
+++ Formula/haskell-stack.rb
@@ -5,9 +5,9 @@ class HaskellStack < Formula
 
   desc "The Haskell Tool Stack"
   homepage "https://haskellstack.org/"
-  url "https://github.com/commercialhaskell/stack/releases/download/v1.9.3/stack-1.9.3-sdist-1.tar.gz"
-  version "1.9.3"
-  sha256 "14e06a71bf6fafbb2d468f83c70fd4e9490395207d6530ab7b9fc056f8972a46"
+  url "https://github.com/commercialhaskell/stack/archive/v1.9.3.1.tar.gz"
+  version "1.9.3.1"
+  sha256 "f3a076692b5ba88f4c671ba91a2212a6a67b3421ec684109bf9e1c659ba4de2e"
   head "https://github.com/commercialhaskell/stack.git"
 
   bottle do
@@ -22,9 +22,9 @@ class HaskellStack < Formula
 
   # Build using a stack config that matches the default Homebrew version of GHC
   resource "stack_lts_12_yaml" do
-    url "https://raw.githubusercontent.com/commercialhaskell/stack/v1.9.3/stack-lts-12.yaml"
-    version "1.9.3"
-    sha256 "0b4fb72f7c08c96ca853e865036e743cbdc84265dd5d5c4cf5154d305cd680de"
+    url "https://raw.githubusercontent.com/commercialhaskell/stack/v1.9.3.1/stack-lts-12.yaml"
+    version "1.9.3.1"
+    sha256 "12f11d6cc4d9c802c8e0076d3836ebb01d964f9547537108176d65e5695f6f4a"
   end
 
   def install
