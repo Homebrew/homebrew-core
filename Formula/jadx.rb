02c20b1f41f47d5c50dfbcfde236bbbb571fc30d
Jia Chen <fashioncj@users.noreply.github.com>
2019-06-24 17:12:17 +0800

    fix audit error

--- Formula/jadx.rb
+++ Formula/jadx.rb
@@ -26,8 +26,8 @@ class Jadx < Formula
     else
       libexec.install Dir["*"]
     end
-    system "chmod +x #{prefix}/libexec/bin/jadx"
-    system "chmod +x #{prefix}/libexec/bin/jadx-gui"
+    system "chmod +x #{libexec}/bin/jadx"
+    system "chmod +x #{libexec}/bin/jadx-gui"
     bin.install_symlink libexec/"bin/jadx"
     bin.install_symlink libexec/"bin/jadx-gui"
   end
