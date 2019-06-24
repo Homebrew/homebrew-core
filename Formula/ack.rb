1a87b0f44f40a4832934dfabeda53a56cfbee0ea
Chongyu Zhu <i@lembacon.com>
2019-06-24 17:00:43 +0800

    ack 3.0.0

--- Formula/ack.rb
+++ Formula/ack.rb
@@ -1,8 +1,8 @@
 class Ack < Formula
   desc "Search tool like grep, but optimized for programmers"
   homepage "https://beyondgrep.com/"
-  url "https://beyondgrep.com/ack-2.28-single-file"
-  sha256 "0ab3df19513a2c71aa7901f7f522a5baf72ce69e6e0e34879979f157210734f6"
+  url "https://beyondgrep.com/ack-v3.0.0"
+  sha256 "b9e342095fabb03da0159c69b4e5f99ce458c38ca8cbeafc7d03abcfd789536e"
 
   head do
     url "https://github.com/petdance/ack2.git", :branch => "dev"
