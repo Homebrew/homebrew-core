520697c54aae3b24deb53492bbbcb6b5a4d6b7de
Victor Jacobs <victor@vjcbs.dev>
2019-07-09 15:29:18 +0200

    sorbet 0.4.4405 (new formula)
    Adds formula for the [Sorbet Ruby type checker](https://github.com/sorbet/sorbet/).

---
+++ Formula/sorbet.rb
@@ -0,0 +1,22 @@
+class Sorbet < Formula
+  desc "Ruby type checker"
+  homepage "https://sorbet.org"
+  url "https://github.com/sorbet/sorbet/archive/0.4.4405.20190708170651-4215f5eec.tar.gz"
+  version "0.4.4405"
+  sha256 "64f039938eb7cf9be3fd07fce9b9cfa993af7ca782d609f81800e56fc6dc1961"
+
+  depends_on "autoconf" => :build
+  depends_on "bazel" => :build
+  depends_on "coreutils" => :build
+  depends_on "parallel" => :build
+  depends_on :xcode => :build
+
+  def install
+    system "bazel", "build", "//main:sorbet", "--config=release-mac"
+    bin.install "bazel-bin/main/sorbet"
+  end
+
+  test do
+    system "bazel", "test", "//...", "--config=dbg"
+  end
+end
