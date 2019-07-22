6dc296fc9e66fd08d6a1191163b1e87be6388246
Igor Kapkov <igasgeek@me.com>
2019-07-22 18:04:19 +1000

    calabash 1.1.27-99

--- Formula/calabash.rb
+++ Formula/calabash.rb
@@ -1,8 +1,8 @@
 class Calabash < Formula
   desc "XProc (XML Pipeline Language) implementation"
   homepage "https://xmlcalabash.com/"
-  url "https://github.com/ndw/xmlcalabash1/releases/download/1.1.24-98/xmlcalabash-1.1.24-98.zip"
-  sha256 "e80bca50cf7e32bf4fd3ae4c2e426b781224fc726ecdb55f9251c2be0ee53a64"
+  url "https://github.com/ndw/xmlcalabash1/releases/download/1.1.27-99/xmlcalabash-1.1.27-99.zip"
+  sha256 "e5c4249acfb01d56026a91ffcda2dab3acf1abc4567efb7eb0f5870192f3bc6f"
 
   bottle :unneeded
 
