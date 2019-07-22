85f7da2ffa3c8dda1e42667942d2abbbe8a5adcb
Sha Sha Chu <shasha@pinterest.com>
2019-07-22 16:28:46 -0700

    ktlint 0.34.1

--- Formula/ktlint.rb
+++ Formula/ktlint.rb
@@ -1,8 +1,8 @@
 class Ktlint < Formula
   desc "Anti-bikeshedding Kotlin linter with built-in formatter"
   homepage "https://ktlint.github.io/"
-  url "https://github.com/pinterest/ktlint/releases/download/0.34.0/ktlint"
-  sha256 "07e23adbd6ffa552e28e7a85458a508a7ca42f4ceecde5c5957455ac524a3ed7"
+  url "https://github.com/pinterest/ktlint/releases/download/0.34.1/ktlint"
+  sha256 "c1ee90ffec9fd16237d8bf5e93d5850b41ed00c76b1bfac19acea5bb1ba45a39"
 
   bottle :unneeded
 
