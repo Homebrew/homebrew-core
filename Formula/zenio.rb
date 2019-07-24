02601d3f0d4412868ff2c0403fb0bc5e8d88f907
Nikita Eliseenko <nikita.eliseenko@zensoft.io>
2019-07-24 13:02:16 +0300

    url changed

--- Formula/zenio.rb
+++ Formula/zenio.rb
@@ -1,7 +1,7 @@
 class Zenio < Formula
   desc "CLI Zenio Tool"
   homepage "https://dev.zenio.co"
-  url "https://dev.zenio.co/cli/zenio-0.8.1.tar"
+  url "https://dev.zenio.co/cli/download/zenio-0.8.1.tar"
   sha256 "8838c70497d0d4e99365c32a7a197f90f2d3e67a8b1cab5e5a6ebac4f6d1bc92"
 
   depends_on :java => "8"
