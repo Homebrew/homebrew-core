0128a195a995cbe23b79fa2886170853bcd83f60
Avinash Mohan <avilanchee@gmail.com>
2019-08-11 10:45:49 +0530

    Confluent CLI

---
+++ Formula/confluent-cli.rb
@@ -0,0 +1,27 @@
+class ConfluentCli < Formula
+  desc "Confluent command line interface (CLI) to administer a development Confluent Platform environment"
+  homepage "https://docs.confluent.io/current/cli/index.html"
+  url "https://s3-us-west-2.amazonaws.com/confluent.cloud/confluent-cli/archives/latest/confluent_latest_darwin_amd64.tar.gz"
+  version "0.141.0"
+  sha256 "daf36a696b84c80ecaa7f9f7efd986cd228db778a68aba0b545049d228c229c9"
+
+  bottle :unneeded
+  depends_on "confluent-platform"
+
+  def install
+    bin.install "confluent"
+  end
+
+  def caveats; <<~EOS
+    Pass --path /usr/local/bin flag to the command confluent local
+      or 
+    set environment variable CONFLUENT_HOME to /usr/local/bin 
+    export CONFLUENT_HOME = /usr/local/bin
+  EOS
+  end
+
+  test do
+    system "#{bin}/confluent", "version"
+    assert_match "Confluent CLI", shell_output("#{bin}/confluent version")
+  end
+end
