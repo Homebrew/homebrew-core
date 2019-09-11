8c7b054c5353af3256bc570265f4c953d6179ad3
Vitor Oliveira <vbrazo@gmail.com>
2019-09-11 03:28:38 -0700

    Separate system commands

--- Formula/vlang.rb
+++ Formula/vlang.rb
@@ -5,7 +5,8 @@ class Vlang < Formula
   sha256 "3f3407a78aca7fc3b42a3fc1f1d2b9724c1e4c71fbd5d37ff12976cd2305cec1"
 
   def install
-    system "make", "v symlink"
+    system "make"
+    system "v symlink"
   end
 
   test do
