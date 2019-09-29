90eabc451cfb1af3a065736605defe666840dcd7
大师兄 <616782041@qq.com>
2019-09-29 21:01:30 +0800

    fvm@1.0.2

---
+++ Formula/fvm.rb
@@ -0,0 +1,36 @@
+class Fvm < Formula
+  desc "Flutter SDK versions Manager"
+  homepage "https://github.com/xinfeng-tech/fvm"
+  url "https://github.com/xinfeng-tech/fvm/archive/v1.0.2.tar.gz"
+  sha256 "5d919fd900f318b657eeb7fc31e9117828d4d3cb12fcfa24c5f5ee2014b56402"
+  head "https://github.com/xinfeng-tech/fvm.git"
+
+  bottle :unneeded
+
+  def install
+    prefix.install "fvm.sh", "init.sh"
+  end
+
+  def caveats; <<~EOS
+    You should create FVM's working directory if it doesn't exist:
+
+      mkdir ~/.fvm
+
+    Add the following to #{shell_profile} or your desired shell
+    configuration file:
+
+      export FVM_DIR="$HOME/.fvm"
+      \. "#{opt_prefix}/init.sh"  # This loads fvm
+
+    You can set $FVM_DIR to any location.
+
+    Type `fvm help` for further information.
+  EOS
+  end
+
+  test do
+    output = pipe_output("#{prefix}/init.sh 2>&1")
+    assert_no_match /No such file or directory/, output
+    assert_no_match /fvm: command not found/, output
+  end
+end
