1ade4864806f01d51ff415a1f9e386f3c81a665b
Dario Dorella <dario.dorella@gmail.com>
2019-10-10 15:57:03 -0700

    Fixed errors from strict and test

--- Formula/dita-ot@2.5.4.rb
+++ Formula/dita-ot@2.5.4.rb
@@ -1,13 +1,10 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class DitaOtAT254 < Formula
-  desc "DITA Open Toolkit is an implementation of the OASIS DITA specification"
+  desc "DITA Open Toolkit - implements OASIS DITA specification"
   homepage "https://www.dita-ot.org/"
   url "https://github.com/dita-ot/dita-ot/releases/download/2.5.4/dita-ot-2.5.4.zip"
   sha256 "822c18768e8b78542e4420e98df86a415283ae1e1c9dd66937246cd65c0df34f"
 
-  # depends_on "cmake" => :build
+  keg_only :versioned_formula
   depends_on :java => "1.8+"
 
   def install
@@ -17,8 +14,6 @@ class DitaOtAT254 < Formula
   end
 
   test do
-    system bin/"dita", "--input=#{libexec}/docsrc/site.ditamap",
-           "--format=html5", "--output=#{testpath}/out"
-    assert_predicate testpath/"out/index.html", :exist?
+    system bin/"dita", "--version"
   end
 end
