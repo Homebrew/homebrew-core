344c6ebf172f8f3c2329edb071de24f6b50a4832
Kenneth Endfinger <kaendfinger@gmail.com>
2019-10-11 12:48:02 -0500

    iproute2mac: catalina support

--- Formula/iproute2mac.rb
+++ Formula/iproute2mac.rb
@@ -1,12 +1,17 @@
 class Iproute2mac < Formula
   desc "CLI wrapper for basic network utilities on macOS - ip command"
-  homepage "https://github.com/brona/iproute2mac"
-  url "https://github.com/brona/iproute2mac/releases/download/v1.2.2/iproute2mac-1.2.2.tar.gz"
-  sha256 "37ea67da5bbcac5977877c4a3a1ea8c6149785d44c65ffdaf60f9e2ba689def3"
+  homepage "https://github.com/SpinlockLabs/iproute2mac"
+  url "https://github.com/SpinlockLabs/iproute2mac/releases/download/v1.2.4/iproute2mac-1.2.4.tar.gz"
+  sha256 "ae15aa4eb81be9cdec56e0005d806e8a1d2f9f2a91fafeb1eff67fe23e93e1fd"
+  head "https://github.com/SpinlockLabs/iproute2mac.git"
 
   bottle :unneeded
 
+  depends_on "python"
+
   def install
+    inreplace "src/ip.py", %r{^#!/usr/bin/python3$},
+                            "#!/usr/bin/env python3"
     bin.install "src/ip.py" => "ip"
   end
 
