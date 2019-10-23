42def48e604db454f1b48d0be3e4a53147eda98a
Alois Schloegl <alois.schloegl@ist.ac.at>
2019-10-23 17:26:08 +0200

    Address reviewers comments to 1st pull request: * remove template comments in the `test` block. * remove commented out `version` attribute  commented out. * point to official `sigviewer` in the homepage.

--- Formula/libb64.rb
+++ Formula/libb64.rb
@@ -15,15 +15,6 @@ class Libb64 < Formula
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test libb64`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
     system "#{bin}/base64", "-e", "/dev/null", "/dev/null"
   end
 end
