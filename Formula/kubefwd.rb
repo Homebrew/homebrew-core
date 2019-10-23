01d1f0ad3359475e235a500b78825e6eada65a6b
karancode <karan.thanvi@paypay-corp.co.jp>
2019-10-23 20:54:58 +0900

    kubefwd v1.9.2
    Remove . from the end of description
    Fix the url to the source url

--- Formula/kubefwd.rb
+++ Formula/kubefwd.rb
@@ -1,11 +1,11 @@
 class Kubefwd < Formula
-    desc "Bulk port forwarding Kubernetes services for local development."
+    desc "Bulk port forwarding Kubernetes services for local development"
     homepage "https://github.com/txn2/kubefwd"
-    url "https://github.com/txn2/kubefwd/releases/download/v1.9.2/kubefwd_macOS_amd64.tar.gz"
+    url "https://github.com/txn2/kubefwd.git"
     version "1.9.2"
     sha256 "3f49349f7de2c8f60ae4d1248be4f936c9082d2b23cf9fa093caef20a60685d4"
   
-    depends_on "go@1.11.5" => :build
+    depends_on "go@1.11" => :build
   
     def install
       bin.install "kubefwd"
