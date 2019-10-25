3a353ea0d25b8574e56c987baaae4a31a0d1fd5f
Martin <mycha0@hotmail.com>
2019-10-25 11:32:16 +0800

    elm 0.19.1

--- Formula/elm.rb
+++ Formula/elm.rb
@@ -1,33 +1,16 @@
-require "language/haskell"
+require 'formula'
 
 class Elm < Formula
-  include Language::Haskell::Cabal
-
   desc "Functional programming language for building browser-based GUIs"
   homepage "https://elm-lang.org"
-  url "https://github.com/elm/compiler/archive/0.19.0.tar.gz"
-  sha256 "494df33724224307d6e2b4d0b342448cc927901483384ee4f8cfee2cb38e993c"
+  url "https://github.com/elm/compiler/releases/download/0.19.1/binary-for-mac-64-bit.gz"
+  version "0.19.1"
+  sha256 "05289f0e3d4f30033487c05e689964c3bb17c0c48012510dbef1df43868545d1"
 
-  bottle do
-    cellar :any_skip_relocation
-    sha256 "5934d1b214fd2449b1a0222ad5c7b97b7309a21c97078634d6b32b3191dddc45" => :mojave
-    sha256 "4655392e3e985c90f9e23b03438a8587475b48c1e1b0dabc2d74358f1b06daef" => :high_sierra
-    sha256 "e0e45eb3388efe4a8147a65121d067ffad875952146c11519e8e564095de139b" => :sierra
-    sha256 "e2a5f47750fb6a487e5220a3ac234b209acdb7edcde7e42c2ebe2507f8a3a5cf" => :el_capitan
-  end
-
-  depends_on "cabal-install" => :build
-  depends_on "ghc@8.2" => :build
+  bottle :unneeded
 
   def install
-    # elm-compiler needs to be staged in a subdirectory for the build process to succeed
-    (buildpath/"elm-compiler").install Dir["*"]
-
-    cabal_sandbox do
-      cabal_sandbox_add_source "elm-compiler"
-      cabal_install "--only-dependencies", "elm"
-      cabal_install "--prefix=#{prefix}", "elm"
-    end
+    bin.install "elm"
   end
 
   test do
@@ -39,7 +22,7 @@ class Elm < Formula
         "source-directories": [
                   "."
         ],
-        "elm-version": "0.19.0",
+        "elm-version": "0.19.1",
         "dependencies": {
                 "direct": {
                     "elm/browser": "1.0.0",
