97fe95f0509d8f7d15d57fa3f68d5422226ebd89
human-bean <misterplow@yandex.ru>
2019-10-26 19:04:58 +0200

    replaced 'depends_on' with 'uses_from_macos'; removed bottle do block

--- Formula/mpv.rb
+++ Formula/mpv.rb
@@ -5,22 +5,16 @@ class Mpv < Formula
   sha256 "33a1bcb7e74ff17f070e754c15c52228cf44f2cefbfd8f34886ae81df214ca35"
   head "https://github.com/mpv-player/mpv.git"
 
-  bottle do
-    sha256 "a91d2f0d616a23d37308c5a0c1f4902b07eec44f2eb6619c285044d3e4bb0124" => :mojave
-    sha256 "27b27bc1bfe887f696b5c625dc5ac5dab5806a02cfa6104be1214e1eb6d3ec53" => :high_sierra
-    sha256 "61471c7206414f25b4c23da82b239197000d4d94104fdd51e0893e07d44b8737" => :sierra
-  end
-
   depends_on "docutils" => :build
   depends_on "pkg-config" => :build
   depends_on "python" => :build
 
   depends_on "ffmpeg"
   depends_on "jpeg"
-  depends_on "libarchive"
   depends_on "libass"
   depends_on "little-cms2"
   depends_on "lua@5.1"
+  uses_from_macos "libarchive"
 
   depends_on "mujs"
   depends_on "uchardet"
