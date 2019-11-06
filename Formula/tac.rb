3ac68d5f4ecd1ba83845eb8796105028c0a6873f
WestleyK <westley@sylabs.io>
2019-11-05 17:55:37 -0800

    Add tac Formula (new formula)
    This PR adds a new formula `tac`, since macOS dose not include this for
    some reason, it will be really nice to have this here!
    
    This is a quick little tac program I wrote up, should work as a
    substitute for your Linux tac command.
    
    _I'm very new to brew, (and I'm liking it!), never used ruby before,
    hopfully I did everything correctly..._

---
+++ Formula/tac.rb
@@ -0,0 +1,18 @@
+class Tac < Formula
+  desc "tac command for macOS (linux too)"
+  homepage "https://github.com/WestleyR/tac"
+  url "https://github.com/WestleyR/tac/archive/v1.0.1.tar.gz"
+  sha256 "e2458464709a0b027bb7834fe6362fb0ee25ac9bbe938050c1d9fbe0d9846387"
+
+  # depends_on "cmake" => :build
+
+  def install
+    system "make", "install", "PREFIX=#{prefix}"
+  end
+
+  test do
+    system "touch", "test-file"
+    system "#{bin}/tac", "test-file"
+  end
+end
+
