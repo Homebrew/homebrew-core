40a5ef94608c7af45a9119c1a6a05a1058472519
vikrantrathore <admin@vyomtech.com>
2019-11-07 15:01:50 +0800

    Updated to haskell-stack, missed it earlier
    Updated to haskell-stack.

--- Formula/elm.rb
+++ Formula/elm.rb
@@ -17,7 +17,7 @@ class Elm < Formula
   end
 
   depends_on "cabal-install" => :build
-  depends_on "ghc@8.6" => :build
+  depends_on "haskell-stack" => :build
 
   def install
     # elm-compiler needs to be staged in a subdirectory for the build process to succeed
