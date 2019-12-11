3d114162710d164bd043d4dc632319b63d5a8cf1
Zach Auten <zachauten@gmail.com>
2019-12-10 21:28:36 -0500

    gegl 0.4.18

--- Formula/gegl.rb
+++ Formula/gegl.rb
@@ -1,9 +1,8 @@
 class Gegl < Formula
   desc "Graph based image processing framework"
   homepage "http://www.gegl.org/"
-  url "https://download.gimp.org/pub/gegl/0.4/gegl-0.4.16.tar.bz2"
-  sha256 "0112df690301d9eb993cc48965fc71b7751c9021a4f4ee08fcae366c326b5e5a"
-  revision 2
+  url "https://download.gimp.org/pub/gegl/0.4/gegl-0.4.18.tar.bz2"
+  sha256 "f0269e04132ebc8ae407a0bcae34ff81db4327e268af2f82839aaf19a23442c6"
 
   bottle do
     sha256 "c85c022cbec147dc1c206c6efaaab0ad1cc9db52b90706a3e131b6918eed7f89" => :catalina
