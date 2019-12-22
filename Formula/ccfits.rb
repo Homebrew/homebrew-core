2c501c856b09b6848b9bcfc2c979c303bc55622d
chenxu <chenxu@mail.ustc.edu.cn>
2019-12-22 16:35:06 +0800

    merge ccfit from brewsci/science/ccfits add cnpy [a c++ io of numpy]

---
+++ Formula/ccfits.rb
@@ -0,0 +1,18 @@
+class Ccfits < Formula
+  desc "c++ fits api"
+  homepage "https://heasarc.gsfc.nasa.gov/fitsio/CCfits/"
+  url "https://heasarc.gsfc.nasa.gov/fitsio/CCfits/CCfits-2.5.tar.gz"
+  sha256 "938ecd25239e65f519b8d2b50702416edc723de5f0a5387cceea8c4004a44740"
+
+  depends_on "cfitsio"
+
+  def install
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make"
+    system "make", "check" if build.with? "check"
+    system "make", "install"
+  end
+end
