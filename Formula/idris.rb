70fbccd2b0a3fd651d359f7fdf71c7e3eef11c4d
Graham Ollis <plicease@cpan.org>
2020-01-10 15:47:40 -0700

    idris: revision bump for libffi 3.3

--- Formula/idris.rb
+++ Formula/idris.rb
@@ -7,6 +7,7 @@ class Idris < Formula
   homepage "https://www.idris-lang.org/"
   url "https://github.com/idris-lang/Idris-dev/archive/v1.3.2.tar.gz"
   sha256 "48429b8ed80980f9a4d38c29e51fcbb51ea511640806cd3cb7752bfdbb4644d2"
+  revision 1
   head "https://github.com/idris-lang/Idris-dev.git"
 
   bottle do
