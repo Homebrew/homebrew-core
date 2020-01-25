a68e1683ee8f4c4845cb529504736d1eda926ab3
Okke Timm <okke@timm.sh>
2020-01-25 11:38:10 +0100

    Removed version due to brew audit complaining it is redundant

--- Formula/testssl.rb
+++ Formula/testssl.rb
@@ -2,7 +2,6 @@ class Testssl < Formula
   desc "Tool which checks for the support of TLS/SSL ciphers and flaws"
   homepage "https://testssl.sh/"
   url "https://github.com/drwetter/testssl.sh/archive/3.0.tar.gz"
-  version "3.0"
   sha256 "ab3c9a000f0f6703e4fc94821e06f531de6d2799322bf534188ebf766365a9c1"
   head "https://github.com/drwetter/testssl.sh.git", :branch => "3.1dev"
 
