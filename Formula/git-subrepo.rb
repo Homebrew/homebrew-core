bb7886ccf4dde2da8a9d8ddcc56bc4affaae5b5c
Alexandr Burdiyan <burdiyan@gmail.com>
2020-01-31 11:12:18 +0100

    Update git-subrepo

--- Formula/git-subrepo.rb
+++ Formula/git-subrepo.rb
@@ -1,8 +1,8 @@
 class GitSubrepo < Formula
   desc "Git Submodule Alternative"
   homepage "https://github.com/ingydotnet/git-subrepo"
-  url "https://github.com/ingydotnet/git-subrepo/archive/0.4.0.tar.gz"
-  sha256 "e60243efeebd9ae195559400220366e7e04718123481b9da38344e75bab71d21"
+  url "https://github.com/ingydotnet/git-subrepo/archive/0.4.1.tar.gz"
+  sha256 "64cc2490c54fe1e5396bb14f6bbf0aa8378085f3b8847fd8ed171e5ddd886065"
   head "https://github.com/ingydotnet/git-subrepo.git"
 
   bottle do
