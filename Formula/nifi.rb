d5bde204bc2b80ae1181586613f6e136563e4486
Markus Reiter <me@reitermark.us>
2020-02-04 11:06:36 +0100

    nifi: depend on openjdk

--- Formula/nifi.rb
+++ Formula/nifi.rb
@@ -3,18 +3,18 @@ class Nifi < Formula
   homepage "https://nifi.apache.org"
   url "http://mirror.cc.columbia.edu/pub/software/apache/nifi/1.11.1/nifi-1.11.1-bin.tar.gz"
   sha256 "b0b35a9db0e6c8c299ca174e668acf8389488976b1674f08771f9361d884c5f3"
+  revision 1
 
   bottle :unneeded
 
-  depends_on :java => "1.8+"
+  depends_on "openjdk"
 
   def install
     libexec.install Dir["*"]
 
-    ENV["NIFI_HOME"] = libexec
-
-    bin.install libexec/"bin/nifi.sh" => "nifi"
-    bin.env_script_all_files libexec/"bin/", :NIFI_HOME => libexec
+    (bin/"nifi").write_env_script libexec/"bin/nifi.sh",
+                                  :NIFI_HOME => libexec,
+                                  :JAVA_HOME => Formula["openjdk"].opt_prefix
   end
 
   test do
