eb9ba9ad106be8ea3a08858155abf284ad0eedfa
chenrui <rui@meetup.com>
2020-02-15 13:32:31 -0500

    Update storm.rb

--- Formula/storm.rb
+++ Formula/storm.rb
@@ -2,7 +2,7 @@ class Storm < Formula
   desc "Distributed realtime computation system to process data streams"
   homepage "https://storm.apache.org"
   url "https://www.apache.org/dyn/closer.cgi?path=storm/apache-storm-2.1.0/apache-storm-2.1.0.tar.gz"
-  sha256 "fe2b31b62dc3457dcb776a012f22cb884da41e7381274b4f83a3f412e862c963"
+  sha256 "e279a495dda42af7d9051543989f74a1435a5bda53e795a1de4a1def32027fc4"
 
   bottle :unneeded
 
