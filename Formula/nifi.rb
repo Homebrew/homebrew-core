9b9d561d3cb73d57927ffd16c6356c255f728319
Markus Reiter <me@reitermark.us>
2020-02-20 00:52:20 +0100

    nifi: use apache mirror selector
    Co-Authored-By: Alexander Bayandin <a.bayandin@gmail.com>

--- Formula/nifi.rb
+++ Formula/nifi.rb
@@ -1,7 +1,8 @@
 class Nifi < Formula
   desc "Easy to use, powerful, and reliable system to process and distribute data"
   homepage "https://nifi.apache.org"
-  url "http://mirror.cc.columbia.edu/pub/software/apache/nifi/1.11.1/nifi-1.11.1-bin.tar.gz"
+  url "https://www.apache.org/dyn/closer.lua?path=nifi/1.11.1/nifi-1.11.1-bin.tar.gz"
+  mirror "https://archive.apache.org/dist/nifi/1.11.1/nifi-1.11.1-bin.tar.gz"
   sha256 "b0b35a9db0e6c8c299ca174e668acf8389488976b1674f08771f9361d884c5f3"
   revision 1
 
