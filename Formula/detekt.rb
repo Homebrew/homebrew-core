1424022034aa77f5f932033063855935e2cab6e3
wasanx25 <wasanx25@gmail.com>
2020-02-21 19:20:52 +0900

    Upgrade 1.3.0 -> 1.5.1

--- Formula/detekt.rb
+++ Formula/detekt.rb
@@ -1,8 +1,8 @@
 class Detekt < Formula
   desc "Static code analysis for Kotlin"
   homepage "https://github.com/arturbosch/detekt"
-  url "https://jcenter.bintray.com/io/gitlab/arturbosch/detekt/detekt-cli/1.3.0/detekt-cli-1.3.0-all.jar"
-  sha256 "610b0211b909d8e79c1b623119e1d94cb833a8e98b32e5a0e991fa2d214992f6"
+  url "https://jcenter.bintray.com/io/gitlab/arturbosch/detekt/detekt-cli/1.5.1/detekt-cli-1.5.1-all.jar"
+  sha256 "7bfff3b000cc798f44355ef29499f05625f72250c667c56aa3d571b0bb9567d7"
   revision 1
 
   bottle :unneeded
