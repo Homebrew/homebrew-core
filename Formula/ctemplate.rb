e18d6f8e567b904c8e67ca60ad9cbb6ee4550f2c
Rui Chen <chenrui333@gmail.com>
2020-03-09 22:46:42 -0400

    ctemplate 2.4

--- Formula/ctemplate.rb
+++ Formula/ctemplate.rb
@@ -1,8 +1,8 @@
 class Ctemplate < Formula
   desc "Template language for C++"
   homepage "https://github.com/olafvdspek/ctemplate"
-  url "https://github.com/OlafvdSpek/ctemplate/archive/ctemplate-2.3.tar.gz"
-  sha256 "99e5cb6d3f8407d5b1ffef96b1d59ce3981cda3492814e5ef820684ebb782556"
+  url "https://github.com/OlafvdSpek/ctemplate/archive/ctemplate-2.4.tar.gz"
+  sha256 "ccc4105b3dc51c82b0f194499979be22d5a14504f741115be155bd991ee93cfa"
   head "https://github.com/olafvdspek/ctemplate.git"
 
   bottle do
