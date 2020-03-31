2bcae276ade6ad8f5b2495833f26286f66d85d52
EricFromCanada <enk3@outlook.com>
2020-03-31 10:41:20 -0400

    spice-gtk 0.38

--- Formula/spice-gtk.rb
+++ Formula/spice-gtk.rb
@@ -1,9 +1,8 @@
 class SpiceGtk < Formula
   desc "GTK client/libraries for SPICE"
   homepage "https://www.spice-space.org"
-  url "https://www.spice-space.org/download/gtk/spice-gtk-0.37.tar.bz2"
-  sha256 "1f28b706472ad391cda79a93fd7b4c7a03e84b88fc46ddb35dddbe323c923bb7"
-  revision 3
+  url "https://www.spice-space.org/download/gtk/spice-gtk-0.38.tar.xz"
+  sha256 "5ae974731baf2b41316d4f0b3ae0c2e47f00bff91a5a617e189cd3dedcd96d8e"
 
   bottle do
     sha256 "c1ff4c17afdcb6dbd3e6dcaa35ef019b2ead8ee11828a289fb0ac9fd15df7139" => :catalina
