c67773639d912a7d94b6d591d054807df6349507
homebrew-updater <bepsvpt/homebrew-updater>
2020-04-10 18:00:08 +0800

    composer 1.10.5

--- Formula/composer.rb
+++ Formula/composer.rb
@@ -1,8 +1,8 @@
 class Composer < Formula
   desc "Dependency Manager for PHP"
   homepage "https://getcomposer.org/"
-  url "https://getcomposer.org/download/1.10.1/composer.phar"
-  sha256 "1831f266580f221189dc04d4b58d7fc50c934ffc3a0eca89ecb4a53aa44867e2"
+  url "https://getcomposer.org/download/1.10.5/composer.phar"
+  sha256 "d5f3fddd0be28a5fc9bf2634a06f51bc9bd581fabda93fee7ca8ca781ae43129"
 
   bottle :unneeded
 
