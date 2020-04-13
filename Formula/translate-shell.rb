64688294b6a8798468999b2abc92827fa2ef2c6e
Michka Popoff <michkapopoff@gmail.com>
2020-04-13 11:43:39 +0200

    translate-shell: depend on util-linux on linux

--- Formula/translate-shell.rb
+++ Formula/translate-shell.rb
@@ -17,6 +17,10 @@ class TranslateShell < Formula
   depends_on "gawk"
   depends_on "rlwrap"
 
+  on_linux do
+    depends_on "util-linux"
+  end
+
   def install
     system "make"
     bin.install "build/trans"
