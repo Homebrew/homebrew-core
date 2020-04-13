fef640d66172c60b2dec9fd4108a6e0c5f1aab66
Rui Chen <chenrui333@gmail.com>
2020-04-12 20:26:32 -0400

    libxc 5.0.0

--- Formula/libxc.rb
+++ Formula/libxc.rb
@@ -1,12 +1,11 @@
 class Libxc < Formula
   desc "Library of exchange and correlation functionals for codes"
   homepage "https://tddft.org/programs/libxc/"
-  url "https://gitlab.com/libxc/libxc/-/archive/4.3.4/libxc-4.3.4.tar.bz2"
-  sha256 "0efe8b33d151de8787e33c4ba8e2161ffb9da978753f3bd12c5c0a018e7d3ef5"
+  url "https://gitlab.com/libxc/libxc/-/archive/5.0.0/libxc-5.0.0.tar.bz2"
+  sha256 "b4206940db57ba4ddb9cb635fd852914c8dd045cb07bab32427013e3ab294f4f"
 
   bottle do
     cellar :any
-    rebuild 1
     sha256 "072df8c5f3e00bf045f4e062993ecb08e324872daf3503b8f2bacef866a3de14" => :catalina
     sha256 "7727321091982306464ad87e055074b2675d83ee3c8416cf6b0681a4db31bc85" => :mojave
     sha256 "c8f820ca8dce64220c8c1e60002a13c4ed21d5decfd6b1189b6d286ca5c47ab4" => :high_sierra
