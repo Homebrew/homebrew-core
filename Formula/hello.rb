2a6299ecea384f3db947a22d334b37fb89048871
Jonathan Chang <jchang641@gmail.com>
2020-04-14 18:24:31 +1000

    drop! hello: introduce deliberate error

--- Formula/hello.rb
+++ Formula/hello.rb
@@ -1,8 +1,8 @@
 class Hello < Formula
   desc "Program providing model for GNU coding standards and practices"
   homepage "https://www.gnu.org/software/hello/"
-  url "https://ftp.gnu.org/gnu/hello/hello-2.10.tar.gz"
   sha256 "31e066137a962676e89f69d1b65382de95a7ef7d914b8cb956f41ea72e0f516b"
+  url "https://ftp.gnu.org/gnu/hello/hello-2.10.tar.gz"
 
   bottle do
     cellar :any_skip_relocation
