d3e57af17b2bf5d0a5e67b1712f0f01fe45c03fa
Alexander Bayandin <a.bayandin@gmail.com>
2020-04-17 12:21:28 +0100

    homeworlds: revision bump for wxmac

--- Formula/homeworlds.rb
+++ Formula/homeworlds.rb
@@ -3,6 +3,7 @@ class Homeworlds < Formula
   homepage "https://github.com/Quuxplusone/Homeworlds/"
   url "https://github.com/Quuxplusone/Homeworlds.git",
     :revision => "917cd7e7e6d0a5cdfcc56cd69b41e3e80b671cde"
+  revision 1
   version "20141022"
 
   bottle do
