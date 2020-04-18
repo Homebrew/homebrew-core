3f45fc5ad7adaedda2b52a7d6a9540845de59310
Nick <nick@manasys.co.nz>
2020-04-19 11:35:38 +1200

    Added a basic test using "confluent local start" Set CONFLUENT_HOME env var

--- Formula/confluent-platform.rb
+++ Formula/confluent-platform.rb
@@ -21,5 +21,9 @@ class ConfluentPlatform < Formula
 
   test do
     assert_match version.to_s, shell_output("#{bin}/kafka-broker-api-versions --version")
+
+    ENV["CONFLUENT_HOME"] = "/usr/local/Cellar/confluent-platform/5.4.1/)
+    shell_output("#{bin}/confluent local start")
+    assert_equal "0\n", shell_output("echo $?") 
   end
 end
