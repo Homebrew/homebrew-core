42b649c9f3b9ccd9623b2cd3320398ff37018246
Persi.Liao <xiangchu.liao@gmail.com>
2020-04-19 09:50:26 +0800

    delete comment

--- Formula/composer-switcher.rb
+++ Formula/composer-switcher.rb
@@ -1,6 +1,3 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class ComposerSwitcher < Formula
   desc "Composer Mirror Repository Switcher"
   homepage "https://www.github.com/persiliao/composer-switcher"
