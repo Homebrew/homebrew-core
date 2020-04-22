21f17a8a3ddef6d149f59d1a230477577d300052
Pavel Zhovner <pavel@zhovner.com>
2020-04-23 01:34:01 +0300

    Update hcxtools.rb
    fix warnings

--- Formula/hcxtools.rb
+++ Formula/hcxtools.rb
@@ -1,41 +1,37 @@
 class Hcxtools < Formula
-  desc "Portable solution for capturing wlan traffic and conversion to hashcat formats (recommended by hashcat) and to John the Ripper formats. hcx: h = hash, c = capture, convert and calculate candidates, x = different hashtypes"
-  homepage "https://github.com/ZerBea/hcxtools" 
+  desc "Utils for conversion of cap/pcap/pcapng WiFi dump files"
+  homepage "https://github.com/ZerBea/hcxtools"
   url "https://github.com/ZerBea/hcxtools/archive/6.0.2.tar.gz"
   sha256 "963029bf00d3a98f2caa934d5765a91e29f5d1b0e0036a4c53328b5cf9c3c003"
-  
-
-  head do
-    url "https://github.com/ZerBea/hcxtools.git"
-  end
+  head "https://github.com/ZerBea/hcxtools.git"
 
   depends_on "openssl@1.1"
-
 
   def install
     ENV.prepend "CPPFLAGS", "-I#{Formula["openssl@1.1"].opt_include}"
     ENV.prepend "LDFLAGS", "-L#{Formula["openssl@1.1"].opt_lib}"
 
     system "make"
+
     bin.install "hcxessidtool",
-                "hcxhash2cap",
-                "hcxhashcattool",
-                "hcxhashtool",
-                "hcxmactool",
-                "hcxpcapngtool",
-                "hcxpcaptool",
-                "hcxpmkidtool",
-                "hcxpsktool",
-                "hcxwltool",
-                "whoismac",
-                "wlancap2wpasec",
-                "wlanhcx2john",
-                "wlanhcx2ssid",
-                "wlanhcxcat",
-                "wlanhcxinfo",
-                "wlanjohn2hcx",
-                "wlanpmk2hcx",
-                "wlanwkp2hcx"
+      "hcxhash2cap",
+      "hcxhashcattool",
+      "hcxhashtool",
+      "hcxmactool",
+      "hcxpcapngtool",
+      "hcxpcaptool",
+      "hcxpmkidtool",
+      "hcxpsktool",
+      "hcxwltool",
+      "whoismac",
+      "wlancap2wpasec",
+      "wlanhcx2john",
+      "wlanhcx2ssid",
+      "wlanhcxcat",
+      "wlanhcxinfo",
+      "wlanjohn2hcx",
+      "wlanpmk2hcx",
+      "wlanwkp2hcx"
   end
 
   test do
