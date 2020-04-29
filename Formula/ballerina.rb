a752ce2597fc6d789536f0f16c70731af99f4663
Nicolae Vlădescu <nicolaevladescu@users.noreply.github.com>
2020-04-29 16:05:30 +0300

    Fix sha256 for ballerina

--- Formula/ballerina.rb
+++ Formula/ballerina.rb
@@ -2,7 +2,7 @@ class Ballerina < Formula
   desc "Programming Language for Network Distributed Applications"
   homepage "https://ballerina.io"
   url "https://product-dist.ballerina.io/downloads/1.2.2/ballerina-1.2.2.zip"
-  sha256 "785fabc6b128fe06ab11a9964311020fd97dd4f9affbc8e2ff799b613c614ec0"
+  sha256 "8ca5993c8bc53056b2415acdc6a15f0ea64263e3cfaa17fb9f4febdaab9b67a9"
 
   bottle :unneeded
 
