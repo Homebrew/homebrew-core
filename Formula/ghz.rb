9e7d139b94d2add17688a4902616b4e5b50046fc
Bojan <dbojan@gmail.com>
2020-05-06 22:33:29 -0300

    ghz 0.52.0 (new formula) add additional test and improve install command

--- Formula/ghz.rb
+++ Formula/ghz.rb
@@ -7,15 +7,24 @@ class Ghz < Formula
   depends_on "go" => :build
 
   def install
-    ENV["CGO_ENABLED"] = "0"
     system "go", "build",
       "-ldflags", "-s -w -X main.version=#{version}",
-      "-trimpath", "-o", bin/"ghz",
+      *std_go_args,
       "cmd/ghz/main.go"
     prefix.install_metafiles
   end
 
   test do
     assert_match version.to_s, shell_output("#{bin}/ghz -v 2>&1")
+    (testpath/"config.toml").write <<~EOS
+      proto = "greeter.proto"
+      call = "helloworld.Greeter.SayHello"
+      host = "0.0.0.0:50051"
+      insecure = true
+      [data]
+      name = "Bob"
+    EOS
+    assert_match "open greeter.proto: no such file or directory",
+      shell_output("#{bin}/ghz --config config.toml 2>&1", 1)
   end
 end
