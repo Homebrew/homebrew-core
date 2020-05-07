4ebad0a64078f5200ee7487fc0d626c573db958d
Bojan <dbojan@gmail.com>
2020-05-06 22:01:13 -0300

    ghz-web 0.52.0 (new formula) add additional test and improve install command

--- Formula/ghz-web.rb
+++ Formula/ghz-web.rb
@@ -10,12 +10,20 @@ class GhzWeb < Formula
     ENV["CGO_ENABLED"] = "1"
     system "go", "build",
       "-ldflags", "-s -w -X main.version=#{version}",
-      "-trimpath", "-o", bin/"ghz-web",
+      *std_go_args,
       "cmd/ghz-web/main.go"
     prefix.install_metafiles
   end
 
   test do
     assert_match version.to_s, shell_output("#{bin}/ghz-web -v 2>&1")
+    port = free_port
+    ENV["GHZ_SERVER_PORT"] = port.to_s
+    fork do
+      exec "#{bin}/ghz-web"
+    end
+    sleep 1
+    cmd = "curl -sIm3 -XGET http://localhost:#{port}/"
+    assert_match /200 OK/m, shell_output(cmd)
   end
 end
