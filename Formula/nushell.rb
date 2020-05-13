c9fe199095309e5d5faa087de676a83957d256ec
Andrei Dziahel <develop7@develop7.info>
2020-05-13 14:32:31 +0300

    nushell: fix test for 0.14
    from-json → from json

--- Formula/nushell.rb
+++ Formula/nushell.rb
@@ -22,7 +22,7 @@ class Nushell < Formula
   end
 
   test do
-    assert_equal pipe_output("#{bin}/nu", 'echo \'{"foo":1, "bar":2}\' | from-json | get bar | echo $it'),
-    "Welcome to Nushell #{version} (type 'help' for more info)\n~ \n❯ 2~ \n❯ "
+    assert_equal pipe_output("#{bin}/nu", 'echo \'{"foo":1, "bar":2}\' | from json | get bar | echo $it'),
+    "Welcome to Nushell #{version} (type 'help' for more info)\n~ \n❯ 2\n~ \n❯ "
   end
 end
