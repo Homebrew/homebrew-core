4aab652526c9ea8f43f00d29bc41557aecc57749
Lee Briggs <lee@leebriggs.co.uk>
2020-05-13 20:04:13 -0700

    Update to v2.2.0

--- Formula/pulumi.rb
+++ Formula/pulumi.rb
@@ -1,11 +1,10 @@
 class Pulumi < Formula
   desc "Cloud native development platform"
   homepage "https://pulumi.io/"
-  version "2.1.1"
+  version "2.2.0"
   bottle :unneeded
   url "https://get.pulumi.com/releases/sdk/pulumi-v#{version}-darwin-x64.tar.gz"
-  sha256 "3efe812e88e0edc55107efad8ef0f65ca35b2bdcc669fa656cde68cbcbe11808"
-
+  sha256 "8cab8e2ffe9b98f652cd4543d8af4a1fe9062d728912a4376e310acf48c1de8d"
 
   def install
     bin.install Dir["*"]
