5d5dc40cb5b289ba2b650d07f43b633710ebb0fb
Andrei Dziahel <develop7@develop7.info>
2020-05-14 11:13:20 +0300

    nushell: pipe_output → shell_output in tests

--- Formula/nushell.rb
+++ Formula/nushell.rb
@@ -22,7 +22,7 @@ class Nushell < Formula
   end
 
   test do
-    assert_equal pipe_output("#{bin}/nu", 'echo \'{"foo":1, "bar":2}\' | from json | get bar | echo $it'),
-    "Welcome to Nushell #{version} (type 'help' for more info)\n~ \n❯ 2\n~ \n❯ "
+    assert_equal shell_output("#{bin}/nu -c 'echo \\'{\"foo\":1, \"bar\":2}\\' | from json | get bar | echo $it'"),
+    "2"
   end
 end
