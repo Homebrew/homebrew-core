900546066d2d4d3182feff4c7767942a604d3628
Jens-Uwe Mager <jum@anubis.han.de>
2020-05-15 21:25:38 +0200

    Updated for cdo 1.9.8.

--- Formula/cdo.rb
+++ Formula/cdo.rb
@@ -1,8 +1,8 @@
 class Cdo < Formula
   desc "Climate Data Operators"
   homepage "https://code.mpimet.mpg.de/projects/cdo"
-  url "https://code.mpimet.mpg.de/attachments/download/18911/cdo-1.9.6rc4.tar.gz"
-  sha256 "c236f48f3c920a29b4e66aa3883eeb7666fc5b4ee7109b2d24f8618709f6b60b"
+  url "https://code.mpimet.mpg.de/attachments/download/20826/cdo-1.9.8.tar.gz"
+  sha256 "f2660ac6f8bf3fa071cf2a3a196b3ec75ad007deb3a782455e80f28680c5252a"
 
   depends_on "eccodes"
   depends_on "hdf5"
@@ -22,8 +22,5 @@ class Cdo < Formula
 
     system "./configure", *args
     system "make", "install"
-  end
-
-  test do
   end
 end
