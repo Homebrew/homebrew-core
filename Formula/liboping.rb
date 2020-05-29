8083d8173550789f9a8d355c35551164b2e3f822
Arne Schwabe <arne@rfc2549.org>
2020-05-29 13:52:04 +0200

    [liboping] Fix pinging unreachable hosts on the local networks
    On *BSD and macOS a different error than on Linux can be reported
    when pinging LAN hosts. This fixes liboping here. For more details see:
    
    https://github.com/octo/liboping/pull/55

--- Formula/liboping.rb
+++ Formula/liboping.rb
@@ -3,6 +3,7 @@ class Liboping < Formula
   homepage "https://noping.cc/"
   url "https://noping.cc/files/liboping-1.10.0.tar.bz2"
   sha256 "eb38aa93f93e8ab282d97e2582fbaea88b3f889a08cbc9dbf20059c3779d5cd8"
+  revision 1
 
   bottle do
     sha256 "997e8eb17c7878cbd0c34bd6532b76ef804899751a58b3b434656d1b9ced07d9" => :catalina
@@ -14,6 +15,11 @@ class Liboping < Formula
   end
 
   uses_from_macos "ncurses"
+
+  patch do
+    url "https://github.com/octo/liboping/pull/55/commits/b5c5091d0214dd2c6cc504c02e2f78907abdee00.patch?full_index=1"
+    sha256 "5f91925a692acd4ca81697faddedd4419e5647d269c6b55b70731d7a39b14b67"
+  end
 
   def install
     system "./configure", "--disable-debug",
