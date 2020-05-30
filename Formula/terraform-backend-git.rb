4df48169742e6106971b28631a5af59a7a71c3d3
Dmytro Kryvenko <llibicpep@gmail.com>
2020-05-30 15:09:41 -0700

    fix Description is too long

--- Formula/terraform-backend-git.rb
+++ Formula/terraform-backend-git.rb
@@ -1,5 +1,5 @@
 class TerraformBackendGit < Formula
-  desc "Terraform HTTP Backend that uses Git repository as storage"
+  desc "HTTP Backend that uses Git repository as storage"
   homepage "https://github.com/plumber-cd/terraform-backend-git"
   url "https://github.com/plumber-cd/terraform-backend-git/archive/v0.0.13.tar.gz"
   sha256 "a97c4b4c56cff393550d5da1ae4add9e37e353e569d5846f680506e88a44027c"
