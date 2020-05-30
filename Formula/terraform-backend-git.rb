ccf6d18bf5b191838084b3c174133f2ad3ed5d43
Dmytro Kryvenko <llibicpep@gmail.com>
2020-05-30 14:58:34 -0700

    remove bottle block as per PR review

--- Formula/terraform-backend-git.rb
+++ Formula/terraform-backend-git.rb
@@ -4,13 +4,6 @@ class TerraformBackendGit < Formula
   url "https://github.com/plumber-cd/terraform-backend-git/archive/v0.0.13.tar.gz"
   sha256 "a97c4b4c56cff393550d5da1ae4add9e37e353e569d5846f680506e88a44027c"
 
-  bottle do
-    cellar :any_skip_relocation
-    # sha256 "a032752d893a2be4c7ade05fed422146c879a5cd4bcf69c8bee4489050ee7244" => :catalina
-    # sha256 "44d9ee3cd35d32a1dc33fde30c3e250bd0ac8c92ad6b99451dc2df3c785f59dd" => :mojave
-    # sha256 "21588d35f03ccd744d7c4cea52c5e44e34f6a8830d23710bf95965f04280cdf5" => :high_sierra
-  end
-
   depends_on "go" => :build
   depends_on "terraform"
 
