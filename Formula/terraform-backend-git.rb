ca04f67bfbe3cc20e513182e3681acedf7ceb408
Dee Kryvenko <109895+dee-kryvenko@users.noreply.github.com>
2020-05-30 20:31:57 -0700

    Use v0.0.14

--- Formula/terraform-backend-git.rb
+++ Formula/terraform-backend-git.rb
@@ -1,7 +1,7 @@
 class TerraformBackendGit < Formula
   desc "HTTP Backend that uses Git repository as storage"
   homepage "https://github.com/plumber-cd/terraform-backend-git"
-  url "https://github.com/plumber-cd/terraform-backend-git/archive/v0.0.13.tar.gz"
+  url "https://github.com/plumber-cd/terraform-backend-git/archive/v0.0.14.tar.gz"
   sha256 "a97c4b4c56cff393550d5da1ae4add9e37e353e569d5846f680506e88a44027c"
 
   depends_on "go" => :build
