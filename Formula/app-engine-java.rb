76b7ebdc3b5fa0c9be31c01eb84be301f928c628
Michael Stringer <stringbean@users.noreply.github.com>
2020-06-04 22:21:38 +0100

    app-engine-java 1.9.80

--- Formula/app-engine-java.rb
+++ Formula/app-engine-java.rb
@@ -1,8 +1,8 @@
 class AppEngineJava < Formula
   desc "Google App Engine for Java"
   homepage "https://cloud.google.com/appengine/docs/java/"
-  url "https://storage.googleapis.com/appengine-sdks/featured/appengine-java-sdk-1.9.72.zip"
-  sha256 "66af92c909c0403730aba7c4b9fbdc6d7ceb0f5310e7c2f1a653622dfa76c6fb"
+  url "https://storage.googleapis.com/appengine-sdks/featured/appengine-java-sdk-1.9.80.zip"
+  sha256 "15e66c4e949e699d3df6db5bae9fa30fbb1a8a5b5b1619ef191e7f409563c8ac"
 
   bottle :unneeded
 
