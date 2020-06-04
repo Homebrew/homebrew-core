6a0af5fbcc41bf2a8e525224db6aaa3b02c58888
miccal <miccal.matthews@gmail.com>
2020-06-05 07:32:30 +0800

    sqlite 3.32.2

--- Formula/sqlite.rb
+++ Formula/sqlite.rb
@@ -1,9 +1,9 @@
 class Sqlite < Formula
   desc "Command-line interface for SQLite"
   homepage "https://sqlite.org/"
-  url "https://sqlite.org/2020/sqlite-autoconf-3320100.tar.gz"
-  version "3.32.1"
-  sha256 "486748abfb16abd8af664e3a5f03b228e5f124682b0c942e157644bf6fff7d10"
+  url "https://sqlite.org/2020/sqlite-autoconf-3320200.tar.gz"
+  version "3.32.2"
+  sha256 "2dbef1254c1dbeeb5d13d7722d37e633f18ccbba689806b0a65b68701a5b6084"
 
   bottle do
     cellar :any
