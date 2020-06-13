b3692e31822f80b2c77c8afa34d1756c2ba56bed
Dustin Rodrigues <dust.rod@gmail.com>
2020-06-12 21:05:51 -0400

    graphql-cli 4.0.0

--- Formula/graphql-cli.rb
+++ Formula/graphql-cli.rb
@@ -3,8 +3,8 @@ require "language/node"
 class GraphqlCli < Formula
   desc "Command-line tool for common GraphQL development workflows"
   homepage "https://github.com/Urigo/graphql-cli"
-  url "https://registry.npmjs.org/graphql-cli/-/graphql-cli-3.0.14.tgz"
-  sha256 "bf56bbe425795198bf6fcc488f24fe10779327749671b1a331d4137684d8a72d"
+  url "https://registry.npmjs.org/graphql-cli/-/graphql-cli-4.0.0.tgz"
+  sha256 "1517777bc00b35f3ca3cc7a5a0a639ee9562871e4f4ac3b67143cabc0b4e2222"
 
   bottle do
     cellar :any_skip_relocation
@@ -23,37 +23,6 @@ class GraphqlCli < Formula
   end
 
   test do
-    (testpath/".graphqlconfig.yaml").write <<~EOS
-      projects:
-        test:
-          schemaPath: schema.graphql
-    EOS
-
-    script = (testpath/"test.sh")
-    script.write <<~EOS
-      #!/usr/bin/env expect -f
-      set timeout -1
-
-      spawn #{bin}/graphql add-project
-
-      expect -exact "? Enter project name for new project: "
-      send -- "homebrew\r"
-
-      expect -exact "? Local schema file path: "
-      send -- "\r"
-
-      expect -exact "? Endpoint URL (Enter to skip): "
-      send -- "\r"
-
-      expect -exact "? Is this ok? (Y/n) "
-      send -- "Y\r"
-
-      expect eof
-    EOS
-
-    script.chmod 0700
-    system "./test.sh"
-
-    assert_match "homebrew", File.read(testpath/".graphqlconfig.yaml")
+    assert_match version.to_s, shell_output("#{bin}/graphql --version")
   end
 end
