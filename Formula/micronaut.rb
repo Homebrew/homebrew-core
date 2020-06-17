7977fd72a3f503ea71ac61452ca0b37fcea0597d
Alvaro Sanchez-Mariscal <alvaro.sanchezmariscal@gmail.com>
2020-06-17 11:38:18 +0200

    Fix audit issues

--- Formula/micronaut.rb
+++ Formula/micronaut.rb
@@ -1,27 +1,25 @@
 class Micronaut < Formula
   desc "Modern JVM-based framework for building modular microservices"
   homepage "https://micronaut.io/"
-  version "2.0.0.RC1"
-  url "https://github.com/micronaut-projects/micronaut-starter/archive/v#{version}.zip"
-  sha256 "7bbbcab85e70c126ea4ffb20c178b1fc17f44b9758df7d2f348e20920c79aad5"
+  url "https://github.com/micronaut-projects/micronaut-starter/archive/v2.0.0.RC1.tar.gz"
+  sha256 "91dc71b200c598a91b95a20792dcc3f3cb160b8e169b581f6b98689dc713acad"
 
-  GRAALVM_HOME="./graalvm/Contents/Home"
-  
+  GRAALVM_HOME="./graalvm/Contents/Home".freeze
+
+  depends_on :java => ["1.8", :build]
+
   def install
     with_env "GRAAL_OS" => "darwin-amd64", "GRAAL_VERSION" => "20.1.0" do
       system "./install-graal.sh"
     end
-    
-    
     system "#{GRAALVM_HOME}/bin/gu", "install", "native-image"
-    
     with_env "JAVA_HOME" => GRAALVM_HOME do
       system "./gradlew", "micronaut-cli:shadowJar", "--no-daemon"
     end
-    
-    system "echo", "#{GRAALVM_HOME}/bin/native-image", "--no-fallback", "--no-server", "-cp", "starter-cli/build/libs/micronaut-cli-#{version}-all.jar" 
-    system "#{GRAALVM_HOME}/bin/native-image", "--no-fallback", "--no-server", "-cp", "starter-cli/build/libs/micronaut-cli-#{version}-all.jar" 
-    
+    system "echo", "#{GRAALVM_HOME}/bin/native-image", "--no-fallback", "--no-server", "-cp",
+            "starter-cli/build/libs/micronaut-cli-#{version}-all.jar"
+    system "#{GRAALVM_HOME}/bin/native-image", "--no-fallback", "--no-server", "-cp",
+            "starter-cli/build/libs/micronaut-cli-#{version}-all.jar"
     bin.install "mn"
   end
 
