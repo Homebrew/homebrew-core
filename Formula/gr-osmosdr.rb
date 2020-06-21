ec857d741f58657f947a1b4c34e7be3b68437fce
Pascâl Hartmann <lopablo@protonmail.com>
2020-06-21 16:15:22 +0200

    GR-Osmosdr 0.2.0

--- Formula/gr-osmosdr.rb
+++ Formula/gr-osmosdr.rb
@@ -1,16 +1,9 @@
 class GrOsmosdr < Formula
   desc "Osmocom GNU Radio Blocks"
   homepage "https://osmocom.org/projects/sdr/wiki/GrOsmoSDR"
-  url "https://github.com/osmocom/gr-osmosdr/archive/v0.1.4.tar.gz"
-  sha256 "bcf9a9b1760e667c41a354e8cd41ef911d0929d5e4a18e0594ccb3320d735066"
-  revision 10
-
-  bottle do
-    cellar :any
-    sha256 "65570c9ef4058f8ace042beeed7672b445e7d7c069ca9c6b28c535bbe0a2b711" => :catalina
-    sha256 "971a785a7acf94c36761047326a56ce5e375bf9d5e633b24df196cef4c0035e6" => :mojave
-    sha256 "0a79c514b8da09932af265cf6582789edb8fd1b87cef0796e10dab7b847779bb" => :high_sierra
-  end
+  url "https://github.com/osmocom/gr-osmosdr/archive/v0.2.0.tar.gz"
+  sha256 "9812429d97bc54f0a8917b880ca9e7e2421c66aeaac8ce5608161a8ae7007122"
+  revision 12
 
   depends_on "cmake" => :build
   depends_on "swig" => :build
@@ -19,7 +12,7 @@ class GrOsmosdr < Formula
   depends_on "gnuradio"
   depends_on "hackrf"
   depends_on "librtlsdr"
-  depends_on :macos # gnuradio uses Python 2
+  depends_on "python@3.8"
   depends_on "uhd"
 
   resource "Cheetah" do
@@ -28,13 +21,11 @@ class GrOsmosdr < Formula
   end
 
   def install
-    ENV.prepend_create_path "PYTHONPATH", libexec/"vendor/lib/python2.7/site-packages"
-
-    resource("Cheetah").stage do
-      system "python", *Language::Python.setup_install_args(libexec/"vendor")
-    end
+    xy = Language::Python.major_minor_version "python3"
+    ENV.prepend_create_path "PYTHONPATH", libexec/"vendor/lib/python#{xy}/site-packages"
 
     system "cmake", ".", *std_cmake_args
+    system "make"
     system "make", "install"
   end
 
