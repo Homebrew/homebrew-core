5c2728674b50a2ca8d91de33062aa26272fc42a3
Rui Chen <rui@meetup.com>
2020-06-21 13:45:26 -0400

    dartsim: revision for ipopt

--- Formula/dartsim.rb
+++ Formula/dartsim.rb
@@ -3,7 +3,7 @@ class Dartsim < Formula
   homepage "https://dartsim.github.io/"
   url "https://github.com/dartsim/dart/archive/v6.9.2.tar.gz"
   sha256 "7d46d23c04d74d3b78331f9fa7deb5ab32fd4b0c03b93548cd84a2d67771d816"
-  revision 8
+  revision 9
 
   bottle do
     sha256 "bccbbcb136d0c77f46de622359c8085ad10436431deca27424bb894e01fb3d1c" => :catalina
