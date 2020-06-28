9edd0cda9a911d701c7f23a44624a724e3970e60
Dustin Rodrigues <dust.rod@gmail.com>
2020-06-28 15:42:21 -0400

    libosmium 2.15.6

--- Formula/libosmium.rb
+++ Formula/libosmium.rb
@@ -1,8 +1,8 @@
 class Libosmium < Formula
   desc "Fast and flexible C++ library for working with OpenStreetMap data"
   homepage "https://osmcode.org/libosmium/"
-  url "https://github.com/osmcode/libosmium/archive/v2.15.5.tar.gz"
-  sha256 "b32ab1f3c75c229806d90c087872064050d2f22e1afa70eed8dea32360cde358"
+  url "https://github.com/osmcode/libosmium/archive/v2.15.6.tar.gz"
+  sha256 "55879b9da0f3756f3216f9fb857f35afb3b01294b5f18e0d1719334e7d4f5ac9"
 
   bottle do
     cellar :any_skip_relocation
@@ -17,8 +17,8 @@ class Libosmium < Formula
   uses_from_macos "expat"
 
   resource "protozero" do
-    url "https://github.com/mapbox/protozero/archive/v1.6.8.tar.gz"
-    sha256 "019a0f3789ad29d7e717cf2e0a7475b36dc180508867fb47e8c519885b431706"
+    url "https://github.com/mapbox/protozero/archive/v1.7.0.tar.gz"
+    sha256 "beffbdfab060854fd770178a8db9c028b5b6ee4a059a2fed82c46390a85f3f31"
   end
 
   def install
