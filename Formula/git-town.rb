bafa28891391fe52386c2552ee33337a57fa462d
Kevin Goslar <kevin.goslar@gmail.com>
2020-07-05 09:11:50 -0500

    Fix line length

--- Formula/git-town.rb
+++ Formula/git-town.rb
@@ -18,7 +18,8 @@ class GitTown < Formula
     ENV["GOPATH"] = buildpath
     (buildpath/"src/github.com/git-town").mkpath
     ln_sf buildpath, buildpath/"src/github.com/git-town/git-town"
-    system "go", "build", "-o", bin/"git-town", "-ldflags", "-X", "github.com/git-town/git-town/src/cmd.version=v7.4.0", "-X", "github.com/git-town/git-town/src/cmd.buildDate=2020/07/05"
+    system "go", "build", "-o", bin/"git-town", "-ldflags",
+           "-X github.com/git-town/git-town/src/cmd.version=v7.4.0 -X github.com/git-town/git-town/src/cmd.buildDate=2020/07/05"
   end
 
   test do
