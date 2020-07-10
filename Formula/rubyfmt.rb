7b74aa5150f2e1ebd870b82603112a7ea39c9040
Dustin Rodrigues <dust.rod@gmail.com>
2020-07-10 19:09:34 -0400

    rubyfmt 0.3.0

--- Formula/rubyfmt.rb
+++ Formula/rubyfmt.rb
@@ -1,8 +1,10 @@
 class Rubyfmt < Formula
   desc "Ruby autoformatter"
   homepage "https://github.com/penelopezone/rubyfmt"
-  url "https://github.com/penelopezone/rubyfmt/archive/v0.2.0.tar.gz"
-  sha256 "68ebc0fd30933b1e27b609cc34c69a3bc886747d11c5b949e460ce01814adaeb"
+  # pull from git tag to get submodules
+  url "https://github.com/penelopezone/rubyfmt.git",
+      :tag      => "v0.3.0",
+      :revision => "f4296cbf9f5077ef684d9afb3c25f70db7d4c4bd"
   license "MIT"
 
   bottle do
@@ -12,11 +14,14 @@ class Rubyfmt < Formula
     sha256 "8d9ed80d496220e02b9df146c41870079116cf798ab90734212d3cdc6080bb8b" => :high_sierra
   end
 
+  depends_on "autoconf" => :build
+  depends_on "rust" => :build
   uses_from_macos "ruby"
 
   def install
-    system "make"
-    bin.install "build/rubyfmt.rb" => "rubyfmt"
+    system "make", "submodules"
+    system "make", "target/release/rubyfmt-main"
+    bin.install "target/release/rubyfmt-main" => "rubyfmt"
   end
 
   test do
