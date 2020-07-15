effb0767b383172d2e6396478a9b834296f83b67
Claudia Pellegrino <claui@users.noreply.github.com>
2020-07-15 21:12:11 +0200

    lynx: remove spaces in empty line

--- Formula/lynx.rb
+++ Formula/lynx.rb
@@ -19,7 +19,6 @@ class Lynx < Formula
 
   def install
     ENV["PKG_CONFIG"] = Formula["openssl@1.1"].opt_lib/"pkgconfig"
-    
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
                           "--mandir=#{man}",
