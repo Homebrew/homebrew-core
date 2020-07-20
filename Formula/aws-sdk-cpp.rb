32ef54438cde08137b8ea1ff1bda2c73f83e50f9
Dustin Rodrigues <dust.rod@gmail.com>
2020-07-20 13:34:09 -0400

    aws-sdk-cpp: use throttle_rate

--- Formula/aws-sdk-cpp.rb
+++ Formula/aws-sdk-cpp.rb
@@ -1,11 +1,11 @@
 class AwsSdkCpp < Formula
   desc "AWS SDK for C++"
   homepage "https://github.com/aws/aws-sdk-cpp"
-  # aws-sdk-cpp should only be updated every 10 releases on multiples of 10
   url "https://github.com/aws/aws-sdk-cpp/archive/1.8.1.tar.gz"
   sha256 "cfe718405fcc8a7268927a143174677f9cea4a779ab39c5f0862a1b193ea1f18"
   license "Apache-2.0"
   head "https://github.com/aws/aws-sdk-cpp.git"
+  throttle_rate 10
 
   bottle do
     sha256 "c03d8fc638156a10d777e83b0ef58541faacc135d7621991a91139c90b6fc9a2" => :catalina
