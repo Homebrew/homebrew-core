21373713c5d1159f7a21ad19fa05549db2faa019
Oleg Butuzov <butuzov@users.noreply.github.com>
2020-07-22 08:55:29 +0300

    golangci-lint 1.29.0 (new formula)

--- Formula/golangci-lint.rb
+++ Formula/golangci-lint.rb
@@ -6,7 +6,7 @@ class GolangciLint < Formula
     :revision => "6a689074bf17fd4ae1db779a74dba821a162b6c8"
   license "GPL-3.0"
 
-  depends_on "go" => :build
+  depends_on "go"
 
   def install
     commit = Utils.safe_popen_read("git", "rev-parse", "--short=7", "HEAD").chomp
@@ -17,20 +17,46 @@ class GolangciLint < Formula
       -X main.date=#{Time.now.utc.rfc3339}
     ].join(" ")
 
-    system "go", "build", "-trimpath", "-ldflags", ldflags, "-o", "golangci-lint", "./cmd/golangci-lint"
-
-    bin.install "golangci-lint"
+    system "go", "build", *std_go_args, "-ldflags", ldflags, "./cmd/golangci-lint"
 
     output = Utils.safe_popen_read({ "SHELL" => "bash" }, "#{bin}/golangci-lint", "completion", "bash")
     (bash_completion/"golangci-lint").write output
 
     output = Utils.safe_popen_read({ "SHELL" => "zsh" }, "#{bin}/golangci-lint", "completion", "zsh")
     (zsh_completion/"golangci-lint").write output
-
-    prefix.install_metafiles
   end
 
   test do
-    system "#{bin}/golangci-lint", "--version"
+    str_version = shell_output("#{bin}/golangci-lint --version")
+    assert_match "golangci-lint has version #{version} built from", str_version
+
+    str_help = shell_output("#{bin}/golangci-lint --help")
+    str_default = shell_output("#{bin}/golangci-lint")
+    assert_equal str_default, str_help
+    assert_match "Usage:", str_help
+    assert_match "Available Commands:", str_help
+
+    (testpath/"try.go").write <<~EOS
+      package try
+
+      func add(nums ...int) (res int) {
+        for _, n := range nums {
+          res += n
+        }
+        return
+      }
+    EOS
+
+    args = %w[
+      --color=never
+      --disable-all
+      --issues-exit-code=0
+      --print-issued-lines=false
+      --enable=deadcode
+    ].join(" ")
+
+    ok_test = shell_output("#{bin}/golangci-lint run #{args} #{testpath}/try.go")
+    expected_message = "try.go:3:6: `add` is unused (deadcode)"
+    assert_match expected_message, ok_test
   end
 end
