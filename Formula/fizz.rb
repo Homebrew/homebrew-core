f38dbd168961bb4aafe861dc0a9837dbbaf5392a
Andrew Kane <andrew@chartkick.com>
2020-07-24 22:21:35 -0700

    fizz 2020.07.20.00 (new formula)

---
+++ Formula/fizz.rb
@@ -0,0 +1,54 @@
+class Fizz < Formula
+  desc "C++14 implementation of the TLS-1.3 standard"
+  homepage "https://github.com/facebookincubator/fizz"
+  url "https://github.com/facebookincubator/fizz/archive/v2020.07.20.00.tar.gz"
+  sha256 "c40c43015ae19dc4b3232eed329ddefd14b1616a46b4198637aa0b65b84b1a3d"
+  license "BSD-2-Clause"
+
+  depends_on "cmake" => :build
+  depends_on "boost"
+  depends_on "double-conversion"
+  depends_on "fmt"
+  depends_on "folly"
+  depends_on "gflags"
+  depends_on "glog"
+  depends_on "libevent"
+  depends_on "libsodium"
+  depends_on "openssl@1.1"
+
+  fails_with :clang do
+    build 1000
+    cause "error: no matching constructor"
+  end
+
+  def install
+    mkdir "fizz/build" do
+      system "cmake", "..", *std_cmake_args, "-DBUILD_SHARED_LIBS=ON"
+      system "make"
+      system "make", "install"
+    end
+  end
+
+  test do
+    (testpath/"test.cpp").write <<~EOS
+      #include <fizz/client/AsyncFizzClient.h>
+      #include <iostream>
+
+      int main() {
+        auto context = fizz::client::FizzClientContext();
+        std::cout << toString(context.getSupportedVersions()[0]) << std::endl;
+      }
+    EOS
+    system ENV.cxx, "-std=c++14", "test.cpp", "-o", "test",
+                    "-I#{include}",
+                    "-I#{Formula["openssl@1.1"].opt_include}",
+                    "-L#{lib}", "-lfizz",
+                    "-L#{Formula["folly"].opt_lib}", "-lfolly",
+                    "-L#{Formula["gflags"].opt_lib}", "-lgflags",
+                    "-L#{Formula["glog"].opt_lib}", "-lglog",
+                    "-L#{Formula["libevent"].opt_lib}", "-levent",
+                    "-L#{Formula["libsodium"].opt_lib}", "-lsodium",
+                    "-L#{Formula["openssl@1.1"].opt_lib}", "-lcrypto", "-lssl"
+    assert_match "TLS", shell_output("./test")
+  end
+end
