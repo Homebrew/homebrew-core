75e755949c7ac90692c6428b82f223f49fb7e410
Seeker <meaningseeking@protonmail.com>
2020-07-30 14:55:22 -0700

    csshx: update license

--- Formula/csshx.rb
+++ Formula/csshx.rb
@@ -4,8 +4,8 @@ class Csshx < Formula
   url "https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/csshx/csshX-0.74.tgz"
   mirror "https://distfiles.macports.org/csshX/csshX-0.74.tgz"
   sha256 "eaa9e52727c8b28dedc87398ed33ffa2340d6d0f3ea9d261749c715cb7a0e9c8"
-  # same terms as Perl
-  license "GPL-1.0"
+  # Licensed under same terms as Perl
+  license ["GPL-1.0-or-later", "Artistic-1.0-Perl"]
   head "https://github.com/brockgr/csshx.git"
 
   bottle :unneeded
