cf6a201ebe8a2e730c8f94929029e51b77f8b859
Lucas Bleme <andreybleme1@gmail.com>
2020-07-31 19:39:57 -0300

    fetch 0.3.9 (new formula)

---
+++ Formula/fetch.rb
@@ -0,0 +1,18 @@
+class Fetch < Formula
+  desc "Download assets from a commit, branch, or tag of GitHub repos"
+  homepage "https://www.gruntwork.io/"
+  url "https://github.com/gruntwork-io/fetch/archive/v0.3.9.tar.gz"
+  sha256 "bf9b42e1c71885e56de5f9ceba232d6c46027a45369ce99cccdb14af71cce919"
+  license "MIT"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", "-ldflags", "-X main.VERSION=v#{version}", *std_go_args
+  end
+
+  test do
+    assert_match "fetch - fetch makes it easy to download files, folders, and release assets",
+    shell_output("#{bin}/fetch --help 2>1&")
+  end
+end
