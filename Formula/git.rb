ae7092897029aa085c9013fdb2dc037a0ddab7ec
Marlon Richert <marlonrichert@users.noreply.github.com>
2020-08-03 12:35:18 +0300

    Do not install Git completion for Zsh
    The Zsh completion supplied with Git is buggy and outdated.
    Zsh itself includes much better Git completion out of the box.

--- Formula/git.rb
+++ Formula/git.rb
@@ -114,8 +114,6 @@ class Git < Formula
     # install the completion script first because it is inside "contrib"
     bash_completion.install "contrib/completion/git-completion.bash"
     bash_completion.install "contrib/completion/git-prompt.sh"
-    zsh_completion.install "contrib/completion/git-completion.zsh" => "_git"
-    cp "#{bash_completion}/git-completion.bash", zsh_completion
 
     elisp.install Dir["contrib/emacs/*.el"]
     (share/"git-core").install "contrib"
