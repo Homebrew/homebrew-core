3e848f8c0563da04cdae8f7f8704c08ee51af6c2
Seeker <meaningseeking@protonmail.com>
2020-08-04 12:15:26 -0700

    alac: add license

--- Formula/alac.rb
+++ Formula/alac.rb
@@ -3,6 +3,7 @@ class Alac < Formula
   homepage "https://web.archive.org/web/20150319040222/craz.net/programs/itunes/alac.html"
   url "https://web.archive.org/web/20150510210401/craz.net/programs/itunes/files/alac_decoder-0.2.0.tgz"
   sha256 "7f8f978a5619e6dfa03dc140994fd7255008d788af848ba6acf9cfbaa3e4122f"
+  license "MIT"
 
   bottle do
     cellar :any_skip_relocation
