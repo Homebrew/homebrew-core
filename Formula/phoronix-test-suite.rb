0a080a1db68b2ff7701378fd84da2ffae7221c7a
Rylan Polster <rslpolster@gmail.com>
2020-08-05 11:14:15 -0400

    phoronix-test-suite: remove deprecated license

--- Formula/phoronix-test-suite.rb
+++ Formula/phoronix-test-suite.rb
@@ -3,7 +3,6 @@ class PhoronixTestSuite < Formula
   homepage "https://www.phoronix-test-suite.com/"
   url "https://github.com/phoronix-test-suite/phoronix-test-suite/archive/v9.8.0.tar.gz"
   sha256 "6313c5ad1fa4a62726f1347c3674a704582ec5a3a0ca1af20b034ccc6f64af9e"
-  license "GPL-3.0"
   head "https://github.com/phoronix-test-suite/phoronix-test-suite.git"
 
   bottle :unneeded
