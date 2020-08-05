dcd93f13d95e4c440201eb785bc13d16d6f2c0d8
Rylan Polster <rslpolster@gmail.com>
2020-08-05 11:13:24 -0400

    libical: remove deprecated license

--- Formula/libical.rb
+++ Formula/libical.rb
@@ -4,7 +4,6 @@ class Libical < Formula
   url "https://github.com/libical/libical/releases/download/v3.0.8/libical-3.0.8.tar.gz"
   sha256 "09fecacaf75ba5a242159e3a9758a5446b5ce4d0ab684f98a7040864e1d1286f"
   # license ["LGPL-2.1", "MPL-2.0"] - pending https://github.com/Homebrew/brew/pull/7953
-  license "LGPL-2.1"
   revision 2
 
   bottle do
