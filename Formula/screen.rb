e857c2cdadd10b0ef371a6b8be918e61f018baa6
Rylan Polster <rslpolster@gmail.com>
2020-08-05 11:14:31 -0400

    screen: remove deprecated license

--- Formula/screen.rb
+++ Formula/screen.rb
@@ -1,7 +1,6 @@
 class Screen < Formula
   desc "Terminal multiplexer with VT100/ANSI terminal emulation"
   homepage "https://www.gnu.org/software/screen"
-  license "GPL-3.0"
 
   stable do
     url "https://ftp.gnu.org/gnu/screen/screen-4.8.0.tar.gz"
