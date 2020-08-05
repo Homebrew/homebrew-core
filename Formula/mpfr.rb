c0c14a1913099c16af8d3d68401f1af54ea66e2a
Rylan Polster <rslpolster@gmail.com>
2020-08-05 11:13:52 -0400

    mpfr: remove deprecated license

--- Formula/mpfr.rb
+++ Formula/mpfr.rb
@@ -4,7 +4,6 @@ class Mpfr < Formula
   url "https://ftp.gnu.org/gnu/mpfr/mpfr-4.1.0.tar.xz"
   mirror "https://ftpmirror.gnu.org/mpfr/mpfr-4.1.0.tar.xz"
   sha256 "0c98a3f1732ff6ca4ea690552079da9c597872d30e96ec28414ee23c95558a7f"
-  license "GPL-3.0"
 
   bottle do
     cellar :any
