15779bf31739749648c17774b005a710b28b07ed
Rylan Polster <rslpolster@gmail.com>
2020-08-05 11:13:04 -0400

    jack: remove deprecated license

--- Formula/jack.rb
+++ Formula/jack.rb
@@ -12,7 +12,6 @@ class Jack < Formula
   url "https://github.com/jackaudio/jack1.git",
       tag:      "0.125.0",
       revision: "f5e00e485e7aa4c5baa20355b27e3b84a6912790"
-  license "GPL-2.0"
   revision 4
   head "https://github.com/jackaudio/jack1.git"
 
