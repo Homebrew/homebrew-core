be0f420efc8323d9ccdca6e2a5732788165bd58e
Rylan Polster <rslpolster@gmail.com>
2020-08-05 11:11:50 -0400

    checkstyle: remove deprecated license

--- Formula/checkstyle.rb
+++ Formula/checkstyle.rb
@@ -3,7 +3,6 @@ class Checkstyle < Formula
   homepage "https://checkstyle.sourceforge.io/"
   url "https://github.com/checkstyle/checkstyle/releases/download/checkstyle-8.35/checkstyle-8.35-all.jar"
   sha256 "80b1ce8a36741b028708254d1f5e13cada4c6ae4c6a5f5d1af1d7bbe582275cb"
-  license "LGPL-2.1"
 
   bottle :unneeded
 
