1ba70b7d926deeafe84df08254603fcd91862a2f
Rylan Polster <rslpolster@gmail.com>
2020-08-05 11:12:29 -0400

    flint: remove deprecated license

--- Formula/flint.rb
+++ Formula/flint.rb
@@ -3,7 +3,6 @@ class Flint < Formula
   homepage "http://flintlib.org"
   url "http://flintlib.org/flint-2.6.2.tar.gz"
   sha256 "5f9b45113c3b50c10564a04e9dd929eb8ad96488b5eb901db5723cd21bbae1da"
-  license "LGPL-2.1"
   head "https://github.com/wbhart/flint2.git", branch: "trunk"
 
   bottle do
