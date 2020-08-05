d04dbb5f5e5bd57acf51924cbf165b2b204c6b8f
Rylan Polster <rslpolster@gmail.com>
2020-08-05 11:12:55 -0400

    homebank: remove deprecated license

--- Formula/homebank.rb
+++ Formula/homebank.rb
@@ -3,7 +3,6 @@ class Homebank < Formula
   homepage "http://homebank.free.fr"
   url "http://homebank.free.fr/public/homebank-5.4.2.tar.gz"
   sha256 "c6ce84b421e7199ae545ea8b8d981f347af4d67b4cd5912b6789cf1450db722e"
-  license "GPL-2.0"
   revision 2
 
   bottle do
