1cb2d165edf49655b4bb5b030e1ace6a277d36db
Zach Auten <zachauten@gmail.com>
2020-08-12 09:17:40 -0400

    emscripten 2.0.0

--- Formula/emscripten.rb
+++ Formula/emscripten.rb
@@ -8,8 +8,8 @@ class Emscripten < Formula
   license "MIT"
 
   stable do
-    url "https://github.com/emscripten-core/emscripten/archive/1.40.1.tar.gz"
-    sha256 "e15ad7ffa1cce35c25cac7c797d6daa0c5868905eaaf5ed1431a8228b8803dfc"
+    url "https://github.com/emscripten-core/emscripten/archive/2.0.0.tar.gz"
+    sha256 "a3e443c58db1320d2928a9e8e2b74d484adc3412a332fa4493e3cd0022e4632e"
 
     resource "fastcomp" do
       url "https://github.com/emscripten-core/emscripten-fastcomp/archive/1.40.1.tar.gz"
