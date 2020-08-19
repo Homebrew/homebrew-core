8da877c6d831edd8ce3019736b0816e1065c5636
Rui Chen <rui@meetup.com>
2020-08-19 19:19:18 -0400

    should be https url

--- Formula/darcs.rb
+++ Formula/darcs.rb
@@ -1,7 +1,7 @@
 class Darcs < Formula
   desc "Distributed version control system that tracks changes, via Haskell"
   homepage "http://darcs.net/"
-  url "http://hackage.haskell.org/package/darcs-2.16.2/candidate/darcs-2.16.2.tar.gz"
+  url "https://hackage.haskell.org/package/darcs-2.16.2/candidate/darcs-2.16.2.tar.gz"
   sha256 "161752851d910fb8d2168cb49b3b9bbe0571e0b622cd1d36d1ea174aa25255db"
   license "GPL-2.0"
 
