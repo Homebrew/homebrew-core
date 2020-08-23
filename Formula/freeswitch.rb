209a0c9458ff479e47a6fac139b3a0b64605be98
Rui Chen <rui@meetup.com>
2020-08-22 22:30:00 -0400

    freeswitch 1.10.5

--- Formula/freeswitch.rb
+++ Formula/freeswitch.rb
@@ -2,8 +2,8 @@ class Freeswitch < Formula
   desc "Telephony platform to route various communication protocols"
   homepage "https://freeswitch.org"
   url "https://github.com/signalwire/freeswitch.git",
-      tag:      "v1.10.3",
-      revision: "129de34d845098b7864a49281f3eb946e0b50314"
+      tag:      "v1.10.5",
+      revision: "25569c16311afb3fe04a445830a8ab5c88488a5e"
   license "MPL-1.1"
   head "https://github.com/signalwire/freeswitch.git"
 
