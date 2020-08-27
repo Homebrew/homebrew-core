3b85e8cda7804b9ecbfec350e70ca8c2eeb0809f
Andrey Meshkov <am@adguard.com>
2020-08-27 15:06:35 +0300

    fix audit issues

--- Formula/dnslookup.rb
+++ Formula/dnslookup.rb
@@ -1,10 +1,10 @@
 class Dnslookup < Formula
-  desc "Simple command line utility to make DNS lookups using any DNS protocol (DoH, DoT, DNSCrypt, DoQ)"
+  desc "Simple command-line utility to make DNS lookups using any protocol"
   homepage "https://github.com/ameshkov/dnslookup/"
   url "https://github.com/ameshkov/dnslookup.git",
       tag:      "v1.4.1",
       revision: "99af6421f9c7f409a2c645d81bd3d8212414296d"
-  license "GPL-3.0"
+  license "GPL-3.0-only"
 
   depends_on "go" => :build
 
