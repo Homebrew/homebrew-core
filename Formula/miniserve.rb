48a5709a7fe6f89fd048ae595ddb7b05ce2c3252
chenrui <chenrui333@gmail.com>
2020-09-06 20:38:26 +0000

    miniserve 0.8.0

--- Formula/miniserve.rb
+++ Formula/miniserve.rb
@@ -3,8 +3,8 @@ class Miniserve < Formula
   homepage "https://github.com/svenstaro/miniserve"
   # Bumpable only when it doesn't use features only available in Rust Nightly.
   # Check for resolution of https://github.com/svenstaro/miniserve/issues/291.
-  url "https://github.com/svenstaro/miniserve/archive/v0.3.0.tar.gz"
-  sha256 "80ee5d661730ddad14671f961b560467f3b3a9f0544b9b11dec65098eb4a1f7e"
+  url "https://github.com/svenstaro/miniserve/archive/v0.8.0.tar.gz"
+  sha256 "852542d1f1421c59d0addd5ed5371788f1095b89372ca5c6b293c69f570635f7"
   license "MIT"
 
   bottle do
