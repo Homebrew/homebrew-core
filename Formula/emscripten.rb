0bac914aebea96bda19c47679aa9f2f5b1dfc192
chenrui <chenrui333@gmail.com>
2020-09-11 04:22:28 +0000

    emscripten 2.0.3

--- Formula/emscripten.rb
+++ Formula/emscripten.rb
@@ -8,8 +8,8 @@ class Emscripten < Formula
   license "MIT"
 
   stable do
-    url "https://github.com/emscripten-core/emscripten/archive/1.40.1.tar.gz"
-    sha256 "e15ad7ffa1cce35c25cac7c797d6daa0c5868905eaaf5ed1431a8228b8803dfc"
+    url "https://github.com/emscripten-core/emscripten/archive/2.0.3.tar.gz"
+    sha256 "ac9ccb17cb14603da19009179687417fe9e63b8185e50703dbab53405276ff97"
 
     resource "fastcomp" do
       url "https://github.com/emscripten-core/emscripten-fastcomp/archive/1.40.1.tar.gz"
