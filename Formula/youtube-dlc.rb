0100e86250a3233a31efcb4c70d7423b266cd51c
Christopher Nethercott <ccnethercott@gmail.com>
2020-09-14 20:42:50 +0100

    Removed redundent comment and used version as variable for the URL

--- Formula/youtube-dlc.rb
+++ Formula/youtube-dlc.rb
@@ -1,12 +1,10 @@
 class YoutubeDlc < Formula
   desc "Download YouTube videos from the command-line"
   homepage "https://blackjack4494.github.io/youtube-dlc/"
-  url "https://github.com/blackjack4494/youtube-dlc/releases/download/2020.09.13/youtube-dlc"
   version "2020.09.13"
+  url "https://github.com/blackjack4494/youtube-dlc/releases/download/#{version}/youtube-dlc"
   sha256 "633caf38c990ab1b21cfe849151601e8a1548ddcdd539caf755e1b3d47941af9"
   license "Unlicense"
-
-  # depends_on "cmake" => :build
 
   def install
     bin.install "youtube-dlc"
