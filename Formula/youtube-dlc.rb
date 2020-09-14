9e0e97718b430845acb0a7029c847b348aa294f6
Christopher Nethercott <ccnethercott@gmail.com>
2020-09-14 20:32:32 +0100

    Removed unusued comments for youtube-dlc

--- Formula/youtube-dlc.rb
+++ Formula/youtube-dlc.rb
@@ -1,6 +1,3 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class YoutubeDlc < Formula
   desc "Download YouTube videos from the command-line"
   homepage "https://blackjack4494.github.io/youtube-dlc/"
@@ -12,26 +9,10 @@ class YoutubeDlc < Formula
   # depends_on "cmake" => :build
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    # Remove unrecognized options if warned by configure
-    #system "./configure", "--disable-debug",
-    #                      "--disable-dependency-tracking",
-    #                      "--disable-silent-rules",
-    #                      "--prefix=#{prefix}"
-    # system "cmake", ".", *std_cmake_args
-    bin.install "youtube-dlc"
+    bin.install "youtube-dlc  "
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test youtube-dlc`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
     # commit history of homebrew-core repo
     system "#{bin}/youtube-dlc", "--simulate", "https://www.youtube.com/watch?v=pOtd1cbOP7k"
     # homebrew playlist
