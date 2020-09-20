a0cc1b160a9d81f94aae3df3ff99961c29960592
Rui Chen <rui@meetup.com>
2020-09-19 21:08:28 -0400

    swift 5.3.2
    swift: add patch for llbuild
    
    Signed-off-by: Rui Chen <rui@meetup.com>

--- Formula/swift.rb
+++ Formula/swift.rb
@@ -2,10 +2,9 @@ class Swift < Formula
   desc "High-performance system programming language"
   homepage "https://swift.org"
   # NOTE: Keep version in sync with resources below
-  url "https://github.com/apple/swift/archive/swift-5.2.5-RELEASE.tar.gz"
-  sha256 "2353bb00dada11160945729a33af94150b7cf0a6a38fbe975774a6e244dbc548"
+  url "https://github.com/apple/swift/archive/swift-5.3.2-RELEASE.tar.gz"
+  sha256 "2087bb002dfef7ea2d7fbfaa097eeafd10d997cae52e2f34bb115fca68dd8039"
   license "Apache-2.0"
-  revision 1
 
   livecheck do
     url "https://swift.org/download/"
@@ -24,38 +23,43 @@ class Swift < Formula
 
   # Has strict requirements on the minimum version of Xcode
   # https://github.com/apple/swift/tree/swift-#{version}-RELEASE#system-requirements
-  depends_on xcode: ["11.2", :build]
+  depends_on xcode: ["12.0", :build]
 
   uses_from_macos "icu4c"
 
   resource "llvm-project" do
-    url "https://github.com/apple/llvm-project/archive/swift-5.2.5-RELEASE.tar.gz"
-    sha256 "f3e6bf2657edf7c290befdfc9d534ed776c0f344c0df373ccecc60ab2c928a51"
+    url "https://github.com/apple/llvm-project/archive/swift-5.3.2-RELEASE.tar.gz"
+    sha256 "838e41803613ce4890f4259eef1d99ee4cf1b7cbdf1c664e04e4958863999b38"
   end
 
   resource "cmark" do
-    url "https://github.com/apple/swift-cmark/archive/swift-5.2.5-RELEASE.tar.gz"
-    sha256 "71ef5641ebbb60ddd609320bdbf4d378cdcd89941b6f17f658ee5be40c98a232"
+    url "https://github.com/apple/swift-cmark/archive/swift-5.3.2-RELEASE.tar.gz"
+    sha256 "d20bc09a2c76c3fb161886025334cb7e3e9884037ad91d2fa4b42ef36dfee958"
   end
 
   resource "llbuild" do
-    url "https://github.com/apple/swift-llbuild/archive/swift-5.2.5-RELEASE.tar.gz"
-    sha256 "07db561275697634f4790d9cd7d817272ffa37ebd7a69e0abc5de51bcdb4efb7"
+    url "https://github.com/apple/swift-llbuild/archive/swift-5.3.2-RELEASE.tar.gz"
+    sha256 "296cee1a19c17375877d88084e20e5d98bcc7a87950f3ade143c0222439f1c9d"
+
+    patch do
+      url "https://raw.githubusercontent.com/vgorloff/swift-everywhere-toolchain/6721d91/Patches/llbuild/CMakeLists.txt.diff"
+      sha256 "93fb0402e5a63d2d9e606641a91b34b054ecd1f66887847e8f2a8de724f57806"
+    end
   end
 
   resource "swiftpm" do
-    url "https://github.com/apple/swift-package-manager/archive/swift-5.2.5-RELEASE.tar.gz"
-    sha256 "f7197556bf299f4fc7b88e63fed78797fd85f94bf590f34e3de845ad5e62afbe"
+    url "https://github.com/apple/swift-package-manager/archive/swift-5.3.2-RELEASE.tar.gz"
+    sha256 "e62bbe51d90a93ad52b668e7a882c6a6488782b7e2a5cd74737c177882ebd681"
   end
 
   resource "indexstore-db" do
-    url "https://github.com/apple/indexstore-db/archive/swift-5.2.5-RELEASE.tar.gz"
-    sha256 "cefe69f9b63869acee0564d38d8eb98f449647db9c8df7cd1c59538f506e7f1e"
+    url "https://github.com/apple/indexstore-db/archive/swift-5.3.2-RELEASE.tar.gz"
+    sha256 "4bee784429e5dc7bbdc919eaa9a6035229636884eccb9d15ffa5d079b5aba19a"
   end
 
   resource "sourcekit-lsp" do
-    url "https://github.com/apple/sourcekit-lsp/archive/swift-5.2.5-RELEASE.tar.gz"
-    sha256 "2cb2dffc585a068cefa3bf33d873394f3bccddf8e5e7269889d6960f387ddcfc"
+    url "https://github.com/apple/sourcekit-lsp/archive/swift-5.3.2-RELEASE.tar.gz"
+    sha256 "846d4875e77874bf0a86398cf05446cfb8ed19eaa4dbcc45086c9726a7eafc89"
   end
 
   def install
