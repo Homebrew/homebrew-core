cf072c6d5ec28a1625b74f6be71e9292d46a3a79
ryenus <ryenus@gmail.com>
2020-09-22 20:33:53 -0500

    Update Formula/sonarqube-lts.rb
    Co-authored-by: Sean Molenaar <SMillerDev@users.noreply.github.com>

--- Formula/sonarqube-lts.rb
+++ Formula/sonarqube-lts.rb
@@ -62,14 +62,7 @@ class SonarqubeLts < Formula
       end
 
       puts "pid: #{pid}"
-
-      seconds = 0
-      loop do
-        status = shell_output("#{bin}/sonar status")
-        break if status.match?("is running") || (seconds += 5) > 60
-
-        sleep 5
-      end
+      sleep 60
 
       assert_match "pong", shell_output("curl -s http://127.0.0.1:9000/api/system/ping")
     ensure
