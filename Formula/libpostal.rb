6dadf64e996a170de4622a9c30ea2bd9133927d6
Davey Shafik <dshafik@lyft.com>
2020-09-24 14:12:53 -0700

    Remove unnecessary curl dependency (uses the system curl)

--- Formula/libpostal.rb
+++ Formula/libpostal.rb
@@ -8,7 +8,6 @@ class Libpostal < Formula
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
-  depends_on "curl" => :build
   depends_on "libtool" => :build
   depends_on "pkg-config" => :build
 
