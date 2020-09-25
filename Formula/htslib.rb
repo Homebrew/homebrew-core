830c71ac5ae4185aa5c0ae54e7c0e59633e1d036
Colin <colin.diesh@gmail.com>
2020-09-25 09:24:13 -0400

    htslib 1.11

--- Formula/htslib.rb
+++ Formula/htslib.rb
@@ -1,10 +1,9 @@
 class Htslib < Formula
   desc "C library for high-throughput sequencing data formats"
   homepage "https://www.htslib.org/"
-  url "https://github.com/samtools/htslib/releases/download/1.10.2/htslib-1.10.2.tar.bz2"
-  sha256 "e3b543de2f71723830a1e0472cf5489ec27d0fbeb46b1103e14a11b7177d1939"
+  url "https://github.com/samtools/htslib/releases/download/1.11/htslib-1.11.tar.bz2"
+  sha256 "cffadd9baa6fce27b8fe0b01a462b489f06a5433dfe92121f667f40f632538d7"
   license "MIT"
-  revision 1
 
   livecheck do
     url "https://github.com/samtools/htslib/releases/latest"
