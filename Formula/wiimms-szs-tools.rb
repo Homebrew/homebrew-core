b981a832bb65dc0b8155bc9a1f3a9926d0dfca3e
Larsen Vallecillo <larsen@Weborican.com>
2020-09-25 12:17:08 -0500

    Fix syntax again

--- Formula/wiimms-szs-tools.rb
+++ Formula/wiimms-szs-tools.rb
@@ -1,15 +1,15 @@
 class WiimmsSzsTools < Formula
-  desc "Wiimms SZS Toolset is a set of command-line tools to manipulate files of Mario Kart Wii"
+  desc "Wiimms SZS Toolset is a set of command-line tools to manipulate Wii files"
   homepage "https://szs.wiimm.de/"
   url "https://download.wiimm.de/source/wiimms-szs-tools/wiimms-szs-tools.source-2.19a.txz"
   license "GPL-2.0-or-later"
 
-  depends_on "libpng" => :build
-  uses_from_macos "ncurses"
-
   head do
     url "https://github.com/Wiimm/wiimms-szs-tools.git"
   end
+  
+  depends_on "libpng" => :build
+  uses_from_macos "ncurses"
 
   def install
     cd "project" do
