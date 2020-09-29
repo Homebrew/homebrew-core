85853b548ba0a7849d1a8e646a5e3e91ab363a25
Alistair Scott <ali.scott@gmail.com>
2020-09-29 14:59:57 +0100

    infracost: use go build instead of make install

--- Formula/infracost.rb
+++ Formula/infracost.rb
@@ -1,9 +1,8 @@
 class Infracost < Formula
   desc "Cost estimates for Terraform"
   homepage "https://www.infracost.io/docs/"
-  url "https://github.com/infracost/infracost.git",
-      tag:      "v0.6.0",
-      revision: "ee7f695a199b1ab699cc995a3d5b381088ae2006"
+  url "https://github.com/infracost/infracost/archive/v0.6.0.tar.gz"
+  sha256 "0811501c923eeab88cc8a9bd0402344268402d0caa92398c8eab685b201bb8ce"
   license "Apache-2.0"
   head "https://github.com/infracost/infracost.git"
 
@@ -18,13 +17,13 @@ class Infracost < Formula
   depends_on "terraform" => :test
 
   def install
-    system "git", "--version"
-    system "make", "install"
-    bin.install Dir["#{ENV["GOPATH"]}/bin/infracost"]
+    ENV["CGO_ENABLED"] = "0"
+    ldflags = "-X github.com/infracost/infracost/pkg/version.Version=v#{version}"
+    system "go", "build", *std_go_args, "-ldflags", ldflags, "./cmd/infracost"
   end
 
   test do
-    output = shell_output("#{bin}/infracost --no-color", 1)
+    output = shell_output("#{bin}/infracost --no-color", result = 1)
     assert_equal output.lines.first, "No INFRACOST_API_KEY environment variable is set.\n"
   end
 end
