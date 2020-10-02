93dbb542dd52d41231c0629a85938be0e4d62d91
Michael Bryzek <mike@flow.io>
2020-10-02 12:51:04 -0400

    apibuilder-cli 0.1.39

--- Formula/apibuilder-cli.rb
+++ Formula/apibuilder-cli.rb
@@ -1,8 +1,8 @@
 class ApibuilderCli < Formula
   desc "Command-line interface to generate clients for api builder"
   homepage "https://www.apibuilder.io"
-  url "https://github.com/apicollective/apibuilder-cli/archive/0.1.37.tar.gz"
-  sha256 "4363cba64296e3add243cd3cf9e6e479764451d36f199a2e969251371fad96b1"
+  url "https://github.com/apicollective/apibuilder-cli/archive/0.1.39.tar.gz"
+  sha256 "3c81427519447600154662904c511646673281d9bd38931b63e7b5d9eab776c8"
 
   bottle :unneeded
 
