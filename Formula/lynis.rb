d558e8508a80683dd7c4185fe194185063f799cd
Alex Merkel <mail@alexmerkel.com>
2020-10-06 09:53:22 +0200

    add license

--- Formula/lynis.rb
+++ Formula/lynis.rb
@@ -3,6 +3,7 @@ class Lynis < Formula
   homepage "https://cisofy.com/lynis/"
   url "https://github.com/CISOfy/lynis/archive/3.0.1.tar.gz"
   sha256 "476aa45d6ade0e9624093db13909ecaf670ff8c4f1b372197e400e34f506ce1f"
+  license "GPL-3.0-only"
 
   livecheck do
     url "https://cisofy.com/downloads/lynis/"
