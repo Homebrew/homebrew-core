99501e35e9437319e6648c66054d72c9c0740c12
Tim Bedard <tpbedard@gmail.com>
2020-10-14 10:28:00 -0500

    fix exa completion installation

--- Formula/exa.rb
+++ Formula/exa.rb
@@ -30,9 +30,15 @@ class Exa < Formula
   def install
     system "cargo", "install", *std_cargo_args
 
-    bash_completion.install "contrib/completions.bash" => "exa"
-    zsh_completion.install  "contrib/completions.zsh"  => "_exa"
-    fish_completion.install "contrib/completions.fish" => "exa.fish"
+    begin  # > 0.9
+      bash_completion.install "completions/completions.bash" => "exa"
+      zsh_completion.install  "completions/completions.zsh"  => "_exa"
+      fish_completion.install "completions/completions.fish" => "exa.fish"
+    rescue  # <= 0.9
+      bash_completion.install "contrib/completions.bash" => "exa"
+      zsh_completion.install  "contrib/completions.zsh"  => "_exa"
+      fish_completion.install "contrib/completions.fish" => "exa.fish"
+    end
   end
 
   test do
