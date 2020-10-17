21f9de5fbfc448c17c00f4708318e9134c0c690e
Rui Chen <rui@meetup.com>
2020-10-17 17:05:24 -0400

    vala 0.50.1

--- Formula/vala.rb
+++ Formula/vala.rb
@@ -1,8 +1,8 @@
 class Vala < Formula
   desc "Compiler for the GObject type system"
   homepage "https://wiki.gnome.org/Projects/Vala"
-  url "https://download.gnome.org/sources/vala/0.48/vala-0.48.11.tar.xz"
-  sha256 "54fd1bb356d69824009691955c885bae357eec4eaced6c2e2d038180322e306a"
+  url "https://download.gnome.org/sources/vala/0.50/vala-0.50.1.tar.xz"
+  sha256 "958d9f06c9c3d7d1b2145512a9bc2a7c6aefbbf0416a04c7a0ecf463f7138f6c"
   license "LGPL-2.1-or-later"
 
   livecheck do
