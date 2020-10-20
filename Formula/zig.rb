96b5b570ad8d1f2f76c0c88d4dd1b4acdf08b3cc
Jakub Konka <kubkon@jakubkonka.com>
2020-10-20 11:29:13 +0200

    zig: patch CMakeLists only on stable
    `head` has already been patched, hence it is not necessary.
    
    Signed-off-by: Jakub Konka <kubkon@jakubkonka.com>

--- Formula/zig.rb
+++ Formula/zig.rb
@@ -1,11 +1,18 @@
 class Zig < Formula
   desc "Programming language designed for robustness, optimality, and clarity"
   homepage "https://ziglang.org/"
-  url "https://ziglang.org/download/0.6.0/zig-0.6.0.tar.xz"
-  sha256 "5d167dc19354282dd35dd17b38e99e1763713b9be8a4ba9e9e69284e059e7204"
   license "MIT"
   revision 1
   head "https://github.com/ziglang/zig.git"
+
+  stable do
+    url "https://ziglang.org/download/0.6.0/zig-0.6.0.tar.xz"
+    sha256 "5d167dc19354282dd35dd17b38e99e1763713b9be8a4ba9e9e69284e059e7204"
+
+    # Fix linking issues
+    # https://github.com/Homebrew/homebrew-core/issues/53198
+    patch :DATA
+  end
 
   bottle do
     cellar :any
@@ -16,10 +23,6 @@ class Zig < Formula
 
   depends_on "cmake" => :build
   depends_on "llvm"
-
-  # Fix linking issues
-  # https://github.com/Homebrew/homebrew-core/issues/53198
-  patch :DATA
 
   def install
     system "cmake", ".", *std_cmake_args
