8902cf6a19bfd5f559e5a560b208aff11cda1a3a
Thomas Gotwig <tgotwig@gmail.com>
2020-10-22 19:37:15 +0200

    🦀 Update feroxbuster.rb

--- Formula/feroxbuster.rb
+++ Formula/feroxbuster.rb
@@ -1,27 +1,12 @@
+# typed: false
 # frozen_string_literal: true
 
+# :-)
 class Feroxbuster < Formula
-  desc 'Fast, simple, recursive content discovery tool written in Rust. 🦀'
-  homepage 'https://github.com/epi052/feroxbuster'
-  url 'https://github.com/epi052/feroxbuster/releases/download/v1.1.0/x86_64-macos-feroxbuster.tar.gz'
-  sha256 'cebccff81d0c949918e112032a5daeafaec09b79f855e1cd3df71069359c782d'
-
-  resource 'ferox-config' do
-    url 'https://raw.githubusercontent.com/epi052/feroxbuster/master/ferox-config.toml.example'
-    sha256 '70ace4e70c7f532cc4f7e7958106d035c62bd9d12a6a91de433b815f607911ba'
-  end
-
-  def install
-    config = "#{bin}/ferox-config.toml"
-    example = "#{bin}/ferox-config.toml.example"
-
-    bin.install 'feroxbuster'
-
-    resource('ferox-config').stage do
-      bin.install resource('ferox-config')
-      system 'cp', '-n', example, config
-    end
-  end
+  desc "Fast, simple, recursive content discovery tool written in Rust. 🦀"
+  homepage "https://github.com/epi052/feroxbuster"
+  url "https://github.com/epi052/feroxbuster/releases/download/v1.0.3/x86_64-macos-feroxbuster.tar.gz"
+  sha256 "2fe01730b28a925d3542ea53bc2fcebd659f1c861235b9d8f8492c2c661d35a7"
 
   test do
     assert true
