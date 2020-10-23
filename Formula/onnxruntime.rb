e2142189b54a2d905f260c99f9743804ace34266
Francois-Xavier Coudert <fxcoudert@gmail.com>
2020-10-23 16:05:16 +0200

    onnxruntime: migrate to python@3.9

--- Formula/onnxruntime.rb
+++ Formula/onnxruntime.rb
@@ -5,6 +5,7 @@ class Onnxruntime < Formula
     tag:      "v1.3.1",
     revision: "530117cfdb230228c3429ab39d1b7cf1f68c0567"
   license "MIT"
+  revision 1
 
   bottle do
     cellar :any
@@ -14,13 +15,16 @@ class Onnxruntime < Formula
   end
 
   depends_on "cmake" => :build
-  depends_on "python@3.8" => :build
+  depends_on "python@3.9" => :build
 
   def install
+    # Avoid "no member named 'signbit' in the global namespace"
+    ENV.delete("HOMEBREW_SDKROOT") if MacOS.version == :high_sierra
+
     cmake_args = %W[
       -Donnxruntime_RUN_ONNX_TESTS=OFF
       -Donnxruntime_GENERATE_TEST_REPORTS=OFF
-      -DPYTHON_EXECUTABLE=#{Formula["python@3.8"].opt_bin}/python3
+      -DPYTHON_EXECUTABLE=#{Formula["python@3.9"].opt_bin}/python3
       -Donnxruntime_BUILD_SHARED_LIB=ON
       -Donnxruntime_BUILD_UNIT_TESTS=OFF
       -DCMAKE_BUILD_TYPE=Release
