a26876f9014036986ac95f337cc69eb73befde2c
Francois-Xavier Coudert <fxcoudert@gmail.com>
2020-10-24 00:00:36 +0200

    auditbeat 7.9.3

--- Formula/auditbeat.rb
+++ Formula/auditbeat.rb
@@ -5,6 +5,7 @@ class Auditbeat < Formula
       tag:      "v7.9.3",
       revision: "7aab6a9659749802201db8020c4f04b74cec2169"
   license "Apache-2.0"
+  revision 1
   head "https://github.com/elastic/beats.git"
 
   bottle do
@@ -15,21 +16,20 @@ class Auditbeat < Formula
   end
 
   depends_on "go" => :build
-  depends_on "python@3.8" => :build
+  depends_on "python@3.9" => :build
+  depends_on "freetype"
+  depends_on "jpeg"
 
   resource "virtualenv" do
-    url "https://files.pythonhosted.org/packages/d4/0c/9840c08189e030873387a73b90ada981885010dd9aea134d6de30cd24cb8/virtualenv-15.1.0.tar.gz"
-    sha256 "02f8102c2436bb03b3ee6dede1919d1dac8a427541652e5ec95171ec8adbc93a"
-  end
-
-  # Update MarkupSafe to 1.1.1, remove with next release
-  # https://github.com/elastic/beats/pull/20105
-  patch do
-    url "https://github.com/elastic/beats/commit/5a6ca609259956ff5dd8e4ec80b73e6c96ff54b2.patch?full_index=1"
-    sha256 "b362f8921611297a0879110efcb88a04cf660d120ad81cd078356d502ba4c2ce"
+    url "https://files.pythonhosted.org/packages/28/a8/96e411bfe45092f8aeebc5c154b2f0892bd9ea462d6934b534c1ce7b7402/virtualenv-20.0.35.tar.gz"
+    sha256 "2a72c80fa2ad8f4e2985c06e6fc12c3d60d060e410572f553c90619b0f6efaf3"
   end
 
   def install
+    # Fix compatibility with recent setuptools, remove in 7.10
+    # https://github.com/elastic/beats/pull/20105
+    inreplace "libbeat/tests/system/requirements.txt", "MarkupSafe==1.0", "MarkupSafe==1.1.1"
+
     # remove non open source files
     rm_rf "x-pack"
 
@@ -39,8 +39,11 @@ class Auditbeat < Formula
     xy = Language::Python.major_minor_version "python3"
     ENV.prepend_create_path "PYTHONPATH", buildpath/"vendor/lib/python#{xy}/site-packages"
 
+    # Help Pillow find zlib
+    ENV.append_to_cflags "-I#{MacOS.sdk_path}/usr/include"
+
     resource("virtualenv").stage do
-      system Formula["python@3.8"].opt_bin/"python3", *Language::Python.setup_install_args(buildpath/"vendor")
+      system Formula["python@3.9"].opt_bin/"python3", *Language::Python.setup_install_args(buildpath/"vendor")
     end
 
     ENV.prepend_path "PATH", buildpath/"vendor/bin" # for virtualenv
@@ -54,7 +57,7 @@ class Auditbeat < Formula
 
       system "make", "mage"
       # prevent downloading binary wheels during python setup
-      system "make", "PIP_INSTALL_PARAMS=--no-binary :all", "python-env"
+      system "make", "PIP_INSTALL_PARAMS=--use-pep517 --no-binary :all:", "python-env"
       system "mage", "-v", "build"
       system "mage", "-v", "update"
 
