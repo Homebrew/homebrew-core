6521d0842497031e700dd0103d881a845d31d867
FX Coudert <fxcoudert@gmail.com>
2020-10-26 11:57:06 +0100

    Update taskwarrior-tui.rb

--- Formula/taskwarrior-tui.rb
+++ Formula/taskwarrior-tui.rb
@@ -1,9 +1,8 @@
 class TaskwarriorTui < Formula
   desc "Terminal user interface for taskwarrior"
   homepage "https://github.com/kdheepak/taskwarrior-tui"
-  url "https://github.com/kdheepak/taskwarrior-tui"
-  version "0.8.9"
-  sha256 "6828f3f19ae4fa760258f413e13aa5d62b7e4a6feebc736a0996234227c8389d"
+  url "https://github.com/kdheepak/taskwarrior-tui/archive/v0.8.9.tar.gz"
+  sha256 "d2f5f3fcdf5c3ac1823115f8bd4db8ca6a2806d864d20aa9ce473f1937c2acc9"
   license "MIT"
   head "https://github.com/kdheepak/taskwarrior-tui.git"
 
