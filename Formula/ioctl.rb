bb94332be211f962060556a2ef2335ec3e12b014
Madongming <elsove812@gmail.com>
2020-10-26 10:29:39 +0800

    ioctl v1.1.2

--- Formula/ioctl.rb
+++ Formula/ioctl.rb
@@ -1,8 +1,8 @@
 class Ioctl < Formula
   desc "Command-line interface for interacting with the IoTeX blockchain"
   homepage "https://docs.iotex.io/developer/get-started/ioctl-install.html"
-  url "https://github.com/iotexproject/iotex-core/archive/v1.1.1.tar.gz"
-  sha256 "fcff5ce4231253ebbe04e0405a36a7b89fb2d6c1b30ab0b5deac6ac6f84f9f8b"
+  url "https://github.com/iotexproject/iotex-core/archive/v1.1.2.zip"
+  sha256 "2c33c38481292aad71670a060445cf0d658ac5b22609cc8131f76caa346cdfbb"
   license "Apache-2.0"
 
   bottle do
