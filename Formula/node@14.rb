8812950826906be2754bf09846ef6f8af1c7512d
Eric Veenendaal <eric@veenendaal.co>
2020-10-26 07:23:29 -0500

    Fixing whitespace

--- Formula/node@14.rb
+++ Formula/node@14.rb
@@ -20,13 +20,13 @@ class NodeAT14 < Formula
     system "./configure", "--prefix=#{prefix}", "--with-intl=system-icu"
     system "make", "install"
   end
-  
+
   def post_install
-    (lib/"node_modules/npm/npmrc").atomic_write("prefix = #{HOMEBREW_PREFIX}\n")
+    (lib / "node_modules/npm/npmrc").atomic_write("prefix = #{HOMEBREW_PREFIX}\n")
   end
 
   test do
-    path = testpath/"test.js"
+    path = testpath / "test.js"
     path.write "console.log('hello');"
 
     output = shell_output("#{bin}/node #{path}").strip
@@ -40,14 +40,14 @@ class NodeAT14 < Formula
     # make sure npm can find node
     ENV.prepend_path "PATH", opt_bin
     ENV.delete "NVM_NODEJS_ORG_MIRROR"
-    assert_equal which("node"), opt_bin/"node"
-    assert_predicate bin/"npm", :exist?, "npm must exist"
-    assert_predicate bin/"npm", :executable?, "npm must be executable"
+    assert_equal which("node"), opt_bin / "node"
+    assert_predicate bin / "npm", :exist?, "npm must exist"
+    assert_predicate bin / "npm", :executable?, "npm must be executable"
     npm_args = ["-ddd", "--cache=#{HOMEBREW_CACHE}/npm_cache", "--build-from-source"]
     system "#{bin}/npm", *npm_args, "install", "npm@latest"
     system "#{bin}/npm", *npm_args, "install", "bufferutil"
-    assert_predicate bin/"npx", :exist?, "npx must exist"
-    assert_predicate bin/"npx", :executable?, "npx must be executable"
+    assert_predicate bin / "npx", :exist?, "npx must exist"
+    assert_predicate bin / "npx", :executable?, "npx must be executable"
     assert_match "< hello >", shell_output("#{bin}/npx cowsay hello")
   end
 end
