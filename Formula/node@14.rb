3dd9de9e5565505b93ca8a4db52c40b655caf100
Eric Veenendaal <eric@veenendaal.co>
2020-10-27 08:35:08 -0500

    Apply suggestions from code review
    Co-authored-by: Diogo Azevedo <diogoazevedos@gmail.com>

--- Formula/node@14.rb
+++ Formula/node@14.rb
@@ -18,18 +18,18 @@ class NodeAT14 < Formula
 
   def install
     # make sure subprocesses spawned by make are using our Python 3
-    ENV["PYTHON"] = Formula["python@3.9"].opt_bin / "python3"
+    ENV["PYTHON"] = Formula["python@3.9"].opt_bin/"python3"
 
     system "python3", "configure.py", "--prefix=#{prefix}", "--with-intl=system-icu"
     system "make", "install"
   end
 
   def post_install
-    (lib / "node_modules/npm/npmrc").atomic_write("prefix = #{HOMEBREW_PREFIX}\n")
+    (lib/"node_modules/npm/npmrc").atomic_write("prefix = #{HOMEBREW_PREFIX}\n")
   end
 
   test do
-    path = testpath / "test.js"
+    path = testpath/"test.js"
     path.write "console.log('hello');"
 
     output = shell_output("#{bin}/node #{path}").strip
@@ -43,14 +43,14 @@ class NodeAT14 < Formula
     # make sure npm can find node
     ENV.prepend_path "PATH", opt_bin
     ENV.delete "NVM_NODEJS_ORG_MIRROR"
-    assert_equal which("node"), opt_bin / "node"
-    assert_predicate bin / "npm", :exist?, "npm must exist"
-    assert_predicate bin / "npm", :executable?, "npm must be executable"
+    assert_equal which("node"), opt_bin/"node"
+    assert_predicate bin/"npm", :exist?, "npm must exist"
+    assert_predicate bin/"npm", :executable?, "npm must be executable"
     npm_args = ["-ddd", "--cache=#{HOMEBREW_CACHE}/npm_cache", "--build-from-source"]
     system "#{bin}/npm", *npm_args, "install", "npm@latest"
     system "#{bin}/npm", *npm_args, "install", "bufferutil"
-    assert_predicate bin / "npx", :exist?, "npx must exist"
-    assert_predicate bin / "npx", :executable?, "npx must be executable"
+    assert_predicate bin/"npx", :exist?, "npx must exist"
+    assert_predicate bin/"npx", :executable?, "npx must be executable"
     assert_match "< hello >", shell_output("#{bin}/npx cowsay hello")
   end
 end
