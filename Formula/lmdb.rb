1dec80fdddaca46e5cee7150c78e387c2bcf42f4
Rui Chen <rui@meetup.com>
2020-10-29 01:13:44 -0400

    lmdb: add license
    Signed-off-by: Rui Chen <rui@meetup.com>

--- Formula/lmdb.rb
+++ Formula/lmdb.rb
@@ -3,6 +3,7 @@ class Lmdb < Formula
   homepage "https://symas.com/lmdb/"
   url "https://git.openldap.org/openldap/openldap/-/archive/LMDB_0.9.27/openldap-LMDB_0.9.27.tar.bz2"
   sha256 "2746e429364bfa6f048f2980b8aab6ef461937e33e5c955d7b6242719b2527c4"
+  license "OLDAP-2.8"
   version_scheme 1
   head "https://git.openldap.org/openldap/openldap.git", branch: "mdb.master"
 
