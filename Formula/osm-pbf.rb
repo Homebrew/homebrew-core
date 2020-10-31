854e1b2d4bef9e968a636ccc63b1be06a88a09e2
Rui Chen <chenrui333@gmail.com>
2020-10-30 22:53:18 -0400

    osm-pbf 1.4.0

--- Formula/osm-pbf.rb
+++ Formula/osm-pbf.rb
@@ -1,10 +1,9 @@
 class OsmPbf < Formula
   desc "Tools related to PBF (an alternative to XML format)"
   homepage "https://wiki.openstreetmap.org/wiki/PBF_Format"
-  url "https://github.com/scrosby/OSM-binary/archive/v1.3.3.tar.gz"
-  sha256 "a109f338ce6a8438a8faae4627cd08599d0403b8977c185499de5c17b92d0798"
+  url "https://github.com/scrosby/OSM-binary/archive/v1.4.0.tar.gz"
+  sha256 "be634444d10ad23c127946e0aa7a44bd2d975fc89f53fc022e7504221fbc2cae"
   license "LGPL-3.0"
-  revision 5
 
   bottle do
     cellar :any_skip_relocation
