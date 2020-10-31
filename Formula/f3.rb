b3c8ab2ea332b0482d6af9967f6dac84e03468b6
FX Coudert <fxcoudert@gmail.com>
2020-10-31 22:22:39 +0100

    Update f3.rb

--- Formula/f3.rb
+++ Formula/f3.rb
@@ -3,7 +3,7 @@ class F3 < Formula
   homepage "https://web.archive.org/web/20200110163924/oss.digirati.com.br/f3/"
   url "https://github.com/AltraMayor/f3/archive/v8.0.tar.gz"
   sha256 "fb5e0f3b0e0b0bff2089a4ea6af53278804dfe0b87992499131445732e311ab4"
-  license "GPL-3.0"
+  license "GPL-3.0-only"
   head "https://github.com/AltraMayor/f3.git"
 
   bottle do
