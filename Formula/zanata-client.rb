8b9ef586b71948d1c6fd022747ce24f1b82d0ddf
Jonathan Chang <jchang641@gmail.com>
2020-11-01 15:14:51 +1100

    Update zanata-client.rb

--- Formula/zanata-client.rb
+++ Formula/zanata-client.rb
@@ -3,8 +3,8 @@ class ZanataClient < Formula
   homepage "http://zanata.org/"
   url "https://search.maven.org/remotecontent?filepath=org/zanata/zanata-cli/4.6.2/zanata-cli-4.6.2-dist.tar.gz"
   sha256 "6d4bac8c5b908abf734ff23e0aca9b05f4bc13e66588c526448f241d90473132"
-  revision 2
   license "LGPL-2.1-or-later"
+  revision 2
 
   livecheck do
     url "https://search.maven.org/remotecontent?filepath=org/zanata/zanata-cli/"
