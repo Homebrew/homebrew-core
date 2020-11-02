5d088a235dac0e2f3acd698660e6635fdce89892
mprince <marcel.r.prince@gmail.com>
2020-11-02 11:54:06 -0800

    Add license and livecheck

--- Formula/xmlsh.rb
+++ Formula/xmlsh.rb
@@ -3,6 +3,12 @@ class Xmlsh < Formula
   homepage "http://www.xmlsh.org"
   url "http://xmlsh-org-downloads.s3-website-us-east-1.amazonaws.com/archives%2Frelease-1_3_1%2Fxmlsh_1_3_1.zip"
   sha256 "b52daadac61920a8a5b241795f1348298327db9841c052114f2351b365a23e2f"
+  license "LGPL-3.0-or-later"
+
+  livecheck do	
+    url :stable
+    regex(%r{href=archives%2Frelease-?(\d+(?:\_\d+)+)%2Fxmlsh?(\d+(?:\_\d+)+).zip}i)
+  end
 
   bottle :unneeded
 
