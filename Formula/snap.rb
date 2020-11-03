760d34de3b73738299383cfa2fb05df296498d38
Rui Chen <rui@meetup.com>
2020-11-03 16:45:06 -0500

    snap 2.47.1

--- Formula/snap.rb
+++ Formula/snap.rb
@@ -1,9 +1,9 @@
 class Snap < Formula
   desc "Tool to work with .snap files"
   homepage "https://snapcraft.io/"
-  url "https://github.com/snapcore/snapd/releases/download/2.46/snapd_2.46.vendor.tar.xz"
-  version "2.46"
-  sha256 "c4f532018ca9d2a5f87a95909b3674f8e299e97ba5cb5575895bcdd29be23db3"
+  url "https://github.com/snapcore/snapd/releases/download/2.47.1/snapd_2.47.1.vendor.tar.xz"
+  version "2.47.1"
+  sha256 "4f9666cd15d588017d4287aafdb3e7855748509afaa3002e6d149db1424e032f"
   license "GPL-3.0-only"
 
   livecheck do
