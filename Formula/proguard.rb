865bacedaa90a7ba0f0795940fa7902f98cdcf1c
chenrui <chenrui333@gmail.com>
2020-11-04 20:11:11 +0000

    proguard 7.0.1

--- Formula/proguard.rb
+++ Formula/proguard.rb
@@ -1,8 +1,8 @@
 class Proguard < Formula
   desc "Java class file shrinker, optimizer, and obfuscator"
   homepage "https://www.guardsquare.com/en/products/proguard"
-  url "https://github.com/Guardsquare/proguard/releases/download/v7.0.0/proguard-7.0.0.tar.gz"
-  sha256 "699f16644024000e90c2555083fa6f78a9cdf1d71f384535e82bf987d764f9fb"
+  url "https://github.com/Guardsquare/proguard/releases/download/v7.0.1/proguard-7.0.1.tar.gz"
+  sha256 "b7fd1ee6da650b392ab9fe619f0bfd01f1fe8272620d9471fcfc7908b5216d71"
   license "GPL-2.0"
 
   livecheck do
