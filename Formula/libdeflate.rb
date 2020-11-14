d115aa2d736e4b4b5f9858c4390ed6fba4eadaf2
chenrui <rui@meetup.com>
2020-11-13 20:44:12 -0500

    remove patch

--- Formula/libdeflate.rb
+++ Formula/libdeflate.rb
@@ -12,13 +12,6 @@ class Libdeflate < Formula
     sha256 "841ca895ade3760d2ded53aa4734a2919ca1f74cdf8acfb8cc63c9f3aa4d1165" => :high_sierra
   end
 
-  # Install shared lib symlink as dylib on macOS
-  # https://github.com/ebiggers/libdeflate/pull/74
-  patch do
-    url "https://github.com/ebiggers/libdeflate/commit/061282f1c1e22cf9372835ca163bbe1819b892b9.patch?full_index=1"
-    sha256 "ed1ccd205f3d070aa2a50755e6c27a530fc8273de09e6d9a4a3ea79d529ccdbe"
-  end
-
   def install
     system "make", "install", "PREFIX=#{prefix}"
   end
