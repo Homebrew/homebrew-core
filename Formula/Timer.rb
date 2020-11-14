f2c2339cb5affef9fa6181139c84c85d07a54631
annpocoyo <68903277+annpocoyo@users.noreply.github.com>
2020-11-14 23:10:58 +0000

    Add files via upload

--- Formula/Timer.rb
+++ Formula/Timer.rb
@@ -1,9 +1,11 @@
 class Timer < Formula
-  desc "Command-line tool to set a timer"
+  desc "A command line tool to set a timer"
   homepage "https://github.com/annpocoyo/timer"
   url "https://github.com/annpocoyo/timer/raw/main/archive/Timer-1.0.0.tar.gz"
-  version "1.0.0"
   sha256 "f81266d409a6429be6a6d2cf21bb9caad1714c908484013a1049e3407c67a928"
+  version "1.0.0"
+
+
 
   bottle :unneeded
 
