7f070e155f989da638b0b738717027558a414228
annpocoyo <68903277+annpocoyo@users.noreply.github.com>
2020-11-14 23:42:06 +0000

    Update Timer.rb

--- Formula/Timer.rb
+++ Formula/Timer.rb
@@ -4,7 +4,7 @@ class Timer < Formula
   url "https://github.com/annpocoyo/timer/raw/main/archive/Timer-1.0.0.tar.gz"
   version "1.0.0"
   sha256 "f81266d409a6429be6a6d2cf21bb9caad1714c908484013a1049e3407c67a928"
-  
+
   bottle :unneeded
 
   def install
