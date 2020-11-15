b285e71991fe31a310c2daffd6c5adbfc6452138
annpocoyo <68903277+annpocoyo@users.noreply.github.com>
2020-11-15 00:06:29 +0000

    Update Timer.rb

--- Formula/Timer.rb
+++ Formula/Timer.rb
@@ -1,7 +1,7 @@
 class Timer < Formula
   desc "Command-line tool to set a timer"
   homepage "https://github.com/annpocoyo/timer"
-  url "https://github.com/annpocoyo/timer/raw/main/archive/Timer-1.0.0.tar.gz"
+  url "https://github.com/annpocoyo/timer/raw/master/archive/Timer-1.0.0.tar.gz"
   version "1.0.0"
   sha256 "f81266d409a6429be6a6d2cf21bb9caad1714c908484013a1049e3407c67a928"
 
