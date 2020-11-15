2f86b3b14f8700492324c7eb3eb04a4b17210a42
chenrui <rui@meetup.com>
2020-11-14 20:16:29 -0500

    another try

--- Formula/swift-format.rb
+++ Formula/swift-format.rb
@@ -13,7 +13,7 @@ class SwiftFormat < Formula
     sha256 "d8f72c33efc125e2904e1bec2c8942cca75d75cf81dcab7fcf08ba124af16170" => :catalina
   end
 
-  depends_on xcode: ["12.0", :build]
+  depends_on xcode: "12.0"
 
   def install
     system "swift", "build", "--disable-sandbox", "-c", "release"
