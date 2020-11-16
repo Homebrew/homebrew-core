ba600351eb4582738f25696a534df4002bc793c8
hyuraku <32809703+hyuraku@users.noreply.github.com>
2020-11-16 23:51:07 +0900

    cassandra-reaper: migrate to openjdk8

--- Formula/cassandra-reaper.rb
+++ Formula/cassandra-reaper.rb
@@ -4,10 +4,11 @@ class CassandraReaper < Formula
   url "https://github.com/thelastpickle/cassandra-reaper/releases/download/1.3.0/cassandra-reaper-1.3.0-release.tar.gz"
   sha256 "79c190c51c3404c2efc7f7f1aafa7cfd91f2280cbb1fe719e668966836904efd"
   license "Apache-2.0"
+  revision 1
 
   bottle :unneeded
 
-  depends_on java: "1.8"
+  depends_on "openjdk@8"
 
   def install
     prefix.install "bin"
