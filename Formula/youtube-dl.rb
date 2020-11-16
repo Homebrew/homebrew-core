c43bb7dfa1c6dde51a10066fad528f926824dbbf
DesWurstes <DesWurstes@users.noreply.github.com>
2020-11-16 17:12:04 +0000

    youtube-dl: Revert #63415 To the original repo

--- Formula/youtube-dl.rb
+++ Formula/youtube-dl.rb
@@ -1,23 +1,25 @@
 class YoutubeDl < Formula
-  include Language::Python::Virtualenv
-
   desc "Download YouTube videos from the command-line"
   homepage "https://youtube-dl.org/"
-  url "https://files.pythonhosted.org/packages/e5/1f/a9e2ec4219761bb9ec568fb5457bd870627c2556d6bd2ce8cdbea8694cde/youtube_dl-2020.11.12.tar.gz"
-  sha256 "1491df1707f47207bfd47dd8497d26e9125dd9e4fe2e00780103d4c1b4b2088d"
+  url "https://github.com/ytdl-org/youtube-dl/releases/download/2020.11.12/youtube-dl-2020.11.12.tar.gz"
+  sha256 "aeb00b2079c4711de7eea2a91f6923ebade84c5e985b0caeb47d572395d42831"
   license "Unlicense"
+  revision 1
 
-  bottle do
-    cellar :any_skip_relocation
-    sha256 "4c71c4df2322e5a31e36123d529031b7af2e6626f94bfa79857835df40c7a589" => :catalina
-    sha256 "ede378615a66acc53a13a07b6ee4384ac74decb832179b8893a70ae550c73712" => :mojave
-    sha256 "a5bb66156c7c2d8a8beff32358efb1bc342514397e13be4eaf7b05c6cae0b54e" => :high_sierra
+  head do
+    url "https://github.com/ytdl-org/youtube-dl.git"
+    depends_on "pandoc" => :build
   end
 
-  depends_on "python@3.9"
+  bottle :unneeded
 
   def install
-    virtualenv_install_with_resources
+    system "make", "PREFIX=#{prefix}" if build.head?
+    bin.install "youtube-dl"
+    man1.install "youtube-dl.1"
+    bash_completion.install "youtube-dl.bash-completion"
+    zsh_completion.install "youtube-dl.zsh" => "_youtube-dl"
+    fish_completion.install "youtube-dl.fish"
   end
 
   test do
