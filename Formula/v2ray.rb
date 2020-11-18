c15e680b282e6f5bba9c625340d4a216aa6b4e58
Kid <44045911+kidonng@users.noreply.github.com>
2020-11-18 20:03:59 +0800

    fix license

--- Formula/v2ray.rb
+++ Formula/v2ray.rb
@@ -3,7 +3,7 @@ class V2ray < Formula
   homepage "https://v2fly.org/"
   url "https://github.com/v2fly/v2ray-core/archive/v4.32.1.tar.gz"
   sha256 "f5553ffd04ff226573e8132d9beaa63f4d8f4882eba047225b33620848bc6917"
-  license "MIT"
+  license all_of: ["MIT", "CC-BY-SA-4.0"]
   head "https://github.com/v2fly/v2ray-core.git"
 
   depends_on "go" => :build
