5d12daa3a004c6d1b31101086eb2c9ed6a6b2274
naveen <172697+naveensrinivasan@users.noreply.github.com>
2020-11-19 13:43:31 -0500

    Fixed the formatting issue.

--- kfctl.rb
+++ kfctl.rb
@@ -1,29 +1,28 @@
 class Kfctl < Formula
-    desc "kfctl is a CLI for deploying and managing Kubeflow"
-    homepage "https://github.com/kubeflow/kfctl"
-    url "https://github.com/kubeflow/kfctl.git",
-        tag:      "v1.1.0",
-        revision: "9a3621e2b2ce6d2baa5c78acccec5028c4e4cbe1"
-    sha256 "64fef513c2b648dae771ee07a4f9b63eb0e2faac9b4f6a27a2f7baa7071471f1"
-    license "Apache-2.0"
+  desc "The kfctl is a CLI for deploying and managing Kubeflow"
+  homepage "https://github.com/kubeflow/kfctl"
+  url "https://github.com/kubeflow/kfctl.git",
+      tag:      "v1.1.0",
+      revision: "9a3621e2b2ce6d2baa5c78acccec5028c4e4cbe1"
+  license "Apache-2.0"
+
+  depends_on "go@1.13" => :build
   
-    depends_on "go@1.13" => :build
+  def install
+    system "make", "build"
+    bin.install "bin/darwin/kfctl"
+
+    # Install bash completion
+    output = Utils.safe_popen_read("#{bin}/kfctl", "completion", "bash")
+    (bash_completion/"kfctl").write output
+
+    # Install zsh completion
+    output = Utils.safe_popen_read("#{bin}/kfctl", "completion",  "zsh")
+    (zsh_completion/"kfctl").write output
+  end
   
-    def install
-       system "make", "build"
-       bin.install "bin/darwin/kfctl"
-  
-       # Install bash completion
-       output = Utils.safe_popen_read("#{bin}/kfctl", "completion", "bash")
-       (bash_completion/"kfctl").write output
-  
-       # Install zsh completion
-       output = Utils.safe_popen_read("#{bin}/kfctl", "completion",  "zsh")
-       (zsh_completion/"kfctl").write output
-    end
-  
-    test do
-      run_output = shell_output("#{bin}/kfctl version 2>&1")
-      assert_match "kfctl v1.1.0-0-g9a3621e", run_output
-    end
-  end 
+  test do
+    run_output = shell_output("#{bin}/kfctl version 2>&1")
+    assert_match "kfctl v1.1.0-0-g9a3621e", run_output
+  end
+end
