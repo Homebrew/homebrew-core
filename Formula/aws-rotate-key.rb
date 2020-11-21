ac25a7370b526126146388300937fdfee1f1b39e
Emmanuel Pinault <emmanuel.pinault@outreach.io>
2020-11-20 19:54:54 -0800

    few adjustement for circle build

--- Formula/aws-rotate-key.rb
+++ Formula/aws-rotate-key.rb
@@ -1,11 +1,19 @@
 class AwsRotateKey < Formula
-  desc "Easily rotate your AWS key. 🔑"
+  desc "Easily rotate your AWS access key. 🔑"
   homepage "https://github.com/stefansundin/aws-rotate-key"
-  version "1.0.7"
-  url "https://github.com/stefansundin/aws-rotate-key/releases/download/v#{version}/aws-rotate-key-#{version}-darwin_amd64.zip"
-  sha256 "972d52f480c8e6efe1d2b2a95756a5eef6dff595281b17126b39bc768eee3b9d"
+  url "https://github.com/stefansundin/aws-rotate-key/archive/v1.0.7.tar.gz"
+  sha256 "9dadb689583dc4d8869a346c2e7f12201e1fe65d5bf1d64eb09b69f65518bc44"
+  license "MIT"
+  head "https://github.com/stefansundin/aws-rotate-key.git"
+
+  depends_on "go" => :build
 
   def install
-    bin.install "aws-rotate-key"
+    system "go", "build", *std_go_args
+  end
+  
+  test do
+    assert_match(/^Usage of #{bin}\/aws-rotate-key:/, shell_output("#{bin}/aws-rotate-key --help 2>&1"))
+    assert_match("1.0.7", shell_output("#{bin}/aws-rotate-key --version"))
   end
 end
