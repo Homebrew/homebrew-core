215a62c53f079edb7f36331ecef232830c157287
Peter Newman <peternewman@users.noreply.github.com>
2020-11-22 22:09:34 +0000

    ola: Add back in a patch we do still need and switch back to protobuf 3.6

--- Formula/ola.rb
+++ Formula/ola.rb
@@ -21,12 +21,18 @@ class Ola < Formula
   depends_on "libmicrohttpd"
   depends_on "libusb"
   depends_on "numpy"
-  depends_on "protobuf"
+  depends_on "protobuf@3.6"
   depends_on "python@3.9"
+
+  # remove in version 0.11
+  patch do
+    url "https://raw.githubusercontent.com/macports/macports-ports/89b697d200c7112839e8f2472cd2ff8dfa6509de/net/ola/files/patch-protobuf3.diff"
+    sha256 "bbbcb5952b0bdcd01083cef92b72a747d3adbe7ca9e50d865a0c69ae31a8fb4a"
+  end
 
   def install
     xy = Language::Python.major_minor_version Formula["python@3.9"].bin/"python3"
-    protobuf_pth = Formula["protobuf"].opt_lib/"python#{xy}/site-packages/homebrew-protobuf.pth"
+    protobuf_pth = Formula["protobuf@3.6"].opt_lib/"python#{xy}/site-packages/homebrew-protobuf.pth"
     (buildpath/".brew_home/Library/Python/#{xy}/lib/python/site-packages").install_symlink protobuf_pth
 
     args = %W[
