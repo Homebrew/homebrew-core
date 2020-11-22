518a185920c5d3c9d584cde91cf7ca284aa2c80b
Rui Chen <rui@meetup.com>
2020-11-22 02:29:44 -0500

    octant 0.16.2

--- Formula/octant.rb
+++ Formula/octant.rb
@@ -2,8 +2,8 @@ class Octant < Formula
   desc "Kubernetes introspection tool for developers"
   homepage "https://octant.dev"
   url "https://github.com/vmware-tanzu/octant.git",
-      tag:      "v0.16.1",
-      revision: "8aebb34922f83894fb02ad393740e96ee1b3d8fe"
+      tag:      "v0.16.2",
+      revision: "9a1888da3286b7f4a686939a9c262da2bd4e6f93"
   license "Apache-2.0"
   head "https://github.com/vmware-tanzu/octant.git"
 
