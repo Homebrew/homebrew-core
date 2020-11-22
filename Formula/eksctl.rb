5436d97b78d9006a2842d0059eaed6ef419c6b70
chenrui <chenrui333@gmail.com>
2020-11-22 02:19:46 -0500

    Update eksctl.rb

--- Formula/eksctl.rb
+++ Formula/eksctl.rb
@@ -2,7 +2,7 @@ class Eksctl < Formula
   desc "Simple command-line tool for creating clusters on Amazon EKS"
   homepage "https://eksctl.io"
   url "https://github.com/weaveworks/eksctl.git",
-      tag:      "v0.32.0",
+      tag:      "0.32.0",
       revision: "41d40c256b68823f42a8c71cce7a2eb4d6236ce7"
   license "Apache-2.0"
   head "https://github.com/weaveworks/eksctl.git"
