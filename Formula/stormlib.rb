0ca40269922ae22b990f9c62ff14a61a447013e0
Rui Chen <rui@meetup.com>
2020-11-22 15:23:10 -0500

    fix test
    Signed-off-by: Rui Chen <rui@meetup.com>

--- Formula/stormlib.rb
+++ Formula/stormlib.rb
@@ -22,6 +22,10 @@ class Stormlib < Formula
   patch :DATA
 
   def install
+    # remove in the next release
+    inreplace "src/StormLib.h", "dwCompressionNext = MPQ_COMPRESSION_NEXT_SAME",
+                                  "dwCompressionNext"
+
     system "cmake", ".", *std_cmake_args
     system "make", "install"
     system "cmake", ".", "-DBUILD_SHARED_LIBS=ON", *std_cmake_args
@@ -45,14 +49,25 @@ end
 
 __END__
 diff --git a/CMakeLists.txt b/CMakeLists.txt
-index 76c6aa9..4fd0a46 100644
+index bd8d336..927a47d 100644
 --- a/CMakeLists.txt
 +++ b/CMakeLists.txt
-@@ -297,7 +297,6 @@ target_include_directories(${LIBRARY_NAME} PUBLIC src/)
- set_target_properties(${LIBRARY_NAME} PROPERTIES PUBLIC_HEADER "src/StormLib.h;src/StormPort.h")
- if(BUILD_SHARED_LIBS)
+@@ -314,7 +314,6 @@ if(BUILD_SHARED_LIBS)
+     message(STATUS "Linking against dependent libraries dynamically")
+
      if(APPLE)
 -        set_target_properties(${LIBRARY_NAME} PROPERTIES FRAMEWORK true)
          set_target_properties(${LIBRARY_NAME} PROPERTIES LINK_FLAGS "-framework Carbon")
      endif()
      if(UNIX)
+diff --git a/src/StormLib.h b/src/StormLib.h
+index f254290..43fefb8 100644
+--- a/src/StormLib.h
++++ b/src/StormLib.h
+@@ -480,7 +480,9 @@ typedef void (WINAPI * SFILE_ADDFILE_CALLBACK)(void * pvUserData, DWORD dwBytesW
+ typedef void (WINAPI * SFILE_COMPACT_CALLBACK)(void * pvUserData, DWORD dwWorkType, ULONGLONG BytesProcessed, ULONGLONG TotalBytes);
+
+ struct TFileStream;
++typedef struct TFileStream TFileStream;
+ struct TMPQBits;
++typedef struct TMPQBits TMPQBits;
