688a73127308cd6834a254aac6638190046111e7
Emmanuel Pinault <emmanuel.pinault@outreach.io>
2020-11-24 12:56:54 -0800

    improves tests

--- Formula/aws-rotate-key.rb
+++ Formula/aws-rotate-key.rb
@@ -5,15 +5,23 @@ class AwsRotateKey < Formula
   sha256 "9dadb689583dc4d8869a346c2e7f12201e1fe65d5bf1d64eb09b69f65518bc44"
   license "MIT"
   head "https://github.com/stefansundin/aws-rotate-key.git"
-
   depends_on "go" => :build
-
+  
   def install
     system "go", "build", *std_go_args
   end
-  
+
   test do
-    assert_match(%r{/^Usage of #{bin}/aws-rotate-key:/}, shell_output("#{bin}/aws-rotate-key --help 2>&1"))
-    assert_match("1.0.7", shell_output("#{bin}/aws-rotate-key --version"))
+    (testpath/"credentials").write <<~EOF
+      [default]
+      aws_access_key_id=AKIA123
+      aws_secret_access_key=abc
+    EOF
+    assert_match Regexp.new(<<~EOS), shell_output("AWS_SHARED_CREDENTIALS_FILE=#{testpath}/credentials #{bin}/aws-rotate-key -y 2>&1", 1)
+      ^Using access key AKIA123 from profile "default"\\.
+      Error getting caller identity\\. Is the key disabled\\?
+      InvalidClientTokenId: The security token included in the request is invalid\\.
+      \tstatus code: 403, request id: [a-f0-9\\-]+$
+    EOS
   end
 end
