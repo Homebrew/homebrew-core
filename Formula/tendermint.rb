0a591abff8d1a4b40c198e7ab9f42e168b5fa842
Callum Waters <cmwaters19@gmail.com>
2020-11-25 16:55:33 +0100

    fix syntax

--- Formula/tendermint.rb
+++ Formula/tendermint.rb
@@ -1,5 +1,5 @@
 class Tendermint < Formula
-  desc "BFT state machine replication for applications written in any programming language."
+  desc "BFT state machine replication for applications written in any programming language"
   homepage "https://tendermint.com/"
   url "https://github.com/tendermint/tendermint/archive/v0.34.0.tar.gz"
   sha256 "3a28fac4c5e610fc32763db1b717ec0a1e12d39262e321d6da223c3b0acfea7f"
@@ -15,8 +15,8 @@ class Tendermint < Formula
 
   test do
     shell_output("#{bin}/tendermint init --home /tmp")
-    assert_true File.exists?("/tmp/config/genesis.json")
-    assert_true File.exists?("/tmp/config/config.toml")
-    assert_true Dir.exists?("/tmp/data")
+    assert_true File.exist?("/tmp/config/genesis.json")
+    assert_true File.exist?("/tmp/config/config.toml")
+    assert_true Dir.exist?("/tmp/data")
   end
 end
