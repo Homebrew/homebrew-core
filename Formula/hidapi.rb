d8f90bae732d06cbe6f8d5686608deaac3961fbb
chenrui <chenrui333@gmail.com>
2020-11-25 10:03:08 -0500

    hidapi: update license
    Co-authored-by: Sean Molenaar <SMillerDev@users.noreply.github.com>

--- Formula/hidapi.rb
+++ Formula/hidapi.rb
@@ -3,7 +3,7 @@ class Hidapi < Formula
   homepage "https://github.com/libusb/hidapi"
   url "https://github.com/libusb/hidapi/archive/hidapi-0.10.1.tar.gz"
   sha256 "f71dd8a1f46979c17ee521bc2117573872bbf040f8a4750e492271fc141f2644"
-  license any_of: ["GPL-3.0-only", "BSD-3-Clause"]
+  license :cannot_represent
   head "https://github.com/libusb/hidapi.git"
 
   bottle do
