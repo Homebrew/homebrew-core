ef1f47df03f6a12fa99869545d60c9b99b28f285
Callum Waters <cmwaters19@gmail.com>
2020-11-25 16:05:52 +0100

    use a better test and fix description

--- Formula/tendermint.rb
+++ Formula/tendermint.rb
@@ -1,20 +1,22 @@
 class Tendermint < Formula
-  desc "⟁ Tendermint Core (BFT Consensus) in Go"
+  desc "BFT state machine replication for applications written in any programming language."
   homepage "https://tendermint.com/"
   url "https://github.com/tendermint/tendermint/archive/v0.34.0.tar.gz"
   sha256 "3a28fac4c5e610fc32763db1b717ec0a1e12d39262e321d6da223c3b0acfea7f"
   license "Apache-2.0"
-  depends_on "go" => :build
   head "https://github.com/tendermint/tendermint.git"
 
+  depends_on "go" => :build
+
   def install
-    system "make", "tools"
     system "make", "build", "LDFLAGS=-X github.com/tendermint/tendermint/version.TMCoreSemVer=#{version}"
     bin.install "build/tendermint"
   end
 
   test do
-    tendermint_version_without_hash = shell_output("#{bin}/tendermint version").partition("-").first
-    assert_match version.to_s, tendermint_version_without_hash
+    shell_output("#{bin}/tendermint init --home /tmp")
+    assert_true File.exists?("/tmp/config/genesis.json")
+    assert_true File.exists?("/tmp/config/config.toml")
+    assert_true Dir.exists?("/tmp/data")
   end
 end
