1c970358b07d7c0c8410a44ddd878ecffee642b9
Francois-Xavier Coudert <fxcoudert@gmail.com>
2020-11-27 16:01:47 +0100

    znc: revision bump for icu4c

--- Formula/znc.rb
+++ Formula/znc.rb
@@ -4,7 +4,7 @@ class Znc < Formula
   url "https://znc.in/releases/archive/znc-1.8.2.tar.gz"
   sha256 "ff238aae3f2ae0e44e683c4aee17dc8e4fdd261ca9379d83b48a7d422488de0d"
   license "Apache-2.0"
-  revision 1
+  revision 2
 
   bottle do
     sha256 "3ee6a7b433414a20d4497d267711ae787f1f0c985e89f40352ae0e8db8fd7a6e" => :big_sur
