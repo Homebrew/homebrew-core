0eff8c4b6316c3f8aecd86e1044cf87dcbd4c627
Francois-Xavier Coudert <fxcoudert@gmail.com>
2020-11-27 16:01:38 +0100

    tepl: revision bump for icu4c

--- Formula/tepl.rb
+++ Formula/tepl.rb
@@ -4,6 +4,7 @@ class Tepl < Formula
   url "https://download.gnome.org/sources/tepl/5.0/tepl-5.0.1.tar.xz"
   sha256 "b1274967609f524484b38775fa9ecb296c6d6616aabd052f286339a289912804"
   license "LGPL-2.1-or-later"
+  revision 1
 
   livecheck do
     url :stable
