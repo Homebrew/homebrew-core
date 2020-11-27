f56c23fa5b6292fd9a4aaa2e91e66be9d5ef929a
Dustin Rodrigues <dust.rod@gmail.com>
2020-11-27 18:18:08 -0500

    appium 1.19.1

--- Formula/appium.rb
+++ Formula/appium.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Appium < Formula
   desc "Automation for Apps"
   homepage "https://appium.io/"
-  url "https://registry.npmjs.org/appium/-/appium-1.18.1.tgz"
-  sha256 "938783100df8be224cb85b5dbdcebeb91d164f003d4cdd2aa9c801480bd027b4"
+  url "https://registry.npmjs.org/appium/-/appium-1.19.1.tgz"
+  sha256 "c24f11fe3dba96a9c9d41300e53251b7e8423b88911abd4a5d1ef42bb77fffa0"
   license "Apache-2.0"
   head "https://github.com/appium/appium.git"
 
