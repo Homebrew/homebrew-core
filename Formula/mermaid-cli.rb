8042a86cef460a98c4a0b5982b72a97a50395377
BastianZim <10774221+BastianZim@users.noreply.github.com>
2020-11-27 15:06:58 +0100

    mermaid-cli 8.8.1 (new formula)
    mermaid-cli 8.8.1 (new formula)
    
    Add test

--- Formula/mermaid-cli.rb
+++ Formula/mermaid-cli.rb
@@ -15,5 +15,24 @@ class MermaidCli < Formula
   end
 
   test do
+  
+  shell_output("\
+      cat << EOF | mmdc -o testpath/out.svg\
+      sequenceDiagram\
+          participant Alice\
+          participant Bob\
+          Alice->>John: Hello John, how are you?\
+          loop Healthcheck\
+              John->>John: Fight against hypochondria\
+          end\
+          Note right of John: Rational thoughts <br/>prevail!\
+          John-->>Alice: Great!\
+          John->>Bob: How about you?\
+          Bob-->>John: Jolly good!\
+      EOF\
+    ")
+  
+    assert_predicate(testpath/"out.svg", :exist?)
+  
   end
 end
