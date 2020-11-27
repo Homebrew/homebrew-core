c866b45cf330bb8891cec05c7f791394d4a5d48d
BastianZim <10774221+BastianZim@users.noreply.github.com>
2020-11-27 16:47:27 +0100

    mermaid-cli 8.8.3-2 (new formula)
    mermaid-cli 8.8.3-2 (new formula)
    
    Fix indentation

--- Formula/mermaid-cli.rb
+++ Formula/mermaid-cli.rb
@@ -16,19 +16,19 @@ class MermaidCli < Formula
 
   test do
     (testpath/"test.mmd").write <<~EOS
-            sequenceDiagram
-                participant Alice
-                participant Bob
-                Alice->>John: Hello John, how are you?
-                loop Healthcheck
-                    John->>John: Fight against hypochondria
-                end
-                Note right of John: Rational thoughts <br/>prevail!
-                John-->>Alice: Great!
-                John->>Bob: How about you?
-                Bob-->>John: Jolly good!
+      sequenceDiagram
+          participant Alice
+          participant Bob
+          Alice->>John: Hello John, how are you?
+          loop Healthcheck
+              John->>John: Fight against hypochondria
+          end
+          Note right of John: Rational thoughts <br/>prevail!
+          John-->>Alice: Great!
+          John->>Bob: How about you?
+          Bob-->>John: Jolly good!
     EOS
-    system "mmdc -i #{testpath}/test.mmd -o #{testpath}/out.svg"
+    system "mmdc", "-i", "#{testpath}/test.mmd", "-o", "#{testpath}/out.svg"
 
     assert_predicate(testpath/"out.svg", :exist?)
   end
