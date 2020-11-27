03ef3d0d58eafd394a15e44b6e481ea8291ccb5b
peaceiris <30958501+peaceiris@users.noreply.github.com>
2020-11-27 18:48:46 +0900

    hugo 0.79.0

--- Formula/hugo.rb
+++ Formula/hugo.rb
@@ -1,8 +1,8 @@
 class Hugo < Formula
   desc "Configurable static site generator"
   homepage "https://gohugo.io/"
-  url "https://github.com/gohugoio/hugo/archive/v0.78.2.tar.gz"
-  sha256 "bbfacf3bf379911bc348e6300912838fd07a75ad6de6d41082c54226e0055029"
+  url "https://github.com/gohugoio/hugo/archive/v0.79.0.tar.gz"
+  sha256 "83e9b7e4bd3b321d140d1f35c75eafa6a70d3b814f2cac8e2f78b11feb23f1b2"
   license "Apache-2.0"
   head "https://github.com/gohugoio/hugo.git"
 
