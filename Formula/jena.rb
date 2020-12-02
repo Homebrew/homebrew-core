fa0cf33ae3839d76da98930aedbee3dbfe637f3f
Rui Chen <rui@meetup.com>
2020-12-02 13:12:25 -0500

    jena 3.17.0

--- Formula/jena.rb
+++ Formula/jena.rb
@@ -1,9 +1,9 @@
 class Jena < Formula
   desc "Framework for building semantic web and linked data apps"
   homepage "https://jena.apache.org/"
-  url "https://www.apache.org/dyn/closer.lua?path=jena/binaries/apache-jena-3.16.0.tar.gz"
-  mirror "https://archive.apache.org/dist/jena/binaries/apache-jena-3.16.0.tar.gz"
-  sha256 "6ef65ab3e24948f6f8fa97281a936276d74a8732fb4a14c624c1aa9fa93adb30"
+  url "https://www.apache.org/dyn/closer.lua?path=jena/binaries/apache-jena-3.17.0.tar.gz"
+  mirror "https://archive.apache.org/dist/jena/binaries/apache-jena-3.17.0.tar.gz"
+  sha256 "1c2a51c8a0137c9c12a8928e035351bc6dea99d716a8d2458b9c9467cebc6ec2"
   license "Apache-2.0"
 
   livecheck do
