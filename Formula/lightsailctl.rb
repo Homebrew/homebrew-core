8e6b623cecc2d60f4467fae7e369438d56e0c62d
Dylson Valente Neto <ama@n370.info>
2020-12-02 18:24:25 -0300

    Add a number version to the formula

--- Formula/lightsailctl.rb
+++ Formula/lightsailctl.rb
@@ -2,7 +2,7 @@ class Lightsailctl < Formula
   desc "AWS CLI plugin for publishing images to AWS Lightsail container services"
   homepage "https://lightsail.aws.amazon.com/ls/docs/en_us/articles/amazon-lightsail-install-software"
   url "https://s3.us-west-2.amazonaws.com/lightsailctl/latest/darwin-amd64/lightsailctl"
-  version "latest"
+  version "1.0.0"
   license "Apache-2.0"
 
   depends_on "awscli" => ">2.1.1"
