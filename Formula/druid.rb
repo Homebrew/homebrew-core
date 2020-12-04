0fc920d211e8d82441984f3f144ffb3b9e92197d
Alexander Bayandin <a.bayandin@gmail.com>
2020-12-04 15:19:17 +0000

    druid: undeprecate and fix test

--- Formula/druid.rb
+++ Formula/druid.rb
@@ -12,10 +12,8 @@ class Druid < Formula
 
   bottle :unneeded
 
-  deprecate! because: :does_not_build
-
+  depends_on "zookeeper" => :test
   depends_on "openjdk@8"
-  depends_on "zookeeper"
 
   resource "mysql-metadata-storage" do
     url "http://static.druid.io/artifacts/releases/mysql-metadata-storage-0.12.3.tar.gz"
@@ -48,7 +46,7 @@ class Druid < Formula
     end
 
     bin.install Dir["#{libexec}/bin/*.sh"]
-    bin.env_script_all_files(libexec/"bin", Language::Java.java_home_env("1.8"))
+    bin.env_script_all_files libexec/"bin", Language::Java.overridable_java_home_env("1.8")
 
     Pathname.glob("#{bin}/*.sh") do |file|
       mv file, bin/"druid-#{file.basename}"
@@ -69,13 +67,15 @@ class Druid < Formula
   end
 
   test do
-    ENV["DRUID_CONF_DIR"] = libexec/"conf-quickstart/druid"
+    ENV["DRUID_CONF_DIR"] = libexec/"conf/druid/single-server/nano-quickstart"
     ENV["DRUID_LOG_DIR"] = testpath
     ENV["DRUID_PID_DIR"] = testpath
+    ENV["ZOO_LOG_DIR"] = testpath
 
+    system Formula["zookeeper"].opt_bin/"zkServer", "start"
     begin
       pid = fork { exec bin/"druid-broker.sh", "start" }
-      sleep 30
+      sleep 40
       output = shell_output("curl -s http://localhost:8082/status")
       assert_match /version/m, output
     ensure
