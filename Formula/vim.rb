bf85b4cc7125a1e82730034ce3653dd55cdf8525
chenrui <chenrui333@gmail.com>
2020-12-04 00:13:38 +0000

    vim 8.2.2086

--- Formula/vim.rb
+++ Formula/vim.rb
@@ -2,10 +2,9 @@ class Vim < Formula
   desc "Vi 'workalike' with many additional features"
   homepage "https://www.vim.org/"
   # vim should only be updated every 50 releases on multiples of 50
-  url "https://github.com/vim/vim/archive/v8.2.2050.tar.gz"
-  sha256 "e1013e6c013f57e6d9843c7bb549256673c778e2afd5fbbb131b7aa14e021743"
+  url "https://github.com/vim/vim/archive/v8.2.2086.tar.gz"
+  sha256 "e81ec08f0dfa8cdf5b7836766b077d7a399c446f375c811f4a78b9c104d1c0eb"
   license "Vim"
-  revision 1
   head "https://github.com/vim/vim.git"
 
   bottle do
