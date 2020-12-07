41626e9b1cc4711ae7a2bbc01a48fa0581f4fd9f
chenrui <chenrui333@gmail.com>
2020-12-07 12:07:16 +0000

    osm2pgsql 1.4.0

--- Formula/osm2pgsql.rb
+++ Formula/osm2pgsql.rb
@@ -1,10 +1,9 @@
 class Osm2pgsql < Formula
   desc "OpenStreetMap data to PostgreSQL converter"
   homepage "https://wiki.openstreetmap.org/wiki/Osm2pgsql"
-  url "https://github.com/openstreetmap/osm2pgsql/archive/1.3.0.tar.gz"
-  sha256 "1c0f229047491cf7d054c6f8fcb846eb3163c336340fa82035e19cc2bb7f0179"
+  url "https://github.com/openstreetmap/osm2pgsql/archive/1.4.0.tar.gz"
+  sha256 "403e25a0310d088183a868d80e5325dceee88617d0df570056e50a2930905369"
   license "GPL-2.0-only"
-  revision 1
   head "https://github.com/openstreetmap/osm2pgsql.git"
 
   bottle do
