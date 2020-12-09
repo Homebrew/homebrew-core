b8384f2fae15e92ce6464dd2025b9e76e6d0186e
carlocab <30379873+carlocab@users.noreply.github.com>
2020-12-09 23:39:23 +0000

    winetricks: update license

--- Formula/winetricks.rb
+++ Formula/winetricks.rb
@@ -3,7 +3,7 @@ class Winetricks < Formula
   homepage "https://github.com/Winetricks/winetricks"
   url "https://github.com/Winetricks/winetricks/archive/20201206.tar.gz"
   sha256 "2eb5c09e30be942383e242694e654aa294ed2a396e68c68b5eaa17427bd8242b"
-  license "LGPL-2.1"
+  license "LGPL-2.1-or-later"
   head "https://github.com/Winetricks/winetricks.git"
 
   bottle :unneeded
