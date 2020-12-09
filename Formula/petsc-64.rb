85d29ac89a2864ba58ac62f6c17b83655bbcf52e
gcastigl <giacomo.castiglioni@epfl.ch>
2020-12-09 09:14:32 +0100

    petsc-64 3.14.2 (new formula, new variant of petsc)

---
+++ Formula/petsc-64.rb
@@ -0,0 +1,54 @@
+class Petsc64 < Formula
+  desc "Portable, Extensible Toolkit for Scientific Computation (real, 64 bit indeces)"
+  homepage "https://www.mcs.anl.gov/petsc/"
+  url "https://ftp.mcs.anl.gov/pub/petsc/release-snapshots/petsc-lite-3.14.2.tar.gz"
+  sha256 "87a04fd05cac20a2ec47094b7d18b96e0651257d8c768ced2ef7db270ecfb9cb"
+  license "BSD-2-Clause"
+
+  livecheck do
+    url "https://www.mcs.anl.gov/petsc/download/index.html"
+    regex(/href=.*?petsc-lite[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  depends_on "hdf5"
+  depends_on "hwloc"
+  depends_on "metis"
+  depends_on "netcdf"
+  depends_on "open-mpi"
+  depends_on "scalapack"
+  depends_on "suite-sparse"
+
+  conflicts_with "petsc", because: "petsc must be installed with either 32 bit or 64 bit indeces, not both"
+  conflicts_with "petsc-complex", because: "petsc must be installed with either real or complex support, not both"
+
+  def install
+    system "./configure", "--prefix=#{prefix}",
+                          "--with-64-bit-indices=1",
+                          "--with-debugging=0",
+                          "--with-scalar-type=real",
+                          "--with-x=0",
+                          "--CC=mpicc",
+                          "--CXX=mpicxx",
+                          "--F77=mpif77",
+                          "--FC=mpif90",
+                          "MAKEFLAGS=$MAKEFLAGS"
+    system "make", "all"
+    system "make", "install"
+
+    # Avoid references to Homebrew shims
+    rm_f lib/"petsc/conf/configure-hash"
+    inreplace lib/"petsc/conf/petscvariables", "#{HOMEBREW_SHIMS_PATH}/mac/super/", ""
+  end
+
+  test do
+    test_case = "#{share}/petsc/examples/src/ksp/ksp/tutorials/ex1.c"
+    system "mpicc", test_case, "-I#{include}", "-L#{lib}", "-lpetsc", "-o", "test"
+    output = shell_output("./test")
+    # This PETSc example prints several lines of output. The last line contains
+    # an error norm, expected to be small.
+    line = output.lines.last
+    assert_match /^Norm of error .+, Iterations/, line, "Unexpected output format"
+    error = line.split[3].to_f
+    assert (error >= 0.0 && error < 1.0e-13), "Error norm too large"
+  end
+end
