6689106c6958eb0802635537ec308385fd7a6d8f
carlocab <30379873+carlocab@users.noreply.github.com>
2020-12-10 19:23:31 +0000

    root: revision bump (xrootd 5.0.3)

--- Formula/root.rb
+++ Formula/root.rb
@@ -4,6 +4,7 @@ class Root < Formula
   url "https://root.cern.ch/download/root_v6.22.04.source.tar.gz"
   sha256 "a2f066d85db8eb5b5f1c72573923484a6782a47b94954eff64879609f360e951"
   license "LGPL-2.1-or-later"
+  revision 1
   head "https://github.com/root-project/root.git"
 
   livecheck do
