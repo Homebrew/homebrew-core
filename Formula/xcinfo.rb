c826fea76fb908c4942a2c6cac236e87ea56f795
Gunnar Herzog <gunnar.herzog@kf-interactive.com>
2020-12-10 11:08:58 +0100

    xcinfo 0.5.0 (new formula)
    Adds xcinfo to Homebrew. With xcinfo you can access all information available at xcodereleases.com and install available Xcode versions from Apple's Developer Portal. It also finds and lists installed Xcode applications on hard drive and you can remove them safely.

---
+++ Formula/xcinfo.rb
@@ -0,0 +1,20 @@
+class Xcinfo < Formula
+  desc "Tool to get information about and install available Xcode versions"
+  homepage "https://github.com/xcodereleases/xcinfo"
+  url "https://github.com/xcodereleases/xcinfo/archive/0.5.0.tar.gz"
+  sha256 "07b6c2f5a5b6a5f3cd6d190ac16c36c5011a87f2a1da2a109ccaa909eb6bcc2c"
+  license "MIT"
+
+  depends_on xcode: "12.0"
+
+  def install
+    system "swift", "build",
+        "--configuration", "release",
+        "--disable-sandbox"
+    bin.install '.build/release/xcinfo'
+  end
+
+  test do
+    system "true"
+  end
+end
