99997dc2396539ab583945c8283c25a0f8dd368e
carlocab <30379873+carlocab@users.noreply.github.com>
2020-12-10 20:09:15 +0000

    add license
    See https://github.com/libusb/libusb/search?q=license

--- Formula/libusb.rb
+++ Formula/libusb.rb
@@ -3,7 +3,7 @@ class Libusb < Formula
   homepage "https://libusb.info/"
   url "https://github.com/libusb/libusb/releases/download/v1.0.24/libusb-1.0.24.tar.bz2"
   sha256 "7efd2685f7b327326dcfb85cee426d9b871fd70e22caa15bb68d595ce2a2b12a"
-  license "LGPL-2.1"
+  license "LGPL-2.1-or-later"
 
   livecheck do
     url :stable
