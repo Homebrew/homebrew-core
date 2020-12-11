da33ce0c91b9f204e1fef439b6a0508e507b9324
JP Simard <jp@jpsim.com>
2020-12-11 12:15:01 -0500

    sourcekitten 0.31.0

--- Formula/sourcekitten.rb
+++ Formula/sourcekitten.rb
@@ -2,8 +2,8 @@ class Sourcekitten < Formula
   desc "Framework and command-line tool for interacting with SourceKit"
   homepage "https://github.com/jpsim/SourceKitten"
   url "https://github.com/jpsim/SourceKitten.git",
-      tag:      "0.30.1",
-      revision: "c0f960f72fa1e6151695074ffa696e4da6c45ce8"
+      tag:      "0.31.0",
+      revision: "7f4be006fe73211b0fd9666c73dc2f2303ffa756"
   license "MIT"
   head "https://github.com/jpsim/SourceKitten.git"
 
