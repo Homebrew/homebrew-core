a6abce15421834fc11bec01e327551bc9dbfa777
Bastian Müller <bastian@axiomzen.co>
2020-12-15 14:11:44 -0800

    flow-cli v0.12.1 (new formula)

---
+++ Formula/flow-cli.rb
@@ -0,0 +1,19 @@
+class FlowCli < Formula
+  desc "Command-line interface that provides utilities for building Flow applications"
+  homepage "https://onflow.org"
+  url "https://github.com/onflow/flow-cli/archive/v0.12.1.tar.gz"
+  sha256 "ad231ca19296ca65522eb71115900c9bd6918335caafef5913689acc1ec7effe"
+  license "Apache-2.0"
+  head "https://github.com/onflow/flow-cli.git"
+
+  depends_on "go@1.14" => :build
+
+  def install
+    system "make", "cmd/flow/flow", "VERSION=v0.12.1"
+    bin.install "cmd/flow/flow"
+  end
+
+  test do
+    system "#{bin}/flow", "version"
+  end
+end
