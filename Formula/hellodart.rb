3275428cfe94084732f269ee768f5f90159d15f3
Sainath <Sainath@Macs-MacBook-Pro.local>
2020-12-15 14:54:15 +0530

    formula addind

--- Formula/hellodart.rb
+++ Formula/hellodart.rb
@@ -1,19 +1,18 @@
 class Hellodart < Formula
-  desc "HelloDart"
-  homepage "https://www.example.com"
+  desc "Example App"
+  homepage "https://github.com/SainathChallagundla/helloDart/archive/v0.0.1.zip"
   url "https://github.com/SainathChallagundla/helloDart/archive/v0.0.1.tar.gz"
   version "0.0.1-start"
   sha256 "28531fce0cd32d3358d683c727d76536de2c0a7c968c19843343fdb5da1c49d4"
   license "MIT"
+
   depends_on "cmake" => :build
+
   def install
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
-    system "cmake", ".", *std_cmake_args
+    bin.install "helloDart"
   end
+
   test do
-     `test do`
+    assert_match "helloDart version 1.0.0", shell_output("#{bin}/mytool -v", 2)
   end
 end
