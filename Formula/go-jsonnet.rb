003ea4415aa674411a4b3a4e953de8efe46274a0
Christian Höltje <docwhat@gerf.org>
2020-12-15 16:38:00 -0500

    go-jsonnet: enable deps util

--- Formula/go-jsonnet.rb
+++ Formula/go-jsonnet.rb
@@ -20,6 +20,7 @@ class GoJsonnet < Formula
   def install
     system "go", "build", "-o", bin/"jsonnet", "./cmd/jsonnet"
     system "go", "build", "-o", bin/"jsonnetfmt", "./cmd/jsonnetfmt"
+    system "go", "build", "-o", bin/"jsonnet-deps", "./cmd/jsonnet-deps"
   end
 
   test do
