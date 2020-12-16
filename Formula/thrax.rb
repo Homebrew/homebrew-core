be541fddb7e3785c2329c6464e10b6bf03f3e111
Marcus Better <mbetter@spotify.com>
2020-12-16 13:30:58 -0500

    Shorten description

--- Formula/thrax.rb
+++ Formula/thrax.rb
@@ -1,5 +1,5 @@
 class Thrax < Formula
-  desc "Tools for compiling context-dependent grammars into finite state transducers"
+  desc "Tools for compiling grammars into finite state transducers"
   homepage "http://www.openfst.org/twiki/bin/view/GRM/Thrax"
   url "http://www.openfst.org/twiki/pub/GRM/ThraxDownload/thrax-1.3.5.tar.gz"
   sha256 "823182c9bca7f866437c0d8db9fc4c90688766f4492239bfbd73be20687c622e"
