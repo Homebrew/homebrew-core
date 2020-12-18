53ab1bedefd4d648b234158293bb2924e6ffa950
San Juan <rsanjuan1987@gmail.com>
2020-12-18 18:49:44 -0300

    Update picta-dl.rb

--- Formula/picta-dl.rb
+++ Formula/picta-dl.rb
@@ -1,16 +1,13 @@
 class PictaDl < Formula
   desc "Download videos from Picta.cu Plataforma de Contenidos Audiovisuales"
   homepage "https://github.com/oleksis/picta-dl/"
-  url "https://github.com/oleksis/picta-dl/archive/v2020.12.14.zip"
+  url "https://github.com/oleksis/picta-dl/archive/v2020.12.14.tar.gz"
   version "2020.12.14"
   license "Unlicense"
-  sha256 ""
-  
   depends_on "python@3.9"
-    
   def install
     libexec.install Dir['*']
-    system "echo \"python #{libexec}/picta-dl \\$@\"> #{libexec}/picta-dl.run "
+    system 'echo "python #{libexec}/picta-dl \\$@"> #{libexec}/picta-dl.run'
     bin.install_symlink "#{libexec}/picta-dl.run" => "picta-dl"
     libexec.install_symlink "#{libexec}/picta_dl/__main__.py" => "picta-dl"
     system "chmod +x #{bin}/picta-dl"
