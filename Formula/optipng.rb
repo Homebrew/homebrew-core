4380a537b37bf3f552e8844c2a2074465329624f
Ignacio Serrano <ignic@ignic.com>
2020-12-21 21:10:45 +0100

    optipng: add license

--- Formula/optipng.rb
+++ Formula/optipng.rb
@@ -3,6 +3,7 @@ class Optipng < Formula
   homepage "https://optipng.sourceforge.io/"
   url "https://downloads.sourceforge.net/project/optipng/OptiPNG/optipng-0.7.7/optipng-0.7.7.tar.gz"
   sha256 "4f32f233cef870b3f95d3ad6428bfe4224ef34908f1b42b0badf858216654452"
+  license "Zlib"
   head "http://hg.code.sf.net/p/optipng/mercurial", using: :hg
 
   livecheck do
