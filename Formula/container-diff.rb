9eb154941bcf8bcd0489981df6354cff07583a05
Rui Chen <rui@meetup.com>
2020-12-22 12:10:13 -0500

    container-diff 0.16.0

--- Formula/container-diff.rb
+++ Formula/container-diff.rb
@@ -1,8 +1,8 @@
 class ContainerDiff < Formula
   desc "Diff your Docker containers"
   homepage "https://github.com/GoogleContainerTools/container-diff"
-  url "https://github.com/GoogleContainerTools/container-diff/archive/v0.15.0.tar.gz"
-  sha256 "4bdd73a81b6f7a988cf270236471016525d0541f5fe04286043f3db28e4b250c"
+  url "https://github.com/GoogleContainerTools/container-diff/archive/v0.16.0.tar.gz"
+  sha256 "255e08e82ffb9139b78054cd0caf0c20b1e6ab8fc359a9a8558da3912b70aba5"
   license "Apache-2.0"
 
   bottle do
