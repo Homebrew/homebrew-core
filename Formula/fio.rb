c6dde7bb1186573e53060b770742c29ed2fde351
Rui Chen <rui@meetup.com>
2020-12-23 17:33:02 -0500

    fio 3.25

--- Formula/fio.rb
+++ Formula/fio.rb
@@ -1,8 +1,8 @@
 class Fio < Formula
   desc "I/O benchmark and stress test"
   homepage "https://github.com/axboe/fio"
-  url "https://github.com/axboe/fio/archive/fio-3.19.tar.gz"
-  sha256 "809963b1d023dbc9ac7065557af8129aee17b6895e0e8c5ca671b0b14285f404"
+  url "https://github.com/axboe/fio/archive/fio-3.25.tar.gz"
+  sha256 "d8157676bc78a50f3ac82ffc6f80ffc3bba93cbd892fc4882533159a0cdbc1e8"
   license "GPL-2.0"
 
   bottle do
