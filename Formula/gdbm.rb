598b4c12e26a6623bc6809046670fd7aa551d9cc
chenrui <rui@meetup.com>
2020-12-23 14:44:44 -0500

    gdbm: update license

--- Formula/gdbm.rb
+++ Formula/gdbm.rb
@@ -4,7 +4,7 @@ class Gdbm < Formula
   url "https://ftp.gnu.org/gnu/gdbm/gdbm-1.19.tar.gz"
   mirror "https://ftpmirror.gnu.org/gdbm/gdbm-1.19.tar.gz"
   sha256 "37ed12214122b972e18a0d94995039e57748191939ef74115b1d41d8811364bc"
-  license "GPL-3.0"
+  license "GPL-2.0-or-later"
 
   livecheck do
     url :stable
