6039bab6a18ede9efa8f057570eafb7cb3b454b2
Rui Chen <rui@meetup.com>
2020-12-24 15:48:30 -0500

    esptool: switch to pypi source
    Signed-off-by: Rui Chen <rui@meetup.com>

--- Formula/esptool.rb
+++ Formula/esptool.rb
@@ -3,8 +3,8 @@ class Esptool < Formula
 
   desc "ESP8266 and ESP32 serial bootloader utility"
   homepage "https://github.com/espressif/esptool"
-  url "https://github.com/espressif/esptool/archive/v3.0.tar.gz"
-  sha256 "4c0069f0dccdf9c0927e890dafd8e0307a29872bf15f6deed12c8776acd1579d"
+  url "https://files.pythonhosted.org/packages/dd/3d/d1d4c004927e6e6807c441ce70330ed969c725d2906053fbd2ff994b4439/esptool-3.0.tar.gz"
+  sha256 "87953d235fed2c9adb1292b3769df0149686c9afdb1896dd963f730453cbc934"
   license "GPL-2.0-or-later"
 
   bottle do
