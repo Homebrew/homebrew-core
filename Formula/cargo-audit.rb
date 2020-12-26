5acb0ca733890ecf6c1924f52a6ddb1fda7932ef
Rui Chen <rui@meetup.com>
2020-12-26 15:44:08 -0500

    cargo-audit: simplify assertion check
    Signed-off-by: Rui Chen <rui@meetup.com>

--- Formula/cargo-audit.rb
+++ Formula/cargo-audit.rb
@@ -21,26 +21,7 @@ class CargoAudit < Formula
     assert_predicate HOMEBREW_CACHE/"cargo_cache/advisory-db", :exist?
     assert_match "Couldn't load Cargo.lock: I/O error", output
 
-    expected_output = <<~EOS
-          Fetching advisory database from `https://github.com/RustSec/advisory-db.git`
-            Loaded 170 security advisories (from #{HOMEBREW_CACHE}/cargo_cache/advisory-db)
-          Updating crates.io index
-          Scanning Cargo.lock for vulnerabilities (3 crate dependencies)
-      Crate:         base64
-      Version:       0.5.1
-      Title:         Integer overflow leads to heap-based buffer overflow in encode_config_buf
-      Date:          2017-05-03
-      ID:            RUSTSEC-2017-0004
-      URL:           https://rustsec.org/advisories/RUSTSEC-2017-0004
-      Solution:      Upgrade to >=0.5.2
-      Dependency tree: 
-      base64 0.5.1
-      └── base64_vuln 0.1.0
-
-      error: 1 vulnerability found!
-    EOS
-
     cp_r "#{pkgshare}/support/base64_vuln/.", testpath
-    assert_equal expected_output, shell_output("#{bin}/cargo-audit audit 2>&1", 1)
+    assert_match "error: 1 vulnerability found!", shell_output("#{bin}/cargo-audit audit 2>&1", 1)
   end
 end
