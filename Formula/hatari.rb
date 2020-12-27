64a9ed63c5331cdb6faa5797204ef6254f4c7ea8
Rui Chen <rui@meetup.com>
2020-12-27 12:49:25 -0500

    hatari: update emutos image
    Signed-off-by: Rui Chen <rui@meetup.com>

--- Formula/hatari.rb
+++ Formula/hatari.rb
@@ -27,8 +27,8 @@ class Hatari < Formula
 
   # Download EmuTOS ROM image
   resource "emutos" do
-    url "https://downloads.sourceforge.net/project/emutos/emutos/0.9.10/emutos-512k-0.9.10.zip"
-    sha256 "773bbbfc418827d863c313c4f1d3c73ef3d296c5f23b4d00ee4f38f080a9c255"
+    url "https://downloads.sourceforge.net/project/emutos/emutos/1.0.1/emutos-512k-1.0.1.zip"
+    sha256 "96c698aa0fc0f51ecdb0f8b53484df9de273215467b5de3f44d245821dff795e"
   end
 
   def install
