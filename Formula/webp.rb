718b6e26990690b2b4a1ef3956b5c8964c872c22
Sam Ford <1584702+samford@users.noreply.github.com>
2020-12-27 19:21:39 -0500

    webp: relocate head

--- Formula/webp.rb
+++ Formula/webp.rb
@@ -5,6 +5,13 @@ class Webp < Formula
   sha256 "98a052268cc4d5ece27f76572a7f50293f439c17a98e67c4ea0c7ed6f50ef043"
   license "BSD-3-Clause"
 
+  head do
+    url "https://chromium.googlesource.com/webm/libwebp.git"
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+  end
+
   bottle do
     cellar :any
     sha256 "5723d4b2701f6c4348419164c38b11cd9c274dca35925fc8c3fbf0c8c14cfc4d" => :big_sur
@@ -12,13 +19,6 @@ class Webp < Formula
     sha256 "27c76a7054277ff5a2e844c5996fc731d8644acbaaa505d35dba42c4a48a0c51" => :catalina
     sha256 "819c76cbf75c1d1d51db88602b69a9d9cd24975cc65834a9eb5a804c4b96ee35" => :mojave
     sha256 "069cac577750d53095cc43a05a3eab54310c35ea819ea05fa6bf425bcb0313d2" => :high_sierra
-  end
-
-  head do
-    url "https://chromium.googlesource.com/webm/libwebp.git"
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
-    depends_on "libtool" => :build
   end
 
   depends_on "jpeg"
