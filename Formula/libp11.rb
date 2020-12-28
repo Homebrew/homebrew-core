615527e890cc2a21a2c91f62064bd1e3c7805f08
Sam Ford <1584702+samford@users.noreply.github.com>
2020-12-27 19:21:01 -0500

    libp11: relocate head

--- Formula/libp11.rb
+++ Formula/libp11.rb
@@ -5,6 +5,12 @@ class Libp11 < Formula
   sha256 "57d47a12a76fd92664ae30032cf969284ebac1dfc25bf824999d74b016d51366"
   license "LGPL-2.1-or-later"
 
+  head do
+    url "https://github.com/OpenSC/libp11.git"
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+  end
+
   livecheck do
     url :head
     regex(/^libp11[._-]v?(\d+(?:\.\d+)+)$/i)
@@ -17,12 +23,6 @@ class Libp11 < Formula
     sha256 "9da63ed34ade8ca89b600207b22d9fcc9a707aee31e3325f73c5da473e1df481" => :catalina
     sha256 "14a94b35751b0b820206edecd55dd713079ea20a1e72b049d290b10283a80895" => :mojave
     sha256 "95fac2824261ddc121f443a372174131eb9f31fe784a724bee7667af1302bba8" => :high_sierra
-  end
-
-  head do
-    url "https://github.com/OpenSC/libp11.git"
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
   end
 
   depends_on "pkg-config" => :build
