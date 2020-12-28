546d8e63006c0d6ce732c0a809f2154c3db187b1
Sam Ford <1584702+samford@users.noreply.github.com>
2020-12-27 19:21:02 -0500

    libslax: relocate head

--- Formula/libslax.rb
+++ Formula/libslax.rb
@@ -5,6 +5,13 @@ class Libslax < Formula
   sha256 "4da6fb9886e50d75478d5ecc6868c90dae9d30ba7fc6e6d154fc92e6a48d9a95"
   license "BSD-3-Clause"
 
+  head do
+    url "https://github.com/Juniper/libslax.git"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+  end
+
   livecheck do
     url :stable
     strategy :github_latest
@@ -16,13 +23,6 @@ class Libslax < Formula
     sha256 "8b4506f10c72d75425ad849f17918a6574c349ebdf29ab740ad323811d1a4d02" => :catalina
     sha256 "5e024a22f8a47c0a11724d7543cd50141e8246b3669155cd734854ee74ec9d71" => :mojave
     sha256 "95e8b6bdc7010103110d8c7a92c33dd8e2e04228e037ca81c3a5cb69ea955ab2" => :high_sierra
-  end
-
-  head do
-    url "https://github.com/Juniper/libslax.git"
-
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
   end
 
   depends_on "libtool" => :build
