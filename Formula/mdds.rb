7a9d8a7f84a17bfd5edc4db448fab21a9e902520
Sam Ford <1584702+samford@users.noreply.github.com>
2020-12-27 19:21:08 -0500

    mdds: relocate head

--- Formula/mdds.rb
+++ Formula/mdds.rb
@@ -5,6 +5,12 @@ class Mdds < Formula
   sha256 "a66a2a8293a3abc6cd9baff7c236156e2666935cbfb69a15d64d38141638fecf"
   license "MIT"
 
+  head do
+    url "https://gitlab.com/mdds/mdds.git"
+
+    depends_on "automake" => :build
+  end
+
   livecheck do
     url :head
   end
@@ -16,12 +22,6 @@ class Mdds < Formula
     sha256 "a5e6a996bf112bca0d5c7e628fd15128977b9075938155a4185aaf5613d136bc" => :catalina
     sha256 "5146b50529f63030c978dbdab3755ce7a7383d7c8049e03ae1186fa231f867c9" => :mojave
     sha256 "67f497efa10f695da64e4769d3ef5de6fb0e9d3d0d62026c2105b7c5148b91a9" => :high_sierra
-  end
-
-  head do
-    url "https://gitlab.com/mdds/mdds.git"
-
-    depends_on "automake" => :build
   end
 
   depends_on "autoconf" => :build
