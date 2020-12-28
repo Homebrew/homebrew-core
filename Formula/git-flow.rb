368d9a31048acc34d91e4f5e62a449e978fd85bd
Sam Ford <1584702+samford@users.noreply.github.com>
2020-12-27 19:20:40 -0500

    git-flow: relocate head

--- Formula/git-flow.rb
+++ Formula/git-flow.rb
@@ -15,6 +15,14 @@ class GitFlow < Formula
     end
   end
 
+  head do
+    url "https://github.com/nvie/gitflow.git", branch: "develop"
+
+    resource "completion" do
+      url "https://github.com/bobthecow/git-flow-completion.git", branch: "develop"
+    end
+  end
+
   bottle do
     cellar :any_skip_relocation
     sha256 "46009ab147b52d74bbda2a0b4d97374f0e515a5157db4ff803f6270ded7fde48" => :big_sur
@@ -26,14 +34,6 @@ class GitFlow < Formula
     sha256 "56826b30d91ffb54829f4792f88c673b1c3e748aa662bef5806e4a6f5d0ee015" => :el_capitan
     sha256 "a5e97d4d5c082194b36c18e7b051c43b2d5b37366b2ac56c5ea9407f6315685b" => :yosemite
     sha256 "8e931605a0d12cffa282db7244e0041cc14f8a7692e184a6bc1975800be2dac0" => :mavericks
-  end
-
-  head do
-    url "https://github.com/nvie/gitflow.git", branch: "develop"
-
-    resource "completion" do
-      url "https://github.com/bobthecow/git-flow-completion.git", branch: "develop"
-    end
   end
 
   conflicts_with "git-flow-avh", because: "both install `git-flow` binaries and completions"
