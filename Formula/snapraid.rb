a77b5a5ca16d47be94dfadcfad182e44c59c5d8a
Sam Ford <1584702+samford@users.noreply.github.com>
2020-12-27 19:21:32 -0500

    snapraid: relocate head

--- Formula/snapraid.rb
+++ Formula/snapraid.rb
@@ -5,6 +5,13 @@ class Snapraid < Formula
   sha256 "1f5267261bdbcf4d48b9359ce67184df11905590739140f740327fb73bcecafa"
   license "GPL-3.0"
 
+  head do
+    url "https://github.com/amadvance/snapraid.git"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+  end
+
   bottle do
     cellar :any_skip_relocation
     sha256 "4d5bca9f5667e2f9a4fc841cb89f60ec5de200a59ace28513b777b6092c1f13f" => :big_sur
@@ -12,13 +19,6 @@ class Snapraid < Formula
     sha256 "d92da7dbdf737efbbeee7025c7d1ed0bc01c1f3cbaf29f0b8ded4b264ff627e8" => :catalina
     sha256 "8ccbba9450a1f49a2d1b9d0424d9dee7ffaac4348f0cf4edf0bfb2b8858f5885" => :mojave
     sha256 "db38538cd61796483d63bb4cf8aa687f8801796abb4074e2a5a69e8cefcaae96" => :high_sierra
-  end
-
-  head do
-    url "https://github.com/amadvance/snapraid.git"
-
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
   end
 
   def install
