bfd2e1b4f2a87e4b1e8c8b79365a3bd332653540
Sam Ford <1584702+samford@users.noreply.github.com>
2020-12-27 19:21:22 -0500

    pdfgrep: relocate head

--- Formula/pdfgrep.rb
+++ Formula/pdfgrep.rb
@@ -5,6 +5,13 @@ class Pdfgrep < Formula
   sha256 "0ef3dca1d749323f08112ffe68e6f4eb7bc25f56f90a2e933db477261b082aba"
   license "GPL-2.0"
 
+  head do
+    url "https://gitlab.com/pdfgrep/pdfgrep.git"
+    depends_on "asciidoc" => :build
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+  end
+
   bottle do
     cellar :any
     sha256 "cd405f59148773985d647019bbec794d99477ca93216510f007864f8d6612b32" => :big_sur
@@ -13,13 +20,6 @@ class Pdfgrep < Formula
     sha256 "4e6828ef5db24086dae00e10c9c18671352303c6e79a2148f62bd9104678ea08" => :mojave
     sha256 "95ffadce5ed5baa82a48c71e1bb8915d080c9e9d4a14e63982945eb543e58b10" => :high_sierra
     sha256 "b004e7801489c6cb0361c5032278d11cafd4ace151a02ee97214c79dba0f89be" => :sierra
-  end
-
-  head do
-    url "https://gitlab.com/pdfgrep/pdfgrep.git"
-    depends_on "asciidoc" => :build
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
   end
 
   depends_on "pkg-config" => :build
