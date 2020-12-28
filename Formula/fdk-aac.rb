36367290f872693a1ecc758eec6062b5f0f9d16c
Sam Ford <1584702+samford@users.noreply.github.com>
2020-12-27 19:20:34 -0500

    fdk-aac: relocate head

--- Formula/fdk-aac.rb
+++ Formula/fdk-aac.rb
@@ -4,6 +4,14 @@ class FdkAac < Formula
   url "https://downloads.sourceforge.net/project/opencore-amr/fdk-aac/fdk-aac-2.0.1.tar.gz"
   sha256 "840133aa9412153894af03b27b03dde1188772442c316a4ce2a24ed70093f271"
 
+  head do
+    url "https://git.code.sf.net/p/opencore-amr/fdk-aac.git"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+  end
+
   livecheck do
     url :stable
   end
@@ -15,14 +23,6 @@ class FdkAac < Formula
     sha256 "526c83a79b7f208f07e8d04ad5ce47c8104d90a76034e42422d124fba128ba3c" => :catalina
     sha256 "9353da38e4b43913964f9cdc5fc2a28c4b6c0a19ceef4e2a58db62ba3fdb4d49" => :mojave
     sha256 "a8812e355f28272c7d8153bcda0e48a58c0bd0a880ed0256a2bc460143d1df78" => :high_sierra
-  end
-
-  head do
-    url "https://git.code.sf.net/p/opencore-amr/fdk-aac.git"
-
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
-    depends_on "libtool" => :build
   end
 
   def install
