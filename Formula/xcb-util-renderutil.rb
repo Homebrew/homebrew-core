a54a47031e6de3bb0169ac8a1b3df4bfdf523737
Sam Ford <1584702+samford@users.noreply.github.com>
2020-12-27 19:21:42 -0500

    xcb-util-renderutil: relocate head

--- Formula/xcb-util-renderutil.rb
+++ Formula/xcb-util-renderutil.rb
@@ -5,20 +5,20 @@ class XcbUtilRenderutil < Formula
   sha256 "c6e97e48fb1286d6394dddb1c1732f00227c70bd1bedb7d1acabefdd340bea5b"
   license all_of: ["X11", "HPND-sell-variant"]
 
-  bottle do
-    cellar :any
-    sha256 "0941200260ef409b5daa61664cad100fe69b08e99b8cb440297079387e2dadff" => :big_sur
-    sha256 "7091c73aa3571aa8c4cc2568175f91b3dffe7714dbb1ab334c86da174395d2e1" => :arm64_big_sur
-    sha256 "5fb7ef030a443af89504e74d04fccf3000ac04bf152798e7d4242247e2378ae2" => :catalina
-    sha256 "b0a2c992673650129ee49fcb4fbe6873ef4b8d29a5677ae873a27e05fc7a0d27" => :mojave
-  end
-
   head do
     url "https://gitlab.freedesktop.org/xorg/lib/libxcb-render-util.git"
 
     depends_on "autoconf" => :build
     depends_on "automake" => :build
     depends_on "libtool" => :build
+  end
+
+  bottle do
+    cellar :any
+    sha256 "0941200260ef409b5daa61664cad100fe69b08e99b8cb440297079387e2dadff" => :big_sur
+    sha256 "7091c73aa3571aa8c4cc2568175f91b3dffe7714dbb1ab334c86da174395d2e1" => :arm64_big_sur
+    sha256 "5fb7ef030a443af89504e74d04fccf3000ac04bf152798e7d4242247e2378ae2" => :catalina
+    sha256 "b0a2c992673650129ee49fcb4fbe6873ef4b8d29a5677ae873a27e05fc7a0d27" => :mojave
   end
 
   depends_on "pkg-config" => [:build, :test]
