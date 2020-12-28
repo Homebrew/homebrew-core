a9eaf155e9cad604a00b6982df372c92cbd785ac
Sam Ford <1584702+samford@users.noreply.github.com>
2020-12-27 19:20:19 -0500

    avrdude: relocate head

--- Formula/avrdude.rb
+++ Formula/avrdude.rb
@@ -6,6 +6,14 @@ class Avrdude < Formula
   sha256 "0f9f731b6394ca7795b88359689a7fa1fba818c6e1d962513eb28da670e0a196"
   revision 1
 
+  head do
+    url "https://svn.savannah.nongnu.org/svn/avrdude/trunk/avrdude"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+  end
+
   livecheck do
     url "https://download.savannah.gnu.org/releases/avrdude/"
     regex(/href=.*?avrdude[._-]v?(\d+(?:\.\d+)+)\.t/i)
@@ -18,14 +26,6 @@ class Avrdude < Formula
     sha256 "b0cb94b5c4f01fcc870f286bca293218c98fda23d76397db8a831272f7087038" => :high_sierra
     sha256 "e8e26af5565cd897867d4e6e71e66e6e946e1e21eb4e27d3cd49f199f088fc5d" => :sierra
     sha256 "c953526dc893a9b162a109d074edf8bb71d7049c63990282edc994c63de90c44" => :el_capitan
-  end
-
-  head do
-    url "https://svn.savannah.nongnu.org/svn/avrdude/trunk/avrdude"
-
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
-    depends_on "libtool" => :build
   end
 
   depends_on "libelf"
