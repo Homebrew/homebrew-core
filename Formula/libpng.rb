6bbbd3a7ec535866243a8e2328345a39029fcf68
Sam Ford <1584702+samford@users.noreply.github.com>
2020-12-27 19:21:01 -0500

    libpng: relocate head

--- Formula/libpng.rb
+++ Formula/libpng.rb
@@ -6,6 +6,14 @@ class Libpng < Formula
   sha256 "505e70834d35383537b6491e7ae8641f1a4bed1876dbfe361201fc80868d88ca"
   license "libpng-2.0"
 
+  head do
+    url "https://github.com/glennrp/libpng.git"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+  end
+
   livecheck do
     url :stable
     regex(%r{url=.*?/libpng[._-]v?(\d+(?:\.\d+)+)\.t}i)
@@ -19,14 +27,6 @@ class Libpng < Formula
     sha256 "53bbd14cc27c86c16605e256e7646a1b5656c253abca084958c5d80a2961cb01" => :mojave
     sha256 "bbdd94bdd5954bc50c096391486e67265dce5631efb913dcffe4469806a242b6" => :high_sierra
     sha256 "e66797079a9a8134f91bd36b58054c6c32f6a9cd161c1bd19f0192319edb80aa" => :sierra
-  end
-
-  head do
-    url "https://github.com/glennrp/libpng.git"
-
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
-    depends_on "libtool" => :build
   end
 
   uses_from_macos "zlib"
