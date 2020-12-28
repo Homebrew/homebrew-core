edb947d2ce0c4107c087fd53f6c3f99b8a3e36bd
Sam Ford <1584702+samford@users.noreply.github.com>
2020-12-27 19:20:48 -0500

    isl: relocate head

--- Formula/isl.rb
+++ Formula/isl.rb
@@ -12,6 +12,14 @@ class Isl < Formula
   sha256 "28658ce0f0bdb95b51fd2eb15df24211c53284f6ca2ac5e897acc3169e55b60f"
   license "MIT"
 
+  head do
+    url "https://repo.or.cz/isl.git"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+  end
+
   livecheck do
     url :homepage
     regex(/href=.*?isl[._-]v?(\d+(?:\.\d+)+)\.t/i)
@@ -24,14 +32,6 @@ class Isl < Formula
     sha256 "b5319e3bbbb36ef3536d841999b7497b3dce4bf9e07fb04f6b0db716e087896d" => :catalina
     sha256 "29213891860c971e084d1e2a3d1ad00c92371140dea599aae2894e26ec0d6874" => :mojave
     sha256 "a1193c8b06c31abc4bf9c1ef9bb93c4879ff5ba4050b4a06c22c2a0048d3c87f" => :high_sierra
-  end
-
-  head do
-    url "https://repo.or.cz/isl.git"
-
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
-    depends_on "libtool" => :build
   end
 
   depends_on "gmp"
