90d1a851d1ff52923d5d9c66e7af0564878ea0fc
Sam Ford <1584702+samford@users.noreply.github.com>
2020-12-27 19:20:33 -0500

    eventql: relocate head

--- Formula/eventql.rb
+++ Formula/eventql.rb
@@ -5,20 +5,20 @@ class Eventql < Formula
   sha256 "a61f093bc45a1f9b9b374331ab40665c0c1060a2278b2833c0b6eb6c547b4ef4"
   license "AGPL-3.0"
 
-  bottle do
-    cellar :any
-    rebuild 1
-    sha256 "f14adb77f2c1a4ab8ca08a55a14884e5f87058e10895bf3558e7e5b5df6329f6" => :catalina
-    sha256 "b6f264a76ce93195c2de6708d497c59dcb7192da13038247a33b3fd7aae5ce9a" => :mojave
-    sha256 "9f0440ead195557859530cfb429c82cea72b3ad7caf3dbb6e149b5959890ad4e" => :high_sierra
-  end
-
   head do
     url "https://github.com/eventql/eventql.git"
 
     depends_on "autoconf" => :build
     depends_on "automake" => :build
     depends_on "libtool" => :build
+  end
+
+  bottle do
+    cellar :any
+    rebuild 1
+    sha256 "f14adb77f2c1a4ab8ca08a55a14884e5f87058e10895bf3558e7e5b5df6329f6" => :catalina
+    sha256 "b6f264a76ce93195c2de6708d497c59dcb7192da13038247a33b3fd7aae5ce9a" => :mojave
+    sha256 "9f0440ead195557859530cfb429c82cea72b3ad7caf3dbb6e149b5959890ad4e" => :high_sierra
   end
 
   def install
