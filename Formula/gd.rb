fbe31e994dc000e0231039c734393a121ebbdfd9
Sam Ford <1584702+samford@users.noreply.github.com>
2020-12-27 19:20:38 -0500

    gd: relocate head

--- Formula/gd.rb
+++ Formula/gd.rb
@@ -5,6 +5,14 @@ class Gd < Formula
   sha256 "ecd9155b9a417fb3f837f29e5966323796de247789163761dd72dbf83bfcac58"
   license :cannot_represent
 
+  head do
+    url "https://github.com/libgd/libgd.git"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+  end
+
   bottle do
     cellar :any
     sha256 "57db02960b120179fcd18257989a043f6d6f82212dde034e6efeffd9ec75434a" => :big_sur
@@ -12,14 +20,6 @@ class Gd < Formula
     sha256 "ebc4192da4580942545084cf2f5c36dc4645a5c83244224905e01dee4e50837e" => :catalina
     sha256 "c014efe5f692b3146a4416c0acdaad3c632064d50aad2c18598cfb32fb31ee69" => :mojave
     sha256 "0bd97ae0be0bfaa7554d0628a69b5fd8cba27de7ff5bde0533d4a1b6445be614" => :high_sierra
-  end
-
-  head do
-    url "https://github.com/libgd/libgd.git"
-
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
-    depends_on "libtool" => :build
   end
 
   depends_on "fontconfig"
