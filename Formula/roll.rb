fa279c13b895d3b39e4b73081ee61b2ef889a3a0
Sam Ford <1584702+samford@users.noreply.github.com>
2020-12-27 19:21:27 -0500

    roll: relocate head

--- Formula/roll.rb
+++ Formula/roll.rb
@@ -5,6 +5,12 @@ class Roll < Formula
   sha256 "a06b9782225442c71347eaf745e47684a9227d55575e865f503266c06454586c"
   license "GPL-2.0"
 
+  head do
+    url "https://github.com/matteocorti/roll.git"
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+  end
+
   bottle do
     cellar :any_skip_relocation
     sha256 "ac062af086fdaa6ad2d85967c4aed4bb2153f7a2111c0fcd84662a2023f00025" => :big_sur
@@ -13,12 +19,6 @@ class Roll < Formula
     sha256 "c1c2251d5ac3b6a03065877f6c211f326909c7710f85441adf0d9987815b6c35" => :mojave
     sha256 "0fe1da205f9d37a29489ac8b437cefcd915041c47ddf95cae577369b46d8d7ea" => :high_sierra
     sha256 "f9c7a833dad6d95fe4ee83fccf0890f49dee962e9965a624cf5d483d86c72eab" => :sierra
-  end
-
-  head do
-    url "https://github.com/matteocorti/roll.git"
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
   end
 
   def install
