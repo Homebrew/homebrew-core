fc841b0ce0a64e74f9dc0f484e972217d8fd2208
Sam Ford <1584702+samford@users.noreply.github.com>
2020-12-27 19:20:28 -0500

    deja-gnu: relocate head

--- Formula/deja-gnu.rb
+++ Formula/deja-gnu.rb
@@ -6,6 +6,12 @@ class DejaGnu < Formula
   sha256 "0d0671e1b45189c5fc8ade4b3b01635fb9eeab45cf54f57db23e4c4c1a17d261"
   license "GPL-3.0"
 
+  head do
+    url "https://git.savannah.gnu.org/git/dejagnu.git"
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+  end
+
   livecheck do
     url :stable
   end
@@ -18,12 +24,6 @@ class DejaGnu < Formula
     sha256 "eea1adea3a1c062fd6ba0e85fefebe9f9036736a9d3a3744cec79123390270f3" => :mojave
     sha256 "eea1adea3a1c062fd6ba0e85fefebe9f9036736a9d3a3744cec79123390270f3" => :high_sierra
     sha256 "5c1100eaf8ae4f28b1c4311241ddff8e0d195d0d241e106051bc60490d28d0e5" => :sierra
-  end
-
-  head do
-    url "https://git.savannah.gnu.org/git/dejagnu.git"
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
   end
 
   uses_from_macos "expect"
