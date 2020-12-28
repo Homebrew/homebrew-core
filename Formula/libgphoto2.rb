b0c858dccb29ae0dd66699d4c06fbbd04d5021f1
Sam Ford <1584702+samford@users.noreply.github.com>
2020-12-27 19:20:56 -0500

    libgphoto2: relocate head

--- Formula/libgphoto2.rb
+++ Formula/libgphoto2.rb
@@ -5,6 +5,14 @@ class Libgphoto2 < Formula
   sha256 "7740fea3cbb78489071cdfec06c07fab528e69e27ac82734eae6a73eaeeabcd4"
   license "LGPL-2.1-or-later"
 
+  head do
+    url "https://github.com/gphoto/libgphoto2.git"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "gettext" => :build
+  end
+
   livecheck do
     url :stable
     regex(%r{url=.*?/libgphoto2[._-]v?(\d+(?:\.\d+)+)\.t}i)
@@ -16,14 +24,6 @@ class Libgphoto2 < Formula
     sha256 "9ab56abc466d07fdb2d16aced13ae7afffb9e71642b8c86a0b1610304e34baba" => :catalina
     sha256 "434336b976e6c930255891e9d478fec925c484f50fdf153dca262a9184b6874b" => :mojave
     sha256 "918032eba6577fda5b956841d50abd91c5272a0d1b69d21f3ac9f40eb24027af" => :high_sierra
-  end
-
-  head do
-    url "https://github.com/gphoto/libgphoto2.git"
-
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
-    depends_on "gettext" => :build
   end
 
   depends_on "pkg-config" => :build
