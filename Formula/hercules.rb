072007e1f3f66192546fb694e00a903c8ad87e72
Sam Ford <1584702+samford@users.noreply.github.com>
2020-12-27 19:20:45 -0500

    hercules: relocate head

--- Formula/hercules.rb
+++ Formula/hercules.rb
@@ -5,6 +5,13 @@ class Hercules < Formula
   sha256 "890c57c558d58708e55828ae299245bd2763318acf53e456a48aac883ecfe67d"
   license "QPL-1.0"
 
+  head do
+    url "https://github.com/hercules-390/hyperion.git"
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+  end
+
   livecheck do
     url :homepage
     regex(/href=.*?hercules[._-]v?(\d+(?:\.\d+)+)\.t/i)
@@ -17,13 +24,6 @@ class Hercules < Formula
     sha256 "f1feaf922ae9105c64ba207bc9e2d9b573ddcae8b6feaba501a6daf3068e9901" => :high_sierra
     sha256 "a9ca5fff16a7aa506e2067d7bad9bbb8c54ede7af0e1102150f5d385a7097e9d" => :sierra
     sha256 "cf3d8203cb207792e0c800aadc86ee78714795316e936870f5a4ceae53bfdacc" => :el_capitan
-  end
-
-  head do
-    url "https://github.com/hercules-390/hyperion.git"
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
-    depends_on "libtool" => :build
   end
 
   skip_clean :la
