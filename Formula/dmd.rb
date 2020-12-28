5c79c2e9b20cde5874c627183866820180f2a92a
Sam Ford <1584702+samford@users.noreply.github.com>
2020-12-27 19:20:30 -0500

    dmd: relocate head

--- Formula/dmd.rb
+++ Formula/dmd.rb
@@ -23,12 +23,6 @@ class Dmd < Formula
     end
   end
 
-  bottle do
-    sha256 "2e9456d05fbcda78e4b2a7dbd56ba423a36ff06b754c80a15c4a1b483ed484bd" => :big_sur
-    sha256 "4524e9544890a56bcac7adf0928f91fc918c438d6a174fe4c97d7808c4b80412" => :catalina
-    sha256 "7a8683ad4e9c67dbcd9f0851ae677649150620d1a167a9901ec1bab69f941581" => :mojave
-  end
-
   head do
     url "https://github.com/dlang/dmd.git"
 
@@ -43,6 +37,12 @@ class Dmd < Formula
     resource "tools" do
       url "https://github.com/dlang/tools.git"
     end
+  end
+
+  bottle do
+    sha256 "2e9456d05fbcda78e4b2a7dbd56ba423a36ff06b754c80a15c4a1b483ed484bd" => :big_sur
+    sha256 "4524e9544890a56bcac7adf0928f91fc918c438d6a174fe4c97d7808c4b80412" => :catalina
+    sha256 "7a8683ad4e9c67dbcd9f0851ae677649150620d1a167a9901ec1bab69f941581" => :mojave
   end
 
   uses_from_macos "unzip" => :build
