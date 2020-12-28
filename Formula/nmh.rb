6083a561a2f8ae05c182c25cbf394005e1590d3f
Sam Ford <1584702+samford@users.noreply.github.com>
2020-12-27 19:21:16 -0500

    nmh: relocate head

--- Formula/nmh.rb
+++ Formula/nmh.rb
@@ -5,6 +5,13 @@ class Nmh < Formula
   sha256 "f1fb94bbf7d95fcd43277c7cfda55633a047187f57afc6c1bb9321852bd07c11"
   revision 1
 
+  head do
+    url "https://git.savannah.nongnu.org/git/nmh.git"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+  end
+
   livecheck do
     url "https://download.savannah.gnu.org/releases/nmh/"
     regex(/href=.*?nmh[._-]v?(\d+(?:\.\d+)+)\.t/i)
@@ -17,13 +24,6 @@ class Nmh < Formula
     sha256 "9c53df993c2f8f0dd45dea112d082d7cfc67efd34e58be2f62d95bfb44d3c9b7" => :mojave
     sha256 "f46c5ca5a87081a9a8f7f454e0cb36de543dff666ac0ba8e1bcb867a2f7e7763" => :high_sierra
     sha256 "8b70320e7046fc756ba490a44d5f7923a0e45db4f645dc02f80c19823eefa823" => :sierra
-  end
-
-  head do
-    url "https://git.savannah.nongnu.org/git/nmh.git"
-
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
   end
 
   depends_on "openssl@1.1"
