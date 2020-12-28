36b7513804952a69b8bd5228c4669895c43f0c62
Sam Ford <1584702+samford@users.noreply.github.com>
2020-12-27 19:20:35 -0500

    fping: relocate head

--- Formula/fping.rb
+++ Formula/fping.rb
@@ -5,6 +5,12 @@ class Fping < Formula
   sha256 "ed38c0b9b64686a05d1b3bc1d66066114a492e04e44eef1821d43b1263cd57b8"
   license "BSD-3-Clause"
 
+  head do
+    url "https://github.com/schweikert/fping.git"
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+  end
+
   bottle do
     cellar :any_skip_relocation
     sha256 "012cad012acbaf32885f1d260cfa464478a0a71ad396f0711813c7f2b183112d" => :big_sur
@@ -12,12 +18,6 @@ class Fping < Formula
     sha256 "bd1255921afca543ba440bbf84f86f7c3b0b10db4bbf1aa659a2aa686496e4d5" => :catalina
     sha256 "47f38d4902f03da1e407331848e1f3a75a2b8692e4366d8a0a341e66f36962f1" => :mojave
     sha256 "e2d14a6c1de9032a244f7185ba8a629d61f8ed2964b96490890c87336ff4d521" => :high_sierra
-  end
-
-  head do
-    url "https://github.com/schweikert/fping.git"
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
   end
 
   def install
