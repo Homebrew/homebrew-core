15bd40dde451e8824a21454f04be726923585730
Sam Ford <1584702+samford@users.noreply.github.com>
2020-12-27 19:20:22 -0500

    chinadns-c: relocate head

--- Formula/chinadns-c.rb
+++ Formula/chinadns-c.rb
@@ -5,6 +5,12 @@ class ChinadnsC < Formula
   sha256 "abfd433e98ac0f31b8a4bd725d369795181b0b6e8d1b29142f1bb3b73bbc7230"
   license "GPL-3.0"
 
+  head do
+    url "https://github.com/shadowsocks/ChinaDNS.git"
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+  end
+
   bottle do
     cellar :any_skip_relocation
     sha256 "d15cde6788156aa67dffd280752d52f5aac1ef1e8f56c8e5864ce05b9c81647a" => :big_sur
@@ -16,12 +22,6 @@ class ChinadnsC < Formula
     sha256 "a620bce8421a9773233c51886c6845995569a1fda80e252efa86f6271c1d274c" => :el_capitan
     sha256 "329351a4f82e4f871cbc2b384902b0c84040bd1df222970d67be7513bbead207" => :yosemite
     sha256 "4266825a8ecbb6a84d459b41465fe9318a918b171880ac8ddfd8bdf3e37573d1" => :mavericks
-  end
-
-  head do
-    url "https://github.com/shadowsocks/ChinaDNS.git"
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
   end
 
   def install
