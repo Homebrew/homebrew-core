7e30747081e6943d04f3c9a0824771a1ef316cf9
Sam Ford <1584702+samford@users.noreply.github.com>
2020-12-27 19:21:34 -0500

    swig: relocate head

--- Formula/swig.rb
+++ Formula/swig.rb
@@ -5,6 +5,13 @@ class Swig < Formula
   sha256 "d53be9730d8d58a16bf0cbd1f8ac0c0c3e1090573168bfa151b01eb47fa906fc"
   license "GPL-3.0"
 
+  head do
+    url "https://github.com/swig/swig.git"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+  end
+
   livecheck do
     url :stable
   end
@@ -15,13 +22,6 @@ class Swig < Formula
     sha256 "530e80b7e7dcd28469b52fc3b668683a97b72642ebf2b6d4e6708d14f05e7286" => :catalina
     sha256 "50afb5930cb37af2e400f0369f6da15b1d4922c1f72f45d13e7e3f8bd9d6d27b" => :mojave
     sha256 "8bab440005b048ce454a3dd50ba608e1f85391edd73e9e40510269e923cad238" => :high_sierra
-  end
-
-  head do
-    url "https://github.com/swig/swig.git"
-
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
   end
 
   depends_on "pcre"
