60a2b6eeb459e12913cb2bc547694f40e8f39652
Sam Ford <1584702+samford@users.noreply.github.com>
2020-12-27 19:20:37 -0500

    fsevents-tools: relocate head

--- Formula/fsevents-tools.rb
+++ Formula/fsevents-tools.rb
@@ -5,6 +5,14 @@ class FseventsTools < Formula
   sha256 "498528e1794fa2b0cf920bd96abaf7ced15df31c104d1a3650e06fa3f95ec628"
   license "Apache-2.0"
 
+  head do
+    url "https://github.com/ggreer/fsevents-tools.git"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "pkg-config" => :build
+  end
+
   bottle do
     cellar :any_skip_relocation
     sha256 "da9e4eed81589e2ea9e7f6a9186cd178ad965d5cba6b71ed2a3515729cd1cfb7" => :big_sur
@@ -16,14 +24,6 @@ class FseventsTools < Formula
     sha256 "a15d1b927a74d8b76b553d696343ac5fe82396f8b5b052e15adc6c3270f2ccd6" => :el_capitan
     sha256 "9d4bda3175c503f8ff65f134495de43471854eacc3396a9cf1895d5c2b91cb63" => :yosemite
     sha256 "045442f0279c20196e0941fbf160557ebdea04169267ba8105717d7d207cc346" => :mavericks
-  end
-
-  head do
-    url "https://github.com/ggreer/fsevents-tools.git"
-
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
-    depends_on "pkg-config" => :build
   end
 
   def install
