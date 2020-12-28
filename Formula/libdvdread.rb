3e77d1b89e3afd8179fb3e94c2f6c51fa2ca2f0b
Sam Ford <1584702+samford@users.noreply.github.com>
2020-12-27 19:20:55 -0500

    libdvdread: relocate head

--- Formula/libdvdread.rb
+++ Formula/libdvdread.rb
@@ -5,6 +5,13 @@ class Libdvdread < Formula
   sha256 "3e357309a17c5be3731385b9eabda6b7e3fa010f46022a06f104553bf8e21796"
   license "GPL-2.0"
 
+  head do
+    url "https://code.videolan.org/videolan/libdvdread.git"
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+  end
+
   bottle do
     cellar :any
     sha256 "732ca22426ca97c3b96a975a3ee25ad12b74de870f58b506f36c7e9b7e91d328" => :big_sur
@@ -12,13 +19,6 @@ class Libdvdread < Formula
     sha256 "83bebe58015f6f34973afa003934f183e7ac9202f5e579cfd12536f9ceac1719" => :catalina
     sha256 "7405838fee2b93209c2bd0834db89c2a2334a94f7d368feb87599da1b08062f6" => :mojave
     sha256 "c881a8c1c872d922f45bf8a692b9d79b5f6ade1a2f4a48d470d05491bc017436" => :high_sierra
-  end
-
-  head do
-    url "https://code.videolan.org/videolan/libdvdread.git"
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
-    depends_on "libtool" => :build
   end
 
   depends_on "libdvdcss"
