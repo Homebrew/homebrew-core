f17fc794b1a520310f6d9ec7119574d8ed4f8368
Sam Ford <1584702+samford@users.noreply.github.com>
2020-12-27 19:21:39 -0500

    verilator: relocate head

--- Formula/verilator.rb
+++ Formula/verilator.rb
@@ -5,6 +5,12 @@ class Verilator < Formula
   sha256 "e31f43fcd984f17c0f10fb4e26bed4203919a5c8c9f1beafc9aafcb5f85d72b7"
   license any_of: ["LGPL-3.0-only", "Artistic-2.0"]
 
+  head do
+    url "https://git.veripool.org/git/verilator", using: :git
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+  end
+
   livecheck do
     url "https://github.com/verilator/verilator.git"
     regex(/^v?(\d+(?:\.\d+)+)$/i)
@@ -15,12 +21,6 @@ class Verilator < Formula
     sha256 "11944229b6818c17042010a7267303350da32275864d0c5d6414d00097522263" => :arm64_big_sur
     sha256 "8c9d1253dd911dca93ddc8e2da53aed9b634c0cafa26609b13a123254d7679c0" => :catalina
     sha256 "f45ae45c72884a8da166bbc4fb0eea4f4e602aaa8c89c2c2594cc851dd46338b" => :mojave
-  end
-
-  head do
-    url "https://git.veripool.org/git/verilator", using: :git
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
   end
 
   skip_clean "bin" # Allows perl scripts to keep their executable flag
