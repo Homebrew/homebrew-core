70d676d5ea327a36beebcd76e329d8406aae8e5f
Sam Ford <1584702+samford@users.noreply.github.com>
2020-12-27 19:21:36 -0500

    thrift: relocate head

--- Formula/thrift.rb
+++ Formula/thrift.rb
@@ -6,6 +6,15 @@ class Thrift < Formula
   sha256 "7ad348b88033af46ce49148097afe354d513c1fca7c607b59c33ebb6064b5179"
   license "Apache-2.0"
 
+  head do
+    url "https://github.com/apache/thrift.git"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+    depends_on "pkg-config" => :build
+  end
+
   livecheck do
     url :stable
   end
@@ -18,15 +27,6 @@ class Thrift < Formula
     sha256 "9fff4084e59bf612da35f7e731c82f5a1d714aec8ba860a2521c0ca1d73731d4" => :catalina
     sha256 "840fbc8db938bc1b8e50d16f733bcd22a8918efee276cbf969fc79f779380b5d" => :mojave
     sha256 "bec0a20279bf36bcd960c71b9e417e41a53479e8a575034bef426994e7ecc546" => :high_sierra
-  end
-
-  head do
-    url "https://github.com/apache/thrift.git"
-
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
-    depends_on "libtool" => :build
-    depends_on "pkg-config" => :build
   end
 
   depends_on "bison" => :build
