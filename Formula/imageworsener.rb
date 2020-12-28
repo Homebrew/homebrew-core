3ef811c4879eae6b9400e5c2f2ec8507f223407f
Sam Ford <1584702+samford@users.noreply.github.com>
2020-12-27 19:20:47 -0500

    imageworsener: relocate head

--- Formula/imageworsener.rb
+++ Formula/imageworsener.rb
@@ -5,6 +5,13 @@ class Imageworsener < Formula
   sha256 "7c4b6e5f3da333e336f014805c441bc08aded652dd9dde2abd40be33b1aa3e25"
   license "MIT"
 
+  head do
+    url "https://github.com/jsummers/imageworsener.git"
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+  end
+
   bottle do
     cellar :any
     sha256 "3391e73af0da2054295db408889f460025d789d6088162dd92ce67229fdfb564" => :big_sur
@@ -12,13 +19,6 @@ class Imageworsener < Formula
     sha256 "b5e6ce352f0e698cf10452d273ae0e61f50554565f77010de4e62a6fdddd911f" => :mojave
     sha256 "2332dd0ecedf78344ee5fbd3d00abb0eccc7b28b7e8609c9a18e8e6ab81669de" => :high_sierra
     sha256 "847f3211aba4095e280d589a87698234b7cd6e3ec77a6a50cf578a3fa6d0236e" => :sierra
-  end
-
-  head do
-    url "https://github.com/jsummers/imageworsener.git"
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
-    depends_on "libtool" => :build
   end
 
   depends_on "jpeg"
