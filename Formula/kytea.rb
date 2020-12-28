c00af2bc286c0734ddbbc9b9d8d55da3510ceab0
Sam Ford <1584702+samford@users.noreply.github.com>
2020-12-27 19:20:51 -0500

    kytea: relocate head

--- Formula/kytea.rb
+++ Formula/kytea.rb
@@ -5,6 +5,13 @@ class Kytea < Formula
   sha256 "534a33d40c4dc5421f053c71a75695c377df737169f965573175df5d2cff9f46"
   license "Apache-2.0"
 
+  head do
+    url "https://github.com/neubig/kytea.git"
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+  end
+
   bottle do
     sha256 "2efc4bc6d1c77859c5012819331672e30b9e8c4491c696aac132e8356e08b483" => :big_sur
     sha256 "927aac3d562cc2977f84670c850ab262a05a010bfe7e7f16aa0eb7d9532eae7b" => :catalina
@@ -14,13 +21,6 @@ class Kytea < Formula
     sha256 "3e0c66a7efb34ddb8e4f80d9b95562779e224271b8d63d38f9bc8176103427e2" => :el_capitan
     sha256 "2f2dda314728cd74750db339ebc2d166b8b611ad54668cc3e7b6225d39aec3f5" => :yosemite
     sha256 "045d0c9ad0cf35e003b8839cb0213e3f49d9107dfbc955e449b36fd4b6596640" => :mavericks
-  end
-
-  head do
-    url "https://github.com/neubig/kytea.git"
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
-    depends_on "libtool" => :build
   end
 
   def install
