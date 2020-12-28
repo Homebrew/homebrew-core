4950a115a90cc6a267b0114b601776629277d5b7
Sam Ford <1584702+samford@users.noreply.github.com>
2020-12-27 19:21:13 -0500

    nasm: relocate head

--- Formula/nasm.rb
+++ Formula/nasm.rb
@@ -5,6 +5,14 @@ class Nasm < Formula
   sha256 "3caf6729c1073bf96629b57cee31eeb54f4f8129b01902c73428836550b30a3f"
   license "BSD-2-Clause"
 
+  head do
+    url "https://github.com/netwide-assembler/nasm.git"
+    depends_on "asciidoc" => :build
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "xmlto" => :build
+  end
+
   livecheck do
     url "https://www.nasm.us/pub/nasm/releasebuilds/"
     regex(%r{href=.*?v?(\d+(?:\.\d+)+)/?["' >]}i)
@@ -17,14 +25,6 @@ class Nasm < Formula
     sha256 "1875e67160bac1675dd505d66a6b78469767d2dfe8baab2652409f91ac0549ef" => :catalina
     sha256 "4b3614f857264edfa9aeab961c523b3910cdef0ceccaf9957888b477c1c512f4" => :mojave
     sha256 "8f8c181994b1f05bf425a4034f76d6973c9e1a85ecb64af7f67d47556f23a0d6" => :high_sierra
-  end
-
-  head do
-    url "https://github.com/netwide-assembler/nasm.git"
-    depends_on "asciidoc" => :build
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
-    depends_on "xmlto" => :build
   end
 
   def install
