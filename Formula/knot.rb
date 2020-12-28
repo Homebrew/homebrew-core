b9fb144d47b46a4fc23ac3a9d3fb2b319b4a25c4
Sam Ford <1584702+samford@users.noreply.github.com>
2020-12-27 19:20:50 -0500

    knot: relocate head

--- Formula/knot.rb
+++ Formula/knot.rb
@@ -5,6 +5,14 @@ class Knot < Formula
   sha256 "fbc51897ef0ed0639ebad59b988a91382b9544288a2db8254f0b1de433140e38"
   license all_of: ["GPL-3.0-or-later", "0BSD", "BSD-3-Clause", "LGPL-2.0-or-later", "MIT"]
 
+  head do
+    url "https://gitlab.labs.nic.cz/knot/knot-dns.git"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+  end
+
   livecheck do
     url "https://secure.nic.cz/files/knot-dns/"
     regex(/href=.*?knot[._-]v?(\d+(?:\.\d+)+)\.t/i)
@@ -14,14 +22,6 @@ class Knot < Formula
     sha256 "f34e1d8fb415de903e2721f21c9fe0e5df4fec0614e11ef52d965334441a6cc3" => :big_sur
     sha256 "e32ba366f9893e1ad34e532240eb0cafe16426e644d898ce649f2fd1558004d1" => :catalina
     sha256 "2e71f0cc8fd060663d80e6385c99e14ac6fb9ec2390d5170e138fb1dfb43f266" => :mojave
-  end
-
-  head do
-    url "https://gitlab.labs.nic.cz/knot/knot-dns.git"
-
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
-    depends_on "libtool" => :build
   end
 
   depends_on "pkg-config" => :build
