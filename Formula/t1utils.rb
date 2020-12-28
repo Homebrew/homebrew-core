1751ced0ebc107451ee465ee65abbe0dc301b011
Sam Ford <1584702+samford@users.noreply.github.com>
2020-12-27 19:21:35 -0500

    t1utils: relocate head

--- Formula/t1utils.rb
+++ Formula/t1utils.rb
@@ -5,6 +5,13 @@ class T1utils < Formula
   sha256 "61877935b1987044ddff4bb90a05200ca7164678a355e170bf5f1a5556cc9f29"
   license "MIT"
 
+  head do
+    url "https://github.com/kohler/t1utils.git"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+  end
+
   bottle do
     cellar :any_skip_relocation
     sha256 "f00f838c4ebef97926c3a2cd9940d2105029839a25543f9861903ec1a71939e1" => :big_sur
@@ -12,13 +19,6 @@ class T1utils < Formula
     sha256 "dfaaef0c838273e5c4cee7d6d2eb515e91c77c3226913b4c4486ca0086c2e6bc" => :catalina
     sha256 "1b511df389dee041c0cdadae94e38e987ea978024730d687b8642623cb054e09" => :mojave
     sha256 "c17de51c95690f3133933cd508873e21734a8e4f8ed80ec6546ab3c7fb82edd2" => :high_sierra
-  end
-
-  head do
-    url "https://github.com/kohler/t1utils.git"
-
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
   end
 
   def install
