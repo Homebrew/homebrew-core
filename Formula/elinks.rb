b62d1efa49ae75d3d8d437f1d9f69b7e6af85ae1
Sam Ford <1584702+samford@users.noreply.github.com>
2020-12-27 19:20:32 -0500

    elinks: relocate head

--- Formula/elinks.rb
+++ Formula/elinks.rb
@@ -5,6 +5,14 @@ class Elinks < Formula
   sha256 "456db6f704c591b1298b0cd80105f459ff8a1fc07a0ec1156a36c4da6f898979"
   revision 3
 
+  head do
+    url "http://elinks.cz/elinks.git"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+  end
+
   livecheck do
     url :head
     regex(/^elinks[._-]v?(\d+(?:\.\d+)+)$/i)
@@ -17,14 +25,6 @@ class Elinks < Formula
     sha256 "67ab168d9d6d5bb65791d4c432e7e1e0109a09076039d4d6b2addec9219bef43" => :catalina
     sha256 "c48e70700c0ad0c4b66a376e6634417cd84c84de064bad74d384469d8f7597ab" => :mojave
     sha256 "219f12e44db5b6e966e2f8999fc1d5553c834b58645531f5167e6031aaa6e89b" => :high_sierra
-  end
-
-  head do
-    url "http://elinks.cz/elinks.git"
-
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
-    depends_on "libtool" => :build
   end
 
   depends_on "openssl@1.1"
