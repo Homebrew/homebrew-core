b62923ea5f28480b9aaa371c416659138208628f
Sam Ford <1584702+samford@users.noreply.github.com>
2020-12-27 19:21:40 -0500

    xboard: relocate head

--- Formula/xboard.rb
+++ Formula/xboard.rb
@@ -6,6 +6,12 @@ class Xboard < Formula
   sha256 "2b2e53e8428ad9b6e8dc8a55b3a5183381911a4dae2c0072fa96296bbb1970d6"
   revision 3
 
+  head do
+    url "https://git.savannah.gnu.org/git/xboard.git"
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+  end
+
   livecheck do
     url :stable
   end
@@ -16,12 +22,6 @@ class Xboard < Formula
     sha256 "c94386e2985c9a4175aba3280658670810269c0a6fe8315676cc49198070bf14" => :mojave
     sha256 "eecee1fb605e34564d8906a72f41d1516a210cb41af86c9dd51cdd05376d8b48" => :high_sierra
     sha256 "5c9c512b8267d66e69842e9f11b9f63169ae2b953108df72f200122267724f9d" => :sierra
-  end
-
-  head do
-    url "https://git.savannah.gnu.org/git/xboard.git"
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
   end
 
   depends_on "pkg-config" => :build
