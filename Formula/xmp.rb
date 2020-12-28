a8ff0ef41da7a63fc3d37d7dcb2b6ac336329826
Sam Ford <1584702+samford@users.noreply.github.com>
2020-12-27 19:21:42 -0500

    xmp: relocate head

--- Formula/xmp.rb
+++ Formula/xmp.rb
@@ -5,6 +5,14 @@ class Xmp < Formula
   sha256 "1dbd61074783545ac7bef5b5daa772fd2110764cb70f937af8c3fad30f73289e"
   license "GPL-2.0"
 
+  head do
+    url "https://git.code.sf.net/p/xmp/xmp-cli.git"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool"  => :build
+  end
+
   livecheck do
     url :stable
     regex(%r{url=.*?/xmp[._-]v?(\d+(?:\.\d+)+)\.t}i)
@@ -18,14 +26,6 @@ class Xmp < Formula
     sha256 "dc4399be2df77f0534bf1151201fd52b61694df7285bd58d9c1fe16522f199f6" => :catalina
     sha256 "197be59a2a0c3495aeed49eeeedea65b060534f4ff5ad234cdd35f6da19fb9e1" => :mojave
     sha256 "c76b4335844295d6daaaaca97f462828d39a9ce511c859d0ebf66165b12a6354" => :high_sierra
-  end
-
-  head do
-    url "https://git.code.sf.net/p/xmp/xmp-cli.git"
-
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
-    depends_on "libtool"  => :build
   end
 
   depends_on "pkg-config" => :build
