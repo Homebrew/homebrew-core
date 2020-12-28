b901e9f19623a86dd2ff631a73cfd861c26538b8
Sam Ford <1584702+samford@users.noreply.github.com>
2020-12-27 19:20:24 -0500

    cmu-sphinxbase: relocate head

--- Formula/cmu-sphinxbase.rb
+++ Formula/cmu-sphinxbase.rb
@@ -5,6 +5,15 @@ class CmuSphinxbase < Formula
   sha256 "55708944872bab1015b8ae07b379bf463764f469163a8fd114cbb16c5e486ca8"
   license "BSD-2-Clause"
 
+  head do
+    url "https://github.com/cmusphinx/sphinxbase.git"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+    depends_on "swig" => :build
+  end
+
   livecheck do
     url :stable
     regex(%r{url=.*?/sphinxbase[._-]v?(\d+(?:\.\d+)+)\.t}i)
@@ -19,15 +28,6 @@ class CmuSphinxbase < Formula
     sha256 "b55c9f16e8b89fc515d9bf8bd6ed91f532d0c82a46be01cd9792bb27076a6a51" => :mojave
     sha256 "2ebde8d649a3e78c3e219c83e1f12e6cee924f5404b0d68e8fe7d220c8dad0f5" => :high_sierra
     sha256 "fde603304716876e192bef822f8df21c26e09688d43580d3f9a61c78e03dbbb0" => :sierra
-  end
-
-  head do
-    url "https://github.com/cmusphinx/sphinxbase.git"
-
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
-    depends_on "libtool" => :build
-    depends_on "swig" => :build
   end
 
   depends_on "pkg-config" => :build
