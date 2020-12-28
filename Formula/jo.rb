7c1ec6af082245dcbf33bf63ece71ebcc2951fd9
Sam Ford <1584702+samford@users.noreply.github.com>
2020-12-27 19:20:49 -0500

    jo: relocate head

--- Formula/jo.rb
+++ Formula/jo.rb
@@ -5,6 +5,13 @@ class Jo < Formula
   sha256 "24c64d2eb863900947f58f32b502c95fec8f086105fd31151b91f54b7b5256a2"
   license "GPL-2.0"
 
+  head do
+    url "https://github.com/jpmens/jo.git"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+  end
+
   bottle do
     cellar :any_skip_relocation
     sha256 "e7e0509c877e132429eb5cd71b4150c3ea573c9604e92b4bb31c2de4508eb182" => :big_sur
@@ -12,13 +19,6 @@ class Jo < Formula
     sha256 "15bee62d31331c60c1768949ca11916d242fbe96aafcdc7a66a8359c0f4a9c3c" => :catalina
     sha256 "6741c18bb9a9519e325ac4b30989cdd0c735107ee34772097d3d8fde103880eb" => :mojave
     sha256 "bd19a24ded348995844cf428f74729dc91b3c23a9f144ca1b117108c3d3b5401" => :high_sierra
-  end
-
-  head do
-    url "https://github.com/jpmens/jo.git"
-
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
   end
 
   def install
