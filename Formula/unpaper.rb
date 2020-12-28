e6f8633d4f10ddc9057e7fd35f21050cccd9e8df
Sam Ford <1584702+samford@users.noreply.github.com>
2020-12-27 19:21:38 -0500

    unpaper: relocate head

--- Formula/unpaper.rb
+++ Formula/unpaper.rb
@@ -5,18 +5,18 @@ class Unpaper < Formula
   sha256 "237c84f5da544b3f7709827f9f12c37c346cdf029b1128fb4633f9bafa5cb930"
   revision 6
 
+  head do
+    url "https://github.com/Flameeyes/unpaper.git"
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+  end
+
   bottle do
     cellar :any
     sha256 "4272a437e23c502b03ceea130c0ea2c2d1312a11834452638862251d395e0ecc" => :big_sur
     sha256 "20b2e6bf4adebadfeb7705f2a3b6437aac39cbec0eeeac0a924a2985d15f014d" => :catalina
     sha256 "722874cb52df909ea30a72d519f3db40a9c98389281629b4910aeefbdf88b959" => :mojave
     sha256 "649ba3d0be5c4c2ce5e32f32c2023ea3296bb59ff0473f641092173f8a664552" => :high_sierra
-  end
-
-  head do
-    url "https://github.com/Flameeyes/unpaper.git"
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
   end
 
   depends_on "pkg-config" => :build
