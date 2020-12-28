f6547af45cf5099811e20bb13202a631088cb297
Sam Ford <1584702+samford@users.noreply.github.com>
2020-12-27 19:20:34 -0500

    fish: relocate head

--- Formula/fish.rb
+++ Formula/fish.rb
@@ -5,6 +5,12 @@ class Fish < Formula
   sha256 "d5b927203b5ca95da16f514969e2a91a537b2f75bec9b21a584c4cd1c7aa74ed"
   license "GPL-2.0"
 
+  head do
+    url "https://github.com/fish-shell/fish-shell.git", shallow: false
+
+    depends_on "sphinx-doc" => :build
+  end
+
   livecheck do
     url :head
     regex(/^v?(\d+(?:\.\d+)+)$/i)
@@ -17,12 +23,6 @@ class Fish < Formula
     sha256 "b158b7f8640feb7c622ff3ca92b1bd88565f274f3e761499f5926bb124eeff7d" => :catalina
     sha256 "6797636eaba364d0cbbc0459103a8767598e985f01846cca6cb57c986dfee7b8" => :mojave
     sha256 "2609577a0d9f6b661331adccf5d1d8e010662ffe128869757e0af9a6760e26fb" => :high_sierra
-  end
-
-  head do
-    url "https://github.com/fish-shell/fish-shell.git", shallow: false
-
-    depends_on "sphinx-doc" => :build
   end
 
   depends_on "cmake" => :build
