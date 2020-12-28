1fc49dc62c50ab43df1c8aaba83bff7c542739f6
Sam Ford <1584702+samford@users.noreply.github.com>
2020-12-27 19:20:20 -0500

    bindfs: relocate head

--- Formula/bindfs.rb
+++ Formula/bindfs.rb
@@ -5,18 +5,18 @@ class Bindfs < Formula
   sha256 "e5ca5aff55204b993a025a77c3f8c0e2ee901ba8059d71bea11de2cc685ec497"
   license "GPL-2.0"
 
-  bottle do
-    cellar :any
-    sha256 "bf4fb90d788651299973a3f48300824ae6b4ec4ce1441dd94d544180f54379bd" => :catalina
-    sha256 "cc9d4950a1b74a27307bac429892fd85ad439f5707f53a66e800a39b23a32fdf" => :mojave
-    sha256 "50df08ee8d3cc6f141d8488341f12c32ce478f02dfb8016f533bfeababfe4537" => :high_sierra
-  end
-
   head do
     url "https://github.com/mpartel/bindfs.git"
     depends_on "autoconf" => :build
     depends_on "automake" => :build
     depends_on "libtool" => :build
+  end
+
+  bottle do
+    cellar :any
+    sha256 "bf4fb90d788651299973a3f48300824ae6b4ec4ce1441dd94d544180f54379bd" => :catalina
+    sha256 "cc9d4950a1b74a27307bac429892fd85ad439f5707f53a66e800a39b23a32fdf" => :mojave
+    sha256 "50df08ee8d3cc6f141d8488341f12c32ce478f02dfb8016f533bfeababfe4537" => :high_sierra
   end
 
   deprecate! date: "2020-11-10", because: "requires FUSE"
