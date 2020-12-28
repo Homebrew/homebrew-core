1ae09e67755dac62528915af718dec8c0d55e135
Sam Ford <1584702+samford@users.noreply.github.com>
2020-12-27 19:20:59 -0500

    libmaxminddb: relocate head

--- Formula/libmaxminddb.rb
+++ Formula/libmaxminddb.rb
@@ -5,6 +5,14 @@ class Libmaxminddb < Formula
   sha256 "a5fdf6c7b4880fdc7620f8ace5bd5cbe9f65650c9493034b5b9fc7d83551a439"
   license "Apache-2.0"
 
+  head do
+    url "https://github.com/maxmind/libmaxminddb.git"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+  end
+
   bottle do
     cellar :any
     sha256 "5ba5e0260d3c3116330850358b7619f9c216ada217a75007939a46b17b9bf487" => :big_sur
@@ -12,14 +20,6 @@ class Libmaxminddb < Formula
     sha256 "a9d031d21d9ed59bad9acd3c78c8831ba15893b4939dc94652fd5834e3078cd8" => :catalina
     sha256 "0144263e2fd98cf957c38aa330b52298787f03af684b3b41efe1a0669b831bff" => :mojave
     sha256 "274e81dd90f2d35de472b21c93b1f87aaab44121c0625b116ccf859ca13cf647" => :high_sierra
-  end
-
-  head do
-    url "https://github.com/maxmind/libmaxminddb.git"
-
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
-    depends_on "libtool" => :build
   end
 
   def install
