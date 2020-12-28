7db191608bd5de9a91343db2aa1a461524866854
Sam Ford <1584702+samford@users.noreply.github.com>
2020-12-27 19:20:58 -0500

    libimobiledevice: relocate head

--- Formula/libimobiledevice.rb
+++ Formula/libimobiledevice.rb
@@ -5,6 +5,13 @@ class Libimobiledevice < Formula
   sha256 "53f2640c6365cd9f302a6248f531822dc94a6cced3f17128d4479a77bd75b0f6"
   license "LGPL-2.1"
 
+  head do
+    url "https://git.libimobiledevice.org/libimobiledevice.git"
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+  end
+
   bottle do
     cellar :any
     sha256 "0fe21433f470130b972354d411d05f43ab37d82198565bb6b947734a95e98c5d" => :big_sur
@@ -12,13 +19,6 @@ class Libimobiledevice < Formula
     sha256 "eb7f28d86797461d5ef859d00629176e1ce3234790ef17b9ee3f9c9990a664e2" => :catalina
     sha256 "5143eaf34011a22dd1951f10495a7568e77a2e862fb9f4dbae9bab2f784f926e" => :mojave
     sha256 "072d224a0fa2a77bccde27eee39b65300a387613b41f07fc677108a7812ec003" => :high_sierra
-  end
-
-  head do
-    url "https://git.libimobiledevice.org/libimobiledevice.git"
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
-    depends_on "libtool" => :build
   end
 
   depends_on "pkg-config" => :build
