b7c3a1b424908fef4254e8b878301d8a4fe40946
Sam Ford <1584702+samford@users.noreply.github.com>
2020-12-27 19:20:27 -0500

    datetime-fortran: relocate head

--- Formula/datetime-fortran.rb
+++ Formula/datetime-fortran.rb
@@ -5,6 +5,14 @@ class DatetimeFortran < Formula
   sha256 "cff4c1f53af87a9f8f31256a3e04176f887cc3e947a4540481ade4139baf0d6f"
   license "MIT"
 
+  head do
+    url "https://github.com/wavebitscientific/datetime-fortran.git"
+
+    depends_on "autoconf"   => :build
+    depends_on "automake"   => :build
+    depends_on "pkg-config" => :build
+  end
+
   bottle do
     cellar :any_skip_relocation
     sha256 "13b551703e1afcdcb1c4a92939afdce7447fbf96e071c984944a8bee8e833496" => :big_sur
@@ -12,14 +20,6 @@ class DatetimeFortran < Formula
     sha256 "82d8b0e2a51fb7df321659ed4f5da43c24edd5aba81e5e05250508b541f2eb4b" => :catalina
     sha256 "ef59feabc30610c41a5ac4b2e594f1378d3edeb3b13dd7912825c48815d547e2" => :mojave
     sha256 "cf59b21c0539aa14f5e0274387669d13dae47b3e11267cdb1baed8545f2bd535" => :high_sierra
-  end
-
-  head do
-    url "https://github.com/wavebitscientific/datetime-fortran.git"
-
-    depends_on "autoconf"   => :build
-    depends_on "automake"   => :build
-    depends_on "pkg-config" => :build
   end
 
   depends_on "gcc" # for gfortran
