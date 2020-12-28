1685645c85278c194166a4c288c4a4222d4eb5b8
Sam Ford <1584702+samford@users.noreply.github.com>
2020-12-27 19:20:55 -0500

    libdvdnav: relocate head

--- Formula/libdvdnav.rb
+++ Formula/libdvdnav.rb
@@ -5,6 +5,13 @@ class Libdvdnav < Formula
   sha256 "f697b15ea9f75e9f36bdf6ec3726308169f154e2b1e99865d0bbe823720cee5b"
   revision 1
 
+  head do
+    url "https://code.videolan.org/videolan/libdvdnav.git"
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+  end
+
   bottle do
     cellar :any
     sha256 "c95ff0063f69997b0041d1bc4fd31a517eeb131a9bf92f549feb7fc07606ea23" => :big_sur
@@ -12,13 +19,6 @@ class Libdvdnav < Formula
     sha256 "e20bd00c923e4e837a1eac6f89933377afd17957be74a175734d7896efceb27a" => :catalina
     sha256 "6c13d8aa2220232e9a0e5317371bd52909ec55b0bb08ed7b591612b60814dc7a" => :mojave
     sha256 "a9c90f8109ce908ffc4fc0b6972ff4869638de8f52787bedd2c471ce457e1edf" => :high_sierra
-  end
-
-  head do
-    url "https://code.videolan.org/videolan/libdvdnav.git"
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
-    depends_on "libtool" => :build
   end
 
   depends_on "pkg-config" => :build
