3ace5d5e5fa386e566e2f0a45f618deb28274b71
David Zhang <crispgm@gmail.com>
2020-12-29 20:06:01 +0800

    feat: new formula tt 0.0.1

---
+++ Formula/tt.rb
@@ -0,0 +1,18 @@
+class Tt < Formula
+  desc "A terminal based typing test"
+  homepage "https://github.com/lemnos/tt"
+  url "https://github.com/lemnos/tt/archive/0.0.1.tar.gz"
+  sha256 "007ccfabae069035bf01274b482c84af36b5f0588066da1cd784e62caa461f1e"
+  license "MIT"
+  head "https://github.com/lemnos/tt.git"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", "-o", bin/"tt"
+  end
+
+  test do
+    assert_match(/nord/, shell_output("#{bin}/tt -list themes"))
+  end
+end
