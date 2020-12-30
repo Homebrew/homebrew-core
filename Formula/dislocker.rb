3622ca54b966519fd414602205aafe94d3ae41bf
carlocab <30379873+carlocab@users.noreply.github.com>
2020-12-30 17:24:10 +0000

    dislocker 0.7.3

--- Formula/dislocker.rb
+++ Formula/dislocker.rb
@@ -1,10 +1,9 @@
 class Dislocker < Formula
   desc "FUSE driver to read/write Windows' BitLocker-ed volumes"
   homepage "https://github.com/Aorimn/dislocker"
-  url "https://github.com/Aorimn/dislocker/archive/v0.7.1.tar.gz"
-  sha256 "742fb5c1b3ff540368ced54c29eae8b488ae5a5fcaca092947e17c2d358a6762"
+  url "https://github.com/Aorimn/dislocker/archive/v0.7.3.tar.gz"
+  sha256 "8d5275577c44f2bd87f6e05dd61971a71c0e56a9cbedf000bd38deadd8b6c1e6"
   license "GPL-2.0"
-  revision 5
 
   bottle do
     sha256 "e0049b9ff51ad9f3e4008df1edac9b52aa0d8df55e119990553b4d9cec651b90" => :catalina
