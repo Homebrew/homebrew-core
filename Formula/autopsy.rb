de3217b2d085c2bf224ae2a3bd05560d1c7b2a9c
Rui Chen <rui@chenrui.dev>
2020-12-29 23:59:48 -0500

    autopsy 4.17.0

--- Formula/autopsy.rb
+++ Formula/autopsy.rb
@@ -1,8 +1,8 @@
 class Autopsy < Formula
   desc "Graphical interface to Sleuth Kit investigation tools"
   homepage "https://www.sleuthkit.org/autopsy/index.php"
-  url "https://downloads.sourceforge.net/project/autopsy/autopsy/2.24/autopsy-2.24.tar.gz"
-  sha256 "ab787f519942783d43a561d12be0554587f11f22bc55ab79d34d8da703edc09e"
+  url "https://github.com/sleuthkit/autopsy/releases/download/autopsy-4.17.0/autopsy-4.17.0.zip"
+  sha256 "ad198aa48a0f60bc574605ce168b3330f73d4f89351e7f68e79ce2df5e38bff4"
 
   livecheck do
     url "https://github.com/sleuthkit/autopsy.git"
