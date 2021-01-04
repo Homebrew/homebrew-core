3d4aa659199c47bd5f7ddb22d85a246abe4346c4
PaperChalice <lgamma@163.com>
2021-01-04 10:15:27 +0800

    add qt-core5-compat

---
+++ Formula/qt-core5-compat.rb
@@ -0,0 +1,94 @@
+class QtCore5Compat < Formula
+  desc "Qt module contains unsupported Qt 5 APIs"
+  homepage "https://www.qt.io/"
+  url "https://download.qt.io/archive/qt/6.0/6.0.0/submodules/qt5compat-everywhere-src-6.0.0.tar.xz"
+  sha256 "13b9d78aa698609d07376e3e9e9d1a82a96084236aba0642f8ac695c181ab25f"
+  license all_of: ["GFDL-1.3-only", "GPL-2.0-only", "GPL-3.0-only", "LGPL-2.1-only", "LGPL-3.0-only"]
+
+  bottle do
+    root_url "https://dl.bintray.com/paperchalice/dev-bottle"
+    cellar :any
+    sha256 "f67414810be33b2ec67dd48f25e6bc2df82ff0ccdd15916faa3de6a9dbbd1e35" => :big_sur
+  end
+
+  depends_on "cmake" => :build
+  depends_on "ninja" => :build
+
+  depends_on "qt-base"
+
+  def install
+    args = std_cmake_args.reject { |s| s["CMAKE_INSTALL_PREFIX"] } + %W[
+      -DCMAKE_INSTALL_PREFIX=/usr/local
+      -DCMAKE_STAGING_PREFIX=#{prefix}
+    ]
+    system "cmake", "-G", "Ninja", ".", *args
+    system "ninja"
+    system "ninja", "install"
+
+    # Some config scripts will only find Qt in a "Frameworks" folder
+    frameworks.install_symlink Dir["#{lib}/*.framework"]
+
+    # The pkg-config files installed suggest that headers can be found in the
+    # `include` directory. Make this so by creating symlinks from `include` to
+    # the Frameworks' Headers folders.
+    Pathname.glob("#{lib}/*.framework/Headers") do |path|
+      include.install_symlink path => path.parent.basename(".framework")
+    end
+  end
+
+  test do
+    (testpath/"CMakeLists.txt").write <<~EOS
+      cmake_minimum_required(VERSION 3.16.0)
+
+      project(test_project VERSION 1.0.0 LANGUAGES CXX)
+
+      set(CMAKE_CXX_STANDARD 17)
+      set(CMAKE_CXX_STANDARD_REQUIRED ON)
+
+      set(CMAKE_AUTOMOC ON)
+      set(CMAKE_AUTORCC ON)
+      set(CMAKE_AUTOUIC ON)
+
+      find_package(Qt6 COMPONENTS Widgets REQUIRED)
+      find_package(Qt6Core5Compat COMPONENTS Widgets REQUIRED)
+
+      add_executable(test
+          main.cpp
+      )
+
+      target_link_libraries(test PRIVATE Qt6::Widgets Qt6::Core5Compat)
+    EOS
+
+    (testpath/"test.pro").write <<~EOS
+      QT       += core core5compat
+      QT       -= gui
+      TARGET = test
+      CONFIG   += console
+      CONFIG   -= app_bundle
+      TEMPLATE = app
+      SOURCES += main.cpp
+    EOS
+
+    (testpath/"main.cpp").write <<~EOS
+      #include <QCoreApplication>
+      #include <QtCore5Compat/QLinkedList>
+      #include <QDebug>
+
+      int main(int argc, char *argv[])
+      {
+        QLinkedList list = {1, 2, 3};
+        return 0;
+      }
+    EOS
+
+    system "cmake", testpath
+    system "make"
+    system "./test"
+
+    # Work around "error: no member named 'signbit' in the global namespace"
+    ENV.delete "CPATH"
+    system "qmake", testpath/"hello.pro"
+    system "make"
+    system "./test"
+  end
+end
