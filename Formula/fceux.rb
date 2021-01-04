b83575ac7cbc0c5685ad66419212d6440f57c2f0
chenrui <rui@meetup.com>
2021-01-04 10:50:31 -0500

    recover bottle block

--- Formula/fceux.rb
+++ Formula/fceux.rb
@@ -8,6 +8,13 @@ class Fceux < Formula
     sha256 "f166f2fea084162f5b823bbbc6843141e23908ae4ee69c8c4e0849b320d7a1dc"
   end
 
+  bottle do
+    cellar :any
+    sha256 "7c7550b97011321d5d48f8f689c7158223aee5054698a6c707a185404e469e35" => :catalina
+    sha256 "800e46a45f554876ad2a63ea6a62f6d672e5aefd2c9cca8f58fe615b82eb9ea7" => :mojave
+    sha256 "3f587de213706a92fb02b14676514f6cba079e3c3b7ded2e57a8e718ebf9cf20" => :high_sierra
+  end
+
   head do
     url "https://github.com/TASVideos/fceux.git"
   end
