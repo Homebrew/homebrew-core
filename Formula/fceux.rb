d6ed6e23f62dc178a3cd664bc3cb237eb2ea43af
mjbudd77 <mjbudd77@gmail.com>
2021-01-03 21:13:46 -0500

    brew audit cleanup of fceux.rb

--- Formula/fceux.rb
+++ Formula/fceux.rb
@@ -1,33 +1,29 @@
 class Fceux < Formula
   desc "All-in-one NES/Famicom Emulator"
   homepage "https://fceux.com/"
-  revision 1
+  # version "2.3.0"
   license "GPL-2.0-only"
-
-  version "2.3.0"
+  revision 1
 
   stable do
     url "https://downloads.sourceforge.net/project/fceultra/Binaries/2.3.0/fceux-2.3.0.tar.gz"
     sha256 "f166f2fea084162f5b823bbbc6843141e23908ae4ee69c8c4e0849b320d7a1dc"
   end
 
-  head "https://github.com/TASVideos/fceux.git"
-
   head do
     url "https://github.com/TASVideos/fceux.git"
   end
 
-  depends_on "pkg-config" => :build
   depends_on "cmake" => :build
-  depends_on "qt5"
-  depends_on "sdl2"
+  depends_on "pkg-config" => :build
   depends_on "minizip"
+  depends_on "qt"
+  depends_on "sdl2"
 
   def install
-
     system "cmake", ".", *std_cmake_args
     system "make"
-    system "cp", "src/auxlib.lua", "output/luaScripts"
+    cp "src/auxlib.lua", "output/luaScripts"
     libexec.install "src/fceux.app/Contents/MacOS/fceux"
     pkgshare.install ["output/luaScripts", "output/palettes", "output/tools"]
     (bin/"fceux").write <<~EOS
