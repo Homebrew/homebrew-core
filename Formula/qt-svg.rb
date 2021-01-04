700692d1efb5a60bcf4e08131f9429bed60f291d
PaperChalice <lgamma@163.com>
2021-01-04 10:17:36 +0800

    add qt-svg

---
+++ Formula/qt-svg.rb
@@ -0,0 +1,96 @@
+class QtSvg < Formula
+  desc "SVG support library for Qt"
+  homepage "https://www.qt.io/"
+  url "https://download.qt.io/archive/qt/6.0/6.0.0/submodules/qtsvg-everywhere-src-6.0.0.tar.xz"
+  sha256 "9703c9a69e21ad373fb52d0107338da7ef0a46966f69107b0d879e9c366dd91b"
+  license all_of: ["GPL-2.0-only", "LGPL-3.0-only"]
+
+  bottle do
+    root_url "https://dl.bintray.com/paperchalice/dev-bottle"
+    cellar :any
+    sha256 "f0e1f6b22b1e6b144788132801b51523637306cd46b1a8d479b72adbdd716812" => :big_sur
+  end
+
+  depends_on "cmake" => :build
+  depends_on "ninja" => :build
+
+  depends_on "qt-base"
+
+  uses_from_macos "zlib"
+
+  def install
+    args = std_cmake_args.reject { |s| s["CMAKE_INSTALL_PREFIX"] } + %W[
+      -DCMAKE_INSTALL_PREFIX=/usr/local
+      -DCMAKE_STAGING_PREFIX=#{prefix}
+    ]
+    system "cmake", "-G", "Ninja", ".", *args
+    system "ninja"
+    system "ninja", "install"
+
+    # Some config scripts will only find Qt in a "Frameworks" folder
+    frameworks.install_symlink Dir["#{lib}/*.framework"]
+
+    # The pkg-config files installed suggest that headers can be found in the
+    # `include` directory. Make this so by creating symlinks from `include` to
+    # the Frameworks' Headers folders.
+    Pathname.glob("#{lib}/*.framework/Headers") do |path|
+      include.install_symlink path => path.parent.basename(".framework")
+    end
+  end
+
+  test do
+    (testpath/"CMakeLists.txt").write <<~EOS
+      cmake_minimum_required(VERSION 3.16.0)
+
+      project(test VERSION 1.0.0 LANGUAGES CXX)
+
+      set(CMAKE_CXX_STANDARD 17)
+      set(CMAKE_CXX_STANDARD_REQUIRED ON)
+
+      set(CMAKE_AUTOMOC ON)
+      set(CMAKE_AUTORCC ON)
+      set(CMAKE_AUTOUIC ON)
+
+      find_package(Qt6 COMPONENTS Widgets REQUIRED)
+      find_package(Qt6Svg COMPONENTS Widgets REQUIRED)
+
+      add_executable(test
+          main.cpp
+      )
+
+      target_link_libraries(test PRIVATE Qt6::Widgets Qt6::Svg)
+    EOS
+
+    (testpath/"test.pro").write <<~EOS
+      QT       += core svg
+      QT       -= gui
+      TARGET = test
+      CONFIG   += console
+      CONFIG   -= app_bundle
+      TEMPLATE = app
+      SOURCES += main.cpp
+    EOS
+
+    (testpath/"main.cpp").write <<~EOS
+      #include <QCoreApplication>
+      #include <QtSvg/QtSvg>
+      #include <QDebug>
+
+      int main(int argc, char *argv[])
+      {
+        QSvgGenerator generator;
+        return 0;
+      }
+    EOS
+
+    system "cmake", testpath
+    system "make"
+    system "./test"
+
+    # Work around "error: no member named 'signbit' in the global namespace"
+    ENV.delete "CPATH"
+    system "qmake", testpath/"test.pro"
+    system "make"
+    system "./test"
+  end
+end
