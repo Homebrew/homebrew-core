e715a58fb1f170454fb40af37527cf86a74102c2
PaperChalice <lgamma@163.com>
2021-01-04 10:16:41 +0800

    add qt-quick-timeline

---
+++ Formula/qt-quick-timeline.rb
@@ -0,0 +1,48 @@
+class QtQuickTimeline < Formula
+  desc "Module for keyframe-based timeline construction."
+  homepage "https://www.qt.io/"
+  url "https://download.qt.io/archive/qt/6.0/6.0.0/submodules/qtquicktimeline-everywhere-src-6.0.0.tar.xz"
+  sha256 "7a71495c07a98279a852d518bc9ca0f07b49b495ceb65bfdd000c826ee156b0c"
+  license all_of: ["GFDL-1.3-only", "GPL-2.0-only", "GPL-3.0-only", "LGPL-2.1-only", "LGPL-3.0-only"]
+
+  bottle do
+    root_url "https://dl.bintray.com/paperchalice/dev-bottle"
+    cellar :any
+    sha256 "ba0a762a018572d784bdf9c91ee68065c7b9568f9d5361ac27d406de2c37164e" => :big_sur
+  end
+
+  depends_on "cmake" => :build
+  depends_on "ninja" => :build
+
+  depends_on "qt-declarative"
+
+  def install
+    args = std_cmake_args.reject { |s| s["CMAKE_INSTALL_PREFIX"] } + %W[
+      -DCMAKE_INSTALL_PREFIX=/usr/local
+      -DCMAKE_STAGING_PREFIX=#{prefix}
+    ]
+    system "cmake", "-G", "Ninja", ".", *args
+    system "ninja"
+    system "ninja", "install"
+  end
+
+  test do
+    (testpath/"hello.qml").write <<~EOS
+      import QtQuick 2.0
+      import QtQuick.Timeline 1.0
+      Rectangle {
+          id: page
+          width: 320; height: 480
+          color: "lightgray"
+          Text {
+              id: helloText
+              text: "Hello world!"
+              y: 30
+              anchors.horizontalCenter: page.horizontalCenter
+              font.pointSize: 24; font.bold: true
+          }
+      }
+    EOS
+    system "qmlscene", "--quit", testpath/"hello.qml"
+  end
+end
