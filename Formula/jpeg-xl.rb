f13915187f47392768b1823a0a73ecc3e791f146
Dan Milne <d@nmilne.com>
2021-01-08 09:58:47 +1100

    Correct formatting. Use specifc cjxl binary for testing. Refer to versioned dependency script

--- Formula/jpeg-xl.rb
+++ Formula/jpeg-xl.rb
@@ -16,19 +16,22 @@ class JpegXl < Formula
   depends_on "webp"
 
   # These resources are versioned according to the script supplied with jpeg-xl to download the dependencies:
-  # https://gitlab.com/wg1/jpeg-xl/-/blob/master/deps.sh
+  # https://gitlab.com/wg1/jpeg-xl/-/blob/v#{version}/deps.sh
   resource "highway" do
     url "https://github.com/google/highway.git",
         revision: "311c183c9d96e69b123f61eedc21025dd27be000"
   end
+
   resource "lodepng" do
     url "https://github.com/lvandeve/lodepng.git",
         revision: "48e5364ef48ec2408f44c727657ac1b6703185f8"
   end
+
   resource "sjpeg" do
     url "https://github.com/webmproject/sjpeg.git",
         revision: "868ab558fad70fcbe8863ba4e85179eeb81cc840"
   end
+
   resource "skcms" do
     url "https://skia.googlesource.com/skcms.git",
         revision: "64374756e03700d649f897dbd98c95e78c30c7da"
@@ -44,7 +47,7 @@ class JpegXl < Formula
   end
 
   test do
-    system "cjxl", test_fixtures("test.jpg"), "test.jxl"
+    system "#{bin}/cjxl", test_fixtures("test.jpg"), "test.jxl"
     assert_predicate testpath/"test.jxl", :exist?
   end
 end
