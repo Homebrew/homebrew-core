e4f99d093a8f4fdae6515361eed3d80d941f0fc2
ruki <waruqi@gmail.com>
2021-01-11 23:26:46 +0800

    Update xmake.rb

--- Formula/xmake.rb
+++ Formula/xmake.rb
@@ -22,13 +22,8 @@ class Xmake < Formula
       ENV["XMAKE_ROOT"] = "y" if ENV["CI"]
     end
 
-    # This should probably be automatic in future versions
-    # https://github.com/xmake-io/xmake/issues/1190
-    args = []
-    #args << "BUILD_ARCH=arm64" if Hardware::CPU.arm?
-
-    system "make", "-j1", *args
-    system "make", "install", "prefix=#{prefix}", *args
+    system "make", "-j1"
+    system "make", "install", "prefix=#{prefix}"
   end
 
   test do
