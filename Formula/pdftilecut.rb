fcd750c25c002e4ad390bbed4273fbf2756f13cb
Laurent Goussard <loranger@free.fr>
2021-01-12 21:56:03 +0100

    pdftilecut 0.4

---
+++ Formula/pdftilecut.rb
@@ -0,0 +1,24 @@
+class Pdftilecut < Formula
+  desc "Sub-divide a PDF page(s) into smaller pages so you can print them"
+  homepage "https://github.com/oxplot/pdftilecut"
+  # url "https://github.com/oxplot/pdftilecut/archive/v0.4.tar.gz"
+  url "git@github.com:oxplot/pdftilecut.git", using: :git, tag: "v0.4", revision: "af9ca3bff60eed47026a6e1c40b6ef85e5b36393"
+  license "BSD-3-Clause"
+
+  depends_on "autoconf" => :build
+  depends_on "autogen" => :build
+  depends_on "automake" => :build
+  depends_on "cmake" => :build
+  depends_on "coreutils" => :build
+  depends_on "go" => :build
+  depends_on "yasm" => :build
+
+  def install
+    system "make"
+    bin.install "bin/pdftilecut" => "pdftilecut"
+  end
+
+  test do
+    system bin/"pdftilecut", "-h"
+  end
+end
