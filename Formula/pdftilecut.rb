0542441e8dfea20be761dc5100af75d011cd7990
Laurent Goussard <loranger@free.fr>
2021-01-12 23:34:27 +0100

    Missing required checksum

--- Formula/pdftilecut.rb
+++ Formula/pdftilecut.rb
@@ -2,6 +2,7 @@ class Pdftilecut < Formula
   desc "Sub-divide a PDF page(s) into smaller pages so you can print them"
   homepage "https://github.com/oxplot/pdftilecut"
   url "https://github.com/oxplot/pdftilecut/archive/v0.4.tar.gz"
+  sha256 "8a75a0d2e196d156bca4dc006f84c9b7730f20be8c0fc2c90521c538aa627188"
   license "BSD-3-Clause"
 
   depends_on "go" => :build
