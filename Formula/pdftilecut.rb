0e03d9ea735277502404133dcf14ef263c1643f1
Laurent Goussard <loranger@free.fr>
2021-01-12 23:28:07 +0100

    Build options from @carlocab

--- Formula/pdftilecut.rb
+++ Formula/pdftilecut.rb
@@ -1,22 +1,15 @@
 class Pdftilecut < Formula
   desc "Sub-divide a PDF page(s) into smaller pages so you can print them"
   homepage "https://github.com/oxplot/pdftilecut"
-  url "https://github.com/oxplot/pdftilecut.git",
-      tag:      "v0.4",
-      revision: "af9ca3bff60eed47026a6e1c40b6ef85e5b36393"
+  url "https://github.com/oxplot/pdftilecut/archive/v0.4.tar.gz"
   license "BSD-3-Clause"
 
-  depends_on "autoconf" => :build
-  depends_on "autogen" => :build
-  depends_on "automake" => :build
-  depends_on "cmake" => :build
-  depends_on "coreutils" => :build
   depends_on "go" => :build
-  depends_on "yasm" => :build
+  depends_on "jpeg-turbo" => :build
+  depends_on "qpdf" => :build
 
   def install
-    system "make"
-    bin.install "bin/pdftilecut" => "pdftilecut"
+    system "go", "build", *std_go_args
   end
 
   test do
