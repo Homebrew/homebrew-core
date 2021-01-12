c5674c63c7a1abbb4e0cad79e2eac1477ed23c53
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-01-12 15:26:54 +0000

    azure-storage-cpp: revision bump (icu4c 68.2)

--- Formula/azure-storage-cpp.rb
+++ Formula/azure-storage-cpp.rb
@@ -4,6 +4,7 @@ class AzureStorageCpp < Formula
   url "https://github.com/Azure/azure-storage-cpp/archive/v7.5.0.tar.gz"
   sha256 "446a821d115949f6511b7eb01e6a0e4f014b17bfeba0f3dc33a51750a9d5eca5"
   license "Apache-2.0"
+  revision 1
 
   bottle do
     cellar :any
