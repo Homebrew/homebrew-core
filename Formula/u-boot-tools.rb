347d154a0e3c16c38af5ee70b6fd5e36d7d55e38
rui <rui@meetup.com>
2021-01-13 12:29:54 -0500

    u-boot-tools: add license

--- Formula/u-boot-tools.rb
+++ Formula/u-boot-tools.rb
@@ -3,6 +3,7 @@ class UBootTools < Formula
   homepage "https://www.denx.de/wiki/U-Boot/"
   url "https://ftp.denx.de/pub/u-boot/u-boot-2021.01.tar.bz2"
   sha256 "b407e1510a74e863b8b5cb42a24625344f0e0c2fc7582d8c866bd899367d0454"
+  license all_of: ["GPL-2.0-only", "GPL-2.0-or-later", "BSD-3-Clause"]
 
   livecheck do
     url "https://ftp.denx.de/pub/u-boot/"
