daf8c3ae0bf5e6760fa1935d716346845ad06ac3
chenjian <com.me@foxmail.com>
2021-01-14 20:29:19 +0800

    add gojq

---
+++ Formula/gojq.rb
@@ -0,0 +1,18 @@
+class Gojq < Formula
+  desc "Pure Go implementation of jq"
+  homepage "https://github.com/itchyny/gojq"
+  url "https://github.com/itchyny/gojq/archive/v0.12.0.tar.gz"
+  sha256 "3f280536f53ab6670b89424b0cd295d096b2d2928a5ed5d53bc3a23333051c05"
+  license "MIT"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", "-ldflags", "-s -w", "-trimpath", "-o", bin/"gojq", "cmd/gojq/main.go"
+    prefix.install_metafiles
+  end
+
+  test do
+    assert_equal "2\n", pipe_output("#{bin}/gojq .bar", '{"foo":1, "bar":2}')
+  end
+end
