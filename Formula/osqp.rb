4e7992eb4b2ac02a31858993acc3533500c1ef1d
Rui Chen <rui@chenrui.dev>
2021-01-14 20:37:16 -0500

    osqp 0.6.2

--- Formula/osqp.rb
+++ Formula/osqp.rb
@@ -1,8 +1,8 @@
 class Osqp < Formula
   desc "Operator splitting QP solver"
   homepage "https://osqp.org/"
-  url "https://github.com/oxfordcontrol/osqp/archive/v0.6.0.tar.gz"
-  sha256 "6e00d11d1f88c1e32a4419324b7539b89e8f9cbb1c50afe69f375347c989ba2b"
+  url "https://github.com/oxfordcontrol/osqp/archive/v0.6.2.tar.gz"
+  sha256 "d973c33c3164caa381ed7387375347a46f7522523350a4e51989479b9d3b59c7"
   license "Apache-2.0"
 
   bottle do
