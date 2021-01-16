67eeee77900ac6fac6a584389eede7128df2f595
rui <rui@meetup.com>
2021-01-16 13:36:38 -0500

    makeself: update license

--- Formula/makeself.rb
+++ Formula/makeself.rb
@@ -3,7 +3,7 @@ class Makeself < Formula
   homepage "http://www.megastep.org/makeself/"
   url "https://github.com/megastep/makeself/archive/release-2.4.3.tar.gz"
   sha256 "b4c81c3d0a7c81459459319e9fc75320e057ea908430aa8a020be1993d6b74c8"
-  license "GPL-2.0"
+  license "GPL-2.0-or-later"
   head "https://github.com/megastep/makeself.git"
 
   bottle :unneeded
