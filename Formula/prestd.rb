11a4078d417e058e9ce1756d92f2f4b0b0a4686c
rui <rui@meetup.com>
2021-01-17 10:41:43 -0500

    Update prestd.rb

--- Formula/prestd.rb
+++ Formula/prestd.rb
@@ -6,14 +6,6 @@ class Prestd < Formula
   license "MIT"
   head "https://github.com/prest/prest.git"
 
-  bottle do
-    cellar :any_skip_relocation
-    sha256 "72637c929700df77fb813dd4bc8300774e4b2510d761b701416cc33dbb5f0a3c" => :big_sur
-    sha256 "1e7874a2cf67448b62447eba3b9d81c39e40a0fccefc093e71979ce484209f5d" => :arm64_big_sur
-    sha256 "1d7280403f36a51dafeba47bc389ed9a3f57242f9c8d61e1341bc203aa8d3e3f" => :catalina
-    sha256 "4e053d872bab5d1f102f7a94fc2e8a314157e3dd35f1f624feddd9e7d5130f8f" => :mojave
-  end
-
   depends_on "go" => :build
 
   def install
