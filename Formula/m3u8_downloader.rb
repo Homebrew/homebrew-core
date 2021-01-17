7d522b9fd46bb5304ab6d977339579afa96074b1
Pavel Ershov <devnulpavel@gmail.com>
2021-01-17 23:31:09 +0300

    m3u8_downloader 1.0.0 (MIT License)

--- Formula/m3u8_downloader.rb
+++ Formula/m3u8_downloader.rb
@@ -3,6 +3,7 @@ class M3u8Downloader < Formula
   homepage "https://github.com/DevNulPavel/m3u8_downloader"
   url "https://github.com/DevNulPavel/m3u8_downloader/releases/download/v1.0.0/m3u8_downloader-universal.tar.gz"
   sha256 "825ac395664ce89b48fd4d2672f551eec8eaa42e44962d979e2995a896695d4a"
+  license "MIT"
 
   def install
     bin.install "m3u8_downloader"
