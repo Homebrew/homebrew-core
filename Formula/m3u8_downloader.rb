ea2eb52691b67f67dd0e092a95597882104ed2f3
Pavel Ershov <devnulpavel@gmail.com>
2021-01-17 22:58:37 +0300

    m3u8_downloader 1.0.0 (link fix)

--- Formula/m3u8_downloader.rb
+++ Formula/m3u8_downloader.rb
@@ -1,7 +1,7 @@
 class M3u8Downloader < Formula
   desc "Download m3u8 HLS streams, written in Rust"
   homepage "https://github.com/DevNulPavel/m3u8_downloader"
-  url "https://github.com/DevNulPavel/m3u8_downloader/releases/download/1.0.0/m3u8_downloader-universal.tar.gz"
+  url "https://github.com/DevNulPavel/m3u8_downloader/releases/download/v1.0.0/m3u8_downloader-universal.tar.gz"
   sha256 "825ac395664ce89b48fd4d2672f551eec8eaa42e44962d979e2995a896695d4a"
 
   def install
