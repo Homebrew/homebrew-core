4393ede209667efbbf34954b01ebbe1a8d9bd7dc
Rui Chen <rui@chenrui.dev>
2021-01-18 13:01:43 -0500

    bower 1.8.12

--- Formula/bower.rb
+++ Formula/bower.rb
@@ -4,8 +4,8 @@ class Bower < Formula
   desc "Package manager for the web"
   homepage "https://bower.io/"
   # Use Github tarball to avoid bowers npm 4+ incompatible bundled dep usage
-  url "https://github.com/bower/bower/archive/v1.8.10.tar.gz"
-  sha256 "1485f6b216cde0e156dc22b576ebcf895b3088930393967c26f5bc30a1ac624a"
+  url "https://github.com/bower/bower/archive/v1.8.12.tar.gz"
+  sha256 "ae5ca750299d12a60db61e1e13fc1d69760556531200a0bb4ec586f78fae95e9"
   license "MIT"
 
   livecheck do
