75cb259ce1604c37b0c4367764800d271daa9b15
Rui Chen <rui@chenrui.dev>
2021-01-19 20:21:06 -0500

    django-completion: update homepage and livecheck
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/django-completion.rb
+++ Formula/django-completion.rb
@@ -1,14 +1,14 @@
 class DjangoCompletion < Formula
   desc "Bash completion for Django"
-  homepage "https://github.com/django/django"
+  homepage "https://www.djangoproject.com/"
   url "https://github.com/django/django/archive/3.1.5.tar.gz"
   sha256 "b481d9ffd70c51bd20039a3b5cd33ffe3ac188ba95d29b37e88e456472317eaf"
   license "BSD-3-Clause"
   head "https://github.com/django/django.git"
 
   livecheck do
-    url :head
-    regex(/^v?(\d+(?:\.\d+)+)$/i)
+    url :homepage
+    regex(%r{latest release: .*?v?(\d+(?:\.\d+)+)<}i)
   end
 
   bottle :unneeded
