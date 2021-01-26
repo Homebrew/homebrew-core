a9236496111060b0b5f566008e30c50d8445264b
Rui Chen <rui@chenrui.dev>
2021-01-26 14:38:41 -0500

    remove gopath
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/gron.rb
+++ Formula/gron.rb
@@ -1,5 +1,3 @@
-require "language/go"
-
 class Gron < Formula
   desc "Make JSON greppable"
   homepage "https://github.com/tomnomnom/gron"
@@ -19,37 +17,8 @@ class Gron < Formula
 
   depends_on "go" => :build
 
-  go_resource "github.com/fatih/color" do
-    url "https://github.com/fatih/color.git",
-        revision: "2d684516a8861da43017284349b7e303e809ac21"
-  end
-
-  go_resource "github.com/mattn/go-colorable" do
-    url "https://github.com/mattn/go-colorable.git",
-        revision: "efa589957cd060542a26d2dd7832fd6a6c6c3ade"
-  end
-
-  go_resource "github.com/mattn/go-isatty" do
-    url "https://github.com/mattn/go-isatty.git",
-        revision: "6ca4dbf54d38eea1a992b3c722a76a5d1c4cb25c"
-  end
-
-  go_resource "github.com/nwidger/jsoncolor" do
-    url "https://github.com/nwidger/jsoncolor.git",
-        revision: "75a6de4340e59be95f0884b9cebdda246e0fdf40"
-  end
-
-  go_resource "github.com/pkg/errors" do
-    url "https://github.com/pkg/errors.git",
-        revision: "816c9085562cd7ee03e7f8188a1cfd942858cded"
-  end
-
   def install
-    ENV["GOPATH"] = buildpath
-    (buildpath/"src/github.com/tomnomnom").mkpath
-    ln_s buildpath, buildpath/"src/github.com/tomnomnom/gron"
-    Language::Go.stage_deps resources, buildpath/"src"
-    system "go", "build", "-o", bin/"gron"
+    system "go", "build", *std_go_args, "-ldflags", "-s -w"
   end
 
   test do
