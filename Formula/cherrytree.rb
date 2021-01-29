ce327f59b4ebaf5c86075630cdafb58e1b6da82e
Giuseppe Penone <giuspen@gmail.com>
2021-01-29 22:20:32 +0000

    replaced test with proper document load and export from command line

--- Formula/cherrytree.rb
+++ Formula/cherrytree.rb
@@ -19,11 +19,33 @@ class Cherrytree < Formula
 
   def install
     system "cmake", ".", "-DBUILD_TESTING=''", *std_cmake_args
-    system "make", "-j4"
+    system "make"
     system "make", "install"
   end
 
   test do
-    system "#{bin}/cherrytree", "--version"
+    (testpath/"homebrew.ctd").write <<~EOS
+      <?xml version="1.0" encoding="UTF-8"?>
+      <cherrytree>
+        <bookmarks list=""/>
+        <node name="rich text" unique_id="1" prog_lang="custom-colors" tags="" readonly="0" custom_icon_id="0" is_bold="0" foreground="" ts_creation="1611952177" ts_lastsave="1611952932">
+          <rich_text>this is a </rich_text>
+          <rich_text weight="heavy">simple</rich_text>
+          <rich_text> </rich_text>
+          <rich_text foreground="#ffff00000000">command line</rich_text>
+          <rich_text> </rich_text>
+          <rich_text style="italic">test</rich_text>
+          <rich_text> </rich_text>
+          <rich_text family="monospace">for</rich_text>
+          <rich_text> </rich_text>
+          <rich_text link="webs https://brew.sh/">homebrew</rich_text>
+        </node>
+        <node name="code" unique_id="2" prog_lang="python3" tags="" readonly="0" custom_icon_id="0" is_bold="0" foreground="" ts_creation="1611952391" ts_lastsave="1611952667">
+          <rich_text>print("hello world")</rich_text>
+        </node>
+      </cherrytree>
+    EOS
+    system "#{bin}/cherrytree", testpath/"homebrew.ctd", "--export_to_txt_dir", testpath, "--export_single_file"
+    assert_predicate testpath/"homebrew.ctd.txt", :exist?
   end
 end
