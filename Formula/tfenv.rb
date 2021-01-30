850bed81a56ac45cddb72d33c825e4d7eb104f4d
Rui Chen <rui@chenrui.dev>
2021-01-30 14:14:19 -0500

    fix code style
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/tfenv.rb
+++ Formula/tfenv.rb
@@ -2,7 +2,7 @@ class Tfenv < Formula
   desc "Terraform version manager inspired by rbenv"
   homepage "https://github.com/tfutils/tfenv"
   url "https://github.com/tfutils/tfenv/archive/v2.1.0.tar.gz"
-  sha256 "f8f1e14e1d5064c47ddbb041e31ce15ac0a849e620541598db09efff575b3c9b"  
+  sha256 "f8f1e14e1d5064c47ddbb041e31ce15ac0a849e620541598db09efff575b3c9b"
   license "MIT"
   head "https://github.com/tfutils/tfenv.git"
 
