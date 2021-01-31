d463d76b661a71115d32599f4ea9adf15b05e0e0
rui <rui@chenrui.dev>
2021-01-31 13:37:58 -0500

    remove patch

--- Formula/mkclean.rb
+++ Formula/mkclean.rb
@@ -18,10 +18,6 @@ class Mkclean < Formula
     sha256 "a5db5b2309de19ea395efaafcf828c253e38133464faca623545a221f2b0ba52" => :el_capitan
   end
 
-  # Fixes compile error with Xcode-4.3+, a hardcoded /Developer.  Reported as:
-  # https://sourceforge.net/p/matroska/bugs/9/
-  patch :DATA
-
   def install
     system "./mkclean/configure"
     system "make", "mkclean"
@@ -34,20 +30,3 @@ class Mkclean < Formula
     assert_match version.to_s, output
   end
 end
-
-__END__
---- a/corec/tools/coremake/gcc_osx_x64.build	2017-08-22 06:38:25.000000000 -0700
-+++ b/corec/tools/coremake/gcc_osx_x64.build	2017-11-18 22:53:56.000000000 -0800
-@@ -4,11 +4,10 @@
- 
- PLATFORMLIB = osx_x86
- SVNDIR = osx_x86
--SDK = /Developer/SDKs/MacOSX10.5.sdk
-
- //CC = xcrun --sdk macosx clang
- 
--CCFLAGS=%(CCFLAGS) -arch x86_64 -mdynamic-no-pic -mmacosx-version-min=10.5
-+CCFLAGS=%(CCFLAGS) -arch x86_64 -mdynamic-no-pic
- ASMFLAGS = -f macho64 -D_MACHO -D_HIDDEN
- 
- #include "gcc_osx.inc"
