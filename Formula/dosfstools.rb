9ab581629efbead02548801a20d1cabcc20e4e09
Rui Chen <rui@chenrui.dev>
2021-01-31 11:37:26 -0500

    remove patch and update license
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/dosfstools.rb
+++ Formula/dosfstools.rb
@@ -1,19 +1,11 @@
 class Dosfstools < Formula
   desc "Tools to create, check and label file systems of the FAT family"
   homepage "https://github.com/dosfstools"
-  license "GPL-3.0"
+  url "https://github.com/dosfstools/dosfstools/releases/download/v4.2/dosfstools-4.2.tar.gz"
+  sha256 "64926eebf90092dca21b14259a5301b7b98e7b1943e8a201c7d726084809b527"
+  license "GPL-3.0-or-later"
   head "https://github.com/dosfstools/dosfstools.git"
 
-  stable do
-    url "https://github.com/dosfstools/dosfstools/releases/download/v4.2/dosfstools-4.2.tar.gz"
-    sha256 "64926eebf90092dca21b14259a5301b7b98e7b1943e8a201c7d726084809b527"
-
-    # This patch restores the old defaults in versions up to v3.0.28
-    patch do
-      url "https://github.com/dosfstools/dosfstools/commit/1e76e577.patch?full_index=1"
-      sha256 "7dafc9e5642f8c1f316ac9dedbc0e4becf198b582b255b53bdf45b8de84bf6b6"
-    end
-  end
   bottle do
     cellar :any_skip_relocation
     rebuild 1
