c830d822f9e57b84976ecd4ed6c610e9d185a307
miccal <miccal.matthews@gmail.com>
2021-02-01 22:25:43 +0800

    scummvm-tools: revision bump

--- Formula/scummvm-tools.rb
+++ Formula/scummvm-tools.rb
@@ -1,7 +1,7 @@
 class ScummvmTools < Formula
   desc "Collection of tools for ScummVM"
   homepage "https://www.scummvm.org/"
-  url "https://www.scummvm.org/frs/scummvm-tools/2.2.0/scummvm-tools-2.2.0.tar.xz"
+  url "https://downloads.scummvm.org/frs/scummvm-tools/2.2.0/scummvm-tools-2.2.0.tar.xz"
   sha256 "1e72aa8f21009c1f7447c755e7f4cf499fe9b8ba3d53db681ea9295666cb48a4"
   license "GPL-2.0-or-later"
   head "https://github.com/scummvm/scummvm-tools.git"
