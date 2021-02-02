9dd893a0f333099f8026e8a7c660654b95d71214
André Peric Tavares <andre.peric.tavares@gmail.com>
2021-02-02 17:27:48 -0300

    Update clojure-lsp.rb

--- Formula/clojure-lsp.rb
+++ Formula/clojure-lsp.rb
@@ -2,8 +2,8 @@ class ClojureLsp < Formula
   desc "Language Server (LSP) for Clojure"
   homepage "https://github.com/clojure-lsp/clojure-lsp"
   # Switch to use git tag/revision as needed by `lein-git-version`
-  url "https://github.com/clojure-lsp/clojure-lsp/releases/download/2021.02.02-14.02.23/clojure-lsp",
-  sha256 33182e16557abf76af9d6246da99539c12be46703aae6d9f8c9825b371165a02,
+  url "https://github.com/clojure-lsp/clojure-lsp/releases/download/2021.02.02-14.02.23/clojure-lsp"
+  sha256 "33182e16557abf76af9d6246da99539c12be46703aae6d9f8c9825b371165a02"
   license "MIT"
   version_scheme 1
   head "https://github.com/snoe/clojure-lsp.git"
