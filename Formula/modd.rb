6733414057b50e44dfedd9859d6171f05dcf8000
Francois-Xavier Coudert <fxcoudert@gmail.com>
2021-02-02 12:14:52 +0100

    modd: add GO111MODULE for go1.16

--- Formula/modd.rb
+++ Formula/modd.rb
@@ -17,6 +17,7 @@ class Modd < Formula
   depends_on "go" => :build
 
   def install
+    ENV["GO111MODULE"] = "auto"
     ENV["GOPATH"] = buildpath
     (buildpath/"src/github.com/cortesi/modd").install buildpath.children
     cd "src/github.com/cortesi/modd" do
