0c0d93a242a244a715e7f5753ce88e5ee0119db1
Francois-Xavier Coudert <fxcoudert@gmail.com>
2021-02-02 12:14:14 +0100

    docker-swarm: add GO111MODULE for go1.16

--- Formula/docker-swarm.rb
+++ Formula/docker-swarm.rb
@@ -22,6 +22,7 @@ class DockerSwarm < Formula
   depends_on "go" => :build
 
   def install
+    ENV["GO111MODULE"] = "auto"
     ENV["GOPATH"] = buildpath
     (buildpath/"src/github.com/docker/swarm").install buildpath.children
     cd "src/github.com/docker/swarm" do
