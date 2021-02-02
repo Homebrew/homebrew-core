c0710d49c27679ee75c4255ae56d036859c1d63b
Francois-Xavier Coudert <fxcoudert@gmail.com>
2021-02-02 12:15:22 +0100

    vert: add GO111MODULE for go1.16

--- Formula/vert.rb
+++ Formula/vert.rb
@@ -18,6 +18,7 @@ class Vert < Formula
   depends_on "go" => :build
 
   def install
+    ENV["GO111MODULE"] = "auto"
     ENV["GOPATH"] = buildpath
     (buildpath/"src/github.com/Masterminds/vert").install buildpath.children
     cd "src/github.com/Masterminds/vert" do
