60b04eb53e2c7af6875698ce827774dfe79f81f3
rui <rui@chenrui.dev>
2021-02-03 20:07:11 +0000

    buildozer 4.0.0

--- Formula/buildozer.rb
+++ Formula/buildozer.rb
@@ -2,8 +2,8 @@ class Buildozer < Formula
   desc "Rewrite bazel BUILD files using standard commands"
   homepage "https://github.com/bazelbuild/buildtools"
   url "https://github.com/bazelbuild/buildtools.git",
-      tag:      "3.5.0",
-      revision: "10384511ce98d864faf064a8ed54cdf31b98ac04"
+      tag:      "4.0.0",
+      revision: "840218fa3afc7e7145c1eeb3bfeb612c497e67f7"
   license "Apache-2.0"
   head "https://github.com/bazelbuild/buildtools.git"
 
