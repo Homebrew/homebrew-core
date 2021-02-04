394b24636f66a8c44bcbee3c4193b425d0903e72
André Peric Tavares <andre.peric.tavares@gmail.com>
2021-02-04 13:58:49 -0300

    Update clojure-lsp.rb

--- Formula/clojure-lsp.rb
+++ Formula/clojure-lsp.rb
@@ -2,18 +2,29 @@ class ClojureLsp < Formula
   desc "Language Server (LSP) for Clojure"
   homepage "https://github.com/clojure-lsp/clojure-lsp"
   # Switch to use git tag/revision as needed by `lein-git-version`
-  url "https://github.com/clojure-lsp/clojure-lsp/releases/download/2021.02.02-14.02.23/clojure-lsp"
-  sha256 "33182e16557abf76af9d6246da99539c12be46703aae6d9f8c9825b371165a02"
+  url "https://github.com/clojure-lsp/clojure-lsp.git",
+      tag:      "2021.02.04-02.08.58",
+      revision: "ab32504073688d507b53e47c354733cd6603bc88"
+  version "2021.02.04-02.08.58"
   license "MIT"
   version_scheme 1
-  head "https://github.com/snoe/clojure-lsp.git"
-  bottle :unneeded
+  head "https://github.com/clojure/clojure-lsp.git"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, big_sur:  "a647293f345eead229f83e2707fb2c542958c9b4e33fb0bf4e63c7217548d392"
+    sha256 cellar: :any_skip_relocation, catalina: "079f2087995cd399f1c99dddc5f1d6d92e55af2facf67b427fb633a80faba842"
+    sha256 cellar: :any_skip_relocation, mojave:   "fc1b26dc8f000fc728c26bbedff9f9ab0d6f2071ef17eeb4a0f71c9626184cc7"
+  end
+
+  depends_on "leiningen" => :build
   # The Java Runtime version only recognizes class file versions up to 52.0
   depends_on "openjdk@11"
 
   def install
-    bin.install "clojure-lsp"
+    system "lein", "uberjar"
+    jar = Dir["target/clojure-lsp-*-standalone.jar"][0]
+    libexec.install jar
+    bin.write_jar_script libexec/File.basename(jar), "clojure-lsp"
   end
 
   test do
