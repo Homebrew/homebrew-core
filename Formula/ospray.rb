5349d372ab958a1063b97bcb375ce595ec560103
rui <rui@chenrui.dev>
2021-02-08 20:38:56 +0000

    ospray 2.5.0

--- Formula/ospray.rb
+++ Formula/ospray.rb
@@ -1,8 +1,8 @@
 class Ospray < Formula
   desc "Ray-tracing-based rendering engine for high-fidelity visualization"
   homepage "https://www.ospray.org/"
-  url "https://github.com/ospray/ospray/archive/v2.4.0.tar.gz"
-  sha256 "5eaf7409b08147cbeaf087dbf4b3887c15ffeeaa9cfd16dae3ee85504d9014c2"
+  url "https://github.com/ospray/ospray/archive/v2.5.0.tar.gz"
+  sha256 "074bfd83b5a554daf8da8d9b778b6ef1061e54a1688eac13e0bdccf95593883d"
   license "Apache-2.0"
   head "https://github.com/ospray/ospray.git"
 
