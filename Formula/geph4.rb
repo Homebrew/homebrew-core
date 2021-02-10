9bdc5623fcf8d2223772754944e556d24359aae2
CaptainYukinoshitaHachiman <CaptainYukinoshitaHachiman@protonmail.com>
2021-02-10 12:20:33 +0800

    Add Geph4 v4.1.2

---
+++ Formula/geph4.rb
@@ -0,0 +1,20 @@
+class Geph4 < Formula
+  desc "Modular Internet censorship circumvention system to deal with national filtering"
+  homepage "https://geph.io/"
+  url "https://github.com/geph-official/geph4/archive/v4.1.2.tar.gz"
+  sha256 "7b2a9a7532cf1780b8b3c7a421ae6def63ad0948570813bd251dd50befb3ff77"
+  license "GPL-3.0-only"
+
+  depends_on "rust" => :build
+
+  def install
+    File.delete("Cross.toml")
+    remove_dir(".cargo")
+    Dir.chdir "geph4-client"
+    system "cargo", "install", "--bin", "geph4-client", *std_cargo_args
+  end
+
+  test do
+    assert_equal "geph4-client #{version}", shell_output("#{bin}/geph4-client -V").strip
+  end
+end
