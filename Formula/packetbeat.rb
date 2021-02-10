af83f45198c5cdc260a6ce0c62e4daf8b157b7f4
rui <rui@chenrui.dev>
2021-02-10 16:42:37 +0000

    packetbeat 7.11.0

--- Formula/packetbeat.rb
+++ Formula/packetbeat.rb
@@ -2,8 +2,8 @@ class Packetbeat < Formula
   desc "Lightweight Shipper for Network Data"
   homepage "https://www.elastic.co/products/beats/packetbeat"
   url "https://github.com/elastic/beats.git",
-      tag:      "v7.10.2",
-      revision: "aacf9ecd9c494aa0908f61fbca82c906b16562a8"
+      tag:      "v7.11.0",
+      revision: "84c4d4c4034fcb49c1a318ccdc7311d70adee15b"
   license "Apache-2.0"
   head "https://github.com/elastic/beats.git"
 
