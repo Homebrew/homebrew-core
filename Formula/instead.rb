d33103bd65a518c85bfd3b90c8ea0551680389d6
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-02-10 19:15:44 +0000

    instead: depend on gtk+3

--- Formula/instead.rb
+++ Formula/instead.rb
@@ -14,6 +14,7 @@ class Instead < Formula
 
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
+  depends_on "gtk+3"
   depends_on "luajit-openresty"
   depends_on "sdl2"
   depends_on "sdl2_image"
