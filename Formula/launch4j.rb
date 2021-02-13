50890dd919cdfb784cb30f77ab81185e1c835e90
rui <rui@meetup.com>
2021-02-13 18:25:24 -0500

    launch4j: add license

--- Formula/launch4j.rb
+++ Formula/launch4j.rb
@@ -4,6 +4,7 @@ class Launch4j < Formula
   url "https://downloads.sourceforge.net/project/launch4j/launch4j-3/3.13/launch4j-3.13-macosx-x86.tgz"
   version "3.13"
   sha256 "df232427bd5a4d1674e2e66232127954d18de21d10417ee8e11534d9a914bd0d"
+  license all_of: ["BSD-3-Clause", "MIT"]
 
   bottle :unneeded
 
