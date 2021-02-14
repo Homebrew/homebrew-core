b5ac7b21d792482eb50c98f7b31a9ee312fd6d09
Rui Chen <rui@chenrui.dev>
2021-02-13 20:23:30 -0500

    x264 r3048
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/x264.rb
+++ Formula/x264.rb
@@ -7,8 +7,8 @@ class X264 < Formula
   stable do
     # the latest commit on the stable branch
     url "https://code.videolan.org/videolan/x264.git",
-        revision: "59c06095de90db362b959a4975d8cc6f1566dbdb"
-    version "r3043"
+        revision: "544c61f082194728d0391fb280a6e138ba320a96"
+    version "r3048"
   end
 
   # There's no guarantee that the versions we find on the `release-macos` index
