98132aef6e7f1a085872635122d80fc46598f99e
Alexander Bayandin <a.bayandin@gmail.com>
2021-02-14 21:15:01 +0000

    z.lua: fix version

--- Formula/z.lua.rb
+++ Formula/z.lua.rb
@@ -1,8 +1,8 @@
 class ZLua < Formula
   desc "New cd command that helps you navigate faster by learning your habits"
   homepage "https://github.com/skywind3000/z.lua"
-  url "https://github.com/skywind3000/z.lua/archive/2.8.12.tar.gz"
-  sha256 "5092f186bf23e92437f3fed8bc5862a1e92ebfa322b7aed56887af9029bf6770"
+  url "https://github.com/skywind3000/z.lua/archive/1.8.12.tar.gz"
+  sha256 "1c3d871bf625b5a2eef9ecbb4face9d6432565d05ea8d26322b7a65b2e1d99d2"
   license "MIT"
   head "https://github.com/skywind3000/z.lua.git"
 
