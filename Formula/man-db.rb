049c0934c5ffce877eb09d3146899e6769db0b44
rui <rui@meetup.com>
2021-02-13 21:15:16 -0500

    libpipeline 1.5.3

--- Formula/man-db.rb
+++ Formula/man-db.rb
@@ -28,8 +28,8 @@ class ManDb < Formula
   end
 
   resource "libpipeline" do
-    url "https://download.savannah.gnu.org/releases/libpipeline/libpipeline-1.5.2.tar.gz"
-    sha256 "fd59c649c1ae9d67604d1644f116ad4d297eaa66f838e3dfab96b41e85b059fb"
+    url "https://download.savannah.gnu.org/releases/libpipeline/libpipeline-1.5.3.tar.gz"
+    sha256 "5dbf08faf50fad853754293e57fd4e6c69bb8e486f176596d682c67e02a0adb0"
   end
 
   def install
