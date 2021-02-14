02b0e853517f9f25cf4c4b02c4825017563d14f6
rui <rui@chenrui.dev>
2021-02-14 20:07:52 +0000

    z.lua 2.8.12

--- Formula/z.lua.rb
+++ Formula/z.lua.rb
@@ -1,8 +1,8 @@
 class ZLua < Formula
   desc "New cd command that helps you navigate faster by learning your habits"
   homepage "https://github.com/skywind3000/z.lua"
-  url "https://github.com/skywind3000/z.lua/archive/1.8.11.tar.gz"
-  sha256 "213ea4c6287fa2952fb17db3191dcbaee014b28bff5230f615b23a1869aad912"
+  url "https://github.com/skywind3000/z.lua/archive/2.8.12.tar.gz"
+  sha256 "5092f186bf23e92437f3fed8bc5862a1e92ebfa322b7aed56887af9029bf6770"
   license "MIT"
   head "https://github.com/skywind3000/z.lua.git"
 
