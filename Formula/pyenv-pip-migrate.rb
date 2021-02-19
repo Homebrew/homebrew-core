a0859f3796db0a4c34b47de16729a28dfc1d16b5
Sam Doran <sdoran@redhat.com>
2021-02-19 12:20:42 -0500

    pyenv-pip-migrate: 20181205
    Update to latest commit and following CalVer used in the current (and only)
    release. The project has not made official realeses in quite some time and the
    current release contains bugs.

--- Formula/pyenv-pip-migrate.rb
+++ Formula/pyenv-pip-migrate.rb
@@ -1,8 +1,9 @@
 class PyenvPipMigrate < Formula
   desc "Migrate pip packages from one Python version to another"
   homepage "https://github.com/pyenv/pyenv-pip-migrate"
-  url "https://github.com/pyenv/pyenv-pip-migrate/archive/v20130527.tar.gz"
-  sha256 "1ec5a590a05792843d493a66825ede852b6afc6e95a6a2d2a813e73497c6637a"
+  url "https://github.com/pyenv/pyenv-pip-migrate/archive/88f09de2a06f95bd1933b950ec2b66671ae36fbd.tar.gz"
+  version "20181205"
+  sha256 "bd79295dd5412a6b90311e316e1b78553fc2c612dd6b4dec15cc030d7213fcfb"
   license "MIT"
   head "https://github.com/pyenv/pyenv-pip-migrate.git"
 
