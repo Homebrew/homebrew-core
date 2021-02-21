c82bba880611faeedf37e85098e24e3e08adf6a9
rui <rui@meetup.com>
2021-02-20 21:01:57 -0500

    pyenv-pip-migrate: update artifact and sha

--- Formula/pyenv-pip-migrate.rb
+++ Formula/pyenv-pip-migrate.rb
@@ -1,9 +1,8 @@
 class PyenvPipMigrate < Formula
   desc "Migrate pip packages from one Python version to another"
   homepage "https://github.com/pyenv/pyenv-pip-migrate"
-  url "https://github.com/pyenv/pyenv-pip-migrate/archive/88f09de2a06f95bd1933b950ec2b66671ae36fbd.tar.gz"
-  version "20181205"
-  sha256 "bd79295dd5412a6b90311e316e1b78553fc2c612dd6b4dec15cc030d7213fcfb"
+  url "https://github.com/pyenv/pyenv-pip-migrate/archive/v20181205.tar.gz"
+  sha256 "c064c76b854fa905c40e71b5223699bacf18ca492547aad93cdde2b98ca4e58c"
   license "MIT"
   head "https://github.com/pyenv/pyenv-pip-migrate.git"
 
