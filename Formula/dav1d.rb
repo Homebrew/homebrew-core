be3bcd3a0ae92259d6126a5f6757a405690d416c
MoonFruit <dkmoonfruit@gmail.com>
2021-02-22 09:53:55 +0800

    dav1d 0.8.2

--- Formula/dav1d.rb
+++ Formula/dav1d.rb
@@ -1,8 +1,8 @@
 class Dav1d < Formula
   desc "AV1 decoder targeted to be small and fast"
   homepage "https://code.videolan.org/videolan/dav1d"
-  url "https://code.videolan.org/videolan/dav1d/-/archive/0.8.1/dav1d-0.8.1.tar.bz2"
-  sha256 "842da2945afcf54e651d17112bf2823a238e6c935a6c8dff3a8e96a2eb740269"
+  url "https://code.videolan.org/videolan/dav1d/-/archive/0.8.2/dav1d-0.8.2.tar.bz2"
+  sha256 "78ec7a1714d98a8f4ecbc4255e83e6c4c944cdd881871ea234ce40153fd3df04"
   license "BSD-2-Clause"
 
   bottle do
