48ed353c537d76eb2445ce359d3e13ca9fc1277f
JamesZBL <zhengbaole@gmail.com>
2021-02-23 10:20:06 +0800

    jencrypt 2.0.1

--- Formula/jencrypt.rb
+++ Formula/jencrypt.rb
@@ -2,7 +2,7 @@ class Jencrypt < Formula
   desc "File and directory encryption tool for automatically mounting data volumes"
   homepage "https://github.com/JamesZBL/jencrypt"
   url "https://github.com/JamesZBL/jencrypt/archive/v2.0.1.tar.gz"
-  sha256 "7c33062910afb2776317172ecfd3a162af1d6ac3bf564729040fb8ef5dd4b764"
+  sha256 "fc7a3d0214147c07a59ec24978c1ca0d47438f54"
   license "Apache-2.0"
 
   def install
