d169bb06b7d735d27fd7d224016734729af64727
ChannelJuanNews <jruel006@ucr.edu>
2021-02-26 04:55:27 -0800

    latino 1.3.0

--- Formula/latino.rb
+++ Formula/latino.rb
@@ -1,26 +1,12 @@
 class Latino < Formula
   desc "Lenguaje de programación de código abierto para latinos y de habla hispana"
   homepage "https://www.lenguajelatino.org/"
+  url "https://github.com/lenguaje-latino/Latino/archive/v1.3.0.tar.gz"
+  sha256 "28118a047265394ad9a5fcb2c76895d08af4b43a5886f52afce98a0f8cdc86fe"
   license "MIT"
-  stable do
-    url "https://github.com/lenguaje-latino/Latino/archive/v1.3.0.tar.gz"
-    sha256 "28118a047265394ad9a5fcb2c76895d08af4b43a5886f52afce98a0f8cdc86fe"
-    depends_on "cmake" => :build
-    depends_on "make" => :build
-  end
-  head do
-    url "https://github.com/lenguaje-latino/Latino.git"
-    depends_on "cmake" => :build
-    depends_on "make" => :build
-  end
-  fails_with :clang do
-    build 600
-    cause "multiple configure and compile errors"
-  end
-  fails_with :gcc do
-    build 600
-    cause "multiple configure and compile errors"
-  end
+  head "https://github.com/lenguaje-latino/Latino.git"
+  depends_on "cmake" => :build
+  depends_on "make" => :build
   def install
     mv "logo/Latino-logo.png", "logo/desktop.png"
     system "cmake", ".", *std_cmake_args
@@ -28,6 +14,11 @@ class Latino < Formula
     system "make", "install"
   end
   test do
-    system bin/"latino", "-v"
+    (testpath/"test1.lat").write "poner('hola mundo')"
+    (testpath/"test2.lat").write "i=0\nrepetir\n\tponer(i)\n\ti++\nhasta(i>=10)"
+    output = shell_output("#{bin}/latino test1.lat")
+    assert_equal "hola mundo", output.chomp
+    output2 = shell_output("#{bin}/latino test2.lat")
+    assert_equal "0\n1\n2\n3\n4\n5\n6\n7\n8\n9", output2.chomp
   end
 end
