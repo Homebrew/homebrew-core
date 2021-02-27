6c76bd266e3e3d1c3b874f830e7cae3abc5b3fda
Michka Popoff <michkapopoff@gmail.com>
2021-02-27 23:27:56 +0100

    zabbix: set iconv sdk path only on mac
    Remove clock gettime workaround, we do not support el capitan anymore

--- Formula/zabbix.rb
+++ Formula/zabbix.rb
@@ -31,21 +31,18 @@ class Zabbix < Formula
   end
 
   def install
-    sdk = MacOS::CLT.installed? ? "" : MacOS.sdk_path
-
     args = %W[
       --disable-dependency-tracking
       --prefix=#{prefix}
       --sysconfdir=#{etc}/zabbix
       --enable-agent
-      --with-iconv=#{sdk}/usr
       --with-libpcre=#{Formula["pcre"].opt_prefix}
       --with-openssl=#{Formula["openssl@1.1"].opt_prefix}
     ]
 
-    if MacOS.version == :el_capitan && MacOS::Xcode.version >= "8.0"
-      inreplace "configure", "clock_gettime(CLOCK_REALTIME, &tp);",
-                             "undefinedgibberish(CLOCK_REALTIME, &tp);"
+    on_macos do
+      sdk = MacOS::CLT.installed? ? "" : MacOS.sdk_path
+      args << "--with-iconv=#{sdk}/usr"
     end
 
     system "./configure", *args
