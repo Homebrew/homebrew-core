1ac418e9bc1c446e2a5e05d43d296b31a5b66fd4
Rui Chen <rui@chenrui.dev>
2021-02-28 01:43:24 -0500

    try some fix
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/imgproxy.rb
+++ Formula/imgproxy.rb
@@ -17,6 +17,9 @@ class Imgproxy < Formula
   depends_on "pkg-config" => :build
   depends_on "vips"
 
+  # fix github.com/bugsnag/panicwrap@v1.2.2/monitor.go:56:8: undefined: dup2
+  patch :DATA
+
   def install
     ENV["CGO_LDFLAGS_ALLOW"]="-s|-w"
     ENV["CGO_CFLAGS_ALLOW"]="-Xpreprocessor"
@@ -52,3 +55,30 @@ class Imgproxy < Formula
     Process.wait(pid)
   end
 end
+
+__END__
+diff --git a/go.mod b/go.mod
+index f8042c9..fbab8e9 100644
+--- a/go.mod
++++ b/go.mod
+@@ -33,3 +33,5 @@ require (
+ replace git.apache.org/thrift.git => github.com/apache/thrift v0.0.0-20180902110319-2566ecd5d999
+
+ replace github.com/shirou/gopsutil => github.com/shirou/gopsutil v2.20.9+incompatible
++
++replace github.com/bugsnag/panicwrap => github.com/bugsnag/panicwrap v1.3.1
+diff --git a/go.sum b/go.sum
+index 4647f94..461b703 100644
+--- a/go.sum
++++ b/go.sum
+@@ -118,8 +118,8 @@ github.com/bmizerany/assert v0.0.0-20160611221934-b7ed37b82869 h1:DDGfHa7BWjL4Yn
+ github.com/bmizerany/assert v0.0.0-20160611221934-b7ed37b82869/go.mod h1:Ekp36dRnpXw/yCqJaO+ZrUyxD+3VXMFFr56k5XYrpB4=
+ github.com/bugsnag/bugsnag-go v1.8.0 h1:XnZesafFtcqPryuLUNsWlaTWhdaOcDT66tiorzWVdu4=
+ github.com/bugsnag/bugsnag-go v1.8.0/go.mod h1:2oa8nejYd4cQ/b0hMIopN0lCRxU0bueqREvZLWFrtK8=
+-github.com/bugsnag/panicwrap v1.2.2 h1:2VFfhwhgp270P+wDa/JBt5x7qSm3zOCYoomGX93N8O0=
+-github.com/bugsnag/panicwrap v1.2.2/go.mod h1:D/8v3kj0zr8ZAKg1AQ6crr+5VwKN5eIywRkfhyM/+dE=
++github.com/bugsnag/panicwrap v1.3.1 h1:pmuhHlhbUV4OOrGDvoiMjHSZzwRcL+I9cIzYKiW4lII=
++github.com/bugsnag/panicwrap v1.3.1/go.mod h1:D/8v3kj0zr8ZAKg1AQ6crr+5VwKN5eIywRkfhyM/+dE=
+ github.com/casbin/casbin/v2 v2.1.2/go.mod h1:YcPU1XXisHhLzuxH9coDNf2FbKpjGlbCg3n9yuLkIJQ=
+ github.com/cenkalti/backoff v2.2.1+incompatible/go.mod h1:90ReRw6GdpyfrHakVjL/QHaoyV4aDUVVkXQJJJ3NXXM=
+ github.com/census-instrumentation/opencensus-proto v0.2.1/go.mod h1:f6KPmirojxKA12rnyqOA5BBL4O983OfeGPqjHWSTneU=
