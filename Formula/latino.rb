33459cb1a5f1ea9123e719f39803cd1d232b6256
Juan Ruelas <jruel006@ucr.edu>
2021-03-03 00:20:59 -0800

    Update Formula/latino.rb
    Co-authored-by: Sean Molenaar <SMillerDev@users.noreply.github.com>

--- Formula/latino.rb
+++ Formula/latino.rb
@@ -14,6 +14,7 @@ class Latino < Formula
     system "make"
     system "make", "install"
   end
+
   test do
     (testpath/"test1.lat").write "poner('hola mundo')"
     (testpath/"test2.lat").write <<~EOS
