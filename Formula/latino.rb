e8cb5587ecbdf25ac689dc7989ad9ede7b3f5871
ChannelJuanNews <jruel006@ucr.edu>
2021-03-03 00:13:38 -0800

    latino 1.3.0

--- Formula/latino.rb
+++ Formula/latino.rb
@@ -1,13 +1,11 @@
 class Latino < Formula
   desc "Lenguaje de programación de código abierto para latinos y de habla hispana"
   homepage "https://www.lenguajelatino.org/"
-  url "https://github.com/lenguaje-latino/Latino/archive/v1.3.0.tar.gz"
+  url "https://github.com/MelvinG24/Latino/archive/v1.3.0.tar.gz"
   sha256 "28118a047265394ad9a5fcb2c76895d08af4b43a5886f52afce98a0f8cdc86fe"
   license "MIT"
-  head "https://github.com/lenguaje-latino/Latino.git"
-
+  head "https://github.com/MelvinG24/Latino"
   depends_on "cmake" => :build
-
   def install
     mv "logo/Latino-logo.png", "logo/desktop.png"
     system "cmake", ".", *std_cmake_args
