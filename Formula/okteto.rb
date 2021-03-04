71607e25242235651f25622abd9fc5048dcf407a
rui <rui@chenrui.dev>
2021-03-04 08:31:44 +0000

    okteto 1.11.1

--- Formula/okteto.rb
+++ Formula/okteto.rb
@@ -1,8 +1,8 @@
 class Okteto < Formula
   desc "Build better apps by developing and testing code directly in Kubernetes"
   homepage "https://okteto.com"
-  url "https://github.com/okteto/okteto/archive/1.11.0.tar.gz"
-  sha256 "5ccc467470a9020196c52ff8a8719bdb61e37ccdf579a9f7ce9a8e10d8cbb715"
+  url "https://github.com/okteto/okteto/archive/1.11.1.tar.gz"
+  sha256 "146e94eff3297b6fc9289530ebe2b8b667861c01f1fee6957df5baac3f09cc44"
   license "Apache-2.0"
   head "https://github.com/okteto/okteto.git"
 
