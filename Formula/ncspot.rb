e26c9ff5a50cc8b8aa0d622e6d8df2eee5731adf
rui <rui@chenrui.dev>
2021-03-07 02:23:34 +0000

    ncspot 0.5.0

--- Formula/ncspot.rb
+++ Formula/ncspot.rb
@@ -1,8 +1,8 @@
 class Ncspot < Formula
   desc "Cross-platform ncurses Spotify client written in Rust"
   homepage "https://github.com/hrkfdn/ncspot"
-  url "https://github.com/hrkfdn/ncspot/archive/v0.4.0.tar.gz"
-  sha256 "d43785832cb3283aa8969b366916a2cbd72c5511b6d429aaae306da5287caea6"
+  url "https://github.com/hrkfdn/ncspot/archive/v0.5.0.tar.gz"
+  sha256 "301c99bfe1c62d9f48f3b22a1f7795f61bd9b341ed904afd59d0ceb8693e295d"
   license "BSD-2-Clause"
 
   bottle do
