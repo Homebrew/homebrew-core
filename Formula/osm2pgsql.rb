071524bb23075201f936bcac9949adb31185f686
Alexander Bayandin <a.bayandin@gmail.com>
2021-03-08 10:42:50 +0000

    osm2pgsql: revision bump for proj

--- Formula/osm2pgsql.rb
+++ Formula/osm2pgsql.rb
@@ -4,7 +4,7 @@ class Osm2pgsql < Formula
   url "https://github.com/openstreetmap/osm2pgsql/archive/1.4.1.tar.gz"
   sha256 "33c4817dceed99764b089ead0e8e2f67c4c6675e761772339b635800970e66e2"
   license "GPL-2.0-only"
-  revision 1
+  revision 2
   head "https://github.com/openstreetmap/osm2pgsql.git"
 
   bottle do
