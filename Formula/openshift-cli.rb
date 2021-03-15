2747db1f6755362a2a4e3495f55aaac022fad7d4
Vince Power <vince@power.sh>
2021-03-15 13:22:18 -0300

    missed a word in desc

--- Formula/openshift-cli.rb
+++ Formula/openshift-cli.rb
@@ -1,5 +1,5 @@
 class OpenshiftCli < Formula
-  desc "Red Hat OpenShift Command Line Tools"
+  desc "Red Hat OpenShift command line interface tool"
   homepage "https://www.openshift.com/"
   url "https://mirror.openshift.com/pub/openshift-v4/clients/ocp/4.7.2/openshift-client-mac-4.7.2.tar.gz"
   sha256 "21d18aabc0b15e89d82d94f3a4e67737497000a6e701d746f8394a9197d74848"
