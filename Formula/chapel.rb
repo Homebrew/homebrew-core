69171710a12ac521dc8f692597c0685ced72da6b
Joe Tursi <joe.p.tursi@gmail.com>
2021-03-18 11:05:32 -0400

    Changing url
    Signed-off-by: Joe Tursi <joe.p.tursi@gmail.com>

--- Formula/chapel.rb
+++ Formula/chapel.rb
@@ -1,7 +1,7 @@
 class Chapel < Formula
   desc "Emerging programming language designed for parallel computing"
   homepage "https://chapel-lang.org/"
-  url "https://github.com/chapel-lang/chapel/releases/tag/1.24.0"
+  url "https://github.com/chapel-lang/chapel/releases/download/1.24.0/chapel-1.24.0.tar.gz"
   sha256 "77c6087f3e0837268470915f2ad260d49cf7ac4adf16f5b44862ae624c1be801"
   license "Apache-2.0"
 
