51e29d2a989752c0b5092d329dc58c0b0c526846
Joe Tursi <joe.p.tursi@gmail.com>
2021-03-18 09:31:14 -0400

    chapel 1.24.0

--- Formula/chapel.rb
+++ Formula/chapel.rb
@@ -1,8 +1,8 @@
 class Chapel < Formula
   desc "Emerging programming language designed for parallel computing"
   homepage "https://chapel-lang.org/"
-  url "https://github.com/chapel-lang/chapel/releases/download/1.23.0/chapel-1.23.0.tar.gz"
-  sha256 "7ae2c8f17a7b98ac68378e94a842cf16d4ab0bcfeabc0fee5ab4aaa07b205661"
+  url "https://github.com/chapel-lang/chapel/releases/tag/1.24.0"
+  sha256 "77c6087f3e0837268470915f2ad260d49cf7ac4adf16f5b44862ae624c1be801"
   license "Apache-2.0"
 
   bottle do
