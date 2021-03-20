ac9cb13ef9bf43d94b3a227d50682ce15e89a5b4
rui <rui@chenrui.dev>
2021-03-20 16:47:36 +0000

    gopass 1.12.3

--- Formula/gopass.rb
+++ Formula/gopass.rb
@@ -1,10 +1,9 @@
 class Gopass < Formula
   desc "Slightly more awesome Standard Unix Password Manager for Teams"
   homepage "https://github.com/gopasspw/gopass"
-  url "https://github.com/gopasspw/gopass/releases/download/v1.12.2/gopass-1.12.2.tar.gz"
-  sha256 "b4254ecbc14b62a68e1e98c99d08d53c50a5b5b15b8b5b592266a6d581c93f13"
+  url "https://github.com/gopasspw/gopass/releases/download/v1.12.3/gopass-1.12.3.tar.gz"
+  sha256 "3b38ef45df8caed208cd1d34cbf69fa640cd44908f60042b8d3f0e318d1dd62d"
   license "MIT"
-  revision 2
   head "https://github.com/gopasspw/gopass.git"
 
   bottle do
