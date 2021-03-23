6341adff5f083010648ef26ce3bebba95811f5a6
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-03-23 03:40:23 +0000

    pyqt-networkauth 6.0.3

--- Formula/pyqt-networkauth.rb
+++ Formula/pyqt-networkauth.rb
@@ -1,8 +1,8 @@
 class PyqtNetworkauth < Formula
   desc "Python bindings for The Qt Company’s Qt Network Authorization library"
   homepage "https://www.riverbankcomputing.com/software/pyqtnetworkauth"
-  url "https://files.pythonhosted.org/packages/56/7e/a400ce770ca83194cca6a129ca6e8339b2ec71b5dbe03fd8f1570be385e0/PyQt6_NetworkAuth-6.0.2.tar.gz"
-  sha256 "ba3c55dc7ce7f4b9034f8675c5fe8d37830d986716ff2ef0cca29ee80240f609"
+  url "https://files.pythonhosted.org/packages/21/de/a7c4ef992a66be7ee458ab25ad7cdf93483712517ba807a74c30dc7c9375/PyQt6_NetworkAuth-6.0.3.tar.gz"
+  sha256 "ae3c1540e9504eea024ee4da13c6ead146f987aa5fe1942c8d7465ef631e8ba8"
   license "GPL-3.0-only"
 
   bottle do
