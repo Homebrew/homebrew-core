214802fcc54ee866773e5accc61af1cbf750d1ff
Sean Molenaar <SMillerDev@users.noreply.github.com>
2021-03-23 20:56:09 +0100

    lightgbm: use git checkout
    Co-authored-by: Nikita Titov <nekit94-08@mail.ru>

--- Formula/lightgbm.rb
+++ Formula/lightgbm.rb
@@ -1,8 +1,9 @@
 class Lightgbm < Formula
   desc "Fast, distributed, high performance gradient boosting framework"
   homepage "https://github.com/microsoft/LightGBM"
-  url "https://github.com/microsoft/LightGBM/releases/download/v3.2.0/LightGBM-3.2.0-complete_source_code_tar_gz.tar.gz"
-  sha256 "a470b46cc5768f119b7ce5c79a38b8c88ca8d3d7db5d958f6c38f37e4fb74b83"
+  url "https://github.com/microsoft/LightGBM.git",
+      tag:      "v3.2.0",
+      revision: "2e54d5f518a4c7db721483347bec6676a8d20c89"
   license "MIT"
 
   bottle do
