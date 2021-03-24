9fa560343cc5ac04d2cc4c8ae77d99a87acd8859
Antti Honkela <antti.honkela@helsinki.fi>
2021-03-24 09:13:20 +0200

    pandoc-plot: improved test using graphviz

--- Formula/pandoc-plot.rb
+++ Formula/pandoc-plot.rb
@@ -7,6 +7,7 @@ class PandocPlot < Formula
 
   depends_on "cabal-install" => :build
   depends_on "ghc" => :build
+  depends_on "graphviz" => :test
   depends_on "pandoc"
 
   def install
@@ -15,6 +16,21 @@ class PandocPlot < Formula
   end
 
   test do
-    system "#{bin}/pandoc-plot", "toolkits"
+    input_markdown = <<~EOS
+      # pandoc-plot demo
+
+      ```{.graphviz}
+      digraph {
+        pandoc -> plot
+      }
+      ```
+    EOS
+    expected_html = <<~EOS
+      <h1 id="pandoc-plot-demo">pandoc-plot demo</h1>
+      <figure>
+      <img src="plots/15141642126418287350.png" />
+      </figure>
+    EOS
+    assert_equal expected_html, pipe_output("pandoc --filter #{bin}/pandoc-plot -f markdown -t html5", input_markdown)
   end
 end
