187028b3e9a3818485166a22349541f35b268800
rui <rui@chenrui.dev>
2021-03-24 08:03:30 +0000

    instead 3.3.4

--- Formula/instead.rb
+++ Formula/instead.rb
@@ -1,8 +1,8 @@
 class Instead < Formula
   desc "Interpreter of simple text adventures"
   homepage "https://instead.syscall.ru/"
-  url "https://github.com/instead-hub/instead/archive/3.3.3.tar.gz"
-  sha256 "1425c559ab91c8ec3e02ad74f6989a4408ee7141d69361d17aeff174d194fe57"
+  url "https://github.com/instead-hub/instead/archive/3.3.4.tar.gz"
+  sha256 "6577235e42a22d8f7f628fedc998d718e65da9e1a3cb61f09be6d1cd648fc061"
   license "MIT"
 
   bottle do
