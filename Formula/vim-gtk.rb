0f3a2fafbbc1add97415e0c8675bb248305b8cd9
brewfixvim <81582700+brewfixvim@users.noreply.github.com>
2021-03-31 16:52:42 +0300

    vim-gtk 8.2.2650 (new formula)

---
+++ Formula/vim-gtk.rb
@@ -0,0 +1,75 @@
+class VimGtk < Formula
+  desc "Vi 'workalike' with many additional features"
+  homepage "https://www.vim.org/"
+  # vim should only be updated every 25 releases on multiples of 25
+  url "https://github.com/vim/vim/archive/v8.2.2650.tar.gz"
+  sha256 "0284b0e66287641b196b29496ea7d44ad5a728da0fc3b8eea36f1ed3636b661e"
+  license "Vim"
+  head "https://github.com/vim/vim.git"
+
+  depends_on "gettext"
+  depends_on "gtk+3"
+  depends_on "lua"
+  depends_on "perl"
+  depends_on "python@3.9"
+  depends_on "ruby"
+
+  uses_from_macos "ncurses"
+
+  conflicts_with "ex-vi",
+    because: "vim and ex-vi both install bin/ex and bin/view"
+
+  conflicts_with "macvim",
+    because: "vim and macvim both install vi* binaries"
+
+  conflicts_with "vim",
+    because: "vim and macvim both install vi* binaries"
+
+  def install
+    ENV.prepend_path "PATH", Formula["python@3.9"].opt_libexec/"bin"
+
+    # https://github.com/Homebrew/homebrew-core/pull/1046
+    ENV.delete("SDKROOT")
+
+    # vim doesn't require any Python package, unset PYTHONPATH.
+    ENV.delete("PYTHONPATH")
+
+    # We specify HOMEBREW_PREFIX as the prefix to make vim look in the
+    # the right place (HOMEBREW_PREFIX/share/vim/{vimrc,vimfiles}) for
+    # system vimscript files. We specify the normal installation prefix
+    # when calling "make install".
+    # Homebrew will use the first suitable Perl & Ruby in your PATH if you
+    # build from source. Please don't attempt to hardcode either.
+    system "./configure", "--prefix=#{HOMEBREW_PREFIX}",
+                          "--mandir=#{man}",
+                          "--enable-multibyte",
+                          "--with-tlib=ncurses",
+                          "--with-compiledby=Homebrew",
+                          "--enable-cscope",
+                          "--enable-perlinterp",
+                          "--enable-rubyinterp",
+                          "--enable-python3interp",
+                          "--enable-gui=gtk3",
+                          "--enable-luainterp",
+                          "--with-lua-prefix=#{Formula["lua"].opt_prefix}"
+    system "make"
+    # Parallel install could miss some symlinks
+    # https://github.com/vim/vim/issues/1031
+    ENV.deparallelize
+    # If stripping the binaries is enabled, vim will segfault with
+    # statically-linked interpreters like ruby
+    # https://github.com/vim/vim/issues/114
+    system "make", "install", "prefix=#{prefix}", "STRIP=#{which "true"}"
+    bin.install_symlink "vim" => "vi"
+  end
+
+  test do
+    (testpath/"commands.vim").write <<~EOS
+      :python3 import vim; vim.current.buffer[0] = 'hello python3'
+      :wq
+    EOS
+    system bin/"vim", "-T", "dumb", "-s", "commands.vim", "test.txt"
+    assert_equal "hello python3", File.read("test.txt").chomp
+    assert_match "+gettext", shell_output("#{bin}/vim --version")
+  end
+end
