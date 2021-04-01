954abce10ab9a357795ec0e170feea2bfc5dcfd7
Matthew Thompson <matthew.thompson@nasa.gov>
2021-04-01 08:50:19 -0400

    Update boxes.rb for version 2.0.0

--- Formula/boxes.rb
+++ Formula/boxes.rb
@@ -1,8 +1,8 @@
 class Boxes < Formula
   desc "Draw boxes around text"
   homepage "https://boxes.thomasjensen.com/"
-  url "https://github.com/ascii-boxes/boxes/archive/v1.3.tar.gz"
-  sha256 "cbb46b3b0ef2c016f9ebcebea5acf6c2bfec39dfb6696dc8f4427a3f844cd567"
+  url "https://github.com/ascii-boxes/boxes/archive/v2.0.0.tar.gz"
+  sha256 "952c85eccde42d23252dfe590d6f296a2179297ad1c54745330fb794907f5ad9"
   license "GPL-2.0"
   head "https://github.com/ascii-boxes/boxes.git"
 
@@ -14,6 +14,9 @@ class Boxes < Formula
     sha256 high_sierra:   "ca1c4e0e76f03ee4a60789f30093d2eee3794ff54b989da1a8a3ae555228f081"
     sha256 sierra:        "d31462128d1f55cd3014ae942b4620f1ec4d06e72e8a47cae5ef56afcf65e791"
   end
+  
+  depends_on "libunistring"
+  depends_on "pcre"
 
   def install
     # distro uses /usr/share/boxes change to prefix
