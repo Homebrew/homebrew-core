8aa718216f804857ec995f8ae5b2893217795354
rui <rui@chenrui.dev>
2021-04-15 00:26:28 +0000

    latino 1.4.0

--- Formula/latino.rb
+++ Formula/latino.rb
@@ -1,8 +1,8 @@
 class Latino < Formula
   desc "Lenguaje de programación de código abierto para latinos y de habla hispana"
   homepage "https://www.lenguajelatino.org/"
-  url "https://github.com/MelvinG24/Latino/archive/v1.3.0.tar.gz"
-  sha256 "28118a047265394ad9a5fcb2c76895d08af4b43a5886f52afce98a0f8cdc86fe"
+  url "https://github.com/MelvinG24/Latino/archive/v1.4.0.tar.gz"
+  sha256 "71ebe3cc6c041cd8b30dbf21ee1accb966cf4b094b615f54b5d358e594994ece"
   license "MIT"
   head "https://github.com/MelvinG24/Latino"
 
