6a70bf58b3c07b605a33ad258bbd92ceffeb589f
rui <rui@chenrui.dev>
2021-04-19 08:07:01 +0000

    embulk 0.10.30

--- Formula/embulk.rb
+++ Formula/embulk.rb
@@ -4,8 +4,8 @@ class Embulk < Formula
   # https://www.embulk.org/articles/2020/03/13/embulk-v0.10.html
   # v0.10.* is a "development" series, not for your production use.
   # In your production, keep using v0.9.* stable series.
-  url "https://github.com/embulk/embulk/releases/download/v0.9.23/embulk-0.9.23.jar"
-  sha256 "153977fad482bf52100dd96f47e897c87b48de4fb13bccd6b3101475d3a5ebb9"
+  url "https://github.com/embulk/embulk/releases/download/v0.10.30/embulk-0.10.30.jar"
+  sha256 "ab6ede91ca025dae998bac09b9ad55340e8f14ff91bedd696e973bb302aa5816"
   license "Apache-2.0"
   version_scheme 1
 
