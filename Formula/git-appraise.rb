987d1b4bb6b3659d3924f0027faddefbd0a1229d
rui <rui@chenrui.dev>
2021-04-23 00:31:20 +0000

    git-appraise 0.7

--- Formula/git-appraise.rb
+++ Formula/git-appraise.rb
@@ -1,8 +1,8 @@
 class GitAppraise < Formula
   desc "Distributed code review system for Git repos"
   homepage "https://github.com/google/git-appraise"
-  url "https://github.com/google/git-appraise/archive/v0.6.tar.gz"
-  sha256 "5c674ee7f022cbc36c5889053382dde80b8e80f76f6fac0ba0445ed5313a36f1"
+  url "https://github.com/google/git-appraise/archive/v0.7.tar.gz"
+  sha256 "b57dd4ac4746486e253658b2c93422515fd8dc6fdca873b5450a6fb0f9487fb3"
   license "Apache-2.0"
   head "https://github.com/google/git-appraise.git"
 
