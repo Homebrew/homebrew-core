fbc629278014c8346fceadba028a8685a2f6a6af
rui <rui@chenrui.dev>
2021-04-24 16:12:00 +0000

    bmake 20210420

--- Formula/bmake.rb
+++ Formula/bmake.rb
@@ -1,8 +1,8 @@
 class Bmake < Formula
   desc "Portable version of NetBSD make(1)"
   homepage "http://www.crufty.net/help/sjg/bmake.html"
-  url "http://www.crufty.net/ftp/pub/sjg/bmake-20200902.tar.gz"
-  sha256 "082c0442f03f2dbef8c3171000398c1936047aa0d5a2e1efc2c8474d69403bec"
+  url "http://www.crufty.net/ftp/pub/sjg/bmake-20210420.tar.gz"
+  sha256 "47e551293843d504fe4197413aa6d7ee003090669ac200932ff40e9ccb4658aa"
   license "BSD-3-Clause"
 
   livecheck do
