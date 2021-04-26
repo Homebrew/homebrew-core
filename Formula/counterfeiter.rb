dfd8d4a2e819aa70cdc48ab05319151d29897d15
rui <rui@chenrui.dev>
2021-04-26 01:00:28 -0400

    refine build syntax
    Co-authored-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>

--- Formula/counterfeiter.rb
+++ Formula/counterfeiter.rb
@@ -16,7 +16,7 @@ class Counterfeiter < Formula
   depends_on "go"
 
   def install
-    system "go", "build", "-ldflags", "-s -w", *std_go_args
+    system "go", "build", *std_go_args(ldflags: "-s -w")
   end
 
   test do
