b62d2ad6e8fd7c8b6ac9dac05fb299276091a193
Amar1729 <amar.paul16@gmail.com>
2021-04-28 00:40:24 -0400

    idnits: remove `bottle :unneeded`

--- Formula/idnits.rb
+++ Formula/idnits.rb
@@ -10,8 +10,6 @@ class Idnits < Formula
     regex(/href=.*?idnits[._-]v?(\d+(?:\.\d+)+)\.t/i)
   end
 
-  bottle :unneeded
-
   resource "test" do
     url "https://tools.ietf.org/id/draft-ietf-tcpm-undeployed-03.txt"
     sha256 "34e72c2c089409dc1935e18f75351025af3cfc253dee50db042d188b46733550"
