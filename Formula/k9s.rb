4ef18a2aa840d700a6d5301761669babad4662ed
rui <rui@chenrui.dev>
2021-04-29 18:18:23 +0000

    k9s 0.24.8

--- Formula/k9s.rb
+++ Formula/k9s.rb
@@ -2,8 +2,8 @@ class K9s < Formula
   desc "Kubernetes CLI To Manage Your Clusters In Style!"
   homepage "https://k9scli.io/"
   url "https://github.com/derailed/k9s.git",
-      tag:      "v0.24.7",
-      revision: "303de07663dcb20899852a98d3ebf6ce2f1c7922"
+      tag:      "v0.24.8",
+      revision: "627c38cb360f7a9b6eb5f856c7658838644d1540"
   license "Apache-2.0"
   head "https://github.com/derailed/k9s.git"
 
