adbe59bb3db2e9cebcff41556ebcbdf5aa4c3c52
Amar1729 <amar.paul16@gmail.com>
2021-04-28 20:47:21 -0400

    zinc: remove `bottle :unneeded`

--- Formula/zinc.rb
+++ Formula/zinc.rb
@@ -4,8 +4,6 @@ class Zinc < Formula
   url "https://downloads.typesafe.com/zinc/0.3.15/zinc-0.3.15.tgz"
   sha256 "5ec4df3fa2cbb271d65a5b478c940a9da6ef4902aa8c9d41a76dd253e3334ca7"
 
-  bottle :unneeded
-
   deprecate! date: "2018-06-10", because: :repo_archived
 
   def install
