da500ec34caa9a065fb459eb710982db6770fc9f
Yurii Kolesnykov <root@yurikoles.com>
2021-04-30 04:52:59 +0300

    openjdk 15.0.3

--- Formula/openjdk.rb
+++ Formula/openjdk.rb
@@ -8,8 +8,8 @@ class Openjdk < Formula
     sha256 "29df31b5eefb5a6c016f50b2518ca29e8e61e3cfc676ed403214e1f13a78efd5"
     version "15.0.2"
   else
-    url "https://hg.openjdk.java.net/jdk-updates/jdk15u/archive/jdk-15.0.2-ga.tar.bz2"
-    sha256 "d07b45b5b319e7034e8ebc41cd78c496e6ee8b1f6e08310dee303beaee8b4a3a"
+    url "https://hg.openjdk.java.net/jdk-updates/jdk15u/archive/jdk-15.0.3-ga.tar.bz2"
+    sha256 "b9d2a6fdcf22af448f5226c748ecf7375cceda414313a3573561c2cb23ee86ba"
   end
   license :cannot_represent
 
