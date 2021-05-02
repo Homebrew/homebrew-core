8ab41fbb2d76f136c3c37c789fa941a5985ec094
Rui Chen <rui@chenrui.dev>
2021-05-02 17:26:30 -0400

    fix build

--- Formula/cadence-workflow.rb
+++ Formula/cadence-workflow.rb
@@ -1,8 +1,9 @@
 class CadenceWorkflow < Formula
   desc "Distributed, scalable, durable, and highly available orchestration engine"
   homepage "https://cadenceworkflow.io/"
-  url "https://github.com/uber/cadence/archive/refs/tags/v0.20.0.tar.gz"
-  sha256 "7d08b62b78c4c9d64d6e6c6c5e7874a0c0abad01369c378777d97c39c3d77c3a"
+  url "https://github.com/uber/cadence.git",
+    tag:      "v0.20.0",
+    revision: "175850a28469c79c3ec43da7832d5a997c079667"
   license "MIT"
   head "https://github.com/uber/cadence.git"
 
@@ -18,10 +19,8 @@ class CadenceWorkflow < Formula
   conflicts_with "cadence", because: "both install an `cadence` executable"
 
   # build patch, remove in next release
-  patch do
-    url "https://github.com/uber/cadence/commit/b6b70ffbced027f0833696aea09be350bd41d15e.patch?full_index=1"
-    sha256 "1547ac2d655e8b8cebb3fb00431bf0e5ff6c0ed4100d2dbe16ee0afbb404e1cf"
-  end
+  # ref commit, https://github.com/uber/cadence/commit/b6b70ffbced027f0833696aea09be350bd41d15e
+  patch :DATA
 
   def install
     system "make", "cadence", "cadence-server", "cadence-canary", "cadence-sql-tool", "cadence-cassandra-tool"
@@ -42,3 +41,127 @@ class CadenceWorkflow < Formula
     assert_match "Error: Operation DescribeDomain failed", output
   end
 end
+
+__END__
+diff --git a/Makefile b/Makefile
+index 55f32cb..437c34b 100644
+--- a/Makefile
++++ b/Makefile
+@@ -128,8 +128,8 @@ $(BIN)/goimports: go.mod
+ $(BIN)/revive: go.mod
+ 	$(call go_build_tool,github.com/mgechev/revive)
+
+-$(BIN)/protoc-gen-gofast: go.mod | $(BIN)
+-	$(call go_build_tool,github.com/gogo/protobuf/protoc-gen-gofast)
++$(BIN)/protoc-gen-gogofast: go.mod | $(BIN)
++	$(call go_build_tool,github.com/gogo/protobuf/protoc-gen-gogofast)
+
+ $(BIN)/protoc-gen-yarpc-go: go.mod | $(BIN)
+ 	$(call go_build_tool,go.uber.org/yarpc/encoding/protobuf/protoc-gen-yarpc-go)
+@@ -156,7 +156,7 @@ $(BIN)/$(BUF_VERSION_BIN): | $(BIN)
+ 	@chmod +x $@
+
+ # https://www.grpc.io/docs/languages/go/quickstart/
+-# protoc-gen-gofast (yarpc) are versioned via tools.go + go.mod (built above) and will be rebuilt as needed.
++# protoc-gen-gogofast (yarpc) are versioned via tools.go + go.mod (built above) and will be rebuilt as needed.
+ # changing PROTOC_VERSION will automatically download and use the specified version
+ PROTOC_VERSION = 3.14.0
+ PROTOC_URL = https://github.com/protocolbuffers/protobuf/releases/download/v$(PROTOC_VERSION)/protoc-$(PROTOC_VERSION)-$(subst Darwin,osx,$(OS))-$(ARCH).zip
+@@ -166,7 +166,7 @@ PROTOC_UNZIP_DIR = $(BIN)/protoc-$(PROTOC_VERSION)-zip
+ # otherwise this must be a .PHONY rule, or the buf bin / symlink could become out of date.
+ PROTOC_VERSION_BIN = protoc-$(PROTOC_VERSION)
+ $(BIN)/$(PROTOC_VERSION_BIN): | $(BIN)
+-	@echo "downloading protoc $(PROTOC_VERSION)"
++	@echo "downloading protoc $(PROTOC_VERSION): $(PROTOC_URL)"
+ 	@# recover from partial success
+ 	@rm -rf $(BIN)/protoc.zip $(PROTOC_UNZIP_DIR)
+ 	@# download, unzip, copy to a normal location
+@@ -178,6 +178,15 @@ $(BIN)/$(PROTOC_VERSION_BIN): | $(BIN)
+ # Codegen targets
+ # ====================================
+
++# IDL submodule must be populated, or files will not exist -> prerequisites will be wrong -> build will fail.
++# Because it must exist before the makefile is parsed, this cannot be done automatically as part of a build.
++# Instead: call this func in targets that require the submodule to exist, so that target will not be built.
++#
++# THRIFT_FILES is just an easy identifier for "the submodule has files", others would work fine as well.
++define ensure_idl_submodule
++$(if $(THRIFT_FILES),,$(error idls/ submodule must exist, or build will fail.  Run `git submodule update --init` and try again))
++endef
++
+ # codegen is done when thrift and protoc are done
+ $(BUILD)/codegen: $(BUILD)/thrift $(BUILD)/protoc | $(BUILD)
+ 	@touch $@
+@@ -190,6 +199,7 @@ THRIFT_GEN := $(subst idls/thrift/,.build/,$(THRIFT_FILES))
+
+ # thrift is done when all sub-thrifts are done
+ $(BUILD)/thrift: $(THRIFT_GEN) | $(BUILD)
++	$(call ensure_idl_submodule)
+ 	@touch $@
+
+ # how to generate each thrift book-keeping file.
+@@ -209,23 +219,24 @@ $(THRIFT_GEN): $(THRIFT_FILES) $(BIN)/thriftrw $(BIN)/thriftrw-plugin-yarpc | $(
+ PROTO_ROOT := proto
+ # output location is defined by `option go_package` in the proto files, all must stay in sync with this
+ PROTO_OUT := .gen/proto
+-PROTO_FILES = $(shell find ./$(PROTO_ROOT) -name "*.proto" | grep -v "persistenceblobs")
++PROTO_FILES = $(shell find -L ./$(PROTO_ROOT) -name "*.proto" | grep -v "persistenceblobs")
+ PROTO_DIRS = $(sort $(dir $(PROTO_FILES)))
+
+-# protoc splits proto files into directories, otherwise protoc-gen-gofast is complaining about inconsistent package
++# protoc splits proto files into directories, otherwise protoc-gen-gogofast is complaining about inconsistent package
+ # import paths due to multiple packages being compiled at once.
+ #
+ # After compilation files are moved to final location, as plugins adds additional path based on proto package.
+-$(BUILD)/protoc: $(PROTO_FILES) $(BIN)/$(PROTOC_VERSION_BIN) $(BIN)/protoc-gen-gofast $(BIN)/protoc-gen-yarpc-go | $(BUILD)
++$(BUILD)/protoc: $(PROTO_FILES) $(BIN)/$(PROTOC_VERSION_BIN) $(BIN)/protoc-gen-gogofast $(BIN)/protoc-gen-yarpc-go | $(BUILD)
++	$(call ensure_idl_submodule)
+ 	@mkdir -p $(PROTO_OUT)
+ 	@echo "protoc..."
+ 	@$(foreach PROTO_DIR,$(PROTO_DIRS),$(BIN)/$(PROTOC_VERSION_BIN) \
+-		--plugin $(BIN)/protoc-gen-gofast \
++		--plugin $(BIN)/protoc-gen-gogofast \
+ 		--plugin $(BIN)/protoc-gen-yarpc-go \
+ 		-I=$(PROTO_ROOT)/public \
+ 		-I=$(PROTO_ROOT)/internal \
+ 		-I=$(PROTOC_UNZIP_DIR)/include \
+-		--gofast_out=Mgoogle/protobuf/duration.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/field_mask.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/timestamp.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/wrappers.proto=github.com/gogo/protobuf/types,paths=source_relative:$(PROTO_OUT) \
++		--gogofast_out=Mgoogle/protobuf/duration.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/field_mask.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/timestamp.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/wrappers.proto=github.com/gogo/protobuf/types,paths=source_relative:$(PROTO_OUT) \
+ 		--yarpc-go_out=$(PROTO_OUT) \
+ 		$$(find $(PROTO_DIR) -name '*.proto');\
+ 	)
+@@ -243,16 +254,22 @@ $(BUILD)/protoc: $(PROTO_FILES) $(BIN)/$(PROTOC_VERSION_BIN) $(BIN)/protoc-gen-g
+ # this will ensure that committed code will be used rather than re-generating.
+ # must be manually run before (nearly) any other targets.
+ .fake-codegen: .fake-protoc .fake-thrift
++	$(warning build-tool binaries have been faked, you will need to delete the $(BIN) folder if you wish to build real ones)
++	@# touch a marker-file for a `make clean` warning.  this does not impact behavior.
++	touch $(BIN)/fake-codegen
+
+ # "build" fake binaries, and touch the book-keeping files, so Make thinks codegen has been run.
+ # order matters, as e.g. a $(BIN) newer than a $(BUILD) implies Make should run the $(BIN).
+ .fake-protoc: | $(BIN) $(BUILD)
+-	touch $(BIN)/$(PROTOC_VERSION_BIN) $(BIN)/protoc-gen-gofast $(BIN)/protoc-gen-yarpc-go
++	touch $(BIN)/$(PROTOC_VERSION_BIN) $(BIN)/protoc-gen-gogofast $(BIN)/protoc-gen-yarpc-go
+ 	touch $(BUILD)/protoc
+
+ .fake-thrift: | $(BIN) $(BUILD)
+ 	touch $(BIN)/thriftrw $(BIN)/thriftrw-plugin-yarpc
+-	touch $(THRIFT_GEN)
++	@# if the submodule exists, touch thrift_gen markers to fake their generation.
++	@# if it does not, do nothing - there are none.
++	$(if $(THRIFT_GEN),touch $(THRIFT_GEN),)
++	touch $(BUILD)/thrift
+
+ # ====================================
+ # other intermediates
+@@ -385,7 +402,9 @@ release: ## Re-generate generated code and run tests
+ clean: ## Clean binaries and build folder
+ 	rm -f $(BINS)
+ 	rm -Rf $(BUILD)
+-	@echo '# rm -rf $(BIN) # not removing tools dir, it is rarely necessary'
++	$(if \
++		$(filter $(BIN)/fake-codegen, $(wildcard $(BIN)/*)), \
++		$(warning fake build tools may exist, delete the $(BIN) folder to get real ones if desired),)
+
+ # v----- not yet cleaned up -----v
