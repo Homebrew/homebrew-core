e2172f893bea249f3b76f0d103eded035a622a71
rui <rui@chenrui.dev>
2021-05-02 22:07:01 +0000

    macchina 0.7.3

--- Formula/macchina.rb
+++ Formula/macchina.rb
@@ -1,8 +1,8 @@
 class Macchina < Formula
   desc "System information fetcher, with an emphasis on performance and minimalism"
   homepage "https://github.com/Macchina-CLI/macchina"
-  url "https://github.com/Macchina-CLI/macchina/archive/v0.7.0.tar.gz"
-  sha256 "6ff1497864a400f5eade2b46984cbe1259a0f7a75bf7191a2a75e111a8ffe119"
+  url "https://github.com/Macchina-CLI/macchina/archive/v0.7.3.tar.gz"
+  sha256 "3d770b2a06f7ef2c7163809dc24c3d214b228d985ac70516f59234a5581febb0"
   license "MIT"
 
   bottle do
