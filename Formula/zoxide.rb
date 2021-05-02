3c98adb0b1ab7b6764afdb49635e62afc7615a19
rui <rui@chenrui.dev>
2021-05-02 00:12:20 +0000

    zoxide 0.7.0

--- Formula/zoxide.rb
+++ Formula/zoxide.rb
@@ -1,8 +1,8 @@
 class Zoxide < Formula
   desc "Shell extension to navigate your filesystem faster"
   homepage "https://github.com/ajeetdsouza/zoxide"
-  url "https://github.com/ajeetdsouza/zoxide/archive/v0.6.0.tar.gz"
-  sha256 "fe93aa4d1fe8d4f94b302335f30bc543c7a1664df2a3fde73e6253c74f576c35"
+  url "https://github.com/ajeetdsouza/zoxide/archive/v0.7.0.tar.gz"
+  sha256 "5329bc396ef2f40c8df9bdc5737847426906f839bfbc26245e5c5491ead10a7f"
   license "MIT"
 
   bottle do
