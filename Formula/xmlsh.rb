d2a353749d7d5cc319455a25af545a16358d4922
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-05-02 01:50:05 +0100

    xmlsh: remove `bottle :unneeded`
    Also, add `openjdk` dependency and modify env script appropriately.

--- Formula/xmlsh.rb
+++ Formula/xmlsh.rb
@@ -9,17 +9,15 @@ class Xmlsh < Formula
     regex(%r{url=.*?/v?(\d+(?:\.\d+)+)/xmlsh}i)
   end
 
-  bottle :unneeded
+  depends_on "openjdk"
 
   def install
     rm_rf %w[win32 cygwin]
     libexec.install Dir["*"]
     chmod 0755, "#{libexec}/unix/xmlsh"
-    (bin/"xmlsh").write <<~EOS
-      #!/bin/bash
-      export XMLSH=#{libexec}
-      exec #{libexec}/unix/xmlsh "$@"
-    EOS
+    env = { XMLSH: libexec }
+    env.merge! Language::Java.overridable_java_home_env
+    (bin/"xmlsh").write_env_script libexec/"unix/xmlsh", env
   end
 
   test do
