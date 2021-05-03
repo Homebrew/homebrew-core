cca13bf5ff03a15711b8b928cf91ae51082d9de9
Rui Chen <rui@chenrui.dev>
2021-05-03 16:09:45 -0400

    docker-machine-nfs: remove `bottle :unneeded`
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/docker-machine-nfs.rb
+++ Formula/docker-machine-nfs.rb
@@ -5,8 +5,6 @@ class DockerMachineNfs < Formula
   sha256 "ecb8d637524eaeb1851a0e12da797d4ffdaec7007aa28a0692f551e9223a71b7"
   license "MIT"
 
-  bottle :unneeded
-
   def install
     bin.install "docker-machine-nfs.sh" => "docker-machine-nfs"
   end
