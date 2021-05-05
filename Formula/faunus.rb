989e1bf8919b09db1f5616bc2146ddd709ba9109
Mikael Lund <mikael.lund@teokem.lu.se>
2021-05-05 15:10:23 +0200

    Initial commit

---
+++ Formula/faunus.rb
@@ -0,0 +1,42 @@
+class Faunus < Formula
+  include Language::Python::Virtualenv
+
+  desc "A Framework for Metropolis Monte Carlo Simulation of Molecular Systems"
+  homepage "https://mlund.github.io/faunus"
+  url "https://github.com/mlund/faunus/archive/522213d.zip"
+  #url "https://github.com/mlund/faunus/archive/refs/tags/v2.4.2.tar.gz"
+  #sha256 "a751d59191ae3b82741a1a63dea70620b461d0ba6935f17e5babc3706ad9e746"
+  license "MIT"
+  version "2.5.0"
+
+  depends_on "cmake" => :build
+  depends_on "python@3.9"
+
+  resource "Jinja2" do
+    url "https://files.pythonhosted.org/packages/4f/e7/65300e6b32e69768ded990494809106f87da1d436418d5f1367ed3966fd7/Jinja2-2.11.3.tar.gz"
+    sha256 "a6d58433de0ae800347cab1fa3043cebbabe8baa9d29e668f1c768cb87a333c6"
+  end
+  resource "jsonschema" do
+    url "https://files.pythonhosted.org/packages/69/11/a69e2a3c01b324a77d3a7c0570faa372e8448b666300c4117a516f8b1212/jsonschema-3.2.0.tar.gz"
+    sha256 "c8a85b28d377cc7737e46e2d9f2b4f44ee3c0e1deac6bf46ddefc7187d30797a"
+  end
+  resource "ruamel.yaml" do
+    url "https://files.pythonhosted.org/packages/9a/ee/55cd64bbff971c181e2d9e1c13aba9a27fd4cd2bee545dbe90c44427c757/ruamel.yaml-0.15.100.tar.gz"
+    sha256 "8e42f3067a59e819935a2926e247170ed93c8f0b2ab64526f888e026854db2e4"
+  end
+
+  def install
+    venv_root = libexec/"venv"
+    xy = Language::Python.major_minor_version "python3"
+    ENV.prepend_create_path "PYTHONPATH", "#{venv_root}/lib/python#{xy}/site-packages"
+    venv = virtualenv_create(venv_root, "python3")
+    venv.pip_install resources
+    system "cmake", "-DENABLE_OPENMP=off", "-DENABLE_PYTHON=on", "-S", ".", *std_cmake_args, "-DPYTHON_EXECUTABLE=#{venv_root}/bin/python"
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/faunus", "test"
+    system "#{bin}/yason.py", "-h"
+  end
+end
