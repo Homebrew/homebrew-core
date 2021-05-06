79b952105c5b76fc3b91c3e881c55084a4ec69f9
Mikael Lund <mikael.lund@teokem.lu.se>
2021-05-06 19:26:26 +0200

    PR review changes

--- Formula/faunus.rb
+++ Formula/faunus.rb
@@ -7,7 +7,7 @@ class Faunus < Formula
   version "2.5.0"
   sha256 "d7b3d6bfddafa808fd21108c916583c850719d736a2f344aac3ab6088d2d72cc"
   license "MIT"
-  revision 1
+  revision 0
 
   depends_on "cmake" => :build
   depends_on "libtool" => :build
@@ -60,7 +60,7 @@ class Faunus < Formula
   end
 
   test do
-    system "#{bin}/faunus", "--version"
+    system "#{bin}/faunus", "test"
     system "#{bin}/yason.py", "-h"
   end
 end
