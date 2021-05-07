b118fe69f2733ffefe352799417ba1c177bfeec7
rui <rui@meetup.com>
2021-05-07 10:41:53 -0400

    remove patch

--- Formula/mydumper.rb
+++ Formula/mydumper.rb
@@ -22,15 +22,6 @@ class Mydumper < Formula
 
   uses_from_macos "zlib"
 
-  # This patch allows cmake to find .dylib shared libs in macOS. A bug report has
-  # been filed upstream here: https://bugs.launchpad.net/mydumper/+bug/1517966
-  # It also ignores .a libs because of an issue with glib's static libraries now
-  # being included by default in homebrew.
-  #
-  # Although we override the mysql library location this patch is still required
-  # because the setting of ${CMAKE_FIND_LIBRARY_SUFFIXES} affects other probes as well.
-  patch :p0, :DATA
-
   def install
     system "cmake", ".", *std_cmake_args,
            # Override location of mysql-client:
@@ -46,16 +37,3 @@ class Mydumper < Formula
     system bin/"mydumper", "--help"
   end
 end
-
-__END__
---- cmake/modules/FindMySQL.cmake	2015-09-16 16:11:34.000000000 -0400
-+++ cmake/modules/FindMySQL.cmake	2015-09-16 16:10:56.000000000 -0400
-@@ -84,7 +84,7 @@
- )
-
- set(TMP_MYSQL_LIBRARIES "")
--set(CMAKE_FIND_LIBRARY_SUFFIXES .so .a .lib .so.1)
-+set(CMAKE_FIND_LIBRARY_SUFFIXES .so .lib .dylib .so.1)
- foreach(MY_LIB ${MYSQL_ADD_LIBRARIES})
-     find_library("MYSQL_LIBRARIES_${MY_LIB}" NAMES ${MY_LIB}
-         HINTS
