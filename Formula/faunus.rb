8cdc978ec77555754d7cb7b56547048a7ba69cb8
Mikael Lund <mikael.lund@teokem.lu.se>
2021-05-08 07:24:28 +0200

    Revert to previous piping

--- Formula/faunus.rb
+++ Formula/faunus.rb
@@ -62,7 +62,7 @@ class Faunus < Formula
   end
 
   test do
-    pipe_output("#{bin}/faunus --quiet", "#{bin}/yason.py #{share}/faunus/examples/minimal.yml")
+    system "#{bin}/yason.py #{share}/faunus/examples/minimal.yml | #{bin}/faunus --quiet"
     system "#{bin}/faunus", "test"
   end
 end
