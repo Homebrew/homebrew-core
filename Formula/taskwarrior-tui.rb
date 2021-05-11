41f885da2d72d2ff21191e9c0b931cb994a6d3db
Dheepak Krishnamurthy <me@kdheepak.com>
2021-05-11 15:22:50 -0600

    Fix test

--- Formula/taskwarrior-tui.rb
+++ Formula/taskwarrior-tui.rb
@@ -40,6 +40,6 @@ class TaskwarriorTui < Formula
   test do
     assert_match version.to_s, shell_output("#{bin}/taskwarrior-tui --version")
     assert_match "The argument '--config <FILE>' requires a value but none was supplied",
-      shell_output("#{bin}/taskwarrior-tui --config 2>&1", 1)
+      shell_output("#{bin}/taskwarrior-tui --config 2>&1", 2)
   end
 end
