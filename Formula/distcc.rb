3c77347bb3e69b9beefdfad410fd02b30aa36e45
Rui Chen <rui@chenrui.dev>
2021-05-11 23:43:05 -0400

    distcc 3.4

--- Formula/distcc.rb
+++ Formula/distcc.rb
@@ -1,8 +1,8 @@
 class Distcc < Formula
   desc "Distributed compiler client and server"
   homepage "https://github.com/distcc/distcc/"
-  url "https://github.com/distcc/distcc/releases/download/v3.3.5/distcc-3.3.5.tar.gz"
-  sha256 "6a20c0896d546c507dbf608660d22f5da606509f52d2e97f6c47ca9b8c1cc405"
+  url "https://github.com/distcc/distcc/releases/download/v3.4/distcc-3.4.tar.gz"
+  sha256 "2b99edda9dad9dbf283933a02eace6de7423fe5650daa4a728c950e5cd37bd7d"
   license "GPL-2.0-or-later"
   head "https://github.com/distcc/distcc.git"
 
