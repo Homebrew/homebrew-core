73d511136d7a8894fb86db001fbb48c7e8228adc
PaperChalice <lgamma@163.com>
2021-05-12 17:10:04 +0800

    openmodelica: 1.17.0

--- Formula/openmodelica.rb
+++ Formula/openmodelica.rb
@@ -3,11 +3,15 @@ class Openmodelica < Formula
   homepage "https://openmodelica.org/"
   # GitHub's archives lack submodules, must pull:
   url "https://github.com/OpenModelica/OpenModelica.git",
-    tag:      "v1.16.5",
-    revision: "11fcab4f2d6895f2db073572b2bff1a43177313f"
+    tag:      "v1.17.0",
+    revision: "08fd3f9144235f209a4ed7602bfadb32b1823628"
   license "GPL-3.0-only"
-  revision 1
   head "https://github.com/OpenModelica/OpenModelica.git"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
 
   bottle do
     rebuild 1
@@ -19,7 +23,7 @@ class Openmodelica < Formula
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "cmake" => :build
-  depends_on "gcc@10" => :build # for gfortran
+  depends_on "gcc" => :build # for gfortran
   depends_on "gnu-sed" => :build
   depends_on "libtool" => :build
   depends_on "openjdk" => :build
@@ -46,6 +50,18 @@ class Openmodelica < Formula
   patch do
     url "https://github.com/OpenModelica/OpenModelica/commit/71aa2f871639041f3569fafe1b1cea25b84981ff.patch?full_index=1"
     sha256 "0f794a01481227b4c58a4c57c3f37035962de3955b9878f78207d0d3ebfbce09"
+  end
+
+  # attempt to fix MacOS builds
+  patch do
+    url "https://github.com/OpenModelica/OpenModelica/commit/65bb214bd59e6ab6610e0d390b252c24409a5abc.patch?full_index=1"
+    sha256 "cea5c51f88e1b55c73065f64cae8c85e3bf9450f2e6167f155eb07757e0cf4de"
+  end
+
+  # more fixes for MacOS
+  patch do
+    url "https://github.com/OpenModelica/OpenModelica/commit/ed8ef0a961b7cba917305e58e4a53d08126d58a0.patch?full_index=1"
+    sha256 "c4c66fc0a7320aeee26d1d94df2d973f78798d3cdc5a8e47a1bd5d1275c1dada"
   end
 
   def install
