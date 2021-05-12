a3a99943dd9e4961c552c4cbbc0640e71dca4b39
Michael Bianco <iloveitaly@gmail.com>
2021-05-12 14:35:24 -0600

    Start cod server in test

--- Formula/cod.rb
+++ Formula/cod.rb
@@ -13,6 +13,7 @@ class Cod < Formula
   end
 
   test do
+    assert_match "", shell_output("cod daemon")
     assert_match "", shell_output("cod remove cod")
     assert_match 'learned completions: "--help" "--help-long" and 10 more', shell_output("cod learn cod")
   end
