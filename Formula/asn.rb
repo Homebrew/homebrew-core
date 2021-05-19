989c8f83a850967d56b938c51201548f131c2d9a
Filippo Vitale <filippo.vitale@netscout.com>
2021-05-19 22:14:46 +0200

    asn: add test and rephrase description

--- Formula/asn.rb
+++ Formula/asn.rb
@@ -1,5 +1,5 @@
 class Asn < Formula
-  desc "ASN / IPv4 / IPv6 / Prefix / AS Path / Organization lookup and server tool"
+  desc "Organization lookup and server tool (ASN / IPv4 / IPv6 / Prefix / AS Path)"
   homepage "https://github.com/nitefood/asn"
   url "https://raw.githubusercontent.com/nitefood/asn/64e92533f57e9261d1d6588b4714a4183fc3c304/asn"
   version "0.71.6"
@@ -14,5 +14,9 @@ class Asn < Formula
 
   def install
     bin.install "asn"
+  end
+
+  test do
+    assert_match version.to_s, shell_output(%Q|sed -n 's/^ASN_VERSION="\\(.*\\)"$/\\1/p' #{bin}/asn|)
   end
 end
