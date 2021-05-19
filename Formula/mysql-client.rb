ebda9cbab6db111a691253fddd24e33531bd69de
Yasuo Honda <yasuo.honda@gmail.com>
2021-05-19 23:30:41 +0900

    Use system libevent zlib and zstd

--- Formula/mysql-client.rb
+++ Formula/mysql-client.rb
@@ -21,9 +21,12 @@ class MysqlClient < Formula
   depends_on "cmake" => :build
   # GCC is not supported either, so exclude for El Capitan.
   depends_on macos: :sierra if DevelopmentTools.clang_build_version < 900
+  depends_on "libevent"
   depends_on "openssl@1.1"
+  depends_on "zstd"
 
   uses_from_macos "libedit"
+  uses_from_macos "zlib"
 
   def install
     # -DINSTALL_* are relative to `CMAKE_INSTALL_PREFIX` (`prefix`)
@@ -39,6 +42,9 @@ class MysqlClient < Formula
       -DINSTALL_MYSQLSHAREDIR=share/mysql
       -DWITH_BOOST=boost
       -DWITH_EDITLINE=system
+      -DWITH_LIBEVENT=system
+      -DWITH_ZLIB=system
+      -DWITH_ZSTD=system
       -DWITH_SSL=yes
       -DWITH_UNIT_TESTS=OFF
       -DWITHOUT_SERVER=ON
