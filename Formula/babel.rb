26ccc6220e5dc1f908fd977201b3df484262c67e
rui <rui@chenrui.dev>
2021-05-18 20:22:43 -0400

    cli 7.14.3

--- Formula/babel.rb
+++ Formula/babel.rb
@@ -15,8 +15,8 @@ class Babel < Formula
   depends_on "node"
 
   resource "babel-cli" do
-    url "https://registry.npmjs.org/@babel/cli/-/cli-7.13.16.tgz"
-    sha256 "a48de92838d7c33e6b024ad313f0168ec62035be26f39ba4c42bd81108bda0f4"
+    url "https://registry.npmjs.org/@babel/cli/-/cli-7.14.3.tgz"
+    sha256 "a55ae9dd75e17388610061af9ac3deb2f3243b17d9cfa270231c696e02a4b843"
   end
 
   def install
