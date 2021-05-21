87e8d6a5c8a3e62e7f552680f4337f8215a6bbb4
Philipp Wollermann <philipp@philwo.de>
2021-05-21 18:42:34 +0200

    Update USE_BAZEL_VERSION to 4.1.0.
    This is the first version where Bazel supplies macOS arm64 binaries, so the test works on both platforms.

--- Formula/bazelisk.rb
+++ Formula/bazelisk.rb
@@ -39,7 +39,7 @@ class Bazelisk < Formula
     # This is an older than current version, so that we can test that bazelisk
     # will target an explicit version we specify. This version shouldn't need to
     # be bumped.
-    ENV["USE_BAZEL_VERSION"] = "0.28.0"
-    assert_match "Build label: 0.28.0", shell_output("#{bin}/bazelisk version")
+    ENV["USE_BAZEL_VERSION"] = "4.1.0"
+    assert_match "Build label: 4.1.0", shell_output("#{bin}/bazelisk version")
   end
 end
