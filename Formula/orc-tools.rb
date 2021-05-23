e37b8782ad0ce5333d5987ba7daa7dab705771bd
Rui Chen <rui@chenrui.dev>
2021-05-23 18:26:08 -0400

    orc-tools 1.6.8

--- Formula/orc-tools.rb
+++ Formula/orc-tools.rb
@@ -1,8 +1,8 @@
 class OrcTools < Formula
   desc "ORC java command-line tools and utilities"
   homepage "https://orc.apache.org/"
-  url "https://search.maven.org/remotecontent?filepath=org/apache/orc/orc-tools/1.6.7/orc-tools-1.6.7-uber.jar"
-  sha256 "ade496bf92a2a52f6008e18e1431d770801a93c74dfa0f863cb66b7e9f06b44e"
+  url "https://search.maven.org/remotecontent?filepath=org/apache/orc/orc-tools/1.6.8/orc-tools-1.6.8-uber.jar"
+  sha256 "f0ba77e063ac180302686e935cb2cc2cd74fb82595a7036600e9f7617cefe2b8"
   license "Apache-2.0"
 
   livecheck do
