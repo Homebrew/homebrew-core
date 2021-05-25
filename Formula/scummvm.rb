8ab50fda64b1868aaeefc23c03b94526c086c177
Michael Cho <cho-m@tuta.io>
2021-05-24 17:31:57 -0700

    scummvm: revision bump

--- Formula/scummvm.rb
+++ Formula/scummvm.rb
@@ -4,6 +4,7 @@ class Scummvm < Formula
   url "https://downloads.scummvm.org/frs/scummvm/2.2.0/scummvm-2.2.0.tar.xz"
   sha256 "1469657e593bd8acbcfac0b839b086f640ebf120633e93f116cab652b5b27387"
   license "GPL-2.0-or-later"
+  revision 1
   head "https://github.com/scummvm/scummvm.git"
 
   livecheck do
