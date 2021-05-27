1feac85bcbc145b9315c20ee2a3f6065f8a66653
Nikolai Shelekhov <nickshv13@gmail.com>
2021-05-27 16:42:50 +0800

    Update stox.rb

--- Formula/stox.rb
+++ Formula/stox.rb
@@ -4,16 +4,18 @@ class Stox < Formula
   url "https://github.com/xbladesub/stox/archive/1.0.1.tar.gz"
   sha256 "f037220a0b810775dad6e4df77f335aa9e96ed10b94784ae8d2821a69c628e3b"
   license "MIT"
-  
+
   depends_on xcode: "12.4"
-  
+
   def install
-    system "swift", "build", "--disable-sandbox", "-c", "release", "-Xswiftc",
-                    "-cross-module-optimization", "--enable-test-discovery"
-    bin.install ".build/release/stox"
+    system "swift", "build", "--disable-sandbox", "-c", "release", "-Xswiftc", \
+      "-cross-module-optimization", "--enable-test-discovery"
+    mv ".build/release/stox", "stox"
+    bin.install "stox"
   end
-  
+
   test do
-    system bin/"stox", "all"
+      output = shell_output("#{bin}/stox del")
+      assert_match("\nDelete all lists? (y / n): ", output)
   end
 end
