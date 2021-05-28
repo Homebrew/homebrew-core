092dba87971114687a314b0d2f81a4a1dfb97e22
McDull-GitHub <30206217+McDull-GitHub@users.noreply.github.com>
2021-05-28 13:18:21 +0800

    brook 20210601

--- Formula/brook.rb
+++ Formula/brook.rb
@@ -1,8 +1,8 @@
 class Brook < Formula
   desc "Cross-platform strong encryption and not detectable proxy. Zero-Configuration"
   homepage "https://txthinking.github.io/brook/"
-  url "https://github.com/txthinking/brook/archive/refs/tags/v20210401.tar.gz"
-  sha256 "6229b2f0b53d94acb873e246d10f2a4662af2a031a03e7fb5c3befffcd998731"
+  url "https://github.com/txthinking/brook/archive/refs/tags/v20210601.tar.gz"
+  sha256 "3f52f902ea88fb6e871961056c47333c0a72e59804981ca96aa5684708426ea4"
   license "GPL-3.0-only"
 
   bottle do
