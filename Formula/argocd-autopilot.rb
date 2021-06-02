01569c35683b56995fa3d56e28e44c094606162a
Stéphane Este-Gracias <sestegra@gmail.com>
2021-06-02 16:25:14 +0200

    Upgrade to v0.1.10
    Signed-off-by: Stéphane Este-Gracias <sestegra@gmail.com>

--- Formula/argocd-autopilot.rb
+++ Formula/argocd-autopilot.rb
@@ -2,14 +2,13 @@ class ArgocdAutopilot < Formula
   desc "Opinionated way of installing Argo CD and managing GitOps repositories"
   homepage "https://argoproj.io"
   url "https://github.com/argoproj-labs/argocd-autopilot.git",
-      tag:      "v0.1.9",
-      revision: "fee1ff03dabbcc0d8b72b8d618ded836caaedb32"
+      tag:      "v0.1.10",
+      revision: "646b3faa598281d0404c4b85443361597173c6c7"
   license "Apache-2.0"
 
   depends_on "go" => :build
 
   def install
-    inreplace "hack/build.sh", "-extldflags\ \'-static\'", ""
     system "make", "cli-package"
     bin.install "dist/argocd-autopilot"
   end
