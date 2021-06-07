f0ea8651eb2228f0cdb897af97a69ede50dec0ce
Max Isom <codetheweb@users.noreply.github.com>
2021-06-06 19:22:42 -0500

    Fix license
    Co-authored-by: Nanda H Krishna <me@nandahkrishna.com>

--- Formula/libmobi.rb
+++ Formula/libmobi.rb
@@ -3,7 +3,7 @@ class Libmobi < Formula
   homepage "https://github.com/bfabiszewski/libmobi/"
   url "https://github.com/bfabiszewski/libmobi/releases/download/v0.6/libmobi-0.6.tar.gz"
   sha256 "c35bd44279575bf8b102d23eba482805bfc1e5a49df8414d851507e8ea811c5d"
-  license "LGPL-3.0-only"
+  license "LGPL-3.0-or-later"
 
   def install
     system "./configure", *std_configure_args, "--disable-silent-rules"
