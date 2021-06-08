139c22bcaf9909d1b9d71cbbbf8d601a02c30f93
David Sánchez <david.sanchez.lt@gmail.com>
2021-06-08 23:57:08 +0100

    Switch dependency order
    As indicated by output of brew audit --strict

--- Formula/shellcheck.rb
+++ Formula/shellcheck.rb
@@ -14,8 +14,8 @@ class Shellcheck < Formula
 
   depends_on "cabal-install" => :build
   depends_on "ghc" => :build
-  depends_on "pandoc" => :build
   depends_on "llvm" => :build if Hardware::CPU.arm?
+  depends_on "pandoc" => :build
 
   def install
     system "cabal", "v2-update"
