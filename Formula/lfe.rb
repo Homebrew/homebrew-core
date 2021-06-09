b6adde597c09944c8ad2f29de713859aaf53e4da
Pratik Karki <pratik@prertik.com>
2021-06-09 22:33:57 +0545

    lfe: remove emacs dependency from lfe
    Emacs isn't a dependency for LFE and having Emacs as
    a dependency causes installation issues when emacs-plus
    is present.
    
    Signed-off-by: Pratik Karki <pratik@prertik.com>

--- Formula/lfe.rb
+++ Formula/lfe.rb
@@ -13,18 +13,15 @@ class Lfe < Formula
     sha256 cellar: :any_skip_relocation, mojave:        "16d8e8a9f9ab091b1184dac366bb9d83cfccfdb728f79c035362b7b86940a7af"
   end
 
-  depends_on "emacs" if MacOS.version >= :catalina
   depends_on "erlang"
 
   def install
     system "make"
     system "make", "MANINSTDIR=#{man}", "install-man"
-    system "make", "emacs"
     libexec.install "bin", "ebin"
     bin.install_symlink (libexec/"bin").children
     doc.install Dir["doc/*.txt"]
     pkgshare.install "dev", "examples", "test"
-    elisp.install Dir["emacs/*.elc"]
   end
 
   test do
