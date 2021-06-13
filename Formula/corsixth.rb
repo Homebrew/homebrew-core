619182e7bfeafd1b14c36eda0aeabe12e05a43f1
Rui Chen <rui@chenrui.dev>
2021-06-12 22:56:18 -0400

    corsixth: use WITH_LUAROCKS and simplify build
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/corsixth.rb
+++ Formula/corsixth.rb
@@ -15,61 +15,33 @@ class Corsixth < Formula
 
   depends_on "cmake" => :build
   depends_on "luarocks" => :build
-  depends_on xcode: :build
   depends_on "ffmpeg"
   depends_on "freetype"
   depends_on "lua"
   depends_on "sdl2"
   depends_on "sdl2_mixer"
 
-  resource "lpeg" do
-    url "http://www.inf.puc-rio.br/~roberto/lpeg/lpeg-1.0.2.tar.gz"
-    mirror "https://sources.voidlinux.org/lua-lpeg-1.0.2/lpeg-1.0.2.tar.gz"
-    sha256 "48d66576051b6c78388faad09b70493093264588fcd0f258ddaab1cdd4a15ffe"
-  end
-
-  resource "luafilesystem" do
-    url "https://github.com/keplerproject/luafilesystem/archive/v1_7_0_2.tar.gz"
-    sha256 "23b4883aeb4fb90b2d0f338659f33a631f9df7a7e67c54115775a77d4ac3cc59"
-  end
-
   def install
-    # Make sure I point to the right version!
     lua = Formula["lua"]
 
-    ENV["TARGET_BUILD_DIR"] = "."
-    ENV["FULL_PRODUCT_NAME"] = "CorsixTH.app"
+    args = std_cmake_args + %W[
+      -DLUA_INCLUDE_DIR=#{lua.opt_include}/lua
+      -DLUA_LIBRARY=#{lua.opt_lib}/liblua.dylib
+      -DCORSIX_TH_DATADIR=#{prefix}/CorsixTH.app/Contents/Resources/
+      -DWITH_LUAROCKS=ON
+    ]
+    system "cmake", ".", *args
 
-    luapath = libexec/"vendor"
-    ENV["LUA_PATH"] = "#{luapath}/share/lua/#{lua.version.major_minor}/?.lua"
-    ENV["LUA_CPATH"] = "#{luapath}/lib/lua/#{lua.version.major_minor}/?.so"
-
-    resources.each do |r|
-      r.stage do
-        system "luarocks", "build", r.name, "--tree=#{luapath}"
-      end
-    end
-
-    system "cmake", ".", "-DLUA_INCLUDE_DIR=#{lua.opt_include}/lua",
-                         "-DLUA_LIBRARY=#{lua.opt_lib}/liblua.dylib",
-                         "-DLUA_PROGRAM_PATH=#{lua.opt_bin}/lua",
-                         "-DCORSIX_TH_DATADIR=#{prefix}/CorsixTH.app/Contents/Resources/",
-                         *std_cmake_args
     system "make"
-    cp_r %w[CorsixTH/CorsixTH.lua CorsixTH/Lua CorsixTH/Levels CorsixTH/Campaigns CorsixTH/Graphics CorsixTH/Bitmap],
-         "CorsixTH/CorsixTH.app/Contents/Resources/"
-    prefix.install "CorsixTH/CorsixTH.app"
-
-    env = { LUA_PATH: ENV["LUA_PATH"], LUA_CPATH: ENV["LUA_CPATH"] }
-    (bin/"CorsixTH").write_env_script(prefix/"CorsixTH.app/Contents/MacOS/CorsixTH", env)
+    system "make", "install"
+    bin.write_exec_script prefix/"CorsixTH.app/Contents/MacOS/CorsixTH"
   end
 
   test do
-    # Make sure I point to the right version!
     lua = Formula["lua"]
 
     app = prefix/"CorsixTH.app/Contents/MacOS/CorsixTH"
     assert_includes MachO::Tools.dylibs(app),
-                    "#{lua.opt_lib}/liblua.dylib"
+                    "#{lua.opt_lib}/liblua.#{lua.version.major_minor}.dylib"
   end
 end
