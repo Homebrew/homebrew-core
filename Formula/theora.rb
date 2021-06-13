900fc89896eef7bc1b55708881864ee00807e1e1
James Hilliard <james.hilliard1@gmail.com>
2021-06-13 06:39:10 -0600

    theora: fix download
    We need to use curl with support for TLSv1.3 or newer to download
    from https://downloads.xiph.org, use our built in curl instead of
    the system curl as it is newer and supports TLSv1.3.

--- Formula/theora.rb
+++ Formula/theora.rb
@@ -1,8 +1,16 @@
 class Theora < Formula
   desc "Open video compression format"
   homepage "https://www.theora.org/"
-  url "https://downloads.xiph.org/releases/theora/libtheora-1.1.1.tar.bz2"
-  sha256 "b6ae1ee2fa3d42ac489287d3ec34c5885730b1296f0801ae577a35193d3affbc"
+  license "BSD-3-Clause"
+
+  stable do
+    url "https://downloads.xiph.org/releases/theora/libtheora-1.1.1.tar.bz2"
+    sha256 "b6ae1ee2fa3d42ac489287d3ec34c5885730b1296f0801ae577a35193d3affbc"
+
+    depends_on "curl" => :build
+
+    ENV["HOMEBREW_CURL"] = "#{Formula["curl"].opt_bin}/curl"
+  end
 
   livecheck do
     url "https://www.theora.org/downloads/"
