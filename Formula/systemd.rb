7818431e48cf189cd9a4cc95cb4459dc84a05b19
rui <rui@chenrui.dev>
2021-06-13 02:03:52 -0400

    systemd: depends_on rsync

--- Formula/systemd.rb
+++ Formula/systemd.rb
@@ -18,6 +18,7 @@ class Systemd < Formula
   depends_on "meson" => :build
   depends_on "ninja" => :build
   depends_on "pkg-config" => :build
+  depends_on "rsync" => :build
   depends_on "expat"
   depends_on "libcap"
   depends_on :linux
