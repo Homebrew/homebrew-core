bee21f431dc2a36573d940a0de36d1bc386f6493
Kostas Chatzikokolakis <kostas@chatzi.org>
2021-06-15 02:43:39 -0700

    Update Formula/lci.rb
    Co-authored-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>

--- Formula/lci.rb
+++ Formula/lci.rb
@@ -22,7 +22,7 @@ class Lci < Formula
 
   def install
     mkdir "build" do
-      system "cmake", "..", "-DCMAKE_BUILD_TYPE=Release", "-DCMAKE_INSTALL_PREFIX=#{prefix}"
+      system "cmake", "..", *std_cmake_args
       system "make", "install"
     end
   end
