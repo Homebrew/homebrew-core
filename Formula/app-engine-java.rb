5b82e6151d310f7ff06d4dfb2666c25aa22751b8
Mike McQuaid <mike@mikemcquaid.com>
2021-06-16 13:22:29 +0100

    app-engine-java: remove bottle :unneeded.
    https://github.com/Homebrew/homebrew-core/issues/75943

--- Formula/app-engine-java.rb
+++ Formula/app-engine-java.rb
@@ -4,8 +4,6 @@ class AppEngineJava < Formula
   url "https://storage.googleapis.com/appengine-sdks/featured/appengine-java-sdk-1.9.83.zip"
   sha256 "1d585a36303c14f4fa44790bba97d5d8b75a889ad48ffce8187333488511e43e"
 
-  bottle :unneeded
-
   # https://cloud.google.com/appengine/docs/standard/java/sdk-gcloud-migration
   deprecate! date: "2019-07-30", because: :deprecated_upstream
 
