72455117c17830ee8c405ab27f7aa6ffcc240cac
Mike McQuaid <mike@mikemcquaid.com>
2021-06-16 13:24:16 +0100

    scala@2.11: remove bottle :unneeded.
    https://github.com/Homebrew/homebrew-core/issues/75943

--- Formula/scala@2.11.rb
+++ Formula/scala@2.11.rb
@@ -6,7 +6,6 @@ class ScalaAT211 < Formula
   mirror "https://www.scala-lang.org/files/archive/scala-2.11.12.tgz"
   sha256 "b11d7d33699ca4f60bc3b2b6858fd953e3de2b8522c943f4cda4b674316196a8"
   revision 1
-  bottle :unneeded
 
   keg_only :versioned_formula
 
