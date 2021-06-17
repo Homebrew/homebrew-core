661f214a3021d8b2326db4004ad6278726d3acfe
大师兄 <616782041@qq.com>
2021-06-17 14:37:29 +0800

    Update fvm.rb
    version 1.1.5

--- Formula/fvm.rb
+++ Formula/fvm.rb
@@ -1,9 +1,9 @@
 class Fvm < Formula
   desc "Flutter SDK versions Manager"
-  homepage "https://github.com/xinfeng-tech/fvm"
-  url "https://github.com/xinfeng-tech/fvm/archive/v1.1.2.tar.gz"
-  sha256 "83e4fe0836dcf521f69c1c8eb5e7c380fcfc89c8786a5ce9830a21e4a34f43cc"
-  head "https://github.com/xinfeng-tech/fvm.git"
+  homepage "https://github.com/dashixiong91/fvm"
+  url "https://github.com/dashixiong91/fvm/archive/v1.1.5.tar.gz"
+  sha256 "3ad05b34dcba7370753f60e5323ec80f3a3108d26498ded5be9fcd7cb15b5015"
+  head "https://github.com/dashixiong91/fvm.git"
 
   bottle :unneeded
 
@@ -17,17 +17,12 @@ class Fvm < Formula
 
   def caveats; <<~EOS
     You should create FVM's working directory if it doesn't exist:
-
       mkdir ~/.fvm
-
     Add the following to #{shell_profile} or your desired shell
     configuration file:
-
       export FVM_DIR="$HOME/.fvm"
       source "#{opt_prefix}/init.sh"  # This loads fvm
-
     You can set $FVM_DIR to any location.
-
     Type `fvm help` for further information.
   EOS
   end
