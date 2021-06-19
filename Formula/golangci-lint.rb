a03b9c085a5bf8220501519c53f9c92bcf895db6
Rui Chen <rui@chenrui.dev>
2021-06-19 15:10:40 -0400

    golangci-lint 1.41.1

--- Formula/golangci-lint.rb
+++ Formula/golangci-lint.rb
@@ -2,8 +2,8 @@ class GolangciLint < Formula
   desc "Fast linters runner for Go"
   homepage "https://golangci-lint.run/"
   url "https://github.com/golangci/golangci-lint.git",
-      tag:      "v1.41.0",
-      revision: "eed344f619dea041f87bcbaf200cdba1af2c859e"
+      tag:      "v1.41.1",
+      revision: "a20748098fb3cb4b69f6b6ebb7809e7741122ef8"
   license "GPL-3.0-only"
   head "https://github.com/golangci/golangci-lint.git"
 
