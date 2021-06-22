b88517b4910a0404a879677c1fbd9ef8de6d767b
rui <rui@chenrui.dev>
2021-06-22 11:13:28 -0400

    v2ray: update resources

--- Formula/v2ray.rb
+++ Formula/v2ray.rb
@@ -21,13 +21,13 @@ class V2ray < Formula
   depends_on "go" => :build
 
   resource "geoip" do
-    url "https://github.com/v2fly/geoip/releases/download/202105270041/geoip.dat"
-    sha256 "a5f1cec9c252197a07a4b7b7e89da08dec65692715beb5ecad1106c2dea3c73c"
+    url "https://github.com/v2fly/geoip/releases/download/202106170022/geoip.dat"
+    sha256 "af7b0e6b92fa7b1d55b2b9d8155e9a2372b6b0be0e1b9e09353d9859680f6bb1"
   end
 
   resource "geosite" do
-    url "https://github.com/v2fly/domain-list-community/releases/download/20210526032424/dlc.dat"
-    sha256 "fd83fe6cd88aaf2391e506fc6aba2d75067df729555341c747c00290a25d323d"
+    url "https://github.com/v2fly/domain-list-community/releases/download/20210621183458/dlc.dat"
+    sha256 "b78075874cb24c82fd2096e521452ff673fadb3d4d8048da73681f9b4722d0e2"
   end
 
   def install
