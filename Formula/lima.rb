2a0ca268a1358dc46fa0d4cb4384251352ba02b8
rui <rui@chenrui.dev>
2021-06-22 10:01:52 -0400

    lima 0.4.0

--- Formula/lima.rb
+++ Formula/lima.rb
@@ -1,8 +1,8 @@
 class Lima < Formula
   desc "Linux virtual machines"
   homepage "https://github.com/AkihiroSuda/lima"
-  url "https://github.com/AkihiroSuda/lima/archive/v0.3.0.tar.gz"
-  sha256 "0debed5cd2fcb34ce90114c4f07a3610a3b2d4aaf4592608135e9053cb5ce213"
+  url "https://github.com/AkihiroSuda/lima/archive/v0.4.0.tar.gz" 
+  sha256 "482e4cff30103aac5bf93feb76721f1ada26a7618e503365040c4adae2a28043"
   license "Apache-2.0"
 
   depends_on "go" => :build
