5927845bbd82b8a93a18ae6f7203eb0af5fed9ff
Charles Karney <charles.karney@sri.com>
2021-06-23 00:10:55 -0400

    geographiclib 1.52: restore bottle stuff

--- Formula/geographiclib.rb
+++ Formula/geographiclib.rb
@@ -5,6 +5,14 @@ class Geographiclib < Formula
   sha256 "5d4145cd16ebf51a2ff97c9244330a340787d131165cfd150e4b2840c0e8ac2b"
   license "MIT"
 
+  bottle do
+    rebuild 1
+    sha256 cellar: :any, arm64_big_sur: "9c9fcfba39ac076e5681018ffd222742f6d813e156f44389bbffae7f54a9c602"
+    sha256 cellar: :any, big_sur:       "330b858cbb533864caa4324b2385399e49807ce93671be290ec6b16c09c874d6"
+    sha256 cellar: :any, catalina:      "a8d601014c3a569b8b0ebb48e0f92055c57c2f915354a1e4030a7cda4a1afcfe"
+    sha256 cellar: :any, mojave:        "145e83f5076511c2d86ec4d8ff4de7536f909411d70b8ba81fa976f7c756d11d"
+  end
+
   depends_on "cmake" => :build
 
   def install
