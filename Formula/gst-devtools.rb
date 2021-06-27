21d8ca7a86e91aecd13c6e982f20f47ae970c6ea
Rui Chen <rui@chenrui.dev>
2021-06-27 03:44:19 -0400

    gst-devtools 1.19.1

--- Formula/gst-devtools.rb
+++ Formula/gst-devtools.rb
@@ -3,8 +3,8 @@ class GstDevtools < Formula
 
   desc "GStreamer development and validation tools"
   homepage "https://gstreamer.freedesktop.org/modules/gstreamer.html"
-  url "https://gstreamer.freedesktop.org/src/gst-devtools/gst-devtools-1.18.4.tar.xz"
-  sha256 "ffbd194c40912cb5e7fca2863648bf9dd8257b7af97d3a60c4fcd4efd8526ccf"
+  url "https://gstreamer.freedesktop.org/src/gst-devtools/gst-devtools-1.19.1.tar.xz"
+  sha256 "81f31d87f5f883a93d6b642073ed1d7ab2aef3f913f2476605a28063190d42fc"
   license "LGPL-2.1-or-later"
   head "https://gitlab.freedesktop.org/gstreamer/gst-devtools.git"
 
