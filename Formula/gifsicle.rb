4ae455eb0cc0f4faa93f62ed1c71b8aebb5b2434
Thierry Moisan <thierry.moisan@gmail.com>
2021-06-30 11:53:28 -0400

    gifsicle 1.93

--- Formula/gifsicle.rb
+++ Formula/gifsicle.rb
@@ -1,8 +1,8 @@
 class Gifsicle < Formula
   desc "GIF image/animation creator/editor"
   homepage "https://www.lcdf.org/gifsicle/"
-  url "https://www.lcdf.org/gifsicle/gifsicle-1.92.tar.gz"
-  sha256 "5ab556c01d65fddf980749e3ccf50b7fd40de738b6df679999294cc5fabfce65"
+  url "https://www.lcdf.org/gifsicle/gifsicle-1.93.tar.gz"
+  sha256 "92f67079732bf4c1da087e6ae0905205846e5ac777ba5caa66d12a73aa943447"
   license "GPL-2.0"
 
   bottle do
