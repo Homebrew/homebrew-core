0569a9b54d9a429e42c5f2c071c9ba3df4eb7e3d
Owen Voke <development@voke.dev>
2021-07-02 13:42:37 +0100

    scorecard (apply review changes)

--- Formula/scorecard.rb
+++ Formula/scorecard.rb
@@ -4,12 +4,11 @@ class Scorecard < Formula
   url "https://github.com/ossf/scorecard/archive/refs/tags/v2.0.0.tar.gz"
   sha256 "9d3f0dd0a82cdc4cb3cd69149ad0967291b1bafb751bb07bc9c92db00af613a9"
   license "Apache-2.0"
-  head "https://github.com/ossf/scorecard.git"
+  head "https://github.com/ossf/scorecard.git", branch: "main"
 
   depends_on "go" => :build
 
   def install
-    ENV["GOPATH"] = buildpath
     ENV["GO111MODULE"] = "on"
 
     system "make", "install"
@@ -19,5 +18,11 @@ class Scorecard < Formula
 
   test do
     system "#{bin}/scorecard", "help"
+
+    output = shell_output("GITHUB_AUTH_TOKEN=ABCDEFG123 #{bin}/scorecard --repo=github.com/kubernetes/kubernetes --checks=Active 2>&1", 1)
+
+    assert_match /^Starting \[Active\]/, output
+    assert_match /repo cannot be accessed/, output
+    assert_match /401 Bad credentials/, output
   end
 end
