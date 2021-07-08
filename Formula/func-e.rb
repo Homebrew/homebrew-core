c641c73bdaa65bf081d7ebbfcfbed5b51b7f038e
Crypt Keeper <64215+codefromthecrypt@users.noreply.github.com>
2021-07-08 17:32:21 +0800

    Update Formula/func-e.rb
    Co-authored-by: Sean Molenaar <SMillerDev@users.noreply.github.com>

--- Formula/func-e.rb
+++ Formula/func-e.rb
@@ -1,5 +1,5 @@
 class FuncE < Formula
-  desc "Easily run Envoy®"
+  desc "Easily run Envoy"
   homepage "https://func-e.io"
   url "https://github.com/tetratelabs/func-e/archive/v0.5.0.tar.gz"
   sha256 "17fc6c5c0f7bea8ce59e0bfb315198cc9f9ecfda98d51909149d42ffd125d72f"
