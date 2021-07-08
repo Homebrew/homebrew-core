4977ba85fc4a54125449f00b72162d14650cfc00
Rui Chen <rui@chenrui.dev>
2021-07-08 09:27:50 -0400

    tfsec 0.44.0

--- Formula/tfsec.rb
+++ Formula/tfsec.rb
@@ -1,8 +1,8 @@
 class Tfsec < Formula
   desc "Static analysis security scanner for your terraform code"
   homepage "https://tfsec.dev/"
-  url "https://github.com/tfsec/tfsec/archive/v0.42.3.tar.gz"
-  sha256 "84a85225d29c36e3b6942577d81136fd4fed28bc60d36ef3de121ca54042ca14"
+  url "https://github.com/tfsec/tfsec/archive/v0.44.0.tar.gz"
+  sha256 "6c4cb4c28bf86acaf5ff3ff000bce0b02cc514d88904ded87886defe89b4e8f4"
   license "MIT"
   head "https://github.com/tfsec/tfsec.git"
 
