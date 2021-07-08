23f1bdd141acd1480c8f6793022790dd13d93b40
Rui Chen <rui@chenrui.dev>
2021-07-08 00:47:09 -0400

    autodiff 0.6.0

--- Formula/autodiff.rb
+++ Formula/autodiff.rb
@@ -1,8 +1,8 @@
 class Autodiff < Formula
   desc "Automatic differentiation made easier for C++"
   homepage "https://autodiff.github.io"
-  url "https://github.com/autodiff/autodiff/archive/v0.5.13.tar.gz"
-  sha256 "a73dc571bcaad6b44f74865fed51af375f5a877db44321b5568d94a4358b77a1"
+  url "https://github.com/autodiff/autodiff/archive/v0.6.0.tar.gz"
+  sha256 "b76e6a96e539f173a2a24eefa6f4e7cff54b1144cc51c51eba44ac3779a14013"
   license "MIT"
   head "https://github.com/autodiff/autodiff.git"
 
