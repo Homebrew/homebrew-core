bf7324852c6697bd714c7d07a698918ae2828236
rui <rui@chenrui.dev>
2021-07-10 14:08:28 -0400

    depends on ninja for build

--- Formula/vala-language-server.rb
+++ Formula/vala-language-server.rb
@@ -7,6 +7,7 @@ class ValaLanguageServer < Formula
   license "LGPL-2.1-only"
 
   depends_on "meson" => :build
+  depends_on "ninja" => :build
   depends_on "pkg-config" => :build
   depends_on "vala" => :build
   depends_on "glib"
