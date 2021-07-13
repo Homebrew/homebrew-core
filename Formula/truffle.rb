56abc42ca1d6dc8dfe2a155323b154cc939805c6
Rui Chen <rui@chenrui.dev>
2021-07-13 00:33:43 -0400

    truffle: fix linux build
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/truffle.rb
+++ Formula/truffle.rb
@@ -16,6 +16,10 @@ class Truffle < Formula
 
   depends_on "node"
 
+  on_linux do
+    depends_on "gcc"
+  end
+
   def install
     system "npm", "install", *Language::Node.std_npm_install_args(libexec)
     bin.install_symlink Dir["#{libexec}/bin/*"]
