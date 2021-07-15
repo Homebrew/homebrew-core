68386050588902a30c4a9ec084e13b24ce59fda1
Alexei Ledenev <alexei.led@gmail.com>
2021-07-15 13:34:05 +0300

    remove comments

--- Formula/kube-no-trouble.rb
+++ Formula/kube-no-trouble.rb
@@ -1,6 +1,3 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class KubeNoTrouble < Formula
   desc "Easily check your cluster for use of deprecated APIs"
   homepage "https://www.doit-intl.com"
