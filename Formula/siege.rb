b84ff8973b444b165224cd1c26e1ec03f58ba60e
Thierry Moisan <thierry.moisan@gmail.com>
2021-07-21 12:59:22 -0400

    Update siege.rb

--- Formula/siege.rb
+++ Formula/siege.rb
@@ -3,7 +3,7 @@ class Siege < Formula
   homepage "https://www.joedog.org/siege-home/"
   url "http://download.joedog.org/siege/siege-4.1.1.tar.gz"
   sha256 "143c25727b9bb2c439486c35121602f4d963385305aab4ed1ff9851581bfe4a8"
-  license "GPL-3.0"
+  license "GPL-3.0-or-later"
 
   livecheck do
     url "http://download.joedog.org/siege/?C=M&O=D"
