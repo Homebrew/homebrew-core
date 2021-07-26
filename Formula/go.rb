32f5605e8af48c268d23d8a0937eb1de2ca29d66
andig <cpuidle@gmx.de>
2021-07-26 12:26:41 +0200

    go 1.17rc1

--- Formula/go.rb
+++ Formula/go.rb
@@ -1,9 +1,9 @@
 class Go < Formula
   desc "Open source programming language to build simple/reliable/efficient software"
   homepage "https://golang.org"
-  url "https://golang.org/dl/go1.16.6.src.tar.gz"
-  mirror "https://fossies.org/linux/misc/go1.16.6.src.tar.gz"
-  sha256 "a3a5d4bc401b51db065e4f93b523347a4d343ae0c0b08a65c3423b05a138037d"
+  url "https://golang.org/dl/go1.17rc1.src.tar.gz"
+  mirror "https://fossies.org/linux/misc/go1.17rc1.src.tar.gz"
+  sha256 "0d63be0f3abc79d35efcb60dfce4445e64bb1ea194edcfd9783a76316b7e85e2"
   license "BSD-3-Clause"
   head "https://go.googlesource.com/go.git"
 
