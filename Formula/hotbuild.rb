64bf7e83d080190b88db4dea10937ed9f98b0d4d
wander <wander@ffactory.org>
2021-07-28 17:42:32 +0800

    Revert "hotbuild 1.0.7"
    This reverts commit 788604b23204ae5b3fda161f2790ed278da937f4.

--- Formula/hotbuild.rb
+++ Formula/hotbuild.rb
@@ -1,18 +1,34 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Hotbuild < Formula
   desc "a cross platform hot compilation tool for golang"
   homepage "https://hotbuild.ffactory.org"
-  url "https://github.com/wandercn/hotbuild/archive/refs/tags/v1.0.7.tar.gz"
-  sha256 "22d5feb6771ca1ba668b5371e0dccf06c4b201a41ac42a0651403e2798d68386"
+  url "https://github.com/wandercn/hotbuild/releases/download/v1.0.7/hotbuild_v1.0.7_darwin_amd64.zip"
+  sha256 "341aaf26292bc7576815875172148cba8513ba278ae3ac43d2a84531e5a18380"
   license "GPL-3.0"
 
-  depends_on "go" => :build
+  # depends_on "cmake" => :build
 
   def install
-      system "go build"
-      bin.install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    # system "./configure", *std_configure_args, "--disable-silent-rules"
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    bin.install "hotbuild"
   end
 
   test do
-    system "go test"
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test hotbuild`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
   end
 end
