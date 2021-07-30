880a4a1f2df9b7c3d434ff6b56cc173340ff7299
wander <wander@ffactory.org>
2021-07-30 11:50:20 +0800

    hotbuild 1.0.7

--- Formula/hotbuild.rb
+++ Formula/hotbuild.rb
@@ -1,7 +1,7 @@
 class Hotbuild < Formula
   desc "Cross platfrom hot compilation tool for go"
   homepage "https://hotbuild.ffactory.org"
-  url "https://github.com/wandercn/hotbuild/archive/refs/tags/v1.0.7.tar.gz"
+  url "https://hotbuild.ffactory.org/v1.0.7.tar.gz"
   sha256 "22d5feb6771ca1ba668b5371e0dccf06c4b201a41ac42a0651403e2798d68386"
   license "GPL-3.0-only"
   depends_on "go" => :build
