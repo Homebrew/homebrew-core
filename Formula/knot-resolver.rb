2350e13e3a74a82d43ea86bc2654004acb55e18a
Alexander Bayandin <a.bayandin@gmail.com>
2021-08-04 11:38:29 +0100

    knot-resolver: revision bump for knot

--- Formula/knot-resolver.rb
+++ Formula/knot-resolver.rb
@@ -4,6 +4,7 @@ class KnotResolver < Formula
   url "https://secure.nic.cz/files/knot-resolver/knot-resolver-5.3.2.tar.xz"
   sha256 "8b6f447d5fe93422d4c129a2d4004a977369c3aa6e55258ead1cbd488bc01436"
   license all_of: ["CC0-1.0", "GPL-3.0-or-later", "LGPL-2.1-or-later", "MIT"]
+  revision 1
   head "https://gitlab.labs.nic.cz/knot/knot-resolver.git"
 
   livecheck do
