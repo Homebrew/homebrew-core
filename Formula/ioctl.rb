382706e72021f9768c43562b0531ea3404ad56bd
Rui Chen <rui@chenrui.dev>
2021-08-06 00:23:40 -0400

    ioctl 1.3.2

--- Formula/ioctl.rb
+++ Formula/ioctl.rb
@@ -1,8 +1,8 @@
 class Ioctl < Formula
   desc "Command-line interface for interacting with the IoTeX blockchain"
   homepage "https://docs.iotex.io/"
-  url "https://github.com/iotexproject/iotex-core/archive/v1.3.0.tar.gz"
-  sha256 "2b96cab6a639408906db5e5ccac6291918a8914f3c2de1c47f32bb70f5f08022"
+  url "https://github.com/iotexproject/iotex-core/archive/v1.3.2.tar.gz"
+  sha256 "557222fcf4af396119289db006882ec4be50ae1e060745806d3597b829f78a62"
   license "Apache-2.0"
 
   bottle do
