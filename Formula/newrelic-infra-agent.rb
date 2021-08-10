e649f262c542ab9565a5546943e7c24560c1eb41
Grigorii Merkushev <brushknight@gmail.com>
2021-08-10 10:08:11 +0200

    fix: split macos and linux, bind to x86

--- Formula/newrelic-infra-agent.rb
+++ Formula/newrelic-infra-agent.rb
@@ -7,26 +7,35 @@ class NewrelicInfraAgent < Formula
   head "https://github.com/newrelic/infrastructure-agent.git"
 
   depends_on "go" => :build
+  depends_on arch: :x86_64
 
   def install
     goarch = Hardware::CPU.arm? ? "arm64" : "amd64"
-    cgo = Hardware::CPU.arm? ? "0" : "1"
     ENV["VERSION"] = "1.20.3"
-    ENV["GOOS"] = "darwin"
     ENV["GOARCH"] = goarch
-    ENV["CGO_ENABLED"] = cgo
-    system "make", "dist-for-os"
     on_macos do
+      ENV["GOOS"] = "darwin"
+      ENV["CGO_ENABLED"] = "1"
+      system "make", "dist-for-os"
       bin.install "dist/darwin-newrelic-infra_darwin_#{goarch}/newrelic-infra"
       bin.install "dist/darwin-newrelic-infra-ctl_darwin_#{goarch}/newrelic-infra-ctl"
       bin.install "dist/darwin-newrelic-infra-service_darwin_#{goarch}/newrelic-infra-service"
     end
+    on_linux do
+      ENV["GOOS"] = "linux"
+      ENV["CGO_ENABLED"] = "0"
+      system "make", "dist-for-os"
+      bin.install "dist/linux-newrelic-infra_linux_#{goarch}/newrelic-infra"
+      bin.install "dist/linux-newrelic-infra-ctl_linux_#{goarch}/newrelic-infra-ctl"
+      bin.install "dist/linux-newrelic-infra-service_linux_#{goarch}/newrelic-infra-service"
+    end
+  end
 
-    (etc/"newrelic-infra").mkpath
-    (var/"log/newrelic-infra").mkpath
-    (var/"db/newrelic-infra").mkpath
-
-    (var/"db/newrelic-infra").install "assets/licence/LICENSE.macos.txt"
+  def post_install
+      (etc/"newrelic-infra").mkpath
+      (var/"log/newrelic-infra").mkpath
+      (var/"db/newrelic-infra").mkpath
+      (var/"db/newrelic-infra").install "assets/licence/LICENSE.macos.txt"
   end
 
   service do
