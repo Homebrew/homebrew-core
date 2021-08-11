b7a413efcff183adfa6b141bb67c99d3f7dc2a90
Sean Molenaar <smillerdev@me.com>
2021-08-11 09:57:25 +0200

    teleport 7.0.0

--- Formula/teleport.rb
+++ Formula/teleport.rb
@@ -1,8 +1,8 @@
 class Teleport < Formula
   desc "Modern SSH server for teams managing distributed infrastructure"
   homepage "https://gravitational.com/teleport"
-  url "https://github.com/gravitational/teleport/archive/v6.2.8.tar.gz"
-  sha256 "bbc128d0778d253ec6e8af44286df750caec65d8c8e2616c85cf2656c267460f"
+  url "https://github.com/gravitational/teleport/archive/v7.0.0.tar.gz"
+  sha256 "cee16006019273c3826d4bd9ca425f50a18135846a8dd61af5b46207ae5b7afe"
   license "Apache-2.0"
   head "https://github.com/gravitational/teleport.git"
 
