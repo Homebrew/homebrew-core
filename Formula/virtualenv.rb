d4a16969c60bd8268a5cf1adf401bf1b6ce3b244
Dustin Rodrigues <dust.rod@gmail.com>
2021-08-10 23:18:10 -0400

    virtualenv: update default head branch

--- Formula/virtualenv.rb
+++ Formula/virtualenv.rb
@@ -6,7 +6,7 @@ class Virtualenv < Formula
   url "https://files.pythonhosted.org/packages/6d/89/9691b0d0521c069bd238585ccb7f4035b8393d843a50491270e7dae0209e/virtualenv-20.7.2.tar.gz"
   sha256 "9ef4e8ee4710826e98ff3075c9a4739e2cb1040de6a2a8d35db0055840dc96a0"
   license "MIT"
-  head "https://github.com/pypa/virtualenv.git"
+  head "https://github.com/pypa/virtualenv.git", branch: "main"
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_big_sur: "4835c6d1130017193331275e8df237dcf89f0d6a885b62e22392376d2fe8dd76"
