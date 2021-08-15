ae4f5774cce0e3611413381fe38c9c88ad9d0c36
مهدي شينون (Mehdi Chinoune) <79349457+MehdiChinoune@users.noreply.github.com>
2021-08-15 20:06:36 +0100

    Build qwt against qt6

--- Formula/qwt.rb
+++ Formula/qwt.rb
@@ -10,14 +10,7 @@ class Qwt < Formula
     regex(%r{url=.*?/qwt[._-]v?(\d+(?:\.\d+)+)\.t}i)
   end
 
-  bottle do
-    sha256 cellar: :any, arm64_big_sur: "0538bfe404c21c264efe57fbc36d3cff81c39b86679d67c48501166597ab8cad"
-    sha256 cellar: :any, big_sur:       "14a5fd16a5abcf3a04b3f6d097649fbc1dd51e9fbb50d05f885757a9d9f3d9f9"
-    sha256 cellar: :any, catalina:      "c3a727be657b20efdd6a8ddec980bd28f5367ae41a0a7abefb74af86c1f24e83"
-    sha256 cellar: :any, mojave:        "5bb62a4122ade6485247357b22f0619ff35f518d9ea3f454f05c7d5c4b60985e"
-  end
-
-  depends_on "qt@5"
+  depends_on "qt"
 
   # Update designer plugin linking back to qwt framework/lib after install
   # See: https://sourceforge.net/p/qwt/patches/45/
@@ -44,8 +37,8 @@ class Qwt < Formula
     spec << "-arm64" if Hardware::CPU.arm?
     args << spec
 
-    qt5 = Formula["qt@5"].opt_prefix
-    system "#{qt5}/bin/qmake", *args
+    qt = Formula["qt"].opt_prefix
+    system "#{qt}/bin/qmake", *args
     system "make"
     system "make", "install"
   end
@@ -62,21 +55,21 @@ class Qwt < Formula
       system ENV.cxx, "test.cpp", "-o", "out",
         "-std=c++11",
         "-framework", "qwt", "-framework", "QtCore",
-        "-F#{lib}", "-F#{Formula["qt@5"].opt_lib}",
+        "-F#{lib}", "-F#{Formula["qt"].opt_lib}",
         "-I#{lib}/qwt.framework/Headers",
-        "-I#{Formula["qt@5"].opt_lib}/QtCore.framework/Versions/5/Headers",
-        "-I#{Formula["qt@5"].opt_lib}/QtGui.framework/Versions/5/Headers"
+        "-I#{Formula["qt"].opt_lib}/QtCore.framework/Versions/#{Formula["qt"].version.major}/Headers",
+        "-I#{Formula["qt"].opt_lib}/QtGui.framework/Versions/#{Formula["qt"].version.major}/Headers"
     end
     on_linux do
       system ENV.cxx,
-        "-I#{Formula["qt@5"].opt_include}",
-        "-I#{Formula["qt@5"].opt_include}/QtCore",
-        "-I#{Formula["qt@5"].opt_include}/QtGui",
+        "-I#{Formula["qt"].opt_include}",
+        "-I#{Formula["qt"].opt_include}/QtCore",
+        "-I#{Formula["qt"].opt_include}/QtGui",
         "test.cpp",
-        "-lqwt", "-lQt5Core", "-lQt5Gui",
-        "-L#{Formula["qt@5"].opt_lib}",
+        "-lqwt", "-lQt#{Formula["qt"].version.major}Core", "-lQt#{Formula["qt"].version.major}Gui",
+        "-L#{Formula["qt"].opt_lib}",
         "-L#{Formula["qwt"].opt_lib}",
-        "-Wl,-rpath=#{Formula["qt@5"].opt_lib}",
+        "-Wl,-rpath=#{Formula["qt"].opt_lib}",
         "-Wl,-rpath=#{Formula["qwt"].opt_lib}",
         "-o", "out", "-std=c++11", "-fPIC"
     end
