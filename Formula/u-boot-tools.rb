8ab6a43f824645da74fca20e92ced6bb2de105ff
k-ronny <78017977+k-ronny@users.noreply.github.com>
2021-08-15 19:56:07 +0200

    Update u-boot-tools.rb
    enable build on macOS 11.5.2-arm64

--- Formula/u-boot-tools.rb
+++ Formula/u-boot-tools.rb
@@ -1,8 +1,8 @@
 class UBootTools < Formula
   desc "Universal boot loader"
   homepage "https://www.denx.de/wiki/U-Boot/"
-  url "https://ftp.denx.de/pub/u-boot/u-boot-2021.01.tar.bz2"
-  sha256 "b407e1510a74e863b8b5cb42a24625344f0e0c2fc7582d8c866bd899367d0454"
+  url "https://ftp.denx.de/pub/u-boot/u-boot-2021.07.tar.bz2"
+  sha256 "312b7eeae44581d1362c3a3f02c28d806647756c82ba8c72241c7cdbe68ba77e"
   license all_of: ["GPL-2.0-only", "GPL-2.0-or-later", "BSD-3-Clause"]
 
   livecheck do
@@ -26,8 +26,9 @@ class UBootTools < Formula
     # Replace keyword not present in make 3.81
     inreplace "Makefile", "undefine MK_ARCH", "unexport MK_ARCH"
 
-    system "make", "sandbox_defconfig"
-    system "make", "tools", "NO_SDL=1"
+    ENV["SOURCE_DATE_EPOCH"] = ""
+    system "make", "tools-only_defconfig"
+    system "make", "tools-only", "NO_SDL=1"
     bin.install "tools/mkimage"
     bin.install "tools/dumpimage"
     man1.install "doc/mkimage.1"
