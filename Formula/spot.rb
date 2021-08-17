2623ff53a68421f1962c5a703bdf63c581adc0ef
Leon Bohn <leon.bohn@rwth-aachen.de>
2021-08-17 22:50:45 +0200

    Fix description, add make step and add livecheck

--- Formula/spot.rb
+++ Formula/spot.rb
@@ -1,5 +1,5 @@
 class Spot < Formula
-  desc ": a platform for LTL and ω-automata manipulation"
+  desc "A platform for LTL and ω-automata manipulation"
   homepage "https://spot.lrde.epita.fr/"
   url "https://www.lrde.epita.fr/dload/spot/spot-2.9.8.tar.gz"
   sha256 "b7f404bb90a335a5914384ecc3fc3a2021ff22c57ee97a40c07bb2ab40e20cf9"
@@ -7,9 +7,15 @@ class Spot < Formula
 
   def install
     system "./configure", *std_configure_args, "--disable-silent-rules"
+    system "make", "install"
   end
 
   test do
     system bin/"randltl -n20 a b c d | ltlcross 'ltl2tgba -H -D %f >%O' 'ltl2tgba -s %f >%O' 'ltl2tgba -DP %f >%O'"
+  end
+
+  livecheck do
+    url "https://spot.lrde.epita.fr/install.html"
+    regex(/href=.*?spot[._-]v?(\d+(?:\.\d+)+)\.t/i)
   end
 end
