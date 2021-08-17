a20aaa65d52d59bad0d3ca9a174bf0e7ee60d4ca
Leon Bohn <leon.bohn@rwth-aachen.de>
2021-08-17 22:54:58 +0200

    Moved livecheck block up and fixed description.

--- Formula/spot.rb
+++ Formula/spot.rb
@@ -1,9 +1,14 @@
 class Spot < Formula
-  desc "A platform for LTL and ω-automata manipulation"
+  desc "Platform for LTL and ω-automata manipulation"
   homepage "https://spot.lrde.epita.fr/"
   url "https://www.lrde.epita.fr/dload/spot/spot-2.9.8.tar.gz"
   sha256 "b7f404bb90a335a5914384ecc3fc3a2021ff22c57ee97a40c07bb2ab40e20cf9"
   license "GPL-3.0-or-later"
+
+  livecheck do
+    url "https://spot.lrde.epita.fr/install.html"
+    regex(/href=.*?spot[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
 
   def install
     system "./configure", *std_configure_args, "--disable-silent-rules"
@@ -12,10 +17,5 @@ class Spot < Formula
 
   test do
     system bin/"randltl -n20 a b c d | ltlcross 'ltl2tgba -H -D %f >%O' 'ltl2tgba -s %f >%O' 'ltl2tgba -DP %f >%O'"
-  end
-
-  livecheck do
-    url "https://spot.lrde.epita.fr/install.html"
-    regex(/href=.*?spot[._-]v?(\d+(?:\.\d+)+)\.t/i)
   end
 end
