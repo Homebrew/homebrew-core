dc17843dad0f3f2c60a14fa8aab7f8f871a48140
Nanda H Krishna <me@nandahkrishna.com>
2021-08-18 21:07:16 +0530

    austin: add `head` branch

--- Formula/austin.rb
+++ Formula/austin.rb
@@ -4,7 +4,7 @@ class Austin < Formula
   url "https://github.com/P403n1x87/austin/archive/v3.0.0.tar.gz"
   sha256 "df57cd33e74fe9b7b0c87ea82827b0d7863cc5761bd3e75b3367d7d3384c3836"
   license "GPL-3.0-or-later"
-  head "https://github.com/P403n1x87/austin.git"
+  head "https://github.com/P403n1x87/austin.git", branch: "master"
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_big_sur: "7f3464bfa7fd1f576832f25a56e19f551bf5c6778d77a158ca1b25b48f7c671d"
