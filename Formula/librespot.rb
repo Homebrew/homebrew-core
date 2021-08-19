03505ab82388d58461758088d26cb199231c4e1b
Carlos Precioso <511681+cprecioso@users.noreply.github.com>
2021-08-19 12:29:08 +0200

    use tarball

--- Formula/librespot.rb
+++ Formula/librespot.rb
@@ -1,9 +1,8 @@
 class Librespot < Formula
   desc "Open Source Spotify client library"
   homepage "https://github.com/librespot-org/librespot"
-  url "https://github.com/librespot-org/librespot.git",
-      tag:      "v0.2.0",
-      revision: "59683d7965480e63c581dd03082ded6a080a1cd3"
+  url "https://github.com/librespot-org/librespot/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "cd6e7ae63583584598dead53b5e8e4077ab37df6e3b59c2a7416459b9a649fc3"
   license "MIT"
   head "https://github.com/librespot-org/librespot.git", branch: "dev"
 
