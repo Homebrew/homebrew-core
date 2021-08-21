3aa97c98573562ac3de70636c05009c43e69ed29
rui <rui@chenrui.dev>
2021-08-21 15:25:34 -0400

    sound-touch: update license

--- Formula/sound-touch.rb
+++ Formula/sound-touch.rb
@@ -3,7 +3,7 @@ class SoundTouch < Formula
   homepage "https://www.surina.net/soundtouch/"
   url "https://gitlab.com/soundtouch/soundtouch/-/archive/2.3.0/soundtouch-2.3.0.tar.gz"
   sha256 "b0f38497c72bb545d5d5c505243c5daa3598567641d25728d015b27678a68ad8"
-  license "LGPL-2.1"
+  license "LGPL-2.1-or-later"
 
   bottle do
     sha256 cellar: :any,                 arm64_big_sur: "3f1a856a43235f03b83369a1edf8e29cedc4457129c74147433deb48ad102fbf"
