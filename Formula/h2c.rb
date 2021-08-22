0c2fc7e41857316f1aa689d2330c4679ff68966a
tscp <45612704+tddschn@users.noreply.github.com>
2021-08-22 16:43:06 +0800

    h2c fee2a036 (new formula)

---
+++ Formula/h2c.rb
@@ -0,0 +1,15 @@
+class H2c < Formula
+  desc "Headers 2 curl"
+  homepage "https://curl.se/h2c/"
+  url "https://github.com/curl/h2c/archive/fee2a0361b4124dbd5565cee2a90ffb0c80ab7b0.tar.gz"
+  sha256 "8eba84aef62506dfff4f0d975a062fa5f347c3298784c8f4f778a0244fec4df0"
+  license "MIT"
+
+  def install
+    bin.install "h2c"
+  end
+
+  test do
+    assert_match "h2c.pl [options] < file", shell_output("#{bin}/gh", --help)
+  end
+end
