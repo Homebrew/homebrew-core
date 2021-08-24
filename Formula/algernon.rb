0483faa710677c77c26dc022a0c693fb6c6c9a65
Alexander F. Rødseth <52813+xyproto@users.noreply.github.com>
2021-08-24 15:05:35 +0200

    Update Formula/algernon.rb
    Co-authored-by: Branch Vincent <branchevincent@gmail.com>

--- Formula/algernon.rb
+++ Formula/algernon.rb
@@ -1,6 +1,6 @@
 class Algernon < Formula
   desc "Pure Go web server with Lua, Markdown, HTTP/2 and template support"
-  homepage "https://algernon.roboticoverlords.org/"
+  homepage "https://github.com/xyproto/algernon"
   url "https://github.com/xyproto/algernon/archive/1.12.14.tar.gz"
   sha256 "cab5b01923142e0326ea2a01797814bb2e8ea9f7c6c41a3ea0ae7df3b667e86e"
   license "MIT"
