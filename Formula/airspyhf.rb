7b8c611935b542d5b40b473df3ef88a37ef13b5b
Dmitry Obedkov <dr.fruct@gmail.com>
2021-08-24 09:24:08 +0300

    airspyhf 1.6.8

---
+++ Formula/airspyhf.rb
@@ -0,0 +1,28 @@
+class Airspyhf < Formula
+  desc "Driver and tools for a software-defined radio"
+  homepage "https://airspy.com/"
+  url "https://github.com/airspy/airspyhf/archive/1.6.8.tar.gz"
+  sha256 "cd1e5ae89e09b813b096ae4a328e352c9432a582e03fd7da86760ba60efa77ab"
+  head "https://github.com/airspy/airspyhf.git", branch: "master"
+
+  depends_on "cmake" => :build
+  depends_on "pkg-config" => :build
+  depends_on "libusb"
+
+  def install
+    args = std_cmake_args
+
+    libusb = Formula["libusb"]
+    args << "-DLIBUSB_INCLUDE_DIR=#{libusb.opt_include}/libusb-1.0"
+    args << "-DLIBUSB_LIBRARIES=#{libusb.opt_lib}/#{shared_library("libusb-1.0")}"
+
+    mkdir "build" do
+      system "cmake", "..", *args
+      system "make", "install"
+    end
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/airspyhf_lib_version").chomp
+  end
+end
