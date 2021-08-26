71caadef0b948e1e63c8f438148e6ea91978a7c4
Rui Chen <rui@chenrui.dev>
2021-08-26 17:19:53 -0400

    vue-cli 4.5.13 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/vue-cli.rb
@@ -0,0 +1,28 @@
+require "language/node"
+
+class VueCli < Formula
+  desc "Standard Tooling for Vue.js Development"
+  homepage "https://cli.vuejs.org/"
+  url "https://registry.npmjs.org/@vue/cli/-/cli-4.5.13.tgz"
+  sha256 "540e6931c55f4b73487a5b714fb57a2e697c40aa6796aac7e85a8ec98cdbdd53"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    (testpath/".vuerc").write <<~EOS
+      {
+        "useTaobaoRegistry": false,
+        "packageManager": "yarn"
+      }
+    EOS
+
+    assert_match "yarn", shell_output(bin/"vue config")
+    assert_match "npm", shell_output(bin/"vue info")
+  end
+end
