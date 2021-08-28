e307d49582d96f2fb158285904a2edcb6fff26a4
Sebastian Riehm <herrriehm@users.noreply.github.com>
2021-08-28 07:41:13 +0200

    Update Formula/yt-dlp.rb
    Co-authored-by: codetrotter <36199671+ctsrc@users.noreply.github.com>

--- Formula/yt-dlp.rb
+++ Formula/yt-dlp.rb
@@ -1,22 +1,34 @@
 class YtDlp < Formula
-  include Language::Python::Virtualenv
-
   desc "Fork of youtube-dl with additional features and fixes"
-  homepage "https://discord.gg/H5MNcFW63r"
-  url "https://github.com/yt-dlp/yt-dlp/releases/download/2021.08.10/yt-dlp.tar.gz"
-  sha256 "15bd1028840e9230f7b89ee928914b119858b9011c7083c6cb0c5a60d525f1e6"
+  homepage "https://github.com/yt-dlp/yt-dlp"
+  url "https://github.com/yt-dlp/yt-dlp/archive/2021.08.10.tar.gz"
+  sha256 "d847e32bc18767be2f27183f5da2ba05d0142e194e95ac6b3e69494353ee366b"
   license "Unlicense"
+  head "https://github.com/yt-dlp/yt-dlp.git", branch: "master"
 
+  livecheck do
+    url :stable
+    strategy :github_latest
+    regex(%r{href=.*?/tag/v?(\d+(?:[.-]\d+)+)["' >]}i)
+  end
+
+  depends_on "pandoc" => :build
   depends_on "python@3.9"
+  uses_from_macos "zip" => :build
 
   def install
-    virtualenv_install_with_resources
+    system "make </dev/null"
+    bin.install "yt-dlp"
+    bash_completion.install "completions/bash/yt-dlp"
+    zsh_completion.install "completions/zsh/_yt-dlp"
+    fish_completion.install "completions/fish/yt-dlp.fish"
+    man1.install "yt-dlp.1"
   end
 
   test do
-    # commit history of homebrew-core repo
-    system "#{bin}/youtube-dl", "--simulate", "https://www.youtube.com/watch?v=pOtd1cbOP7k"
-    # homebrew playlist
-    system "#{bin}/youtube-dl", "--simulate", "--yes-playlist", "https://www.youtube.com/watch?v=pOtd1cbOP7k&list=PLMsZ739TZDoLj9u_nob8jBKSC-mZb0Nhj"
+    # "History of homebrew-core", uploaded 3 Feb 2020
+    system "#{bin}/yt-dlp", "--simulate", "https://www.youtube.com/watch?v=pOtd1cbOP7k"
+    # "homebrew", playlist last updated 3 Mar 2020
+    system "#{bin}/yt-dlp", "--simulate", "--yes-playlist", "https://www.youtube.com/watch?v=pOtd1cbOP7k&list=PLMsZ739TZDoLj9u_nob8jBKSC-mZb0Nhj"
   end
 end
