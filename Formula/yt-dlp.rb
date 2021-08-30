92abae500c8dc0f92225b5ae74763480b8b60db8
Kid <44045911+kidonng@users.noreply.github.com>
2021-08-30 19:54:57 +0800

    Fix revision position

--- Formula/yt-dlp.rb
+++ Formula/yt-dlp.rb
@@ -6,8 +6,8 @@ class YtDlp < Formula
   url "https://files.pythonhosted.org/packages/ee/59/d763a51fa975639946c327ffbf85e109dbaa87f8aa5cae54a4ee5d09593c/yt-dlp-2021.8.10.tar.gz"
   sha256 "8da1bf4dc4641d37d137443c4783109ee8393caad5e0d270d9d1d534e8f25240"
   license "Unlicense"
-  head "https://github.com/yt-dlp/yt-dlp.git", branch: "master"
   revision 1
+  head "https://github.com/yt-dlp/yt-dlp.git", branch: "master"
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_big_sur: "96d6684e34d20fa121b4bad4c0706f024bb1a406ece55db3a188e0e4bd8f08c5"
