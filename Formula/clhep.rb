6194d69f9fddae1f2d21f658a0ea5076a95fcd4f
Rui Chen <rui@chenrui.dev>
2021-09-02 13:37:24 -0400

    clhep 2.4.5.1

--- Formula/clhep.rb
+++ Formula/clhep.rb
@@ -1,8 +1,8 @@
 class Clhep < Formula
   desc "Class Library for High Energy Physics"
   homepage "https://proj-clhep.web.cern.ch/proj-clhep/"
-  url "https://proj-clhep.web.cern.ch/proj-clhep/dist1/clhep-2.4.4.2.tgz"
-  sha256 "0782964fc19f06b55998a091d5a0b70e1adb682f6c062bcb5a25467a2c060e8d"
+  url "https://proj-clhep.web.cern.ch/proj-clhep/dist1/clhep-2.4.5.1.tgz"
+  sha256 "2517c9b344ad9f55974786ae6e7a0ef8b22f4abcbf506df91194ea2299ce3813"
   license "GPL-3.0"
 
   livecheck do
