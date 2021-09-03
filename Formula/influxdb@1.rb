385076b7408e0bcbecb82d7f16a8eb36c7aee271
rui <rui@chenrui.dev>
2021-09-03 07:33:58 -0400

    depends_on rust for build

--- Formula/influxdb@1.rb
+++ Formula/influxdb@1.rb
@@ -21,6 +21,7 @@ class InfluxdbAT1 < Formula
 
   depends_on "go" => :build
   depends_on "pkg-config" => :build
+  depends_on "rust" => :build
 
   # NOTE: The version here is specified in the go.mod of influxdb.
   # If you're upgrading to a newer influxdb version, check to see if this needs upgraded too.
