474abdf322f1e967a240c3ab9c6604dfc5d8ea00
Rui Chen <rui@chenrui.dev>
2021-09-07 01:09:21 -0400

    fn 0.6.9

--- Formula/fn.rb
+++ Formula/fn.rb
@@ -1,8 +1,8 @@
 class Fn < Formula
   desc "Command-line tool for the fn project"
   homepage "https://fnproject.io"
-  url "https://github.com/fnproject/cli/archive/0.6.8.tar.gz"
-  sha256 "eebfc7bea0da0f56cbe392c0dc62b35804f6531ba9dd6b49ddf4875f32505fae"
+  url "https://github.com/fnproject/cli/archive/0.6.9.tar.gz"
+  sha256 "77c7f52b595e53740ecc7f82e19ed0e66ea20267dd8957854d04605ad9540e3a"
   license "Apache-2.0"
 
   bottle do
