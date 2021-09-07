38375187e0b4d4884eb1880f59b5bf684e6733b5
Chris Long <chris@chrislong.dev>
2021-09-07 03:28:40 -0400

    murex 2.2.1200 (new formula)

---
+++ Formula/murex.rb
@@ -0,0 +1,24 @@
+class Murex < Formula
+  desc "Bash-like shell designed for greater command-line productivity and safer scripts"
+  homepage "https://murex.rocks"
+  url "https://github.com/lmorg/murex/archive/refs/tags/v2.2.1200.tar.gz"
+  sha256 "0f0ab62c5bc85eaf440b4a68135253c7dbc1ef264bc32cead4bc0c1fc0b8d0a2"
+  license "GPL-2.0-only"
+
+  depends_on "go" => :build
+
+  def install
+    ENV["GOPATH"] = buildpath
+    ENV["GO111MODULE"] = "auto"
+    dir = buildpath/"src/github.com/lmorg/murex"
+    dir.install buildpath.children
+    cd dir do
+      system "go", "build", *std_go_args
+    end
+  end
+
+  test do
+    system "#{bin}/murex", "--run-tests"
+    assert_equal "homebrew", shell_output("#{bin}/murex -c 'echo homebrew'").chomp
+  end
+end
