526a1a173d4cb8a5c6b3ba298e4b2210d6ae337d
rui <rui@chenrui.dev>
2021-09-07 21:36:25 -0400

    botocore 2.0.0dev144

--- Formula/awscli.rb
+++ Formula/awscli.rb
@@ -13,9 +13,9 @@ class Awscli < Formula
     # 1. Get `botocore` version at https://github.com/aws/aws-cli/blob/#{version}/setup.py
     # 2. Get commit matching version at https://github.com/boto/botocore/commits/v2
     resource "botocore" do
-      url "https://github.com/boto/botocore/archive/71e230d3fd182da7a59acb9eb0279ff2374aa701.tar.gz"
-      sha256 "934851b11d6c5c448b04ffe9586b6ac17aac3fbe2d63c43c17b92d22e13e4e51"
-      version "2.0.0dev143"
+      url "https://github.com/boto/botocore/archive/1bdeebfa056550de3000b6623af7117fce4177f1.tar.gz"
+      sha256 "f43d627bcd52820ae9ad212922507918298913709e7b02fc76018bb8939bc4a8"
+      version "2.0.0dev144"
     end
   end
 
