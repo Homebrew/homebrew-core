f483b5fe4b6ad2aa17f7cf410e0d906d5c6737f7
Florian Weber <florian.weber@ueberbit.de>
2021-09-09 12:16:24 +0200

    drush-launcher 0.9.1 (new formula)

---
+++ Formula/drush-launcher.rb
@@ -0,0 +1,18 @@
+class DrushLauncher < Formula
+  desc "Small wrapper around Drush for your global $PATH"
+  homepage "https://github.com/drush-ops/drush-launcher"
+  url "https://github.com/drush-ops/drush-launcher/releases/download/0.9.1/drush.phar"
+  sha256 "52b5e22031789e087e4f79ac49c74d94c0379ad6859f3ec14671ce1647a3b7a6"
+  license "GPL-2.0-or-later"
+
+  uses_from_macos "php"
+  depends_on "php" => :test
+
+  def install
+    bin.install "drush.phar" => "drush"
+  end
+
+  test do
+    system "#{bin}/drush", "--drush-launcher-version"
+  end
+end
