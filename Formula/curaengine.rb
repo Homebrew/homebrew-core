382916b7268daf0f15ce3964a390f989d6171880
rui <rui@chenrui.dev>
2021-09-10 10:33:56 -0400

    fix linux build

--- Formula/curaengine.rb
+++ Formula/curaengine.rb
@@ -27,6 +27,12 @@ class Curaengine < Formula
 
   depends_on "cmake" => :build
 
+  on_linux do
+    depends_on "gcc"
+  end
+
+  fails_with gcc: "5"
+
   # The version tag in these resources (e.g., `/1.2.3/`) should be changed as
   # part of updating this formula to a new version.
   resource "fdmextruder_defaults" do
