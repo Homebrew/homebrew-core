08aa5fb4eb30e913d9f1ad8c5d15e40bcfa11a86
rui <rui@chenrui.dev>
2021-09-11 14:46:03 -0400

    depends_on gcc for c++17

--- Formula/mdds.rb
+++ Formula/mdds.rb
@@ -23,6 +23,12 @@ class Mdds < Formula
   depends_on "autoconf" => :build
   depends_on "boost"
 
+  on_linux do
+    depends_on "gcc" # for C++17
+  end
+
+  fails_with gcc: "5"
+
   def install
     args = %W[
       --prefix=#{prefix}
