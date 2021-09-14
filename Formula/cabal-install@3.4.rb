8d18262e45daa88b10f0a0b7dc8dd2b6a33183af
FTS <fishtreesugar@users.noreply.github.com>
2021-09-14 22:01:31 +0800

    Update Formula/cabal-install@3.4.rb
    Co-authored-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>

--- Formula/cabal-install@3.4.rb
+++ Formula/cabal-install@3.4.rb
@@ -11,23 +11,6 @@ class CabalInstallAT34 < Formula
   depends_on "ghc"
   uses_from_macos "zlib"
 
-  resource "bootstrap" do
-    on_macos do
-      if Hardware::CPU.intel?
-        url "https://downloads.haskell.org/~cabal/cabal-install-3.2.0.0/cabal-install-3.2.0.0-x86_64-apple-darwin17.7.0.tar.xz"
-        sha256 "9197c17d2ece0f934f5b33e323cfcaf486e4681952687bc3d249488ce3cbe0e9"
-      else
-        # https://github.com/haskell/cabal/issues/7433#issuecomment-858590474
-        url "https://downloads.haskell.org/~ghcup/unofficial-bindists/cabal/3.6.0.0/cabal-install-3.6.0.0-aarch64-darwin-big-sur.tar.xz"
-        sha256 "7acf740946d996ede835edf68887e6b2f1e16d1b95e94054d266463f38d136d9"
-      end
-    end
-    on_linux do
-      url "https://downloads.haskell.org/~cabal/cabal-install-3.2.0.0/cabal-install-3.2.0.0-x86_64-unknown-linux.tar.xz"
-      sha256 "32d1f7cf1065c37cb0ef99a66adb405f409b9763f14c0926f5424ae408c738ac"
-    end
-  end
-
   def install
     resource("bootstrap").stage buildpath
     cabal = buildpath/"cabal"
