63353f60ef52ad396615cfa14da6a844ffd9c160
Rvn0xsy <rvn0xsy@gmail.com>
2021-09-14 15:59:26 +0800

    add red-tldr

---
+++ Formula/red-tldr.rb
@@ -0,0 +1,17 @@
+class RedTldr < Formula
+  desc "Used to help red team staff quickly find the commands and key points"
+  homepage "https://payloads.online/red-tldr/"
+  url "https://github.com/Rvn0xsy/red-tldr/archive/refs/tags/v0.4.tar.gz"
+  sha256 "47894a194da21e676901985909ba60c616f8b832df7e74834ae3f2da44b64b02"
+  license "MIT"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args
+  end
+
+  test do
+    system "red-tldr"
+  end
+end
