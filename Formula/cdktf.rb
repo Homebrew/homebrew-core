70d7dacc7d485f032bac3c60a96c11bacf4bedae
Dustin Rodrigues <dust.rod@gmail.com>
2021-09-16 08:39:32 -0400

    cdktf 0.6.0

--- Formula/cdktf.rb
+++ Formula/cdktf.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Cdktf < Formula
   desc "Cloud Development Kit for Terraform"
   homepage "https://github.com/hashicorp/terraform-cdk"
-  url "https://registry.npmjs.org/cdktf-cli/-/cdktf-cli-0.5.0.tgz"
-  sha256 "245768d2698bfe2bf8f38128b4d5f3fd5fe8988d95aaaebe71237143379f1d99"
+  url "https://registry.npmjs.org/cdktf-cli/-/cdktf-cli-0.6.0.tgz"
+  sha256 "629717452b1733b83168c0822294fd4b9a6e9bc3cd361a3cb9bda265a7f06238"
   license "MPL-2.0"
 
   bottle do
