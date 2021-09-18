454e7e3d78e69380b42a95b28341ed3f38b17cb5
Denis Glazachev <d.glazachev@gmail.com>
2021-09-18 16:53:32 +0400

    Switch from git url to tarball

--- Formula/clickhouse-cpp.rb
+++ Formula/clickhouse-cpp.rb
@@ -1,9 +1,8 @@
 class ClickhouseCpp < Formula
   desc "C++ client library for ClickHouse"
   homepage "https://github.com/ClickHouse/clickhouse-cpp#readme"
-  url "https://github.com/ClickHouse/clickhouse-cpp.git",
-      tag:      "1.5.0",
-      revision: "1415b5936a2ac2f084850b09057e05fb5798b2f1"
+  url "https://github.com/ClickHouse/clickhouse-cpp/archive/refs/tags/1.5.0.tar.gz"
+  sha256 "bb6f268f9c788deb9beccb0b05c2caccf77b141afa408343e09993f12bff55a9"
   license "Apache-2.0"
   head "https://github.com/ClickHouse/clickhouse-cpp.git", branch: "master"
 
