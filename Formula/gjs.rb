693ff82c1d74ec2d4ea8ff8af69a5fbd00f19a2d
Nanda H Krishna <me@nandahkrishna.com>
2021-09-18 23:05:52 -0400

    gjs: depend on `six`

--- Formula/gjs.rb
+++ Formula/gjs.rb
@@ -22,11 +22,7 @@ class Gjs < Formula
   depends_on "llvm"
   depends_on "nspr"
   depends_on "readline"
-
-  resource "six" do
-    url "https://files.pythonhosted.org/packages/6b/34/415834bfdafca3c5f451532e8a8d9ba89a21c9743a0c59fbd0205c7f9426/six-1.15.0.tar.gz"
-    sha256 "30639c035cdb23534cd4aa2dd52c3bf48f06e5f4a941509c8bafd8ce11080259"
-  end
+  depends_on "six"
 
   resource "mozjs78" do
     url "https://archive.mozilla.org/pub/firefox/releases/78.10.1esr/source/firefox-78.10.1esr.source.tar.xz"
