2b924b2c4f954d3a05cdc5a255823455d1e11280
Rui Chen <rui@chenrui.dev>
2021-09-19 01:25:55 -0400

    flux: update to use pkg-config-wrapper

--- Formula/flux.rb
+++ Formula/flux.rb
@@ -21,27 +21,14 @@ class Flux < Formula
   end
 
   depends_on "go" => :build
+  depends_on "pkg-config-wrapper" => :build
   depends_on "rust" => :build
 
   on_linux do
     depends_on "pkg-config" => :build
   end
 
-  # NOTE: The version here is specified in the go.mod of influxdb.
-  # If you're upgrading to a newer influxdb version, check to see if this needs upgraded too.
-  resource "pkg-config-wrapper" do
-    url "https://github.com/influxdata/pkg-config/archive/refs/tags/v0.2.8.tar.gz"
-    sha256 "9d3f3bbcac7c787f6e8846e70172d06bd4d7394b4bcd0b8572fe2f1d03edc11b"
-  end
-
   def install
-    # Set up the influxdata pkg-config wrapper to enable just-in-time compilation & linking
-    # of the Rust components in the server.
-    resource("pkg-config-wrapper").stage do
-      system "go", "build", *std_go_args, "-o", buildpath/"bootstrap/pkg-config"
-    end
-    ENV.prepend_path "PATH", buildpath/"bootstrap"
-
     system "make", "build"
     system "go", "build", *std_go_args(ldflags: "-s -w"), "./cmd/flux"
     include.install "libflux/include/influxdata"
