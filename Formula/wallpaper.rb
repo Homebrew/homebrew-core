29117c9d0f4735d779a259996d5b335fc7913e9a
Rui Chen <rui@chenrui.dev>
2021-09-18 22:34:26 -0400

    wallpaper 2.2.0

--- Formula/wallpaper.rb
+++ Formula/wallpaper.rb
@@ -1,10 +1,9 @@
 class Wallpaper < Formula
   desc "Manage the desktop wallpaper"
   homepage "https://github.com/sindresorhus/macos-wallpaper"
-  url "https://github.com/sindresorhus/macos-wallpaper/archive/v2.1.0.tar.gz"
-  sha256 "4925247524535c0cc128dcc4d87f5538a5ce3b5d3a3c211127fd646ee00252b6"
+  url "https://github.com/sindresorhus/macos-wallpaper/archive/v2.2.0.tar.gz"
+  sha256 "239ba18397a5886066fc0fbee4235e3282edcf7ea075109889eb2552a04f0d1f"
   license "MIT"
-  revision 1
   head "https://github.com/sindresorhus/macos-wallpaper.git", branch: "main"
 
   bottle do
