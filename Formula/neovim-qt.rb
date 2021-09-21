172889d5d0737e0c3724264cd7231283f0801a48
Greg Werbin <gwerbin@understood.org>
2021-09-21 14:27:27 -0400

    Re-add license after rebase issue

--- Formula/neovim-qt.rb
+++ Formula/neovim-qt.rb
@@ -2,6 +2,7 @@ class NeovimQt < Formula
   desc "Neovim GUI, in Qt5"
   homepage "https://github.com/equalsraf/neovim-qt"
   url "https://github.com/equalsraf/neovim-qt/archive/v0.2.16.1.tar.gz"
+  license "ISC"
   sha256 "971d4597b40df2756b313afe1996f07915643e8bf10efe416b64cc337e4faf2a"
   head "https://github.com/equalsraf/neovim-qt.git"
 
@@ -49,6 +50,7 @@ class NeovimQt < Formula
 
     puts "#{bin}/nvim-qt --nofork -- --listen #{testserver}"
     nvimqt_pid = spawn bin/"nvim-qt", "--nofork", "--", "--listen", testserver
+    sleep 2.0
     system "nvr", *nvr_opts, "--remote", testfile
     system "nvr", *nvr_opts, "-c", testcommand
     system "nvr", *nvr_opts, "-c", "w"
