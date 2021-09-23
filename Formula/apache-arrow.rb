c5133bda87c7b27f3fd39f1a698af21c860a9508
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-09-23 18:11:05 +0800

    apache-arrow: switch to `openssl@3`

--- Formula/apache-arrow.rb
+++ Formula/apache-arrow.rb
@@ -5,7 +5,7 @@ class ApacheArrow < Formula
   mirror "https://archive.apache.org/dist/arrow/arrow-5.0.0/apache-arrow-5.0.0.tar.gz"
   sha256 "c3b4313eca594c20f761a836719721aaf0760001af896baec3ab64420ff9910a"
   license "Apache-2.0"
-  revision 2
+  revision 3
   head "https://github.com/apache/arrow.git", branch: "master"
 
   bottle do
@@ -24,7 +24,7 @@ class ApacheArrow < Formula
   depends_on "grpc"
   depends_on "lz4"
   depends_on "numpy"
-  depends_on "openssl@1.1"
+  depends_on "openssl@3"
   depends_on "protobuf"
   depends_on "python@3.9"
   depends_on "rapidjson"
