9f2939d089663d1b3e35e565072a31c1231492d5
max furman <mx.furman@gmail.com>
2021-09-24 15:19:08 -0700

    step 0.17.3

--- Formula/step.rb
+++ Formula/step.rb
@@ -1,8 +1,8 @@
 class Step < Formula
   desc "Crypto and x509 Swiss-Army-Knife"
   homepage "https://smallstep.com"
-  url "https://github.com/smallstep/cli/releases/download/v0.17.2/step_0.17.2.tar.gz"
-  sha256 "a44ae9db467284c924f91f965a292ecadf404f3ffcde8953abb6b158fa518d91"
+  url "https://github.com/smallstep/cli/releases/download/v0.17.3/step_0.17.3.tar.gz"
+  sha256 "d890e7cb8df5310dbdff625ef6b8037307e8d8ca4d61ddf9a9dfa1baecc862ed"
   license "Apache-2.0"
 
   bottle do
@@ -16,12 +16,13 @@ class Step < Formula
   depends_on "go" => :build
 
   resource "certificates" do
-    url "https://github.com/smallstep/certificates/releases/download/v0.17.2/step-ca_0.17.2.tar.gz"
-    sha256 "78e32437a200a563046510f5adec085f29e744db150e88a2b214905d4f423ef2"
+    url "https://github.com/smallstep/certificates/releases/download/v0.17.3/step-ca_0.17.3.tar.gz"
+    sha256 "56a7a2a1e0aec520ab62ae10da659bae329b3325c5023d6de332c6320e0f8d3e"
   end
 
   def install
     ENV["VERSION"] = version.to_s
+    ENV["CGO_OVERRIDE"] = "CGO_ENABLED=1"
     system "make", "build"
     bin.install "bin/step" => "step"
     bash_completion.install "autocomplete/bash_autocomplete" => "step"
@@ -29,6 +30,7 @@ class Step < Formula
 
     resource("certificates").stage do |r|
       ENV["VERSION"] = r.version.to_s
+      ENV["CGO_OVERRIDE"] = "CGO_ENABLED=1"
       system "make", "build"
       bin.install "bin/step-ca" => "step-ca"
     end
