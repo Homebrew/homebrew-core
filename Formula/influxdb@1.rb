cadb612c917095b7855c87810644b98607882713
Dawid Dziurla <dawidd0811@gmail.com>
2021-09-26 22:41:37 +0200

    influxdb@1: set pkg-config-wrapper up

--- Formula/influxdb@1.rb
+++ Formula/influxdb@1.rb
@@ -22,7 +22,21 @@ class InfluxdbAT1 < Formula
   depends_on "go" => :build
   depends_on "pkg-config" => :build
 
+  # NOTE: The version here is specified in the go.mod of influxdb.
+  # If you're upgrading to a newer influxdb version, check to see if this needs upgraded too.
+  resource "pkg-config-wrapper" do
+    url "https://github.com/influxdata/pkg-config/archive/refs/tags/v0.2.8.tar.gz"
+    sha256 "9d3f3bbcac7c787f6e8846e70172d06bd4d7394b4bcd0b8572fe2f1d03edc11b"
+  end
+
   def install
+    # Set up the influxdata pkg-config wrapper to enable just-in-time compilation & linking
+    # of the Rust components in the server.
+    resource("pkg-config-wrapper").stage do
+      system "go", "build", *std_go_args, "-o", buildpath/"bootstrap/pkg-config"
+    end
+    ENV.prepend_path "PATH", buildpath/"bootstrap"
+
     ldflags = "-s -w -X main.version=#{version}"
 
     %w[influxd influx influx_stress influx_inspect].each do |f|
