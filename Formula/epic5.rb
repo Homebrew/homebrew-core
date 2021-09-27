5f2a5baa8697defc1a50f1004b5e661b8f3506e5
Jonathan Chang <me@jonathanchang.org>
2021-09-26 20:54:11 -0700

    epic5: revision bump for openssl

--- Formula/epic5.rb
+++ Formula/epic5.rb
@@ -5,6 +5,7 @@ class Epic5 < Formula
   mirror "https://www.mirrorservice.org/sites/distfiles.macports.org/epic5/epic5-2.1.5.tar.xz"
   sha256 "7ca53b9d266fa1b7ba7a1f68a23e65c57b1a89b94bce1a0ea56fafa7d6298cb4"
   license "BSD-3-Clause"
+  revision 1
   head "http://git.epicsol.org/epic5.git"
 
   livecheck do
@@ -19,7 +20,7 @@ class Epic5 < Formula
     sha256 mojave:        "33233d7cd4cbadfb333079083a746791797402df481411568b99c874582fd440"
   end
 
-  depends_on "openssl@1.1"
+  depends_on "openssl@3"
 
   def install
     system "./configure", "--disable-debug",
@@ -27,7 +28,7 @@ class Epic5 < Formula
                           "--prefix=#{prefix}",
                           "--mandir=#{man}",
                           "--with-ipv6",
-                          "--with-ssl=#{Formula["openssl@1.1"].opt_prefix}"
+                          "--with-ssl=#{Formula["openssl@3"].opt_prefix}"
     system "make"
     system "make", "install"
   end
