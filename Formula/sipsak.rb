bbf3a004b3398131f2f3697fb12f24d62a58d7ed
Jonathan Chang <me@jonathanchang.org>
2021-09-26 20:54:34 -0700

    sipsak: revision bump for openssl

--- Formula/sipsak.rb
+++ Formula/sipsak.rb
@@ -4,6 +4,7 @@ class Sipsak < Formula
   url "https://github.com/nils-ohlmeier/sipsak/releases/download/0.9.8.1/sipsak-0.9.8.1.tar.gz"
   sha256 "c6faa022cd8c002165875d4aac83b7a2b59194f0491802924117fc6ac980c778"
   license "GPL-2.0-or-later"
+  revision 1
 
   bottle do
     sha256 cellar: :any,                 arm64_big_sur: "4525ec303fc0f5ffd3b752ccf1dcdc7fdb14921ad35d8e12109e257c47ce14fc"
@@ -13,7 +14,7 @@ class Sipsak < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "7a953aef939a2e020ceb548db309d98462b4b2ca0ea2e9f70c0f298a7f0dbd3c"
   end
 
-  depends_on "openssl@1.1"
+  depends_on "openssl@3"
 
   def install
     ENV.append "CFLAGS", "-std=gnu89"
