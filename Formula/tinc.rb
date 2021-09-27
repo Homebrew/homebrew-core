7381939fc26d672b6ada5620ba6db9967f26cada
Jonathan Chang <me@jonathanchang.org>
2021-09-26 20:54:39 -0700

    tinc: revision bump for openssl

--- Formula/tinc.rb
+++ Formula/tinc.rb
@@ -4,6 +4,7 @@ class Tinc < Formula
   url "https://tinc-vpn.org/packages/tinc-1.0.36.tar.gz"
   sha256 "40f73bb3facc480effe0e771442a706ff0488edea7a5f2505d4ccb2aa8163108"
   license "GPL-2.0-or-later"
+  revision 1
 
   livecheck do
     url "https://www.tinc-vpn.org/download/"
@@ -21,13 +22,13 @@ class Tinc < Formula
   end
 
   depends_on "lzo"
-  depends_on "openssl@1.1"
+  depends_on "openssl@3"
 
   uses_from_macos "zlib"
 
   def install
     system "./configure", "--prefix=#{prefix}", "--sysconfdir=#{etc}",
-                          "--with-openssl=#{Formula["openssl@1.1"].opt_prefix}"
+                          "--with-openssl=#{Formula["openssl@3"].opt_prefix}"
     system "make", "install"
   end
 
