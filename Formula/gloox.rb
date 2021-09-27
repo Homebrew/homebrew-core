c2239da5ba5dac14878c7fdf660e5961313ec66d
Jonathan Chang <me@jonathanchang.org>
2021-09-26 20:54:13 -0700

    gloox: revision bump for openssl

--- Formula/gloox.rb
+++ Formula/gloox.rb
@@ -3,6 +3,7 @@ class Gloox < Formula
   homepage "https://camaya.net/gloox/"
   url "https://camaya.net/download/gloox-1.0.24.tar.bz2"
   sha256 "ae1462be2a2eb8fe5cd054825143617c53c2c9c7195606cb5a5ba68c0f68f9c9"
+  revision 1
 
   livecheck do
     url :homepage
@@ -20,7 +21,7 @@ class Gloox < Formula
 
   depends_on "pkg-config" => :build
   depends_on "libidn"
-  depends_on "openssl@1.1"
+  depends_on "openssl@3"
 
   uses_from_macos "zlib"
 
@@ -28,7 +29,7 @@ class Gloox < Formula
     system "./configure", "--prefix=#{prefix}",
                           "--with-zlib",
                           "--disable-debug",
-                          "--with-openssl=#{Formula["openssl@1.1"].opt_prefix}"
+                          "--with-openssl=#{Formula["openssl@3"].opt_prefix}"
     system "make", "install"
   end
 
