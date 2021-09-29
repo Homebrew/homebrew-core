4fa236a511cdf826d7ba89875a654b2bad6b7737
MoonFruit <dkmoonfruit@gmail.com>
2021-09-29 13:53:13 +0800

    macvim: remove unused Sparkle.framework

--- Formula/macvim.rb
+++ Formula/macvim.rb
@@ -54,6 +54,7 @@ class Macvim < Formula
                           "--with-macarchs=#{Hardware::CPU.arch}"
     system "make"
 
+    rm_rf Dir["src/MacVim/build/Release/MacVim.app/Contents/Frameworks/Sparkle.framework"]
     prefix.install "src/MacVim/build/Release/MacVim.app"
     bin.install_symlink prefix/"MacVim.app/Contents/bin/mvim"
 
