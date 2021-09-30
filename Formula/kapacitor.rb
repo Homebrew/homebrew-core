97302cd81a1b062355047da8fdcd0fa760271624
Rui Chen <rui@chenrui.dev>
2021-09-29 21:35:33 -0400

    kapacitor 1.6.2

--- Formula/kapacitor.rb
+++ Formula/kapacitor.rb
@@ -2,8 +2,8 @@ class Kapacitor < Formula
   desc "Open source time series data processor"
   homepage "https://github.com/influxdata/kapacitor"
   url "https://github.com/influxdata/kapacitor.git",
-      tag:      "v1.6.1",
-      revision: "0eaf1848f412d555f57ed9724325e934ec4838e7"
+      tag:      "v1.6.2",
+      revision: "2eec58255d33330360b7ee1db36117bde7346b8e"
   license "MIT"
   head "https://github.com/influxdata/kapacitor.git"
 
