97fbca5dfa6a508592be28e561c81403f51ca01b
Jonathan Chang <me@jonathanchang.org>
2021-10-03 16:00:50 -0700

    bsponmpi: add license

--- Formula/bsponmpi.rb
+++ Formula/bsponmpi.rb
@@ -3,6 +3,7 @@ class Bsponmpi < Formula
   homepage "https://sourceforge.net/projects/bsponmpi/"
   url "https://downloads.sourceforge.net/project/bsponmpi/bsponmpi/0.3/bsponmpi-0.3.tar.gz"
   sha256 "bc90ca22155be9ff65aca4e964d8cd0bef5f0facef0a42bc1db8b9f822c92a90"
+  license "LGPL-2.1-or-later"
   revision 2
 
   bottle do
