caf9ed0f0c1db14ab4ded9cf275751a8c19ae5d0
rui <rui@chenrui.dev>
2021-10-03 13:43:37 -0400

    update deps

--- Formula/latexml.rb
+++ Formula/latexml.rb
@@ -151,8 +151,8 @@ class Latexml < Formula
 
   resource "JSON::XS" do
     on_linux do
-      url "https://cpan.metacpan.org/authors/id/M/ML/MLEHMANN/JSON-XS-4.02.tar.gz"
-      sha256 "a5ad172138071a14729da8a01921ca233da4fe2bed290ffdfb8e560dd8adcf16"
+      url "https://cpan.metacpan.org/authors/id/M/ML/MLEHMANN/JSON-XS-4.03.tar.gz"
+      sha256 "515536f45f2fa1a7e88c8824533758d0121d267ab9cb453a1b5887c8a56b9068"
     end
   end
 
