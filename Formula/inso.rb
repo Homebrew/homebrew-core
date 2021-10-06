ba3466233b7ec84cb5d22a13e82525bb0c707c08
Isa Farnik <if_then@hey.com>
2021-10-06 12:56:46 -0700

    appease the elder brew gods

--- Formula/inso.rb
+++ Formula/inso.rb
@@ -7,11 +7,33 @@ class Inso < Formula
   sha256 "3ae32671053f87957b508f40a2ffd5a610b03cae85ecffd801432c8288abcc03"
   license "MIT"
 
-  depends_on "curl"
   depends_on "node"
 
+  uses_from_macos "curl"
+
   def install
-    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    # std_npm_install_args includes --build-from-source when brew itself is run
+    # with the flag (as both CI does and formula developers are instructed to
+    # do)
+    #
+    # in the case of the combination of uses_from_macos "curl", and
+    # node-libcurl, that flags causes node-gyp to attempt to source Mac OS's
+    # own libcurl.dylib, which fails as system dylibs are not made available in
+    # such a way these days
+    #
+    # ignoring the --build-from-source flags below means that even in,
+    # situations where brew requires that flag, it will be ignored for the npm
+    # install command, and node will avoid attempting to source libcurl from
+    # Mac OS's private dylibs
+    npm_arguments = [
+      "npm",
+      "install",
+      Language::Node.std_npm_install_args(libexec).delete_if do |arg|
+        arg == "--build-from-source"
+      end,
+    ].flatten
+
+    system(*npm_arguments)
     bin.install_symlink Dir["#{libexec}/bin/*"]
 
     deuniversalize_machos
