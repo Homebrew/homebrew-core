99073b681505d185467733637d6c98fde671cb47
rui <rui@chenrui.dev>
2021-10-06 19:40:38 -0400

    solana: use short url

--- Formula/solana.rb
+++ Formula/solana.rb
@@ -1,7 +1,7 @@
 class Solana < Formula
   desc "Web-Scale Blockchain for decentralized apps and marketplaces"
   homepage "https://solana.com"
-  url "https://github.com/solana-labs/solana/archive/refs/tags/v1.7.14.tar.gz"
+  url "https://github.com/solana-labs/solana/archive/v1.7.14.tar.gz"
   sha256 "65b4b9c3ea53b3cd06f905c685b938fad75f675ee9c46c044474d3c1302287d5"
   license "Apache-2.0"
 
