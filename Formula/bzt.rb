c374039d1233ee9b83ad8cb703788ee5378500c6
greyfenrir <grey.fenrir@gmail.com>
2021-10-07 11:02:07 +0300

    fix

--- Formula/bzt.rb
+++ Formula/bzt.rb
@@ -216,7 +216,7 @@ class Bzt < Formula
   end
 
   test do
-    cmd = "#{bin}/bzt -v -o execution.executor=apiritif -o execution.iterations=1 -o execution.scenario.requests.0=https://gettaurus.org/"
+    cmd = "#{bin}/bzt -v -o execution.executor=locust -o execution.iterations=1 -o execution.scenario.requests.0=https://gettaurus.org/"
     # assert_match /INFO: Samples count: 1, .*% failures/, shell_output(cmd)
     system(cmd)
   end
