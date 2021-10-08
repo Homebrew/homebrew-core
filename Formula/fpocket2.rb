0d7833c9c446f62c4c2f877eecb77eeed8d13201
Romain Studer <romain.studer@benevolent.ai>
2021-10-08 12:54:41 +0100

    Add fpocket2 package

---
+++ Formula/fpocket2.rb
@@ -0,0 +1,17 @@
+class Fpocket2 < Formula
+  desc "Protein pocket detection algorithm based on Voronoi tessellation"
+  homepage "https://fpocket.sourceforge.io/"
+  url "https://netcologne.dl.sourceforge.net/project/fpocket/fpocket2.tar.gz"
+  sha256 "76c90514e3e81d8068007ecabfe8454699bee8bf22fea384b3cb14879c105668"
+  license "GPL-3.0-or-later"
+
+  def install
+    system "make"
+    bin.install Dir["bin/*pocket"]
+    bin.install "bin/pcheck"
+  end
+
+  test do
+    system "#{bin}/pcheck"
+  end
+end
