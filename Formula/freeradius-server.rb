a751d74029ec48328e55f40a59be3bb65bbc7d22
rui <rui@chenrui.dev>
2021-10-09 22:15:24 -0400

    freeradius-server: update license

--- Formula/freeradius-server.rb
+++ Formula/freeradius-server.rb
@@ -3,7 +3,7 @@ class FreeradiusServer < Formula
   homepage "https://freeradius.org/"
   url "https://github.com/FreeRADIUS/freeradius-server/archive/release_3_0_25.tar.gz"
   sha256 "493b0b9bef3d9f0e6949fcfd6aa282164f6b8e6404231f2b0d1353739199ffcd"
-  license "GPL-2.0"
+  license all_of: ["GPL-2.0-or-later", "LGPL-2.1-or-later"]
   head "https://github.com/FreeRADIUS/freeradius-server.git"
 
   livecheck do
