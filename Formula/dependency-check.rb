f6233e4c1211d14cb08c2746dcc3d07fc9669488
Jeremy Long <jeremy.long@gmail.com>
2021-10-11 17:38:29 -0400

    dependency-check 6.4.1

--- Formula/dependency-check.rb
+++ Formula/dependency-check.rb
@@ -1,8 +1,8 @@
 class DependencyCheck < Formula
   desc "OWASP dependency-check"
   homepage "https://owasp.org/www-project-dependency-check/"
-  url "https://github.com/jeremylong/DependencyCheck/releases/download/v6.4.0/dependency-check-6.4.0-release.zip"
-  sha256 "584ed061b71727844697301ec27839b6a4830221d7c3a2dfcdb436dd4833b9e0"
+  url "https://github.com/jeremylong/DependencyCheck/releases/download/v6.4.1/dependency-check-6.4.1-release.zip"
+  sha256 "d7eb3169bebc7f0235cc8c44e1cb342fb685cfab99dbe8ce58740f171f7890aa"
   license "Apache-2.0"
 
   livecheck do
