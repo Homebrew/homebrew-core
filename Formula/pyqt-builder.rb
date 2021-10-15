472b84692d79c9f01f62324170bdcf00cdeacfa5
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-15 14:14:44 +0800

    pyqt-builder: migrate to python@3.10

--- Formula/pyqt-builder.rb
+++ Formula/pyqt-builder.rb
@@ -6,21 +6,22 @@ class PyqtBuilder < Formula
   url "https://files.pythonhosted.org/packages/62/fd/e74d789fa6ca10be33f2d782a96482a4bcf0104c9287bac151f3062d3984/PyQt-builder-1.11.0.tar.gz"
   sha256 "40f6df88c00e6aa9ac9a8bc5688f9fe2a4bd56c06cdb0a0b00ce8955ec34ffe5"
   license any_of: ["GPL-2.0-only", "GPL-3.0-only"]
+  revision 1
   head "https://www.riverbankcomputing.com/hg/PyQt-builder", using: :hg
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "d8e7c1d772d32266c1d454480bbbe29419f42507035d388e0287a89feb86343e"
   end
 
-  depends_on "python@3.9"
+  depends_on "python@3.10"
   depends_on "sip"
 
   def install
-    system Formula["python@3.9"].opt_bin/"python3", *Language::Python.setup_install_args(prefix)
+    system "python3", *Language::Python.setup_install_args(prefix)
   end
 
   test do
     system bin/"pyqt-bundle", "-V"
-    system Formula["python@3.9"].opt_bin/"python3", "-c", "import pyqtbuild"
+    system Formula["python@3.10"].opt_bin/"python3", "-c", "import pyqtbuild"
   end
 end
