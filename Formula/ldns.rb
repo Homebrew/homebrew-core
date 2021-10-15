fb0443de45de2d060103ced65cc1f9784f4f9f62
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-15 14:14:43 +0800

    ldns: migrate to python@3.10

--- Formula/ldns.rb
+++ Formula/ldns.rb
@@ -4,7 +4,7 @@ class Ldns < Formula
   url "https://nlnetlabs.nl/downloads/ldns/ldns-1.7.1.tar.gz"
   sha256 "8ac84c16bdca60e710eea75782356f3ac3b55680d40e1530d7cea474ac208229"
   license "BSD-3-Clause"
-  revision 4
+  revision 5
 
   # https://nlnetlabs.nl/downloads/ldns/ since the first-party site has a
   # tendency to lead to an `execution expired` error.
@@ -23,7 +23,7 @@ class Ldns < Formula
 
   depends_on "swig" => :build
   depends_on "openssl@1.1"
-  depends_on "python@3.9"
+  depends_on "python@3.10"
 
   conflicts_with "drill", because: "both install a `drill` binary"
 
@@ -34,7 +34,7 @@ class Ldns < Formula
       --with-examples
       --with-ssl=#{Formula["openssl@1.1"].opt_prefix}
       --with-pyldns
-      PYTHON_SITE_PKG=#{lib}/python3.9/site-packages
+      PYTHON_SITE_PKG=#{prefix/Language::Python.site_packages("python3")}
       --disable-dane-verify
       --without-xcode-sdk
     ]
@@ -42,7 +42,7 @@ class Ldns < Formula
     # Fixes: ./contrib/python/ldns_wrapper.c:2746:10: fatal error: 'ldns.h' file not found
     inreplace "contrib/python/ldns.i", "#include \"ldns.h\"", "#include <ldns/ldns.h>"
 
-    ENV["PYTHON"] = Formula["python@3.9"].opt_bin/"python3"
+    ENV["PYTHON"] = which("python3")
     system "./configure", *args
 
     if OS.mac?
