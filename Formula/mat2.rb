ed8ac35048626d916b9be03f626c41882afeb1b3
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-15 12:52:28 +0800

    mat2: migrate to python@3.10

--- Formula/mat2.rb
+++ Formula/mat2.rb
@@ -4,6 +4,7 @@ class Mat2 < Formula
   url "https://0xacab.org/jvoisin/mat2/-/archive/0.12.2/mat2-0.12.2.tar.gz"
   sha256 "f3ef46fc0ccc13fd055991841c7fbee8a1322ba895d83a8da404bdaf0c638b98"
   license "LGPL-3.0-or-later"
+  revision 1
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "d4521aaeb4a33e93180be0d1fe846d4d78ff2d04089219c9c08d1beec69679ff"
@@ -16,7 +17,7 @@ class Mat2 < Formula
   depends_on "poppler"
   depends_on "py3cairo"
   depends_on "pygobject3"
-  depends_on "python@3.9"
+  depends_on "python@3.10"
 
   resource "mutagen" do
     url "https://files.pythonhosted.org/packages/f3/d9/2232a4cb9a98e2d2501f7e58d193bc49c956ef23756d7423ba1bd87e386d/mutagen-1.45.1.tar.gz"
@@ -24,19 +25,18 @@ class Mat2 < Formula
   end
 
   def install
-    version = Language::Python.major_minor_version Formula["python@3.9"].bin/"python3"
-    pygobject3 = Formula["pygobject3"]
-    ENV["PYTHONPATH"] = lib/"python#{version}/site-packages"
-    ENV.append_path "PYTHONPATH", pygobject3.opt_lib+"python#{version}/site-packages"
-    ENV.prepend_create_path "PYTHONPATH", libexec/"vendor/lib/python#{version}/site-packages"
+    site_packages = Language::Python.site_packages("python3")
+    ENV["PYTHONPATH"] = prefix/site_packages
+    ENV.append_path "PYTHONPATH", Formula["pygobject3"].opt_prefix/site_packages
+    ENV.prepend_create_path "PYTHONPATH", libexec/"vendor"/site_packages
 
     resources.each do |r|
       r.stage do
-        system Formula["python@3.9"].bin/"python3", *Language::Python.setup_install_args(libexec/"vendor")
+        system "python3", *Language::Python.setup_install_args(libexec/"vendor")
       end
     end
 
-    system Formula["python@3.9"].bin/"python3", *Language::Python.setup_install_args(prefix)
+    system "python3", *Language::Python.setup_install_args(prefix)
 
     bin.env_script_all_files(libexec/"bin", PYTHONPATH: ENV["PYTHONPATH"])
   end
