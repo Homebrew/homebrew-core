ecf31ce03b0dafe38949800fadd88e12f208575f
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-15 12:52:30 +0800

    osinfo-db-tools: migrate to python@3.10

--- Formula/osinfo-db-tools.rb
+++ Formula/osinfo-db-tools.rb
@@ -4,6 +4,7 @@ class OsinfoDbTools < Formula
   url "https://releases.pagure.org/libosinfo/osinfo-db-tools-1.9.0.tar.xz"
   sha256 "255f1c878bacec70c3020ff5a9cb0f6bd861ca0009f24608df5ef6f62d5243c0"
   license "GPL-2.0-or-later"
+  revision 1
 
   livecheck do
     url "https://releases.pagure.org/libosinfo/?C=M&O=D"
@@ -26,7 +27,7 @@ class OsinfoDbTools < Formula
   depends_on "json-glib"
   depends_on "libarchive"
   depends_on "libsoup"
-  depends_on "python@3.9"
+  depends_on "python@3.10"
 
   uses_from_macos "pod2man" => :build
   uses_from_macos "libxml2"
