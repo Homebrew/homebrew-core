e4446aff148413582c30262f6bccefd964ca079f
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-15 12:52:27 +0800

    lilypond: migrate to python@3.10

--- Formula/lilypond.rb
+++ Formula/lilypond.rb
@@ -11,6 +11,7 @@ class Lilypond < Formula
     :public_domain,
     "MIT",
   ]
+  revision 1
 
   livecheck do
     url "https://lilypond.org/source.html"
@@ -44,7 +45,7 @@ class Lilypond < Formula
   depends_on "ghostscript"
   depends_on "guile@2"
   depends_on "pango"
-  depends_on "python@3.9"
+  depends_on "python@3.10"
 
   uses_from_macos "flex" => :build
   uses_from_macos "perl" => :build
