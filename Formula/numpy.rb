58b9398cd3ba5a60fda26f957627dcdd43abef4f
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-10-15 14:14:43 +0800

    numpy: migrate to python@3.10

--- Formula/numpy.rb
+++ Formula/numpy.rb
@@ -4,6 +4,7 @@ class Numpy < Formula
   url "https://files.pythonhosted.org/packages/3a/be/650f9c091ef71cb01d735775d554e068752d3ff63d7943b26316dc401749/numpy-1.21.2.zip"
   sha256 "423216d8afc5923b15df86037c6053bf030d15cc9e3224206ef868c2d63dd6dc"
   license "BSD-3-Clause"
+  revision 1
   head "https://github.com/numpy/numpy.git", branch: "main"
 
   bottle do
@@ -17,14 +18,14 @@ class Numpy < Formula
   depends_on "cython" => :build
   depends_on "gcc" => :build # for gfortran
   depends_on "openblas"
-  depends_on "python@3.9"
+  depends_on "python@3.10"
 
   fails_with gcc: "5"
 
   def install
     openblas = Formula["openblas"].opt_prefix
     ENV["ATLAS"] = "None" # avoid linking against Accelerate.framework
-    ENV["BLAS"] = ENV["LAPACK"] = "#{openblas}/lib/#{shared_library("libopenblas")}"
+    ENV["BLAS"] = ENV["LAPACK"] = openblas/"lib"/shared_library("libopenblas")
 
     config = <<~EOS
       [openblas]
@@ -35,16 +36,15 @@ class Numpy < Formula
 
     Pathname("site.cfg").write config
 
-    xy = Language::Python.major_minor_version Formula["python@3.9"].opt_bin/"python3"
-    ENV.prepend_create_path "PYTHONPATH", Formula["cython"].opt_libexec/"lib/python#{xy}/site-packages"
+    site_packages = Language::Python.site_packages("python3")
+    ENV.prepend_create_path "PYTHONPATH", Formula["cython"].opt_prefix/site_packages
 
-    system Formula["python@3.9"].opt_bin/"python3", "setup.py", "build",
-        "--fcompiler=gfortran", "--parallel=#{ENV.make_jobs}"
-    system Formula["python@3.9"].opt_bin/"python3", *Language::Python.setup_install_args(prefix)
+    system "python3", "setup.py", "build", "--fcompiler=gfortran", "--parallel=#{ENV.make_jobs}"
+    system "python3", *Language::Python.setup_install_args(prefix)
   end
 
   test do
-    system Formula["python@3.9"].opt_bin/"python3", "-c", <<~EOS
+    system Formula["python@3.10"].opt_bin/"python3", "-c", <<~EOS
       import numpy as np
       t = np.ones((3,3), int)
       assert t.sum() == 9
