d11e882e1039165ae87593d6ebaa03831de8c5c5
Bo Anderson <mail@boanderson.me>
2021-10-16 02:44:47 +0100

    Update latexml.rb

--- Formula/latexml.rb
+++ Formula/latexml.rb
@@ -191,7 +191,7 @@ class Latexml < Formula
       r.stage do
         ENV["PERL_CANARY_STABILITY_NOPROMPT"] = "1" if OS.linux?
 
-        system "perl", "Makefile.PL", "INSTALL_BASE=#{libexec}", "DEBUG=1", "verbose=1", "verb=1"
+        system "perl", "Makefile.PL", "INSTALL_BASE=#{libexec}", "DEBUG=1", "verbose=1", "verb=1", "INST_ARCHLIB=#{Pathname.pwd}/blib/arch"
         system "make"
         system "make", "install"
       end
