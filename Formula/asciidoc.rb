22b5536d81fc8288bb1c3bf88f8b463756ec7881
Matthew Peveler <matt.peveler@gmail.com>
2021-10-16 20:05:38 -1000

    asciidoc: Update homepage
    Signed-off-by: Matthew Peveler <matt.peveler@gmail.com>

--- Formula/asciidoc.rb
+++ Formula/asciidoc.rb
@@ -2,11 +2,11 @@ class Asciidoc < Formula
   include Language::Python::Shebang
 
   desc "Formatter/translator for text files to numerous formats. Includes a2x"
-  homepage "https://asciidoc.org/"
+  homepage "https://asciidoc-py.github.io/"
   url "https://github.com/asciidoc-py/asciidoc-py/archive/9.1.1.tar.gz"
   sha256 "914dfc1542c30bd47faa0aaaae0985cb57d0ca584015729ccd1b94d90da3a616"
   license "GPL-2.0-only"
-  revision 1
+  revision 2
   head "https://github.com/asciidoc-py/asciidoc-py.git", branch: "main"
 
   livecheck do
