a68cd9b54aba010c22ee7cc808f0978c238f3b72
Rui Chen <rui@chenrui.dev>
2021-10-18 22:22:34 -0400

    fix build
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/talisman.rb
+++ Formula/talisman.rb
@@ -22,7 +22,7 @@ class Talisman < Formula
   depends_on "go" => :build
 
   def install
-    system "go", "build", *std_go_args(ldflags: "-X main.Version=#{version}")
+    system "go", "build", *std_go_args(ldflags: "-X main.Version=#{version}"), "./cmd"
   end
 
   test do
