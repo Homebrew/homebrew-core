1e12b7647a513c78c2ca51377d1de19188484309
curoky <cccuroky@gmail.com>
2021-10-19 11:16:04 +0800

    iputils: add license and head

--- Formula/iputils.rb
+++ Formula/iputils.rb
@@ -3,6 +3,8 @@ class Iputils < Formula
   homepage 'https://github.com/iputils/iputils'
   url 'https://github.com/iputils/iputils/archive/refs/tags/20210202.tar.gz'
   sha256 '3f557ecfd2ace873801231d2c1f42de73ced9fbc1ef3a438d847688b5fb0e8ab'
+  head "https://github.com/iputils/iputils.git", branch: "master"
+  license all_of: ["GPL-2.0-or-later", "BSD-3-Clause"]
 
   depends_on :linux
   depends_on 'meson' => :build
