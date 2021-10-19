95a96593453b570d3d9ae9a0c0ad9c7a0f1618b9
curoky <cccuroky@gmail.com>
2021-10-19 15:16:59 +0800

    terminalizer: fix build failed on linux

--- Formula/terminalizer.rb
+++ Formula/terminalizer.rb
@@ -12,17 +12,26 @@ class Terminalizer < Formula
     url :stable
   end
 
-  depends_on "node"
+  depends_on "node" if OS.linux?
+  depends_on "node@14" if OS.mac?
+  depends_on "python@3.9"
+  depends_on "gcc"
 
   def install
-    mkdir_p libexec/"lib"
+    on_linux do
+      gcc_major_ver = Formula["gcc"].any_installed_version.major
+      ENV["CC"] = Formula["gcc"].opt_bin / "gcc-#{gcc_major_ver}"
+      ENV["CXX"] = Formula["gcc"].opt_bin / "g++-#{gcc_major_ver}"
+    end
+
+    mkdir_p libexec / "lib"
     system "npm", "install", *Language::Node.std_npm_install_args(libexec)
     bin.install_symlink Dir["#{libexec}/bin/*"]
   end
 
   test do
     output = shell_output("#{bin}/terminalizer --version")
-    assert_match "0.7.2", output.strip
+    assert_match "#{version}", output.strip
 
     output = shell_output("#{bin}/terminalizer config")
     assert_match "config.yml", output.strip
