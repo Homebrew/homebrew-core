47b68e1604793c7f404c5d109c20a4b6323760d7
curoky <cccuroky@gmail.com>
2021-10-19 11:32:20 +0800

    iputils: modify the parameter order

--- Formula/iputils.rb
+++ Formula/iputils.rb
@@ -1,15 +1,15 @@
 class Iputils < Formula
-  desc "The iputils package is set of small useful utilities for Linux networking"
+  desc "Iputils package is set of small useful utilities for Linux networking"
   homepage "https://github.com/iputils/iputils"
   url "https://github.com/iputils/iputils/archive/refs/tags/20210202.tar.gz"
   sha256 "3f557ecfd2ace873801231d2c1f42de73ced9fbc1ef3a438d847688b5fb0e8ab"
-  head "https://github.com/iputils/iputils.git", branch: "master"
   license all_of: ["GPL-2.0-or-later", "BSD-3-Clause"]
+  head "https://github.com/iputils/iputils.git", branch: "master"
 
-  depends_on :linux
   depends_on "meson" => :build
   depends_on "ninja" => :build
   depends_on "libcap"
+  depends_on :linux
 
   def install
     ENV.prepend_path "PATH", Formula["libcap"].sbin
