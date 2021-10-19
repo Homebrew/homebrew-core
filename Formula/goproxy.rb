108fa08ebc6cc35e16b085fe1f8e0f956191ee24
curoky <cccuroky@gmail.com>
2021-10-19 10:58:54 +0800

    goproxy 2.0.7 (new formula)

---
+++ Formula/goproxy.rb
@@ -0,0 +1,27 @@
+class Goproxy < Formula
+  desc 'A global proxy for Go modules.'
+  homepage 'https://github.com/goproxyio/goproxy'
+  url 'https://github.com/goproxyio/goproxy/archive/v2.0.7.tar.gz'
+  sha256 'd87f3928467520f8d6b0ba8adcbf5957dc6eb2dc9936249edd6568ceb01a71ca'
+  license 'MIT'
+  head 'https://github.com/goproxyio/goproxy.git'
+
+  depends_on 'go' => :build
+
+  def install
+    system 'go', 'build', *std_go_args, '-ldflags', '-s -w', '.'
+  end
+
+  test do
+    bind_address = "127.0.0.1:#{free_port}"
+    begin
+      server = IO.popen("#{bin}/goproxy -proxy=https://goproxy.io -listen=#{bind_address}")
+      sleep 1
+      assert_match '',
+                   shell_output("GOPROXY='http://#{bind_address}' go get -v golang.org/x/tools@upgrade")
+    ensure
+      Process.kill('SIGINT', server.pid)
+      Process.wait(server.pid)
+    end
+  end
+end
