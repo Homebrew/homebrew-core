b63eff995ca40becd643ae61454d91574d631113
Rui Chen <rui@chenrui.dev>
2021-10-20 01:03:07 -0400

    remove e2e test and update to use `--kubernetes-version`
    ```
    Flag --kube-conformance-image-version has been deprecated,
    Use --kubernetes-version instead.
    ```

--- Formula/sonobuoy.rb
+++ Formula/sonobuoy.rb
@@ -15,24 +15,16 @@ class Sonobuoy < Formula
 
   depends_on "go" => :build
 
-  resource "sonobuoyresults" do
-    url "https://raw.githubusercontent.com/vmware-tanzu/sonobuoy/master/pkg/client/results/testdata/results-0.10.tar.gz"
-    sha256 "a945ba4d475e33820310a6138e3744f301a442ba01977d38f2b635d2e6f24684"
-  end
-
   def install
     system "go", "build", *std_go_args(ldflags: "-s -w -X github.com/vmware-tanzu/sonobuoy/pkg/buildinfo.Version=v#{version}")
   end
 
   test do
-    resources.each { |r| r.verify_download_integrity(r.fetch) }
     assert_match "Sonobuoy is a Kubernetes component that generates reports on cluster conformance",
       shell_output("#{bin}/sonobuoy 2>&1")
     assert_match version.to_s,
       shell_output("#{bin}/sonobuoy version 2>&1")
     assert_match "name: sonobuoy",
-      shell_output("#{bin}/sonobuoy gen --kube-conformance-image-version=v1.14 2>&1")
-    assert_match "all tests",
-      shell_output("#{bin}/sonobuoy e2e --show=all " + resource("sonobuoyresults").cached_download + " 2>&1")
+      shell_output("#{bin}/sonobuoy gen --kubernetes-version=v1.21 2>&1")
   end
 end
