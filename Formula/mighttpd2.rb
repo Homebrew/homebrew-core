117847794ab59e791a967714b7b5ec8446b48777
Rui Chen <rui@chenrui.dev>
2021-10-24 23:57:21 -0400

    mighttpd2: update homepage url
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/mighttpd2.rb
+++ Formula/mighttpd2.rb
@@ -1,6 +1,6 @@
 class Mighttpd2 < Formula
   desc "HTTP server"
-  homepage "https://www.mew.org/~kazu/proj/mighttpd/en/"
+  homepage "https://kazu-yamamoto.github.io/mighttpd2/"
   url "https://hackage.haskell.org/package/mighttpd2-4.0.0/mighttpd2-4.0.0.tar.gz"
   sha256 "5afc8acb4e268401dc19964b710230e5013399b8ad3baa7ae6d5e5802ad4ac42"
   license "BSD-3-Clause"
