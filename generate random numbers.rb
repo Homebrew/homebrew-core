0fc9f8522cfadcbb09637777cc0dc12290e70e2a
Suvoji01 <47207810+Suvoji01@users.noreply.github.com>
2021-10-26 23:26:53 +0530

    Create generate random numbers.rb

---
+++ generate random numbers.rb
@@ -0,0 +1,10 @@
+puts "Enter upper limit"
+up=gets.chomp.to_i
+puts "Enter lower limit"
+lm=gets.chomp.to_i
+puts "The random numbers are..."
+puts rand(lm..up)
+puts rand(lm..up)
+puts rand(lm..up)
+puts rand(lm..up)
+puts rand(lm..up)
