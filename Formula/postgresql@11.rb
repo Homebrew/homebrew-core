96660bef7fa9d35ab47de89ccf4c6a2e743f2475
Bo Anderson <mail@boanderson.me>
2021-10-31 01:00:53 +0000

    postgresql@11: revision bump for icu4c

--- Formula/postgresql@11.rb
+++ Formula/postgresql@11.rb
@@ -4,6 +4,7 @@ class PostgresqlAT11 < Formula
   url "https://ftp.postgresql.org/pub/source/v11.15/postgresql-11.15.tar.bz2"
   sha256 "c8f58e8ebd4f4567f4f9ba1032eb3e99e0251d87cbe3e564b485590e37a879e3"
   license "PostgreSQL"
+  revision 1
 
   livecheck do
     url "https://ftp.postgresql.org/pub/source/"
