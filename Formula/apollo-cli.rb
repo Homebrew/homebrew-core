be1702fb4569085545a12c19756b30192db15ab7
Rui Chen <rui@chenrui.dev>
2021-10-31 12:01:20 -0400

    apollo-cli 2.33.7

--- Formula/apollo-cli.rb
+++ Formula/apollo-cli.rb
@@ -3,8 +3,8 @@ require "language/node"
 class ApolloCli < Formula
   desc "Command-line tool for Apollo GraphQL"
   homepage "https://apollographql.com"
-  url "https://registry.npmjs.org/apollo/-/apollo-2.33.4.tgz"
-  sha256 "a32f25b86e2a200f08c762100239ec69a76990fc53ce4b8f6b355a966f6a84a0"
+  url "https://registry.npmjs.org/apollo/-/apollo-2.33.7.tgz"
+  sha256 "466ccc8866db694b5faa6e68c896b59858c0f3b26d1f1eae213bc129bcefe1c9"
   license "MIT"
 
   bottle do
