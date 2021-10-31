a682df41a4815e2a9f169f19053d7e34d31cbb88
Bo Anderson <mail@boanderson.me>
2021-10-31 01:55:50 +0100

    node@10: revision bump for icu4c

--- Formula/node@10.rb
+++ Formula/node@10.rb
@@ -4,7 +4,7 @@ class NodeAT10 < Formula
   url "https://nodejs.org/dist/v10.24.1/node-v10.24.1.tar.xz"
   sha256 "d72fc2c244603b4668da94081dc4d6067d467fdfa026e06a274012f16600480c"
   license "MIT"
-  revision 1
+  revision 2
 
   bottle do
     sha256 cellar: :any, monterey: "5714defcf3505e22bc5af68f26872b34b45c955db2d4a1701b080378c6b56166"
