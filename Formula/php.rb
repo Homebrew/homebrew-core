71ddbdd5ca9507e355fcbc12401610edcb108192
Bo Anderson <mail@boanderson.me>
2021-10-31 01:57:33 +0100

    php: revision bump for icu4c

--- Formula/php.rb
+++ Formula/php.rb
@@ -6,6 +6,7 @@ class Php < Formula
   mirror "https://fossies.org/linux/www/php-8.1.3.tar.xz"
   sha256 "5d65a11071b47669c17452fb336c290b67c101efb745c1dbe7525b5caf546ec6"
   license "PHP-3.01"
+  revision 1
 
   livecheck do
     url "https://www.php.net/downloads"
