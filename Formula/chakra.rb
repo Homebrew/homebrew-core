7ec3b8290531ccdbf1908fd234a52457cf08b763
Bo Anderson <mail@boanderson.me>
2021-10-31 01:45:28 +0100

    chakra: revision bump for icu4c

--- Formula/chakra.rb
+++ Formula/chakra.rb
@@ -4,7 +4,7 @@ class Chakra < Formula
   url "https://github.com/chakra-core/ChakraCore/archive/v1.11.24.tar.gz"
   sha256 "b99e85f2d0fa24f2b6ccf9a6d2723f3eecfe986a9d2c4d34fa1fd0d015d0595e"
   license "MIT"
-  revision 2
+  revision 3
 
   bottle do
     rebuild 1
