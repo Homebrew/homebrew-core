45ef95dab68f8878c800e3de611b93103f33ab22
Riddhesh Sanghvi <riddhesh237@gmail.com>
2021-11-02 15:31:30 +0530

    easyengine 4.4.0
    Signed-off-by: Riddhesh Sanghvi <riddhesh237@gmail.com>

--- Formula/easyengine.rb
+++ Formula/easyengine.rb
@@ -1,8 +1,8 @@
 class Easyengine < Formula
   desc "Command-line control panel to manage WordPress sites"
   homepage "https://easyengine.io/"
-  url "https://github.com/EasyEngine/easyengine/releases/download/v4.3.1/easyengine.phar"
-  sha256 "125a80fe3f0e067cbc5add818b92e12b5208179ed0e6d493872e3a9d59b1eecc"
+  url "https://github.com/EasyEngine/easyengine/releases/download/v4.4.0/easyengine.phar"
+  sha256 "fbec9321593c387a6d60c6a8b94b6804d0f71ce0fb83a29dc84b2a911eaf5b2e"
   license "MIT"
 
   bottle do
@@ -22,6 +22,26 @@ class Easyengine < Formula
 
   def install
     bin.install "easyengine.phar" => "ee"
+  end
+
+  def caveats
+    <<~EOS
+      Hey there, we have recently introduced a feature which would allow you to create more than 27 sites.
+
+      To enable it, you need to perform the following steps manually:
+
+      1. Please open Docker Desktop and in taskbar, go to Preferences > Daemon > Advanced.
+      2. If the file is empty, add the following:
+
+      {
+       "default-address-pools": [{"base":"10.0.0.0/8","size":24}]
+      }
+
+      If the file already contains JSON, just add the key "default-address-pools": [{"base":"10.0.0.0/8","size":24}]
+      being careful to add a comma to the end of the line if it is not the last line before the closing bracket.
+
+      3. Restart Docker
+    EOS
   end
 
   test do
