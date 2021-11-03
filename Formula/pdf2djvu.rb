c3792eb9c0067eeafeb058c5eb5bb8e89d77049d
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-11-04 02:56:41 +0800

    pdf2djvu: build with GCC on Linux

--- Formula/pdf2djvu.rb
+++ Formula/pdf2djvu.rb
@@ -12,6 +12,12 @@ class Pdf2djvu < Formula
   depends_on "gettext"
   depends_on "poppler"
 
+  on_linux do
+    depends_on "gcc"
+  end
+
+  fails_with gcc: "5" # poppler compiles with GCC
+
   def install
     system "./configure", "--prefix=#{prefix}"
     system "make"
