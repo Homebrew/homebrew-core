fdd4601f002320dda6a174d64b2a544ff9a572fa
Timo von Holtz <tvh@thor.lan>
2021-11-04 10:39:55 +0100

    leveldb: Disable tcmalloc
    This should fix https://github.com/google/leveldb/issues/634

--- Formula/leveldb.rb
+++ Formula/leveldb.rb
@@ -4,6 +4,7 @@ class Leveldb < Formula
   url "https://github.com/google/leveldb/archive/1.23.tar.gz"
   sha256 "9a37f8a6174f09bd622bc723b55881dc541cd50747cbd08831c2a82d620f6d76"
   license "BSD-3-Clause"
+  revision 1
 
   bottle do
     sha256 cellar: :any,                 arm64_monterey: "41f1ceea1e85dafe0552acf94a4903ef45fd54fc63e38c277b7d101c836e5801"
@@ -16,7 +17,6 @@ class Leveldb < Formula
   end
 
   depends_on "cmake" => :build
-  depends_on "gperftools"
   depends_on "snappy"
 
   def install
