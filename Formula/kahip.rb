c87a6f9e78a4338225ef9f70197ffc789252da56
Guanyang Xue <12702149+BrushXue@users.noreply.github.com>
2021-11-04 12:09:46 -0400

    kahip: fix building issue when metis is present
    Adding metis dependency so the building result can be consistent.
    No longer requires gcc to build.
    Bump to v3.12.

--- Formula/kahip.rb
+++ Formula/kahip.rb
@@ -1,8 +1,8 @@
 class Kahip < Formula
   desc "Karlsruhe High Quality Partitioning"
   homepage "https://algo2.iti.kit.edu/documents/kahip/index.html"
-  url "https://github.com/KaHIP/KaHIP/archive/v3.11.tar.gz"
-  sha256 "347575d48c306b92ab6e47c13fa570e1af1e210255f470e6aa12c2509a8c13e3"
+  url "https://github.com/KaHIP/KaHIP/archive/v3.12.tar.gz"
+  sha256 "df923b94b552772d58b4c1f359b3f2e4a05f7f26ab4ebd00a0ab7d2579f4c257"
   license "MIT"
   head "https://github.com/KaHIP/KaHIP.git"
 
@@ -16,19 +16,10 @@ class Kahip < Formula
   end
 
   depends_on "cmake" => :build
+  depends_on "metis"
   depends_on "open-mpi"
 
-  on_macos do
-    depends_on "gcc"
-  end
-
   def install
-    if OS.mac?
-      gcc_major_ver = Formula["gcc"].any_installed_version.major
-      ENV["CC"] = Formula["gcc"].opt_bin/"gcc-#{gcc_major_ver}"
-      ENV["CXX"] = Formula["gcc"].opt_bin/"g++-#{gcc_major_ver}"
-    end
-
     mkdir "build" do
       system "cmake", *std_cmake_args, ".."
       system "make", "install"
