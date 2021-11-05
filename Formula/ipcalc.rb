a6f08911cb87fef2315823a3c5a6da0e76eda960
rui <rui@chenrui.dev>
2021-11-04 23:27:43 -0400

    use default livecheck and add license

--- Formula/ipcalc.rb
+++ Formula/ipcalc.rb
@@ -3,11 +3,7 @@ class Ipcalc < Formula
   homepage "http://jodies.de/ipcalc"
   url "https://github.com/kjokjo/ipcalc/archive/refs/tags/0.51.tar.gz"
   sha256 "a4dbfaeb7511b81830793ab9936bae9d7b1b561ad33e29106faaaf97ba1c117e"
-
-  livecheck do
-    url "http://jodies.de/ipcalc-archive/"
-    regex(/href=.*?ipcalc[._-]v?(\d+(?:\.\d+)+)\.t/i)
-  end
+  license "GPL-2.0-or-later"
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "fdb1f6c264ecbd824e0a514704000f46825e6c092487cdd467c964f12d3bf960"
