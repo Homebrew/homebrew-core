c8aba3bd195b89f7b6feebc1a8dd4a71f5fac6c3
Rui Chen <rui@chenrui.dev>
2021-11-04 20:48:15 -0400

    libjson-rpc-cpp: revision bump (jsoncpp 1.9.5)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/libjson-rpc-cpp.rb
+++ Formula/libjson-rpc-cpp.rb
@@ -4,6 +4,7 @@ class LibjsonRpcCpp < Formula
   url "https://github.com/cinemast/libjson-rpc-cpp/archive/v1.4.0.tar.gz"
   sha256 "8fef7628eadbc0271c685310082ef4c47f1577c3df2e4c8bd582613d1bd10599"
   license "MIT"
+  revision 1
   head "https://github.com/cinemast/libjson-rpc-cpp.git"
 
   bottle do
