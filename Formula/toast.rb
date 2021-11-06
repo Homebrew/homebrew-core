460f57bf3d7da1b84f8cbe4c99ef974bcda035a0
rui <rui@chenrui.dev>
2021-11-06 02:18:43 -0400

    toast: add head build

--- Formula/toast.rb
+++ Formula/toast.rb
@@ -4,6 +4,7 @@ class Toast < Formula
   url "https://github.com/stepchowfun/toast/archive/v0.45.1.tar.gz"
   sha256 "d71eea4b8aa4b8ef573f8d49e5c39874adbbd3a86837310408ef05807bcf2609"
   license "MIT"
+  head "https://github.com/stepchowfun/toast.git", branch: "main"
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_monterey: "cfd6f72e504105e9ce168cedbe4b9bded961a8835a0bc5b4c33fbab50718c62a"
