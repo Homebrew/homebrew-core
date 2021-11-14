74f65c1689665c336b617b10a9c2131578ad8f03
nitrocode <nitrocode@users.noreply.github.com>
2021-11-14 12:29:22 -0600

    Add new homepage and license

--- Formula/session-manager-plugin.rb
+++ Formula/session-manager-plugin.rb
@@ -4,14 +4,17 @@ class SessionManagerPlugin < Formula
 
   url "https://s3.amazonaws.com/session-manager-downloads/plugin/#{version}/mac/session-manager-plugin.pkg",
       verified: "s3.amazonaws.com/session-manager-downloads/"
+  license "Apache-2.0"
   name "Session Manager Plugin for the AWS CLI"
   desc "Plugin for AWS CLI to start and end sessions that connect to managed instances"
-  homepage "https://docs.aws.amazon.com/systems-manager/latest/userguide/session-manager-working-with-install-plugin.html"
+  homepage "https://github.com/aws/session-manager-plugin"
 
-  livecheck do
-    url :homepage
-    regex(%r{<td>\s*v?(\d+(?:\.\d+)+)\s*</td>}i)
-  end
+  # homepage "https://docs.aws.amazon.com/systems-manager/latest/userguide/session-manager-working-with-install-plugin.html"
+
+  # livecheck do
+  #   url :homepage
+  #   regex(%r{<td>\s*v?(\d+(?:\.\d+)+)\s*</td>}i)
+  # end
 
   pkg "session-manager-plugin.pkg"
   binary "/usr/local/sessionmanagerplugin/bin/session-manager-plugin"
