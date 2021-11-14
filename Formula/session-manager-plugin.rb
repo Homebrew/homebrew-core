848f5ae9d0c739beb60e0e090b2352551149ffa8
nitrocode <nitrocode@users.noreply.github.com>
2021-11-14 12:35:43 -0600

    Update session-manager-plugin.rb

--- Formula/session-manager-plugin.rb
+++ Formula/session-manager-plugin.rb
@@ -2,11 +2,11 @@ class SessionManagerPlugin < Formula
   version "1.2.279.0"
   sha256 "8f14d12aa528d27ba787b20e7ea8492e07cc80321de51843ecd44436dcd64dc1"
 
+  desc "Plugin for AWS CLI to start and end sessions that connect to managed instances"
   url "https://s3.amazonaws.com/session-manager-downloads/plugin/#{version}/mac/session-manager-plugin.pkg",
       verified: "s3.amazonaws.com/session-manager-downloads/"
   license "Apache-2.0"
   name "Session Manager Plugin for the AWS CLI"
-  desc "Plugin for AWS CLI to start and end sessions that connect to managed instances"
   homepage "https://github.com/aws/session-manager-plugin"
 
   # homepage "https://docs.aws.amazon.com/systems-manager/latest/userguide/session-manager-working-with-install-plugin.html"
@@ -16,8 +16,8 @@ class SessionManagerPlugin < Formula
   #   regex(%r{<td>\s*v?(\d+(?:\.\d+)+)\s*</td>}i)
   # end
 
-  pkg "session-manager-plugin.pkg"
-  binary "/usr/local/sessionmanagerplugin/bin/session-manager-plugin"
+  # pkg "session-manager-plugin.pkg"
+  # binary "/usr/local/sessionmanagerplugin/bin/session-manager-plugin"
 
-  uninstall pkgutil: "session-manager-plugin"
+  # uninstall pkgutil: "session-manager-plugin"
 end
