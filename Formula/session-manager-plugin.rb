cca4443f229391e4d1ed743d8463d177f374e9a7
nitrocode <nitrocode@users.noreply.github.com>
2021-11-14 12:40:30 -0600

    Update session-manager-plugin.rb

--- Formula/session-manager-plugin.rb
+++ Formula/session-manager-plugin.rb
@@ -1,12 +1,9 @@
 class SessionManagerPlugin < Formula
-  version "1.2.279.0"
-  sha256 "8f14d12aa528d27ba787b20e7ea8492e07cc80321de51843ecd44436dcd64dc1"
-
   desc "Plugin for AWS CLI to start and end sessions that connect to managed instances"
-  url "https://s3.amazonaws.com/session-manager-downloads/plugin/#{version}/mac/session-manager-plugin.pkg",
-      verified: "s3.amazonaws.com/session-manager-downloads/"
-  license "Apache-2.0"
   homepage "https://github.com/aws/session-manager-plugin"
+  url "https://github.com/aws/session-manager-plugin/archive/refs/tags/1.2.279.0.tar.gz"
+  sha256 "8f14d12aa528d27ba787b20e7ea8492e07cc80321de51843ecd44436dcd64dc1"
+  license "Apache-2.0"
 
   depends_on "go" => :build
 
