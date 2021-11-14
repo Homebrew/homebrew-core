53f603d7ccf903dd3ae06d7984f1c5a33b937a8b
nitrocode <nitrocode@users.noreply.github.com>
2021-11-14 16:25:02 -0600

    Update session-manager-plugin.rb

--- Formula/session-manager-plugin.rb
+++ Formula/session-manager-plugin.rb
@@ -2,17 +2,24 @@ class SessionManagerPlugin < Formula
   desc "Plugin for AWS CLI to start and end sessions that connect to managed instances"
   homepage "https://github.com/aws/session-manager-plugin"
   url "https://github.com/aws/session-manager-plugin/archive/refs/tags/1.2.279.0.tar.gz"
-  sha256 "8f14d12aa528d27ba787b20e7ea8492e07cc80321de51843ecd44436dcd64dc1"
   license "Apache-2.0"
+  sha256 "50ea46e00063d21cfa87ecfc95f03055acb86587ac006bad165541290b8da875"
 
   depends_on "go" => :build
 
   def install
     ENV["GOPATH"] = buildpath
     ENV["GO111MODULE"] = "auto"
-    Language::Go.stage_deps resources, buildpath/"src"
 
-    system "go", "build", "-o", "session-manager-plugin"
-    bin.install "session-manager-plugin"
+    # open source, but the code base does not compile
+    # gofmt -w src && go mod init || true && go mod vendor && make clean release
+    system "gofmt", "-w", "src/"
+    system "make", "release"
+    # system "go", "build", "-o", "session-manager-plugin"
+    # bin.install "session-manager-plugin"
+  end
+
+  test do
+    system bin/"session-manager-plugin"
   end
 end
