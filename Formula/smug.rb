30a1ca601fc69accf6d75dfc70471db6c046a582
Jonathan Dahan <git@jonathan.is>
2021-11-15 11:12:40 -0500

    smug 0.2.5 (new formula)

---
+++ Formula/smug.rb
@@ -0,0 +1,17 @@
+class Smug < Formula
+  desc "Automate your tmux workflow"
+  homepage "https://github.com/ivaaaan/smug"
+  url "https://github.com/ivaaaan/smug/archive/refs/tags/v0.2.5.tar.gz"
+  sha256 "314711f616ea71613fe883fe579e7d3bc0051f540f27c7925a1ce6a0ece69378"
+  license "MIT"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args, "-ldflags", "-X main.version=#{version}"
+  end
+
+  test do
+    assert_equal(version, shell_output("smug").lines.first.split("Version").last.chomp)
+  end
+end
