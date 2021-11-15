0478237a1d112f4969eec5ba40fada9d0bdd13b6
Rui Chen <rui@chenrui.dev>
2021-11-14 21:44:47 -0500

    radare2 5.5.0

--- Formula/radare2.rb
+++ Formula/radare2.rb
@@ -1,8 +1,8 @@
 class Radare2 < Formula
   desc "Reverse engineering framework"
   homepage "https://radare.org"
-  url "https://github.com/radareorg/radare2/archive/5.4.2.tar.gz"
-  sha256 "d3c337e893d7d1e7d5af8b527af3d4469c92898f0249f1b6263ea3325c9455b9"
+  url "https://github.com/radareorg/radare2/archive/5.5.0.tar.gz"
+  sha256 "48519380f4b1bd3e32e2da776e5b8babbcb34e86dc1bad7bbde430da0d768f44"
   license "LGPL-3.0-only"
   head "https://github.com/radareorg/radare2.git", branch: "master"
 
