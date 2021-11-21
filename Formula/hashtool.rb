b0e750b5e7883447b9aabcc24946b8bd06efeaef
FireLite4 <78030025+FireLite4@users.noreply.github.com>
2021-11-21 10:51:02 +0100

    Added HashTool
    Added the formula HashTool, a simple python script to hash any string input.i

--- Formula/hashtool.rb
+++ Formula/hashtool.rb
@@ -1,6 +1,3 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Hashtool < Formula
   desc "Hash strings from the command line"
   homepage "https://firelite4.github.io/HashTool/"
@@ -8,11 +5,12 @@ class Hashtool < Formula
   version "1.0"
   sha256 "257865c04bc32329338738c6d65962b628d5d5fa7828cd42747bad61f2f06aaa"
   license "Unlicense"
-  depends_on "python@3.10"
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "257865c04bc32329338738c6d65962b628d5d5fa7828cd42747bad61f2f06aaa"
   end
+
+  depends_on "python@3.10"
 
   def install
     bin.install "hashtool.py" => "hashtool"
