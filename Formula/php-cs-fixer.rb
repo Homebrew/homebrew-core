b65bdb28f30dfa65f6455aa651c9f780e0e6f605
Alexander M. Turek <me@derrabus.de>
2021-11-25 20:53:18 +0100

    Install PHP CS Fixer from source instead of PHAR

--- Formula/php-cs-fixer.rb
+++ Formula/php-cs-fixer.rb
@@ -1,18 +1,27 @@
 class PhpCsFixer < Formula
   desc "Tool to automatically fix PHP coding standards issues"
   homepage "https://cs.symfony.com/"
-  url "https://github.com/FriendsOfPHP/PHP-CS-Fixer/releases/download/v3.3.2/php-cs-fixer.phar"
-  sha256 "8a7cb6fcbf916f01d4b423b7850b4401bc687275011b6f437322679a4fc4613f"
+  url "https://github.com/FriendsOfPHP/PHP-CS-Fixer/archive/refs/tags/v3.3.2.tar.gz"
+  sha256 "12d4109a589c730f34591862d016eb058432a010e7f125a4f5be7efb5ffff9c3"
   license "MIT"
+  revision 1
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "c9979da8cbb781fd78fd4a79d36a4be3fd0b8e2bbdff1e89b66e930a0452f0eb"
   end
 
+  depends_on "composer" => :build
   depends_on "php"
 
   def install
-    bin.install "php-cs-fixer.phar" => "php-cs-fixer"
+    system "composer", "config", "platform.php", Formula["php"].version.to_s
+    system "composer", "update"
+
+    inreplace "php-cs-fixer", "#!/usr/bin/env php", "#!#{Formula["php"].opt_bin}/php"
+
+    prefix.install Dir["*"]
+
+    bin.install_symlink prefix/"php-cs-fixer"
   end
 
   test do
--- Formula/php-cs-fixer@2.rb
+++ Formula/php-cs-fixer@2.rb
@@ -1,19 +1,28 @@
 class PhpCsFixerAT2 < Formula
   desc "Tool to automatically fix PHP coding standards issues"
   homepage "https://cs.symfony.com/"
-  url "https://github.com/FriendsOfPHP/PHP-CS-Fixer/releases/download/v2.19.3/php-cs-fixer.phar"
-  sha256 "64238c2940e273f6182abe5279fea0df3707ac3d18f30909f0ab4fb6f9018f94"
+  url "https://github.com/FriendsOfPHP/PHP-CS-Fixer/archive/refs/tags/v2.19.3.tar.gz"
+  sha256 "32c63d1c8cb34a9683958721cf2a9eee2f6231e541758641f416091c9bf650aa"
   license "MIT"
+  revision 1
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "62f0ceae3aec334450704706f22b62d66dc966347a4a7c3324566e530746ffea"
   end
 
   keg_only :versioned_formula
+  depends_on "composer" => :build
   depends_on "php"
 
   def install
-    bin.install "php-cs-fixer.phar" => "php-cs-fixer"
+    system "composer", "config", "platform.php", Formula["php"].version.to_s
+    system "composer", "update"
+
+    inreplace "php-cs-fixer", "#!/usr/bin/env php", "#!#{Formula["php"].opt_bin}/php"
+
+    prefix.install Dir["*"]
+
+    bin.install_symlink prefix/"php-cs-fixer"
   end
 
   test do
