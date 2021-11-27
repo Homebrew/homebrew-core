a8360ec72bb52677f61e5dcd2b7859ce66268363
Harel Ben-Attia <harel@coralogix.com>
2021-11-27 11:41:30 +0200

    fixes after second review

--- Formula/q.rb
+++ Formula/q.rb
@@ -1,13 +1,21 @@
-# frozen_string_literal: true
-
 class Q < Formula
   desc "Run SQL directly on CSV or TSV files"
   homepage "https://harelba.github.io/q/"
   url "https://github.com/harelba/q/archive/v3.1.6.tar.gz"
   sha256 "e63ba4ae49647f764c5255ad7065d2c614fdf03a2f7349a795de69529701fab8"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "c72ca06a7c9dbe3b3eaee1b8db72811edbf7e64fbee5b694bcf2ed7e8d877d50"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c72ca06a7c9dbe3b3eaee1b8db72811edbf7e64fbee5b694bcf2ed7e8d877d50"
+    sha256 cellar: :any_skip_relocation, monterey:       "4511c183df36704ec7cb497b4a319409875ea2ef6068255ae2a2e0a2d7293e29"
+    sha256 cellar: :any_skip_relocation, big_sur:        "4511c183df36704ec7cb497b4a319409875ea2ef6068255ae2a2e0a2d7293e29"
+    sha256 cellar: :any_skip_relocation, catalina:       "4511c183df36704ec7cb497b4a319409875ea2ef6068255ae2a2e0a2d7293e29"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "bed14a331133ff96b85fa37e0729ca695bd273f78ee82e792185d137edf9917a"
+  end
+
   license "GPL-3.0-or-later"
 
+  depends_on arch: :x86_64
   depends_on "pyoxidizer" => :build
   depends_on "python@3.8" => :build
   depends_on "ronn" => :build
