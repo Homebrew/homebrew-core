4a33a3a81c62aaeb356c5c00bf0d6b0519f19d03
Rui Chen <rui@chenrui.dev>
2021-11-28 09:04:13 -1000

    pumba 0.9.0

--- Formula/pumba.rb
+++ Formula/pumba.rb
@@ -1,8 +1,8 @@
 class Pumba < Formula
   desc "Chaos testing tool for Docker"
   homepage "https://github.com/alexei-led/pumba"
-  url "https://github.com/alexei-led/pumba/archive/0.8.0.tar.gz"
-  sha256 "052ece6984a0533d7f93b2b64c66d5e89516bbf93e4cb732a2743322b4eef9da"
+  url "https://github.com/alexei-led/pumba/archive/0.9.0.tar.gz"
+  sha256 "7faa50566898a53b0fff81973e7161874eabec45ad11f9defcd0e04310bddaff"
   license "Apache-2.0"
   head "https://github.com/alexei-led/pumba.git", branch: "master"
 
