5a3dab1611ce49e7009d33a1df5b72a24525a684
Dustin Rodrigues <dust.rod@gmail.com>
2021-11-29 17:56:46 -0500

    homeassistant-cli: migrate to python 3.10

--- Formula/homeassistant-cli.rb
+++ Formula/homeassistant-cli.rb
@@ -6,7 +6,7 @@ class HomeassistantCli < Formula
   url "https://files.pythonhosted.org/packages/f0/f5/a90000b810751a6094761ed5fed3a6cb746c3c8be3bd1c5ed525e77be69a/homeassistant-cli-0.9.3.tar.gz"
   sha256 "daf9c2a256cd2e63fc173c7c96b3462211f045a66639778302eb4f9d125b06a2"
   license "Apache-2.0"
-  revision 1
+  revision 2
   head "https://github.com/home-assistant-ecosystem/home-assistant-cli.git"
 
   bottle do
@@ -20,7 +20,7 @@ class HomeassistantCli < Formula
   end
 
   depends_on "python-tabulate"
-  depends_on "python@3.9"
+  depends_on "python@3.10"
 
   resource "aiohttp" do
     url "https://files.pythonhosted.org/packages/99/f5/90ede947a3ce2d6de1614799f5fea4e93c19b6520a59dc5d2f64123b032f/aiohttp-3.7.4.post0.tar.gz"
