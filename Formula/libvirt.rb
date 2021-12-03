ba560c2f801060e699a0fa9d24be2fffd831634f
Andrea Bolognani <eof@kiyuko.org>
2021-12-03 12:22:52 +0100

    libvirt: Tweak URLs
    The canonical URL to the libvirt homepage doesn't contain the
    www part, and in fact if you try using that version you will
    get immediately redirected.
    
    The primary git hosting for libvirt is GitLab, with GitHub
    only being used as a mirror, so it makes sense for head to
    point to the former.
    
    Signed-off-by: Andrea Bolognani <eof@kiyuko.org>

--- Formula/libvirt.rb
+++ Formula/libvirt.rb
@@ -1,10 +1,10 @@
 class Libvirt < Formula
   desc "C virtualization API"
-  homepage "https://www.libvirt.org"
+  homepage "https://libvirt.org/"
   url "https://libvirt.org/sources/libvirt-7.10.0.tar.xz"
   sha256 "cb318014af097327928c6e3d72922e3be02a3e6401247b2aa52d9ab8e0b480f9"
   license all_of: ["LGPL-2.1-or-later", "GPL-2.0-or-later"]
-  head "https://github.com/libvirt/libvirt.git", branch: "master"
+  head "https://gitlab.com/libvirt/libvirt.git", branch: "master"
 
   livecheck do
     url "https://libvirt.org/sources/"
