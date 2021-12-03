88bf35023fde7bcf3e19d0d01c6aafebcad3002a
DarthSim <darthsim@gmail.com>
2021-12-03 20:26:02 +0600

    hivemind 1.1.0

--- Formula/hivemind.rb
+++ Formula/hivemind.rb
@@ -1,8 +1,8 @@
 class Hivemind < Formula
   desc "Process manager for Procfile-based applications"
   homepage "https://github.com/DarthSim/hivemind"
-  url "https://github.com/DarthSim/hivemind/archive/v1.0.6.tar.gz"
-  sha256 "8ca7884db49268b7938d0503e7e95443cb3a56696478d5dcc2b9813705525a39"
+  url "https://github.com/DarthSim/hivemind/archive/refs/tags/v1.1.0.tar.gz"
+  sha256 "b4f7259663ef5b99906af0d98fe4b964d8f9a4d86a8f5aff30ab8df305d3a996"
   license "MIT"
   head "https://github.com/DarthSim/hivemind.git"
 
