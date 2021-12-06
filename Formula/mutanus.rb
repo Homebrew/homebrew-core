ba4b0be851fff1b7184c87b007275449a82b9475
Iurii Sorokin <YBSorokin@alfabank.ru>
2021-12-06 20:12:56 +0300

    mutanus 0.1.1

--- Formula/mutanus.rb
+++ Formula/mutanus.rb
@@ -1,15 +1,12 @@
 class Mutanus < Formula
   desc "Performs mutation testing of you Swift project"
   homepage "https://github.com/SoriUR/mutanus/"
-  url "https://github.com/SoriUR/mutanus/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "73204fe4b1788c2427d2d12f8a5d2579469cd8910ecfa777974d431118d83155"
+  url "https://github.com/SoriUR/mutanus/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "a534071a0f6f234cd3dc412afab6e825d54181fd8de0a8cc2a3478f1ba2189a8"
   license "MIT"
 
   depends_on xcode: [">= 12.5", :build]
-  depends_on macos: [
-    :catalina,
-    :big_sur,
-  ]
+  depends_on macos: :catalina
 
   def install
     system "swift", "build", "--disable-sandbox", "-c", "release"
