da91544d0e2d84874ad4959f598c01f29f08c853
jeremynac <jeremynac@hotmail.fr>
2021-12-07 20:36:10 +0100

    panoply 0.1.44
    This panoply, a new CLI where a user can save commands and reuse them later

---
+++ Formula/panoply.rb
@@ -0,0 +1,24 @@
+class Panoply < Formula
+  include Language::Python::Virtualenv
+
+  desc "Save your commands and reuse them"
+  homepage "https://github.com/jeremynac/panoply"
+  url "https://files.pythonhosted.org/packages/d0/59/8b3dfab969eb5f74135d9ade90a1d987993b588e944990d8737737cee62e/panoply-0.1.44.tar.gz"
+  sha256 "827d38f7ddaf6f18920ecd551937fd90ee1ddeafeda6dc440a0cf6d69ba46ab2"
+
+  depends_on "python3"
+
+  resource "simple-term-menu" do
+    url "https://files.pythonhosted.org/packages/de/08/08c19f7653a9d7c73df024090243a8a44583b613f8f1ed51d79cbb5ae70e/simple-term-menu-1.4.1.tar.gz"
+    sha256 "401e7efc6726e195b8e62bfcede51298dc0daf8c5f043a521c8828191b0b012e"
+  end
+
+  def install
+    virtualenv_create(libexec, "python3")
+    virtualenv_install_with_resources
+  end
+
+  test do
+    false
+  end
+end
