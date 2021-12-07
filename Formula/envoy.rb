951e12b4568b03b0ca6d0b695ddd4bcb6e5bd40c
Keith Smiley <keithbsmiley@gmail.com>
2021-12-06 18:20:45 -0800

    Update Formula/envoy.rb
    Co-authored-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>

--- Formula/envoy.rb
+++ Formula/envoy.rb
@@ -7,7 +7,6 @@ class Envoy < Formula
       tag:      "v1.20.1",
       revision: "ea23f47b27464794980c05ab290a3b73d801405e"
   license "Apache-2.0"
-  revision 1
   head "https://github.com/envoyproxy/envoy.git", branch: "main"
 
   # Apple M1/arm64 is pending envoyproxy/envoy#16482
