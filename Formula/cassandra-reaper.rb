0ffc42cf9e3b244a166c1a42723c4830e24a0a66
Thierry Moisan <thierry.moisan@gmail.com>
2021-12-07 10:12:07 -0500

    Update Formula/cassandra-reaper.rb
    Co-authored-by: Sean Molenaar <SMillerDev@users.noreply.github.com>

--- Formula/cassandra-reaper.rb
+++ Formula/cassandra-reaper.rb
@@ -13,7 +13,7 @@ class CassandraReaper < Formula
   depends_on "openjdk@8"
 
   def install
-    inreplace "bin/cassandra-reaper", "/usr/share", prefix if OS.linux?
+    inreplace "bin/cassandra-reaper", "/usr/share", prefix
     prefix.install "bin"
     etc.install "resource" => "cassandra-reaper"
     share.install "server/target" => "cassandra-reaper"
