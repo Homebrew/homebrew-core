cb522475afbe737537e03a4f806f3cacf97db5e6
Rui Chen <rui@Ruis-MacBook-Pro.local>
2021-12-08 02:28:40 -0500

    flux 0.144.0

--- Formula/flux.rb
+++ Formula/flux.rb
@@ -2,8 +2,8 @@ class Flux < Formula
   desc "Lightweight scripting language for querying databases"
   homepage "https://www.influxdata.com/products/flux/"
   url "https://github.com/influxdata/flux.git",
-      tag:      "v0.128.0",
-      revision: "1883bac5ff6ec06b4004a70f5823d42828b82bc3"
+      tag:      "v0.144.0",
+      revision: "eb10cfb089b18e702dd81e3f64a660425c7c6b82"
   license "MIT"
   head "https://github.com/influxdata/flux.git", branch: "master"
 
