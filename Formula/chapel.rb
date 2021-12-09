dd1594fe707180e055e3fb04ad77d046f69030f1
Joe Tursi <joe.p.tursi@gmail.com>
2021-12-09 13:32:40 -0500

    chapel 1.25.1
    -Updated formula to use 1.25.1 tar
    -Added dependency on homebrew gmp
    -Added environment variables to interface homebrew gmp with chapel
    -Removed llvm by default workarounds from 1.25.0 release
    -Removed gmp bundled source for further image size reduction
    -Added testing into brew test for c backend in addition to llvm testing
    -Adding HOMEBREW PREFIX to various locations to make locations more dynamic
    -Reworking LLVM Environment variables to fit with proper homebrew conventions
    -Changed the order of our make calls to make sure our llvm compiler is actually built
    -Added an explicit CHPL_LLVM=system call to our second make
    -Added a patch to our tar to allow for our custom LLVM_CONFIG location
    -Added a custom LLVM_CONFIG location
    
    Signed-off-by: Joe Tursi <joe.p.tursi@gmail.com>

--- Formula/chapel.rb
+++ Formula/chapel.rb
@@ -1,10 +1,9 @@
 class Chapel < Formula
   desc "Programming language for productive parallel computing at scale"
   homepage "https://chapel-lang.org/"
-  url "https://github.com/chapel-lang/chapel/releases/download/1.25.0/chapel-1.25.0.tar.gz"
-  sha256 "39f43fc6de98e3b1dcee9694fdd4abbfb96cc941eff97bbaa86ee8ad88e9349b"
+  url "https://github.com/chapel-lang/chapel/releases/download/1.25.1/chapel-1.25.1.tar.gz"
+  sha256 "0c13d7da5892d0b6642267af605d808eb7dd5d4970766f262f38b94fa2405113"
   license "Apache-2.0"
-  revision 1
 
   bottle do
     sha256 monterey:     "91ee14fe8cdb2e5de1bcd0f84cb159a79ff5cfb310644ec427aca1e992c53301"
@@ -14,8 +13,14 @@ class Chapel < Formula
     sha256 x86_64_linux: "7cf01213c80d41ed1b20451fd9f11b4b651a1b9a603729826a383e73475918b3"
   end
 
+  depends_on "gmp"
   depends_on "llvm@11"
   depends_on "python@3.9"
+
+  patch :p0, :DATA do
+    url "https://github.com/chapel-lang/chapel/commit/acc2a62d0446f6e56fbd6b73db42f9ec025dacac"
+    sha256 "33fa2d0bf963517e330916f334482ec3132df1907034fbb06a7aeec70989c058"
+  end
 
   def install
     libexec.install Dir["*"]
@@ -23,19 +28,21 @@ class Chapel < Formula
     ENV["CHPL_HOME"] = libexec
     # This is for mason
     ENV["CHPL_RE2"] = "bundled"
+    ENV["CHPL_GMP"] = "system"
+    ENV["CHPL_INCLUDE_PATH"] = HOMEBREW_PREFIX/"include"
+    ENV["CHPL_LIB_PATH"] = HOMEBREW_PREFIX/"lib"
+    ENV["CHPL_LLVM_CONFIG"] = HOMEBREW_PREFIX/"opt/llvm@11/bin/llvm-config"
 
     # Must be built from within CHPL_HOME to prevent build bugs.
     # https://github.com/Homebrew/legacy-homebrew/pull/35166
     cd libexec do
       system "./util/printchplenv", "--all"
-      system "make"
-      # Need to let chapel choose target compiler with llvm
-      ENV["CHPL_HOST_CC"] = ENV["CC"]
-      ENV["CHPL_HOST_CXX"] = ENV["CXX"]
-      ENV.delete("CC")
-      ENV.delete("CXX")
-      system "./util/printchplenv", "--all"
-      system "make"
+      with_env(CHPL_LLVM: "none") do
+        system "make"
+      end
+      with_env(CHPL_LLVM: "system") do
+        system "make"
+      end
       system "make", "chpldoc"
       system "make", "mason"
       system "make", "cleanall"
@@ -44,6 +51,7 @@ class Chapel < Formula
       rm_rf("third-party/libfabric/libfabric-src")
       rm_rf("third-party/fltk/fltk-1.3.5-source.tar.gz")
       rm_rf("third-party/libunwind/libunwind-1.1.tar.gz")
+      rm_rf("third-party/gmp/gmp-src/")
     end
 
     # Install chpl and other binaries (e.g. chpldoc) into bin/ as exec scripts.
@@ -60,8 +68,17 @@ class Chapel < Formula
 
   test do
     ENV["CHPL_HOME"] = libexec
+    ENV["CHPL_GMP"] = "system"
+    ENV["CHPL_INCLUDE_PATH"] = HOMEBREW_PREFIX/"include"
+    ENV["CHPL_LIB_PATH"] = HOMEBREW_PREFIX/"lib"
+    ENV["CHPL_LLVM_CONFIG"] = HOMEBREW_PREFIX/"opt/llvm@11/bin/llvm-config"
     cd libexec do
-      system "util/test/checkChplInstall"
+      with_env(CHPL_LLVM: "system") do
+        system "util/test/checkChplInstall"
+      end
+      with_env(CHPL_LLVM: "none") do
+        system "util/test/checkChplInstall"
+      end
     end
     system bin/"chpl", "--print-passes", "--print-commands", libexec/"examples/hello.chpl"
   end
