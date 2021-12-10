9dcd6e2bea57beecbaa85bb8fd347ac5551a7367
Rui Chen <rui@chenrui.dev>
2021-12-10 01:37:56 -0500

    vert.x 4.2.1

--- Formula/vert.x.rb
+++ Formula/vert.x.rb
@@ -1,8 +1,8 @@
 class VertX < Formula
   desc "Toolkit for building reactive applications on the JVM"
   homepage "https://vertx.io/"
-  url "https://search.maven.org/remotecontent?filepath=io/vertx/vertx-stack-manager/4.1.5/vertx-stack-manager-4.1.5-full.tar.gz"
-  sha256 "67b4d6d55ffafae0e499883593b93ac132f6b199fe7c694dc177e81954689cf8"
+  url "https://search.maven.org/remotecontent?filepath=io/vertx/vertx-stack-manager/4.2.1/vertx-stack-manager-4.2.1-full.tar.gz"
+  sha256 "f507191178908d54c512bcf1d4c1ced132697dbfddc54499dcbfc4f93538cec8"
   license any_of: ["EPL-2.0", "Apache-2.0"]
 
   livecheck do
