4f491ee8066087bcc1177ba49ad2ffe32e55d4b5
chenjiandongx <chenjiandongx@qq.com>
2021-12-12 22:39:09 +0800

    remove trailing whitespace

--- Formula/sniffer.rb
+++ Formula/sniffer.rb
@@ -4,15 +4,15 @@ class Sniffer < Formula
   url "https://github.com/chenjiandongx/sniffer/archive/v0.5.1.tar.gz"
   sha256 "b9fbead3f72e31a6599e0dffa6e5f7e09b9a68baf6de69b7313f568988e93c6b"
   license "MIT"
-  
+
   depends_on "go" => :build
-  
+
   uses_from_macos "libpcap"
-  
+
   def install
     system "go", "build", *std_go_args
   end
-  
+
   test do
     system bin/"sniffer", "-l"
   end
