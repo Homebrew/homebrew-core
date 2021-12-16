7bb749a521d93d3dbde74f349dbcc818a862acf4
rui <rui@chenrui.dev>
2021-12-16 13:24:17 -0500

    Update opensearch.rb

--- Formula/opensearch.rb
+++ Formula/opensearch.rb
@@ -3,9 +3,6 @@ class Opensearch < Formula
   homepage "https://github.com/opensearch-project/OpenSearch"
   url "https://github.com/opensearch-project/OpenSearch/archive/1.2.1.tar.gz"
   sha256 "b358d53e33c38324a59a216dd8f24dcf617f891c02311025b44aed1bb8ca8201"
-
-  # url "https://github.com/opensearch-project/OpenSearch/archive/1.2.1.tar.gz"
-  # sha256 "b358d53e33c38324a59a216dd8f24dcf617f891c02311025b44aed1bb8ca8201"
   license "Apache-2.0"
 
   bottle do
