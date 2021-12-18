b6918aac2f1ad02facfc41b9695ce0bf07001717
rui <rui@chenrui.dev>
2021-12-18 14:23:40 -0500

    update the build

--- Formula/kittycad.rb
+++ Formula/kittycad.rb
@@ -4,7 +4,6 @@ class Kittycad < Formula
   url "https://github.com/KittyCAD/cli/archive/refs/tags/v0.0.2.tar.gz"
   sha256 "54b0520d6cb033c5c6ff53fd66062b4eda6c01bacd3af5d9493d0d98f24dee6f"
   license "MIT"
-
   head "https://github.com/kittycad/cli.git", branch: "main"
 
   livecheck do
@@ -19,9 +18,9 @@ class Kittycad < Formula
 
     bin.install "cli" => "kittycad"
     man1.install Dir["generated_docs/man/kittycad*.1"]
-    (bash_completion/"kittycad").write Utils.safe_popen_read(bin/"kittycad", "completion", "-s",  "bash")
-    (fish_completion/"kittycad.fish").write Utils.safe_popen_read(bin/"kittycad", "completion", "-s",  "fish")
-    (zsh_completion/"_kittycad").write Utils.safe_popen_read(bin/"kittycad", "completion", "-s",  "zsh")
+    (bash_completion/"kittycad").write Utils.safe_popen_read(bin/"kittycad", "completion", "-s", "bash")
+    (fish_completion/"kittycad.fish").write Utils.safe_popen_read(bin/"kittycad", "completion", "-s", "fish")
+    (zsh_completion/"_kittycad").write Utils.safe_popen_read(bin/"kittycad", "completion", "-s", "zsh")
   end
 
   test do
