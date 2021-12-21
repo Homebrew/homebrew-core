db3a65752b09f8ac165d8d089a6b333bad43f2c8
Rui Chen <rui@chenrui.dev>
2021-12-20 23:05:25 -0500

    update test
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/checkov.rb
+++ Formula/checkov.rb
@@ -360,7 +360,7 @@ class Checkov < Formula
       }
     EOS
 
-    assert_match "Passed checks: 4, Failed checks: 5, Skipped checks: 0",
+    assert_match "Passed checks: 4, Failed checks: 6, Skipped checks: 0",
       shell_output("#{bin}/checkov -f #{testpath}/test.tf 2>&1", 1)
 
     (testpath/"test2.tf").write <<~EOS
@@ -377,7 +377,7 @@ class Checkov < Formula
         }
       }
     EOS
-    assert_match "Passed checks: 4, Failed checks: 4, Skipped checks: 1",
+    assert_match "Passed checks: 4, Failed checks: 5, Skipped checks: 1",
       shell_output("#{bin}/checkov -f #{testpath}/test2.tf 2>&1", 1)
   end
 end
