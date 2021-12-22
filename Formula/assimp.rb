e25fc927ed7cf47134803e6f50cc0179cc514e1d
rui <rui@chenrui.dev>
2021-12-22 09:55:07 -0500

    use newer gcc

--- Formula/assimp.rb
+++ Formula/assimp.rb
@@ -20,6 +20,12 @@ class Assimp < Formula
 
   uses_from_macos "zlib"
 
+  on_linux do
+    depends_on "gcc"
+  end
+
+  fails_with gcc: "5"
+
   def install
     args = std_cmake_args + %W[
       -GNinja
