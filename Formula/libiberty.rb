9bb10bad365f709f47e5dcfadbcd8e0dde22c763
curoky <cccuroky@gmail.com>
2021-12-29 01:38:21 +0800

    libiberty: update url and test

--- Formula/libiberty.rb
+++ Formula/libiberty.rb
@@ -1,9 +1,9 @@
 class Libiberty < Formula
   desc "Collection of subroutines used by various GNU programs"
   homepage "https://gcc.gnu.org/onlinedocs/libiberty"
-  url "https://ftp.gnu.org/gnu/gcc/gcc-11.1.0/gcc-11.2.1.tar.xz"
-  mirror "https://ftpmirror.gnu.org/gcc/gcc-11.1.0/gcc-11.1.0.tar.xz"
-  sha256 "4c4a6fb8a8396059241c2e674b85b351c26a5d678274007f076957afa1cc9ddf"
+  url "https://ftp.gnu.org/gnu/gcc/gcc-11.2.0/gcc-11.2.0.tar.xz"
+  mirror "https://ftpmirror.gnu.org/gcc/gcc-11.2.0/gcc-11.2.0.tar.xz"
+  sha256 "d08edc536b54c372a1010ff6619dd274c0f1603aa49212ba20f7aa2cda36fa8b"
   license "GPL-3.0-or-later" => { with: "GCC-exception-3.1" }
 
   depends_on "gcc" => :build
@@ -32,7 +32,7 @@ class Libiberty < Formula
       #include <libiberty/demangle.h>
       int main() {
         std::string str(
-            cplus_demangle_v3(typeid(std::string).name(), DMGL_PARAMS | DMGL_ANSI | DMGL_TYPES));
+            cplus_demangle_v3(typeid(int64_t).name(), DMGL_PARAMS | DMGL_ANSI | DMGL_TYPES));
         std::cout << str << std::endl;
         return 0;
       }
@@ -40,7 +40,6 @@ class Libiberty < Formula
 
     system ENV.cxx, "test.cpp", "-I#{opt_include}", "#{opt_lib}/libiberty.a"
 
-    assert_match "std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> >",
-      shell_output("./a.out")
+    assert_match "long", shell_output("./a.out")
   end
 end
