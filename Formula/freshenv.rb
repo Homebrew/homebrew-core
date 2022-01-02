930b8ba25afe75c09f1dc543dae47dfe6d875219
Raiyan Yahya <raiyanyahyadeveloper@gmail.com>
2022-01-02 12:52:17 +0100

    Add 3.7 as the depends on version for python

--- Formula/freshenv.rb
+++ Formula/freshenv.rb
@@ -7,7 +7,7 @@ class Freshenv < Formula
   sha256 "9cbed9143cb904b5fa5c2f226ecd95a9f9b5692cee773b85cfb402020176d273"
   license "MIT"
   
-  depends_on "python3"
+  depends_on "python@3.7"
 
   resource "certifi" do
     url "https://files.pythonhosted.org/packages/6c/ae/d26450834f0acc9e3d1f74508da6df1551ceab6c2ce0766a593362d6d57f/certifi-2021.10.8.tar.gz"
