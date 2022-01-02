80776723721d7436f7bbbe8c8b66ce937821f10d
owine <owine@users.noreply.github.com>
2022-01-02 12:27:12 -0600

    spdlog: Bump revision

--- Formula/spdlog.rb
+++ Formula/spdlog.rb
@@ -5,6 +5,7 @@ class Spdlog < Formula
   sha256 "6fff9215f5cb81760be4cc16d033526d1080427d236e86d70bb02994f85e3d38"
   license "MIT"
   head "https://github.com/gabime/spdlog.git", branch: "v1.x"
+  revision 1
 
   bottle do
     sha256 cellar: :any,                 arm64_monterey: "3322b0afb1b04d54503384ad22c23b0d7f93ef7f949bb1c0623a112df7adbe6d"
