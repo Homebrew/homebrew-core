cc9d177a83a770db26bb4f78c5568f61d85d5190
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-01-03 15:00:11 +0800

    nanoflann: fix build failure on Catalina
    Xcode 12.4 provides Clang 10, which has an unfixed bug:
    
        https://bugs.llvm.org/show_bug.cgi?id=23029
    
    Let's also skip building the examples, since this will allow us to build
    an `:all` bottle.

--- Formula/nanoflann.rb
+++ Formula/nanoflann.rb
@@ -18,8 +18,17 @@ class Nanoflann < Formula
 
   depends_on "cmake" => :build
 
+  on_macos do
+    depends_on "gcc" => [:build, :test] if DevelopmentTools.clang_build_version <= 1200
+  end
+
+  fails_with :clang do
+    build 1200
+    cause "https://bugs.llvm.org/show_bug.cgi?id=23029"
+  end
+
   def install
-    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args, "-DNANOFLANN_BUILD_EXAMPLES=OFF"
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
   end
