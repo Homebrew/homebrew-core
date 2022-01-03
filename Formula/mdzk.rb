4bedc29c45ac4615da379f32d53dbab81c6b9066
Rui Chen <rui@chenrui.dev>
2022-01-03 11:32:11 -0500

    update homepage
    ```
    $ curl -I https://mdzk-rs.github.io/
    HTTP/2 404
    ```
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/mdzk.rb
+++ Formula/mdzk.rb
@@ -1,6 +1,6 @@
 class Mdzk < Formula
   desc "Plain text Zettelkasten based on mdBook"
-  homepage "https://mdzk-rs.github.io"
+  homepage "https://mdzk.app/"
   url "https://github.com/mdzk-rs/mdzk/archive/0.5.0.tar.gz"
   sha256 "f8b72c70cee068896a7786fdffc4c7900085aa2f1f53e973759b829183c9a8e2"
   license "MPL-2.0"
