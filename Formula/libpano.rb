f8a374ee41e3866c8ae7fae52f28b2ef3fc5b424
rui <rui@chenrui.dev>
2022-01-03 21:41:17 -0500

    libpano: add license

--- Formula/libpano.rb
+++ Formula/libpano.rb
@@ -4,6 +4,7 @@ class Libpano < Formula
   url "https://downloads.sourceforge.net/project/panotools/libpano13/libpano13-2.9.21/libpano13-2.9.21.tar.gz"
   version "13-2.9.21"
   sha256 "79e5a1452199305e2961462720ef5941152779c127c5b96fc340d2492e633590"
+  license "GPL-2.0-or-later"
 
   livecheck do
     url :stable
