815f3d37a44e22cc784f544794df4ed0e38f0d0b
Rui Chen <rui@chenrui.dev>
2022-01-04 12:56:12 -0500

    dxflib 3.26.4

--- Formula/dxflib.rb
+++ Formula/dxflib.rb
@@ -1,8 +1,9 @@
 class Dxflib < Formula
   desc "C++ library for parsing DXF files"
   homepage "https://www.ribbonsoft.com/en/what-is-dxflib"
-  url "https://www.ribbonsoft.com/archives/dxflib/dxflib-2.5.0.0-1.src.tar.gz"
-  sha256 "20ad9991eec6b0f7a3cc7c500c044481a32110cdc01b65efa7b20d5ff9caefa9"
+  url "https://www.ribbonsoft.com/archives/dxflib/dxflib-3.26.4-src.tar.gz"
+  sha256 "507db4954b50ac521cbb2086553bf06138dc89f55196a8ba22771959c760915f"
+  license "GPL-2.0-or-later"
 
   livecheck do
     url "https://www.ribbonsoft.com/en/dxflib-downloads"
