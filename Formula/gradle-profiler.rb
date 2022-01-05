2663b21599c3595b6c52cd665543d511b80dc233
Antal János Monori <antal.monori@transferwise.com>
2022-01-05 09:36:57 +0000

    Use search.maven.org instead to make audit pass
    Signed-off-by: Antal János Monori <antal.monori@transferwise.com>

--- Formula/gradle-profiler.rb
+++ Formula/gradle-profiler.rb
@@ -1,13 +1,13 @@
 class GradleProfiler < Formula
   desc "Profiling and benchmarking tool for Gradle builds"
   homepage "https://github.com/gradle/gradle-profiler/"
-  url "https://repo1.maven.org/maven2/org/gradle/profiler/gradle-profiler/0.17.0/gradle-profiler-0.17.0.zip"
+  url "https://search.maven.org/remotecontent?filepath=org/gradle/profiler/gradle-profiler/0.17.0/gradle-profiler-0.17.0.zip"
   sha256 "c71812c0fc79e23d8c9eafc831ba2f016bb7744935912b0aac1a98972dc30abe"
   license "Apache-2.0"
   revision 1
 
   livecheck do
-    url "https://repo1.maven.org/maven2/org/gradle/profiler/gradle-profiler/maven-metadata.xml"
+    url "https://search.maven.org/remotecontent?filepath=org/gradle/profiler/gradle-profiler/maven-metadata.xml"
     regex(%r{<version>\s*v?(\d+(?:\.\d+)+)\s*</version>}i)
   end
 
