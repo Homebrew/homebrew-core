8a61ce8583b53fcc035914e9062ca5b141b45b9b
Rui Chen <rui@chenrui.dev>
2022-01-05 18:52:15 -0500

    netlify-cli 8.6.5

--- Formula/netlify-cli.rb
+++ Formula/netlify-cli.rb
@@ -3,8 +3,8 @@ require "language/node"
 class NetlifyCli < Formula
   desc "Netlify command-line tool"
   homepage "https://www.netlify.com/docs/cli"
-  url "https://registry.npmjs.org/netlify-cli/-/netlify-cli-8.6.0.tgz"
-  sha256 "c669d40cde0eb2da4069a03bf4e355d862b976ee6a3592a8d2d994222c8b8e77"
+  url "https://registry.npmjs.org/netlify-cli/-/netlify-cli-8.6.5.tgz"
+  sha256 "57b679822d334318fd6789ff22964ef40bdc31216a2d22f46001928ff65a7c2e"
   license "MIT"
   head "https://github.com/netlify/cli.git", branch: "main"
 
