7e66e270ad72a0c9838725a09fe480b38a6194fc
Porkepix <porkepix@gmail.com>
2022-01-07 15:25:39 +0100

    ii 1.9

--- Formula/ii.rb
+++ Formula/ii.rb
@@ -1,8 +1,8 @@
 class Ii < Formula
   desc "Minimalist IRC client"
   homepage "https://tools.suckless.org/ii/"
-  url "https://dl.suckless.org/tools/ii-1.8.tar.gz"
-  sha256 "b9d9e1eae25e63071960e921af8b217ab1abe64210bd290994aca178a8dc68d2"
+  url "https://dl.suckless.org/tools/ii-1.9.tar.gz"
+  sha256 "850cb323b583d261b51bda9993ee733334352a8e6ca1e2f02b57c154bf568c17"
   license "MIT"
   head "https://git.suckless.org/ii", using: :git, branch: "master"
 
