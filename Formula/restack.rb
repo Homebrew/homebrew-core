ac7fc5639b8ebde2cc2f5eb682c50cc0f236b6fd
leawn <leonrubner2809@gmail.com>
2022-01-11 00:32:00 +0100

    Restack CLI initial commit

--- Formula/restack.rb
+++ Formula/restack.rb
@@ -1,12 +1,10 @@
-require "formula"
-
 class Restack < Formula
-  desc "Restack CLI app"
+  desc "CLI app"
   homepage "https://github.com/restackio/homebrew-restack"
   url "https://github.com/restackio/cli-bin/archive/refs/tags/v0.1.0.tar.gz"
   sha256 "18bfe33bd8bd429b20912e049c3d5a7dbb0b4f8498fe2394a2680c8c705a6d13"
-  head "https://github.com/restackio/cli-bin.git", branch: "v0.1.0"
   license "Apache-2.0"
+  head "https://github.com/restackio/cli-bin.git", branch: "v0.1.0"
 
   def install
     bin.install "restack"
