9941ad53e18a6e7ed5fa5bc48ecbb8b4eb75b844
leawn <leonrubner2809@gmail.com>
2022-01-11 00:42:07 +0100

    fix sha256 commit

--- Formula/restack.rb
+++ Formula/restack.rb
@@ -2,7 +2,7 @@ class Restack < Formula
   desc "CLI app"
   homepage "https://github.com/restackio/homebrew-restack"
   url "https://github.com/restackio/cli-bin/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "18bfe33bd8bd429b20912e049c3d5a7dbb0b4f8498fe2394a2680c8c705a6d13"
+  sha256 "417463c85f93594e158a72e0c08362b079d2166fbd663a168e78423eee4e7edb"
   license "Apache-2.0"
   head "https://github.com/restackio/cli-bin.git", branch: "v0.1.0"
 
