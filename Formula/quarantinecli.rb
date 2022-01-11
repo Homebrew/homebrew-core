7a2ebd5fd4e67646858baf176d365f8919aeccc1
Dabezt <osama.id78@gmail.com>
2022-01-11 22:19:43 +0300

    Restructure quarantine.rb a bit more because audit keeps complaining

--- Formula/quarantinecli.rb
+++ Formula/quarantinecli.rb
@@ -1,13 +1,13 @@
 class Quarantinecli < Formula
-  desc "A CLI Utility to quarantine and manage quarantined files on macOS"
+  desc "CLI Utility to quarantine and manage quarantined files on macOS"
   homepage "https://github.com/Serena-io/quarantineCLI"
   url "https://github.com/Serena-io/quarantineCLI/archive/refs/tags/1.0.0.tar.gz"
   version "1.0.0"
   sha256 "69ab41c71ed37c5b47e7dc5ed54bde28f15eaf38c81d804f768f74fb482a28e6"
   license "MIT"
 
-  depends_on :xcode => "12.3"
   depends_on :macos
+  depends_on :xcode => "12.3"
 
   def install
     system "swift", "build", "--disable-sandbox", "-c", "release"
@@ -15,7 +15,7 @@ class Quarantinecli < Formula
   end
 
   test do
-    system "touch", "foo"
-    system "${bin}/quarantinecli", "--status", "foo"
+    FileUtils.touch("foo")
+    system "#{bin}/quarantinecli", "--quarantine", "foo"
   end
 end
