3b92fa1dc66f45088dcc5ed76bd7324a04d0be55
Serena <48022799+Serena-io@users.noreply.github.com>
2022-01-11 22:39:04 +0300

    Update Formula/quarantinecli.rb
    Co-authored-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>

--- Formula/quarantinecli.rb
+++ Formula/quarantinecli.rb
@@ -2,7 +2,6 @@ class Quarantinecli < Formula
   desc "CLI Utility to quarantine and manage quarantined files on macOS"
   homepage "https://github.com/Serena-io/quarantineCLI"
   url "https://github.com/Serena-io/quarantineCLI/archive/refs/tags/1.0.0.tar.gz"
-  version "1.0.0"
   sha256 "69ab41c71ed37c5b47e7dc5ed54bde28f15eaf38c81d804f768f74fb482a28e6"
   license "MIT"
 
