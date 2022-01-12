1ebb11862459008e5b93cacc79b13650a1dad9f7
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-01-12 22:30:04 +0800

    openimageio: use Python 3.9
    Trying to use Python 3.10 will make this fail the audit.

--- Formula/openimageio.rb
+++ Formula/openimageio.rb
@@ -37,7 +37,7 @@ class Openimageio < Formula
   depends_on "opencolorio"
   depends_on "openexr"
   depends_on "pybind11"
-  depends_on "python@3.10"
+  depends_on "python@3.9"
   depends_on "webp"
 
   def install
@@ -55,18 +55,19 @@ class Openimageio < Formula
     ]
 
     # CMake picks up the system's python shared library, even if we have a brewed one.
-    py3ver = Language::Python.major_minor_version Formula["python@3.10"].opt_bin/"python3"
+    py3ver = Language::Python.major_minor_version Formula["python@3.9"].opt_bin/"python3"
     py3prefix = if OS.mac?
-      Formula["python@3.10"].opt_frameworks/"Python.framework/Versions/#{py3ver}"
+      Formula["python@3.9"].opt_frameworks/"Python.framework/Versions/#{py3ver}"
     else
-      Formula["python@3.10"].opt_prefix
+      Formula["python@3.9"].opt_prefix
     end
 
     ENV["PYTHONPATH"] = lib/"python#{py3ver}/site-packages"
 
-    args << "-DPYTHON_EXECUTABLE=#{py3prefix}/bin/python3"
+    args << "-DPython_EXECUTABLE=#{py3prefix}/bin/python3"
     args << "-DPYTHON_LIBRARY=#{py3prefix}/lib/#{shared_library("libpython#{py3ver}")}"
     args << "-DPYTHON_INCLUDE_DIR=#{py3prefix}/include/python#{py3ver}"
+    args << "-DPYTHON_VERSION=#{py3ver}"
 
     mkdir "build" do
       system "cmake", "..", *args
@@ -84,6 +85,6 @@ class Openimageio < Formula
       import OpenImageIO
       print(OpenImageIO.VERSION_STRING)
     EOS
-    assert_match version.major_minor_patch.to_s, pipe_output(Formula["python@3.10"].opt_bin/"python3", output, 0)
+    assert_match version.major_minor_patch.to_s, pipe_output(Formula["python@3.9"].opt_bin/"python3", output, 0)
   end
 end
