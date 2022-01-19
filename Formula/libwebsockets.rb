fd529a79712d714d393cd85dc24f5cfb7c95625a
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-01-19 20:51:28 +0800

    libwebsockets: use a tarball instead of a Git clone
    Also, let's use more modern `cmake` syntax.

--- Formula/libwebsockets.rb
+++ Formula/libwebsockets.rb
@@ -1,14 +1,13 @@
 class Libwebsockets < Formula
   desc "C websockets server library"
   homepage "https://libwebsockets.org"
-  url "https://github.com/warmcat/libwebsockets.git",
-      tag:      "v4.3.1",
-      revision: "91f0b3bc0f6530f74e4961b9ae49fba6bee8f7b6"
+  url "https://github.com/warmcat/libwebsockets/archive/v4.3.1.tar.gz"
+  sha256 "8fdb1454a1b34cd9a6351beaab237a485e6853806101de7e62bd2bc250bb50af"
   license "MIT"
   head "https://github.com/warmcat/libwebsockets.git", branch: "main"
 
   livecheck do
-    url "https://github.com/warmcat/libwebsockets"
+    url :stable
     regex(/^v?(\d+(?:\.\d+)+)$/i)
   end
 
@@ -30,16 +29,17 @@ class Libwebsockets < Formula
   uses_from_macos "zlib"
 
   def install
-    system "cmake", ".", *std_cmake_args,
+    system "cmake", "-S", ".", "-B", "build",
                     "-DLWS_IPV6=ON",
                     "-DLWS_WITH_HTTP2=ON",
                     "-DLWS_WITH_LIBEVENT=ON",
                     "-DLWS_WITH_LIBUV=ON",
                     "-DLWS_WITH_PLUGINS=ON",
                     "-DLWS_WITHOUT_TESTAPPS=ON",
-                    "-DLWS_UNIX_SOCK=ON"
-    system "make"
-    system "make", "install"
+                    "-DLWS_UNIX_SOCK=ON",
+                    *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
   end
 
   test do
