996b7ced5caccf5ec261d96becd20e8ddc8a3f4d
realbigsean <sean@sigmaprime.io>
2022-01-21 09:10:22 -0500

    Update lighthouse.rb

--- Formula/lighthouse.rb
+++ Formula/lighthouse.rb
@@ -16,11 +16,13 @@ class Lighthouse < Formula
 
   depends_on "cmake" => :build
   depends_on "rust" => :build
+  
+  uses_from_macos "zlib"
+  
   on_linux do
     depends_on "llvm" => :build
   end
 
-  uses_from_macos "zlib"
 
   def install
     system "cargo", "install", *std_cargo_args(path: "./lighthouse")
