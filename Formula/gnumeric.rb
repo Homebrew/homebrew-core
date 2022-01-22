3ebf62e391c3b12a9745260eb7c5f4e90d43bdc4
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-01-23 00:03:02 +0800

    gnumeric: patch incorrect -flat_namespace usage
    This is needed for bottling on Monterey.

--- Formula/gnumeric.rb
+++ Formula/gnumeric.rb
@@ -33,6 +33,12 @@ class Gnumeric < Formula
     end
   end
 
+  # Fix -flat_namespace being used on Big Sur and later.
+  patch do
+    url "https://raw.githubusercontent.com/Homebrew/formula-patches/03cf8088210822aa2c1ab544ed58ea04c897d9c4/libtool/configure-big_sur.diff"
+    sha256 "35acd6aebc19843f1a2b3a63e880baceb0f5278ab1ace661e57a502d9d78c93c"
+  end
+
   def install
     if OS.linux?
       ENV.prepend_create_path "PERL5LIB", libexec/"lib/perl5"
