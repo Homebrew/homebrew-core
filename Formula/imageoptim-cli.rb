6e53a016bfda69db511353106f2fc10256679fb8
Alexander Ronald Altman <alexanderaltman@me.com>
2022-01-25 18:52:18 -0800

    Update imageoptim-cli.rb

--- Formula/imageoptim-cli.rb
+++ Formula/imageoptim-cli.rb
@@ -23,6 +23,7 @@ class ImageoptimCli < Formula
   depends_on "node" => :build
   depends_on "yarn" => :build
   depends_on arch: :x86_64 # Installs pre-built x86-64 binaries
+  depends_on :macos
 
   def install
     Language::Node.setup_npm_environment
