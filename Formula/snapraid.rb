ce1edba6a6f61c1d911da5f38b98fb1490ac1454
rui <rui@chenrui.dev>
2022-01-26 12:06:40 -0500

    update homepage
    Co-authored-by: Michael Cho <cho-m@tuta.io>

--- Formula/snapraid.rb
+++ Formula/snapraid.rb
@@ -1,6 +1,6 @@
 class Snapraid < Formula
   desc "Backup program for disk arrays"
-  homepage "https://snapraid.sourceforge.io/"
+  homepage "https://www.snapraid.it/"
   url "https://github.com/amadvance/snapraid/releases/download/v12.1/snapraid-12.1.tar.gz"
   sha256 "49337d9bafa96c2beac0125463bd22622be2fc00f3b4dee7e4b0e864d2a49661"
   license "GPL-3.0"
