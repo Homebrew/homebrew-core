9dfc3bc878ded090858daf0a549fd37c5ad68d00
Aladdin Koraiem <alaadahmed@gmail.com>
2022-01-26 07:32:45 +0200

    erlang 24.2.1

--- Formula/erlang.rb
+++ Formula/erlang.rb
@@ -2,8 +2,8 @@ class Erlang < Formula
   desc "Programming language for highly scalable real-time systems"
   homepage "https://www.erlang.org/"
   # Download tarball from GitHub; it is served faster than the official tarball.
-  url "https://github.com/erlang/otp/releases/download/OTP-24.2/otp_src_24.2.tar.gz"
-  sha256 "af0f1928dcd16cd5746feeca8325811865578bf1a110a443d353ea3e509e6d41"
+  url "https://github.com/erlang/otp/releases/download/OTP-24.2.1/otp_src_24.2.1.tar.gz"
+  sha256 "380a77aef34ad449bf8370a380b7901364b9be23e3d25068fc5c43258bcbec11"
   license "Apache-2.0"
 
   livecheck do
