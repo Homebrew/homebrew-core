4575172351fd4312020250984b053524bc8cc8cd
iamchokerman <44473782+iamchokerman@users.noreply.github.com>
2022-01-31 10:22:16 +0100

    Update ani-cli.rb

--- Formula/ani-cli.rb
+++ Formula/ani-cli.rb
@@ -4,14 +4,24 @@ class AniCli < Formula
   sha256 "b53a8ebf7bf44bc7497ad4b4247e941ca9a7a89365717d3f4a2da349bcc80e45"
   url "https://github.com/pystardust/ani-cli/archive/refs/tags/v1.6.tar.gz"
   license "GPL-3.0"
+  
+  resource("testdata") do
+  url "https://github.com/iamchokerman/homebrew-ani-cli/blob/main/testdata"
+  sha256 ""
+end
 
+  uses_from_macos "curl"
   depends_on "grep"
-  depends_on "curl"
-  depends_on "openssl"
   depends_on "aria2"
   depends_on "mpv"
+  depends_on "openssl@3"
+
   
   def install
     bin.install 'ani-cli'
+  end
+    test do
+   resource("testdata").stage do
+    assert_match "OK", shell_output("#{bin}/ani-cli -h")
   end
 end
