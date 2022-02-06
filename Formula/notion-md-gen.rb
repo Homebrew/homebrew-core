bfdcaee6dd98250e99da4932e54764857beb8f40
saltbo <saltbo@foxmail.com>
2022-02-06 23:14:01 +0800

    refactor: update the notion-md-gen.rb version and test

--- Formula/notion-md-gen.rb
+++ Formula/notion-md-gen.rb
@@ -1,20 +1,25 @@
 class NotionMdGen < Formula
   desc "Convert notion content to markdown for static blog"
-  homepage "https://github.com/saltbo/notion-md-gen"
-  url "https://github.com/saltbo/notion-md-gen.git",
-      tag:      "v1.2.0",
-      revision: "6db355b7e63ba8b0b745989f84ff42b8c65f4637"
+  homepage "https://github.com/bonaysoft/notion-md-gen"
+  url "https://github.com/bonaysoft/notion-md-gen.git",
+      tag:      "v1.0.0",
+      revision: "72e0d380c7a6c1066aaf9e2dae551a0a92624cde"
+  license "MIT"
 
   depends_on "go" => :build
 
   def install
-    system "go", "build", "-ldflags",
-      "-s -w -X notion-md-gen/cmd.release=#{version} -X notion-md-gen/cmd.commit=#{Utils.git_head} -X notion-md-gen/cmd.repo=#{stable.url}",
-      *std_go_args,
-      "./main.go"
+    ldflags = %W[
+      -s -w
+      -X notion-md-gen/cmd.release=#{version}
+      -X notion-md-gen/cmd.commit=#{Utils.git_head}
+      -X notion-md-gen/cmd.repo=#{stable.url}
+    ]
+    system "go", "build", *std_go_args(ldflags: ldflags), "./main.go"
   end
 
   test do
     assert_match version.to_s, shell_output("#{bin}/notion-md-gen version 2>&1")
+    assert_match "notion-md-gen.yaml", shell_output("#{bin}/notion-md-gen init 2>&1")
   end
 end
