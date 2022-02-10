b3227770e67000acd2541cccb9d5203c5e57d60c
Dan <dan@wadoku.de>
2022-02-10 07:43:26 +0100

    mariadb: disable bundled readline, add readline as linux dependency

--- Formula/mariadb.rb
+++ Formula/mariadb.rb
@@ -43,6 +43,7 @@ class Mariadb < Formula
   on_linux do
     depends_on "gcc"
     depends_on "linux-pam"
+    depends_on "readline" # uses libedit on macOS
   end
 
   conflicts_with "mysql", "percona-server",
@@ -72,7 +73,6 @@ class Mariadb < Formula
       -DINSTALL_DOCDIR=share/doc/#{name}
       -DINSTALL_INFODIR=share/info
       -DINSTALL_MYSQLSHAREDIR=share/mysql
-      -DWITH_READLINE=yes
       -DWITH_SSL=yes
       -DWITH_UNIT_TESTS=OFF
       -DDEFAULT_CHARSET=utf8mb4
