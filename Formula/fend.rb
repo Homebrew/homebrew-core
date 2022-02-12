d72fe2b79a8d9cffd919e2df29c78f0ea98c78a9
Rhys Davies <rhys@memes.nz>
2022-02-12 16:24:01 +1300

    fend 0.1.28 (new formula)

---
+++ Formula/fend.rb
@@ -0,0 +1,23 @@
+class Fend < Formula
+  desc "Arbitrary-precision unit-aware calculator"
+  homepage "https://printfn.github.io/fend-website"
+  url "https://github.com/printfn/fend/archive/refs/tags/v0.1.28.tar.gz"
+  sha256 "f15a6c8c0da2a888caeb912c0ffba23fe4646a14c0fc4ad5508c4e21f79764dc"
+  license "MIT"
+  head "https://github.com/printfn/fend.git", branch: "main"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args(path: "cli")
+  end
+
+  test do
+    assert_equal "2", shell_output("#{bin}/fend 1+1").strip
+  end
+end
