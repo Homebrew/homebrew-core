8f85b92c155b6b3c2d40ed3b93bfab56ee19679d
David Dao <contact.daviddao@gmail.com>
2022-02-14 02:38:04 -0300

    rumble 1.17.0 (new formula)
    RumbleDB formula installs RumbleDB 1.17.0 and its dependencies (apache-spark). It creates a CLI
    `rumbledb` that is a convenience command line tool for "spark-submit
    rumble.jar" (approved by the RumbleDB team).

---
+++ Formula/rumble.rb
@@ -0,0 +1,20 @@
+class Rumble < Formula
+  desc "RumbleDB 1.17.0 'Cacao tree' for Apache Spark"
+  homepage "https://rumbledb.org/"
+  url "https://github.com/RumbleDB/rumbledb-brew-zip/releases/download/v1.17.0/rumble-brew.zip"
+  sha256 "21ebe235b3ff0e306eea3ce2d28f5186da0edf5c64236f5feb43e12309538d3d"
+  license "MIT"
+
+  depends_on "apache-spark"
+
+  def install
+    lib.install Dir["*"]
+    bin.install Dir[lib/"bin/*"]
+    bin.env_script_all_files(lib/"bin", SPARK_HOME_BIN: Formula["apache-spark"].bin)
+  end
+
+  test do
+    assert_match "2",
+      pipe_output(bin/"rumbledb -q '1+1'")
+  end
+end
