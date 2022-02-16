843ec4c3cc7539788d92907d7fdedc50921d63d0
Viacheslav Kormushkin <Viacheslav.kormushkin@jetbrains.com>
2022-02-16 19:11:33 +0300

    kdoctor 0.0.2 (new formula)

---
+++ Formula/kdoctor.rb
@@ -0,0 +1,25 @@
+class Kdoctor < Formula
+  desc "Environment diagnostics for Kotlin Multiplatform Mobile app development"
+  homepage "https://github.com/kotlin/kdoctor"
+  url "https://github.com/Kotlin/kdoctor/archive/refs/tags/v0.0.2.tar.gz"
+  sha256 "e95af8f76fb3a6240758af76b5528e57b41793065354c81bff2fbebe435f22df"
+  license "Apache-2.0"
+  head "https://github.com/Kotlin/kdoctor.git", branch: "master"
+
+  depends_on "openjdk@11"
+
+  def install
+    ENV["JAVA_HOME"] = Formula["openjdk@11"].opt_prefix
+    mac_suffix = Hardware::CPU.intel? ? "x64" : Hardware::CPU.arch.to_s
+    build_task = "linkReleaseExecutableMacos#{mac_suffix}"
+    system "./gradlew", "clean", build_task
+    binary = "kdoctor/build/bin/macos#{mac_suffix}/releaseExecutable/kdoctor.kexe"
+    bin.install binary => "kdoctor"
+  end
+
+  test do
+    r = /(.|\n)*(System)(.|\n)*(Java)(.|\n)*(Android Studio)(.|\n)*(Xcode)(.|\n)*(Cocoapods)(.|\n)*/
+    assert_match r,
+                 shell_output("#{bin}/kdoctor")
+  end
+end
