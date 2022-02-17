02b787550ae8f73ede2b2d7572a32ff7a88430c2
Dustin Rodrigues <dust.rod@gmail.com>
2022-02-17 08:51:26 -0500

    vue-cli: remove term-size dependency

--- Formula/vue-cli.rb
+++ Formula/vue-cli.rb
@@ -19,7 +19,6 @@ class VueCli < Formula
   depends_on "node"
 
   on_macos do
-    depends_on "macos-term-size"
     depends_on "terminal-notifier"
   end
 
@@ -27,20 +26,11 @@ class VueCli < Formula
     system "npm", "install", *Language::Node.std_npm_install_args(libexec)
     bin.install_symlink Dir["#{libexec}/bin/*"]
 
-    # Remove vendored pre-built binary `term-size`
-    term_size_vendor_dir = libexec/"lib/node_modules/@vue/cli/node_modules/term-size/vendor"
-    term_size_vendor_dir.rmtree # remove pre-built binaries
-
     # Remove vendored pre-built binary `terminal-notifier`
     node_notifier_vendor_dir = libexec/"lib/node_modules/@vue/cli/node_modules/node-notifier/vendor"
     node_notifier_vendor_dir.rmtree # remove vendored pre-built binaries
 
     if OS.mac?
-      macos_dir = term_size_vendor_dir/"macos"
-      macos_dir.mkpath
-      # Replace the vendored pre-built term-size with one we build ourselves
-      ln_sf (Formula["macos-term-size"].opt_bin/"term-size").relative_path_from(macos_dir), macos_dir
-
       terminal_notifier_dir = node_notifier_vendor_dir/"mac.noindex"
       terminal_notifier_dir.mkpath
 
