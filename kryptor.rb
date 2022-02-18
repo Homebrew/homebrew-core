353a50370fea0c2159fdf8ce82caa20fd79bbec8
Samuel Lucas <63159663+samuel-lucas6@users.noreply.github.com>
2022-02-18 20:08:12 +0000

    kryptor.rb: Make framework-dependent.

---
+++ kryptor.rb
@@ -0,0 +1,28 @@
+class Kryptor < Formula
+  desc "Simple, modern, and secure file encryption and signing"
+  homepage "https://www.kryptor.co.uk/"
+  url "https://github.com/samuel-lucas6/Kryptor/archive/v3.1.0.tar.gz"
+  sha256 "e7372ac4ccc4676fa6079bcc7be257c3677003b8ec33df486a4d43ba8bd1f3af"
+  license "GPL-3.0-or-later"
+
+  depends_on "dotnet"
+  depends_on "libsodium"
+
+  def install
+    os = OS.mac? ? "osx" : OS.kernel_name.downcase
+    arch = Hardware::CPU.intel? ? "x64" : Hardware::CPU.arch.to_s
+
+    system "dotnet", "publish", "src/KryptorCLI/KryptorCLI.csproj",
+           "-c", "Release",
+           "-r", "#{os}-#{arch}",
+           "--output", libexec
+
+    env = { DOTNET_ROOT: "${DOTNET_ROOT:-#{Formula["dotnet"].opt_libexec}}" }
+    (bin/"kryptor").write_env_script libexec/"kryptor", env
+  end
+
+  test do
+    system bin/"kryptor", "-e", "-p:'Jet-Visa-Famine-Antivirus'", "test"
+    assert_equal "test", shell_output("#{bin}/kryptor -d -p:'Jet-Visa-Famine-Antivirus' test.kryptor")
+  end
+end
