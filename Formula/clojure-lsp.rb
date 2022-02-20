6ff9dc206476bbdf1e62e2f1bcdb1844b2002f12
Rui Chen <rui@chenrui.dev>
2022-02-20 15:41:57 -0500

    clojure-lsp 20220201200232

--- Formula/clojure-lsp.rb
+++ Formula/clojure-lsp.rb
@@ -2,9 +2,9 @@ class ClojureLsp < Formula
   desc "Language Server (LSP) for Clojure"
   homepage "https://github.com/clojure-lsp/clojure-lsp"
   url "https://github.com/clojure-lsp/clojure-lsp.git",
-      tag:      "2021.09.13-22.25.35",
-      revision: "d564f81e25c71cc370c33d745881af1187f97667"
-  version "20210913T222535"
+      tag:      "2022.02.01-20.02.32",
+      revision: "bf449455d823564cda5e4120a7ebb49f6d06b39e"
+  version "20220201200232"
   license "MIT"
   head "https://github.com/clojure-lsp/clojure-lsp.git", branch: "master"
 
