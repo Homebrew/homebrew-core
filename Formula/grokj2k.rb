f3709af8b5a1a9b4e22f5e3331ddb07c7a5daa5a
Michael Cho <cho-m@tuta.io>
2022-02-22 18:39:32 -0800

    grokj2k 9.7.1

--- Formula/grokj2k.rb
+++ Formula/grokj2k.rb
@@ -1,8 +1,8 @@
 class Grokj2k < Formula
   desc "JPEG 2000 Library"
   homepage "https://github.com/GrokImageCompression/grok"
-  url "https://github.com/GrokImageCompression/grok/archive/v9.2.0.tar.gz"
-  sha256 "624828a4175e549a59aedc2c116479184d4f65247acef91bb79594aa6d9256cc"
+  url "https://github.com/GrokImageCompression/grok/archive/v9.7.1.tar.gz"
+  sha256 "a7d433dca92b035349ef8203eb44cb6d0b2c9b41aecd2d12872a9ca2761e0606"
   license "AGPL-3.0-or-later"
   head "https://github.com/GrokImageCompression/grok.git", branch: "master"
 
