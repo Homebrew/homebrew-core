7b61588f39e8ee7697ce016a2153aa8001c4161e
Banee Ishaque K <baneeishaque@gmail.com>
2022-02-23 22:00:31 +0530

    scrcpy 1.23

--- Formula/scrcpy.rb
+++ Formula/scrcpy.rb
@@ -1,8 +1,8 @@
 class Scrcpy < Formula
   desc "Display and control your Android device"
   homepage "https://github.com/Genymobile/scrcpy"
-  url "https://github.com/Genymobile/scrcpy/archive/v1.22.tar.gz"
-  sha256 "96af955957f354cca78971be0fb54dfbd86a5b520f98a2563d3f0b0a54f4ec5a"
+  url "https://github.com/Genymobile/scrcpy/archive/v1.23.tar.gz"
+  sha256 "47686af76e2314404deda0eea58761ca1378a6a1567408b4560683461c7ea18b"
   license "Apache-2.0"
   revision 1
 
@@ -20,6 +20,7 @@ class Scrcpy < Formula
   depends_on "pkg-config" => :build
   depends_on "ffmpeg@4"
   depends_on "sdl2"
+  depends_on "libusb"
 
   on_linux do
     depends_on "gcc" => :build
@@ -29,8 +30,8 @@ class Scrcpy < Formula
   fails_with gcc: "5"
 
   resource "prebuilt-server" do
-    url "https://github.com/Genymobile/scrcpy/releases/download/v1.22/scrcpy-server-v1.22"
-    sha256 "c05d273eec7533c0e106282e0254cf04e7f5e8f0c2920ca39448865fab2a419b"
+    url "https://github.com/Genymobile/scrcpy/releases/download/v1.23/scrcpy-server-v1.23"
+    sha256 "2a913fd47478c0b306fca507cb0beb625e49a19ff9fc7ab904e36ef5b9fe7e68"
   end
 
   def install
