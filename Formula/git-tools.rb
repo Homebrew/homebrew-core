f5432f30a1c3488c19e73be5957b7d073a5a04c8
Chenxing Luo <chenxing.luo@gmail.com>
2022-02-27 13:55:37 -0500

    git-tools 2020.09 (new formula) Adding new formula git-tools.
    Git Tools is an assorted set of git-related scripts and tools, and it
    is avaliable on Debian, Ubuntu 16.04+, etc.

---
+++ Formula/git-tools.rb
@@ -0,0 +1,19 @@
+class GitTools < Formula
+  desc "Assorted git tools, including git-restore-mtime"
+  homepage "https://github.com/MestreLion/git-tools"
+  url "https://github.com/MestreLion/git-tools/archive/refs/tags/v2020.09.tar.gz"
+  sha256 "55d792bc98150aecebd1e1d62ca9c2349ca28737ae60cd6c1e92185b32cc8c9a"
+  license "GPL-3.0-or-later"
+  head "https://github.com/MestreLion/git-tools.git", branch: "main"
+
+  def install
+    bin.install Dir.glob("git-*")
+    man.mkpath
+    man1.install Dir.glob("man1/*")
+  end
+
+  test do
+    system "git", "init"
+    shell_output("#{bin}/git-restore-mtime")
+  end
+end
