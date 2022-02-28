f5b070e0ffac8ba453472dca2fa3e265c39ce589
Zac Bentley <zbentley@users.noreply.github.com>
2022-02-28 13:45:49 -0500

    Update Formula/bkt.rb
    Co-authored-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/bkt.rb
+++ Formula/bkt.rb
@@ -1,5 +1,5 @@
 class Bkt < Formula
-  desc "Utility for caching the resilts of shell commands"
+  desc "Utility for caching the results of shell commands"
   homepage "https://www.bkt.rs"
   url "https://github.com/dimo414/bkt/archive/refs/tags/0.5.2.tar.gz"
   sha256 "e6acab9ae6a617fe471dceed9f69064e1f0cb3a8eb93d82e2087faeab4d48ee8"
