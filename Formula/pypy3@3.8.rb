96356982916fa9a2bb22ad952d60a3a60b980d8e
Anthony Shaw <anthony.p.shaw@gmail.com>
2022-03-05 19:01:54 +1100

    Correct the version identifiers

--- Formula/pypy3@3.8.rb
+++ Formula/pypy3@3.8.rb
@@ -1,4 +1,4 @@
-class Pypy3 < Formula
+class Pypy3AT38 < Formula
   desc "Implementation of Python 3 in Python"
   homepage "https://pypy.org/"
   url "https://downloads.python.org/pypy/pypy3.8-v7.3.8-src.tar.bz2"
