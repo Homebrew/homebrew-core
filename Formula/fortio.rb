b4362bc7eab727997d3d27806e62e2df928f32f4
Laurent Demailly <ldemailly@roku.com>
2022-03-06 15:27:34 -0800

    make the tmp dir relative to avoid multiple build on same machine stepping on one another

--- Formula/fortio.rb
+++ Formula/fortio.rb
@@ -23,7 +23,7 @@ class Fortio < Formula
   depends_on "go" => :build
 
   def install
-    system "make", "official-build-clean", "official-build-version", "OFFICIAL_BIN=#{bin}/fortio"
+    system "make", "official-build-clean", "official-build-version", "OFFICIAL_BIN=#{bin}/fortio", "BUILD_DIR=./tmp/fortio_build"
   end
 
   test do
