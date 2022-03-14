ab5fe1fa6c1531095d5ed4f545373c7d18f254c2
Michael Cho <cho-m@tuta.io>
2022-03-13 18:24:35 -0700

    feedgnuplot: add perl module dependencies on Linux

--- Formula/feedgnuplot.rb
+++ Formula/feedgnuplot.rb
@@ -19,9 +19,19 @@ class Feedgnuplot < Formula
   uses_from_macos "perl"
 
   on_linux do
+    resource "Exporter::Tiny" do
+      url "https://cpan.metacpan.org/authors/id/T/TO/TOBYINK/Exporter-Tiny-1.002002.tar.gz"
+      sha256 "00f0b95716b18157132c6c118ded8ba31392563d19e490433e9a65382e707101"
+    end
+
     resource "List::MoreUtils" do
       url "https://cpan.metacpan.org/authors/id/R/RE/REHSACK/List-MoreUtils-0.430.tar.gz"
       sha256 "63b1f7842cd42d9b538d1e34e0330de5ff1559e4c2737342506418276f646527"
+    end
+
+    resource "List::MoreUtils::XS" do
+      url "https://cpan.metacpan.org/authors/id/R/RE/REHSACK/List-MoreUtils-XS-0.430.tar.gz"
+      sha256 "e8ce46d57c179eecd8758293e9400ff300aaf20fefe0a9d15b9fe2302b9cb242"
     end
   end
 
