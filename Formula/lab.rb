164e08a31d96f96c880429db76864ce7600b5de5
Porkepix <porkepix@gmail.com>
2022-03-14 15:27:19 +0100

    lab 0.24.0

--- Formula/lab.rb
+++ Formula/lab.rb
@@ -1,8 +1,8 @@
 class Lab < Formula
   desc "Git wrapper for GitLab"
   homepage "https://zaquestion.github.io/lab"
-  url "https://github.com/zaquestion/lab/archive/v0.23.0.tar.gz"
-  sha256 "8f20d5f1931e9b5daa0aa2d30fc3176d82dcca91b368905a1e1c05e2b36254b9"
+  url "https://github.com/zaquestion/lab/archive/v0.24.0.tar.gz"
+  sha256 "e71d9d5c9f1c8872bf4317fd2fbbe7b93d11742522616f855e17574ff02d50b3"
   license "CC0-1.0"
   head "https://github.com/zaquestion/lab.git", branch: "master"
 
