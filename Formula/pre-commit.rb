3cd74fda6cd2870c8eef77d135efa8d8ced61686
Anthony Sottile <asottile@umich.edu>
2022-03-15 13:25:04 -0400

    pre-commit does not depend on six

--- Formula/pre-commit.rb
+++ Formula/pre-commit.rb
@@ -18,7 +18,6 @@ class PreCommit < Formula
 
   depends_on "libyaml"
   depends_on "python@3.10"
-  depends_on "six"
 
   resource "cfgv" do
     url "https://files.pythonhosted.org/packages/c4/bf/d0d622b660d414a47dc7f0d303791a627663f554345b21250e39e7acb48b/cfgv-3.3.1.tar.gz"
