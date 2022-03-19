376d9421c16f10113d13479b3f14ff8f84c49143
Piper McCorkle <contact@piperswe.me>
2022-03-18 19:53:42 -0500

    cloudflared: rename to cloudflare-access and update to 2022.3.3
    A couple things happened in this PR:
     - The inclusion of cloudflared in homebrew-core caused it to be prioritized over our internal Homebrew tap, so internal users were getting out-of-date versions since homebrew-core slightly lags behind our auto-updated internal tap. To fix this, I'm renaming cloudflared to cloudflare-access in homebrew-core. I'm open to suggestions for other names.
     - I added better package manager support upstream - now building and installing is a simple `make install` with a few variables passed in.

---
+++ Formula/cloudflare-access.rb
@@ -0,0 +1,28 @@
+class CloudflareAccess < Formula
+  desc "Client for Cloudflare Access, Tunnel, and other Zero Trust products"
+  homepage "https://developers.cloudflare.com/cloudflare-one/connections/connect-apps/install-and-setup/tunnel-guide"
+  url "https://github.com/cloudflare/cloudflared/archive/refs/tags/2022.3.3.tar.gz"
+  sha256 "da352ea84032f8d1143b239041e8092eff0a35a4966b63cf46d073522dc37672"
+  license "Apache-2.0"
+  head "https://github.com/cloudflare/cloudflared.git", branch: "master"
+
+  depends_on "go" => :build
+
+  def install
+    system "make", "install",
+      "VERSION=#{version}",
+      "DATE=#{time.iso8601}",
+      "PACKAGE_MANAGER=Homebrew",
+      "PREFIX=#{prefix}"
+  end
+
+  test do
+    help_output = shell_output("#{bin}/cloudflared help")
+    assert_match "cloudflared - Cloudflare's command-line tool and agent", help_output
+    assert_match version.to_s, help_output
+    assert_equal "unable to find config file\n", shell_output("#{bin}/cloudflared 2>&1", 1)
+    assert_match "Error locating origin cert", shell_output("#{bin}/cloudflared tunnel run abcd 2>&1", 1)
+    assert_match "cloudflared was installed by Homebrew. Please update using the same method.",
+      shell_output("#{bin}/cloudflared update 2>&1")
+  end
+end
