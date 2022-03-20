036c6cee66dc4d2bb956e2b8f8c6dc94215d4f58
Rui Chen <rui@chenrui.dev>
2022-03-20 17:59:16 -0400

    blast 2.13.0

--- Formula/blast.rb
+++ Formula/blast.rb
@@ -1,9 +1,9 @@
 class Blast < Formula
   desc "Basic Local Alignment Search Tool"
   homepage "https://blast.ncbi.nlm.nih.gov/"
-  url "https://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/2.12.0/ncbi-blast-2.12.0+-src.tar.gz"
-  version "2.12.0"
-  sha256 "fda3c9c9d488cad6c1880a98a236d842bcf3610e3e702af61f7a48cf0a714b88"
+  url "https://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/2.13.0/ncbi-blast-2.13.0+-src.tar.gz"
+  version "2.13.0"
+  sha256 "89553714d133daf28c477f83d333794b3c62e4148408c072a1b4620e5ec4feb2"
   license :public_domain
 
   livecheck do
