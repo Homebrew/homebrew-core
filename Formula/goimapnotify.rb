6b9cda3215440686e3d76aea060bbbe01bc19b67
Bradford Folkens <i.am@bradfolkens.com>
2022-03-24 16:55:18 -0500

    Add goimapnotify 2.3.7

--- Formula/goimapnotify.rb
+++ Formula/goimapnotify.rb
@@ -1,9 +1,10 @@
 class Goimapnotify < Formula
   desc "Execute scripts on IMAP mailbox changes using IDLE, golang version"
   homepage "https://gitlab.com/shackra/goimapnotify"
+  url "https://gitlab.com/shackra/goimapnotify/-/archive/2.3.7/goimapnotify-2.3.7.tar.bz2"
+  sha256 "f927732ac4307d0671f3015af6f69cab5e5c82e57fe1374201947f0187214c1c"
   license "GPL-3.0-only"
-  head "https://gitlab.com/shackra/goimapnotify.git",
-    tag: "2.4-rc1", revision: "97de8c15c48cdd3c6d9570a65d714a635b822642"
+  head "https://gitlab.com/shackra/goimapnotify.git", branch: "master"
 
   depends_on "go" => :build
 
@@ -19,6 +20,6 @@ class Goimapnotify < Formula
   end
 
   test do
-    assert_match "Usage of goimapnotify", shell_output("#{bin}/goimapnotify 2>&1")
+    assert_match(/^Usage of .*?goimapnotify/, shell_output("#{bin}/goimapnotify -h 2>&1"))
   end
 end
