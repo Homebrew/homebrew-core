4ce2b2105e72a0fb98abcd6e8490fcdc88cb69b9
Michael Cho <cho-m@tuta.io>
2022-03-25 21:30:13 -0700

    insect: deuniversalize_machos for fsevents

--- Formula/insect.rb
+++ Formula/insect.rb
@@ -23,6 +23,9 @@ class Insect < Formula
   def install
     system "npm", "install", *Language::Node.std_npm_install_args(libexec)
     bin.install_symlink Dir["#{libexec}/bin/*"]
+
+    # Replace universal binaries with their native slices
+    deuniversalize_machos
   end
 
   test do
