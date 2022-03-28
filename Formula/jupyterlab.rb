d7553983bf9aef5e167df459275a64984c94c92e
Xuehai Pan <XuehaiPan@pku.edu.cn>
2022-03-28 21:07:28 +0800

    jupyterlab: migrate to python@3.10

--- Formula/jupyterlab.rb
+++ Formula/jupyterlab.rb
@@ -6,6 +6,7 @@ class Jupyterlab < Formula
   url "https://files.pythonhosted.org/packages/a0/f2/d6bc1c4ea9a0d120ebf2fd8c8aab21748b5ba4b374e28eaece67197f56f3/jupyterlab-3.3.2.tar.gz"
   sha256 "3c716bf5592cb28c5c55c615c6e5bd3efc71898f6957d13719b56478bbbb587a"
   license "BSD-3-Clause"
+  revision 1
 
   bottle do
     sha256 cellar: :any,                 arm64_monterey: "d61536fa57f3fa515b8f94c154176b7d8d4e8446f2dcb409512b4a7764ed1e71"
@@ -18,7 +19,7 @@ class Jupyterlab < Formula
 
   depends_on "node"
   depends_on "pandoc"
-  depends_on "python@3.9"
+  depends_on "python@3.10"
   depends_on "six"
   depends_on "zeromq"
 
@@ -380,7 +381,7 @@ class Jupyterlab < Formula
   end
 
   def install
-    venv = virtualenv_create(libexec, Formula["python@3.9"].opt_bin/"python3")
+    venv = virtualenv_create(libexec, which("python3"))
     ENV["JUPYTER_PATH"] = etc/"jupyter"
 
     # gather packages to link based on options
