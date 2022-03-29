d260b8c564817155604c19d2eb003d5cb722e31e
Yurii Kolesnykov <root@yurikoles.com>
2022-03-29 19:18:15 +0300

    virt-manager: update deps, fix test

--- Formula/virt-manager.rb
+++ Formula/virt-manager.rb
@@ -3,16 +3,17 @@ class VirtManager < Formula
 
   desc "App for managing virtual machines"
   homepage "https://virt-manager.org/"
-  url "https://releases.pagure.org/virt-manager/virt-manager-3.2.0.tar.gz"
-  sha256 "2b6fe3d90d89e1130227e4b05c51e6642d89c839d3ea063e0e29475fd9bf7b86"
+  url "https://virt-manager.org/download/sources/virt-manager/virt-manager-4.0.0.tar.gz"
+  sha256 "515aaa2021a4bf352b0573098fe6958319b1ba8ec508ea37e064803f97f17086"
   license "GPL-2.0-or-later"
-  head "https://github.com/virt-manager/virt-manager.git", branch: "master"
+  head "https://github.com/virt-manager/virt-manager.git", branch: "main"
 
   depends_on "docutils" => :build
   depends_on "intltool" => :build
   depends_on "pkg-config" => :build
 
   depends_on "adwaita-icon-theme"
+  depends_on "cpio"
   depends_on "gtk-vnc"
   depends_on "gtksourceview4"
   depends_on "libosinfo"
@@ -25,34 +26,39 @@ class VirtManager < Formula
   depends_on "spice-gtk"
   depends_on "vte3"
 
-  resource "libvirt-python" do
-    url "https://libvirt.org/sources/python/libvirt-python-7.10.0.tar.gz"
-    sha256 "267774bbdf99d47515274542880499437dc94ae291771f5663c62020a62da975"
-  end
-
-  resource "idna" do
-    url "https://pypi.io/packages/source/i/idna/idna-3.3.tar.gz"
-    sha256 "9d643ff0a55b762d5cdb124b8eaa99c66322e2157b69160bc32796e824360e6d"
-  end
-
   resource "certifi" do
-    url "https://pypi.io/packages/source/c/certifi/certifi-2021.10.8.tar.gz"
+    url "https://files.pythonhosted.org/packages/6c/ae/d26450834f0acc9e3d1f74508da6df1551ceab6c2ce0766a593362d6d57f/certifi-2021.10.8.tar.gz"
     sha256 "78884e7c1d4b00ce3cea67b44566851c4343c120abd683433ce934a68ea58872"
   end
 
   resource "chardet" do
-    url "https://pypi.io/packages/source/c/chardet/chardet-4.0.0.tar.gz"
+    url "https://files.pythonhosted.org/packages/ee/2d/9cdc2b527e127b4c9db64b86647d567985940ac3698eeabc7ffaccb4ea61/chardet-4.0.0.tar.gz"
     sha256 "0d6f53a15db4120f2b08c94f11e7d93d2c911ee118b6b30a04ec3ee8310179fa"
   end
 
-  resource "urllib3" do
-    url "https://pypi.io/packages/source/u/urllib3/urllib3-1.26.7.tar.gz"
-    sha256 "4987c65554f7a2dbf30c18fd48778ef124af6fab771a377103da0585e2336ece"
+  resource "docutils" do
+    url "https://files.pythonhosted.org/packages/57/b1/b880503681ea1b64df05106fc7e3c4e3801736cf63deffc6fa7fc5404cf5/docutils-0.18.1.tar.gz"
+    sha256 "679987caf361a7539d76e584cbeddc311e3aee937877c87346f31debc63e9d06"
+  end
+
+  resource "idna" do
+    url "https://files.pythonhosted.org/packages/62/08/e3fc7c8161090f742f504f40b1bccbfc544d4a4e09eb774bf40aafce5436/idna-3.3.tar.gz"
+    sha256 "9d643ff0a55b762d5cdb124b8eaa99c66322e2157b69160bc32796e824360e6d"
+  end
+
+  resource "libvirt-python" do
+    url "https://files.pythonhosted.org/packages/2a/74/919462bed158ccc2a8061f31d82c157740c2e597ff50490526336e8aa688/libvirt-python-8.1.0.tar.gz"
+    sha256 "a21ecfab6d29ac1bdd1bfd4aa3ef58447f9f70919aefecd03774613f65914e43"
   end
 
   resource "requests" do
-    url "https://pypi.io/packages/source/r/requests/requests-2.26.0.tar.gz"
-    sha256 "b8aa58f8cf793ffd8782d3d8cb19e66ef36f7aba4353eec859e74678b01b07a7"
+    url "https://files.pythonhosted.org/packages/60/f3/26ff3767f099b73e0efa138a9998da67890793bfa475d8278f84a30fec77/requests-2.27.1.tar.gz"
+    sha256 "68d7c56fd5a8999887728ef304a6d12edc7be74f1cfa47714fc8b414525c9a61"
+  end
+
+  resource "urllib3" do
+    url "https://files.pythonhosted.org/packages/1b/a5/4eab74853625505725cefdf168f48661b2cd04e7843ab836f3f63abf81da/urllib3-1.26.9.tar.gz"
+    sha256 "aabaf16477806a5e1dd19aa41f8c2b7950dd3c746362d7e3223dbe6de6ac448e"
   end
 
   def install
@@ -87,9 +93,10 @@ class VirtManager < Formula
 
   test do
     pid = fork do
-      exec bin/"virt-manager", "-c", "test:///default", "--no-fork"
+      exec bin/"virt-manager -c test:///default --debug &> test.log"
     end
-    sleep 3
+    sleep 10
+    system "grep", "-F", "conn=test:///default changed to state=Active", "test.log"
   ensure
     Process.kill("TERM", pid)
     Process.wait(pid)
