24886a1add09aa0e510ab6673d5b0d2e3de57690
Michael Cho <cho-m@tuta.io>
2022-03-29 11:13:54 -0700

    oha: fix url and sha256

--- Formula/oha.rb
+++ Formula/oha.rb
@@ -1,8 +1,8 @@
 class Oha < Formula
   desc "HTTP load generator, inspired by rakyll/hey with tui animation"
   homepage "https://github.com/hatoo/oha/"
-  url "https://github.com/hatoo/oha/archive/v0.5.1.tar.gz"
-  sha256 "5e1fe0cda2623a8f32147d769bb41c2f0015d0ebb245c2891aecb757b1541dd7"
+  url "https://github.com/hatoo/oha/archive/refs/tags/v0.5.1.tar.gz"
+  sha256 "1879b2e155679241560ce805965bbd7ac5fb1b602115d97b2b31afe8a0964051"
   license "MIT"
   head "https://github.com/hatoo/oha.git", branch: "master"
 
