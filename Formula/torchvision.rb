0b797dc24cd71453e7ebf1c27cb8136816f58543
owine <owine@users.noreply.github.com>
2022-04-02 16:32:11 -0500

    torchvision: Revision bump (protobuf 3.20.0)

--- Formula/torchvision.rb
+++ Formula/torchvision.rb
@@ -4,6 +4,7 @@ class Torchvision < Formula
   url "https://github.com/pytorch/vision/archive/refs/tags/v0.12.0.tar.gz"
   sha256 "99e6d3d304184895ff4f6152e2d2ec1cbec89b3e057d9c940ae0125546b04e91"
   license "BSD-3-Clause"
+  revision 1
 
   bottle do
     sha256 cellar: :any,                 arm64_monterey: "8e3da95bf3f38a7ae2c906c865aa1a957ba5d64cbfde891881b220cd58b128b0"
