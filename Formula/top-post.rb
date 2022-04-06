684b751df49c0a04b7d934345876c84b3b3e8bd7
daschaa <extern.joshua.weber@volkswagen.de>
2022-04-06 08:39:48 +0200

    Added test with assertion

--- Formula/top-post.rb
+++ Formula/top-post.rb
@@ -1,7 +1,7 @@
 require "language/node"
 
 class TopPost < Formula
-  desc "⚙️ Show Hacker News articles in the terminal"
+  desc "Show Hacker News articles in the terminal"
   homepage "https://github.com/daschaa/top-post"
   url "https://registry.npmjs.org/top-post/-/top-post-0.0.9.tgz"
   sha256 "cb2f030eed227c026a49864888ccec84c5ad124333294563bbe5050b1c0326cf"
@@ -19,7 +19,7 @@ class TopPost < Formula
   end
 
   test do
-    output = `#{bin}/top-post --help`
-    puts output
+    output = shell_output("#{bin}/top-post")
+    assert !output.to_s.empty?
   end
 end
