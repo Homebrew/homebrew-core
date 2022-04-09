1df38332290532e0da529cb26ef99701e8b9746e
Yurii Kolesnykov <root@yurikoles.com>
2022-04-09 21:16:35 +0300

    pdftoipe: revision bump (poppler 22.04.0)
    Signed-off-by: Yurii Kolesnykov <root@yurikoles.com>

--- Formula/pdftoipe.rb
+++ Formula/pdftoipe.rb
@@ -4,7 +4,7 @@ class Pdftoipe < Formula
   url "https://github.com/otfried/ipe-tools/archive/v7.2.24.1.tar.gz"
   sha256 "561b18fc2a7ae45c37c5d0390443b37f4585549f09cd7765d856456be24e5dbc"
   license "GPL-2.0-or-later"
-  revision 4
+  revision 5
 
   bottle do
     sha256 cellar: :any,                 arm64_monterey: "3ab6a38078e56d64e493b40ffca5eed6cd1bae0286ee487c5b4b429e9d44f850"
