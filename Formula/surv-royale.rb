24be0d90c8735064a1de4f1e2320b3bab6a1e46e
Ilya Ballet <anonimus33@protonmail.com>
2022-04-10 10:00:32 +0300

    Update surv-royale.rb

--- Formula/surv-royale.rb
+++ Formula/surv-royale.rb
@@ -1,5 +1,5 @@
 class SurvRoyale < Formula
-  desc "desktop clone of surviv.io written in C++"
+  desc "Desktop clone of surviv.io written in C++"
   homepage "https://github.com/Fenex330/surv-royale"
   url "https://github.com/Fenex330/surv-royale/archive/refs/tags/v0.0.1.tar.gz"
   sha256 "e2977edff3e1b855845230695839f16a055c6a952de2fbd9d7d9b733ca8629b6"
