2639bb84071af85f1ef6c7bc1dfcea3102f1569f
Rui Chen <rui@chenrui.dev>
2022-04-10 15:39:29 -0400

    caf 0.18.6

--- Formula/caf.rb
+++ Formula/caf.rb
@@ -2,8 +2,8 @@ class Caf < Formula
   # Renamed from libccpa
   desc "Implementation of the Actor Model for C++"
   homepage "https://www.actor-framework.org/"
-  url "https://github.com/actor-framework/actor-framework/archive/0.18.5.tar.gz"
-  sha256 "4c96f896f000218bb65890b4d7175451834add73750d5f33b0c7fe82b7d5a679"
+  url "https://github.com/actor-framework/actor-framework/archive/0.18.6.tar.gz"
+  sha256 "c2ead63a0322d992fea8813a7f7d15b4d16cbb8bbe026722f2616a79109b91cc"
   license "BSD-3-Clause"
   head "https://github.com/actor-framework/actor-framework.git", branch: "master"
 
@@ -28,7 +28,7 @@ class Caf < Formula
 
   def install
     mkdir "build" do
-      system "cmake", "..", *std_cmake_args, "-DCAF_ENABLE_TESTING=OFF"
+      system "cmake", "..", *std_cmake_args, "-DCMAKE_INSTALL_RPATH=#{lib}", "-DCAF_ENABLE_TESTING=OFF"
       system "make"
       system "make", "install"
     end
