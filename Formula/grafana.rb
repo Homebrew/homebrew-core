afc7039b1859cd9d73f704a4726cfa0b821e5e57
Tania <yalyna.ts@gmail.com>
2022-04-12 15:21:40 +0200

    Fix shasum

--- Formula/grafana.rb
+++ Formula/grafana.rb
@@ -2,7 +2,7 @@ class Grafana < Formula
   desc "Gorgeous metric visualizations and dashboards for timeseries databases"
   homepage "https://grafana.com"
   url "https://github.com/grafana/grafana/archive/v8.4.6.tar.gz"
-  sha256 "80ea512e319fdf29da22e723c7d4b0005187da5c9fe2b19f83191a54c9c180d7"
+  sha256 "845ce303f468bb845488387da138e8ced6eed86e7a99baa3aaa3a81a04d44770"
   license "AGPL-3.0-only"
   head "https://github.com/grafana/grafana.git", branch: "main"
 
