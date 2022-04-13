b73b62638cff8971fd3ffcf60a7a31bc2ef2ddd4
shay23b <shay.b@strech.io>
2022-04-13 08:47:33 +0300

    fixed bugs in formulae

--- Formula/memphis.rb
+++ Formula/memphis.rb
@@ -1,13 +1,15 @@
 class Memphis < Formula
- desc "Greetings from Memphis - CLI"
- homepage "https://github.com/Memphis-OS/memphis-cli"
- url "https://github.com/Memphis-OS/memphis-cli/releases/download/v0.1.0-beta/memphis.tar.gz"
- sha256 "7879853453675db79c39bce6037cd137874c5cc21ec09999d1c3e9483efde20b"
- license "MIT"
- version "v0.1.0-beta"
- 
- def install
-   bin.install "memphis"
- end
-end
+  desc "Greetings from Memphis - CLI"
+  homepage "https://github.com/Memphis-OS/memphis-cli"
+  url "https://github.com/Memphis-OS/memphis-cli/releases/download/v0.1.0/memphis.tar.gz"
+  sha256 "7879853453675db79c39bce6037cd137874c5cc21ec09999d1c3e9483efde20b"
+  license "MIT"
 
+  def install
+    bin.install "memphis"
+  end
+
+  test do
+    system "false"
+  end
+end
