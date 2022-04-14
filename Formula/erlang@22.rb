be0d5591c5635d4284c0afbca57c34d242b1b46f
Pavel Kalian <pavel@kalian.cz>
2022-04-14 09:46:04 -0300

    erlang@22: revision bump for wxWidgets 3.1.6

--- Formula/erlang@22.rb
+++ Formula/erlang@22.rb
@@ -5,6 +5,7 @@ class ErlangAT22 < Formula
   url "https://github.com/erlang/otp/releases/download/OTP-22.3.4.25/otp_src_22.3.4.25.tar.gz"
   sha256 "f1e94f0b66fae1f48f4fae45480ba5c7d15a3bae0aac8d1b580a41cf0eddfd9a"
   license "Apache-2.0"
+  revision 1
 
   livecheck do
     url :stable
