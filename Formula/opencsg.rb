92a13379f835c6def5eab2abba04e7e8bda9d0ad
Rui Chen <rui@chenrui.dev>
2022-04-17 14:35:48 -0400

    update urls to https

--- Formula/opencsg.rb
+++ Formula/opencsg.rb
@@ -1,7 +1,7 @@
 class Opencsg < Formula
   desc "Constructive solid geometry rendering library"
-  homepage "http://www.opencsg.org"
-  url "http://www.opencsg.org/OpenCSG-1.5.0.tar.gz"
+  homepage "https://www.opencsg.org"
+  url "https://www.opencsg.org/OpenCSG-1.5.0.tar.gz"
   sha256 "bf2573acf3f8ee634aee074db2406e125a77b262b73ef2a65eff28664051174a"
 
   livecheck do
