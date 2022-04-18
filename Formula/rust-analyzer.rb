2383b08925ef0c0698e868b6d995d0562fc6da8f
studierer <77481149+LC0117@users.noreply.github.com>
2022-04-18 21:06:25 +0800

    rust-analyzer 2022-04-18

--- Formula/rust-analyzer.rb
+++ Formula/rust-analyzer.rb
@@ -2,9 +2,9 @@ class RustAnalyzer < Formula
   desc "Experimental Rust compiler front-end for IDEs"
   homepage "https://rust-analyzer.github.io/"
   url "https://github.com/rust-analyzer/rust-analyzer.git",
-       tag:      "2022-04-11",
-       revision: "24cf957627d5ede1b395f92ff871fd7a281d49a4"
-  version "2022-04-11"
+       tag:      "2022-04-18",
+       revision: "65fbe0a8d17fc257f037f4dd465bff69a3d3054a"
+  version "2022-04-18"
   license any_of: ["Apache-2.0", "MIT"]
 
   bottle do
