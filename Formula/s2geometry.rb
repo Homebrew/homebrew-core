29fae1f3d9c73e5ce4ec5c166381544ceb272443
Michael Cho <cho-m@tuta.io>
2022-04-18 07:42:25 -0700

    s2geometry: remove googletest and s2testing usage in test

--- Formula/s2geometry.rb
+++ Formula/s2geometry.rb
@@ -18,9 +18,8 @@ class S2geometry < Formula
   end
 
   depends_on "cmake" => :build
-  depends_on "glog" => :build
-  depends_on "googletest" => :build
   depends_on "abseil"
+  depends_on "glog"
   depends_on "openssl@1.1"
 
   on_linux do
@@ -34,7 +33,6 @@ class S2geometry < Formula
     inreplace "CMakeLists.txt", "set(CMAKE_CXX_STANDARD 11)", "set(CMAKE_CXX_STANDARD 17)"
 
     ENV["OPENSSL_ROOT_DIR"] = Formula["openssl@1.1"].opt_prefix
-    ENV.append "CXXFLAGS", "-I#{Formula["googletest"].opt_include}"
 
     args = std_cmake_args + %w[
       -DWITH_GFLAGS=1
@@ -55,6 +53,7 @@ class S2geometry < Formula
   test do
     (testpath/"test.cpp").write <<~EOS
       #include <cinttypes>
+      #include <cmath>
       #include <cstdint>
       #include <cstdio>
       #include "s2/base/commandlineflags.h"
@@ -63,16 +62,46 @@ class S2geometry < Formula
       #include "s2/s1chord_angle.h"
       #include "s2/s2closest_point_query.h"
       #include "s2/s2point_index.h"
-      #include "s2/s2testing.h"
 
       S2_DEFINE_int32(num_index_points, 10000, "Number of points to index");
       S2_DEFINE_int32(num_queries, 10000, "Number of queries");
       S2_DEFINE_double(query_radius_km, 100, "Query radius in kilometers");
 
+      inline uint64 GetBits(int num_bits) {
+        S2_DCHECK_GE(num_bits, 0);
+        S2_DCHECK_LE(num_bits, 64);
+        static const int RAND_BITS = 31;
+        uint64 result = 0;
+        for (int bits = 0; bits < num_bits; bits += RAND_BITS) {
+          result = (result << RAND_BITS) + random();
+        }
+        if (num_bits < 64) {  // Not legal to shift by full bitwidth of type
+          result &= ((1ULL << num_bits) - 1);
+        }
+        return result;
+      }
+
+      double RandDouble() {
+        const int NUM_BITS = 53;
+        return ldexp(GetBits(NUM_BITS), -NUM_BITS);
+      }
+
+      double UniformDouble(double min, double limit) {
+        S2_DCHECK_LT(min, limit);
+        return min + RandDouble() * (limit - min);
+      }
+
+      S2Point RandomPoint() {
+        double x = UniformDouble(-1, 1);
+        double y = UniformDouble(-1, 1);
+        double z = UniformDouble(-1, 1);
+        return S2Point(x, y, z).Normalize();
+      }
+
       int main(int argc, char **argv) {
         S2PointIndex<int> index;
         for (int i = 0; i < absl::GetFlag(FLAGS_num_index_points); ++i) {
-          index.Add(S2Testing::RandomPoint(), i);
+          index.Add(RandomPoint(), i);
         }
 
         S2ClosestPointQuery<int> query(&index);
@@ -81,7 +110,7 @@ class S2geometry < Formula
 
         int64_t num_found = 0;
         for (int i = 0; i < absl::GetFlag(FLAGS_num_queries); ++i) {
-          S2ClosestPointQuery<int>::PointTarget target(S2Testing::RandomPoint());
+          S2ClosestPointQuery<int>::PointTarget target(RandomPoint());
           num_found += query.FindClosestPoints(&target).size();
         }
 
@@ -90,7 +119,7 @@ class S2geometry < Formula
     EOS
     system ENV.cxx, "-std=c++11", "test.cpp", "-o", "test",
                     "-I#{Formula["openssl@1.1"].opt_include}",
-                    "-L#{lib}", "-ls2", "-ls2testing"
+                    "-L#{lib}", "-ls2"
     system "./test"
   end
 end
