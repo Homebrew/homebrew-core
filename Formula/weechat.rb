3916ef15f13c746e306fcbe31a88c34cb253c1d4
Alexander Ronald Altman <alexanderaltman@me.com>
2022-04-18 10:32:42 -0700

    weechat: revision bump (perl 5.34.1)

--- Formula/weechat.rb
+++ Formula/weechat.rb
@@ -4,6 +4,7 @@ class Weechat < Formula
   url "https://weechat.org/files/src/weechat-3.4.1.tar.xz"
   sha256 "7e088109ad5dfbcb08a9a6b1dd70ea8236093fed8a13ee9d9c98881d7b1aeae7"
   license "GPL-3.0-or-later"
+  revision 1
   head "https://github.com/weechat/weechat.git", branch: "master"
 
   bottle do
