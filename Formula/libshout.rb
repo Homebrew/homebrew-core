7a5541a129973410093fee0eaafe232bf3747b85
Michael Cho <cho-m@tuta.io>
2022-04-19 16:59:06 -0700

    libshout: patch -flat_namespace

--- Formula/libshout.rb
+++ Formula/libshout.rb
@@ -20,9 +20,6 @@ class Libshout < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "dd94f36cce287abf74b00f18dd17f995afb81accc1199daf5fe07cfa9693c9a8"
   end
 
-  depends_on "autoconf" => :build
-  depends_on "automake" => :build
-  depends_on "libtool" => :build
   depends_on "pkg-config" => :build
   depends_on "libogg"
   depends_on "libvorbis"
@@ -33,8 +30,13 @@ class Libshout < Formula
     depends_on "openssl@1.1"
   end
 
+  # Fix -flat_namespace being used on Big Sur and later.
+  patch do
+    url "https://raw.githubusercontent.com/Homebrew/formula-patches/03cf8088210822aa2c1ab544ed58ea04c897d9c4/libtool/configure-big_sur.diff"
+    sha256 "35acd6aebc19843f1a2b3a63e880baceb0f5278ab1ace661e57a502d9d78c93c"
+  end
+
   def install
-    system "autoreconf", "-fiv"
     system "./configure", *std_configure_args
     system "make", "install"
   end
