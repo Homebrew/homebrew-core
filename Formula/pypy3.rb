229a38d8391b7476100c89e19909fcf240f02b9a
Alexander Ronald Altman <alexanderaltman@me.com>
2022-04-22 22:17:21 -0700

    pypy3 7.3.9

--- Formula/pypy3.rb
+++ Formula/pypy3.rb
@@ -1,8 +1,8 @@
 class Pypy3 < Formula
   desc "Implementation of Python 3 in Python"
   homepage "https://pypy.org/"
-  url "https://downloads.python.org/pypy/pypy3.7-v7.3.8-src.tar.bz2"
-  sha256 "35752be62b148fa6f7fb69e58e1f993c7cc319bea54928eb03ed2e75b8248d5f"
+  url "https://downloads.python.org/pypy/pypy3.7-v7.3.9-src.tar.bz2"
+  sha256 "70426163b194ee46009986eea6d9426098a3ffb552d9cdbd3dfaa64a47373f49"
   license "MIT"
   head "https://foss.heptapod.net/pypy/pypy", using: :hg, branch: "py3.7"
 
