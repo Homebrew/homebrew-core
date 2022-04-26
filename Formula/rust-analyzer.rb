d90e83caba86b297de51ac3f64e823b8b4f6e9b1
peeweep <peeweep@0x0.ee>
2022-04-26 08:10:07 +0000

    rust-analyzer: downgrade smol_str from 0.1.22 to 0.1.21 to fix build failed
    Signed-off-by: peeweep <peeweep@0x0.ee>

--- Formula/rust-analyzer.rb
+++ Formula/rust-analyzer.rb
@@ -18,6 +18,9 @@ class RustAnalyzer < Formula
 
   depends_on "rust" => :build
 
+  # Remove this patch after rust 1.60 (https://github.com/rust-lang/rust-analyzer/issues/12080)
+  patch :DATA
+
   def install
     cd "crates/rust-analyzer" do
       system "cargo", "install", "--bin", "rust-analyzer", *std_cargo_args
@@ -73,3 +76,22 @@ class RustAnalyzer < Formula
     assert_match output, pipe_output("#{bin}/rust-analyzer", input, 0)
   end
 end
+
+
+__END__
+diff --git a/Cargo.lock b/Cargo.lock
+index 1937b8936..e6f321394 100644
+--- a/Cargo.lock
++++ b/Cargo.lock
+@@ -1571,9 +1571,9 @@ checksum = "f2dd574626839106c320a323308629dcb1acfc96e32a8cba364ddc61ac23ee83"
+ 
+ [[package]]
+ name = "smol_str"
+-version = "0.1.22"
++version = "0.1.21"
+ source = "registry+https://github.com/rust-lang/crates.io-index"
+-checksum = "167ee181c12079444893cec9c8f21b13d6b314af789c9fdb041a0645f11ed9d2"
++checksum = "61d15c83e300cce35b7c8cd39ff567c1ef42dde6d4a1a38dbdbf9a59902261bd"
+ dependencies = [
+  "serde",
+ ]
