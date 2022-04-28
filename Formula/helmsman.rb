4a25b9e13e5b8713d547bdd5d5562b5c261f5486
Rui Chen <rui@chenrui.dev>
2022-04-27 21:49:10 -0400

    update test

--- Formula/helmsman.rb
+++ Formula/helmsman.rb
@@ -28,7 +28,7 @@ class Helmsman < Formula
   test do
     assert_match version.to_s, shell_output("#{bin}/helmsman version")
 
-    output = shell_output("#{bin}/helmsman --apply -f #{pkgshare}/example.yaml 2>&1", 1)
+    output = shell_output("#{bin}/helmsman --apply -f #{pkgshare}/example.yaml 2>&1")
     assert_match "helm diff not found", output
   end
 end
