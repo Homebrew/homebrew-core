ace04431c9080277104597f2cb46bafafc89fd51
Porkepix <porkepix@gmail.com>
2022-05-01 12:41:44 +0200

    oil 0.10.0

--- Formula/oil.rb
+++ Formula/oil.rb
@@ -1,8 +1,8 @@
 class Oil < Formula
   desc "Bash-compatible Unix shell with more consistent syntax and semantics"
   homepage "https://www.oilshell.org/"
-  url "https://www.oilshell.org/download/oil-0.9.9.tar.gz"
-  sha256 "e10b6de6da4bda27a012e0b5750a9bee8c7576bd0d75ec13385e1fcf01febafa"
+  url "https://www.oilshell.org/download/oil-0.10.0.tar.gz"
+  sha256 "7a4baeb886f02e9f10770bdabfb92a752b9861e999581d613d0fc3d4e9287911"
   license "Apache-2.0"
 
   livecheck do
