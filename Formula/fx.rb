7931d9dd58fde308b7adc94b954fd8ecd11ca4f9
Rui Chen <rui@chenrui.dev>
2022-05-06 10:39:24 -0400

    Update fx.rb

--- Formula/fx.rb
+++ Formula/fx.rb
@@ -17,7 +17,7 @@ class Fx < Formula
   depends_on "go" => :build
 
   def install
-    system "go", "build", *std_go_args
+    system "go", "build", *std_go_args(ldflags: "-s -w")
   end
 
   test do
