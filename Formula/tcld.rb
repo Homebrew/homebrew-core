4f5c091f8ec083c9cb83ea29f9ef9556b49d4bf1
matt@temporal.io <matthkim@gmail.com>
2022-05-05 18:07:32 -0700

    add test

--- Formula/tcld.rb
+++ Formula/tcld.rb
@@ -12,7 +12,11 @@ class Tcld < Formula
   end
 
   test do
+    # Given tcld is pointless without a server, not much intersting to test here.
     run_output = shell_output("#{bin}/tcld version 2>&1")
     assert_match "Version", run_output
+
+    run_output = shell_output("#{bin}/tcld -s localhost:1234 n l 2>&1")
+    assert_match "rpc error", run_output
   end
 end
