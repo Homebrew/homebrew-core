6a13f77430294f309e96196bcac40cb258fed94b
Rui Chen <rui@chenrui.dev>
2022-05-08 16:00:23 -0400

    kubevela: update repo location
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/kubevela.rb
+++ Formula/kubevela.rb
@@ -1,7 +1,7 @@
 class Kubevela < Formula
   desc "Application Platform based on Kubernetes and Open Application Model"
   homepage "https://kubevela.io"
-  url "https://github.com/oam-dev/kubevela.git",
+  url "https://github.com/kubevela/kubevela.git",
       tag:      "v1.3.3",
       revision: "45e1de19dc736b10b3eb2d908d809210f470b24f"
   license "Apache-2.0"
@@ -21,8 +21,8 @@ class Kubevela < Formula
     ENV["CGO_ENABLED"] = "0"
     ldflags = %W[
       -s -w
-      -X github.com/oam-dev/kubevela/version.VelaVersion=#{version}
-      -X github.com/oam-dev/kubevela/version.GitRevision=#{Utils.git_head}
+      -X github.com/kubevela/kubevela/version.VelaVersion=#{version}
+      -X github.com/kubevela/kubevela/version.GitRevision=#{Utils.git_head}
     ]
 
     system "go", "build", *std_go_args(output: bin/"vela", ldflags: ldflags), "./references/cmd/cli"
