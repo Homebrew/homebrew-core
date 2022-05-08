675a2502f6b27c738ab17517635a3280e397ab86
mikael <13247629622@163.com>
2022-05-08 12:43:46 +0800

    sql2pb 1.0.2

--- Formula/sql2pb.rb
+++ Formula/sql2pb.rb
@@ -1,9 +1,7 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+# Documentation: https://github.com/Mikaelemmmm/sql2pb
 class Sql2pb < Formula
   desc "Generates a protobuf file from your mysql database"
-  homepage ""
+  homepage "https://github.com/Mikaelemmmm/sql2pb"
   url "https://github.com/Mikaelemmmm/sql2pb/archive/refs/tags/v1.0.2.tar.gz"
   sha256 "9ed48666f7863589246c0dad4f8b95f7ed093304f1f60d0e127e37ca7b12363b"
   license "MIT"
