11f2807cc672dd6d5d75a37581063906dda0eaeb
削微寒 <595666367@qq.com>
2022-05-11 11:55:36 +0800

    Update hg-tui.rb

--- Formula/hg-tui.rb
+++ Formula/hg-tui.rb
@@ -3,13 +3,15 @@ class HgTui < Formula
   homepage "https://github.com/kaixinbaba/hg-tui"
   url "https://github.com/kaixinbaba/hg-tui/archive/0.1.4.tar.gz"
   sha256 "8bed6a213760dff1d09d7a043e49491e8383322ac1b1dc2ef585e9e113d0d5cd"
-  license "GPL-3.0"
+  license "GPL"
+
+  depends_on "rust" => :build
 
   def install
     system "cargo", "install", *std_cargo_args
   end
 
   test do
-    system bin/"hggui", "--version"
+    system bin/"hgtui", "--version"
   end
 end
