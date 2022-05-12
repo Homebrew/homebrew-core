675a1b806c488b8139b9702117724e622182ea47
Sharvanath Pathak <sharvanath@gmail.com>
2022-05-11 22:56:07 -0700

    remove comments

--- Formula/kromium.rb
+++ Formula/kromium.rb
@@ -1,6 +1,3 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Kromium < Formula
   desc "Kromium is a bulk file copy/transformation tool."
   homepage "https://kromium.io"
