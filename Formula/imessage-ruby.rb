5e360ce925a9b0f6b97c205d6a96456bfd69e066
Jun Lin <linjunpop@gmail.com>
2022-05-12 16:08:20 +0800

    Bump to v0.4.0.

--- Formula/imessage-ruby.rb
+++ Formula/imessage-ruby.rb
@@ -1,22 +1,34 @@
 class ImessageRuby < Formula
   desc "Command-line tool to send iMessage"
   homepage "https://github.com/linjunpop/imessage"
-  url "https://github.com/linjunpop/imessage/archive/refs/tags/v0.3.3.tar.gz"
-  sha256 "e5f1f69f0a21914cff3ea27d7b804295be6b2cebd2b5b28c47cb25a7b98028b5"
+  url "https://github.com/linjunpop/imessage/archive/refs/tags/v0.4.0.tar.gz"
+  sha256 "09031e60548f34f05e07faeb0e26b002aeb655488d152dd811021fba8d850162"
   license "MIT"
   head "https://github.com/linjunpop/imessage.git", branch: "master"
 
-  bottle do
-    sha256 cellar: :any_skip_relocation, all: "7c538ac24ec3ce437d868267582bd68aaa500eeb5a9bdbd3d0d80398b4bab19d"
-  end
+  depends_on "ruby" if Hardware::CPU.arm?
 
-  depends_on :macos
+  uses_from_macos "ruby", since: :catalina
 
   def install
-    system "rake", "standalone:install", "prefix=#{prefix}"
+    ENV["GEM_HOME"] = libexec
+    ENV["GEM_PATH"] = libexec
+
+    system "gem", "build", "imessage.gemspec", "-o", "imessage-#{version}.gem"
+    system "gem", "install", "--local", "--verbose", "imessage-#{version}.gem", "--no-document"
+    system "gem", "info", "imessage"
+
+    (bin/"imessage").write_env_script libexec/"bin/imessage",
+      PATH:                            "#{Formula["ruby"].opt_bin}:#{libexec}/bin:$PATH",
+      GEM_HOME:                        libexec.to_s,
+      GEM_PATH:                        libexec.to_s
   end
 
   test do
     system "#{bin}/imessage", "--version"
+
+    unless build.head?
+      assert_match "imessage #{version}", shell_output("#{bin}/imessage --version")
+    end
   end
 end
