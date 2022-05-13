c3143f11778bbbdb96e70b37f5ed454181f7b033
Vlad <vzhd1701@gmail.com>
2022-05-13 17:39:26 +0500

    mupdf: build shared library

--- Formula/mupdf.rb
+++ Formula/mupdf.rb
@@ -28,11 +28,11 @@ class Mupdf < Formula
     because: "mupdf and mupdf-tools install the same binaries"
 
   def install
-    ENV.append_to_cflags "-fPIC" if OS.linux?
     glut_cflags = `pkg-config --cflags glut gl`.chomp
     glut_libs = `pkg-config --libs glut gl`.chomp
     system "make", "install",
            "build=release",
+           "shared=yes",
            "verbose=yes",
            "CC=#{ENV.cc}",
            "SYS_GLUT_CFLAGS=#{glut_cflags}",
@@ -42,6 +42,8 @@ class Mupdf < Formula
     # Symlink `mutool` as `mudraw` (a popular shortcut for `mutool draw`).
     bin.install_symlink bin/"mutool" => "mudraw"
     man1.install_symlink man1/"mutool.1" => "mudraw.1"
+
+    lib.install_symlink lib/shared_library("libmupdf") => shared_library("libmupdf-third")
   end
 
   test do
