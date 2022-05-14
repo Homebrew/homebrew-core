a51b9f575998fedf227ecc3471acd187ae51d73f
Chris Fallin <chris@cfallin.org>
2022-05-14 14:02:24 -0700

    Add new formlua: wasi-sdk (15.0).
    WASI-SDK is a distribution of a custom libc (wasi-libc) with clang/LLVM
    to target the WASI (WebAssembly System Interface) platform. WASI is a
    definition of a POSIX-like set of syscalls for WebAssembly modules to
    use. A program compiled for the `wasm32-wasi` target can run in various
    WebAssembly-outside-the-browser environments (server-side, cloud/edge
    computing, command-line runtimes like Wasmtime, etc) and within browsers
    with the appropriate shims.
    
    This formula is a fairly thin wrapper around the custom Makefile in the
    `wasi-sdk` repository [1], which itself is a fairly thin wrapper around
    an LLVM/Clang build with specific options and defaults together with a
    build of the custom sysroot.
    
    [1] https://github.com/WebAssembly/wasi-sdk/

---
+++ Formula/wasi-sdk.rb
@@ -0,0 +1,54 @@
+class WasiSdk < Formula
+  desc "WASI-enabled WebAssembly C/C++ toolchain"
+  homepage "https://github.com/WebAssembly/wasi-sdk"
+  url "https://github.com/WebAssembly/wasi-sdk.git",
+    tag:      "wasi-sdk-15",
+    revision: "e025da6b1099c730af4809484ad0ea84e0c3edbe"
+  version "15.0"
+  license "Apache-2.0"
+
+  keg_only "provides alternative LLVM/Clang binaries specific to WASI"
+
+  depends_on "cmake" => :build
+  depends_on "ninja" => :build
+  depends_on "rsync" => :build
+
+  def install
+    # The `build` target in wasi-sdk's Makefile sets all of the options
+    # appropriately.
+    system "make", "build", "PREFIX=#{prefix}", "NINJA_FLAGS=-v"
+
+    # The `build` target installs everything into a DESTDIR of build/install/
+    # (with the tree starting with `prefix` under that). This is because the
+    # wasi-sdk build process is intended to produce a standalone binary
+    # tarball. We instead want to install locally, so let's copy the whole tree
+    # into `/`. Note that the tree only has content within `prefix` (e.g.
+    # /opt/homebrew/Cellar/wasi-sdk/...).
+    system "rsync", "-rlv", "build/install/", "/"
+  end
+
+  def caveats
+    <<~EOS
+      wasi-sdk provides `clang` and other binaries specifically for building
+      WASI binaries. In order to avoid conflict with your system compiler, these
+      are not put in your PATH by default. Instead, the binaries have been installed
+      in:
+
+        #{bin}
+    EOS
+  end
+
+  test do
+    testc = <<~EOS
+      #include <stdio.h>
+      int main() {
+        printf("hello world");
+      }
+    EOS
+    (testpath/"test.c").write(testc)
+    # Environment variable $CPATH set by Homebrew interferes with this clang
+    # invocation. Unset it.
+    ENV.delete "CPATH"
+    system "#{bin}/clang", testpath/"test.c", "-o", testpath/"test.wasm"
+  end
+end
