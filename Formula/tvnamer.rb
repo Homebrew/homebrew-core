893c37d19a721a8a2b63f1d0e3acfd8d6f36d97a
Rui Chen <rui@chenrui.dev>
2022-05-16 14:52:41 -0400

    fix build
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/tvnamer.rb
+++ Formula/tvnamer.rb
@@ -1,4 +1,6 @@
 class Tvnamer < Formula
+  include Language::Python::Virtualenv
+
   desc "Automatic TV episode file renamer that uses data from thetvdb.com"
   homepage "https://github.com/dbr/tvnamer"
   url "https://files.pythonhosted.org/packages/7e/07/688dc96a86cf212ffdb291d2f012bc4a41ee78324a2eda4c98f05f5e3062/tvnamer-3.0.4.tar.gz"
@@ -55,18 +57,7 @@ class Tvnamer < Formula
   end
 
   def install
-    xy = Language::Python.major_minor_version "python3"
-    ENV.prepend_create_path "PYTHONPATH", libexec/"vendor/lib/python#{xy}/site-packages"
-    resources.each do |r|
-      r.stage do
-        system "python3", *Language::Python.setup_install_args(libexec/"vendor")
-      end
-    end
-
-    ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python#{xy}/site-packages"
-    system "python3", *Language::Python.setup_install_args(libexec)
-    bin.install Dir["#{libexec}/bin/*"]
-    bin.env_script_all_files(libexec/"bin", PYTHONPATH: ENV["PYTHONPATH"])
+    virtualenv_install_with_resources
   end
 
   test do
