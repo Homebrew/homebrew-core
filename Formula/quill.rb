a1f334debec1fe9a4e6f336f2abd1c41a3e788da
Dawid Dziurla <dawidd0811@gmail.com>
2022-05-20 20:01:53 +0200

    needs cxx17 to build

--- Formula/quill.rb
+++ Formula/quill.rb
@@ -17,9 +17,13 @@ class Quill < Formula
 
   depends_on "cmake" => :build
 
-  def install
-    ENV.cxx11
+  on_linux do
+    depends_on "gcc"
+  end
 
+  fails_with gcc: "5"
+
+  def install
     mkdir "quill-build" do
       args = std_cmake_args
       args << ".."
