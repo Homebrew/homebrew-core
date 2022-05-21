4fc17ac6774fc7db98454b6ebfb7cc6fa0516bad
Rui Chen <rui@chenrui.dev>
2022-05-21 18:28:42 -0400

    update url and add livecheck
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/xpipe.rb
+++ Formula/xpipe.rb
@@ -1,9 +1,14 @@
 class Xpipe < Formula
   desc "Split input and feed it into the given utility"
   homepage "https://www.netmeister.org/apps/xpipe.html"
-  url "http://www.netmeister.org/apps/xpipe-1.0.tar.gz"
+  url "https://www.netmeister.org/apps/xpipe-1.0.tar.gz"
   sha256 "6f15286f81720c23f1714d6f4999d388d29f67b6ac6cef427a43563322fb6dc1"
   license "BSD-2-Clause"
+
+  livecheck do
+    url "https://www.netmeister.org/apps/"
+    regex(/href=.*?xpipe[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
 
   def install
     system "make", "PREFIX=#{prefix}", "install"
