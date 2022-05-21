b41b6cfca9edcc2403e00f160ce797ee48fe9cf1
Rui Chen <rui@chenrui.dev>
2022-05-21 17:28:29 -0400

    fix style

--- Formula/markdown-toc.rb
+++ Formula/markdown-toc.rb
@@ -19,6 +19,7 @@ class MarkdownToc < Formula
   end
 
   test do
-    assert_equal "- [One](#one)\n- [Two](#two)", shell_output("bash -c \"#{bin}/markdown-toc - <<< $'# One\\n\\n# Two'\"").strip
+    assert_equal "- [One](#one)\n- [Two](#two)", 
+      shell_output("bash -c \"#{bin}/markdown-toc - <<< $'# One\\n\\n# Two'\"").strip
   end
 end
