ca7284edd600b414fe36aae1c5c54886792e5fe7
Xuehai Pan <XuehaiPan@pku.edu.cn>
2022-05-22 20:49:24 +0800

    tepl: depends on libglib

--- Formula/tepl.rb
+++ Formula/tepl.rb
@@ -5,6 +5,7 @@ class Tepl < Formula
       tag:      "6.0.1",
       revision: "2db87c240f86ec05b6707c069dd132f835be653a"
   license "LGPL-2.1-or-later"
+  revision 1
 
   bottle do
     sha256 arm64_monterey: "36c69d3ea19fc801b127e7bceebb89fef2a5dbcd4ab404fb0f7e69b69839d4cf"
@@ -55,7 +56,7 @@ class Tepl < Formula
     freetype = Formula["freetype"]
     gdk_pixbuf = Formula["gdk-pixbuf"]
     gettext = Formula["gettext"]
-    glib = Formula["glib"]
+    libglib = Formula["libglib"]
     gtkx3 = Formula["gtk+3"]
     gtksourceview4 = Formula["gtksourceview4"]
     harfbuzz = Formula["harfbuzz"]
@@ -74,9 +75,9 @@ class Tepl < Formula
       -I#{freetype.opt_include}/freetype2
       -I#{gdk_pixbuf.opt_include}/gdk-pixbuf-2.0
       -I#{gettext.opt_include}
-      -I#{glib.opt_include}/gio-unix-2.0/
-      -I#{glib.opt_include}/glib-2.0
-      -I#{glib.opt_lib}/glib-2.0/include
+      -I#{libglib.opt_include}/gio-unix-2.0/
+      -I#{libglib.opt_include}/glib-2.0
+      -I#{libglib.opt_lib}/glib-2.0/include
       -I#{gtksourceview4.opt_include}/gtksourceview-4
       -I#{gtkx3.opt_include}/gtk-3.0
       -I#{harfbuzz.opt_include}/harfbuzz
@@ -93,7 +94,7 @@ class Tepl < Formula
       -L#{cairo.opt_lib}
       -L#{gdk_pixbuf.opt_lib}
       -L#{gettext.opt_lib}
-      -L#{glib.opt_lib}
+      -L#{libglib.opt_lib}
       -L#{gtksourceview4.opt_lib}
       -L#{gtkx3.opt_lib}
       -L#{lib}
