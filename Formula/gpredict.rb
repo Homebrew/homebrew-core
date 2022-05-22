b7a3a932448fcfb39a9f46d080979c7d30f08f36
Xuehai Pan <XuehaiPan@pku.edu.cn>
2022-05-22 20:28:29 +0800

    gpredict: depends on libglib

--- Formula/gpredict.rb
+++ Formula/gpredict.rb
@@ -4,7 +4,7 @@ class Gpredict < Formula
   url "https://github.com/csete/gpredict/releases/download/v2.2.1/gpredict-2.2.1.tar.bz2"
   sha256 "e759c4bae0b17b202a7c0f8281ff016f819b502780d3e77b46fe8767e7498e43"
   license "GPL-2.0-or-later"
-  revision 2
+  revision 3
 
   livecheck do
     url :stable
@@ -27,10 +27,10 @@ class Gpredict < Formula
   depends_on "pkg-config" => :build
   depends_on "adwaita-icon-theme"
   depends_on "gettext"
-  depends_on "glib"
   depends_on "goocanvas"
   depends_on "gtk+3"
   depends_on "hamlib"
+  depends_on "libglib"
 
   uses_from_macos "perl" => :build
   uses_from_macos "curl"
