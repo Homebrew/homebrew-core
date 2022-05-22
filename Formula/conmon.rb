b0b0497bfa7665fa58dd0fce314b73ff2a03f246
Xuehai Pan <XuehaiPan@pku.edu.cn>
2022-05-22 19:38:39 +0800

    conmon: depends on libglib

--- Formula/conmon.rb
+++ Formula/conmon.rb
@@ -4,6 +4,7 @@ class Conmon < Formula
   url "https://github.com/containers/conmon/archive/refs/tags/v2.1.0.tar.gz"
   sha256 "874909d831feac75e452562087f6ea0eb39848ef144397bdd8f0daf579c5ee85"
   license "Apache-2.0"
+  revision 1
 
   bottle do
     sha256 cellar: :any_skip_relocation, x86_64_linux: "544c5420a2bd61fe04f0f25c6390b657966ab895b5cd52717cae44995ea0e443"
@@ -12,7 +13,7 @@ class Conmon < Formula
   depends_on "go" => :build
   depends_on "pkg-config" => :build
 
-  depends_on "glib"
+  depends_on "libglib"
   depends_on "libseccomp"
   depends_on :linux
   depends_on "systemd"
