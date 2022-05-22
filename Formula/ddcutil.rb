9447e7a9cb16eb4e7c5787affd3359f6148ad1d0
Xuehai Pan <XuehaiPan@pku.edu.cn>
2022-05-22 19:42:26 +0800

    ddcutil: depends on libglib

--- Formula/ddcutil.rb
+++ Formula/ddcutil.rb
@@ -4,15 +4,16 @@ class Ddcutil < Formula
   url "https://www.ddcutil.com/tarballs/ddcutil-1.2.2.tar.gz"
   sha256 "822faa342b0bcbb41f15c1910a2bc9d50ffd78f695d6dd04387964010b69cba1"
   license "GPL-2.0-or-later"
+  revision 1
 
   bottle do
     sha256 cellar: :any_skip_relocation, x86_64_linux: "cf0178655ff935497c7790df894ff2d01207ef5be117528498d44b6e4e7c85ab"
   end
 
-  depends_on "glib"
   depends_on "i2c-tools"
   depends_on "kmod"
   depends_on "libdrm"
+  depends_on "libglib"
   depends_on "libusb"
   depends_on "libxrandr"
   depends_on :linux
