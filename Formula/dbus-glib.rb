81d10881a4e0eba6ec01b28c566212ca43e496d4
Xuehai Pan <XuehaiPan@pku.edu.cn>
2022-05-22 19:41:01 +0800

    dbus-glib: depends on libglib

--- Formula/dbus-glib.rb
+++ Formula/dbus-glib.rb
@@ -3,6 +3,7 @@ class DbusGlib < Formula
   homepage "https://wiki.freedesktop.org/www/Software/DBusBindings/"
   url "https://dbus.freedesktop.org/releases/dbus-glib/dbus-glib-0.112.tar.gz"
   sha256 "7d550dccdfcd286e33895501829ed971eeb65c614e73aadb4a08aeef719b143a"
+  revision 1
 
   livecheck do
     url "https://dbus.freedesktop.org/releases/dbus-glib/"
@@ -22,7 +23,7 @@ class DbusGlib < Formula
   depends_on "pkg-config" => :build
   depends_on "dbus"
   depends_on "gettext"
-  depends_on "glib"
+  depends_on "libglib"
 
   def install
     system "./configure", "--disable-dependency-tracking",
