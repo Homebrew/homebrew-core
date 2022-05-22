53b01bd848da6e2f8017cda4f5bb24cbea2500f6
Xuehai Pan <XuehaiPan@pku.edu.cn>
2022-05-22 19:48:46 +0800

    gtk-pixbuf: depends on libglib

--- Formula/gdk-pixbuf.rb
+++ Formula/gdk-pixbuf.rb
@@ -4,6 +4,7 @@ class GdkPixbuf < Formula
   url "https://download.gnome.org/sources/gdk-pixbuf/2.42/gdk-pixbuf-2.42.8.tar.xz"
   sha256 "84acea3acb2411b29134b32015a5b1aaa62844b19c4b1ef8b8971c6b0759f4c6"
   license "LGPL-2.1-or-later"
+  revision 1
 
   bottle do
     sha256 arm64_monterey: "a64b406a58be74567a9409ad763937b14a2619754734e5ee91c840965d1bd354"
@@ -18,8 +19,8 @@ class GdkPixbuf < Formula
   depends_on "meson" => :build
   depends_on "ninja" => :build
   depends_on "pkg-config" => :build
-  depends_on "glib"
   depends_on "jpeg"
+  depends_on "libglib"
   depends_on "libpng"
   depends_on "libtiff"
 
@@ -93,20 +94,20 @@ class GdkPixbuf < Formula
       }
     EOS
     gettext = Formula["gettext"]
-    glib = Formula["glib"]
+    libglib = Formula["libglib"]
     libpng = Formula["libpng"]
     pcre = Formula["pcre"]
     flags = (ENV.cflags || "").split + (ENV.cppflags || "").split + (ENV.ldflags || "").split
     flags += %W[
       -I#{gettext.opt_include}
-      -I#{glib.opt_include}/glib-2.0
-      -I#{glib.opt_lib}/glib-2.0/include
+      -I#{libglib.opt_include}/glib-2.0
+      -I#{libglib.opt_lib}/glib-2.0/include
       -I#{include}/gdk-pixbuf-2.0
       -I#{libpng.opt_include}/libpng16
       -I#{pcre.opt_include}
       -D_REENTRANT
       -L#{gettext.opt_lib}
-      -L#{glib.opt_lib}
+      -L#{libglib.opt_lib}
       -L#{lib}
       -lgdk_pixbuf-2.0
       -lglib-2.0
