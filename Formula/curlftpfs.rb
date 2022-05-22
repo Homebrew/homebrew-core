93281c23872b97c7ffca0d1b2334a3d33fd5bdca
Xuehai Pan <XuehaiPan@pku.edu.cn>
2022-05-22 19:39:24 +0800

    curlftpfs: depends on libglib

--- Formula/curlftpfs.rb
+++ Formula/curlftpfs.rb
@@ -3,7 +3,7 @@ class Curlftpfs < Formula
   homepage "https://curlftpfs.sourceforge.io/"
   url "https://downloads.sourceforge.net/project/curlftpfs/curlftpfs/0.9.2/curlftpfs-0.9.2.tar.gz"
   sha256 "4eb44739c7078ba0edde177bdd266c4cfb7c621075f47f64c85a06b12b3c6958"
-  revision 1
+  revision 2
   head ":pserver:anonymous:@curlftpfs.cvs.sourceforge.net:/cvsroot/curlftpfs", using: :cvs
 
   bottle do
@@ -17,7 +17,7 @@ class Curlftpfs < Formula
   depends_on "automake" => :build
   depends_on "libtool" => :build
   depends_on "pkg-config" => :build
-  depends_on "glib"
+  depends_on "libglib"
 
   # TODO: depend on specific X11 formulae instead
 
