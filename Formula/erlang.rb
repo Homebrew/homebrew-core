44ef2b82b491bcb36be04d5873dbc013b89a301f
fishtreesugar <fishtreesugar@gmail.com>
2022-05-23 18:10:11 +0800

    erlang: revision bump

--- Formula/erlang.rb
+++ Formula/erlang.rb
@@ -5,6 +5,7 @@ class Erlang < Formula
   url "https://github.com/erlang/otp/releases/download/OTP-25.0/otp_src_25.0.tar.gz"
   sha256 "2d7678c9bc6fcf3a1242c4d1c3864855d85e73ade792cd80adb8a9f379996711"
   license "Apache-2.0"
+  revision 1
 
   livecheck do
     url :stable
@@ -28,7 +29,7 @@ class Erlang < Formula
     depends_on "libtool" => :build
   end
 
-  depends_on "openssl@1.1"
+  depends_on "openssl"
   depends_on "wxwidgets" # for GUI apps like observer
 
   resource "html" do
