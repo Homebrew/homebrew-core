ae38373114559f394224f69b19200e115324fbad
Jared Allard <jared.allard@outreach.io>
2022-05-27 10:02:47 -0700

    finalize

--- Formula/stencil.rb
+++ Formula/stencil.rb
@@ -1,26 +1,23 @@
+require 'fileutils'
+
 class Stencil < Formula
   desc "Smart templating engine for service development"
   homepage "https://engineering.outreach.io/stencil/"
-  url "https://github.com/getoutreach/stencil/archive/refs/tags/v1.15.1.tar.gz"
-  sha256 "4b3019a99c70751e7d670d911cf8e20dd827caeb1907730425479bc7824770cf"
+  url "https://github.com/getoutreach/stencil/archive/refs/tags/v1.17.1.tar.gz"
+  sha256 "9b5c33b55537536b0c051e949a612b2ab39bc2c692a375c0c6ef7aa33110f2b7"
   license "Apache-2.0"
   head "https://github.com/getoutreach/stencil.git", branch: "main"
 
   depends_on "go" => :build
 
   def install
-    system "go", "build", *std_go_args(ldflags: "-s -w -X github.com/getoutreach/gobox/pkg/app.Version=v#{version}"),
+    system "go", "build", *std_go_args(ldflags: "-s -w -X github.com/getoutreach/gobox/pkg/app.Version=v#{version} -X github.com/getoutreach/gobox/pkg/updater/Disabled=true"),
       "./cmd/stencil"
   end
 
   test do
-    site = testpath/"test-123"
-
-    # Create the directory and change into it because stencil doesn't _currently_ do that for you.
-    Dir.mkdir(site)
-    Dir.chdir(site)
-
-    system "#{bin}/stencil", "create", "templaterepository", "github.com/myorg/#{File.basename(site)}"
-    assert_predicate testpath/"#{site}/manifest.yaml", :exist?
+    File.write("service.yaml", "name: test")
+    system "#{bin}/stencil"
+    assert_predicate testpath/"stencil.lock", :exist?
   end
 end
