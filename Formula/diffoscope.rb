3364d4e15e92dbf98c565431f9943d77d4f9086a
Porkepix <porkepix@gmail.com>
2022-05-27 14:58:01 +0200

    diffoscope 214

--- Formula/diffoscope.rb
+++ Formula/diffoscope.rb
@@ -3,8 +3,8 @@ class Diffoscope < Formula
 
   desc "In-depth comparison of files, archives, and directories"
   homepage "https://diffoscope.org"
-  url "https://files.pythonhosted.org/packages/0f/63/9e720347fce585a9b3f1406d5289de39a97ccf8b18e5ebf0f79379031674/diffoscope-213.tar.gz"
-  sha256 "ce41bcde13df94c00c7bad57ea5662471a247b08f847ae20a41469341592d0ce"
+  url "https://files.pythonhosted.org/packages/d0/15/81dfdc3d3b736d0c42832df5d6e20a224d59804e3756826931ca60b4368f/diffoscope-214.tar.gz"
+  sha256 "d670774498667be9dde75b14d94f57526834fc6b1d6aff89f6c68570a2246b03"
   license "GPL-3.0-or-later"
 
   bottle do
@@ -36,8 +36,8 @@ class Diffoscope < Formula
   end
 
   resource "python-magic" do
-    url "https://files.pythonhosted.org/packages/f7/46/fecfd32c126d26c8dd5287095cad01356ec0a761205f0b9255998bff96d1/python-magic-0.4.25.tar.gz"
-    sha256 "21f5f542aa0330f5c8a64442528542f6215c8e18d2466b399b0d9d39356d83fc"
+    url "https://files.pythonhosted.org/packages/ac/68/2e05edacfbf350cf8c7a4c382aed89a918be03a8f27383de77aa3c3b53df/python-magic-0.4.26.tar.gz"
+    sha256 "8262c13001f904ad5b724d38b5e5b5f17ec0450ae249def398a62e4e33108a50"
   end
 
   def install
