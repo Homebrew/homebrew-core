fa922799927863194f7e3677d265ab60d01a3b31
Sven Over <sven@cord.com>
2022-05-31 09:52:37 +0100

    spr 1.3.3 (new formula)

---
+++ Formula/spr.rb
@@ -0,0 +1,23 @@
+class Spr < Formula
+  desc "Submit pull requests for individual, amendable, rebaseable commits to GitHub"
+  homepage "https://github.com/getcord/spr"
+  url "https://github.com/getcord/spr.git",
+    tag:      "v1.3.3",
+    revision: "1f6d99ef268643cead1341cfc51e57b84c50b962"
+  license "MIT"
+
+  depends_on "rust" => :build
+
+  on_linux do
+    depends_on "pkg-config" => :build
+    depends_on "zlib"
+  end
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    assert_match "spr #{version}", shell_output("#{bin}/spr --version")
+  end
+end
