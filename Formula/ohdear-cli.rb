aa70bfa8cfe9a3cd0ef90fccbbe3ac77b1e0d2c4
Owen Voke <development@voke.dev>
2022-06-03 20:55:58 +0100

    ohdear-cli (fix URLs)

--- Formula/ohdear-cli.rb
+++ Formula/ohdear-cli.rb
@@ -1,7 +1,7 @@
 class OhdearCli < Formula
   desc "Tool to manage your Oh Dear sites"
-  homepage "https://github.com/nunomaduro/ohdear-cli"
-  url "https://github.com/nunomaduro/ohdear-cli/releases/download/v3.2.0/ohdear-cli.phar"
+  homepage "https://github.com/ohdearapp/ohdear-cli"
+  url "https://github.com/ohdearapp/ohdear-cli/releases/download/v3.2.0/ohdear-cli.phar"
   sha256 "9bc29673e7bdb6749cbe798d01386351d7f8f93cf33b5721d6be6179817a3980"
   license "MIT"
 
