1f81e004b18d2243006840e8efb080d19897daaa
Michael Cho <cho-m@tuta.io>
2022-06-08 00:15:13 -0700

    opencl-icd-loader 2022.05.18 (new formula)

---
+++ Formula/opencl-icd-loader.rb
@@ -0,0 +1,33 @@
+class OpenclIcdLoader < Formula
+  desc "OpenCL Installable Client Driver (ICD) Loader"
+  homepage "https://www.khronos.org/registry/OpenCL/"
+  url "https://github.com/KhronosGroup/OpenCL-ICD-Loader/archive/refs/tags/v2022.05.18.tar.gz"
+  sha256 "71f70bba797a501b13b6b0905dc852f3fd6e264d74ce294f2df98d29914c4303"
+  license "Apache-2.0"
+  head "https://github.com/KhronosGroup/OpenCL-ICD-Loader.git", branch: "main"
+
+  keg_only :shadowed_by_macos, "macOS provides OpenCL.framework"
+
+  depends_on "cmake" => :build
+  depends_on "opencl-headers" => [:build, :test]
+
+  conflicts_with "ocl-icd"
+
+  def install
+    inreplace "loader/icd_platform.h", "\"/etc/", "\"#{etc}/"
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+    pkgshare.install "test/loader_test"
+    (pkgshare/"loader_test").install "test/inc/platform", "test/log/icd_test_log.c"
+  end
+
+  test do
+    cp_r (pkgshare/"loader_test").children, testpath
+    system ENV.cc, *testpath.glob("*.c"), "-o", "icd_loader_test",
+                   "-DCL_TARGET_OPENCL_VERSION=300",
+                   "-I#{Formula["opencl-headers"].opt_include}", "-I#{testpath}",
+                   "-L#{lib}", "-lOpenCL"
+    # TODO: Fix test
+  end
+end
