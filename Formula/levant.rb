db10c4dee4fb919f55a617f51faca13d5a96dd9a
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2022-06-08 19:46:56 +0200

    levant 0.3.1 (new formula)

---
+++ Formula/levant.rb
@@ -0,0 +1,39 @@
+class Levant < Formula
+  desc "Templating and deployment tool for HashiCorp Nomad"
+  homepage "https://github.com/hashicorp/levant"
+  url "https://github.com/hashicorp/levant/archive/v0.3.1.tar.gz"
+  sha256 "a2e078168cfe24966209f34f9c0a677df3ff74e22a0eb387103c0fcb68187a53"
+  license "MPL-2.0"
+  head "https://github.com/hashicorp/levant.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w")
+  end
+
+  test do
+    (testpath/"template.nomad").write <<~EOS
+      resources {
+          cpu    = [[.resources.cpu]]
+          memory = [[.resources.memory]]
+      }
+    EOS
+
+    (testpath/"variables.json").write <<~EOS
+      {
+        "resources":{
+          "cpu":250,
+          "memory":512,
+          "network":{
+            "mbits":10
+          }
+        }
+      }
+    EOS
+
+    assert_match "resources {\n    cpu    = 250\n    memory = 512\n}\n",
+      shell_output("#{bin}/levant render -var-file=" +
+                   (testpath/"variables.json") + " " + (testpath/"template.nomad"))
+  end
+end
