637d25ffbd8b686e35a2a0ca0d7bd7d13e7a7405
肖明浩 <88817514+xmh1011@users.noreply.github.com>
2022-06-10 19:28:34 +0800

    Update cnosdb.rb

--- Formula/cnosdb.rb
+++ Formula/cnosdb.rb
@@ -11,10 +11,73 @@ class Cnosdb < Formula
   def install
     ENV["GOBIN"] = buildpath
     system "go", "install", "./..."
-    bin.install %w[cnosdb-cli cnosdb-ctl cnosdb-meta cnosdb-inspect cnosdb-tools]
+    bin.install %w[cnosdb cnosdb-cli cnosdb-ctl cnosdb-meta cnosdb-inspect cnosdb-tools]
+    etc.install "etc/cnosdb.sample.toml" => "cnosdb.conf"
+    inreplace etc/"cnosdb.conf" do |s|
+      s.gsub! "/var/lib/cnosdb/data", "#{var}/cnosdb/data"
+      s.gsub! "/var/lib/cnosdb/meta", "#{var}/cnosdb/meta"
+      s.gsub! "/var/lib/cnosdb/wal", "#{var}/cnosdb/wal"
+    end
+    (var/"cnosdb/data").mkpath
+    (var/"cnosdb/meta").mkpath
+    (var/"cnosdb/wal").mkpath
   end
+  plist_options :manual => "cnosdb --config #{HOMEBREW_PREFIX}/etc/cnosdb.conf"
 
+  def plist
+    <<~EOS
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+        <dict>
+          <key>KeepAlive</key>
+          <dict>
+            <key>SuccessfulExit</key>
+            <false/>
+          </dict>
+          <key>Label</key>
+          <string>#{plist_name}</string>
+          <key>ProgramArguments</key>
+          <array>
+            <string>#{opt_bin}/cnosdb</string>
+            <string>-config</string>
+            <string>#{HOMEBREW_PREFIX}/etc/cnosdb.conf</string>
+          </array>
+          <key>RunAtLoad</key>
+          <true/>
+          <key>WorkingDirectory</key>
+          <string>#{var}</string>
+          <key>StandardErrorPath</key>
+          <string>#{var}/log/cnosdb.log</string>
+          <key>StandardOutPath</key>
+          <string>#{var}/log/cnosdb.log</stringF>
+          <key>SoftResourceLimits</key>
+          <dict>
+            <key>NumberOfFiles</key>
+            <integer>10240</integer>
+          </dict>
+        </dict>
+      </plist>
+    EOS
+  end
+  
   test do
-    system "#{bin}/cnosdb-cli"
+    (testpath/"config.toml").write shell_output("#{bin}/cnosdb config")
+    inreplace testpath/"config.toml" do |s|
+      s.gsub! %r{/.*/.cnosdb/data}, "#{testpath}/cnosdb/data"
+      s.gsub! %r{/.*/.cnosdb/meta}, "#{testpath}/cnosdb/meta"
+      s.gsub! %r{/.*/.cnosdb/wal}, "#{testpath}/cnosdb/wal"
+    end
+    begin
+      pid = fork do
+        exec "#{bin}/cnosdb --config #{testpath}/config.toml"
+      end
+      sleep 6
+      output = shell_output("curl -Is localhost:8086/ping")
+      assert_match /X-cnosdb-Version:/, output
+    ensure
+      Process.kill("SIGTERM", pid)
+      Process.wait(pid)
+    end
   end
 end
