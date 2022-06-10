b174df08901ac73cf26d4e3bf2632352a3401001
肖明浩 <88817514+xmh1011@users.noreply.github.com>
2022-06-10 19:55:32 +0800

    Update cnosdb.rb

--- Formula/cnosdb.rb
+++ Formula/cnosdb.rb
@@ -22,43 +22,6 @@ class Cnosdb < Formula
     (var/"cnosdb/meta").mkpath
     (var/"cnosdb/wal").mkpath
   end
-  plist_options :manual => "cnosdb --config #{HOMEBREW_PREFIX}/etc/cnosdb.conf"
-  def plist
-    <<~EOS
-      <?xml version="1.0" encoding="UTF-8"?>
-      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-      <plist version="1.0">
-        <dict>
-          <key>KeepAlive</key>
-          <dict>
-            <key>SuccessfulExit</key>
-            <false/>
-          </dict>
-          <key>Label</key>
-          <string>#{plist_name}</string>
-          <key>ProgramArguments</key>
-          <array>
-            <string>#{opt_bin}/cnosdb</string>
-            <string>-config</string>
-            <string>#{HOMEBREW_PREFIX}/etc/cnosdb.conf</string>
-          </array>
-          <key>RunAtLoad</key>
-          <true/>
-          <key>WorkingDirectory</key>
-          <string>#{var}</string>
-          <key>StandardErrorPath</key>
-          <string>#{var}/log/cnosdb.log</string>
-          <key>StandardOutPath</key>
-          <string>#{var}/log/cnosdb.log</stringF>
-          <key>SoftResourceLimits</key>
-          <dict>
-            <key>NumberOfFiles</key>
-            <integer>10240</integer>
-          </dict>
-        </dict>
-      </plist>
-    EOS
-  end
   
   test do
     (testpath/"config.toml").write shell_output("#{bin}/cnosdb config")
@@ -73,7 +36,7 @@ class Cnosdb < Formula
       end
       sleep 6
       output = shell_output("curl -Is localhost:8086/ping")
-      assert_match /X-cnosdb-Version:/, output
+      assert_match X-cnosdb-Version, output
     ensure
       Process.kill("SIGTERM", pid)
       Process.wait(pid)
