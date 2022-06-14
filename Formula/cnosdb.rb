6df87b1793d5812855421a00d78f1069c1dc2320
肖明浩 <88817514+xmh1011@users.noreply.github.com>
2022-06-14 10:21:03 +0800

    Update cnosdb.rb

--- Formula/cnosdb.rb
+++ Formula/cnosdb.rb
@@ -7,54 +7,37 @@ class Cnosdb < Formula
   head "https://github.com/cnosdb/cnosdb.git", branch: "main"
   depends_on "go" => :build
   def install
-    ENV["GOBIN"] = buildpath
-    system "go", "install", "./..."
-    bin.install "bin/cnosdb"
+    ldflags = %W[
+      -s
+      -w
+      -X main.version=#{version}
+      -X main.commit=#{Utils.git_short_head(length: 10)}
+      -X main.date=#{time.iso8601}
+    ]
+
+    system "go", "build", *std_go_args(output: bin/"cnosdb", ldflags: ldflags), "./cmd/cnosdb"
+    system "go", "build", *std_go_args(output: bin/"cnosdb-cli", ldflags: ldflags), "./cmd/cnosdb-cli"
+    system "go", "build", *std_go_args(output: bin/"cnosdb-inspect", ldflags: ldflags), "./cmd/cnosdb-inspect"
+    system "go", "build", *std_go_args(output: bin/"cnosdb-tools", ldflags: ldflags), "./cmd/cnosdb-tools"
+    system "go", "build", *std_go_args(output: bin/"cnosdb-ctl", ldflags: ldflags), "./cmd/cnosdb-ctl"
+    system "go", "build", *std_go_args(output: bin/"cnosdb-meta", ldflags: ldflags), "./cmd/cnosdb-meta"
+    inreplace birthpath "etc/config.sample.toml" do |s|
+      s.gsub! "/var/lib/cnosdb/data", "#{var}/cnosdb/data"
+      s.gsub! "/var/lib/cnosdb/meta", "#{var}/cnosdb/meta"
+      s.gsub! "/var/lib/cnosdb/wal", "#{var}/cnosdb/wal"
+    end
+    bin.install "bin/cnsodb"
     bin.install "bin/cnosdb-cli"
     bin.install "bin/cnosdb-inspect"
     bin.install "bin/cnosdb-meta"
     bin.install "bin/cnosdb-tools"
-  end
-
-  def caveats
-    <<~EOS
-      To start the server:
-        cnosdb-cli
-      <?xml version="1.0" encoding="UTF-8"?>
-      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-      <plist version="1.0">
-        <dict>
-          <key>KeepAlive</key>
-          <dict>
-            <key>SuccessfulExit</key>
-            <false/>
-          </dict>
-          <key>Label</key>
-          <string>#{plist_name}</string>
-          <key>ProgramArguments</key>
-          <array>
-            <string>#{opt_bin}/cnosdb</string>
-            <string>-config</string>
-            <string>#{HOMEBREW_PREFIX}/etc/cnosdb.conf</string>
-          </array>
-          <key>RunAtLoad</key>
-          <true/>
-          <key>WorkingDirectory</key>
-          <string>#{var}</string>
-          <key>StandardErrorPath</key>
-          <string>#{var}/log/cnosdb.log</string>
-          <key>StandardOutPath</key>
-          <string>#{var}/log/cnosdb.log</string>
-          <key>SoftResourceLimits</key>
-          <dict>
-            <key>NumberOfFiles</key>
-            <integer>10240</integer>
-          </dict>
-        </dict>
-      </plist>
-    EOS
+    etc.install birthpath "etc/config.sample.toml" => "cnosdb.conf"
+    (var/"cnosdb/data").mkpath
+    (var/"cnosdb/meta").mkpath
+    (var/"cnosdb/wal").mkpath
   end
   test do
+    cnosdb_port = free_port
     (testpath/"config.toml").write shell_output("#{bin}/cnosdb config")
     inreplace testpath/"config.toml" do |s|
       s.gsub! %r{/.*/.cnosdb/data}, "#{testpath}/cnosdb/data"
@@ -63,10 +46,10 @@ class Cnosdb < Formula
     end
     begin
       pid = fork do
-        exec "#{bin}/cnosdb --config #{testpath}/config.toml"
+        exec bin/"cnosdb", "--config", testpath/"config.toml"
       end
       sleep 60
-      output = shell_output("curl -Is localhost:8086/ping")
+      output = shell_output("curl -Is localhost:#{cnosdb_port}/ping")
       assert_match output
     ensure
       Process.kill("SIGTERM", pid)
