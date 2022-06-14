f264b682fa6943c98ba235615be38b8a1aa42b52
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2022-06-14 20:16:02 +0900

    poac 0.3.4 (new formula)

---
+++ Formula/poac.rb
@@ -0,0 +1,34 @@
+class Poac < Formula
+  desc "Package manager for C++"
+  homepage "https://github.com/poacpm/poac"
+  url "https://github.com/poacpm/poac/archive/refs/tags/0.3.4.tar.gz"
+  sha256 "972727bd1f45f9cb1a0aae1994533fae75234614ffe5df2a316690698db9dd74"
+  license "Apache-2.0"
+  head "https://github.com/poacpm/poac.git", branch: "main"
+
+  depends_on "cmake" => :build
+  depends_on "boost"
+  depends_on macos: :big_sur # C++20
+  depends_on "openssl@3"
+
+  on_linux do
+    depends_on "gcc"
+  end
+  fails_with gcc: "5" # C++20
+
+  def install
+    system "cmake", "-B", "build", *std_cmake_args
+    system "cmake", "--build", "build", "--target", "install"
+
+    man1.install (buildpath/"src/etc/man/man1").children
+    bash_completion.install_symlink "src/etc/poac.bash" => "poac"
+    zsh_completion.install_symlink "src/etc/poac.bash" => "_poac"
+  end
+
+  test do
+    system bin/"poac", "create", "hello_world"
+    cd "hello_world" do
+      assert_match "Hello, world!", shell_output("#{bin}/poac run")
+    end
+  end
+end
