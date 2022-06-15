99c56c35f6525d6c70c8836d5329e9f751fa2189
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2022-06-15 17:02:22 +0900

    poac: use `uses_from_macos` for `libarchive`

--- Formula/poac.rb
+++ Formula/poac.rb
@@ -8,10 +8,11 @@ class Poac < Formula
 
   depends_on "cmake" => :build
   depends_on "boost"
-  depends_on "libarchive"
   depends_on "libgit2"
   depends_on macos: :big_sur # C++20
   depends_on "openssl@1.1"
+
+  uses_from_macos "libarchive"
 
   on_linux do
     depends_on "gcc"
