7cb4f48af5025090939d1f97825bfa8ab1311262
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2022-06-15 13:17:04 -0400

    dovecot 2.3.19.1

--- Formula/dovecot.rb
+++ Formula/dovecot.rb
@@ -1,10 +1,9 @@
 class Dovecot < Formula
   desc "IMAP/POP3 server"
   homepage "https://dovecot.org/"
-  url "https://dovecot.org/releases/2.3/dovecot-2.3.19.tar.gz"
-  sha256 "0173f693d441b6248b8a62aa5fd690021a1f04a12902653e0bf2e5b012fe437b"
+  url "https://dovecot.org/releases/2.3/dovecot-2.3.19.1.tar.gz"
+  sha256 "db5abcd87d7309659ea6b45b2cb6ee9c5f97486b2b719a5dd05a759e1f6a5c51"
   license all_of: ["BSD-3-Clause", "LGPL-2.1-or-later", "MIT", "Unicode-DFS-2016", :public_domain]
-  revision 1
 
   livecheck do
     url "https://www.dovecot.org/download/"
