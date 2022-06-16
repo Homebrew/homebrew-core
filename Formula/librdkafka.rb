fb961188df57b176958702f5ccb8f49a28196758
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2022-06-16 10:31:49 -0400

    librdkafka 1.9.0

--- Formula/librdkafka.rb
+++ Formula/librdkafka.rb
@@ -1,8 +1,8 @@
 class Librdkafka < Formula
   desc "Apache Kafka C/C++ library"
   homepage "https://github.com/edenhill/librdkafka"
-  url "https://github.com/edenhill/librdkafka/archive/refs/tags/v1.8.2.tar.gz"
-  sha256 "6a747d293a7a4613bd2897e28e8791476fbe1ae7361f2530a876e0fd483482a6"
+  url "https://github.com/edenhill/librdkafka/archive/refs/tags/v1.9.0.tar.gz"
+  sha256 "59b6088b69ca6cf278c3f9de5cd6b7f3fd604212cd1c59870bc531c54147e889"
   license "BSD-2-Clause"
   head "https://github.com/edenhill/librdkafka.git", branch: "master"
 
