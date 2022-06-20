02ae25ef8ff3d7403b09a2756d2b28592149c69c
Vlad <vzhd1701@gmail.com>
2022-06-20 20:34:56 +0500

    evernote-backup: update python version

--- Formula/evernote-backup.rb
+++ Formula/evernote-backup.rb
@@ -7,7 +7,7 @@ class EvernoteBackup < Formula
   sha256 "0f07f9674214f7b7fee42871b009684564bf9df079b383dab152ab51a22c6dd3"
   license "MIT"
 
-  depends_on "python@3.9"
+  depends_on "python@3.10"
 
   resource "certifi" do
     url "https://files.pythonhosted.org/packages/6c/ae/d26450834f0acc9e3d1f74508da6df1551ceab6c2ce0766a593362d6d57f/certifi-2021.10.8.tar.gz"
