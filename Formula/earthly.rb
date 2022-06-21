ad71f29e33438b6304113186dbc8f9a770f1e297
Griswold the cat <github-service@earthly.dev>
2022-06-21 00:08:55 +0000

    fix typo

--- Formula/earthly.rb
+++ Formula/earthly.rb
@@ -18,7 +18,7 @@ class Earthly < Formula
   depends_on "go" => :build
 
   def install
-    # the earthly_gitsha variable is required by the earthly release script, moving this value it into
+    # the earthly_gitsha variable is required by the earthly release script, moving this value into
     # the ldflags string will break the upstream release process.
     earthly_gitsha = "7e4f1df4c124db1644d51d312b19313217cbe478"
 
