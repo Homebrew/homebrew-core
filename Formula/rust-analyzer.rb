1c683857667582ce165584ba1586bdac0f7e2377
peeweep <peeweep@0x0.ee>
2022-06-22 06:42:01 +0000

    rust-analyzer: remove patch
    Signed-off-by: peeweep <peeweep@0x0.ee>

--- Formula/rust-analyzer.rb
+++ Formula/rust-analyzer.rb
@@ -18,9 +18,6 @@ class RustAnalyzer < Formula
 
   depends_on "rust" => :build
 
-  # Remove this patch after rust 1.60 (https://github.com/rust-lang/rust-analyzer/issues/12080)
-  patch :DATA
-
   def install
     cd "crates/rust-analyzer" do
       system "cargo", "install", "--bin", "rust-analyzer", *std_cargo_args
@@ -76,22 +73,3 @@ class RustAnalyzer < Formula
     assert_match output, pipe_output("#{bin}/rust-analyzer", input, 0)
   end
 end
-
-
-__END__
-diff --git a/Cargo.lock b/Cargo.lock
-index 62b2ac86f..bc06c853d 100644
---- a/Cargo.lock
-+++ b/Cargo.lock
-@@ -1546,9 +1546,9 @@ checksum = "f2dd574626839106c320a323308629dcb1acfc96e32a8cba364ddc61ac23ee83"
-
- [[package]]
- name = "smol_str"
--version = "0.1.23"
-+version = "0.1.21"
- source = "registry+https://github.com/rust-lang/crates.io-index"
--checksum = "7475118a28b7e3a2e157ce0131ba8c5526ea96e90ee601d9f6bb2e286a35ab44"
-+checksum = "61d15c83e300cce35b7c8cd39ff567c1ef42dde6d4a1a38dbdbf9a59902261bd"
- dependencies = [
-  "serde",
- ]
