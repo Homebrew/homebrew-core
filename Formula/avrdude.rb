6852d88b35956a6d55d00a7cbbd88f0f4541cde5
Michka Popoff <michkapopoff@gmail.com>
2022-06-22 20:56:37 +0200

    avrdude: drop libelf dependency
    libelf is deprecated
    It looks like elfutils, the newer equivalent, is only needed on Linux

--- Formula/avrdude.rb
+++ Formula/avrdude.rb
@@ -43,10 +43,6 @@ class Avrdude < Formula
   uses_from_macos "bison" => :build
   uses_from_macos "flex" => :build
 
-  on_macos do
-    depends_on "libelf" => :build
-  end
-
   on_linux do
     depends_on "elfutils"
     depends_on "readline"
