58d040590b50b84018fc934276c2af48d1f428d3
Rui Chen <rui@chenrui.dev>
2022-06-25 15:58:19 -0400

    test against latest LTS, v18
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/nodeenv.rb
+++ Formula/nodeenv.rb
@@ -19,13 +19,13 @@ class Nodeenv < Formula
   end
 
   test do
-    system bin/"nodeenv", "--node=16.0.0", "--prebuilt", "env-16.0.0-prebuilt"
+    system bin/"nodeenv", "--node=18.0.0", "--prebuilt", "env-18.0.0-prebuilt"
     # Dropping into the virtualenv itself requires sourcing activate which
     # isn't easy to deal with. This ensures current Node installed & functional.
-    ENV.prepend_path "PATH", testpath/"env-16.0.0-prebuilt/bin"
+    ENV.prepend_path "PATH", testpath/"env-18.0.0-prebuilt/bin"
 
     (testpath/"test.js").write "console.log('hello');"
     assert_match "hello", shell_output("node test.js")
-    assert_match "v16.0.0", shell_output("node -v")
+    assert_match "v18.0.0", shell_output("node -v")
   end
 end
