79019b0d7dbf3418a8d73d1bbc0e46a52c447d1e
Porkepix <porkepix@gmail.com>
2022-06-26 03:44:06 +0200

    kics 1.5.11-integrations

--- Formula/kics.rb
+++ Formula/kics.rb
@@ -1,9 +1,9 @@
 class Kics < Formula
   desc "Detect vulnerabilities, compliance issues, and misconfigurations"
   homepage "https://kics.io/"
-  url "https://github.com/Checkmarx/kics/archive/refs/tags/v1.5.10-integrations.tar.gz"
-  version "1.5.10-integrations"
-  sha256 "7913464d8b09377415777d6979ddfd9c914c58651e73a65a100d089c3c1845f0"
+  url "https://github.com/Checkmarx/kics/archive/refs/tags/v1.5.11-integrations.tar.gz"
+  version "1.5.11-integrations"
+  sha256 "e1c1d8403fe23522c1431f5c99b12715b23308312dd69e13393dd7ad1a0cf22d"
   license "Apache-2.0"
   head "https://github.com/Checkmarx/kics.git", branch: "master"
 
