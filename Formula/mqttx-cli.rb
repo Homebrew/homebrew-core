308e9a7164fa1838c868b9654f94a2455d47ac82
ysfscream <894402575bt@gmail.com>
2022-06-27 15:13:28 +0800

    mqttx-cli v1.8.0

--- Formula/mqttx-cli.rb
+++ Formula/mqttx-cli.rb
@@ -1,12 +1,15 @@
 class MqttxCli < Formula
-  desc "Open source MQTT 5.0 CLI client and MQTT X on the command-line"
-  homepage "https://github.com/emqx/MQTTX/tree/main/cli"
+  desc "MQTT 5.0 and MQTT X CLI client"
+  homepage "https://mqttx.app"
   url "https://github.com/emqx/MQTTX/releases/download/v1.8.0/mqttx-cli.tar.gz"
-  version "1.8.0"
   sha256 "d9b8db76bb966fa5e9fcdc3b1fc3a17e3302c5dc9cb6f05e20eead814b37241a"
-  license "Apache License 2.0"
+  license "Apache-2.0"
 
   def install
     bin.install "mqttx"
+  end
+
+  test do
+    system "false"
   end
 end
