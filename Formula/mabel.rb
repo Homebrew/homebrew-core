353a01af9e2e2090e348dc8780eef20d19ac66c8
FIGBERT <figbert@figbert.com>
2022-06-28 21:50:39 -0700

    Mabel 0.1.1 (new formula)

---
+++ Formula/mabel.rb
@@ -0,0 +1,28 @@
+class Mabel < Formula
+  desc "Fancy BitTorrent client for the terminal"
+  homepage "https://github.com/smmr-software/mabel"
+  url "https://github.com/smmr-software/mabel.git",
+    tag:      "v0.1.1",
+    revision: "23490e76aea68357b224dd4d7f1aed822ce2cd46"
+  license "GPL-3.0-or-later"
+  head "https://github.com/smmr-software/mabel.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = %W[
+      -s -w
+      -X main.version=#{version}
+      -X main.commit=#{Utils.git_head}
+      -X main.builtBy=homebrew
+      -X main.date=#{time.iso8601}
+    ]
+    system "go", "build", *std_go_args(ldflags: ldflags)
+  end
+
+  test do
+    vrsn_out = shell_output("#{bin}/mabel --version")
+    assert_match "Mabel #{version}", vrsn_out
+    assert_match "Built by: homebrew", vrsn_out
+  end
+end
