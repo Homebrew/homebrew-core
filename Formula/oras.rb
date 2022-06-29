5cf9c0e7cb182c43c1a145396897587b7e0fda55
Rui Chen <rui@chenrui.dev>
2022-06-29 18:45:44 -0400

    update build and test
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/oras.rb
+++ Formula/oras.rb
@@ -20,10 +20,10 @@ class Oras < Formula
   def install
     ldflags = %W[
       -s -w
-      -X github.com/oras-project/oras/internal/version.Version=#{version}
-      -X github.com/oras-project/oras/internal/version.BuildMetadata=Homebrew
+      -X oras.land/oras/internal/version.Version=#{version}
+      -X oras.land/oras/internal/version.BuildMetadata=Homebrew
     ]
-    system "go", "build", *std_go_args(ldflags: ldflags.join(" ")), "./cmd/oras"
+    system "go", "build", *std_go_args(ldflags: ldflags), "./cmd/oras"
   end
 
   test do
@@ -37,16 +37,12 @@ class Oras < Formula
       }
     EOS
     (testpath/"test.json").write(contents)
-    hash = Digest::SHA256.hexdigest(contents)
 
     # Although it might not make much sense passing the JSON as both manifest and payload,
     # it helps make the test consistent as the error can randomly switch between either hash
     output = shell_output("oras push localhost:#{port}/test-artifact:v1 " \
                           "--manifest-config test.json:application/vnd.homebrew.test.config.v1+json " \
                           "./test.json 2>&1", 1)
-    assert_match %r{
-       ^Error:\ failed\ to\ do\ request(.*)
-       http://localhost:#{port}/v2/test-artifact/blobs/sha256:#{hash}
-    }x, output
+    assert_match "dial tcp [::1]:#{port}: connect: connection refused", output
   end
 end
