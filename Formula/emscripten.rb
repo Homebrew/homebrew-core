a0b6a097783d669dec386ea0205664b184205d5e
Clément L <porkepix@gmail.com>
2022-07-02 03:44:36 +0200

    emscripten-3.1.15: update resources
    Update binaryen and llvm resources.

--- Formula/emscripten.rb
+++ Formula/emscripten.rb
@@ -50,7 +50,7 @@ class Emscripten < Formula
   # See llvm resource below for instructions on how to update this.
   resource "binaryen" do
     url "https://github.com/WebAssembly/binaryen.git",
-        revision: "bab21052c3e12df7ad2fd3711d0fe6e69e05f449"
+        revision: "b69d3a8fa0d6d7588811ef92067a48eed576e03f"
   end
 
   # emscripten needs argument '-fignore-exceptions', which is only available in llvm >= 12
@@ -61,7 +61,7 @@ class Emscripten < Formula
   # Then use the listed llvm_project_revision for the resource below.
   resource "llvm" do
     url "https://github.com/llvm/llvm-project.git",
-        revision: "7effcbda49ba32991b8955821b8fdbd4f8f303e2"
+        revision: "b69d3a8fa0d6d7588811ef92067a48eed576e03f"
   end
 
   def install
