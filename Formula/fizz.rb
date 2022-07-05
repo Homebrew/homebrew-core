156496851f60f7e206cf5afd5b2d266ca0e44707
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-07-06 00:19:38 +0800

    fizz: revision bump (fmt 9.0.0)

--- Formula/fizz.rb
+++ Formula/fizz.rb
@@ -4,6 +4,7 @@ class Fizz < Formula
   url "https://github.com/facebookincubator/fizz/releases/download/v2022.08.08.00/fizz-v2022.08.08.00.tar.gz"
   sha256 "74cf7c76289f0eed76e6a000b6161d9e7ef27264f7d05d6bd3c2597313d68e03"
   license "BSD-2-Clause"
+  revision 1
   head "https://github.com/facebookincubator/fizz.git", branch: "main"
 
   bottle do
