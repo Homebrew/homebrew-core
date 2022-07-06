657845415de49e53d08bbd44997bf3237639ca60
Bo Anderson <mail@boanderson.me>
2022-07-06 22:50:42 +0100

    go 1.19rc1

--- Formula/go.rb
+++ Formula/go.rb
@@ -1,9 +1,9 @@
 class Go < Formula
   desc "Open source programming language to build simple/reliable/efficient software"
   homepage "https://go.dev/"
-  url "https://go.dev/dl/go1.18.3.src.tar.gz"
-  mirror "https://fossies.org/linux/misc/go1.18.3.src.tar.gz"
-  sha256 "0012386ddcbb5f3350e407c679923811dbd283fcdc421724931614a842ecbc2d"
+  url "https://go.dev/dl/go1.19rc1.src.tar.gz"
+  mirror "https://fossies.org/linux/misc/go1.19rc1.src.tar.gz"
+  sha256 "7fe9c7421018a6f70dc21e7f1c9d192942b7d86f690772b11fdbd8dec5d76bb5"
   license "BSD-3-Clause"
   head "https://go.googlesource.com/go.git", branch: "master"
 
