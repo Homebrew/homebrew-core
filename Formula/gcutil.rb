7cdad8e707d487a63676d453dfc9c850eab0be89
Rui Chen <rui@chenrui.dev>
2022-07-09 15:44:41 -0400

    gcutil: add license

--- Formula/gcutil.rb
+++ Formula/gcutil.rb
@@ -3,6 +3,7 @@ class Gcutil < Formula
   homepage "https://cloud.google.com/sdk/gcloud/reference/compute"
   url "https://dl.google.com/dl/cloudsdk/release/artifacts/gcutil-1.16.1.zip"
   sha256 "31f438c9ce3471f1404340e3411239b28b63f117d17776271fee1e1a352f3877"
+  license "Apache-2.0"
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "e5de2c3ea2450af30d6797f821b9ccb3c3da8c47f3086f1c2ccb063e91e9c68c"
