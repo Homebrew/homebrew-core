01bdb2eaa14fcf7e460ce1a7b79761a8403ea0b4
Ishaan Ahuja <Ishaan7190@gmail.com>
2022-07-10 02:11:25 -0500

    sgn 2.0 (new formula)

---
+++ Formula/sgn.rb
@@ -0,0 +1,20 @@
+class Sgn < Formula
+  desc "Shikata ga nai (仕方がない) encoder ported into go with several improvements"
+  homepage "https://github.com/EgeBalci/sgn"
+  url "https://github.com/EgeBalci/sgn/archive/refs/tags/2.0.tar.gz"
+  sha256 "b894e4cb396a5bb118a4081db9c54938e4ca903f67a998e7de8ec2763f2fcf53"
+  license "MIT"
+  head "https://github.com/EgeBalci/sgn.git", branch: "master"
+
+  depends_on "go" => :build
+  depends_on "keystone" => :build
+
+  def install
+    system "go", "get", "-u" if build.head? # Repository owner has not updated go.mod and go.sum files in a while and updates are needed to build only from HEAD
+    system "go", "build", *std_go_args
+  end
+
+  test do
+    assert_match "All done ＼(＾O＾)／", shell_output("#{bin}/sgn -o #{testpath}/sgn.out #{text_fixtures("mach/a.out")}")
+  end
+end
