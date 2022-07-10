9d9f9471f0fe0cb4d68ac278f424720fdc7ccb6c
Ishaan Ahuja <Ishaan7190@gmail.com>
2022-07-10 13:22:39 -0500

    retdec 4.0 (new formula)

---
+++ Formula/retdec.rb
@@ -0,0 +1,26 @@
+class Retdec < Formula
+  desc "RetDec is a retargetable machine-code decompiler based on LLVM"
+  homepage "https://github.com/avast/retdec"
+  url "https://github.com/avast/retdec/archive/refs/tags/v4.0.tar.gz"
+  sha256 "b26c2f71556dc4919714899eccdf82d2fefa5e0b3bc0125af664ec60ddc87023"
+  license all_of: ["MIT", "Zlib"]
+  head "https://github.com/avast/retdec.git", branch: "master"
+
+  depends_on macos: :mojave
+  depends_on xcode: :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+  depends_on "cmake" => :build
+  depends_on "python@3.10"
+
+  def install
+    mkdir "build" do
+      system "cmake", "..", *std_cmake_args
+      system "make", "install"
+    end
+  end
+
+  test do
+    assert_match "##### Done!", shell_output("#{bin}/retdec-decompiler.py #{text_fixtures("mach/a.out")")
+  end
+end
