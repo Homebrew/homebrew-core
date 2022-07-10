6bf72d0adf279d87fb48882c6c4f08c2fd7d13fc
Ishaan Ahuja <Ishaan7190@gmail.com>
2022-07-09 22:48:36 -0500

    Update pixiewps.rb

--- Formula/pixiewps.rb
+++ Formula/pixiewps.rb
@@ -5,15 +5,15 @@ class Pixiewps < Formula
   sha256 "c4dc0569e476ebdbd85992da2d1ff799db97ed0040da9dc44e13d08a97a9de1e"
   license "GPL-3.0-or-later"
   head "https://github.com/wiire-a/pixiewps.git", branch: "master"
-  
+
   depends_on "openssl@3"
-  
+
   def install
     inreplace "Makefile", "/usr/local", prefix.to_s
-    
+
     system "make", "OPENSSL=1", "install"
   end
-  
+
   test do
     assert_match version.to_s, shell_output("#{bin}/pixiewps --version 2>/dev/null", 3)
   end
