157c5d103ca6bc75bba05fc4ade53d6be84621e3
Porkepix <porkepix@gmail.com>
2022-07-12 00:53:59 +0200

    boringtun 0.5.0

--- Formula/boringtun.rb
+++ Formula/boringtun.rb
@@ -1,8 +1,8 @@
 class Boringtun < Formula
   desc "Userspace WireGuard implementation in Rust"
   homepage "https://github.com/cloudflare/boringtun"
-  url "https://github.com/cloudflare/boringtun/archive/v0.4.0.tar.gz"
-  sha256 "23a02ae0c01d194ce428c465de46538f683c696fa23a82cfc42d07d40e668e74"
+  url "https://github.com/cloudflare/boringtun/archive/v0.5.0.tar.gz"
+  sha256 "a7eaea4ceb6d0b7e4edc97f60557740dfd89288ca73a1ffb985e9fc39fec7f8a"
   license "BSD-3-Clause"
   head "https://github.com/cloudflare/boringtun.git", branch: "master"
 
