5c15530d04252a7745329ca75f513d26822b46a0
Sean Molenaar <SMillerDev@users.noreply.github.com>
2022-07-13 09:13:26 +0200

    libgit2: disable tests

--- Formula/libgit2.rb
+++ Formula/libgit2.rb
@@ -28,6 +28,7 @@ class Libgit2 < Formula
     args = std_cmake_args
     args << "-DBUILD_EXAMPLES=YES"
     args << "-DBUILD_CLAR=NO" # Don't build tests.
+    args << "-DBUILD_TESTS=OFF" # Really don't build tests.
     args << "-DUSE_SSH=YES"
 
     mkdir "build" do
