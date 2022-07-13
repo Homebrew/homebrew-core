f4476ce2035effba1e454be895e431a052acc69b
Thierry Moisan <thierry.moisan@gmail.com>
2022-07-13 15:05:36 -0400

    ipopt 3.14.8

--- Formula/ipopt.rb
+++ Formula/ipopt.rb
@@ -1,8 +1,8 @@
 class Ipopt < Formula
   desc "Interior point optimizer"
   homepage "https://coin-or.github.io/Ipopt/"
-  url "https://github.com/coin-or/Ipopt/archive/releases/3.14.7.tar.gz"
-  sha256 "cb5615d6d78e7a183288e4f0011a487c23caaf884ac8a2e0075dd134c3b0219e"
+  url "https://github.com/coin-or/Ipopt/archive/releases/3.14.8.tar.gz"
+  sha256 "e04aec64ae6590d93da6f8d008d766f03df014f0f05140c80c745fad589290f0"
   license "EPL-2.0"
   head "https://github.com/coin-or/Ipopt.git", branch: "stable/3.14"
 
@@ -40,8 +40,8 @@ class Ipopt < Formula
   end
 
   resource "test" do
-    url "https://github.com/coin-or/Ipopt/archive/releases/3.14.7.tar.gz"
-    sha256 "cb5615d6d78e7a183288e4f0011a487c23caaf884ac8a2e0075dd134c3b0219e"
+    url "https://github.com/coin-or/Ipopt/archive/releases/3.14.8.tar.gz"
+    sha256 "e04aec64ae6590d93da6f8d008d766f03df014f0f05140c80c745fad589290f0"
   end
 
   def install
