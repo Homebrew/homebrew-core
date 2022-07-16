ba4d9621070304cf34a308584fa8a7037d030876
Sam Ford <1584702+samford@users.noreply.github.com>
2022-07-16 07:41:43 -0400

    irssi: add missing blank line

--- Formula/irssi.rb
+++ Formula/irssi.rb
@@ -12,6 +12,7 @@ class Irssi < Formula
     url :stable
     strategy :github_latest
   end
+
   bottle do
     sha256 arm64_monterey: "6f90ced76f4dff3f6a4a65f47cdb996dc8e0473c677a4ae939019c54e69c88a8"
     sha256 arm64_big_sur:  "745a8f336278ed4d2ccb4f7a396b8dd3b8cb6ac4b8cc20ae9e39822815aeb01b"
