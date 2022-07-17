fef5f258ad00cdc1bc2bd49b56638cb69664d520
Randall Mason <clashthebunny@gmail.com>
2022-07-17 18:02:55 -0500

    fixups from CI and naming convention
    Co-authored-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>

--- Formula/jj.rb
+++ Formula/jj.rb
@@ -1,5 +1,5 @@
 class Jj < Formula
-  desc "Jujutsu is a Git-compatible DVCS"
+  desc "Git-compatible DVCS"
   homepage "https://github.com/martinvonz/jj"
   url "https://github.com/martinvonz/jj/archive/refs/tags/v0.4.0.tar.gz"
   sha256 "f97832d69b4e486997b9548c41f6cc945c68b8a8b546172ca92b7eb23ec71be5"
@@ -8,10 +8,12 @@ class Jj < Formula
 
   depends_on "rust" => :build
   depends_on "openssl@1.1"
+  uses_from_macos "zlib"
 
   on_linux do
     depends_on "pkg-config" => :build
   end
+
   def install
     system "cargo", "install", *std_cargo_args
   end
