7415b43dcac16222e2fc45261059c29bbac09488
Ilesh Thiada <ileshkt@gmail.com>
2022-07-18 11:54:47 +0530

    Update to 4.1.8

--- Formula/ferium.rb
+++ Formula/ferium.rb
@@ -1,8 +1,8 @@
 class Ferium < Formula
   desc "Fast and multi-source CLI program for managing Minecraft mods and modpacks"
   homepage "https://github.com/gorilla-devs/ferium"
-  url "https://github.com/gorilla-devs/ferium/archive/v4.1.7.tar.gz"
-  sha256 "2c2193417aff8af9045ea87a6ec6a35d3d1a48bdfdef05fc520fc4b9877de547"
+  url "https://github.com/gorilla-devs/ferium/archive/v4.1.8.tar.gz"
+  sha256 "1a84141cf81968faf7f5a8061e417ebed9866220d2fc41907dc156e7b68878c5"
   license "MPL-2.0"
   head "https://github.com/gorilla-devs/ferium.git", branch: "main"
 
