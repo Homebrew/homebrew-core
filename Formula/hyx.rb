6405e291d009b3d735399ad629515439990d4fa4
Jeffrey H. Johnson <trnsz@pobox.com>
2022-07-19 12:48:28 -0400

    Workaround Makefile issues via environment; add expect-based function test

--- Formula/hyx.rb
+++ Formula/hyx.rb
@@ -5,11 +5,10 @@ class Hyx < Formula
   sha256 "8d4f14e58584d6cc8f04e43ca38042eed218882a389249c20b086730256da5eb"
   license "MIT"
 
-  def install
-    inreplace "Makefile", " -Wl,-s", " "
-    inreplace "Makefile", " -Wl,-z,relro,-z,now -fpic -pie", " "
-    inreplace "Makefile", " -Werror", " "
+  depends_on "expect" => :test
 
+  def install
+    ENV["CFLAGS"] = "-Os"
     system "make"
 
     bin.install "hyx"
@@ -17,6 +16,8 @@ class Hyx < Formula
   end
 
   test do
-    assert_match "This is hyx", shell_output("#{bin}/hyx -v").chomp
+    pipe_output("env TERM=tty expect -",
+      "spawn #{bin}/hyx;expect \">\";send \":wq new\r\";expect eof")
+    assert_predicate testpath/"new", :exist?
   end
 end
