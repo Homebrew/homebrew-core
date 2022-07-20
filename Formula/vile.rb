41ed426bd5b16cf32e180521cb1f484e707b4aa8
Jeffrey H. Johnson <trnsz@pobox.com>
2022-07-20 12:52:04 -0400

    Fix ordering

--- Formula/vile.rb
+++ Formula/vile.rb
@@ -5,9 +5,9 @@ class Vile < Formula
   sha256 "240edec7bbf3d9df48b3042754bf9854d9a233d371d50bba236ec0edd708eed5"
   license "GPL-2.0-or-later"
 
-  uses_from_macos "expect" => :test
   uses_from_macos "flex" => :build
   uses_from_macos "groff" => :build
+  uses_from_macos "expect" => :test
   uses_from_macos "ncurses"
   uses_from_macos "perl"
 
