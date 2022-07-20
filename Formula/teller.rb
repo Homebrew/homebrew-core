6a5581da4944d4f667f73759f7fbbae0c4c48a96
Elad Kaplan <kaplan.elad@gmail.com>
2022-07-20 14:50:37 +0300

    remove teller show test

--- Formula/teller.rb
+++ Formula/teller.rb
@@ -29,25 +29,6 @@ class Teller < Formula
   end
 
   test do
-    (testpath/"test.env").write <<~EOS
-      foo: var
-    EOS
-
-    (testpath/".teller.yml").write <<~EOS
-      project: brewtest
-
-      providers:
-        # this will fuse vars with the below .env file
-        # use if you'd like to grab secrets from outside of the project tree
-        dotenv:
-          env_sync:
-            path: #{testpath}/test.env
-    EOS
-
-    output = pipe_output("#{bin}/teller -c #{testpath}/.teller.yml show")
-    assert_match "teller: loaded variables for brewtest using #{testpath}/.teller.yml", output
-    assert_match "foo", output
-
     assert_match "Teller #{version}", shell_output("#{bin}/teller version")
   end
 end
