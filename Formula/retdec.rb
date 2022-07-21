db2848a4220a3cc5247a0ce438c934ad9491671a
Ishaan Ahuja <Ishaan7190@gmail.com>
2022-07-21 12:27:32 -0500

    Update retdec.rb

--- Formula/retdec.rb
+++ Formula/retdec.rb
@@ -1,15 +1,22 @@
 class Retdec < Formula
   desc "Retargetable machine-code decompiler based on LLVM"
   homepage "https://github.com/avast/retdec"
-  url "https://github.com/avast/retdec/archive/refs/tags/v4.0.tar.gz"
-  sha256 "b26c2f71556dc4919714899eccdf82d2fefa5e0b3bc0125af664ec60ddc87023"
   license all_of: ["MIT", "Zlib"]
   head "https://github.com/avast/retdec.git", branch: "master"
+
+  stable do
+    url "https://github.com/avast/retdec.git",
+      branch:   "master",
+      revision: "8503282c6d1295ba58ed5fed4ccb5605ed3febfd"
+    version "5.0"
+  end
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "cmake" => :build
   depends_on "libtool" => :build
+  depends_on "pkg-config" => :build
+  depends_on "openssl@1.1"
   depends_on "python@3.10"
 
   on_macos do
@@ -22,19 +29,6 @@ class Retdec < Formula
   end
 
   def install
-    inreplace "cmake/deps.cmake", "1.1.1c", "1.1.1i"
-    inreplace "cmake/deps.cmake",
-      "97ace46e11dba4c4c2b7cb67140b6ec152cfaaf4",
-      "90cebd1b216e0a160fcfd8e0eddca47dad47c183"
-    inreplace "cmake/deps.cmake",
-      "f093df5cfd7521d8f6a09f250d7e69159d1001c47419130e806488de8a6312d8",
-      "b12110d8a344f03b4f182fb111e6caa46a851e4966dc5975c4801af74a85cd7c"
-    inreplace "deps/openssl/CMakeLists.txt",
-      "set(OPENSSL_CONFIGURE_ARCH \"darwin64-x86_64-cc\")",
-      "set(OPENSSL_CONFIGURE_ARCH \"darwin64-x86_64-cc\")
-        elseif(ARCH_ARM64)
-                set(OPENSSL_CONFIGURE_ARCH \"darwin64-arm64-cc\")"
-
     gcc = Formula["gcc@7"] if OS.linux?
 
     cmake_args = std_cmake_args
@@ -49,9 +43,7 @@ class Retdec < Formula
 
   test do
     test_cmd = %W[
-      #{Formula["python@3.10"].opt_bin}/python3
-      #{bin}/retdec-decompiler.py
-      --no-memory-limit
+      #{bin}/retdec-decompiler
       -o
       #{testpath}/a.c
       #{test_fixtures("mach/a.out")}
@@ -59,6 +51,6 @@ class Retdec < Formula
     ]
     test_cmd = test_cmd.join(" ")
 
-    assert_match "\#\#\#\#\# Decompiling", shell_output(test_cmd)
+    assert_match "Running phase: cleanup", shell_output(test_cmd)
   end
 end
