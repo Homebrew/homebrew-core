56a8f8be4000c2aa52384272321939cdf9760987
Jeffrey H. Johnson <trnsz@pobox.com>
2022-07-27 16:03:59 -0400

    Always autoreconf; use coreutils readlink on macOS

--- Formula/libeatmydata.rb
+++ Formula/libeatmydata.rb
@@ -4,14 +4,12 @@ class Libeatmydata < Formula
   url "https://github.com/stewartsmith/libeatmydata/releases/download/v130/libeatmydata-130.tar.gz"
   sha256 "48731cd7e612ff73fd6339378fbbff38dd3bcf6c243593b0d9773ca0051541c0"
   license "GPL-3.0-or-later"
+  head "https://github.com/stewartsmith/libeatmydata.git", branch: "master"
 
-  head do
-    url "https://github.com/stewartsmith/libeatmydata.git", branch: "master"
-    depends_on "autoconf"         => :build
-    depends_on "autoconf-archive" => :build
-    depends_on "automake"         => :build
-    depends_on "libtool"          => :build
-  end
+  depends_on "autoconf"         => :build
+  depends_on "autoconf-archive" => :build
+  depends_on "automake"         => :build
+  depends_on "libtool"          => :build
 
   depends_on "coreutils"
 
@@ -21,9 +19,9 @@ class Libeatmydata < Formula
 
   def install
     # macOS does not support `readlink -f` as used by the `eatmydata` shell wrapper script
-    inreplace "eatmydata.sh.in", "readlink", "#{Formula["coreutils"].opt_libexec}/gnubin/readlink"
+    inreplace "eatmydata.sh.in", "readlink", "#{Formula["coreutils"].opt_bin}/greadlink" if OS.mac?
 
-    system "autoreconf", "--force", "--install", "--verbose" if build.head?
+    system "autoreconf", "--force", "--install", "--verbose"
     system "./configure", *std_configure_args,
                           "--disable-option-checking",
                           "--disable-silent-rules"
