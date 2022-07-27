e0369b4319b26ad83e8c35a376b980622df85e47
Jeffrey H. Johnson <trnsz@pobox.com>
2022-07-27 15:04:14 -0400

    Update desc; only autoreconf HEAD; use coreutils.

--- Formula/libeatmydata.rb
+++ Formula/libeatmydata.rb
@@ -1,23 +1,29 @@
 class Libeatmydata < Formula
-  desc "Because fsync() should be a no-op"
+  desc "LD_PRELOAD library and wrapper to transparently disable fsync and related calls"
   homepage "https://www.flamingspork.com/projects/libeatmydata/"
   url "https://github.com/stewartsmith/libeatmydata/releases/download/v130/libeatmydata-130.tar.gz"
   sha256 "48731cd7e612ff73fd6339378fbbff38dd3bcf6c243593b0d9773ca0051541c0"
   license "GPL-3.0-or-later"
-  head "https://github.com/stewartsmith/libeatmydata.git", branch: "master"
 
-  depends_on "autoconf"         => :build
-  depends_on "autoconf-archive" => :build
-  depends_on "automake"         => :build
-  depends_on "libtool"          => :build
-  depends_on "coreutils"        => :test
+  head do
+    url "https://github.com/stewartsmith/libeatmydata.git", branch: "master"
+    depends_on "autoconf"         => :build
+    depends_on "autoconf-archive" => :build
+    depends_on "automake"         => :build
+    depends_on "libtool"          => :build
+  end
+
+  depends_on "coreutils"
 
   on_linux do
     depends_on "strace" => :test
   end
 
   def install
-    system "autoreconf", "--force", "--install", "--verbose"
+    # macOS does not support `readlink -f` as used by the `eatmydata` shell wrapper script
+    inreplace "eatmydata.sh.in", "readlink", "#{Formula["coreutils"].opt_libexec}/gnubin/readlink"
+
+    system "autoreconf", "--force", "--install", "--verbose" if build.head?
     system "./configure", *std_configure_args,
                           "--disable-option-checking",
                           "--disable-silent-rules"
@@ -25,7 +31,6 @@ class Libeatmydata < Formula
   end
 
   test do
-    ENV.prepend_path "PATH", Formula["coreutils"].opt_libexec/"gnubin"
     system bin/"eatmydata", "sync"
     return if OS.mac?
 
