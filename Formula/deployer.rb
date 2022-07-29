988be57c25ee74173024ce4d34abc0e24a216320
Jonathan Chang <me@jonathanchang.org>
2022-07-29 15:27:13 -0700

    deployer: remove livecheck

--- Formula/deployer.rb
+++ Formula/deployer.rb
@@ -4,13 +4,6 @@ class Deployer < Formula
   url "https://github.com/deployphp/deployer/releases/download/v7.0.0/deployer.phar"
   sha256 "34a234b6acccd1b6f214df570ff118a9fcea91dc06be53f75d0fc702cc38ace5"
 
-  # The first-party download page now uses client-side rendering, so we have to
-  # check a JSON file used on the page that contains the version information.
-  livecheck do
-    url "https://deployer.org/manifest.json"
-    regex(%r{\\?/releases\\?/v?(\d+(?:\.\d+)+)\\?/deployer\.phar}i)
-  end
-
   bottle do
     sha256 cellar: :any_skip_relocation, all: "c8f15a518a11552ea9bdf2ff350918e20b14b4e31b1fab1c2471b24c6ee743f4"
   end
