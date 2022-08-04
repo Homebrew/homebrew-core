94522fdb7bdea19255e4d7d07680bc540ff5ae1f
Vlad <vzhd1701@gmail.com>
2022-08-04 14:55:31 +0500

    enex2notion: fix style

--- Formula/enex2notion.rb
+++ Formula/enex2notion.rb
@@ -8,8 +8,8 @@ class Enex2notion < Formula
   license "MIT"
 
   depends_on "pymupdf"
-  depends_on "six"
   depends_on "python@3.10"
+  depends_on "six"
 
   resource "beautifulsoup4" do
     url "https://files.pythonhosted.org/packages/e8/b0/cd2b968000577ec5ce6c741a54d846dfa402372369b8b6861720aa9ecea7/beautifulsoup4-4.11.1.tar.gz"
