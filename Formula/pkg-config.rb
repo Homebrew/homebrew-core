455b76f33b0d730f8fad8923e52dd9b023f8168f
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-08-07 23:48:20 +0800

    pkg-config: point to `current` symlink
    Companion to Homebrew/brew#13664.

--- Formula/pkg-config.rb
+++ Formula/pkg-config.rb
@@ -6,7 +6,7 @@ class PkgConfig < Formula
   mirror "http://fresh-center.net/linux/misc/legacy/pkg-config-0.29.2.tar.gz"
   sha256 "6fc69c01688c9458a57eb9a1664c9aba372ccda420a02bf4429fe610e7e7d591"
   license "GPL-2.0-or-later"
-  revision 3
+  revision 4
 
   livecheck do
     url "https://pkg-config.freedesktop.org/releases/"
@@ -36,6 +36,7 @@ class PkgConfig < Formula
     pc_path << if OS.mac?
       pc_path << "/usr/local/lib/pkgconfig"
       pc_path << "/usr/lib/pkgconfig"
+      pc_path << "#{HOMEBREW_LIBRARY}/Homebrew/os/mac/pkgconfig/current"
       "#{HOMEBREW_LIBRARY}/Homebrew/os/mac/pkgconfig/#{MacOS.version}"
     else
       "#{HOMEBREW_LIBRARY}/Homebrew/os/linux/pkgconfig"
