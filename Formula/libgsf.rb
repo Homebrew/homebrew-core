065274c32287b6e429a4639bdb369bd26d15cba4
Rui Chen <rui@chenrui.dev>
2022-08-06 21:23:55 -0400

    libgsf 1.14.50

--- Formula/libgsf.rb
+++ Formula/libgsf.rb
@@ -1,8 +1,8 @@
 class Libgsf < Formula
   desc "I/O abstraction library for dealing with structured file formats"
   homepage "https://gitlab.gnome.org/GNOME/libgsf"
-  url "https://download.gnome.org/sources/libgsf/1.14/libgsf-1.14.49.tar.xz"
-  sha256 "e9ebe36688f010c9e6e40c8903f3732948deb8aca032578d07d0751bd82cf857"
+  url "https://download.gnome.org/sources/libgsf/1.14/libgsf-1.14.50.tar.xz"
+  sha256 "6e6c20d0778339069d583c0d63759d297e817ea10d0d897ebbe965f16e2e8e52"
   license "LGPL-2.1-only"
 
   bottle do
