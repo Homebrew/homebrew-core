ddb6d2856f190e0d888d9e83977793429f020128
Rui Chen <rui@chenrui.dev>
2022-08-11 00:02:12 -0400

    cpr 1.9.1
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    
    fix rpath issue
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/cpr.rb
+++ Formula/cpr.rb
@@ -1,8 +1,8 @@
 class Cpr < Formula
   desc "C++ Requests, a spiritual port of Python Requests"
   homepage "https://docs.libcpr.org/"
-  url "https://github.com/libcpr/cpr/archive/1.8.3.tar.gz"
-  sha256 "0784d4c2dbb93a0d3009820b7858976424c56578ce23dcd89d06a1d0bf5fd8e2"
+  url "https://github.com/libcpr/cpr/archive/1.9.1.tar.gz"
+  sha256 "389b9bb78cba0d63a064eac224c120069acdd2ed9172f7d06547ce1e74384330"
   license "MIT"
   head "https://github.com/libcpr/cpr.git", branch: "master"
 
@@ -16,14 +16,15 @@ class Cpr < Formula
   end
 
   depends_on "cmake" => :build
-  depends_on "openssl@1.1"
+  depends_on "openssl@3"
 
   uses_from_macos "curl"
 
   def install
     args = std_cmake_args + %w[
-      -DCPR_FORCE_USE_SYSTEM_CURL=ON
+      -DCPR_USE_SYSTEM_CURL=ON
       -DCPR_BUILD_TESTS=OFF
+      -DCMAKE_INSTALL_RPATH=#{rpath}
     ]
 
     system "cmake", ".", *args, "-DBUILD_SHARED_LIBS=ON"
