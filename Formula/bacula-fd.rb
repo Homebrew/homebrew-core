97fb410a3868cbc1ce28f605d97d766e869fac2e
Sean Molenaar <SMillerDev@users.noreply.github.com>
2022-08-11 10:08:52 +0200

    bacula-fd: update license

--- Formula/bacula-fd.rb
+++ Formula/bacula-fd.rb
@@ -3,7 +3,7 @@ class BaculaFd < Formula
   homepage "https://www.bacula.org/"
   url "https://downloads.sourceforge.net/project/bacula/bacula/13.0.1/bacula-13.0.1.tar.gz"
   sha256 "d63848d695ac15c1ccfc117892753314bcb9232a852c40e32cca88c0e918978a"
-  license "AGPL-3.0"
+  license "AGPL-3.0-only"
   
   bottle do
     sha256                               arm64_monterey: "5ce02dd71c35475c7ce92c54c45e8617b2e5935418317619fda4f5713daf82a3"
