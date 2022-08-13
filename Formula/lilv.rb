e01eadd60522d6ee18cd3bb04fb2a728737b5263
Rui Chen <rui@chenrui.dev>
2022-08-13 13:47:26 -0400

    Update lilv.rb

--- Formula/lilv.rb
+++ Formula/lilv.rb
@@ -21,6 +21,7 @@ class Lilv < Formula
 
   depends_on "meson" => :build
   depends_on "ninja" => :build
+  depends_on "pkg-config" => :build
   depends_on "python@3.10" => [:build, :test]
   depends_on "lv2"
   depends_on "serd"
