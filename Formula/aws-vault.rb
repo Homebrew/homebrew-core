0ac8a2a491cb2e34d77acf52118d773f68e56297
Adam Stirk <67702268+adamstirk-ct@users.noreply.github.com>
2022-08-16 20:20:35 +0100

    Update test

--- Formula/aws-vault.rb
+++ Formula/aws-vault.rb
@@ -36,7 +36,9 @@ class AwsVault < Formula
   end
 
   test do
-    assert_match("aws-vault: error: required argument 'profile' not provided, try --help",
-      shell_output("#{bin}/aws-vault login 2>&1", 1))
+    assert_match("aws-vault: error: login: argument 'profile' not provided, nor any AWS env vars found. Try --help",
+      shell_output("#{bin}/aws-vault --backend=file login 2>&1", 1))
+
+    assert_match version.to_s, shell_output("#{bin}/aws-vault --version 2>&1")
   end
 end
