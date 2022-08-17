07838c948d19b1de496b2dc8eef2743342dced74
Porkepix <porkepix@gmail.com>
2022-08-18 00:36:07 +0200

    fonttools 4.36.0

--- Formula/fonttools.rb
+++ Formula/fonttools.rb
@@ -3,8 +3,8 @@ class Fonttools < Formula
 
   desc "Library for manipulating fonts"
   homepage "https://github.com/fonttools/fonttools"
-  url "https://files.pythonhosted.org/packages/9b/86/1d53f4a83fdd67361e112fd9f92641e647a62d11daa0ae0edd7a353693a6/fonttools-4.35.0.zip"
-  sha256 "1cfb335c0abdeb6231191dc4f9d7ce1173e2ac94b335c617e045b96f9c974aea"
+  url "https://files.pythonhosted.org/packages/df/4b/ca51dbaf267decfcc7ca9300cdc703206c1c05f3e2beb5140a71817c2d7e/fonttools-4.36.0.zip"
+  sha256 "e637d2fe06bddabbfc488e02ef32d04d561e3c71e9ba11abc7782ea753ceb218"
   license "MIT"
   head "https://github.com/fonttools/fonttools.git", branch: "main"
 
@@ -18,11 +18,6 @@ class Fonttools < Formula
   end
 
   depends_on "python@3.10"
-
-  resource "Brotli" do
-    url "https://files.pythonhosted.org/packages/2a/18/70c32fe9357f3eea18598b23aa9ed29b1711c3001835f7cf99a9818985d0/Brotli-1.0.9.zip"
-    sha256 "4d1b810aa0ed773f81dceda2cc7b403d01057458730e309856356d4ef4188438"
-  end
 
   def install
     virtualenv_install_with_resources
