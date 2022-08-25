a1b643a06b9da9321810ad67312f97b226ff9b10
chiefmikey <wolfemikl@gmail.com>
2022-08-25 08:31:38 -0600

    mercurial 6.2.1

--- Formula/mercurial.rb
+++ Formula/mercurial.rb
@@ -3,8 +3,8 @@
 class Mercurial < Formula
   desc "Scalable distributed version control system"
   homepage "https://mercurial-scm.org/"
-  url "https://www.mercurial-scm.org/release/mercurial-6.1.4.tar.gz"
-  sha256 "f361f9802b36e357ac019ceb712ca11de8332b07deadeed8dfa904f05bf7ca78"
+  url "https://www.mercurial-scm.org/release/mercurial-6.2.1.tar.gz"
+  sha256 "8ac5d7a20d1c2ad54d4c263d134164646dc34a8e3a6bed69cfbeabaf8d3882da"
   license "GPL-2.0-or-later"
 
   livecheck do
