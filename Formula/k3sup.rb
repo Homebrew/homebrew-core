a3a730910d3bbd008955a48a06d94eabd3280851
Rui Chen <rui@chenrui.dev>
2022-08-27 09:31:50 -0400

    k3sup 0.12.3

--- Formula/k3sup.rb
+++ Formula/k3sup.rb
@@ -2,8 +2,8 @@ class K3sup < Formula
   desc "Utility to create k3s clusters on any local or remote VM"
   homepage "https://k3sup.dev"
   url "https://github.com/alexellis/k3sup.git",
-      tag:      "0.12.1",
-      revision: "386e15880961ffc869696142db682e2f211b3ed9"
+      tag:      "0.12.3",
+      revision: "c0a48331967e1c1e6b10dbd997b65c1496fbd32d"
   license "MIT"
   head "https://github.com/alexellis/k3sup.git", branch: "master"
 
