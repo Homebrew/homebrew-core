66c9610c061afeae54dab1ada84f5d76cd2adc9b
TK <dk@terminalkitten.com>
2022-08-31 01:20:06 +0200

    chore: add okpt

---
+++ Formula/okpt.rb
@@ -0,0 +1,17 @@
+class Okpt < Formula
+  desc "Healthcare ecosystem built for everyone"
+  homepage "https://openkoppeltaal.nl"
+  url "git@github.com:openkoppeltaal/homebrew-okpt.git@0.0.1"
+  sha256 "761bce8f13727f395aadceefeca36b5cb675855643da1a8c1485ff0682170d9b"
+  license "MIT"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w")
+  end
+
+  test do
+    system "false"
+  end
+end
