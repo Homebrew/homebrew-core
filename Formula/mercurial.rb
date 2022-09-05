e276fd96099816eb170c75145c3fef75c6e058c3
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-09-05 22:04:00 +0800

    mercurial: fix install.
    Also, fix `python3` references. See #108008.

--- Formula/mercurial.rb
+++ Formula/mercurial.rb
@@ -24,24 +24,28 @@ class Mercurial < Formula
   depends_on "python@3.10"
 
   def install
+    python3 = "python3.10"
     ENV["HGPYTHON3"] = "1"
 
     # FIXME: python@3.10 formula's "prefix scheme" patch tries to install into
     # HOMEBREW_PREFIX/{lib,bin}, which fails due to sandbox. As workaround,
     # manually set the installation paths to behave like prior python versions.
-    site_packages = prefix/Language::Python.site_packages("python3")
-    inreplace "Makefile",
-              "--prefix=\"$(PREFIX)\"",
-              "\\0 --install-lib=\"#{site_packages}\" --install-scripts=\"#{prefix}/bin\""
+    site_packages = prefix/Language::Python.site_packages(python3)
+    extra_flags = %W[
+      --install-lib="#{site_packages}"
+      --install-scripts="#{bin}"
+      --install-data="#{prefix}"
+    ]
+    inreplace "Makefile", "--prefix=\"$(PREFIX)\"", "\\0 #{extra_flags.join(" ")}"
 
     system "make", "PREFIX=#{prefix}",
-                   "PYTHON=#{which("python3")}",
+                   "PYTHON=#{which(python3)}",
                    "install-bin"
 
     # Install chg (see https://www.mercurial-scm.org/wiki/CHg)
     cd "contrib/chg" do
       system "make", "PREFIX=#{prefix}",
-                     "PYTHON=#{which("python3")}",
+                     "PYTHON=#{which(python3)}",
                      "HGPATH=#{bin}/hg", "HG=#{bin}/hg"
       bin.install "chg"
     end
@@ -64,9 +68,9 @@ class Mercurial < Formula
   end
 
   def caveats
-    return unless (opt_bin/"hg").exist?
+    return unless (bin/"hg").exist?
 
-    cacerts_configured = `#{opt_bin}/hg config web.cacerts`.strip
+    cacerts_configured = Utils.safe_popen_read(bin/"hg", "config", "web.cacerts").strip
     return if cacerts_configured.empty?
 
     <<~EOS
@@ -80,6 +84,6 @@ class Mercurial < Formula
   end
 
   test do
-    system "#{bin}/hg", "init"
+    system bin/"hg", "init"
   end
 end
