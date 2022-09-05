78768ab97a31ca5aa572d5f59bd0885a7603f2b8
Tamás Nepusz <ntamas@gmail.com>
2022-09-05 14:22:05 +0200

    Fix test case to account for API changes

--- Formula/igraph.rb
+++ Formula/igraph.rb
@@ -62,7 +62,7 @@ class Igraph < Formula
         igraph_t graph;
         igraph_rng_seed(igraph_rng_default(), 42);
         igraph_erdos_renyi_game(&graph, IGRAPH_ERDOS_RENYI_GNP, 1000, 5.0/1000, IGRAPH_UNDIRECTED, IGRAPH_NO_LOOPS);
-        igraph_diameter(&graph, &diameter, 0, 0, 0, IGRAPH_UNDIRECTED, 1);
+        igraph_diameter(&graph, &diameter, 0, 0, 0, 0, IGRAPH_UNDIRECTED, 1);
         printf("Diameter = %f\\n", (double) diameter);
         igraph_destroy(&graph);
       }
