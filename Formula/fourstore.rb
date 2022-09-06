230ff189c6738e1740dfb967321b3678bf4408ae
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-09-06 13:16:40 +0800

    fourstore: revision bump (icu4c 71.1)
    Left out of #98809.

--- Formula/fourstore.rb
+++ Formula/fourstore.rb
@@ -4,7 +4,7 @@ class Fourstore < Formula
   url "https://github.com/4store/4store/archive/v1.1.6.tar.gz"
   sha256 "a0c8143fcceeb2f1c7f266425bb6b0581279129b86fdd10383bf1c1e1cab8e00"
   license "GPL-3.0"
-  revision 2
+  revision 3
 
   bottle do
     sha256 arm64_monterey: "7aed20fb578c260f7d1764679d6ecaad032d68dd6bdacca012002ea815509a4a"
