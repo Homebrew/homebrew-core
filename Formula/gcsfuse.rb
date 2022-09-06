a2ca92688a80efc1fbe7fd731abdced17188f624
Avi <avidullu@google.com>
2022-09-06 15:12:40 +0530

    Update gcsfuse.rb to latest version 0.41.6

--- Formula/gcsfuse.rb
+++ Formula/gcsfuse.rb
@@ -1,7 +1,7 @@
 class Gcsfuse < Formula
   desc "User-space file system for interacting with Google Cloud"
   homepage "https://github.com/googlecloudplatform/gcsfuse"
-  url "https://github.com/GoogleCloudPlatform/gcsfuse/archive/v0.41.0.tar.gz"
+  url "https://github.com/GoogleCloudPlatform/gcsfuse/archive/v0.41.6.tar.gz"
   sha256 "7c3833ed090da8aa762fe831fd7c27d73273c5f47ce431dff4100c330bf53047"
   license "Apache-2.0"
   head "https://github.com/GoogleCloudPlatform/gcsfuse.git", branch: "master"
