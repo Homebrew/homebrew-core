4d7fded47b53acd60cd79081487bfa0f94902799
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-09-09 01:18:41 +0800

    solidity: remove Linux-only `gcc` dep.

--- Formula/solidity.rb
+++ Formula/solidity.rb
@@ -23,10 +23,6 @@ class Solidity < Formula
   depends_on xcode: ["11.0", :build]
   depends_on "boost"
 
-  on_linux do
-    depends_on "gcc" # For C++17
-  end
-
   fails_with gcc: "5"
 
   def install
