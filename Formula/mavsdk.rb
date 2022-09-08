3a4fd7b5952f78419932cdbe2d326d80e91f4abc
Rui Chen <rui@chenrui.dev>
2022-09-07 20:06:46 -0400

    remove gcc dep for linux build

--- Formula/mavsdk.rb
+++ Formula/mavsdk.rb
@@ -41,10 +41,6 @@ class Mavsdk < Formula
     depends_on "llvm" if DevelopmentTools.clang_build_version <= 1100
   end
 
-  on_linux do
-    depends_on "gcc"
-  end
-
   fails_with :clang do
     build 1100
     cause <<-EOS
