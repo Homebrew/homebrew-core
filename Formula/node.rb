1dc76f2e62b98d0e724f8242245e99cef6f2a69f
Luigi Pinca <luigipinca@gmail.com>
2022-09-08 18:14:47 +0200

    node: remove gcc dependency on linux
    Refs: https://github.com/Homebrew/homebrew-core/pull/110005#issuecomment-1240928210

--- Formula/node.rb
+++ Formula/node.rb
@@ -36,10 +36,6 @@ class Node < Formula
     depends_on "llvm" => [:build, :test] if DevelopmentTools.clang_build_version <= 1100
   end
 
-  on_linux do
-    depends_on "gcc"
-  end
-
   fails_with :clang do
     build 1100
     cause <<~EOS
