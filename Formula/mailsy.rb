cb881908133adf9019c74cfd653a21359bb0b313
Balli Asghar <ballihacker@outlook.com>
2022-09-09 04:46:09 +0100

    Added Mailsy Formula

---
+++ Formula/mailsy.rb
@@ -0,0 +1,24 @@
+require "language/node"
+
+class Mailsy < Formula
+  desc "quickly generate a temporary email address"
+  homepage "https://github.com/BalliAsghar/Mailsy#readme"
+  url "https://registry.npmjs.org/mailsy/-/mailsy-3.0.4.tgz"
+  sha256 "0895bf590d3844f03d50f4d6eea4d982a562c7f9d4b921943bf6a02286ddac1f"
+  license "MIT"
+
+  livecheck do
+    url :stable
+  end
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    
+  end
+end
