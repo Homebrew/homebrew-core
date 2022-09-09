8fc5979a7169a8062d9b6c2578a9b6db9815214f
Max Eisner <4730112+max-ae@users.noreply.github.com>
2022-09-09 21:25:46 +0200

    yorkie: add completions

--- Formula/yorkie.rb
+++ Formula/yorkie.rb
@@ -23,6 +23,8 @@ class Yorkie < Formula
   def install
     system "make", "build"
     prefix.install "bin"
+
+    generate_completions_from_executable(bin/"yorkie", "completion")
   end
 
   service do
