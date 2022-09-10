553fa97ee736de0622ab58c9f238f0e5cb1f665b
Balli Asghar <ballihacker@outlook.com>
2022-09-10 18:13:45 +0100

    Update Formula/mailsy.rb
    Co-authored-by: Sean Molenaar <SMillerDev@users.noreply.github.com>

--- Formula/mailsy.rb
+++ Formula/mailsy.rb
@@ -2,7 +2,7 @@ require "language/node"
 
 class Mailsy < Formula
   desc "Quickly generate a temporary email address"
-  homepage "https://github.com/BalliAsghar/Mailsy#readme"
+  homepage "https://github.com/BalliAsghar/Mailsy"
   url "https://registry.npmjs.org/mailsy/-/mailsy-3.0.7.tgz"
   sha256 "f59c8269315be57bdd5ce6e1488aa8bab213a03e5e0812dc2b0c4572f919adc8"
   license "MIT"
