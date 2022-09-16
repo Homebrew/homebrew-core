931cf4ee9f38ddd21949da35835d21b83691fc60
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-09-16 23:40:46 +0800

    got: miscellaneous build fixes.

--- Formula/got.rb
+++ Formula/got.rb
@@ -5,13 +5,22 @@ class Got < Formula
   sha256 "b35324893c9aefbd6bf49602a6c9bb9d6e20670aff3c9734fefead0205bb9cb2"
   license "ISC"
 
+  depends_on "bison" => :build
+  depends_on "pkg-config" => :build
+  depends_on "libevent"
+  depends_on :macos # FIXME: build fails on Linux.
   depends_on "ncurses"
-  depends_on "openssl@3"
-  depends_on "ossp-uuid"
+  depends_on "openssl@1.1"
+  uses_from_macos "zlib"
 
-  uses_from_macos "bison" => :build
+  on_linux do
+    depends_on "libmd"
+    depends_on "util-linux" # for libuuid
+  end
 
   def install
+    # The `configure` script hardcodes our `openssl@3`, but we can't use it due to `libevent`.
+    inreplace "configure", %r{\$\{HOMEBREW_PREFIX?\}/opt/openssl@3}, Formula["openssl@1.1"].opt_prefix
     system "./configure", *std_configure_args, "--disable-silent-rules"
     system "make", "install"
   end
