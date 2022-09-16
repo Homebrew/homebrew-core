980f40eaa24e3d124f055fcc582276f65c132f04
David Tagatac <david@tagatac.net>
2022-09-16 08:39:27 -0600

    bagoup 2.1.2 (new formula)

---
+++ Formula/bagoup.rb
@@ -0,0 +1,19 @@
+class Bagoup < Formula
+  desc "Mac OS Messages export"
+  homepage "https://github.com/tagatac/bagoup"
+  url "https://github.com/tagatac/bagoup/archive/refs/tags/v2.1.2.tar.gz"
+  sha256 "3d4fcecd8ece92f9fd3efafe489af0f245779c0abedbd0237d770615d45a3e84"
+
+  depends_on "go" => :build
+  depends_on "make" => :build
+
+  def install
+    system "make", "from-archive"
+    mkdir bin.to_s
+    cp "bagoup", "#{bin}/bagoup"
+  end
+
+  test do
+    assert_equal "Usage:\n  bagoup [OPTIONS]\n", shell_output("#{bin}/bagoup --help | head -n 2")
+  end
+end
