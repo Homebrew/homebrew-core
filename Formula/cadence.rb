af0bfa9172d699d97ca747c3d531bcdf06ee944b
Rui Chen <rui@chenrui.dev>
2022-09-16 14:32:43 -0400

    cadence 0.26.1

--- Formula/cadence.rb
+++ Formula/cadence.rb
@@ -5,8 +5,8 @@ class Cadence < Formula
   head "https://github.com/onflow/cadence.git", branch: "master"
 
   stable do
-    url "https://github.com/onflow/cadence/archive/v0.26.0.tar.gz"
-    sha256 "6ec4bd26e2563b3ea8df180f55f9aa370ae090ef635043d210e3f5b4d8aedee7"
+    url "https://github.com/onflow/cadence/archive/v0.26.1.tar.gz"
+    sha256 "1ebc6bc59da9d5873d3b7fdc7ce53c184f87b449db939f38924865e4b05a8c52"
 
     # add build patch, remove in next release
     # upstream PR reference
