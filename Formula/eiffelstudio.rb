b938a39529d711a4da1d7c9e6187f66294057166
Michael Cho <cho-m@tuta.io>
2022-09-18 17:23:18 -0700

    eiffelstudio 22.05.10.6302

--- Formula/eiffelstudio.rb
+++ Formula/eiffelstudio.rb
@@ -1,8 +1,9 @@
 class Eiffelstudio < Formula
   desc "Development environment for the Eiffel language"
   homepage "https://www.eiffel.com"
-  url "https://ftp.eiffel.com/pub/download/19.05/eiffelstudio-19.05.10.3187.tar"
-  sha256 "b5f883353405eb9ce834c50a863b3721b21c35950a226103e6d01d0101a932b3"
+  url "https://ftp.eiffel.com/pub/download/22.05/pp/PorterPackage_std_106302.tar"
+  version "22.05.10.6302"
+  sha256 "c2ede38b19cedead58a9e075cf79d6a4b113e049c0723fe9556c4f36ee68b80d"
   license "GPL-2.0"
 
   bottle do
@@ -18,7 +19,7 @@ class Eiffelstudio < Formula
   end
 
   depends_on "pkg-config" => :build
-  depends_on "gtk+"
+  depends_on "gtk+3"
 
   uses_from_macos "pax" => :build
 
@@ -34,7 +35,7 @@ class Eiffelstudio < Formula
     os_tag = "#{os}-x86-64"
     system "./compile_exes", os_tag
     system "./make_images", os_tag
-    prefix.install Dir["Eiffel_19.05/*"]
+    prefix.install Dir["Eiffel_#{version.major}.#{version.minor.to_s.rjust(2, "0")}/*"]
     eiffel_env = { ISE_EIFFEL: prefix, ISE_PLATFORM: os_tag }
     {
       studio:       %w[ec ecb estudio finish_freezing],
