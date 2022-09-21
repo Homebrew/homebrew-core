6290d26d3719dd8e48f67a2e3aea65fa7395f3e7
fishtreesugar <fishtreesugar@gmail.com>
2022-09-21 22:02:31 +0800

    erlang 25.1

--- Formula/erlang.rb
+++ Formula/erlang.rb
@@ -2,8 +2,8 @@ class Erlang < Formula
   desc "Programming language for highly scalable real-time systems"
   homepage "https://www.erlang.org/"
   # Download tarball from GitHub; it is served faster than the official tarball.
-  url "https://github.com/erlang/otp/releases/download/OTP-25.0.4/otp_src_25.0.4.tar.gz"
-  sha256 "8fc707f92a124b2aeb0f65dcf9ac8e27b2a305e7bcc4cc1b2fdf770eec0165bf"
+  url "https://github.com/erlang/otp/releases/download/OTP-25.1/otp_src_25.1.tar.gz"
+  sha256 "a5ea27c1e07511a84bdd869c37f5e254f198c1cecf68ee9c8fedd23010750c31"
   license "Apache-2.0"
 
   livecheck do
