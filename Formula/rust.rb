5f94d98754ccf120e86fb5a81899976b67d4ec24
Rui Chen <rui@chenrui.dev>
2022-09-22 09:57:16 -0400

    rust 1.64.0

--- Formula/rust.rb
+++ Formula/rust.rb
@@ -4,8 +4,8 @@ class Rust < Formula
   license any_of: ["Apache-2.0", "MIT"]
 
   stable do
-    url "https://static.rust-lang.org/dist/rustc-1.63.0-src.tar.gz"
-    sha256 "1f9580295642ef5da7e475a8da2397d65153d3f2cb92849dbd08ed0effca99d0"
+    url "https://static.rust-lang.org/dist/rustc-1.64.0-src.tar.gz"
+    sha256 "b3cd9f481e1a2901bf6f3808d30c69cc4ea80d93c4cc4e2ed52258b180381205"
 
     # From https://github.com/rust-lang/rust/tree/#{version}/src/tools
     resource "cargo" do
