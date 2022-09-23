00e967fc281c982f050d5dc4ab54dcd4b630fce8
andrewoaws <112066778+andrewoaws@users.noreply.github.com>
2022-09-23 09:32:14 +0400

    Adding GoFlood

---
+++ Formula/goflood.rb
@@ -0,0 +1,33 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Goflood < Formula
+  desc "Make a lot of requests to a domain quickly.."
+  homepage "https://github.com/ao/goflood"
+  url "https://github.com/ao/goflood/releases/download/0.0.3/goflood_0.0.3_darwin_amd64.tar.gz"
+  sha256 "7fbffbe1601dec48e851a438397415f627b8efde988c95c3ec69e465a87cd160"
+  license "GNU General Public License v3.0"
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    system "./configure", *std_configure_args, "--disable-silent-rules"
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test goflood`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
