70c57dae844dd64150698e22bc85b38d893270e1
Michael Cho <cho-m@tuta.io>
2022-09-24 13:46:35 -0700

    gopass: remove duplicate completion install

--- Formula/gopass.rb
+++ Formula/gopass.rb
@@ -23,11 +23,7 @@ class Gopass < Formula
 
   def install
     system "make", "install", "PREFIX=#{prefix}/"
-
-    bash_completion.install "bash.completion" => "gopass.bash"
-    fish_completion.install "fish.completion" => "gopass.fish"
-    zsh_completion.install "zsh.completion" => "_gopass"
-    man1.install "gopass.1"
+    bash_completion.install share/"bash-completion/completions/gopass"
   end
 
   test do
