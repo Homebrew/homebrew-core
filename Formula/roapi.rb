2ec9befee47baa451fdad5f66c94134ad5ab69fd
geoHeil <e1225063@student.tuwien.ac.at>
2022-09-29 18:27:26 +0200

    enable features

--- Formula/roapi.rb
+++ Formula/roapi.rb
@@ -19,7 +19,7 @@ class Roapi < Formula
   def install
     # skip default features like snmalloc which errs on ubuntu 16.04
     system "cargo", "install", "--no-default-features",
-                               "--features", "rustls",
+                               "--features", "simd,rustls,database",
                                *std_cargo_args(path: "roapi")
   end
 
