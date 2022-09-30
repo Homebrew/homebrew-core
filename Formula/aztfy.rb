87eee85ac3004ea680c6737b1f6ec540be30d34c
Rui Chen <rui@chenrui.dev>
2022-09-30 10:54:23 -0400

    aztfy: update test
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/aztfy.rb
+++ Formula/aztfy.rb
@@ -16,6 +16,7 @@ class Aztfy < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "a5742042639aa27ef133d0c80061cf2ffb337e545367021d6931ad188c6e2a7c"
   end
   depends_on "go" => :build
+  depends_on "azure-cli" => :test
 
   def install
     ENV["CGO_ENABLED"] = "0"
@@ -27,6 +28,6 @@ class Aztfy < Formula
     assert_match version.to_s, version_output
 
     no_resource_group_specified_output = shell_output("#{bin}/aztfy rg 2>&1", 1)
-    assert_match("Error: No resource group specified", no_resource_group_specified_output)
+    assert_match("ERROR: Please run 'az login' to setup account", no_resource_group_specified_output)
   end
 end
