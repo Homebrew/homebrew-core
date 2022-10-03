7c1d224ad53c7ce34603d88a782bd62c5306736b
David Perrenoud <davidperrenoud@users.noreply.github.com>
2022-10-03 20:52:37 +0200

    nqc 3.2r1 (new formula)

---
+++ Formula/nqc.rb
@@ -0,0 +1,16 @@
+class Nqc < Formula
+  desc "Programming language for LEGO MINDSTORMS RCX, CyberMaster and Scout"
+  homepage "https://bricxcc.sourceforge.net/nqc/"
+  url "https://github.com/jverne/nqc/archive/refs/tags/v3.2r1.tar.gz"
+  sha256 "057ca37e92d4eb7b0418e4c503ed9df2e1d057dc30e9426ce3072ee6eb75271a"
+  license "MPL-2.0"
+
+  def install
+    ENV.deparallelize
+    system "make", "install", "PREFIX=#{prefix}"
+  end
+
+  test do
+    system "#{bin}/nqc"
+  end
+end
