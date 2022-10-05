49796ed449d073e1048f51cc8a1c4fa2b41deaf4
Masaharu TASHIRO <masatsr.kit@gmail.com>
2022-10-05 20:49:40 +0900

    licensed: fix audit errors

--- Formula/licensed.rb
+++ Formula/licensed.rb
@@ -1,20 +1,16 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
 class Licensed < Formula
-  desc "A Ruby gem to cache and verify the licenses of dependencies"
+  desc "Cache and verify the licenses of dependencies"
   homepage "https://github.com/github/licensed"
   url "https://github.com/github/licensed.git",
       tag:      "3.7.3",
       revision: "76727f75d486a24758890a030e540ebf87bba78b"
   license "MIT"
 
-  depends_on "cmake" => :build
-  depends_on "pkg-config" => :build
-
-  uses_from_macos "ruby"
+  depends_on "cmake"
+  depends_on "pkg-config"
+  depends_on "ruby@2.7"
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
     ENV["GEM_HOME"] = libexec
     system "gem", "build", "licensed.gemspec"
     system "gem", "install", "licensed-#{version}.gem"
@@ -23,6 +19,6 @@ class Licensed < Formula
   end
 
   test do
-    assert_equal "#{version}", shell_output("#{bin}/licensed version").strip
+    assert_equal version.to_s, shell_output("#{bin}/licensed version").strip
   end
 end
