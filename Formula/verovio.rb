9187042842d55956c99729d5cf24644d2ff42da2
Andrew Hankinson <andrew.hankinson@gmail.com>
2022-10-06 14:40:44 +0200

    Fix location of resource download

--- Formula/verovio.rb
+++ Formula/verovio.rb
@@ -8,15 +8,15 @@ class Verovio < Formula
 
   depends_on "cmake" => :build
 
+  resource("testdata") do
+    url "https://www.verovio.org/examples/downloads/Ahle_Jesu_meines_Herzens_Freud.mei"
+    sha256 "d08735930f5591b6d86250ed93795af156b8d0297ed38256fed84dc9739ed381"
+  end
+
   def install
     system "cmake", "-S", "./cmake", "-B", "tools", *std_cmake_args
     system "cmake", "--build", "tools"
     system "cmake", "--install", "tools"
-  end
-
-  resource("testdata") do
-    url "https://www.verovio.org/examples/downloads/Ahle_Jesu_meines_Herzens_Freud.mei"
-    sha256 "d08735930f5591b6d86250ed93795af156b8d0297ed38256fed84dc9739ed381"
   end
 
   test do
