13162ed6c4cdd5ac8ed420a720edc0e0de99474c
Rui Chen <rui@chenrui.dev>
2022-10-08 16:44:36 -0400

    ecasound: update homepage
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/ecasound.rb
+++ Formula/ecasound.rb
@@ -1,11 +1,11 @@
 class Ecasound < Formula
   desc "Multitrack-capable audio recorder and effect processor"
-  homepage "https://www.eca.cx/ecasound/"
+  homepage "https://nosignal.fi/ecasound/"
   url "https://ecasound.seul.org/download/ecasound-2.9.3.tar.gz"
   sha256 "468bec44566571043c655c808ddeb49ae4f660e49ab0072970589fd5a493f6d4"
 
   livecheck do
-    url "https://www.eca.cx/ecasound/download.php"
+    url "https://nosignal.fi/ecasound/download.php"
     regex(/href=.*?ecasound[._-]v?(\d+(?:\.\d+)+)\.t/i)
   end
 
