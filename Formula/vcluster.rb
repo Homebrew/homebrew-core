a5d72e769c3eade2382381cee561e71fb41b0f5f
Rui Chen <rui@chenrui.dev>
2022-10-08 12:03:55 -0400

    vcluster 0.12.2

--- Formula/vcluster.rb
+++ Formula/vcluster.rb
@@ -2,8 +2,8 @@ class Vcluster < Formula
   desc "Creates fully functional virtual k8s cluster inside host k8s cluster's namespace"
   homepage "https://www.vcluster.com"
   url "https://github.com/loft-sh/vcluster.git",
-      tag:      "v0.12.1",
-      revision: "86603ca11282e12dd25ecf7c8649dfc93bf196eb"
+      tag:      "v0.12.2",
+      revision: "faaa3c96a1fb737036a628d7e311697530f384a1"
   license "Apache-2.0"
   head "https://github.com/loft-sh/vcluster.git", branch: "main"
 
