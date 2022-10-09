46ed1d37351ee9b49a2ca4a684df652a3e4db6e6
Laslo Hunhold <dev@frign.de>
2022-10-09 14:58:42 +0200

    libgrapheme 2.0.1 (new formula)
    Signed-off-by: Laslo Hunhold <dev@frign.de>

---
+++ Formula/libgrapheme.rb
@@ -0,0 +1,43 @@
+class Libgrapheme < Formula
+  desc "Unicode string library"
+  homepage "https://libs.suckless.org/libgrapheme/"
+  url "https://dl.suckless.org/libgrapheme/libgrapheme-2.0.1.tar.gz"
+  sha256 "3db593a20fcbedcf4aa01c2ff973c430c40e19b961422ded95efe74a216cda2a"
+  license "ISC"
+  head "git://git.suckless.org/libgrapheme/", branch: "master"
+
+  def install
+    args = if OS.mac?
+      [
+        "PREFIX=#{prefix}",
+        "SOFLAGS=-dynamiclib -install_name 'libgrapheme.$(VERSION_MAJOR).dylib' " \
+        "-current_version '$(VERSION_MAJOR).$(VERSION_MINOR).$(VERSION_PATCH)' " \
+        "-compatibility_version '$(VERSION_MAJOR).$(VERSION_MINOR).0'",
+        "SONAME=libgrapheme.$(VERSION_MAJOR).dylib",
+        "SOSYMLINK=false",
+        "LDCONFIG=",
+      ]
+    else
+      [
+        "PREFIX=#{prefix}",
+        "LDCONFIG=",
+      ]
+    end
+
+    system "make", *args, "install"
+  end
+
+  test do
+    (testpath/"example.c").write <<~EOS
+      #include <grapheme.h>
+
+      int
+      main(void)
+      {
+        return (grapheme_next_word_break_utf8("Hello World!", SIZE_MAX) != 5);
+      }
+    EOS
+    system ENV.cc, "example.c", "-I#{include}", "-L#{lib}", "-lgrapheme", "-o", "example"
+    system "./example"
+  end
+end
