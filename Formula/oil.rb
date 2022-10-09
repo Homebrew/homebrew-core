7f37471b5c8367dae44825c855a09273ec93f795
Rui Chen <rui@chenrui.dev>
2022-10-09 16:39:16 -0400

    oil 0.12.7

--- Formula/oil.rb
+++ Formula/oil.rb
@@ -1,8 +1,8 @@
 class Oil < Formula
   desc "Bash-compatible Unix shell with more consistent syntax and semantics"
   homepage "https://www.oilshell.org/"
-  url "https://www.oilshell.org/download/oil-0.12.5.tar.gz"
-  sha256 "e7fad0b14deb64fa28e9db40060dcfa8288f04f0f019acf8d15fc85b60ea5770"
+  url "https://www.oilshell.org/download/oil-0.12.7.tar.gz"
+  sha256 "7ebd3a3270644b903db8b6ae070d8391020b19e4052a413925112d7eb3e0e80a"
   license "Apache-2.0"
 
   livecheck do
