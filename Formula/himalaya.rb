5f200cb945539562cd88d2239abb4607b84379e6
Rui Chen <rui@chenrui.dev>
2022-10-10 19:38:55 -0400

    himalaya 0.6.0

--- Formula/himalaya.rb
+++ Formula/himalaya.rb
@@ -1,8 +1,8 @@
 class Himalaya < Formula
   desc "CLI email client written in Rust"
   homepage "https://github.com/soywod/himalaya"
-  url "https://github.com/soywod/himalaya/archive/v0.5.10.tar.gz"
-  sha256 "2922b60caa37de70464a11df44704d8b5589f6d968532350267ffb136da54cbd"
+  url "https://github.com/soywod/himalaya/archive/v0.6.0.tar.gz"
+  sha256 "b9d88506de8b00ce5c80981f19989b2da062c80f8b35a70c60d349e51628cbb7"
   license "BSD-4-Clause"
 
   bottle do
