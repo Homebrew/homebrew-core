b4c462263c83c1266b16715315ac99d34c48de80
Kiracus <abc.kiracus@gmail.com>
2022-10-12 18:19:22 -0700

    srt: use `openssl@3`

--- Formula/srt.rb
+++ Formula/srt.rb
@@ -17,7 +17,7 @@ class Srt < Formula
 
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
-  depends_on "openssl@1.1"
+  depends_on "openssl@3"
 
   def install
     openssl = Formula["openssl@1.1"]
