044caed8b56615782bac3e38310a351ed0322ee6
Mark Nefedov <mark_nefedov@outlook.com>
2022-10-20 08:50:26 +0000

    Revert to old "make build" because of Mac incompatability.

---
+++ Formula/telepresence.rb
@@ -0,0 +1,24 @@
+class Telepresence < Formula
+  desc "Fast, efficient local development for Kubernetes microservices"
+  homepage "https://www.telepresence.io"
+  url "https://github.com/telepresenceio/telepresence.git",
+      tag:      "v2.8.2",
+      revision: "58ddcc58e5d5cb5ee15e159b26b6399400ebbf5c"
+  license "Apache-2.0"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  depends_on "go" => :build
+
+  def install
+    system "make", "build"
+    bin.install "build-output/bin/telepresence"
+  end
+
+  test do
+    assert_match("Root Daemon: Not running", shell_output("#{bin}/telepresence status"))
+  end
+end
