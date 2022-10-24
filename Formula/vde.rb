e9e8bde3210fce2193b84bac95b5a0923c53478a
Nicholas Ngai <nicholas@ngai.me>
2022-10-24 16:01:11 -0700

    vde: Update license

--- Formula/vde.rb
+++ Formula/vde.rb
@@ -3,7 +3,7 @@ class Vde < Formula
   homepage "https://github.com/virtualsquare/vde-2"
   url "https://github.com/virtualsquare/vde-2/archive/refs/tags/v2.3.3.tar.gz"
   sha256 "a7d2cc4c3d0c0ffe6aff7eb0029212f2b098313029126dcd12dc542723972379"
-  license "GPL-2.0"
+  license all_of: ["GPL-2.0-or-later", "LGPL-2.1-or-later"]
 
   head do
     url "https://github.com/virtualsquare/vde-2"
