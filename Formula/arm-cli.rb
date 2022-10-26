40d8ab534fc3d0d5a958273d197ed87eba4300cd
Costi Roman <costi@armory.io>
2022-10-26 18:47:48 +0300

    Added Armory CLI Formula for Spinnaker

--- Formula/arm-cli.rb
+++ Formula/arm-cli.rb
@@ -5,8 +5,6 @@ class Arm < Formula
   sha256 "c34c1d9a8ca92b2a449412680198d23d4b50ca08fd97ae7a0afaa49113a129b9"
   license "Apache-2.0"
 
-  # depends_on "cmake" => :build
-
   def install
     bin.install "arm-2.3.0-darwin-arm64" => "arm"
   end
@@ -16,5 +14,6 @@ class Arm < Formula
     assert_match "The Armory Platform CLI", run_output
 
     version_output = shell_output("#{bin}/arm version 2>&1")
+    assert_match = "rainmaker", version_output
   end
 end
