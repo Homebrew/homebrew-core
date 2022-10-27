3ea02bcb9058a18804e3e3f8362230f36c754dc7
James Laverack <james.laverack@jetstack.io>
2022-10-27 15:31:43 +0100

    cmctl 1.10.0 (new formula)

---
+++ Formula/cmctl.rb
@@ -0,0 +1,27 @@
+class Cmctl < Formula
+  desc "Command-line tool to manage cert-manager"
+  homepage "https://cert-manager.io"
+  url "https://github.com/cert-manager/cert-manager.git",
+    tag:      "v1.10.0",
+    revision: "da3265115bfd8be5780801cc6105fa857ef71965"
+  license "Apache-2.0"
+  head "https://github.com/cert-manager/cert-manager.git", branch: "master"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = %W[
+      -s -w
+      -X github.com/cert-manager/cert-manager/cmd/ctl/pkg/build.name=cmctl
+      -X github.com/cert-manager/cert-manager/cmd/ctl/pkg/build/commands.registerCompletion=true
+      -X github.com/cert-manager/cert-manager/pkg/util.AppVersion=v#{version}
+      -X github.com/cert-manager/cert-manager/pkg/util.AppGitCommit=#{Utils.git_head}
+    ]
+    system "go", "build", *std_go_args(ldflags: ldflags.join(" ")), "./cmd/ctl"
+    generate_completions_from_executable(bin/"cmctl", "completion")
+  end
+
+  test do
+    assert_match version.to_s, shell_output("cmctl version --client")
+  end
+end
