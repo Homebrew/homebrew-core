1a9c3eec1a9afc16d1091eaf182fd117e4d31db8
flyio-buildbot <63733732+flyio-buildbot@users.noreply.github.com>
2022-10-27 15:40:51 +0000

    flyctl 0.0.423

--- Formula/flyctl.rb
+++ Formula/flyctl.rb
@@ -2,8 +2,8 @@ class Flyctl < Formula
   desc "Command-line tools for fly.io services"
   homepage "https://fly.io"
   url "https://github.com/superfly/flyctl.git",
-      tag:      "v0.0.420",
-      revision: "977028ba41a19da88e55928a2454a23f62dc177f"
+      tag:      "v0.0.423",
+      revision: "8e567c77a73fa073efeb15882f052737a2a4edbf"
   license "Apache-2.0"
   head "https://github.com/superfly/flyctl.git", branch: "master"
 
