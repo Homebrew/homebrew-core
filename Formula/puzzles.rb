2c8d3711469e2ad1d6f08a782fcdc24c66f717db
Rui Chen <rui@chenrui.dev>
2022-10-28 08:42:48 -0400

    puzzles 20221028

--- Formula/puzzles.rb
+++ Formula/puzzles.rb
@@ -2,9 +2,9 @@ class Puzzles < Formula
   desc "Collection of one-player puzzle games"
   homepage "https://www.chiark.greenend.org.uk/~sgtatham/puzzles/"
   # Extract https://www.chiark.greenend.org.uk/~sgtatham/puzzles/puzzles.tar.gz to get the version number
-  url "https://www.chiark.greenend.org.uk/~sgtatham/puzzles/puzzles-20221027.6f5debe.tar.gz"
-  version "20221027"
-  sha256 "73497aeec3fc2685f982a4a596be058856dcd1a02bd55edb45c6ec08897a7d60"
+  url "https://www.chiark.greenend.org.uk/~sgtatham/puzzles/puzzles-20221028.1e8169e.tar.gz"
+  version "20221028"
+  sha256 "917727fa6e15ffafac031958aca6ada924278c3ca904a7c389b43508cf4fe169"
   license "MIT"
   head "https://git.tartarus.org/simon/puzzles.git", branch: "main"
 
