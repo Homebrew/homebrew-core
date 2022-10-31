3f52085174292cb59c552e9b9492dbe0530a31a0
Rui Chen <rui@chenrui.dev>
2022-10-31 12:45:12 -0400

    Update prr.rb

--- Formula/prr.rb
+++ Formula/prr.rb
@@ -16,10 +16,10 @@ class Prr < Formula
   end
 
   depends_on "rust" => :build
+  depends_on "openssl@3"
 
   on_linux do
     depends_on "pkg-config" => :build
-    depends_on "openssl@3"
   end
 
   def install
