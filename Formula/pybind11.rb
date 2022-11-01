f94fbb0768170c6f1f93d379bc2cfd01f8adac53
Rui Chen <rui@chenrui.dev>
2022-11-01 11:42:43 -0400

    pybind11 2.10.1

--- Formula/pybind11.rb
+++ Formula/pybind11.rb
@@ -1,8 +1,8 @@
 class Pybind11 < Formula
   desc "Seamless operability between C++11 and Python"
   homepage "https://github.com/pybind/pybind11"
-  url "https://github.com/pybind/pybind11/archive/v2.10.0.tar.gz"
-  sha256 "eacf582fa8f696227988d08cfc46121770823839fe9e301a20fbce67e7cd70ec"
+  url "https://github.com/pybind/pybind11/archive/v2.10.1.tar.gz"
+  sha256 "111014b516b625083bef701df7880f78c2243835abdb263065b6b59b960b6bad"
   license "BSD-3-Clause"
 
   livecheck do
