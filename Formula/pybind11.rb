9dd76e6978793f72782169ad8d6213223bcdee67
Rui Chen <rui@chenrui.dev>
2022-11-01 14:23:39 -0400

    pybind11: use python 3.11

--- Formula/pybind11.rb
+++ Formula/pybind11.rb
@@ -21,6 +21,7 @@ class Pybind11 < Formula
   end
 
   depends_on "cmake" => :build
+  depends_on "python@3.11" => [:build, :test]
   depends_on "python@3.10" => [:build, :test]
   depends_on "python@3.8" => [:build, :test]
   depends_on "python@3.9" => [:build, :test]
