0fa8e074aeab48eaf9bbda049fb9cc25a2159760
Laslo Hunhold <dev@frign.de>
2022-11-02 21:02:35 +0100

    Bump to version 2.0.2 and use ./configure instead of manual flags
    Signed-off-by: Laslo Hunhold <dev@frign.de>

--- Formula/libgrapheme.rb
+++ Formula/libgrapheme.rb
@@ -1,35 +1,14 @@
 class Libgrapheme < Formula
   desc "Unicode string library"
   homepage "https://libs.suckless.org/libgrapheme/"
-  url "https://dl.suckless.org/libgrapheme/libgrapheme-2.0.1.tar.gz"
-  sha256 "3db593a20fcbedcf4aa01c2ff973c430c40e19b961422ded95efe74a216cda2a"
+  url "https://dl.suckless.org/libgrapheme/libgrapheme-2.0.2.tar.gz"
+  sha256 "a68bbddde76bd55ba5d64116ce5e42a13df045c81c0852de9ab60896aa143125"
   license "ISC"
   head "git://git.suckless.org/libgrapheme/", branch: "master"
 
   def install
-    args = if OS.mac?
-      soflags = %w[
-        -dynamiclib
-        -installname 'libgrapheme.$(VERSION_MAJOR).dylib'
-        -current_version '$(VERSION_MAJOR).$(VERSION_MINOR).$(VERSION_PATCH)'
-        -compatibility_version '$(VERSION_MAJOR).$(VERSION_MINOR).0'
-      ]
-
-      [
-        "PREFIX=#{prefix}",
-        "SOFLAGS=#{soflags.join(" ")}",
-        "SONAME=libgrapheme.$(VERSION_MAJOR).dylib",
-        "SOSYMLINK=false",
-        "LDCONFIG=",
-      ]
-    else
-      [
-        "PREFIX=#{prefix}",
-        "LDCONFIG=",
-      ]
-    end
-
-    system "make", *args, "install"
+    system "./configure"
+    system "make", "PREFIX=#{prefix}", "LDCONFIG=", "install"
   end
 
   test do
