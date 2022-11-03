8aa80f6e0e0c6c2f2b4ad153417c3ecb4f471101
Tarn W. Burton <twburton@gmail.com>
2022-11-03 15:31:44 -0400

    clasp-cl (new formula).

---
+++ Formula/clasp-cl.rb
@@ -0,0 +1,36 @@
+class ClaspCl < Formula
+  desc "Common Lisp implementation that brings Common Lisp and C++ together"
+  homepage "https://github.com/clasp-developers/clasp"
+  url "https://github.com/clasp-developers/clasp/archive/refs/tags/2.0.0.tar.gz"
+  sha256 "de3c5efaad0ca1e87ce58e937a45f2740d0c5cd869a500b606eb9e2ea37cb837"
+  license "GPL-2.0-or-later"
+  head "https://github.com/clasp-developers/clasp.git",
+       using:  :git,
+       branch: "main"
+
+  depends_on "boost" => :build
+  depends_on "libunwind-headers" => :build
+  depends_on "ninja" => :build
+  depends_on "pkg-config" => :build
+  depends_on "sbcl" => :build
+  depends_on arch: :x86_64
+  depends_on "fmt"
+  depends_on "gmp"
+  depends_on "llvm@14"
+
+  conflicts_with "cando", because: "both install `clasp` binaries"
+
+  def install
+    ENV.deparallelize
+    system "./koga", "--reproducible-build", "--bin-path=#{bin}", "--share-path=#{share}/clasp/",
+      "--lib-path=#{lib}/clasp/", "--llvm-config=#{Formula["llvm@14"].opt_bin}/llvm-config",
+      "--pkg-config=#{Formula["pkg-config"].opt_bin}/pkg-config",
+      "--cxxflags=-I#{Formula["boost"].include}/", "--cppflags=-I#{Formula["boost"].include}/",
+      "--skip-sync=ansi-test,mps,cl-bench,cl-who"
+    system "ninja", "-C", "build", "install"
+  end
+
+  test do
+    assert_match "clasp-boehmprecise", shell_output("#{bin}/clasp --version")
+  end
+end
