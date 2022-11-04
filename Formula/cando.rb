5d3776f5fe81320430740183073a0e8cf0a72707
Tarn W. Burton <twburton@gmail.com>
2022-11-04 10:21:56 -0400

    cando (new formula).

---
+++ Formula/cando.rb
@@ -0,0 +1,39 @@
+class Cando < Formula
+  desc "Common Lisp implementation and Cando chemistry system"
+  homepage "https://github.com/clasp-developers/clasp"
+  url "https://github.com/clasp-developers/clasp/archive/refs/tags/2.0.0.tar.gz"
+  sha256 "de3c5efaad0ca1e87ce58e937a45f2740d0c5cd869a500b606eb9e2ea37cb837"
+  license "GPL-2.0-or-later"
+  head "https://github.com/clasp-developers/clasp.git",
+       using:  :git,
+       branch: "main"
+
+  depends_on "boost" => :build
+  depends_on "libunwind-headers" => :build
+  depends_on "pkg-config" => :build
+  depends_on "sbcl" => :build
+  depends_on arch: :x86_64
+  depends_on "expat"
+  depends_on "fmt"
+  depends_on "gmp"
+  depends_on "jupyterlab"
+  depends_on "llvm@14"
+  depends_on "netcdf"
+  depends_on "ninja"
+  depends_on "zeromq"
+
+  conflicts_with "clasp-cl", because: "both install `clasp` binaries"
+
+  def install
+    ENV.deparallelize
+    system "./koga", "--reproducible-build", "--bin-path=#{bin}", "--share-path=#{share}/clasp/",
+      "--lib-path=#{lib}/clasp/", "--extensions=cando,seqan-clasp",
+      "--skip-sync=ansi-test,mps,cl-bench,cl-who"
+    system "ninja", "-C", "build"
+    system "ninja", "-C", "build", "install"
+  end
+
+  test do
+    assert_match "clasp-boehmprecise", shell_output("#{bin}/clasp --version")
+  end
+end
