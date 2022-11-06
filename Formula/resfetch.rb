dd8d2964e95b9c2061d3667e57b36aca76c110f1
HitBlast <67194380+hitblast@users.noreply.github.com>
2022-11-06 16:34:07 +0600

    Update resfetch.rb

--- Formula/resfetch.rb
+++ Formula/resfetch.rb
@@ -2,7 +2,6 @@ class Resfetch < Formula
   desc "Fast and minimal alternative to neofetch"
   homepage "https://github.com/furtidev/resfetch"
   url "https://github.com/furtidev/resfetch/releases/download/1.0.2/resfetch-mac.tar.gz"
-  version "1.0.2"
   sha256 "f24118a8f6f82152308a6c2af359fd631551fc8fbfe380e30d889ce12868bd5e"
   license "MIT"
 
