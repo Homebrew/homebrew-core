11e285ce82bab40ed141fadfade369a989ca0ddd
Michael Cho <cho-m@tuta.io>
2022-11-06 19:45:35 -0800

    libcpuid: use official tarball

--- Formula/libcpuid.rb
+++ Formula/libcpuid.rb
@@ -1,10 +1,9 @@
 class Libcpuid < Formula
   desc "Small C library for x86 CPU detection and feature extraction"
   homepage "https://github.com/anrieff/libcpuid"
-  url "https://github.com/anrieff/libcpuid/archive/v0.6.1.tar.gz"
-  sha256 "115f84ceb14492b5c3c4dd4110b6a41012c879e31d81fae31e8713509b88a363"
+  url "https://github.com/anrieff/libcpuid/releases/download/v0.6.1/libcpuid-0.6.1.tar.gz"
+  sha256 "e11fdd852398499c2cfdee9c06d18d894918f7a38036a35f95b8e10b243082cd"
   license "BSD-2-Clause"
-  head "https://github.com/anrieff/libcpuid.git", branch: "master"
 
   bottle do
     sha256 cellar: :any,                 monterey:     "10ee187d9e292dac42be9924bf2b5ea2f495267335e5e5a56a35779d28ff3036"
@@ -14,15 +13,20 @@ class Libcpuid < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux: "20afc15713aa6f9097f50da5ea44d06beda67451c70c52639d9bb973c0f5d26f"
   end
 
-  depends_on "autoconf" => :build
-  depends_on "automake" => :build
-  depends_on "libtool" => :build
-  # libcpuid is x86 tool
+  head do
+    url "https://github.com/anrieff/libcpuid.git", branch: "master"
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+  end
+
+  # libcpuid is x86 tool. There is an open issue for arm support
+  # Ref: https://github.com/anrieff/libcpuid/issues/96
   depends_on arch: :x86_64
 
   def install
-    system "autoreconf", "-ivf"
-    system "./configure", *std_configure_args
+    system "autoreconf", "--force", "--install", "--verbose" if build.head?
+    system "./configure", *std_configure_args, "--disable-silent-rules"
     system "make", "install"
   end
 
