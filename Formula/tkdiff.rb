c40e146d500290efee279b6ae89e13163f5f4867
Rui Chen <rui@chenrui.dev>
2022-11-11 13:54:15 -0500

    tkdiff 5.6

--- Formula/tkdiff.rb
+++ Formula/tkdiff.rb
@@ -1,9 +1,9 @@
 class Tkdiff < Formula
   desc "Graphical side by side diff utility"
   homepage "https://tkdiff.sourceforge.io/"
-  url "https://downloads.sourceforge.net/project/tkdiff/tkdiff/5.5.2/tkdiff-5-5-2.zip"
-  version "5.5.2"
-  sha256 "31718411fa181ea4a6c9c7f8eea115a7b86c50dff5a1a070bc7ad26381d7176b"
+  url "https://downloads.sourceforge.net/project/tkdiff/tkdiff/5.6/tkdiff-5-6.zip"
+  version "5.6"
+  sha256 "628c7541d486996b6f984f4f702cec7cb3a7fd959d3e87104192bc3e946620bc"
   license "GPL-2.0-only"
 
   livecheck do
