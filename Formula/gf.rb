1b153277d8059fafd0cf7287541c0e05e21b2651
Rainshaw <rxg@live.com>
2022-11-13 17:56:51 +0800

    gf 2.2.2 (new formula)

---
+++ Formula/gf.rb
@@ -0,0 +1,28 @@
+class Gf < Formula
+  desc "App development framework of Golang"
+  homepage "https://goframe.org"
+  url "https://github.com/gogf/gf/archive/refs/tags/v2.2.2.tar.gz"
+  sha256 "6c289efa8ec11a035d350c61788ae041d74b0e61142bec87ea3461f01c16f88e"
+  license "MIT"
+
+  depends_on "go" => :build
+
+  def install
+    cd "cmd/gf" do
+      system "go", "build", *std_go_args(ldflags: "-s -w")
+    end
+  end
+
+  test do
+    output = shell_output("#{bin}/gf --version 2>&1")
+    assert_match "GoFrame CLI Tool v#{version}, https://goframe.org", output
+    assert_match "GoFrame Version: cannot find go.mod", output
+    assert_match "CLI Installed At: #{HOMEBREW_PREFIX}/Cellar/gf/#{version}/bin/gf", output
+    assert_match "Current is a custom installed version, no installation information.", output
+
+    output = shell_output("#{bin}/gf init test 2>&1")
+    assert_match "initializing...", output
+    assert_match "initialization done!", output
+    assert_match "you can now run \"cd test && gf run main.go\" to start your journey, enjoy!", output
+  end
+end
