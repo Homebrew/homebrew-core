f3b09bc793267469a14334478b45ecc9d5a42cc5
Rui Chen <rui@chenrui.dev>
2022-11-13 16:54:58 -0500

    ddh 0.13.0

--- Formula/ddh.rb
+++ Formula/ddh.rb
@@ -1,8 +1,8 @@
 class Ddh < Formula
   desc "Fast duplicate file finder"
   homepage "https://github.com/darakian/ddh"
-  url "https://github.com/darakian/ddh/archive/refs/tags/0.12.0.tar.gz"
-  sha256 "f16dd4da04852670912c1b3fd65ce9b6ebd01ba2d0df97cb8c9bdf91ba453384"
+  url "https://github.com/darakian/ddh/archive/refs/tags/0.13.0.tar.gz"
+  sha256 "87010f845fa68945d2def4a05a3eb796222b67c5d3cea41e576cfaf2ab078ef8"
   license "LGPL-3.0-only"
 
   livecheck do
