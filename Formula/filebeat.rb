132e677bad339a5c26aef999d14b016110dad9b3
Rui Chen <rui@chenrui.dev>
2022-11-15 20:34:20 -0500

    filebeat 8.5.1

--- Formula/filebeat.rb
+++ Formula/filebeat.rb
@@ -2,8 +2,8 @@ class Filebeat < Formula
   desc "File harvester to ship log files to Elasticsearch or Logstash"
   homepage "https://www.elastic.co/products/beats/filebeat"
   url "https://github.com/elastic/beats.git",
-      tag:      "v8.5.0",
-      revision: "6d6754fcb0adf6a2191b055d35f694c961c8ba40"
+      tag:      "v8.5.1",
+      revision: "f81376bad511929eb90d584d2059c4c8a41fc691"
   # Outside of the "x-pack" folder, source code in a given file is licensed
   # under the Apache License Version 2.0
   license "Apache-2.0"
