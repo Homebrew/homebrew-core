3a084235b9b2819914ab2ed88c5fef4eb01a9848
Porkepix <porkepix@gmail.com>
2022-11-18 02:36:36 +0100

    upx 4.0.1

--- Formula/upx.rb
+++ Formula/upx.rb
@@ -1,9 +1,8 @@
 class Upx < Formula
   desc "Compress/expand executable files"
   homepage "https://upx.github.io/"
-  url "https://github.com/upx/upx/releases/download/v3.96/upx-3.96-src.tar.xz"
-  sha256 "47774df5c958f2868ef550fb258b97c73272cb1f44fe776b798e393465993714"
-  revision 1
+  url "https://github.com/upx/upx/releases/download/v4.0.1/upx-4.0.1-src.tar.xz"
+  sha256 "77003c8e2e29aa9804e2fbaeb30f055903420b3e01d95eafe01aed957fb7e190"
   head "https://github.com/upx/upx.git", branch: "devel"
 
   bottle do
