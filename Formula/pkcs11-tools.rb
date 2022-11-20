26d0b339df9fc1b201d662441730208d5fc0f487
Rui Chen <rui@chenrui.dev>
2022-11-20 14:38:29 -0500

    pkcs11-tools: use openssl@3
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/pkcs11-tools.rb
+++ Formula/pkcs11-tools.rb
@@ -17,7 +17,7 @@ class Pkcs11Tools < Formula
 
   depends_on "pkg-config" => :build
   depends_on "softhsm" => :test
-  depends_on "openssl@1.1"
+  depends_on "openssl@3"
   uses_from_macos "bison" => :build
   uses_from_macos "flex" => :build
 
