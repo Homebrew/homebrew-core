d004ff5de05f1bdd4f5fd3e0d0aedbb0f8ecd671
Mike Fikes <mike@fikesfarm.com>
2022-11-20 09:28:53 -0500

    planck 2.27.0

--- Formula/planck.rb
+++ Formula/planck.rb
@@ -1,10 +1,9 @@
 class Planck < Formula
   desc "Stand-alone ClojureScript REPL"
   homepage "https://planck-repl.org/"
-  url "https://github.com/planck-repl/planck/archive/2.26.0.tar.gz"
-  sha256 "e2a01ea5cefcc08399a6bfc7204b228bfd0602b1126d5968fc976f48135a59b2"
+  url "https://github.com/planck-repl/planck/archive/2.27.0.tar.gz"
+  sha256 "d69be456efd999a8ace0f8df5ea017d4020b6bd806602d94024461f1ac36fe41"
   license "EPL-1.0"
-  revision 1
   head "https://github.com/planck-repl/planck.git", branch: "master"
 
   bottle do
