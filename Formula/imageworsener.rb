82d756d0c1abbd58b619ad554c26da1d5cafde44
Rui Chen <rui@chenrui.dev>
2022-11-22 23:31:15 -0500

    imageworsener: remove build patch
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/imageworsener.rb
+++ Formula/imageworsener.rb
@@ -1,18 +1,9 @@
 class Imageworsener < Formula
   desc "Utility and library for image scaling and processing"
   homepage "https://entropymine.com/imageworsener/"
+  url "https://entropymine.com/imageworsener/imageworsener-1.3.5.tar.gz"
+  sha256 "a7fbb65c5ade67d9ebc32e52c58988a4f986bacc008c9021fe36b598466d5c8d"
   license "MIT"
-
-  stable do
-    url "https://entropymine.com/imageworsener/imageworsener-1.3.5.tar.gz"
-    sha256 "a7fbb65c5ade67d9ebc32e52c58988a4f986bacc008c9021fe36b598466d5c8d"
-
-    # Fix -flat_namespace being used on Big Sur and later.
-    patch do
-      url "https://raw.githubusercontent.com/Homebrew/formula-patches/03cf8088210822aa2c1ab544ed58ea04c897d9c4/libtool/configure-big_sur.diff"
-      sha256 "35acd6aebc19843f1a2b3a63e880baceb0f5278ab1ace661e57a502d9d78c93c"
-    end
-  end
 
   livecheck do
     url :homepage
