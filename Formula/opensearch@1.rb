d5ed1a0a3ca267beb48a78f1c46c2264e817a045
Aad Mathijssen <aad.mathijssen@iodigital.com>
2022-11-23 13:40:45 +0100

    Resolve brew style warning

--- Formula/opensearch@1.rb
+++ Formula/opensearch@1.rb
@@ -12,7 +12,9 @@ class OpensearchAT1 < Formula
 
   # Backport of https://github.com/opensearch-project/OpenSearch/pull/1668
   # TODO: Remove when available in release
-  patch :DATA if Hardware::CPU.arm?
+  on_arm do
+    patch :DATA
+  end
 
   def install
     platform = OS.kernel_name.downcase
