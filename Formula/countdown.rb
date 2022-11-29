910787bd4bd7acb9d4d49f066a6375d435fca87c
Anton Medvedev <anton@medv.io>
2022-11-29 09:01:29 +0100

    Update Formula/countdown.rb
    Co-authored-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>

--- Formula/countdown.rb
+++ Formula/countdown.rb
@@ -1,5 +1,5 @@
 class Countdown < Formula
-  desc "Terminla countdown timer"
+  desc "Terminal countdown timer"
   homepage "https://github.com/antonmedv/countdown"
   url "https://github.com/antonmedv/countdown/archive/refs/tags/v1.0.0.tar.gz"
   sha256 "695bb6a57d7ba5a700ac16a7676132ee61c786a5e9a1ca774607d6f2ad5f39d7"
