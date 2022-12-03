5bb78252a7ae4b87685bd844aa56add18ff16232
Rui Chen <rui@chenrui.dev>
2022-12-03 13:24:49 -0500

    fabric-completion: add license
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/fabric-completion.rb
+++ Formula/fabric-completion.rb
@@ -3,6 +3,7 @@ class FabricCompletion < Formula
   homepage "https://github.com/n0740/fabric-completion"
   url "https://github.com/n0740/fabric-completion.git",
       revision: "5b5910492046e6335af0e88550176d2583d9a510"
+  license "MIT"
   version "1"
   head "https://github.com/n0740/fabric-completion.git", branch: "master"
 
