7280e772e1b390cbfbc28c1d203c9b69d3a6caf0
Porkepix <porkepix@gmail.com>
2022-12-05 02:31:08 +0100

    lndir 1.0.4

--- Formula/lndir.rb
+++ Formula/lndir.rb
@@ -1,8 +1,8 @@
 class Lndir < Formula
   desc "Create a shadow directory of symbolic links to another directory tree"
   homepage "https://gitlab.freedesktop.org/xorg/util/lndir"
-  url "https://www.x.org/releases/individual/util/lndir-1.0.3.tar.bz2"
-  sha256 "49f4fab0de8d418db4ce80dad34e9b879a4199f3e554253a8e1ab68f7c7cb65d"
+  url "https://www.x.org/releases/individual/util/lndir-1.0.4.tar.gz"
+  sha256 "b448b49a55d0750acfc3fd992c2511b21838ec2cea870d109bb9fdca2ac028da"
   license "MIT-open-group"
 
   bottle do
