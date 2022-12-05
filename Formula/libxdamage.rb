d3c928d28811c103fea7fb38bbf3a9b0319739cb
Porkepix <porkepix@gmail.com>
2022-12-05 02:22:21 +0100

    libxdamage 1.1.6

--- Formula/libxdamage.rb
+++ Formula/libxdamage.rb
@@ -1,8 +1,8 @@
 class Libxdamage < Formula
   desc "X.Org: X Damage Extension library"
   homepage "https://www.x.org/"
-  url "https://www.x.org/archive/individual/lib/libXdamage-1.1.5.tar.bz2"
-  sha256 "b734068643cac3b5f3d2c8279dd366b5bf28c7219d9e9d8717e1383995e0ea45"
+  url "https://www.x.org/archive/individual/lib/libXdamage-1.1.6.tar.gz"
+  sha256 "2afcc139eb6eb926ffe344494b1fc023da25def42874496e6e6d3aa8acef8595"
   license "MIT"
 
   bottle do
