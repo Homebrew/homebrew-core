4d75b0125834424d1ca228c1f84ab4df5bbddce2
miccal <miccal.matthews@gmail.com>
2022-12-07 05:55:38 +0800

    pcre2 10.41

--- Formula/pcre2.rb
+++ Formula/pcre2.rb
@@ -1,8 +1,8 @@
 class Pcre2 < Formula
   desc "Perl compatible regular expressions library with a new API"
   homepage "https://www.pcre.org/"
-  url "https://github.com/PCRE2Project/pcre2/releases/download/pcre2-10.40/pcre2-10.40.tar.bz2"
-  sha256 "14e4b83c4783933dc17e964318e6324f7cae1bc75d8f3c79bc6969f00c159d68"
+  url "https://github.com/PCRE2Project/pcre2/releases/download/pcre2-10.41/pcre2-10.41.tar.bz2"
+  sha256 "0f78cebd3e28e346475fb92e95fe9999945b4cbaad5f3b42aca47b887fb53308"
   license "BSD-3-Clause"
 
   livecheck do
