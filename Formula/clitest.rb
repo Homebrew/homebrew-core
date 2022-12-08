4879089126723f492233254f8c2c18c02a7934b7
Mogeko <zhengjunyi@live.com>
2022-12-08 20:02:45 +0100

    feat: calculate and set sha256

--- Formula/clitest.rb
+++ Formula/clitest.rb
@@ -3,7 +3,7 @@ class Clitest < Formula
   homepage "https://github.com/aureliojargas/clitest"
   url "https://github.com/aureliojargas/clitest/archive/refs/tags/0.4.0.tar.gz"
   head "https://github.com/aureliojargas/clitest.git", branch: "main"
-  sha256 ""
+  sha256 "e889fb1fdaae44f0911461cc74849ffefb1fef9b200584e1749b355e4f9a3997"
   license "MIT"
 
   uses_from_macos "sh" => :test
