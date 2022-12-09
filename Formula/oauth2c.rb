3de2ae0c9a8251179127c7c144091f9ea84a840d
Welton Rodrigo Torres Nascimento <rodrigo@familianascimento.org>
2022-12-09 13:53:20 -0300

    oauth2c 1.1.0 (new formula)

---
+++ Formula/oauth2c.rb
@@ -0,0 +1,22 @@
+class Oauth2c < Formula
+  desc "User-friendly CLI for OAuth2"
+  homepage "https://github.com/cloudentity/oauth2c"
+  url "https://github.com/cloudentity/oauth2c/archive/refs/tags/v1.1.0.tar.gz"
+  sha256 "ac67abf42b3f86f39d3cbb3412049dceeb7a3f83486435953c7c6e8b44d23df1"
+  license "Apache-2.0"
+  head "https://github.com/cloudentity/oauth2c.git", branch: "master"
+
+  depends_on "go" => :build
+
+  def install
+    ENV["GO111MODULE"] = "on"
+
+    system "go", "build", "-o", "oauth2c"
+    bin.install "oauth2c"
+  end
+
+  test do
+    assert_match "User-friendly command-line for OAuth2",
+      shell_output("#{bin}/oauth2c -h")
+  end
+end
