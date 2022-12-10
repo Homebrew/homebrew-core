635a14bc3824fe4f863ac847e09725009ff3a4fb
William Aaron Cheung <troublor@live.com>
2022-12-10 19:01:24 +0800

    grammarly-languageserver 0.22.1 (new formula)

---
+++ Formula/grammarly-languageserver.rb
@@ -0,0 +1,52 @@
+class GrammarlyLanguageserver < Formula
+  desc "A language server implementation on top of Grammarly's SDK."
+  homepage "https://github.com/znck/grammarly"
+  url "https://github.com/znck/grammarly/archive/refs/tags/v0.22.1.tar.gz"
+  sha256 "749e2577f7c04aa9a80a39e7e8cf619e7f7e85341d3a52b9c8174b3295e2e264"
+  license "MIT"
+
+  depends_on "node@16" 
+  depends_on "pnpm" => :build
+
+  def install
+    (buildpath/".npmrc").append_lines "node-linker = hoisted" 
+    system "pnpm", "install", "--reporter", "append-only"
+    system "pnpm", "rollup", "-c"
+
+    # Disabling shared-workspace-lockfile is necessary to make grammarly-languageserver 
+    # workspace a standalone package, by copying shared package into the node_modules 
+    # of the grammarly-languageserver workspace.
+    # However, this config will break the rollup compilation, so we reinstall dependency 
+    # after compilation (redunant works are automatically igored by pnpm)
+    (buildpath/".npmrc").append_lines "shared-workspace-lockfile = false" 
+    system "pnpm", "install", "--reporter", "append-only"
+    system "pnpm", "install", "--prod", "--reporter", "append-only"
+
+    (libexec/"lib/node_modules").mkpath
+    system "cp", "-Lr", "#{buildpath}/packages/grammarly-languageserver", "#{libexec}/lib/node_modules"
+    (libexec/"bin/grammarly-languageserver").write_env_script "#{libexec}/lib/node_modules/grammarly-languageserver/bin/server.js", :NODE_PATH => "#{libexec}/lib/node_modules"
+    system "chmod", "+x", "#{libexec}/bin/grammarly-languageserver"
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    require "open3"
+    json = <<~JSON
+      {
+        "jsonrpc": "2.0",
+        "id": 1,
+        "method": "initialize",
+        "params": {
+          "rootUri": null,
+          "capabilities": {}
+        }
+      }
+    JSON
+    Open3.popen3("#{bin}/grammarly-languageserver --stdio") do |stdin, stdout, _e, w|
+      stdin.write "Content-Length: #{json.size}\r\n\r\n#{json}"
+      sleep 1
+      assert_match(/^Content-Length: \d+/i, stdout.readline)
+      Process.kill("KILL", w.pid)
+    end
+  end
+end
