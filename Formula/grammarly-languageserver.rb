c7570b1cc3b0ab17b5f53d4829cb52a92dac5685
William Aaron Cheung <troublor@live.com>
2022-12-11 21:38:55 +0800

    grammarly-languageserver 0.0.4: use npm package

--- Formula/grammarly-languageserver.rb
+++ Formula/grammarly-languageserver.rb
@@ -1,41 +1,17 @@
+require "language/node"
+
 class GrammarlyLanguageserver < Formula
   desc "This is a language server implementation of Grammarly"
   homepage "https://github.com/znck/grammarly"
-  url "https://github.com/znck/grammarly/archive/refs/tags/v0.22.1.tar.gz"
-  sha256 "749e2577f7c04aa9a80a39e7e8cf619e7f7e85341d3a52b9c8174b3295e2e264"
+  url "https://registry.npmjs.org/grammarly-languageserver/-/grammarly-languageserver-0.0.4.tgz"
+  sha256 "0d50b88059b5a63c66e3973e94d4f368366087ef59427003106a99bb46c46728"
   license "MIT"
 
-  depends_on "pnpm" => :build
-  depends_on "node@16"
+  depends_on "node"
 
   def install
-    (buildpath/".npmrc").append_lines "node-linker = hoisted"
-    system "pnpm", "install", "--reporter", "append-only"
-    system "pnpm", "rollup", "-c"
-
-    # Disabling shared-workspace-lockfile is necessary to make grammarly-languageserver
-    # workspace a standalone package, by copying shared package into the node_modules
-    # of the grammarly-languageserver workspace.
-    # However, this config will break the rollup compilation, so we reinstall dependency
-    # after compilation (redunant works are automatically igored by pnpm)
-    (buildpath/".npmrc").append_lines "shared-workspace-lockfile = false"
-    system "pnpm", "install", "--reporter", "append-only"
-    # remove depDependencies
-    system "pnpm", "install", "--prod", "--reporter", "append-only"
-
-    dest = (libexec/"lib/node_modules")
-    dest.mkpath
-    dest.install "packages/grammarly-languageserver"
-    dep = dest/"grammarly-languageserver/node_modules"
-    rm dep/"grammarly-richtext-encoder"
-    dep.install "packages/grammarly-richtext-encoder"
-    (bin/"grammarly-languageserver").write <<~EOS
-      #!/bin/bash
-
-      export NODE_PATH="#{dest}"
-      exec "#{Formula["node@16"].bin}/node" "#{dest}/grammarly-languageserver/bin/server.js" "$@"
-    EOS
-    chmod "+x", bin/"grammarly-languageserver"
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
   end
 
   test do
@@ -51,7 +27,7 @@ class GrammarlyLanguageserver < Formula
         }
       }
     JSON
-    Open3.popen3("#{bin}/grammarly-languageserver --stdio") do |stdin, stdout, _e, w|
+    Open3.popen3("#{bin}/grammarly-languageserver --stdio") do |stdin, stdout, _, w|
       stdin.write "Content-Length: #{json.size}\r\n\r\n#{json}"
       sleep 3
       assert_match(/^Content-Length: \d+/i, stdout.readline)
