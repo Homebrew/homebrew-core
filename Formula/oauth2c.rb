c9d78bbd6de38e2e0c91c863e88fb291bd1b815f
Rui Chen <rui@chenrui.dev>
2022-12-10 20:08:00 -0500

    oauth2c: fix code style

--- Formula/oauth2c.rb
+++ Formula/oauth2c.rb
@@ -15,10 +15,10 @@ class Oauth2c < Formula
   test do
     assert_match "Authorization completed",
       shell_output("#{bin}/oauth2c https://oauth2c.us.authz.cloudentity.io/oauth2c/demo " \
-      "--client-id cauktionbud6q8ftlqq0 " \
-      "--client-secret HCwQ5uuUWBRHd04ivjX5Kl0Rz8zxMOekeLtqzki0GPc " \
-      "--grant-type client_credentials " \
-      "--auth-method client_secret_basic " \
-      "--scopes introspect_tokens,revoke_tokens")
+                   "--client-id cauktionbud6q8ftlqq0 " \
+                   "--client-secret HCwQ5uuUWBRHd04ivjX5Kl0Rz8zxMOekeLtqzki0GPc " \
+                   "--grant-type client_credentials " \
+                   "--auth-method client_secret_basic " \
+                   "--scopes introspect_tokens,revoke_tokens")
   end
 end
