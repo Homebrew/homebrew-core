6a85dd4422cdbd30be36b022eff53289a85b2fd1
Mattt <mattt@github.com>
2022-12-12 09:15:11 -0800

    Apply suggestions from code review
    Co-authored-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>

--- Formula/cog.rb
+++ Formula/cog.rb
@@ -5,13 +5,7 @@ class Cog < Formula
       tag:      "v0.5.1",
       revision: "b58bd63e3cd9b7db0a3410ef47c61c1b11255809"
   license "Apache-2.0"
-
   head "https://github.com/replicate/cog.git", branch: "main"
-
-  livecheck do
-    url :stable
-    strategy :github_latest
-  end
 
   depends_on "go" => :build
   depends_on "python@3.10" => :build
