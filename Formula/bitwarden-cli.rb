388cccb62624b93296b1fdf74a8272265b9b980c
Adam Moss <2951486+adam-moss@users.noreply.github.com>
2022-12-12 17:15:48 +0000

    Add BitWarden shell completion
    Signed-off-by: Adam Moss <2951486+adam-moss@users.noreply.github.com>

--- Formula/bitwarden-cli.rb
+++ Formula/bitwarden-cli.rb
@@ -6,6 +6,7 @@ class BitwardenCli < Formula
   url "https://registry.npmjs.org/@bitwarden/cli/-/cli-2022.11.0.tgz"
   sha256 "9c234a5b86d8f53503b96aa236da011b6c5af71a2c16f81a9e23aa1a6d9b6ce4"
   license "GPL-3.0-only"
+  revision 1
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "113d45ccf002db515cdbb089706b55e3c5fa8022bf431432112fcfd814f6b4e7"
@@ -22,6 +23,8 @@ class BitwardenCli < Formula
   def install
     system "npm", "install", *Language::Node.std_npm_install_args(libexec)
     bin.install_symlink Dir[libexec/"bin/*"]
+
+    generate_completions_from_executable(bin/"bw", "completion", shell_parameter_format: :arg, shells: [:zsh])
   end
 
   test do
