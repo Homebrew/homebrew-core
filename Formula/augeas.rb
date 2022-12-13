9d1f256bba415e3f8eee814bfc3f9e17ecfb95ec
Zhong Ruoyu <zhongruoyu@outlook.com>
2022-12-13 18:54:22 +0800

    augeas: fix build
    Fix submitted upstream at hercules-team/augeas#792.

--- Formula/augeas.rb
+++ Formula/augeas.rb
@@ -1,10 +1,21 @@
 class Augeas < Formula
   desc "Configuration editing tool and API"
   homepage "https://augeas.net/"
-  url "https://github.com/hercules-team/augeas/releases/download/release-1.14.0/augeas-1.14.0.tar.gz"
-  sha256 "8c101759ca3d504bd1d805e70e2f615fa686af189dd7cf0529f71d855c087df1"
   license "LGPL-2.1-or-later"
   head "https://github.com/hercules-team/augeas.git", branch: "master"
+
+  # Remove stable block when patch is no longer needed.
+  stable do
+    url "https://github.com/hercules-team/augeas/releases/download/release-1.14.0/augeas-1.14.0.tar.gz"
+    sha256 "8c101759ca3d504bd1d805e70e2f615fa686af189dd7cf0529f71d855c087df1"
+
+    # Fix "fatal error: 'malloc.h' file not found".
+    # Remove when https://github.com/hercules-team/augeas/pull/792 is merged.
+    patch do
+      url "https://github.com/hercules-team/augeas/commit/6cc785a46f2c651a299549eab25c6476c39f3080.patch?full_index=1"
+      sha256 "754beea4f75e6ada6a6093a41f8071d18e067f9d60137b135a4188a6e3a80227"
+    end
+  end
 
   livecheck do
     url :stable
