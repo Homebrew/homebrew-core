3d4e8286c66e51e9b66e5a035d7218a0845eae24
Ruoyu Zhong <zhongruoyu@outlook.com>
2022-12-14 02:52:24 +0800

    libspnav: update homepage
    Following redirection from the previous homepage.

--- Formula/libspnav.rb
+++ Formula/libspnav.rb
@@ -1,6 +1,6 @@
 class Libspnav < Formula
   desc "Client library for connecting to 3Dconnexion's 3D input devices"
-  homepage "https://spacenav.sourceforge.io"
+  homepage "https://spacenav.sourceforge.net/"
   url "https://github.com/FreeSpacenav/libspnav/releases/download/v1.1/libspnav-1.1.tar.gz"
   sha256 "04b297f68a10db4fa40edf68d7f823ba7b9d0442f2b665181889abe2cea42759"
 
