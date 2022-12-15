ae8aad711d4ce5cfa64c757827b68e565401fef4
Bastian Hofmann <bashofmann@gmail.com>
2022-12-15 10:55:53 +0100

    osv-scanner 1.0.0 (new formula)
    Signed-off-by: Bastian Hofmann <mail@bastianhofmann.de>

---
+++ Formula/osv-scanner.rb
@@ -0,0 +1,28 @@
+class OsvScanner < Formula
+  desc "Vulnerability scanner which uses the OSV database"
+  homepage "https://github.com/google/osv-scanner"
+  url "https://github.com/google/osv-scanner.git",
+      tag:      "v1.0.0",
+      revision: "c6ed0fabdbe8ad75465bd7eab81ba904a9c89f42"
+  license "Apache-2.0"
+  head "https://github.com/google/osv-scanner.git", branch: "main"
+
+  depends_on "go" => [:build]
+
+  def install
+    system "go", "build", *std_go_args, "-o", bin/"osv-scanner", "./cmd/osv-scanner/"
+  end
+
+  test do
+    (testpath/"go.mod").write <<~EOS
+      module my-library
+
+      require (
+        github.com/BurntSushi/toml v1.0.0
+      )
+    EOS
+
+    scan_output = shell_output("#{bin}/osv-scanner --lockfile #{testpath}/go.mod")
+    assert_match "found 1 packages", scan_output
+  end
+end
