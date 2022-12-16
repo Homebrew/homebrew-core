1670f256397ae5aeec0f985ba64f29ef49c2f533
Saul Gutierrez <sggutier@meta.com>
2022-12-16 15:42:43 -0800

    downgrade Sapling to Python 3.8

--- Formula/sapling.rb
+++ Formula/sapling.rb
@@ -22,13 +22,13 @@ class Sapling < Formula
   depends_on "gh"
   depends_on "node"
   depends_on "openssl@1.1"
-  depends_on "python@3.10"
+  depends_on "python@3.8"
 
   def install
     ENV["OPENSSL_DIR"] = Formula["openssl@1.1"].opt_prefix
-    ENV["PYTHON_SYS_EXECUTABLE"] = Formula["python@3.10"].opt_prefix/"bin/python3.10"
-    ENV["PYTHON"] = Formula["python@3.10"].opt_prefix/"bin/python3.10"
-    ENV["PYTHON3"] = Formula["python@3.10"].opt_prefix/"bin/python3.10"
+    ENV["PYTHON_SYS_EXECUTABLE"] = Formula["python@3.8"].opt_prefix/"bin/python3.8"
+    ENV["PYTHON"] = Formula["python@3.8"].opt_prefix/"bin/python3.8"
+    ENV["PYTHON3"] = Formula["python@3.8"].opt_prefix/"bin/python3.8"
     ENV["SAPLING_VERSION"] = version.to_s
 
     cd "eden/scm" do
