cd3e6dd349ccdf065d6f35c44bd4c0205120cf21
Rui Chen <rui@chenrui.dev>
2022-12-17 21:50:06 -0500

    keploy: fix code style issue

--- Formula/keploy.rb
+++ Formula/keploy.rb
@@ -1,26 +1,27 @@
 class Keploy < Formula
-  desc 'Testing for Developers. Toolkit that creates test-cases and data mocks from API calls, DB queries, etc.'
-  homepage 'https://keploy.io'
-  url 'https://github.com/keploy/keploy/archive/refs/tags/v0.6.5.tar.gz'
-  sha256 '2132d17037206119807f7d9274df839131a52fc950013d07477710ba00297933'
-  license 'Apache-2.0'
+  desc "Testing Toolkit creates test-cases and data mocks from API calls, DB queries"
+  homepage "https://keploy.io"
+  url "https://github.com/keploy/keploy/archive/refs/tags/v0.6.5.tar.gz"
+  sha256 "2132d17037206119807f7d9274df839131a52fc950013d07477710ba00297933"
+  license "Apache-2.0"
 
-  depends_on 'node' 
-  depends_on 'gatsby-cli' => :build
-  depends_on 'go' => :build
-  resource('ui') do
-    url 'https://github.com/keploy/ui/archive/refs/tags/0.1.0.tar.gz'
-    sha256 'd12cdad7fa1c77b8bd755030e9479007e9fcb476fecd0fa6938f076f6633028e'
+  depends_on "gatsby-cli" => :build
+  depends_on "go" => :build
+  depends_on "node"
+
+  resource("ui") do
+    url "https://github.com/keploy/ui/archive/refs/tags/0.1.0.tar.gz"
+    sha256 "d12cdad7fa1c77b8bd755030e9479007e9fcb476fecd0fa6938f076f6633028e"
   end
 
   def install
-    resource('ui').stage do
-      system 'npm', 'install', '--legacy-peer-deps', *Language::Node.local_npm_install_args
-      system 'gatsby', 'build'
-      buildpath.install Dir['./public']
+    resource("ui").stage do
+      system "npm", "install", "--legacy-peer-deps", *Language::Node.local_npm_install_args
+      system "gatsby", "build"
+      buildpath.install "./public"
     end
-    cp_r 'public', 'web', remove_destination: true
-    system 'go', 'build', *std_go_args, './cmd/server'
+    cp_r "public", "web", remove_destination: true
+    system "go", "build", *std_go_args, "./cmd/server"
   end
   test do
     port = free_port
@@ -30,7 +31,7 @@ class Keploy < Formula
     end
     sleep 6
   ensure
-    Process.kill('TERM', pid)
+    Process.kill("TERM", pid)
     Process.wait(pid)
   end
 end
