e14da386a2d39aa3523bfccc2c28975ca26f58c5
Max Bigras <mbigras22@gmail.com>
2022-12-17 19:50:43 -0800

    pyenv: change xz to depends_on
    Change xz—formerly lzma—pyenv dependency from uses_from_macos to depends_on
    because pyenv can't find any liblzma macOS system library but can find the
    liblzma Homebrew library in /usr/local/lib.
    
    Addresses the following pyenv warning message.
    $ pyenv install 3.10.9
    # ...
    WARNING: The Python lzma extension was not compiled. Missing the lzma lib?
    
    And also the following Python error message.
    $ python -c 'import lzma'
    # ...
    ModuleNotFoundError: No module named '_lzma'

--- Formula/pyenv.rb
+++ Formula/pyenv.rb
@@ -26,12 +26,12 @@ class Pyenv < Formula
   depends_on "openssl@1.1"
   depends_on "pkg-config"
   depends_on "readline"
+  depends_on "xz"
 
   uses_from_macos "python" => :test
   uses_from_macos "bzip2"
   uses_from_macos "libffi"
   uses_from_macos "ncurses"
-  uses_from_macos "xz"
   uses_from_macos "zlib"
 
   def install
