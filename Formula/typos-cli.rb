d9e7a2ab2f854452a66464681fe455cab0ee6e9c
Rui Chen <rui@chenrui.dev>
2022-12-19 23:07:00 -0500

    typos-cli 1.13.5

--- Formula/typos-cli.rb
+++ Formula/typos-cli.rb
@@ -1,8 +1,8 @@
 class TyposCli < Formula
   desc "Source code spell checker"
   homepage "https://github.com/crate-ci/typos"
-  url "https://github.com/crate-ci/typos/archive/refs/tags/v1.12.14.tar.gz"
-  sha256 "ace6adea86c9f0430b6957710d51fa9b48b46224deb10ef215efc5f62fdfc831"
+  url "https://github.com/crate-ci/typos/archive/refs/tags/v1.13.5.tar.gz"
+  sha256 "48cb286f5bcc7f0d82418711582b8236ab0cecf3ce2b320f2fa443794dfba26f"
   license any_of: ["Apache-2.0", "MIT"]
 
   bottle do
