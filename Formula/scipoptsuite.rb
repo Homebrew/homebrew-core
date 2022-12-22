3f4336496d3b1b9704ccfebf9d5a05ac3c4f3a3b
Jurgen Lentz <jurgen.lentz@rwth-aachen.de>
2022-12-22 23:10:54 +0100

    change order of dependencies v2

--- Formula/scipoptsuite.rb
+++ Formula/scipoptsuite.rb
@@ -3,13 +3,13 @@ class Scipoptsuite < Formula
   homepage "https://scipopt.org/"
   url "https://www.scipopt.org/download/release/scipoptsuite-8.0.3.tgz"
   sha256 "5ad50eb42254c825d96f5747d8f3568dcbff0284dfbd1a727910c5a7c2899091"
-  license all_of: ["Apache 2.0", "ZIB-Academic", "LGPL-3.0-or-later"]
+  license all_of: ["Apache-2.0", "LGPL-3.0-or-later"]
 
   depends_on "cmake" => :build
   depends_on "bison"
   depends_on "boost"
-  depends_on "flex"
   depends_on "cppad"
+  depends_on "flex"
   depends_on "gmp"
   depends_on "gnuplot"
   depends_on "ipopt"
@@ -50,13 +50,13 @@ class Scipoptsuite < Formula
     # Verifies that dependencies are properly linked
     output = shell_output("scip --version")
     assert_match "CppAD\s+[0-9]+", output
-    #output = shell_output("scip --version")
-    #assert_match "ZLIB\s+[0-9]+\.[0-9]+\.[0-9]+", output
-    #output = shell_output("scip --version")
-    #assert_match "GMP\s+[0-9]+\.[0-9]+\.[0-9]+", output
-    #output = shell_output("scip --version")
-    #assert_match "bliss\s+[0-9]+\.[0-9]+", output
-    #output = shell_output("scip --version")
-    #assert_match "Ipopt\s+[0-9]+\.[0-9]+\.[0-9]+", output
+    # output = shell_output("scip --version")
+    # assert_match "ZLIB\s+[0-9]+\.[0-9]+\.[0-9]+", output
+    # output = shell_output("scip --version")
+    # assert_match "GMP\s+[0-9]+\.[0-9]+\.[0-9]+", output
+    # output = shell_output("scip --version")
+    # assert_match "bliss\s+[0-9]+\.[0-9]+", output
+    # output = shell_output("scip --version")
+    # assert_match "Ipopt\s+[0-9]+\.[0-9]+\.[0-9]+", output
   end
 end
