89a762794792a353c3aaf9450435586006679cbb
Jurgen Lentz <jurgen.lentz@rwth-aachen.de>
2022-12-23 13:31:15 +0100

    add cmake for test

--- Formula/scipoptsuite.rb
+++ Formula/scipoptsuite.rb
@@ -5,7 +5,7 @@ class Scipoptsuite < Formula
   sha256 "5ad50eb42254c825d96f5747d8f3568dcbff0284dfbd1a727910c5a7c2899091"
   license all_of: ["Apache-2.0", "LGPL-3.0-or-later"]
 
-  depends_on "cmake" => :build
+  depends_on "cmake"
   depends_on "bison@2.7"
   depends_on "boost"
   depends_on "cppad"
@@ -50,7 +50,7 @@ class Scipoptsuite < Formula
 
     system "cmake", "-B", "build", "-S", "#{prefix}/MIPSolver"
     system "cmake", "--build", "build"
-    output = shell_output("#{prefix}/build/scipmip", "-c \"r #{prefix}/p0548.mps opt q\"")
+    output = shell_output("#{prefix}/build/bin/scipmip", "-c \"r #{prefix}/p0548.mps opt q\"")
     assert_match "optimal solution found", output
   end
 end
