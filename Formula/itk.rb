60a60b5ed8930b543e892152624cb62a4f66e0aa
Patrick Linnane <patrick@linnane.io>
2022-12-22 21:49:15 -0800

    itk 5.3.0

--- Formula/itk.rb
+++ Formula/itk.rb
@@ -1,10 +1,9 @@
 class Itk < Formula
   desc "Insight Toolkit is a toolkit for performing registration and segmentation"
   homepage "https://itk.org"
-  url "https://github.com/InsightSoftwareConsortium/ITK/releases/download/v5.2.1/InsightToolkit-5.2.1.tar.gz"
-  sha256 "192d41bcdd258273d88069094f98c61c38693553fd751b54f8cda308439555db"
+  url "https://github.com/InsightSoftwareConsortium/ITK/releases/download/v5.3.0/InsightToolkit-5.3.0.tar.gz"
+  sha256 "57a4471133dc8f76bde3d6eb45285c440bd40d113428884a1487472b7b71e383"
   license "Apache-2.0"
-  revision 4
   head "https://github.com/InsightSoftwareConsortium/ITK.git", branch: "master"
 
   livecheck do
