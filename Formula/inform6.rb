2a8b12503c4a1045e8d815be9d6277d87462e8d8
Patrick Linnane <patrick@linnane.io>
2022-12-22 21:48:52 -0800

    inform6 6.41-r2

--- Formula/inform6.rb
+++ Formula/inform6.rb
@@ -1,9 +1,9 @@
 class Inform6 < Formula
   desc "Design system for interactive fiction"
   homepage "https://inform-fiction.org/inform6.html"
-  url "https://ifarchive.org/if-archive/infocom/compilers/inform6/source/inform-6.36-r4.tar.gz"
-  version "6.36-r4"
-  sha256 "9becbad0cc737e993a5fc969b9ee9689781e3884658d52b17db68dad55010f2d"
+  url "https://ifarchive.org/if-archive/infocom/compilers/inform6/source/inform-6.41-r2.tar.gz"
+  version "6.41-r2"
+  sha256 "a6043b2df82173474635dcbcccfe510cdb2e1ef24be8abb9a8b0e46cd8a5f6e0"
   license "Artistic-2.0"
   head "https://gitlab.com/DavidGriffith/inform6unix.git", branch: "master"
 
