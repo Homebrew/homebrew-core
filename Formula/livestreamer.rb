ef52ce57c931bb96d9a5674979e219dfdcc47036
ryenus <ryenus@gmail.com>
2022-12-26 14:52:40 +0800

    livestreamer: migrate to python@3.11

--- Formula/livestreamer.rb
+++ Formula/livestreamer.rb
@@ -22,7 +22,7 @@ class Livestreamer < Formula
   # https://github.com/chrippa/livestreamer/issues/1427
   deprecate! date: "2022-11-07", because: :unmaintained
 
-  depends_on "python@3.10"
+  depends_on "python@3.11"
 
   resource "certifi" do
     url "https://files.pythonhosted.org/packages/6c/ae/d26450834f0acc9e3d1f74508da6df1551ceab6c2ce0766a593362d6d57f/certifi-2021.10.8.tar.gz"
