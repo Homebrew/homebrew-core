7775b35813b845a7c19a12a85939fe7ca7b56af6
Marcin Iwanicki <miwanicki1@bloomberg.net>
2022-12-29 17:52:36 +0000

    Fix line is too long issue
    se --formula parameter

--- Formula/xcdiff.rb
+++ Formula/xcdiff.rb
@@ -9,7 +9,7 @@ class Xcdiff < Formula
 
   resource "homebrew-testdata" do
     url "https://github.com/bloomberg/xcdiff/archive/refs/tags/0.9.0.tar.gz"
-    sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+    sha256 "f8565e0395a41274019ba691a9588e34f06ae586b921f43c0bd792981dcdb53a"
   end
 
   def install
@@ -26,7 +26,12 @@ class Xcdiff < Formula
         "#{bin}/xcdiff -p1 Fixtures/ios_project_1/Project.xcodeproj -p2 Fixtures/ios_project_1/Project.xcodeproj -d",
       )
       assert_match "✅ BUILD_PHASES > \"Project\" target\n", shell_output(
-        "#{bin}/xcdiff -p1 Fixtures/ios_project_1/Project.xcodeproj -p2 Fixtures/ios_project_1/Project.xcodeproj -g BUILD_PHASES -t Project -v",
+        "#{bin}/xcdiff " \
+        "-p1 Fixtures/ios_project_1/Project.xcodeproj " \
+        "-p2 Fixtures/ios_project_1/Project.xcodeproj " \
+        "-g BUILD_PHASES " \
+        "-t Project " \
+        "-v",
       )
     end
   end
