e64b752cc42d40f73bd12e46c1f58f195aca86eb
Sean Molenaar <SMillerDev@users.noreply.github.com>
2022-12-29 21:26:00 +0100

    dict: update license

--- Formula/dict.rb
+++ Formula/dict.rb
@@ -3,7 +3,7 @@ class Dict < Formula
   homepage "https://www.dict.org/"
   url "https://downloads.sourceforge.net/project/dict/dictd/dictd-1.13.1/dictd-1.13.1.tar.gz"
   sha256 "e4f1a67d16894d8494569d7dc9442c15cc38c011f2b9631c7f1cc62276652a1b"
-  license "GPL-2.0"
+  license "GPL-2.0-or-later"
 
   bottle do
     sha256 arm64_ventura:  "51ab6ccc175d30964e823fe0496fe957adc402eababac5d0ed7dbf766f47a5c6"
