8b10cdb9ba678eddddae2fb44ff143ea9db5b243
Chris Coutinho <chrisbcoutinho@gmail.com>
2022-12-31 20:54:16 +0100

    Add test to temporalite Formula

--- Formula/temporalite.rb
+++ Formula/temporalite.rb
@@ -5,6 +5,7 @@ class Temporalite < Formula
   sha256 "a4980cca9b265694dca97e696fbe9136b2bb4ceecfc44a43cfc7d1ac55a3009b"
   license "MIT"
 
+  depends_on "tctl" => :test
   depends_on "go" => :build
 
   def install
@@ -12,7 +13,15 @@ class Temporalite < Formula
   end
 
   test do
-    run_output = shell_output("#{bin}/temporalite --version")
-    assert_match "temporalite version #{version}", run_output
+    begin
+      fork do
+        exec "temporalite start --headless --ephemeral --namespace default"
+      end
+
+      sleep 5
+
+      system "tctl namespace list"
+      system "tctl workflow list"
+    end
   end
 end
