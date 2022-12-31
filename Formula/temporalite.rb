85f1561f917a79bf8bc4e885566f74109c76cb45
Chris Coutinho <chrisbcoutinho@gmail.com>
2022-12-31 21:02:18 +0100

    fix 'brew audit'

--- Formula/temporalite.rb
+++ Formula/temporalite.rb
@@ -5,23 +5,21 @@ class Temporalite < Formula
   sha256 "a4980cca9b265694dca97e696fbe9136b2bb4ceecfc44a43cfc7d1ac55a3009b"
   license "MIT"
 
-  depends_on "tctl" => :test
   depends_on "go" => :build
+  depends_on "tctl" => :test
 
   def install
     system "go", "build", *std_go_args(ldflags: "-s -w"), "./cmd/temporalite"
   end
 
   test do
-    begin
-      fork do
-        exec "temporalite start --headless --ephemeral --namespace default"
-      end
-
-      sleep 5
-
-      system "tctl namespace list"
-      system "tctl workflow list"
+    fork do
+      exec "temporalite start --headless --ephemeral --namespace default"
     end
+
+    sleep 5
+
+    system "tctl", "namespace", "list"
+    system "tctl", "workflow", "list"
   end
 end
