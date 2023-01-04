dbe4c1b1094f43fee26114fb06181a442d31a63d
Patrick Linnane <patrick@linnane.io>
2023-01-04 08:33:36 -0800

    mx: update resource

--- Formula/mx.rb
+++ Formula/mx.rb
@@ -18,8 +18,8 @@ class Mx < Formula
   depends_on "python@3.11"
 
   resource "homebrew-testdata" do
-    url "https://github.com/oracle/graal/archive/refs/tags/vm-22.1.0.1.tar.gz"
-    sha256 "7653558bc4e4a5f89e5ddef7242ddc1ec5582ec75bdc94997feb76ed12ce8e94"
+    url "https://github.com/oracle/graal/archive/refs/tags/vm-22.3.0.tar.gz"
+    sha256 "410a003b8bab17af86fbc072d549e02e795b862a8396d08af9794febee17bad4"
   end
 
   def install
