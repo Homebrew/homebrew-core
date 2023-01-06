705e5e686cea278b4a0171a5743b7a23ca6492ed
kcleal <clealk@cardiff.ac.uk>
2023-01-06 17:51:13 +0000

    Fixing GW build 2

--- Formula/gw.rb
+++ Formula/gw.rb
@@ -4,9 +4,12 @@ class Gw < Formula
   url "https://github.com/kcleal/gw/archive/refs/tags/v0.3.1.tar.gz"
   sha256 "b479e19c2fed0704a652c0fcbd653517ce01ac429ec7753c95a583c35a733012"
   license "MIT"
+  depends_on "fontconfig"
   depends_on "glfw"
   depends_on "htslib"
+  depends_on "icu4c"
   depends_on "wget"
+  depends_on "zlib"
 
   def install
     system "make", "prep"
