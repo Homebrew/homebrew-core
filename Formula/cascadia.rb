b6e643b3d97f455d27a04f65c456563ce85035f0
Glenn 'devalias' Grant <glenn@devalias.net>
2023-01-09 10:42:20 +1100

    bump cascadia from 1.2.6 to 1.2.7

--- Formula/cascadia.rb
+++ Formula/cascadia.rb
@@ -1,17 +1,14 @@
 class Cascadia < Formula
   desc "Go cascadia package command-line CSS selector"
   homepage "https://github.com/suntong/cascadia"
-  url "https://github.com/suntong/cascadia/archive/refs/tags/v1.2.6.tar.gz"
-  sha256 "eb7f88f23f176e74d1aeb3b34efeb7763de8ad5445566bc92ed9fee33e9cebd1"
+  url "https://github.com/suntong/cascadia/archive/refs/tags/v1.2.7.tar.gz"
+  sha256 "64df164638843322f45aa6b736f2e4a6750b79514722bbb2a927707432bcf22c"
   license "MIT"
   head "https://github.com/suntong/cascadia.git", branch: "master"
 
   depends_on "go" => :build
 
   def install
-    # go mod init/tidy are needed until https://github.com/suntong/cascadia/issues/13 is fixed
-    system "go", "mod", "init", "github.com/suntong/cascadia"
-    system "go", "mod", "tidy"
     system "go", "build", *std_go_args(ldflags: "-s -w")
   end
 
