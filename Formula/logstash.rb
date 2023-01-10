835f656b63ec67fe86e67fbe948f7d4a2d3bc92a
Patrick Linnane <patrick@linnane.io>
2023-01-10 09:55:49 -0800

    logstash 8.6.0

--- Formula/logstash.rb
+++ Formula/logstash.rb
@@ -1,8 +1,8 @@
 class Logstash < Formula
   desc "Tool for managing events and logs"
   homepage "https://www.elastic.co/products/logstash"
-  url "https://github.com/elastic/logstash/archive/v8.5.3.tar.gz"
-  sha256 "423cf8ebb8719afb7b3c1e4f8e26d9e0726494263714ee4ac32e3ccf9533f403"
+  url "https://github.com/elastic/logstash/archive/v8.6.0.tar.gz"
+  sha256 "c7da98206fdd6cdbef3dfa9fbdded07553ddeef8a5e8454fc8f2afae26d530df"
   license "Apache-2.0"
   version_scheme 1
   head "https://github.com/elastic/logstash.git", branch: "main"
