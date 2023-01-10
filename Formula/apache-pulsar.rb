7c284e5519151c4ac6ecf49c872424985e695631
Porkepix <porkepix@gmail.com>
2023-01-10 17:07:31 +0100

    apache-pulsar 2.11.0

--- Formula/apache-pulsar.rb
+++ Formula/apache-pulsar.rb
@@ -1,9 +1,9 @@
 class ApachePulsar < Formula
   desc "Cloud-native distributed messaging and streaming platform"
   homepage "https://pulsar.apache.org/"
-  url "https://www.apache.org/dyn/mirrors/mirrors.cgi?action=download&filename=pulsar/pulsar-2.10.3/apache-pulsar-2.10.3-src.tar.gz"
-  mirror "https://archive.apache.org/dist/pulsar/pulsar-2.10.3/apache-pulsar-2.10.3-src.tar.gz"
-  sha256 "4fca38025c6059b0cb1b8c8ca7526a6c525769529c270a0172e2294d311b8f96"
+  url "https://www.apache.org/dyn/mirrors/mirrors.cgi?action=download&filename=pulsar/pulsar-2.11.0/apache-pulsar-2.11.0-src.tar.gz"
+  mirror "https://archive.apache.org/dist/pulsar/pulsar-2.11.0/apache-pulsar-2.11.0-src.tar.gz"
+  sha256 "a1d45168808ea0b09b4bfe442937b8804bb91181cb502f2ae2c1d13e81fa81fc"
   license "Apache-2.0"
   head "https://github.com/apache/pulsar.git", branch: "master"
 
