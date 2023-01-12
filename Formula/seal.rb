e9ae62515689f9b54db94a4cd5e21a62fcc0fc9f
Rui Chen <rui@chenrui.dev>
2023-01-12 00:36:29 -0500

    seal: remove one patch

--- Formula/seal.rb
+++ Formula/seal.rb
@@ -32,10 +32,6 @@ class Seal < Formula
 
   # patch cmake configs, remove in next release
   patch do
-    url "https://github.com/microsoft/SEAL/commit/0a838b11f1547e265edb75f00a90b245edc664a4.patch?full_index=1"
-    sha256 "9f72279b15e2a4890cc36bc60459b20827883a6fab0048b383a447cd0b60f0fc"
-  end
-  patch do
     url "https://github.com/microsoft/SEAL/commit/13e94ef0e01aa9874885bbfdbca1258ab380ddeb.patch?full_index=1"
     sha256 "19e3dde5aeb78c01dbe5ee73624cf4621060d071ab1a437515eedc00b47310a1"
   end
