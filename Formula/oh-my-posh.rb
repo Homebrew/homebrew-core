9a919fa49d9ecb1a6c647cb6472075154c8fc281
Rui Chen <rui@chenrui.dev>
2023-01-12 11:30:30 -0500

    oh-my-posh 13.1.0

--- Formula/oh-my-posh.rb
+++ Formula/oh-my-posh.rb
@@ -1,8 +1,8 @@
 class OhMyPosh < Formula
   desc "Prompt theme engine for any shell"
   homepage "https://ohmyposh.dev"
-  url "https://github.com/JanDeDobbeleer/oh-my-posh/archive/v13.0.0.tar.gz"
-  sha256 "5e25d663bac09fd8cc924d971fa757514c7a6408d3df6eb08e7549c9e5baee8f"
+  url "https://github.com/JanDeDobbeleer/oh-my-posh/archive/v13.1.0.tar.gz"
+  sha256 "0da13d03ad34efc2816e5835f8052630f27c8d681b933c9a7133f4425da7ecc9"
   license "MIT"
   head "https://github.com/JanDeDobbeleer/oh-my-posh.git", branch: "main"
 
