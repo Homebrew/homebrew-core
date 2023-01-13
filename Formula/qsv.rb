d1faab052e01e89eec497f4b278f76b6c6c1cb44
Patrick Linnane <patrick@linnane.io>
2023-01-13 11:30:42 -0800

    qsv 0.83.0

--- Formula/qsv.rb
+++ Formula/qsv.rb
@@ -1,8 +1,8 @@
 class Qsv < Formula
   desc "Ultra-fast CSV data-wrangling toolkit"
   homepage "https://github.com/jqnatividad/qsv"
-  url "https://github.com/jqnatividad/qsv/archive/refs/tags/0.82.0.tar.gz"
-  sha256 "230f72056ade313078b73398261ba78833eb7d749a44944ab7e04e232c0293fa"
+  url "https://github.com/jqnatividad/qsv/archive/refs/tags/0.83.0.tar.gz"
+  sha256 "588ec7603e16a7fc3d00bd2e78465837c4cdf2fbd0fc648ef6196138640b17b5"
   license any_of: ["MIT", "Unlicense"]
   head "https://github.com/jqnatividad/qsv.git", branch: "master"
 
