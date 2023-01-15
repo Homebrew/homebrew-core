4f1924f7b977d1c99f8f99bc0c45950bc5c32048
Michka Popoff <michkapopoff@gmail.com>
2023-01-15 14:22:48 +0100

    docfx 2.60.0

--- Formula/docfx.rb
+++ Formula/docfx.rb
@@ -1,8 +1,8 @@
 class Docfx < Formula
   desc "Tools for building and publishing API documentation for .NET projects"
   homepage "https://dotnet.github.io/docfx/"
-  url "https://github.com/dotnet/docfx/releases/download/v2.59.4/docfx.zip"
-  sha256 "6de3058630cd89eeee8157d26a81d508a3ce5c0b64b0473c6360bf10a985a52b"
+  url "https://github.com/dotnet/docfx/archive/refs/tags/v2.60.0.tar.gz"
+  sha256 "884a1808f2dc69e7ab281950548690303ece43aad73ce94cb33a7ae9e687c078"
   license "MIT"
 
   bottle do
