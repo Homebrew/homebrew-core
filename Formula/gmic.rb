dc6c7225a3ac002fa6311e62895017fcbf1cb322
Patrick Linnane <patrick@linnane.io>
2023-01-16 18:37:29 -0800

    gmic 3.2.0

--- Formula/gmic.rb
+++ Formula/gmic.rb
@@ -1,8 +1,8 @@
 class Gmic < Formula
   desc "Full-Featured Open-Source Framework for Image Processing"
   homepage "https://gmic.eu/"
-  url "https://gmic.eu/files/source/gmic_3.1.6.tar.gz"
-  sha256 "e73d63ba6520637c782b6e5e6805037a27b6255c011c9e2a60a56f9c28822ce4"
+  url "https://gmic.eu/files/source/gmic_3.2.0.tar.gz"
+  sha256 "8db3ba8ae003c4946a26d643b8173f7aa49e9c84f2ee1f74140165d3f20d3116"
   license "CECILL-2.1"
   head "https://github.com/dtschump/gmic.git", branch: "master"
 
