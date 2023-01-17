8ebd0f5df80a73e6659221b2bb2cce2850706cb4
Patrick Linnane <patrick@linnane.io>
2023-01-16 18:51:23 -0800

    gmic: remove build patch

--- Formula/gmic.rb
+++ Formula/gmic.rb
@@ -32,15 +32,6 @@ class Gmic < Formula
   uses_from_macos "curl"
   uses_from_macos "zlib"
 
-  # Use .dylibs instead of .so on macOS
-  patch do
-    on_macos do
-      url "https://raw.githubusercontent.com/macports/macports-ports/a859c5929c929548f5156f5cab13a2f341982e72/science/gmic/files/patch-src-Makefile.diff"
-      sha256 "5b4914a05135f6c137bb5980d0c3bf8d94405f03d4e12b6ee38bd0e0e004a358"
-      directory "src"
-    end
-  end
-
   def install
     # The Makefile is not safe to run in parallel.
     # Issue ref: https://github.com/dtschump/gmic/issues/406
