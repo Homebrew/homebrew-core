db0b9b411ddeceac5fe10d57b8713c6d91dcbeff
Sean Molenaar <SMillerDev@users.noreply.github.com>
2023-01-18 09:50:27 +0100

    erlang: use std_cmake_args

--- Formula/erlang.rb
+++ Formula/erlang.rb
@@ -53,9 +53,6 @@ class Erlang < Formula
     system "./otp_build", "autoconf" unless File.exist? "configure"
 
     args = %W[
-      --disable-debug
-      --disable-silent-rules
-      --prefix=#{prefix}
       --enable-dynamic-ssl-lib
       --enable-hipe
       --enable-shared-zlib
@@ -73,7 +70,7 @@ class Erlang < Formula
       args << "--with-dynamic-trace=dtrace" if MacOS::CLT.installed?
     end
 
-    system "./configure", *args
+    system "./configure", *std_configure_args, *args
     system "make"
     system "make", "install"
 
