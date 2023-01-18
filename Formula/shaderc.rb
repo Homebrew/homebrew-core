3b68975dd503beeeb85306909acc92de31f43e59
Rui Chen <rui@chenrui.dev>
2023-01-18 17:26:56 -0500

    shaderc 2023.2

--- Formula/shaderc.rb
+++ Formula/shaderc.rb
@@ -4,8 +4,8 @@ class Shaderc < Formula
   license "Apache-2.0"
 
   stable do
-    url "https://github.com/google/shaderc/archive/refs/tags/v2023.1.tar.gz"
-    sha256 "8041c6874a085a0f357d7918855f9e39bbeff9313cbeacab28505aa233fc0da2"
+    url "https://github.com/google/shaderc/archive/refs/tags/v2023.2.tar.gz"
+    sha256 "06c4e2fdd63d62b73450d7011b72e7720b416182fb883fb0aac0afe6db2df3f6"
 
     resource "glslang" do
       # https://github.com/google/shaderc/blob/known-good/known_good.json
