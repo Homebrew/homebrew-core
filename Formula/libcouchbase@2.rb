a59592012c8a857580e2a84bce910319ecbcc5b3
Rui Chen <rui@chenrui.dev>
2023-01-21 10:21:41 -0500

    libcouchbase@2: update `deprecate` wording
    Co-authored-by: Sean Molenaar <SMillerDev@users.noreply.github.com>

--- Formula/libcouchbase@2.rb
+++ Formula/libcouchbase@2.rb
@@ -19,7 +19,7 @@ class LibcouchbaseAT2 < Formula
 
   keg_only :versioned_formula
 
-  deprecate! date: "2023-01-20", because: :versioned_formula
+  deprecate! date: "2023-01-20", because: : deprecated_upstream
 
   depends_on "cmake" => :build
   depends_on "libev"
