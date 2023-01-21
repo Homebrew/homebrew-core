0496c62e8c944d3edc1a2e4f893c66e419b7fe86
Rui Chen <rui@chenrui.dev>
2023-01-20 20:04:39 -0500

    copa: update license and build

--- Formula/copa.rb
+++ Formula/copa.rb
@@ -3,13 +3,13 @@ class Copa < Formula
   homepage "https://github.com/project-copacetic/copacetic"
   url "https://github.com/project-copacetic/copacetic/archive/refs/tags/v0.1.0.tar.gz"
   sha256 "4261caa16e8ba25d5ead96a4fb3c033df4e4ec68305f5ce58714fe336e83f361"
-  license "Apache-2.0"
+  license "MIT"
   head "https://github.com/project-copacetic/copacetic.git", branch: "main"
 
   depends_on "go" => :build
 
   def install
-    system "go", "build", *std_go_args, "-o", bin/"copa", "main.go"
+    system "go", "build", *std_go_args
   end
 
   test do
