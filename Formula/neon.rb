26848446140d8e4d640b8228c1552de78a4702c6
Patrick Linnane <patrick@linnane.io>
2023-01-21 10:35:01 -0800

    neon: remove build patch

--- Formula/neon.rb
+++ Formula/neon.rb
@@ -28,12 +28,6 @@ class Neon < Formula
 
   uses_from_macos "libxml2"
 
-  # Configure switch unconditionally adds the -no-cpp-precomp switch
-  # to CPPFLAGS, which is an obsolete Apple-only switch that breaks
-  # builds under non-Apple compilers and which may or may not do anything
-  # anymore.
-  patch :DATA
-
   def install
     # Work around configure issues with Xcode 12
     ENV.append "CFLAGS", "-Wno-implicit-function-declaration"
@@ -49,17 +43,3 @@ class Neon < Formula
     system "make", "install"
   end
 end
-
-__END__
-diff --git a/configure b/configure
-index d7702d2..5c3b5a3 100755
---- a/configure
-+++ b/configure
-@@ -4224,7 +4224,6 @@ fi
- $as_echo "$ne_cv_os_uname" >&6; }
-
- if test "$ne_cv_os_uname" = "Darwin"; then
--  CPPFLAGS="$CPPFLAGS -no-cpp-precomp"
-   LDFLAGS="$LDFLAGS -flat_namespace"
-   # poll has various issues in various Darwin releases
-   if test x${ac_cv_func_poll+set} != xset; then
