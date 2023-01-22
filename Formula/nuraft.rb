35106b7ff1a621804a686d3f1e02040d67461c49
Rui Chen <rui@chenrui.dev>
2023-01-22 12:34:38 -0500

    nuraft 2.1.0

--- Formula/nuraft.rb
+++ Formula/nuraft.rb
@@ -1,10 +1,9 @@
 class Nuraft < Formula
   desc "C++ implementation of Raft core logic as a replication library"
   homepage "https://github.com/eBay/NuRaft"
-  url "https://github.com/eBay/NuRaft/archive/v1.3.0.tar.gz"
-  sha256 "e09b53553678ddf8fa4823c461fe303e7631d30da0d45f63f90e7652b7e440bb"
+  url "https://github.com/eBay/NuRaft/archive/v2.1.0.tar.gz"
+  sha256 "42d19682149cf24ae12de0dabf70d7ad7e71e49fbfa61d565e9b46e2b3cd517f"
   license "Apache-2.0"
-  revision 1
 
   bottle do
     sha256 cellar: :any,                 arm64_ventura:  "28d93fd60657c2a7eeb21be7fa86c7e129d9e036b2cdac6391b71e156b76b28e"
