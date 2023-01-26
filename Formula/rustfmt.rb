874716cc2864d59dd7e1b30157350d95e1c72dd1
Rui Chen <rui@chenrui.dev>
2023-01-26 10:43:02 -0500

    rustfmt: update nightly build version

--- Formula/rustfmt.rb
+++ Formula/rustfmt.rb
@@ -25,8 +25,8 @@ class Rustfmt < Formula
     system "#{Formula["rustup-init"].bin}/rustup-init", "-qy", "--no-modify-path"
     ENV.prepend_path "PATH", HOMEBREW_CACHE/"cargo_cache/bin"
     # we are using nightly because rustfmt requires nightly in order to build from source
-    # pinning to nightly-2021-11-08 to avoid inconstency
-    nightly_version = "nightly-2021-11-08"
+    # pinning to nightly-2023-01-24 to avoid inconstency
+    nightly_version = "nightly-2023-01-24"
     components = %w[rust-src rustc-dev llvm-tools-preview]
     system "rustup", "toolchain", "install", nightly_version
     system "rustup", "component", "add", *components, "--toolchain", nightly_version
