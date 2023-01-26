fb71d11b7f455a3a11ec107538ef0e61d14238ff
Ruoyu Zhong <zhongruoyu@outlook.com>
2023-01-26 17:21:04 +0800

    docker-slim: rename binaries; use `std_go_args`
    See: https://github.com/slimtoolkit/slim/commit/4ab562bf34b3b13ab7ac862f6c1c469584a0b44e

--- Formula/docker-slim.rb
+++ Formula/docker-slim.rb
@@ -23,24 +23,26 @@ class DockerSlim < Formula
 
   depends_on "go" => :build
 
-  skip_clean "bin/docker-slim-sensor"
+  skip_clean "bin/slim-sensor"
 
   def install
     ENV["CGO_ENABLED"] = "0"
     ldflags = "-s -w -X github.com/docker-slim/docker-slim/pkg/version.appVersionTag=#{version}"
-    system "go", "build", "-trimpath", "-ldflags=#{ldflags}", "-o",
-           bin/"docker-slim", "./cmd/docker-slim"
+    system "go", "build",
+                 *std_go_args(output: bin/"slim", ldflags: ldflags),
+                 "./cmd/slim"
 
-    # docker-slim-sensor is a Linux binary that is used within Docker
+    # slim-sensor is a Linux binary that is used within Docker
     # containers rather than directly on the macOS host.
     ENV["GOOS"] = "linux"
-    system "go", "build", "-trimpath", "-ldflags=#{ldflags}", "-o",
-           bin/"docker-slim-sensor", "./cmd/docker-slim-sensor"
-    (bin/"docker-slim-sensor").chmod 0555
+    system "go", "build",
+                 *std_go_args(output: bin/"slim-sensor", ldflags: ldflags),
+                 "./cmd/slim-sensor"
+    (bin/"slim-sensor").chmod 0555
   end
 
   test do
-    assert_match version.to_s, shell_output("#{bin}/docker-slim --version")
-    system "test", "-x", bin/"docker-slim-sensor"
+    assert_match version.to_s, shell_output("#{bin}/slim --version")
+    system "test", "-x", bin/"slim-sensor"
   end
 end
