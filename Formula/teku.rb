f0955ab0c389bd71935b13bc77a0e6489e283606
Ruoyu Zhong <zhongruoyu@outlook.com>
2023-01-27 21:35:50 +0800

    teku 23.1.0

--- Formula/teku.rb
+++ Formula/teku.rb
@@ -2,8 +2,8 @@ class Teku < Formula
   desc "Java Implementation of the Ethereum 2.0 Beacon Chain"
   homepage "https://docs.teku.consensys.net/"
   url "https://github.com/ConsenSys/teku.git",
-      tag:      "22.12.0",
-      revision: "1a249bc53c52f8d7305224d80d55915aa949ce52"
+      tag:      "23.1.0",
+      revision: "8f1ef50aa26e8280855d2cb658e9970bc5e7c489"
   license "Apache-2.0"
 
   bottle do
