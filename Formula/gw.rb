665d57b7f7e1ff310fbf5b689a464a36e9ed6516
kcleal <clealk@cardiff.ac.uk>
2023-01-31 12:31:34 +0000

    Adding gw formulae

---
+++ Formula/gw.rb
@@ -0,0 +1,27 @@
+class Gw < Formula
+  desc "Browser for genomic sequencing data and variants"
+  homepage "https://github.com/kcleal/gw"
+  url "https://github.com/kcleal/gw/archive/refs/tags/v0.5.0.tar.gz"
+  sha256 "8b6b85b337602052b4ca2a3259152f1be56ffab97c6b6a0150046d7a1bbf4333"
+  license "MIT"
+  depends_on "fontconfig"
+  depends_on "glfw"
+  depends_on "htslib"
+  depends_on "icu4c"
+  depends_on "wget"
+  depends_on "zlib"
+  depends_on "ninja"
+
+  def install
+    system "pwd"
+    system "ls", "-la"
+    system "bash", "build_skia.sh"
+    system "make"
+    bin.install "gw"
+    bin.install ".gw.ini"
+  end
+
+  test do
+    system "gw", "--version"
+  end
+end
