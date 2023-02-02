129c7f8f70bd96956ee8bfc9e66dc042d94a602c
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2023-02-02 15:53:22 +0000

    rtx 1.5.1 (new formula)
    https://github.com/jdxcode/rtx

---
+++ Formula/rtx.rb
@@ -0,0 +1,18 @@
+class Rtx < Formula
+  desc "Polyglot runtime manager (asdf rust clone)"
+  homepage "https://github.com/jdxcode/rtx"
+  url "https://github.com/jdxcode/rtx/archive/v1.5.1.tar.gz"
+  sha256 "a4bd5ba88034837da333af3e14c4d4a14730272f2b2723c5effd0dae7eb66094"
+  license "MIT"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    system "#{bin}/rtx", "install", "nodejs@18.13.0"
+    assert_match "v18.13.0", shell_output("#{bin}/rtx exec nodejs@18.13.0 -- node -v")
+  end
+end
