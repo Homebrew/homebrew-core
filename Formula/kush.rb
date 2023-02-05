93bf56d63a165453fee8adb1cb50bc155571c868
anigkus <anigkus@gmail.com>
2023-02-05 15:05:13 +0800

    kush 0.0.9

--- Formula/kush.rb
+++ Formula/kush.rb
@@ -2,14 +2,16 @@ class Kush < Formula
   desc "Cross-platform command-line SSH remote address connection tool"
   homepage "https://github.com/anigkus/kush"
   url "https://github.com/anigkus/kush/archive/refs/tags/v0.0.9.tar.gz"
-  sha256 "c716330383a1f4710078928951d29e7f0f05bf143162dcabe37b35db453a691d"
+  sha256 "865402e6672112900b686f5f2647485f576c7cda71089c90f4e453dca0c8b059"
   license "Apache-2.0"
   head "https://github.com/anigkus/kush.git"
   depends_on "go" => :build
   def install
-    system "go", "build", "-ldflags=-s -w", "-o", "build/darwin/kush"
+    system "go", "build", *std_go_args
+    generate_completions_from_executable(bin/"kush", "completion")
   end
   test do
-    system "false"
+    output = shell_output("#{bin}/kush version")
+    assert_match "Version:    #{version}", output
   end
 end
