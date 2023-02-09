518889696be9f4b7dd71cc19386f57ac211934b8
Joe Bartlett <joe@redopop.com>
2023-02-08 20:31:25 -0600

    gst-plugins-base: fix head
    Head for gst-plugins-base has moved to the GStreamer monorepo.

--- Formula/gst-plugins-base.rb
+++ Formula/gst-plugins-base.rb
@@ -4,7 +4,7 @@ class GstPluginsBase < Formula
   url "https://gstreamer.freedesktop.org/src/gst-plugins-base/gst-plugins-base-1.22.0.tar.xz"
   sha256 "f53672294f3985d56355c8b1df8f6b49c8c8721106563e19f53be3507ff2229d"
   license "LGPL-2.0-or-later"
-  head "https://gitlab.freedesktop.org/gstreamer/gst-plugins-base.git", branch: "master"
+  head "https://gitlab.freedesktop.org/gstreamer/gstreamer.git", branch: "main"
 
   livecheck do
     url "https://gstreamer.freedesktop.org/src/gst-plugins-base/"
