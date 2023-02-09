fca20dd4368f4d3ac31232c12d79aaacf628f0ac
Joe Bartlett <joe@redopop.com>
2023-02-08 21:24:32 -0600

    gst-plugins-base: move revision ahead of head

--- Formula/gst-plugins-base.rb
+++ Formula/gst-plugins-base.rb
@@ -4,8 +4,8 @@ class GstPluginsBase < Formula
   url "https://gstreamer.freedesktop.org/src/gst-plugins-base/gst-plugins-base-1.22.0.tar.xz"
   sha256 "f53672294f3985d56355c8b1df8f6b49c8c8721106563e19f53be3507ff2229d"
   license "LGPL-2.0-or-later"
-  head "https://gitlab.freedesktop.org/gstreamer/gstreamer.git", branch: "main"
   revision 1
+  head "https://gitlab.freedesktop.org/gstreamer/gstreamer.git", branch: "main"
 
   livecheck do
     url "https://gstreamer.freedesktop.org/src/gst-plugins-base/"
