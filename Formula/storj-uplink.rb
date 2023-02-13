8ba91a1ebe5013b19dae96a31e83e810e49f95c5
Alishan Ladhani <8869764+aladh@users.noreply.github.com>
2023-02-13 17:09:36 -0500

    WIP

--- Formula/storj-uplink.rb
+++ Formula/storj-uplink.rb
@@ -1,7 +1,7 @@
 class StorjUplink < Formula
   desc "Uplink CLI for the Storj network"
   homepage "https://storj.io"
-  url "https://github.com/storj/storj/archive/refs/tags/v1.71.2.tar.gz"
+  url "https://github.com/storj/storj/archive/refs/tags/v1.72.4.tar.gz"
   sha256 "74e70f180c258f58c9e048d24e4e6a2ac4d71240d342d942ee6255cfe2827418"
   license "AGPL-3.0-only"
 
