3ddecd9a8df1d6c6f06bbcfcac10a513797245c3
Sean Molenaar <SMillerDev@users.noreply.github.com>
2023-02-15 09:25:19 +0100

    papilo: reset version scheme

--- Formula/papilo.rb
+++ Formula/papilo.rb
@@ -3,6 +3,7 @@ class Papilo < Formula
   homepage "https://www.scipopt.org"
   url "https://github.com/scipopt/papilo/archive/refs/tags/v2.1.2.tar.gz"
   sha256 "7e3d829c957767028db50b5c5085601449b00671e7efc2d5eb0701a6903d102f"
+  version_scheme 1
   license all_of: ["LGPL-3.0-only", "GPL-3.0-only"]
 
   bottle do
