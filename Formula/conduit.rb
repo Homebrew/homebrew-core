5d91a27a642d05ed0a8a204a4a2c10c5d77903c6
maha-hajja <maha@meroxa.io>
2023-02-16 00:35:47 +0000

    fix sha256

--- Formula/conduit.rb
+++ Formula/conduit.rb
@@ -2,7 +2,7 @@ class Conduit < Formula
   desc "Streams data between data stores. Kafka Connect replacement. No JVM required"
   homepage "https://conduit.io/"
   url "https://github.com/ConduitIO/conduit/archive/refs/tags/v0.5.2.tar.gz"
-  sha256 "ba57506c17a99356c443d3c4459977c825dcce1b039965ba7699140e11d95afc"
+  sha256 "be774e6d14b763d4caa18136aea3017823730cefe90aa31f4d4eeb5d3e35713c"
   license "Apache-2.0"
   head "https://github.com/ConduitIO/conduit.git", branch: "main"
 
