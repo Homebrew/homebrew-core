38f3b4ca1b966aa61c00711325ed534d523b5a15
Balli Asghar <ballihacker@outlook.com>
2023-02-16 12:37:19 +0000

    add missing license
    added missing license to formula file.

--- Formula/killport.rb
+++ Formula/killport.rb
@@ -3,6 +3,7 @@ class Killport < Formula
     homepage "https://github.com/BalliAsghar/killport"
     url "https://github.com/BalliAsghar/killport/archive/refs/tags/1.0.1.tar.gz"
     sha256 "a04c9d71c1def96d1896096fbe2e3d1d5905ecfdef4b10136330f1f824177f7f"
+    license "MIT"
 
     def install
       bin.install "killport.sh" => "killport"
