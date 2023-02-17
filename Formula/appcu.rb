5c90ec996a0d9a66dec490826f182f96d6fa2649
chenluffy <chengluffy@hotmail.com>
2023-02-17 20:49:45 +0800

    更新 appcu.rb

--- Formula/appcu.rb
+++ Formula/appcu.rb
@@ -1,5 +1,5 @@
 class Appcu < Formula
-  desc "一款用于检查更新 macOS 上应用的 CLI 工具 | a CLI tool for checking update applications on macOS"
+  desc "一款用于对 macOS 上应用进行检查更新的 CLI 工具"
   homepage "https://github.com/ChengLuffy/application_check_update"
   url "https://github.com/ChengLuffy/application_check_update/releases/download/v0.1.0/appcu.tar.gz"
   sha256 "752ad84867b81c18d9972aa31cc55420b573c5583b90899871f18b60f5da1f80"
