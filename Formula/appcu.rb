ab964c6b638e0c15f48586bbcb4dc7d98f8c7610
chenluffy <chengluffy@hotmail.com>
2023-02-17 21:33:50 +0800

    更新 appcu.rb

--- Formula/appcu.rb
+++ Formula/appcu.rb
@@ -1,8 +1,8 @@
 class Appcu < Formula
   desc "一款用于对 macOS 上应用进行检查更新的 CLI 工具"
   homepage "https://github.com/ChengLuffy/application_check_update"
-  url "https://github.com/ChengLuffy/application_check_update/releases/download/v0.1.0/appcu.tar.gz"
-  sha256 "752ad84867b81c18d9972aa31cc55420b573c5583b90899871f18b60f5da1f80"
+  url "https://github.com/ChengLuffy/application_check_update/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "8b6eeccd3de87c4d69d5183293d4ca8e82936f7856a0d9b91fccada42db0c953"
   license "MIT"
 
   depends_on "rust" => :build
