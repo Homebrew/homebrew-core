9884972ef36b1f1ffd81126fd54d77d4aa36b506
Patrick Linnane <patrick@linnane.io>
2023-02-17 19:14:13 -0800

    libnotify: don't build docs

--- Formula/libnotify.rb
+++ Formula/libnotify.rb
@@ -36,7 +36,7 @@ class Libnotify < Formula
     ENV["XML_CATALOG_FILES"] = "#{etc}/xml/catalog"
 
     mkdir "build" do
-      system "meson", *std_meson_args, "-Dtests=false", ".."
+      system "meson", *std_meson_args, "-Dgtk_doc=false", "-Dman=false", "-Dtests=false", ".."
       system "ninja"
       system "ninja", "install"
     end
