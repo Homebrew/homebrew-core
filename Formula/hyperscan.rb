82b9699343635e561ab3c8c6215416395b662a66
Patrick Linnane <patrick@linnane.io>
2023-02-21 09:54:34 -0800

    hyperscan: remove build patch

--- Formula/hyperscan.rb
+++ Formula/hyperscan.rb
@@ -23,13 +23,6 @@ class Hyperscan < Formula
   depends_on arch: :x86_64
   depends_on "pcre"
 
-  # fixes glibc 2.34 issue https://github.com/intel/hyperscan/issues/359
-  # remove in version > 5.4.0
-  patch do
-    url "https://github.com/intel/hyperscan/commit/564ed6f65a1058e4e0adab69bdd17ba9138c8a0c.patch?full_index=1"
-    sha256 "21a22ac92c8f61c3b06f72919d356d594fc89d090eb1f238ce11e89d55e22bfb"
-  end
-
   def install
     cmake_args = std_cmake_args + ["-DBUILD_STATIC_AND_SHARED=ON"]
 
