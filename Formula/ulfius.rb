6e252951b203b9079e46f3fd02000c62e0c92a75
Rui Chen <rui@chenrui.dev>
2023-02-22 16:22:55 +0100

    ulfius 2.7.13 (new formula)

--- Formula/ulfius.rb
+++ Formula/ulfius.rb
@@ -1,24 +1,28 @@
 class Ulfius < Formula
   desc "HTTP Framework for REST Applications in C"
   homepage "https://github.com/babelouest/ulfius/"
-  url "https://github.com/babelouest/ulfius/archive/refs/tags/v2.7.12.tar.gz"
-  sha256 "20a359eeb30f1807ef68e4b6329fca5a06167eef1f0faaae3d2e0e8e4406bd70"
+  url "https://github.com/babelouest/ulfius/archive/refs/tags/v2.7.13.tar.gz"
+  sha256 "b1679bc0885acedff66abad84b51f492497ab1114d6911d07d2cf7eb77ccadce"
   license "LGPL-2.1-only"
 
   depends_on "cmake" => :build
+  depends_on "pkg-config" => :test
   depends_on "gnutls"
   depends_on "jansson"
   depends_on "libmicrohttpd"
+
   uses_from_macos "curl"
+  uses_from_macos "zlib"
 
   def install
-    args = %w[
+    args = %W[
       -DWITH_JOURNALD=OFF
       -DWITH_WEBSOCKET=on
       -DWITH_GNUTLS=on
       -DWITH_CURL=on
+      -DCMAKE_INSTALL_RPATH=#{rpath}
     ]
-    system "cmake", "-S", ".", "-B", "ulfius-build", *std_cmake_args, *args
+    system "cmake", "-S", ".", "-B", "ulfius-build", *args, *std_cmake_args
     system "cmake", "--build", "ulfius-build"
     system "cmake", "--install", "ulfius-build"
   end
@@ -32,7 +36,7 @@ class Ulfius < Formula
         return 0;
       }
     EOS
-    system ENV.cc, "test.c", "-L#{lib}", "-lulfius", "-o", "test"
+    system ENV.cc, "test.c", "-I#{include}", "-L#{lib}", "-lulfius", "-o", "test"
     system "./test"
   end
 end
