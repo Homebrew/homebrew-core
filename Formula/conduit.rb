b6e93ea487e2eb7d7709adf72f63a925e5bbb8b4
Maha Hajja <82542081+maha-hajja@users.noreply.github.com>
2023-02-22 16:12:14 +0200

    Update conduit.rb

--- Formula/conduit.rb
+++ Formula/conduit.rb
@@ -24,7 +24,7 @@ class Conduit < Formula
       $stdout.reopen(file)
       pid = fork do
         # Run conduit with random free ports for gRPC and HTTP servers
-        exec bin/"conduit", "--grpc.address", ":0", 
+        exec bin/"conduit", "--grpc.address", ":0",
                             "--http.address", ":0"
       end
       sleep(5)
