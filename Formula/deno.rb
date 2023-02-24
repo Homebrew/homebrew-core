c44dad0e55c127a189024b1998b813dee0df0dae
Rui Chen <rui@chenrui.dev>
2023-02-24 09:52:08 +0100

    deno: update resources

--- Formula/deno.rb
+++ Formula/deno.rb
@@ -38,13 +38,13 @@ class Deno < Formula
   # We use the crate as GitHub tarball lacks submodules and this allows us to avoid git overhead.
   # TODO: Remove this and `v8` resource when https://github.com/denoland/rusty_v8/pull/1063 is released
   resource "rusty-v8" do
-    url "https://static.crates.io/crates/v8/v8-0.60.1.crate"
-    sha256 "07fd5b3ed559897ff02c0f62bc0a5f300bfe79bb4c77a50031b8df771701c628"
+    url "https://static.crates.io/crates/v8/v8-0.63.0.crate"
+    sha256 "547e58962ac268fe0b1fbfb653ed341a08e3953994f7f7c978e46ec30afdf8f0"
   end
 
   resource "v8" do
-    url "https://github.com/denoland/v8/archive/05fb6e97fc6c5fdd2e79e4f9a51c5bf0ca0ef991.tar.gz"
-    sha256 "a083d815b21b0a2a59d1f133fd897c1f803f1418cfdffd411a89583bf37f22a8"
+    url "https://github.com/denoland/v8/archive/7ee97e7ef6d832467dfda629c1ef65b2a8a285fa.tar.gz"
+    sha256 "a060ecebf7525eb10f845f027f08028e9444d5be121a18464dcf71896885e1e0"
   end
 
   # To find the version of gn used:
@@ -54,7 +54,7 @@ class Deno < Formula
   # 4. Find full gn commit hash: https://gn.googlesource.com/gn.git/+/#{gn_commit}
   resource "gn" do
     url "https://gn.googlesource.com/gn.git",
-        revision: "bf4e17dc67b2a2007475415e3f9e1d1cf32f6e35"
+        revision: "70d6c60823c0233a0f35eccc25b2b640d2980bdc"
   end
 
   def install
