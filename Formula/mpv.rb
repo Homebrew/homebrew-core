2d8fa6f5932e5c00ae00f4e5c7ed3d9cf248376e
Kiracus <abc.kiracus@gmail.com>
2023-02-26 15:52:15 -0800

    mpv: build with meson

--- Formula/mpv.rb
+++ Formula/mpv.rb
@@ -18,8 +18,8 @@ class Mpv < Formula
   end
 
   depends_on "docutils" => :build
+  depends_on "meson" => :build
   depends_on "pkg-config" => :build
-  depends_on "python@3.10" => :build
   depends_on xcode: :build
   depends_on "ffmpeg"
   depends_on "jpeg-turbo"
@@ -36,45 +36,33 @@ class Mpv < Formula
     depends_on "alsa-lib"
   end
 
-  fails_with gcc: "5" # ffmpeg is compiled with GCC
-
   def install
     # LANG is unset by default on macOS and causes issues when calling getlocale
     # or getdefaultlocale in docutils. Force the default c/posix locale since
     # that's good enough for building the manpage.
     ENV["LC_ALL"] = "C"
 
-    # Avoid unreliable macOS SDK version detection
-    # See https://github.com/mpv-player/mpv/pull/8939
-    if OS.mac?
-      sdk = (MacOS.version == :big_sur) ? MacOS::Xcode.sdk : MacOS.sdk
-      ENV["MACOS_SDK"] = sdk.path
-      ENV["MACOS_SDK_VERSION"] = "#{sdk.version}.0"
-    end
+    # force meson find ninja from homebrew
+    ENV["NINJA"] = Formula["ninja"].opt_bin/"ninja"
 
     # libarchive is keg-only
     ENV.prepend_path "PKG_CONFIG_PATH", Formula["libarchive"].opt_lib/"pkgconfig"
 
     args = %W[
-      --prefix=#{prefix}
-      --enable-html-build
-      --enable-javascript
-      --enable-libmpv-shared
-      --enable-lua
-      --enable-libarchive
-      --enable-uchardet
-      --confdir=#{etc}/mpv
+      -Dhtml-build=enabled
+      -Djavascript=enabled
+      -Dlibmpv=true
+      -Dlua=luajit
+      -Dlibarchive=enabled
+      -Duchardet=enabled
+      --sysconfdir=#{pkgetc}
       --datadir=#{pkgshare}
       --mandir=#{man}
-      --docdir=#{doc}
-      --zshdir=#{zsh_completion}
-      --lua=luajit
     ]
 
-    python3 = "python3.10"
-    system python3, "bootstrap.py"
-    system python3, "waf", "configure", *args
-    system python3, "waf", "install"
+    system "meson", "setup", "build", *args, *std_meson_args
+    system "meson", "compile", "-C", "build", "--verbose"
+    system "meson", "install", "-C", "build"
   end
 
   test do
