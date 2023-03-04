b2101ecf5056ffea2695e8ace7b385a51f7ff5ac
Brad Wood <brad@bradwood.com>
2023-03-04 15:32:20 -0600

    commandbox 5.8.0

--- Formula/commandbox.rb
+++ Formula/commandbox.rb
@@ -1,8 +1,8 @@
 class Commandbox < Formula
   desc "CFML embedded server, package manager, and app scaffolding tools"
   homepage "https://www.ortussolutions.com/products/commandbox"
-  url "https://downloads.ortussolutions.com/ortussolutions/commandbox/5.7.0/commandbox-bin-5.7.0.zip"
-  sha256 "a45c30d12dd4805c48610b9948e48ed167a5f35cd4c8ce4a5b450b0e07514baf"
+  url "https://downloads.ortussolutions.com/ortussolutions/commandbox/5.8.0/commandbox-bin-5.8.0.zip"
+  sha256 "c790ae6de64992782dd80659c901c2b5b49cfeb50ed4100449a3fe083e1838ba"
   license "LGPL-3.0-or-later"
 
   livecheck do
@@ -18,8 +18,8 @@ class Commandbox < Formula
   depends_on "openjdk@11"
 
   resource "apidocs" do
-    url "https://downloads.ortussolutions.com/ortussolutions/commandbox/5.7.0/commandbox-apidocs-5.7.0.zip"
-    sha256 "960c8363992e7bebf8d48c20a6680d9877d345eb552b908b8406007cf2c4d9a0"
+    url "https://downloads.ortussolutions.com/ortussolutions/commandbox/5.8.0/commandbox-apidocs-5.8.0.zip"
+    sha256 "fd52e249e0d1666b6c9e0705041e194084a5eae088714ec036d0c18a6d7fb977"
   end
 
   def install
