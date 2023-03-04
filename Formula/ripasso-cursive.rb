78ae10cbcbe553ad367b1b2aef49ae1fac0cf06f
Alexander Kjäll <alexander.kjall@gmail.com>
2023-03-04 19:48:16 +0100

    Rewrote the screenshot capture code from using tmux to using a pty and doing non-blocking reads on it with sysread

--- Formula/ripasso-cursive.rb
+++ Formula/ripasso-cursive.rb
@@ -1,15 +1,14 @@
 class RipassoCursive < Formula
   desc "Tui password manager compatible with 'pass', written in rust"
   homepage "https://github.com/cortex/ripasso"
-  url "https://github.com/cortex/ripasso/archive/refs/tags/release-0.6.3.tar.gz"
-  sha256 "2f43e48c3e1064bef47c89269e4797f216b7600b850f3857cfce7e9716ef8f7d"
+  url "https://github.com/cortex/ripasso/archive/refs/tags/release-0.6.4.tar.gz"
+  sha256 "c3444fbc30d3536d1f8aa7b6e6cbefff36e8bfaa5e5ff5ef7faf9382a27f142b"
   license "GPL-3.0-only"
 
   depends_on "libxcb" => :build
   depends_on "llvm" => :build
   depends_on "pkg-config" => :build
   depends_on "rust" => :build
-  depends_on "tmux" => :test
   depends_on "gpgme"
   depends_on "libgpg-error"
   depends_on "nettle"
@@ -26,12 +25,25 @@ class RipassoCursive < Formula
   end
 
   test do
-    system "tmux", "-u", "new-session", "-d", "-x", "80", "-y", "50", "-s", "foo"
-    system "tmux", "send", "-t", "foo", "#{bin}/ripasso-cursive", "ENTER"
-    system "sleep", "1"
-    output = shell_output("tmux capture-pane -pS")
-    system "tmux", "kill-session", "-t", "foo"
+    require "pty"
+    require "io/console"
 
-    assert_match "─┤ Ripasso ├─", output
+    begin
+      r, _w, pid = PTY.spawn "#{bin}/ripasso-cursive"
+      r.winsize = [80, 43]
+      sleep 1
+
+      out = ""
+      iterations = 0
+      while (out.exclude? "Ripasso") && (iterations < 1000)
+        out += r.sysread(400)
+        iterations += 1
+      end
+      assert iterations < 999
+    rescue Errno::EIO
+      # GNU/Linux raises EIO when read is done on closed pty
+    end
+  ensure
+    Process.kill("TERM", pid)
   end
 end
