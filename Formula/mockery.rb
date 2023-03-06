8aa70ff1b581b5ed08d9dca06aa5376e49d3024b
Ruoyu Zhong <zhongruoyu@outlook.com>
2023-03-06 15:34:55 +0800

    mockery: fix test

--- Formula/mockery.rb
+++ Formula/mockery.rb
@@ -29,7 +29,7 @@ class Mockery < Formula
     output = shell_output("#{bin}/mockery --keeptree 2>&1", 1)
     assert_match "Starting mockery dry-run=false version=v#{version}", output
 
-    output = shell_output("#{bin}/mockery --all --dry-run 2>&1", 1)
+    output = shell_output("#{bin}/mockery --all --dry-run 2>&1")
     assert_match "INF Starting mockery dry-run=true version=v#{version}", output
   end
 end
