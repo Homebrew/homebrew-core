7e7bab9f05ba4cb8d9036c57d874f8a6640d4a12
Hào Nguyễn Anh <hao.nguyen@rapiddweller.com>
2023-03-08 02:22:50 +0700

    refactor: adjust code follow feedback

--- Formula/benerator.rb
+++ Formula/benerator.rb
@@ -5,37 +5,112 @@ class Benerator < Formula
   sha256 "194feb051ae18cfcd407b8e1668ce9c60561394bc454f9fc9747c274166843bc"
 
   license "Apache-2.0"
-  depends_on "openjdk@11"
+  depends_on "openjdk"
+
   def install
     # Remove unnecessary files
     rm_f Dir["bin/*.bat", "bin/pom.xml"]
 
-    # Installs only the 'bin' and 'lib' directories from the tarball
-    # Creates the symlinks to 'bin' scripts in /usr/local/bin
-    # Creates the symlink to the installation path in /usr/local/opt
+    # Installs only the "bin" and "lib" directories from the tarball
     libexec.install Dir["bin", "lib"]
-    bin.install_symlink Dir["#{libexec}/bin/*"]
-    # Remove non-executable files
+    # Generate a script that sets the necessary environment variables
+    env_script = libexec / "benerator-env"
+    env_script.write_env_script(
+      libexec / "bin/benerator",
+      JAVA_HOME:      Formula["openjdk"].opt_prefix,
+      BENERATOR_HOME: libexec,
+    )
+
+    # Install the wrapper script
+    bin.install libexec / "benerator-env" => "benerator"
     rm_f "#{bin}/benerator_common"
     rm_f "#{bin}/log4j2.xml"
   end
 
-  # opt_prefix is set to the symlink /usr/local/opt/benerator
   def caveats
     <<~EOS
       To use the benerator commands, please set the following environment variables:
 
       BENERATOR_HOME="#{libexec}"
-      JAVA_HOME="$(/usr/libexec/java_home -v 11)"
+      JAVA_HOME="$(/usr/libexec/java_home)"
 
-      For more information, see:
-      https://github.com/rapiddweller/rapiddweller-benerator-ce
     EOS
   end
 
   test do
-    ENV["JAVA_HOME"] = Formula["openjdk@11"].opt_prefix
-    ENV["BENERATOR_HOME"] = libexec.to_s
-    assert_match "Benerator Community Edition 3.1.0-jdk-11", shell_output("#{libexec}/bin/benerator --version")
+    # Test if version is correct
+    ohai "Test if the correct version of benerator is installed"
+    assert_match "Benerator Community Edition 3.1.0-jdk-11",
+                 shell_output("benerator --version")
+
+    # Test if data is generated follow the corrected scheme.
+    # We feed benerator an xml and a scheme in demo/db/script/h2.multischema.sql.
+    # The XML scheme in myscript.xml have an inhouse test in <evaluate /> to check if the data is generated correctly,
+    # If no, benerator will throw an error, otherwise a success message will be printed.
+    ohai "Test if data generated follow correct scheme"
+
+    File.write(testpath / "myscript.xml", <<~XML)
+      <setup defaultDataset="US" defaultLocale="en_US">
+      <import domains="person,net,product"/>
+      <import platforms="db"/>
+
+      <comment>setting default values</comment>
+      <setting name="stage" default="development"/>
+      <setting name="database" default="h2"/>
+      <setting name="dbUrl" default="jdbc:h2:mem:benerator"/>
+      <setting name="dbDriver" default="org.h2.Driver"/>
+      <setting name="dbSchema" default="public"/>
+      <setting name="dbUser" default="sa"/>
+
+      <comment>log the settings to the console</comment>
+      <echo>{ftl:encoding:${context.defaultEncoding} default pageSize:${context.defaultPageSize}}</echo>
+      <echo>{ftl:JDBC URL: ${dbUrl}}</echo>
+
+      <comment>define a database that will be referred by the id 'db' subsequently</comment>
+      <database id="db"
+      url="{dbUrl}"
+      driver="{dbDriver}"
+      schema="{dbSchema}"
+      user="{dbUser}"
+      />
+
+      <execute type="sql" target="db" uri="demo/db/script/h2.multischema.sql"/>
+
+      <database id="schema1" url="{dbUrl}" driver="{dbDriver}" schema="schema1"
+      user="{dbUser}"/>
+      <database id="schema3" url="{dbUrl}" driver="{dbDriver}" schema="schema3"
+      user="{dbUser}"/>
+
+      <generate type="db_manufacturer" count="100" consumer="schema3">
+      <id name="id" generator="IncrementGenerator"/>
+      <attribute name="name" pattern="[A-Z][A-Z]{5,12}"/>
+      </generate>
+      <generate type="db_Category" count="10" consumer="schema1">
+      <id name="id" generator="IncrementGenerator"/>
+      </generate>
+      <generate type="db_product" count="100" consumer="schema1">
+      <id name="ean_code" generator="EANGenerator"/>
+      <attribute name="price" pattern="[1-9]{1,2}"/>
+      <attribute name="name" pattern="[A-Z][A-Z]{5,12}"/>
+      <attribute name="notes" pattern="[A-Z][\n][a-z][0-9]{1,256}"/>
+      <attribute name="description" pattern="[A-Z][\n][a-z][0-9]{1,256}"/>
+      <reference name="manufacturer_id" source="schema3" targetType="db_manufacturer"/>
+      </generate>
+
+      <echo>Printing all generated data</echo>
+      <iterate name="CAT_TRANS" type="db_Category" source="schema1" consumer="ConsoleExporter"/>
+      <iterate name="PROD_TRANS" type="db_product" source="schema1" consumer="ConsoleExporter"/>
+      <iterate name="MAN_TRANS" type="db_manufacturer" source="schema3" consumer="ConsoleExporter"/>
+
+      <echo>Verifying generated data</echo>
+      <evaluate assert="result == 10" target="schema1">select count(*) from "schema1"."db_Category"</evaluate>
+      <evaluate assert="result == 100" target="schema1">select count(*) from "schema1"."db_product"</evaluate>
+      <evaluate assert="result == 100" target="schema3">select count(*) from "schema3"."db_manufacturer"</evaluate>
+      <echo>No Error Occurs. Data Generated Correcly</echo>
+      </setup>
+      XML
+
+    assert_match "No Error Occurs. Data Generated Correcly",
+                 shell_output("benerator myscript.xml")
   end
 end
