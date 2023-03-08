5d7980969fee45eb8617050fc7c4b503d3e99a2c
Charles <charles@swiftconnect.io>
2023-03-08 14:19:13 -0500

    go: fix failure to build some go module dependencies (e.g., github.com/confluentinc/confluent-kafka-go/v2)

--- Formula/go.rb
+++ Formula/go.rb
@@ -55,7 +55,7 @@ class Go < Formula
 
     cd "src" do
       ENV["GOROOT_FINAL"] = libexec
-      system "./make.bash"
+      system "./make.bash", "--no-clean"
     end
 
     rm_rf "gobootstrap" # Bootstrap not required beyond compile.
