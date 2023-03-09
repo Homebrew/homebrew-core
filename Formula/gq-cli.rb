c28e670f11e90eef0c9393bbd3a3785572705f12
Myke_A <66381519+michaelaboah@users.noreply.github.com>
2023-03-09 11:14:20 -0500

    GoQuant-Cli-v0.0.1
    Version 0.0.1 of the GoQuant (gq) CLI
    
                       
    The addition of a CLI tool for developers working with GoQuant's Live api. Sensitive operations require authentication. More information can be found on https://docs.goquant.io.

---
+++ Formula/gq-cli.rb
@@ -0,0 +1,30 @@
+class GqCli < Formula
+  desc "A command-line tool for goquant.io"
+  homepage "https://goquant.io"
+  url "https://brickelldatabros1aboah.blob.core.windows.net/public/macos-x86.tar.gz"
+  license "MIT"
+
+
+  if Hardware::CPU.arm?
+    url "https://brickelldatabros1aboah.blob.core.windows.net/public/macos-arm.tar.gz"
+    sha256 "d70775df10c53b5e6b38af46c2a963bd094417c0e621db82d5adb398b634913a"
+  else
+    url "https://brickelldatabros1aboah.blob.core.windows.net/public/macos-x86.tar.gz"
+    sha256 "99a64a8b7164c66a03aa8f34e68a7acb4cf2254642345839c742afd989a5f2f1"
+  end
+  def install
+    bin.install "gq"
+  end
+
+  def post_install
+    if system("#{prefix}/install.sh")
+      ohai "Installation successful"
+    else
+      opoo "Installation failed"
+    end
+  end
+
+  test do
+    assert_match "gq", shell_output("#{bin}/gq --help")
+  end
+end
