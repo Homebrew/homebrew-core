2af207316b8c9cc8fb692ef7e5a80f6157081d8f
Hauke Strasdat <strasdat@gmail.com>
2023-03-09 13:17:01 -0800

    style: update homepage URL, remove  as prompted by audit

--- Formula/sophus.rb
+++ Formula/sophus.rb
@@ -1,10 +1,9 @@
 class Sophus < Formula
   desc "C++ implementation of Lie Groups using Eigen"
-  homepage "https://strasdat.github.io/Sophus/"
+  homepage "https://strasdat.github.io/Sophus/latest/"
   url "https://github.com/strasdat/Sophus/archive/refs/tags/1.22.10.tar.gz"
   sha256 "eb1da440e6250c5efc7637a0611a5b8888875ce6ac22bf7ff6b6769bbc958082"
   license "MIT"
-  revision 1
   head "https://github.com/strasdat/Sophus.git", branch: "master"
 
   bottle do
