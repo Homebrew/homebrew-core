2904319ba019f118faecec5a337a8168b1742bce
Myke_A <66381519+michaelaboah@users.noreply.github.com>
2023-03-09 21:32:52 -0500

    Updated gq-cli.rb
    refactored according to @ZhongRuoyu
    
    Added:
    - Better testing
    - Dependencies + install commands
    - Removed any bottles

--- Formula/gq-cli.rb
+++ Formula/gq-cli.rb
@@ -1,33 +1,20 @@
 class GqCli < Formula
   desc "Command-line interface for GQ"
   homepage "https://goquant.io"
+  url "https://brickelldatabros1aboah.blob.core.windows.net/public/gq-cli-source-0.0.4.tar.gz"
+  sha256 "6e2b86cf57035bba045d7db9f1d28ae83283cec6b0ed4335083f320374b951ba"
   license "MIT"
 
-  bottle do
-    sha256 cellar: :any_skip_relocation, monterey: "b21e0a5f4238130218bfae10c6604a05263e9ef52f462af246d54a93f54821d1"
-  end
-
-  on_macos do
-    on_arm do
-      url "https://brickelldatabros1aboah.blob.core.windows.net/public/gq-arm-0.0.1.big_sur.bottle.tar.gz"
-      sha256 "d8d14a758174c9f67bb7adaf4d4a8897f3b3fa3b7f4338eb86f2c2c222e9a785"
-    end
-
-    on_intel do
-      url "https://brickelldatabros1aboah.blob.core.windows.net/public/gq-x86-0.0.1.high_sierra.bottle.tar.gz"
-      sha256 "99a64a8b7164c66a03aa8f34e68a7acb4cf2254642345839c742afd989a5f2f1"
-    end
-  end
+  depends_on "rust" => :build
 
   def install
-    bin.install "gq"
-  end
-
-  def post_install
-    system "sudo", "chmod", "+x", "#{bin}/gq"
+    system "cargo", "install", "--root=#{prefix}", "--path=.", "--debug"
   end
 
   test do
-    system "#{bin}/gq", "--version"
+    system "#{bin}/gq"
+    system "#{bin}/gq", "--help"
+    system "#{bin}/gq", "config", "-p"
+    system "#{bin}/gq", "binance", "-h"
   end
 end
