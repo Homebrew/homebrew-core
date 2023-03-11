e470d93149a2d817a5ec89fa6c39ac69cb75c79b
Yasuhiro Yamada <yamada@gr3.ie>
2023-03-11 18:42:27 +0000

    Apply suggestions from code review
    * Erase escaped characters
    * set RUSTONIG_DYNAMIC_LIBONIG/RUSTONIG_SYSTEM_LIBONIG
    * depends on pkg-config/oniguruma
    
    Co-authored-by: Ruoyu Zhong <zhongruoyu@outlook.com>
    Co-authored-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>

--- Formula/teip.rb
+++ Formula/teip.rb
@@ -1,14 +1,19 @@
 class Teip < Formula
-  desc "Masking tape to help commands \"do one thing well\""
+  desc 'Masking tape to help commands "do one thing well"'
   homepage "https://github.com/greymd/teip"
   url "https://github.com/greymd/teip/archive/v2.3.0.tar.gz"
   sha256 "4c39466613f39d27fa22ae2a6309ac732ea94fdbc8711ecd4149fc1ecdfbaedf"
   license "MIT"
   head "https://github.com/greymd/teip.git", branch: "main"
 
+  depends_on "pkg-config" => :build
   depends_on "rust" => :build
+  depends_on "oniguruma"
+  uses_from_macos "llvm" # for libclang
 
   def install
+    ENV["RUSTONIG_DYNAMIC_LIBONIG"] = "1"
+    ENV["RUSTONIG_SYSTEM_LIBONIG"] = "1"
     system "cargo", "install", "--features", "oniguruma", *std_cargo_args
     man1.install "man/teip.1"
     zsh_completion.install "completion/zsh/_teip"
