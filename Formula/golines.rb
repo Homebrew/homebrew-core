8e5629526b04feb0d4c2bb28cc564305f7da3083
Andrey Mak <mail@darkenmay.com>
2023-03-13 17:21:52 +0300

    golines: apply suggestions from code review
    Co-authored-by: Ruoyu Zhong <zhongruoyu@outlook.com>

--- Formula/golines.rb
+++ Formula/golines.rb
@@ -1,11 +1,12 @@
 class Golines < Formula
   desc "Golang formatter that fixes long lines"
   homepage "https://github.com/segmentio/golines"
-  url "https://github.com/segmentio/golines/archive/v0.11.0.tar.gz"
+  url "https://github.com/segmentio/golines/archive/refs/tags/v0.11.0.tar.gz"
   sha256 "d7336fbddb045bd2448629c4b8ef5ab2dc6136e71a795b6fdd596066bc00adc0"
   license "MIT"
+  head "https://github.com/segmentio/golines.git", branch: "master"
 
-  depends_on "go"
+  depends_on "go" => :build
 
   def install
     system "go", "build", *std_go_args(ldflags: "-s -w")
@@ -22,6 +23,6 @@ class Golines < Formula
 
       var strings = []string{\n\t"foo",\n\t"bar",\n\t"baz",\n}
     EOS
-    assert_match shell_output("#{bin}/golines --max-len=30 given.go"), (testpath/"expected.go").read
+    assert_equal shell_output("#{bin}/golines --max-len=30 given.go"), (testpath/"expected.go").read
   end
 end
