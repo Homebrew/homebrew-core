a95aa197ed356874f2b80c9f1ec620de6e1a8867
Tiago Peixoto <tiago@skewed.de>
2023-03-20 13:21:11 +0100

    graph-tool: require llvm to enable openmp support

--- Formula/graph-tool.rb
+++ Formula/graph-tool.rb
@@ -6,6 +6,7 @@ class GraphTool < Formula
   url "https://downloads.skewed.de/graph-tool/graph-tool-2.48.tar.bz2"
   sha256 "871336d649c231a667502dec9b871f4e396186760a254877ba010b65b0fd7be6"
   license "LGPL-3.0-or-later"
+  revision 1
 
   livecheck do
     url "https://downloads.skewed.de/graph-tool/"
@@ -34,6 +35,8 @@ class GraphTool < Formula
   depends_on "google-sparsehash"
   depends_on "gtk+3"
   depends_on "librsvg"
+  depends_on "libomp"
+  depends_on "llvm"
   depends_on macos: :mojave # for C++17
   depends_on "numpy"
   depends_on "pillow"
@@ -111,7 +114,12 @@ class GraphTool < Formula
       package = Formula[package_name].opt_libexec
       (libexec/site_packages/"homebrew-#{package_name}.pth").write package/site_packages
     end
-
+    
+    ENV["CC"]=Formula["llvm"].opt_bin/"clang"
+    ENV["CXX"]=Formula["llvm"].opt_bin/"clang++"
+    ENV["LDFLAGS"]="-L#{Formula["llvm"].opt_lib} -Wl,-rpath,#{Formula["llvm"].opt_lib} -L#{Formula["libomp"].opt_lib} -Wl,-rpath,#{Formula["libomp"].opt_lib}"
+    ENV["CPPFLAGS"]="-I#{Formula["llvm"].opt_include} -I#{Formula["libomp"].opt_include}"
+    
     args = %W[
       PYTHON=#{python3}
       --with-python-module-path=#{prefix/site_packages}
