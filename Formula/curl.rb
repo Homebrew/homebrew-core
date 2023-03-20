1a11ecb0c4aa512b1d598abbaa7f2a17eba97a2d
Sean Molenaar <SMillerDev@users.noreply.github.com>
2023-03-20 08:34:52 +0100

    curl: restore mirror

--- Formula/curl.rb
+++ Formula/curl.rb
@@ -2,7 +2,9 @@ class Curl < Formula
   desc "Get a file from an HTTP, HTTPS or FTP server"
   homepage "https://curl.se"
   url "https://curl.se/download/curl-8.0.0.tar.bz2"
-  mirror "http://fresh-center.net/linux/www/legacy/url-8.0.0.tar.bz2"
+  mirror "https://github.com/curl/curl/releases/download/curl-8_0_0/curl-8.0.0.tar.bz2"
+  mirror "http://fresh-center.net/linux/www/curl-8.0.0.tar.bz2"
+  mirror "http://fresh-center.net/linux/www/legacy/curl-8.0.0.tar.bz2"
   sha256 "dd6e792593dbd2253cc2da57265808427e3614e84ca86d424fbc75cf9baba08c"
   license "curl"
 
