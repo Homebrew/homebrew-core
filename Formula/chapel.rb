3a8e5238dfa98c0e833aa5e1816653eab9c3bfb6
bhavanijayakumaran <82669529+bhavanijayakumaran@users.noreply.github.com>
2023-03-20 15:15:48 -0500

    chapel 1.30.0 pre-release
    Signed-off-by: bhavanijayakumaran <82669529+bhavanijayakumaran@users.noreply.github.com>

--- Formula/chapel.rb
+++ Formula/chapel.rb
@@ -1,8 +1,8 @@
 class Chapel < Formula
   desc "Programming language for productive parallel computing at scale"
   homepage "https://chapel-lang.org/"
-  url "https://github.com/chapel-lang/chapel/releases/download/1.29.0/chapel-1.29.0.tar.gz"
-  sha256 "f87bc7285c4641ed540f806c63ab904137b650b1e232a11c15f76e874e079804"
+  url "https://chapel-lang.org/tmp/chapel-1.30.0.tar.gz"
+  sha256 "8d933f4b6e497d9699a14deedd222e18c77c523483957d66201731d0d94285d2"
   license "Apache-2.0"
   head "https://github.com/chapel-lang/chapel.git", branch: "main"
 
