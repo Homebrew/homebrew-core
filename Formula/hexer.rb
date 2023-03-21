741e355cbba6664caea1f7c82fa10daa0080fbef
Mike Ryan <mike@ice9.us>
2023-03-20 21:47:24 -0700

    hexer 1.0.6 (new formula)
    Add Linux dependency on ncurses

--- Formula/hexer.rb
+++ Formula/hexer.rb
@@ -4,6 +4,7 @@ class Hexer < Formula
   url "https://devel.ringlet.net/files/editors/hexer/hexer-1.0.6.tar.gz"
   sha256 "fff00fbb0eb0eee959c08455861916ea672462d9bcc5580207eb41123e188129"
   license "BSD-3-Clause"
+  uses_from_macos "ncurses"
 
   def install
     system "make", "install", "PREFIX=#{prefix}", "MANDIR=#{man1}"
