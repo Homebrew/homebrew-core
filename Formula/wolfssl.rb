add2c1ee50ce804ad6ffc99d69bf14dc01381632
Ruoyu Zhong <zhongruoyu@outlook.com>
2023-03-25 07:33:01 +0800

    wolfssl: add build dependency `util-linux`

--- Formula/wolfssl.rb
+++ Formula/wolfssl.rb
@@ -26,6 +26,7 @@ class Wolfssl < Formula
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
+  depends_on "util-linux" => :build
 
   def install
     args = %W[
