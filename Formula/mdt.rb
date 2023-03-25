d909f924df30ae2dcf20b51a65ef9f3bb3f8fdb0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-03-25 22:29:50 +0000

    mdt: add 1.1.1 bottle.

--- Formula/mdt.rb
+++ Formula/mdt.rb
@@ -5,6 +5,10 @@ class Mdt < Formula
   sha256 "12bc8ec4c70487349a216b07182b88b9cb62d15816abae3a9f2b03207d5e019d"
   license "GPL-3.0-or-later"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "8c5242c00dd94dd0421af16d5626c50885666cca44968b7f54224e1e273cd76a"
+  end
+
   depends_on "gum"
 
   def install
