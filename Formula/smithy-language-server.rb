4da265e04be870229f9f27d79bd869f8ff49f104
Henry Fraser <henrymfraser@gmail.com>
2023-03-26 10:59:08 +1000

    Used better test and install

--- Formula/smithy-language-server.rb
+++ Formula/smithy-language-server.rb
@@ -7,12 +7,30 @@ class SmithyLanguageServer < Formula
   license "Apache-2.0"
 
   depends_on "gradle" => :build
+  depends_on "openjdk@11"
 
   def install
-    system "gradle", "build"
+    system "gradle", "installDist"
   end
 
   test do
-    system "gradle", "test"
+    input = 
+       "$version: \"2.0\"\r\n" \
+       "\r\n" \
+       "namespace com.example\r\n" \
+       "\r\n" \
+       "use com.foo#emptyTraitStruct\r\n" \
+       "\r\n" \
+       "@emptyTraitStruct\r\n" \
+       "structure OtherStructure {\r\n" \
+       "    foo: String\r\n" \
+       "    bar: String\r\n" \
+       "    baz: Integer\r\n" \
+       "}\r\n"
+
+    output = pipe_output("#{bin}/smithy-language-server", input, 0)
+
+    # TODO: test this once install works
+    assert_match(/^Content-Length: \d+/i, output)
   end
 end
