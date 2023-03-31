63e680bbae4472dbd324a6a69e90f9587b4766d5
Chris Dolan <chris@r2c.dev>
2023-03-31 09:14:17 -0600

    lint

--- Formula/semgrep.rb
+++ Formula/semgrep.rb
@@ -27,11 +27,11 @@ class Semgrep < Formula
   depends_on "cmake" => :build
   depends_on "coreutils"=> :build
   depends_on "dune" => :build
-  depends_on "gmp"
   depends_on "ocaml" => :build
   depends_on "opam" => :build
   depends_on "pipenv" => :build
   depends_on "pkg-config" => :build
+  depends_on "gmp"
   depends_on "pcre"
   depends_on "python-typing-extensions"
   depends_on "python@3.11"
