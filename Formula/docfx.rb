723ff3cdc191cdeac4c7d3786f41c7aa990772e2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-04-07 05:08:53 +0000

    docfx 2.65.0

--- Formula/docfx.rb
+++ Formula/docfx.rb
@@ -1,8 +1,8 @@
 class Docfx < Formula
   desc "Tools for building and publishing API documentation for .NET projects"
   homepage "https://dotnet.github.io/docfx/"
-  url "https://github.com/dotnet/docfx/archive/refs/tags/v2.63.0.tar.gz"
-  sha256 "78b244b7af1c056825f603ad6364dd3576e7ff9baefffd7c0f4ce8eb905c9122"
+  url "https://github.com/dotnet/docfx/archive/refs/tags/v2.65.0.tar.gz"
+  sha256 "f88ee818f036944f6c499ba8562c4a9fc2d9a4a31dbac0c3fc708eef6fe6f6fe"
   license "MIT"
 
   bottle do
