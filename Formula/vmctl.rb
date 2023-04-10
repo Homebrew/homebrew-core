2fce095188bc953997639bb180d0d0689f222351
Denys Holius <d.holius@victoriametrics.com>
2023-04-10 18:53:44 +0300

    vmctl 1.90.0 (new formula)

---
+++ Formula/vmctl.rb
@@ -0,0 +1,31 @@
+class Vmctl < Formula
+  desc "Data migration tool used to migrate data from supported DBs to VictoriaMetrics"
+  homepage "https://docs.victoriametrics.com/vmctl.html"
+  url "https://github.com/VictoriaMetrics/VictoriaMetrics.git",
+      tag:      "v1.90.0",
+      revision: "b5d18c0d281b5bcd4dc13cc72d897c38fd4bb374"
+  license "Apache-2.0"
+  head "https://github.com/VictoriaMetrics/VictoriaMetrics.git", branch: "master"
+
+  depends_on "go" => :build
+
+  def install
+    system "make", "vmctl"
+    bin.install "bin/vmctl"
+  end
+
+  def caveats
+    <<~EOS
+      For more details, read:
+        https://docs.victoriametrics.com/vmctl.html
+      VictoriaMetrics on Github:
+          https://github.com/VictoriaMetrics/VictoriaMetrics
+      Feel free to ask your question in our communities:
+        https://docs.victoriametrics.com/Single-server-VictoriaMetrics.html#contacts
+    EOS
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/vmctl --version")
+  end
+end
