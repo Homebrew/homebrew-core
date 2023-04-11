eddd5e3370a0191214293dc513caec939af8c1c0
Porkepix <porkepix@gmail.com>
2023-04-11 09:33:45 +0200

    erdtree 1.8.0

--- Formula/erdtree.rb
+++ Formula/erdtree.rb
@@ -1,8 +1,8 @@
 class Erdtree < Formula
   desc "Multi-threaded file-tree visualizer and disk usage analyzer"
   homepage "https://github.com/solidiquis/erdtree"
-  url "https://github.com/solidiquis/erdtree/archive/refs/tags/v1.7.1.tar.gz"
-  sha256 "b945a173367bca8bdf79e95629269290d93be49840225bd00a2ca5290e7a04a5"
+  url "https://github.com/solidiquis/erdtree/archive/refs/tags/v1.8.0.tar.gz"
+  sha256 "1c794f96296e261d4f564947a4b2e2f8689f7c2d81f0163ecc149f1261885f93"
   license "MIT"
 
   bottle do
