22c44a089e6500e06696a9c9b0fc7abfff9485fa
Rui Chen <rui@chenrui.dev>
2023-04-13 14:42:01 -0400

    terminalizer: update build

--- Formula/terminalizer.rb
+++ Formula/terminalizer.rb
@@ -8,15 +8,26 @@ class Terminalizer < Formula
   license "MIT"
   head "https://github.com/faressoft/terminalizer.git", branch: "master"
 
-  livecheck do
-    url :stable
-  end
-
   depends_on "node"
 
   def install
     system "npm", "install", *Language::Node.std_npm_install_args(libexec)
     bin.install_symlink Dir["#{libexec}/bin/*"]
+
+    # remove non-native architecture pre-built binaries
+    os = OS.kernel_name.downcase
+    arch = Hardware::CPU.intel? ? "x64" : Hardware::CPU.arch.to_s
+    node_modules = libexec/"lib/node_modules/terminalizer/node_modules"
+    node_pty_prebuilds = node_modules/"node-pty-prebuilt-multiarch/prebuilds"
+    (node_pty_prebuilds/"linux-x64").glob("node.abi*.musl.node").map(&:unlink)
+    node_pty_prebuilds.each_child { |dir| dir.rmtree if dir.basename.to_s != "#{os}-#{arch}" }
+
+    node_pty_prebuilds.glob("#{os}-#{arch}/*.node").each do |f|
+      next if f.arch == Hardware::CPU.arch
+      next if OS.mac? && f.archs.include?(Hardware::CPU.arch)
+
+      f.unlink
+    end
   end
 
   test do
