b02842688961bcdf4eb865e2fe8a80a2706be3cc
ELLIOTTCABLE <git@ell.io>
2023-04-15 12:56:38 -0600

    dhall-toml: Fix main branch name

--- Formula/dhall-toml.rb
+++ Formula/dhall-toml.rb
@@ -4,7 +4,7 @@ class DhallToml < Formula
   url "https://hackage.haskell.org/package/dhall-toml-1.0.2/dhall-toml-1.0.2.tar.gz"
   sha256 "d027636c5492a04cfe87117fbb8a3f1e80c6145e7c05fccc2ad5eb754780f9f9"
   license "BSD-3-Clause"
-  head "https://github.com/dhall-lang/dhall-haskell.git", branch: "master"
+  head "https://github.com/dhall-lang/dhall-haskell.git", branch: "main"
 
   depends_on "cabal-install" => :build
   depends_on "ghc@9.2" => :build
