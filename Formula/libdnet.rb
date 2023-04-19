40952859250741ffb581e1d5ab556f4daad0ffad
Chuanchan <williamtsai1111@gmail.com>
2023-04-20 00:10:57 +0800

    libdnet 1.16.4

--- Formula/libdnet.rb
+++ Formula/libdnet.rb
@@ -1,8 +1,8 @@
 class Libdnet < Formula
   desc "Portable low-level networking library"
   homepage "https://github.com/ofalk/libdnet"
-  url "https://github.com/ofalk/libdnet/archive/libdnet-1.14.tar.gz"
-  sha256 "592599c54a57102a177270f3a2caabda2c2ac7768b977d7458feba97da923dfe"
+  url "https://github.com/ofalk/libdnet/archive/libdnet-1.16.4.tar.gz"
+  sha256 "7df1f0a3db9cf03b48cf50ab273fd6a20c1be99eb9344b9663fe3fd9ed9dab65"
   license "BSD-3-Clause"
 
   livecheck do
