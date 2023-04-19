507f9c94b30e609c262a71d0d9a2e06ee0ec91b2
Mikl Wolfe <wolfemikl@gmail.com>
2023-04-19 09:32:38 -0600

    docfx 2.65.3

--- Formula/docfx.rb
+++ Formula/docfx.rb
@@ -1,8 +1,8 @@
 class Docfx < Formula
   desc "Tools for building and publishing API documentation for .NET projects"
   homepage "https://dotnet.github.io/docfx/"
-  url "https://github.com/dotnet/docfx/archive/refs/tags/v2.63.0.tar.gz"
-  sha256 "78b244b7af1c056825f603ad6364dd3576e7ff9baefffd7c0f4ce8eb905c9122"
+  url "https://github.com/dotnet/docfx/archive/refs/tags/v2.65.3.tar.gz"
+  sha256 "b59c155b7e09ce829b80c5028f18888182806de822750d97e02bb3d32e541ff4"
   license "MIT"
 
   bottle do
