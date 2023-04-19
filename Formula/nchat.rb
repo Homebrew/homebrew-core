47dddb430a1748b6a0aa69686c49420c4d5415bc
sidneys <sidneys.github.io@outlook.com>
2023-04-19 14:11:07 +0200

    nchat 3.39 (new formula)

---
+++ Formula/nchat.rb
@@ -0,0 +1,32 @@
+class Nchat < Formula
+  desc "Terminal-based Telegram client for Linux and macOS"
+  homepage "https://github.com/d99kris/nchat"
+  url "https://github.com/d99kris/nchat/archive/refs/tags/v3.39.tar.gz"
+  sha256 "5b3f7bd3b9c232fdd1f202ec85d89874530f2d778ecffbba403be7a568e3ec5e"
+  license "MIT"
+  head "https://github.com/d99kris/nchat.git", branch: "master"
+
+  depends_on "cmake" => :build
+  depends_on "ccache"
+  depends_on "go"
+  depends_on "gperf"
+  depends_on "help2man"
+  depends_on "libmagic"
+  depends_on "ncurses"
+  depends_on "openssl"
+  depends_on "readline"
+  depends_on "sqlite"
+
+  def install
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    setup_process = IO.popen "#{bin}/nchat --setup"
+    sleep 5
+    Process.kill "TERM", setup_process.pid
+    assert_match "Telegram", setup_process.read
+  end
+end
