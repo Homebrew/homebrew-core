3b4d103cb555931f2694696d6c85288e4d1aa71a
Patrick Linnane <patrick@linnane.io>
2023-04-21 09:23:31 -0700

    libusbmuxd: revision bump (libplist 2.3.0)

--- Formula/libusbmuxd.rb
+++ Formula/libusbmuxd.rb
@@ -4,6 +4,7 @@ class Libusbmuxd < Formula
   url "https://github.com/libimobiledevice/libusbmuxd/archive/2.0.2.tar.gz"
   sha256 "8ae3e1d9340177f8f3a785be276435869363de79f491d05d8a84a59efc8a8fdc"
   license all_of: ["GPL-2.0-or-later", "LGPL-2.1-or-later"]
+  revision 1
   head "https://github.com/libimobiledevice/libusbmuxd.git", branch: "master"
 
   bottle do
