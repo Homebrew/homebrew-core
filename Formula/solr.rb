34d9cd42e85c130c8edabf7b21891f9bf3cba2fb
Sean Molenaar <SMillerDev@users.noreply.github.com>
2023-05-03 12:22:31 +0200

    solr: remove patch

--- Formula/solr.rb
+++ Formula/solr.rb
@@ -16,11 +16,6 @@ class Solr < Formula
   #   https://issues.apache.org/jira/browse/SOLR-16733
   depends_on "openjdk@17"
 
-  # Fix Java version detection.
-  # Extracted from commit below; commit contains changelog updates that can't be applied.
-  # https://github.com/apache/solr/commit/f7fe594cdadeadd1e0061075a55a529793e72462.patch?full_index=1
-  patch :DATA
-
   def install
     pkgshare.install "bin/solr.in.sh"
     (var/"lib/solr").install "server/solr/README.md", "server/solr/solr.xml", "server/solr/zoo.cfg"
@@ -65,16 +60,3 @@ class Solr < Formula
     shell_output(bin/"solr stop -p #{port}", 1)
   end
 end
-
-__END__
---- a/bin/solr
-+++ b/bin/solr
-@@ -163,7 +163,7 @@ if [[ $? -ne 0 ]] ; then
-   echo >&2 "${PATH}"
-   exit 1
- else
--  JAVA_VER_NUM=$(echo "$JAVA_VER" | head -1 | awk -F '"' '/version/ {print $2}' | sed -e's/^1\.//' | sed -e's/[._-].*$//')
-+  JAVA_VER_NUM=$(echo "$JAVA_VER" | grep -v '_OPTIONS' | head -1 | awk -F '"' '/version/ {print $2}' | sed -e's/^1\.//' | sed -e's/[._-].*$//')
-   if [[ "$JAVA_VER_NUM" -lt "$JAVA_VER_REQ" ]] ; then
-     echo >&2 "Your current version of Java is too old to run this version of Solr."
-     echo >&2 "We found major version $JAVA_VER_NUM, using command '${JAVA} -version', with response:"
