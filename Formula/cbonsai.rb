8a1babe27e924e71d8bf1b1ea8bff96d10fbb27e
Matthew Horwood <matt@horwood.biz>
2023-05-11 20:55:20 +0100

    bug: fix the test

--- Formula/cbonsai.rb
+++ Formula/cbonsai.rb
@@ -2,7 +2,7 @@ class Cbonsai < Formula
   desc "Console Bonsai is a bonsai tree generator, written in C using ncurses"
   homepage "https://github.com/mhzawadi/homebrew-cbonsai"
   url "https://github.com/mhzawadi/homebrew-cbonsai/archive/refs/tags/v1.3.4.tar.gz"
-  sha256 "c2e603c24afc5900ce0f2e98fd86aee75d8ef1f81b52410227465193e246b481"
+  sha256 "d984f50afab06c5160b7f8577672397203d9d52f6857648c56f0fca4a409fb51"
   license "GPL-3.0"
   depends_on "ncurses"
   depends_on "pkg-config"
@@ -15,6 +15,7 @@ class Cbonsai < Formula
   end
 
   test do
-    system "~/.local/bin/cbonsai", "-p"
+    system bin/"cbonsai", "-p"
   end
 end
+
