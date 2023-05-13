2f9ab67fd83ac30e6e3a52271043166982bca552
Patrick Linnane <patrick@linnane.io>
2023-05-12 21:19:23 -0700

    libirecovery 1.1.0

--- Formula/libirecovery.rb
+++ Formula/libirecovery.rb
@@ -1,8 +1,8 @@
 class Libirecovery < Formula
   desc "Library and utility to talk to iBoot/iBSS via USB"
   homepage "https://www.libimobiledevice.org/"
-  url "https://github.com/libimobiledevice/libirecovery/releases/download/1.0.0/libirecovery-1.0.0.tar.bz2"
-  sha256 "cda0aba10a5b6fc2e1d83946b009e3e64d0be36912a986e35ad6d34b504ad9b4"
+  url "https://github.com/libimobiledevice/libirecovery/releases/download/1.1.0/libirecovery-1.1.0.tar.bz2"
+  sha256 "ee3b1afbc0cab5309492cfcf3e132c6cc002617a57664ee0120ae918318e25f9"
   license "LGPL-2.1-only"
 
   bottle do
