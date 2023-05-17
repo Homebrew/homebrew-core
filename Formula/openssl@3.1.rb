0a53cdca86f758862c2d7c8ce782d4764616fc37
Yichen Yan <wenji.yyc@alibaba-inc.com>
2023-05-17 11:49:42 +0800

    Point `openssl@3` to `openssl@3.0`.

--- Formula/openssl@3.1.rb
+++ Formula/openssl@3.1.rb
@@ -1,24 +1,24 @@
-class OpensslAT30 < Formula
+class OpensslAT31 < Formula
   desc "Cryptography and SSL/TLS Toolkit"
   homepage "https://openssl.org/"
-  url "https://www.openssl.org/source/openssl-3.0.8.tar.gz"
-  mirror "https://www.mirrorservice.org/sites/ftp.openssl.org/source/openssl-3.0.8.tar.gz"
-  sha256 "6c13d2bf38fdf31eac3ce2a347073673f5d63263398f1f69d0df4a41253e4b3e"
+  url "https://www.openssl.org/source/openssl-3.1.0.tar.gz"
+  mirror "https://www.mirrorservice.org/sites/ftp.openssl.org/source/openssl-3.1.0.tar.gz"
+  sha256 "aaa925ad9828745c4cad9d9efeb273deca820f2cdcf2c3ac7d7c1212b7c497b4"
   license "Apache-2.0"
 
   livecheck do
     url "https://www.openssl.org/source/"
-    regex(/href=.*?openssl[._-]v?(3\.0(?:\.\d+)+)\.t/i)
+    regex(/href=.*?openssl[._-]v?(\d+(?:\.\d+)+)\.t/i)
   end
 
   bottle do
-    sha256 arm64_ventura:  "110f3c266102f0fa0c82be4cf3ffa6603fed29aa6e1711741672bb6bfe769805"
-    sha256 arm64_monterey: "553ef2386034113e3156593291b331c130f459c0a8d148fb411ea084e036f1e7"
-    sha256 arm64_big_sur:  "46aba17269f861d709a708914347d65a0a126781c1baebcbd7e18a8adede576d"
-    sha256 ventura:        "984d024ec443bd77ea8e83568251140fbbd710a4ec48670903b1ae5809baa1c5"
-    sha256 monterey:       "bf169df9308d0f428e22738863b85464af92f970fe823a24ae7a150d79b2bc24"
-    sha256 big_sur:        "f62ab4c4fd6f33b32ba44d23482fb998b0ac8b1ca07c8e3083ccfe94fe2d1bd9"
-    sha256 x86_64_linux:   "81d153bc01a5863e5430a6d46e3f6e4f4534d0dcb06710befa399a4c1ffbb961"
+    sha256 arm64_ventura:  "1dd3c96c86bcd743dc8a7e1b0d18c7d76eb4d733ce7768e3931ad027f7b1d1fd"
+    sha256 arm64_monterey: "cb8b2e4bbf311516addc310c067e9655aca5e4f697b134580d3147624616aa5f"
+    sha256 arm64_big_sur:  "b994e140d306335dc48a85bb41aea685e0e41dd67978027261acd6c0b6aaa532"
+    sha256 ventura:        "64e237e99be9d8ee4ddfd3813384ace1920f28ed1f74fac2730908839aaddc40"
+    sha256 monterey:       "a6327ea42283c5fdc582b431da337a6aa371ed9ca019e1ca9d8a40e273535a0d"
+    sha256 big_sur:        "2f678f6bb7b0ca91b17aada633fe617dd0f2b05eafd1c9cc2497535a4a8f3f86"
+    sha256 x86_64_linux:   "20295ac304dcaf92f82260fa8fcb2e28a85a2860e571e514f17e7411b45ea1aa"
   end
 
   keg_only :shadowed_by_macos, "macOS provides LibreSSL"
@@ -29,19 +29,33 @@ class OpensslAT30 < Formula
     keg_only "it conflicts with the `openssl@1.1` formula"
 
     resource "Test::Harness" do
-      url "https://cpan.metacpan.org/authors/id/L/LE/LEONT/Test-Harness-3.44.tar.gz"
-      sha256 "7eb591ea6b499ece6745ff3e80e60cee669f0037f9ccbc4e4511425f593e5297"
+      url "https://cpan.metacpan.org/authors/id/L/LE/LEONT/Test-Harness-3.42.tar.gz"
+      sha256 "0fd90d4efea82d6e262e6933759e85d27cbcfa4091b14bf4042ae20bab528e53"
     end
 
     resource "Test::More" do
-      url "https://cpan.metacpan.org/authors/id/E/EX/EXODIST/Test-Simple-1.302194.tar.gz"
-      sha256 "23d4965eb32504c8c7670563ce784ce5dc043c2572d771ce4a584558d6869f48"
+      url "https://cpan.metacpan.org/authors/id/E/EX/EXODIST/Test-Simple-1.302186.tar.gz"
+      sha256 "2895c8da7c3fe632e5714c7cc548705202cdbf3afcbc0e929bc5e6a5172265d4"
     end
 
     resource "ExtUtils::MakeMaker" do
-      url "https://cpan.metacpan.org/authors/id/B/BI/BINGOS/ExtUtils-MakeMaker-7.68.tar.gz"
-      sha256 "270238d253343b833daa005fb57a3a5d8916b59da197698a632b141e7acad779"
+      url "https://cpan.metacpan.org/authors/id/B/BI/BINGOS/ExtUtils-MakeMaker-7.62.tar.gz"
+      sha256 "5022ad857fd76bd3f6b16af099fe2324639d9932e08f21e891fb313d9cae1705"
     end
+  end
+
+  # fix build on macOS pre-10.14; remove in 3.1.1; see openssl/openssl#19361
+  patch do
+    url "https://github.com/openssl/openssl/commit/96f1dbea67247b79b1e7b3f83f2964dc626d86ce.patch?full_index=1"
+    sha256 "95d662ce38c84bb7e4ca1f78420815360f6254e95854915b067739db23e4df20"
+  end
+  patch do
+    url "https://github.com/openssl/openssl/commit/d4765408c705f704f7cf33bd32bfb713061954a7.patch?full_index=1"
+    sha256 "fe79b4bfe1daf2995b49b3a57bc1d4f07d615871300424ce9091096e3bb1501b"
+  end
+  patch do
+    url "https://github.com/openssl/openssl/commit/110dac578358014c29b86cf18d9a4bfe5561e3bc.patch?full_index=1"
+    sha256 "27da17844cb47fbeaac237ad8082d2e2c8ace523fd93b07a5a7fde6d6ad05c62"
   end
 
   # SSLv2 died with 1.1.0, so no-ssl2 no longer required.
@@ -103,7 +117,7 @@ class OpensslAT30 < Formula
   end
 
   def openssldir
-    etc/"openssl@3.0"
+    etc/"openssl@3.1"
   end
 
   def post_install
