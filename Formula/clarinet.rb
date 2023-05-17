910ffa9f1b67063da18d0f571475e4eb97bfa2df
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-05-17 05:04:55 +0000

    clarinet 1.6.1

--- Formula/clarinet.rb
+++ Formula/clarinet.rb
@@ -3,8 +3,8 @@ class Clarinet < Formula
   homepage "https://github.com/hirosystems/clarinet"
   # pull from git tag to get submodules
   url "https://github.com/hirosystems/clarinet.git",
-      tag:      "v1.5.4",
-      revision: "3b741691b0417d06f03b3a83935dc13808363d4c"
+      tag:      "v1.6.1",
+      revision: "acd7a3affaf9f2beb57c11fca888840239c7baba"
   license "GPL-3.0-only"
   head "https://github.com/hirosystems/clarinet.git", branch: "main"
 
