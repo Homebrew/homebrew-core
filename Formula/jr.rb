89689346bde4eea542a13aab2f59751725523748
ram-pi <pierpaolo.rametta@gmail.com>
2023-05-21 23:03:20 +0200

    jr 0.2.1 (new formula)

--- Formula/jr.rb
+++ Formula/jr.rb
@@ -1,5 +1,5 @@
 class Jr < Formula
-    desc "Quality Random Data from the Command line"
+    desc "Quality Random Data from the Command-line"
     homepage "https://github.com/ugol/jr"
     url "https://github.com/ugol/jr/archive/refs/tags/v0.2.1.tar.gz"
     sha256 "d150ae875c9238e3d23b06bd069f0d03855f0b6763b3b6b3eb8b89d2dfc02cae"
@@ -10,7 +10,7 @@ class Jr < Formula
     def install
       system "make", "all"
       bin.install Dir["build/*"]
-      prefix.install Dir["templates"]
+      prefix.install "templates"
     end
   
     test do
