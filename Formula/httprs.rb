114666537531ca3fc11c2602248d3014f552d8ae
Esteban Borai <estebanborai@gmail.com>
2023-05-28 18:18:05 -0400

    feat: provide license

--- Formula/httprs.rb
+++ Formula/httprs.rb
@@ -4,6 +4,7 @@ class Httprs < Formula
   url "https://github.com/http-server-rs/http-server/releases/latest/download/http-server_macos-latest"
   sha256 "c18c5652ab3b0931c9f7e44ec8edc5aafaea38c05c5c9bc232ee50ecc423cb8a"
   version "0.8.5"
+  license "Apache-2.0-or-MIT"
   
   def install
     system "mv", "http-server_macos-latest", "httprs"
