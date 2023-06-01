ec8acf0167776bb72cf6e1619bdec01baa19707a
Dimitris Mantzouranis <d3xter93@gmail.com>
2023-06-01 12:54:52 +0300

    add missing build deps

--- Formula/sonixflasher.rb
+++ Formula/sonixflasher.rb
@@ -5,7 +5,13 @@ class Sonixflasher < Formula
   sha256 "899beef48ca8eca0d8037794870f9f58f7cb463e3a3ddfeb2b5c446edbffbef9"
   license "GPL-3.0-only"
 
+  depends_on "pkg-config" => :build
   depends_on "hidapi"
+
+  on_linux do
+    depends_on "libusb"
+    depends_on "systemd" # for libudev
+  end
 
   def install
     system "make", "clean", "sonixflasher"
