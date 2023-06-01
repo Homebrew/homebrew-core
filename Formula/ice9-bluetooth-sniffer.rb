e738fc2629b7d0411930020d25b966ab526264cb
Mike Ryan <mike@ice9.us>
2023-06-01 16:28:21 -0700

    ice-bluetooth-sniffer 23.06.0 (new formula)

--- Formula/ice9-bluetooth-sniffer.rb
+++ Formula/ice9-bluetooth-sniffer.rb
@@ -2,7 +2,7 @@ class Ice9BluetoothSniffer < Formula
   desc "Wireshark-compatible all-channel BLE sniffer for bladeRF, HackRF, and USRP"
   homepage "https://github.com/mikeryan/ice9-bluetooth-sniffer"
   url "https://github.com/mikeryan/ice9-bluetooth-sniffer/archive/refs/tags/v23.06.0.tar.gz"
-  sha256 "582b95a7e4cd82598c76c30f3ab134fdca0bf65b13e28f231b51ebbc8312124f"
+  sha256 "2fc322c18b775cb0d6101efb24d09aea80f758f26e0772820a4595a7d8313e99"
   license "GPL-2.0-or-later"
 
   depends_on "cmake" => :build
