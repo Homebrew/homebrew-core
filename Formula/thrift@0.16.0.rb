1f48e6754991c0e653c1633f694799d26730c62f
Vinoo Ganesh <vinoo.ganesh@gmail.com>
2023-06-15 16:20:33 -0400

    thrift@0.16.0 0.16.0 (new formula)
    Thrift has bumped their minimum Java version to Java 11. However, there are many libraries that use Thrift that depend on Java 8. One of these libraries includes Parquet. As such, Parquet is unable to upgrade to the latest version of Thrift, and an ongoing conversation is occurring here: apache/thrift#2785.
    
    Homebrew only has 2 versions of Thrift available, 0.9 and 0.18.+, the latter of which is on Java 11. I'd like to re-introduce Thrift 0.16.0 into Homebrew to give developers that are mandated to use Java 8 applications a working version of Thrift.

---
+++ Formula/thrift@0.16.0.rb
@@ -0,0 +1,64 @@
+class ThriftAT0160 < Formula
+  desc "Framework for scalable cross-language services development"
+  homepage "https://thrift.apache.org/"
+  url "https://github.com/apache/thrift/archive/refs/tags/v0.16.0.tar.gz"
+  mirror "https://archive.apache.org/dist/thrift/0.16.0/thrift-0.16.0.tar.gz"
+  sha256 "df2931de646a366c2e5962af679018bca2395d586e00ba82d09c0379f14f8e7b"
+  license "Apache-2.0"
+
+  keg_only :versioned_formula
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "bison" => :build
+  depends_on "libtool" => :build
+  depends_on "pkg-config" => :build
+  depends_on "boost"
+  depends_on "openssl@1.1"
+
+  def install
+    system "./bootstrap.sh"
+
+    args = %W[
+      --disable-debug
+      --disable-tests
+      --prefix=#{prefix}
+      --libdir=#{lib}
+      --with-openssl=#{Formula["openssl@1.1"].opt_prefix}
+      --without-java
+      --without-kotlin
+      --without-python
+      --without-py3
+      --without-ruby
+      --without-haxe
+      --without-netstd
+      --without-perl
+      --without-php
+      --without-php_extension
+      --without-dart
+      --without-erlang
+      --without-go
+      --without-d
+      --without-nodejs
+      --without-nodets
+      --without-lua
+      --without-rs
+      --without-swift
+    ]
+
+    ENV.cxx11 if ENV.compiler == :clang
+
+    # Don't install extensions to /usr:
+    ENV["PY_PREFIX"] = prefix
+    ENV["PHP_PREFIX"] = prefix
+    ENV["JAVA_PREFIX"] = buildpath
+
+    system "./configure", *args
+    ENV.deparallelize
+    system "make", "install"
+  end
+
+  test do
+    assert_match "Thrift", shell_output("#{bin}/thrift --version")
+  end
+end
