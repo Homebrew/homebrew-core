1b71f7d17524db0076b6223cd5bb02354d6ae9c8
Rui Chen <rui@chenrui.dev>
2023-06-16 15:01:24 -0400

    embulk: run with `openjdk`
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/embulk.rb
+++ Formula/embulk.rb
@@ -1,12 +1,10 @@
 class Embulk < Formula
   desc "Data transfer between various databases, file formats and services"
   homepage "https://www.embulk.org/"
-  # https://www.embulk.org/articles/2020/03/13/embulk-v0.10.html
-  # v0.10.* is a "development" series, not for your production use.
-  # In your production, keep using v0.9.* stable series.
   url "https://github.com/embulk/embulk/releases/download/v0.11.0/embulk-0.11.0.jar"
   sha256 "16664089e2676d5f7fe71c1ccf48b2a24f982b89e4f483e650a15f52a72309c1"
   license "Apache-2.0"
+  revision 1
   version_scheme 1
 
   livecheck do
@@ -21,11 +19,11 @@ class Embulk < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux: "f18e539ca70420cd3cc6fb7f7b24cc1d761ecb9cf7af7121d576a0727a17586f"
   end
 
-  depends_on "openjdk@8"
+  depends_on "openjdk"
 
   def install
     libexec.install "embulk-#{version}.jar"
-    bin.write_jar_script libexec/"embulk-#{version}.jar", "embulk", java_version: "1.8"
+    bin.write_jar_script libexec/"embulk-#{version}.jar", "embulk"
   end
 
   test do
