b80d939de5dbf9d7fa79393d5f6b52c6a33cfb2c
Rui Chen <rui@chenrui.dev>
2023-06-16 17:43:13 -0400

    vulkan-tools 1.3.254
    vulkan-tools: update build
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/vulkan-tools.rb
+++ Formula/vulkan-tools.rb
@@ -1,8 +1,8 @@
 class VulkanTools < Formula
   desc "Vulkan utilities and tools"
   homepage "https://github.com/KhronosGroup/Vulkan-Tools"
-  url "https://github.com/KhronosGroup/Vulkan-Tools/archive/refs/tags/v1.3.250.tar.gz"
-  sha256 "a1cd4f7d8d58bec26efb1e5ea73f82dcf36a9662d55401b81a783071b211a35a"
+  url "https://github.com/KhronosGroup/Vulkan-Tools/archive/refs/tags/v1.3.254.tar.gz"
+  sha256 "9d46697d0df39e20950db2f8581b63b3d02ab5dce6b67777b9f7bdf876ae4921"
   license "Apache-2.0"
   head "https://github.com/KhronosGroup/Vulkan-Tools.git", branch: "main"
 
@@ -54,6 +54,7 @@ class VulkanTools < Formula
       "-DBUILD_ICD=ON",
       "-DBUILD_CUBE=ON",
       "-DBUILD_VULKANINFO=ON",
+      "-DTOOLS_CODEGEN=ON", # custom codegen
       "-DINSTALL_ICD=OFF", # we will manually place it in a nonconflicting location
       "-DGLSLANG_INSTALL_DIR=#{Formula["glslang"].opt_prefix}",
       "-DVULKAN_HEADERS_INSTALL_DIR=#{Formula["vulkan-headers"].opt_prefix}",
@@ -70,7 +71,6 @@ class VulkanTools < Formula
       ]
     end
     system "cmake", "-S", ".", "-B", "build", *args, *std_cmake_args
-    system "cmake", "--build", "build", "--target", "VulkanTools_generated_source"
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
 
