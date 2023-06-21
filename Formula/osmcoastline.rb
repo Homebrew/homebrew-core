bb560d49aa6e68d75ff672f5f70ad05ca73c699f
Erica Fischer <enf@felt.com>
2023-06-21 15:58:33 -0700

    osmcoastline-2.4.0-r2

--- Formula/osmcoastline.rb
+++ Formula/osmcoastline.rb
@@ -4,7 +4,7 @@ class Osmcoastline < Formula
   url "https://github.com/osmcode/osmcoastline/archive/v2.4.0.tar.gz"
   sha256 "2c1a28313ed19d6e2fb1cb01cde8f4f44ece378393993b0059f447c5fce11f50"
   license "GPL-3.0-or-later"
-  revision 1
+  revision 2
 
   bottle do
     sha256 cellar: :any,                 arm64_ventura:  "269ea3be14f5395ed5e3d453937ae693470c3594e2d7389fb3180dc7e08b6c80"
@@ -27,6 +27,12 @@ class Osmcoastline < Formula
   uses_from_macos "zlib"
 
   fails_with gcc: "5"
+
+  # To fix gdal-3.7.0
+  patch do
+    url "https://github.com/osmcode/osmcoastline/commit/67cc33161069f65e315acae952492ab5ee07af15.patch?full_index=1"
+    sha256 "31b89e33b22ccdfe289a5da67480f9791bdd4f410c6a7831f0c1e007c4258e68"
+  end
 
   def install
     protozero = Formula["libosmium"].opt_libexec/"include"
