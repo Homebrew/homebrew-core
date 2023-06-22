d575d10a1f05bf601b0c2f5f0269dcac561b2f2f
Daniel Milde <daniel.milde@outreach.io>
2023-06-22 17:38:11 +0200

    gdu: rename binary to gdu-go to mitigate conflict with coreutils

--- Formula/gdu.rb
+++ Formula/gdu.rb
@@ -30,7 +30,7 @@ class Gdu < Formula
       -X "github.com/dundee/gdu/v#{major}/build.User=#{user}"
     ]
 
-    system "go", "build", *std_go_args(ldflags: ldflags), "./cmd/gdu"
+    system "go", "build", *std_go_args(ldflags: ldflags, output: "#{bin}/gdu-go"), "./cmd/gdu"
   end
 
   test do
@@ -38,8 +38,9 @@ class Gdu < Formula
     (testpath/"test_dir"/"file1").write "hello"
     (testpath/"test_dir"/"file2").write "brew"
 
-    assert_match version.to_s, shell_output("#{bin}/gdu -v")
-    assert_match "colorized", shell_output("#{bin}/gdu --help 2>&1")
-    assert_match "4.0 KiB file1", shell_output("#{bin}/gdu --non-interactive --no-progress #{testpath}/test_dir 2>&1")
+    assert_match version.to_s, shell_output("#{bin}/gdu-go -v")
+    assert_match "colorized", shell_output("#{bin}/gdu-go --help 2>&1")
+    assert_match "4.0 KiB file1",
+      shell_output("#{bin}/gdu-go --non-interactive --no-progress #{testpath}/test_dir 2>&1")
   end
 end
