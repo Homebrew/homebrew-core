bcdad2d444f0c9bb2365efba6440343648db8d31
Percy Ma <kecrily@gmail.com>
2023-07-03 17:46:34 +0800

    update

--- Formula/rotz.rb
+++ Formula/rotz.rb
@@ -1,5 +1,5 @@
 class Rotz < Formula
-  desc "Fully cross platform dotfile manager and dev environment bootstrapper written in Rust."
+  desc "Fully cross platform dotfile manager and dev environment bootstrapper"
   homepage "https://volllly.github.io/rotz/"
   url "https://github.com/volllly/rotz/archive/refs/tags/v0.9.3.tar.gz"
   sha256 "b7556bdfbee63c9def84a6ce8b12fa41d3f7ee53e2d3b3f240d35110c9ed2f1b"
@@ -13,6 +13,10 @@ class Rotz < Formula
   end
 
   test do
-    assert_match "rotz ${version}", shell_output("#{bin}/rotz -v")
+    assert_match "rotz #{version}\n", shell_output("#{bin}/rotz -V")
+
+    system bin/"rotz", "clone", "https://github.com/volllly/.dotfiles.git"
+
+    assert_predicate testpath/".dotfiles", :exist?
   end
 end
