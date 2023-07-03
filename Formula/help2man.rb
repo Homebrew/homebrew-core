83389cd4424b44323c5daa2384ec19212b2a4ea5
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2023-07-03 19:59:40 +0800

    help2man: revision bump (perl 5.38.0)

--- Formula/help2man.rb
+++ Formula/help2man.rb
@@ -5,7 +5,7 @@ class Help2man < Formula
   mirror "https://ftpmirror.gnu.org/help2man/help2man-1.49.3.tar.xz"
   sha256 "4d7e4fdef2eca6afe07a2682151cea78781e0a4e8f9622142d9f70c083a2fd4f"
   license "GPL-3.0-or-later"
-  revision 1
+  revision 2
 
   bottle do
     sha256 cellar: :any,                 arm64_ventura:  "ce35577716d9a055e403e65e2c2c13171820cac8bfdb0e366fba51ba633fe9bb"
