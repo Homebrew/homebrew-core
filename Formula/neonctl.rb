970cff190fd7bf985922c27746db20e7bae7b1c5
Alexander Bayandin <alexander@neon.tech>
2023-07-14 11:28:13 +0100

    neonctl 1.17.3

---
+++ Formula/neonctl.rb
@@ -0,0 +1,21 @@
+require "language/node"
+
+class Neonctl < Formula
+  desc "Neon CLI tool"
+  homepage "https://neon.tech/docs/reference/neon-cli"
+  url "https://registry.npmjs.org/neonctl/-/neonctl-1.17.3.tgz"
+  sha256 "24a29d160e50e2b5b0cc90524bb40d2a6cdba79c79c72698d004963506aaa7c5"
+  license "Apache-2.0"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    output = shell_output("#{bin}/neonctl --api-key DOES-NOT-EXIST projects create 2>&1", 1)
+    assert_match("Authentication failed,", output)
+  end
+end
