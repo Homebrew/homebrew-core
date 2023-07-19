db3467f11db47478773be815795d0bf006deff31
Henry Chu <henrytech@outlook.com>
2023-07-20 00:07:26 +0800

    rustfmt 1.6.0

--- Formula/rustfmt.rb
+++ Formula/rustfmt.rb
@@ -1,8 +1,8 @@
 class Rustfmt < Formula
   desc "Format Rust code"
   homepage "https://rust-lang.github.io/rustfmt/"
-  url "https://github.com/rust-lang/rustfmt/archive/refs/tags/v1.5.1.tar.gz"
-  sha256 "dc29a1c066fe4816e1400655c676d632335d667c3b0231ce344b2a7b02acc267"
+  url "https://github.com/rust-lang/rustfmt/archive/refs/tags/v1.6.0.tar.gz"
+  sha256 "32ba647a9715efe2699acd3d011e9f113891be02ac011d314b955a9beea723a2"
   license any_of: ["MIT", "Apache-2.0"]
   head "https://github.com/rust-lang/rustfmt.git", branch: "master"
 
@@ -20,17 +20,37 @@ class Rustfmt < Formula
 
   depends_on "rustup-init" => :build
   depends_on "rust" => :test
+  uses_from_macos "zlib"
 
   def install
-    system "#{Formula["rustup-init"].bin}/rustup-init", "-qy", "--no-modify-path"
+    system "rustup-init", "--profile", "minimal", "-qy", "--no-modify-path", "--default-toolchain", "none"
     ENV.prepend_path "PATH", HOMEBREW_CACHE/"cargo_cache/bin"
-    # we are using nightly because rustfmt requires nightly in order to build from source
-    # pinning to nightly-2021-11-08 to avoid inconstency
-    nightly_version = "nightly-2021-11-08"
-    components = %w[rust-src rustc-dev llvm-tools-preview]
-    system "rustup", "toolchain", "install", nightly_version
-    system "rustup", "component", "add", *components, "--toolchain", nightly_version
+
+    ENV["CFG_RELEASE_CHANNEL"] = "stable"
     system "cargo", "install", *std_cargo_args
+
+    # See https://github.com/NixOS/nixpkgs/blob/6cee3b5893090b0f5f0a06b4cf42ca4e60e5d222/pkgs/development/compilers/rust/rustfmt.nix#L18-L27
+    rust_libs = ["librustc_driver", "libstd"]
+    rust_libs << "libLLVM" if OS.linux?
+    rust_libs.each do |it|
+      libexec.install buildpath.glob(".brew_home/.rustup/toolchains/*/lib/#{shared_library("#{it}-*")}")
+    end
+
+    bins_to_patch = ["rustfmt", "git-rustfmt"]
+    if OS.mac?
+      bins_to_patch.each do |it|
+        MachO::Tools.add_rpath((bin/it).to_s, libexec.to_s)
+        MachO.codesign!((bin/it).to_s) if Hardware::CPU.arm?
+      end
+    elsif OS.linux?
+      bins_to_patch.each do |it|
+        patcher = (bin/it).patchelf_patcher
+        libexec.each_child do |replace_lib|
+          patcher.replace_needed(replace_lib.basename, replace_lib.to_s)
+        end
+        patcher.save(patchelf_compatible: true)
+      end
+    end
   end
 
   test do
