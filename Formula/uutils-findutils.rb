99cc154085e7ac9188a2cd0314d2e9c167268118
Rui Chen <rui@chenrui.dev>
2023-07-21 00:33:39 -0400

    uutils-findutils: update sha
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/uutils-findutils.rb
+++ Formula/uutils-findutils.rb
@@ -2,7 +2,7 @@ class UutilsFindutils < Formula
   desc "Cross-platform Rust rewrite of the GNU findutils"
   homepage "https://github.com/uutils/findutils"
   url "https://github.com/uutils/findutils/archive/refs/tags/0.4.1.tar.gz"
-  sha256 "575cbd0be440c25ba5d8a65020d5076a07c57ecc07265f33d41d95050a3e999e"
+  sha256 "6d6361a46e59c43b7bb2a5ba358868c6d11c060a316e6a9dade63ab5138bdda6"
   license "MIT"
   head "https://github.com/uutils/findutils.git", branch: "main"
 
