23ba5f8e3d8212373b309a9811ca29a0168a02fa
Justin <justintorre75@gmail.com>
2023-07-23 11:24:50 -0700

    fixed desc

--- Formula/gptask.rb
+++ Formula/gptask.rb
@@ -1,7 +1,7 @@
 class Gptask < Formula
   include Language::Python::Virtualenv
 
-  desc "The ultimate file editor powered by GPT4"
+  desc "GPT4 automatic file reviewer and editory"
   homepage "https://github.com/chitalian/gptask"
   url "https://files.pythonhosted.org/packages/72/5a/424f8610cdd28cbcf695f082ff152907ab1eb9f547d23294f21c2c4609f1/gptask_cli-0.1.8.tar.gz"
   sha256 "7469ce3be9789775a453aca94f588041f2041ae93679b54122acb17d092c9813"
