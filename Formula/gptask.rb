a06b5c5eb072be8616455c65e0a166b79ec522f7
Justin <justintorre75@gmail.com>
2023-07-23 12:01:16 -0700

    add deps

--- Formula/gptask.rb
+++ Formula/gptask.rb
@@ -9,6 +9,16 @@ class Gptask < Formula
 
   depends_on "python@3.11"
 
+  resource "click" do
+    url "https://files.pythonhosted.org/packages/72/bd/fedc277e7351917b6c4e0ac751853a97af261278a4c7808babafa8ef2120/click-8.1.6.tar.gz"
+    sha256 "48ee849951919527a045bfe3bf7baa8a959c423134e1a5b98c05c20ba75a1cbd"
+  end
+
+  resource "openai" do
+    url "https://files.pythonhosted.org/packages/89/ee/84bbd0161090f0f24e8a2ac175e6b6a936289ee02e9d5da414ce14d3d332/openai-0.27.8.tar.gz"
+    sha256 "2483095c7db1eee274cebac79e315a986c4e55207bb4fa7b82d185b3a2ed9536"
+  end
+
   def install
     virtualenv_install_with_resources
   end
