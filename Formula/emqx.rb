d630f4b5b4eff1806c9f10e2aeb0fe2b063d804c
Rui Chen <rui@chenrui.dev>
2023-07-28 10:06:53 -0400

    emqx 5.1.3

--- Formula/emqx.rb
+++ Formula/emqx.rb
@@ -2,8 +2,8 @@ class Emqx < Formula
   desc "MQTT broker for IoT"
   homepage "https://www.emqx.io/"
   # TODO: Check if we can use unversioned `erlang` at version bump
-  url "https://github.com/emqx/emqx/archive/refs/tags/v5.1.2.tar.gz"
-  sha256 "b9ecb6275386b410e9c330b4c5b3d30acf377627d41e73c00c6460b4d8fd5f0b"
+  url "https://github.com/emqx/emqx/archive/refs/tags/v5.1.3.tar.gz"
+  sha256 "d2cfde45392b3df36cc68569a01383459bdf13107cd91fddd9da4a1e7a54b35d"
   license "Apache-2.0"
   head "https://github.com/emqx/emqx.git", branch: "master"
 
