e97c4bb82f992a1b44162638950e765a9b7d3634
Dawid Dziurla <dawidd0811@gmail.com>
2023-07-31 09:57:05 +0200

    Update ocp.rb

--- Formula/ocp.rb
+++ Formula/ocp.rb
@@ -42,8 +42,8 @@ class Ocp < Formula
   end
 
   resource "unifont" do
-    url "https://ftp.gnu.org/gnu/unifont/unifont-15.0.01/unifont-15.0.01.tar.gz"
-    sha256 "7d11a924bf3c63ea7fdf2da2b96d6d4986435bedfd1e6816c8ac2e6db47634d5"
+    url "https://ftp.gnu.org/gnu/unifont/unifont-15.0.06/unifont-15.0.06.tar.gz"
+    sha256 "36668eb1326d22e1466b94b3929beeafd10b9838bf3d41f4e5e3b52406ae69f1"
   end
 
   # patch for clockid_t redefinition issue
