6a30dcbd6d4918260b2eca4c8eec6ec4e25ecd54
Egzon Arifi <egzon.r.arifi@gmail.com>
2023-08-09 14:47:35 +0200

    Remove trailing whitespace

--- Formula/lingua.rb
+++ Formula/lingua.rb
@@ -3,11 +3,11 @@ class Lingua < Formula
   homepage "https://github.com/poviolabs/Lingua"
   url "https://github.com/poviolabs/Lingua/releases/download/0.2.5/Lingua"
   sha256 "8507508af349b68e2b4e0d636840767f2cf05371ef3f5a93e4055c34a1276ef7"
-  
+
   def install
     bin.install "Lingua"
   end
-  
+
   test do
     system "#{bin}/Lingua", "--version"
   end
