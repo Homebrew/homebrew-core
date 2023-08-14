3cc10c90d21df1ac125442ab2085b8117ff586c8
Rob Carruthers <40727678+robcarruthers@users.noreply.github.com>
2023-08-14 15:06:49 +1200

    lxi-tools 2.6 fix glib error
    This formula revision fixes an error introduced in the original formula:
    GLib-GIO-ERROR **: No GSettings schemas are installed on the system.
    We mistakenly removed the schemas to workaround an error in the brew CI.
    This revision regenerates the schema post install.
    This is simply a formula revision, not a patch release.

--- Formula/lxi-tools.rb
+++ Formula/lxi-tools.rb
@@ -4,16 +4,7 @@ class LxiTools < Formula
   url "https://github.com/lxi-tools/lxi-tools/archive/refs/tags/v2.6.tar.gz"
   sha256 "a36699387b40080ea9eb8b1abc14d843f5e7a33b3a62fcfedaea9cc54214bdc8"
   license "BSD-3-Clause"
-
-  bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "0ad1eacf1e8ae219d14d1d0584f97da77fb4f866dcb2deb63b5a64278c21c492"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "74f67748c50df2d75290b85eee36792ea4c8eb1b767a1fe81802a0aacf1d8170"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "df3254a41c62da6bfa63e8d7da78e9fb45cc2347710a3fd09dafa91e5d027a62"
-    sha256 cellar: :any_skip_relocation, ventura:        "910d48ed42f6cdf76d6166df6343309d2e50a60550a44b7137cdf9d5000916be"
-    sha256 cellar: :any_skip_relocation, monterey:       "4349534c758dad9cf5cbae6f2d799d5fb3be25887cd69d683ce921890ab55be8"
-    sha256 cellar: :any_skip_relocation, big_sur:        "0f31f8dba717b9434e3798f090f68503aa633b44a4247bdc8a13a39c24961fcc"
-    sha256                               x86_64_linux:   "4f84ffcf6baa6e46f5fa3d3555496db4f9dd3f0815a7f4faee51dae80bd77ad2"
-  end
+  revision 1
 
   depends_on "meson" => :build
   depends_on "ninja" => :build
@@ -21,6 +12,7 @@ class LxiTools < Formula
 
   depends_on "desktop-file-utils"
   depends_on "gtksourceview5"
+  depends_on "hicolor-icon-theme"
   depends_on "json-glib"
   depends_on "libadwaita"
   depends_on "liblxi"
@@ -31,6 +23,11 @@ class LxiTools < Formula
     system "meson", "compile", "-C", "build", "--verbose"
     system "meson", "install", "-C", "build"
     rm_f "#{share}/glib-2.0/schemas/gschemas.compiled"
+  end
+
+  def post_install
+    system "#{Formula["glib"].opt_bin}/glib-compile-schemas", "#{HOMEBREW_PREFIX}/share/glib-2.0/schemas"
+    system "#{Formula["gtk4"].opt_bin}/gtk4-update-icon-cache", "-f", "-t", "#{HOMEBREW_PREFIX}/share/icons/hicolor"
   end
 
   test do
