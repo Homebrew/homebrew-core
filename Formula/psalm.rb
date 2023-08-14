126230e94f7e60c3d69f5076ccf30a8a49a064d3
Sean Molenaar <SMillerDev@users.noreply.github.com>
2023-08-14 09:35:38 +0200

    psalm: update test

--- Formula/psalm.rb
+++ Formula/psalm.rb
@@ -36,7 +36,7 @@ class Psalm < Formula
         "description": "Testing if Psalm has been installed properly.",
         "type": "project",
         "require": {
-          "php": ">=8.1"
+          "php": "#{Formula["php"].version.to_s}"
         },
         "license": "MIT",
         "autoload": {
