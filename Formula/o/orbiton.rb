fedeafbd26c16f8ddf49879bd9313c66de58e7f8
Alexander F. Rødseth <rodseth@gmail.com>
2023-08-28 15:42:20 +0200

    Make adjustments after receiving feedback

--- Formula/o/orbiton.rb
+++ Formula/o/orbiton.rb
@@ -1,28 +1,29 @@
 class Orbiton < Formula
-  desc "Editor"
+  desc "Fast and configuration-free text editor and IDE limited by VT100"
   homepage "https://orbiton.zip/"
   url "https://github.com/xyproto/orbiton.git",
       tag:      "v2.64.1",
       revision: "d01e9de6baa386a08dd6fca6ad76047451fbfa18"
   license "BSD-3-Clause"
-  version_scheme 1
   head "https://github.com/xyproto/orbiton.git"
 
   depends_on "go" => :build
 
   def install
-    ENV["GOPATH"] = buildpath
-    (buildpath/"src/github.com/xyproto/orbiton").install buildpath.children
-    cd "src/github.com/xyproto/orbiton/v2" do
-      system "go", "build", "-o", "o"
-
-      bin.install "o"
-      prefix.install_metafiles
-    end
+    ENV["CXXFLAGS"] = "-Wl,--as-needed,--strip-all,--sort-common,-z,relro,-z,now"
+    system "make"
+    system "make", "install", "symlinks", "easteregg", "license", "DESTDIR=", "PREFIX=#{prefix}"
   end
 
   test do
-    output = shell_output("#{bin}/o --version")
-    assert_match(/Orbiton .*/m, output)
+    # Test 1: Check if 'o --version' output contains "Orbiton"
+    system "#{bin}/o", "--version"
+    assert_match "Orbiton", shell_output("#{bin}/o --version")
+
+     # Test 2: Check if copying and pasting a file with 'o' works
+    (testpath/"hello.txt").write "hello\n"
+    system "#{bin}/o", "-c", "#{testpath}/hello.txt" # copy the contents of hello.txt to the clipboard
+    system "#{bin}/o", "-p", "#{testpath}/hello2.txt" # paste the contents of the clipboard to hello2.txt
+    assert_equal (testpath/"hello.txt").read, (testpath/"hello2.txt").read
   end
 end
