10f88a16d018fdb1662843ea67e51c52710579fa
Alexander F. Rødseth <52813+xyproto@users.noreply.github.com>
2023-08-30 00:10:16 +0200

    Copy and paste within the same xvfb-run session on Linux

--- Formula/o/orbiton.rb
+++ Formula/o/orbiton.rb
@@ -20,8 +20,7 @@ class Orbiton < Formula
   test do
     (testpath/"hello.txt").write "hello\n"
     if OS.linux?
-      system "xvfb-run", "#{bin}/o", "--copy", "#{testpath}/hello.txt"
-      system "xvfb-run", "#{bin}/o", "--paste", "#{testpath}/hello2.txt"
+      system "xvfb-run", "sh", "-c", "#{bin}/o --copy #{testpath}/hello.txt && #{bin}/o --paste #{testpath}/hello2.txt"
     else
       system "#{bin}/o", "--copy", "#{testpath}/hello.txt"
       system "#{bin}/o", "--paste", "#{testpath}/hello2.txt"
