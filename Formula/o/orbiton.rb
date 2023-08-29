528a654cbb28b79e9501b221abceda0b4d32a66e
Alexander F. Rødseth <rodseth@gmail.com>
2023-08-29 12:05:14 +0200

    Use --copy and --paste for the test

--- Formula/o/orbiton.rb
+++ Formula/o/orbiton.rb
@@ -1,8 +1,8 @@
 class Orbiton < Formula
   desc "Fast and configuration-free text editor and IDE limited by VT100"
   homepage "https://orbiton.zip/"
-  url "https://github.com/xyproto/orbiton/archive/refs/tags/v2.64.2.tar.gz"
-  sha256 "304eddd87c85e4cdffaaa25f2bbd95e4280c8e1a3a190f4c5d962362f388e7a4"
+  url "https://github.com/xyproto/orbiton/archive/refs/tags/v2.64.3.tar.gz"
+  sha256 "f3ebdcdc7cb3502705ceda208510da1a43e65b5e0cb33e57b86564667b48303f"
   license "BSD-3-Clause"
   head "https://github.com/xyproto/orbiton.git"
 
@@ -14,8 +14,8 @@ class Orbiton < Formula
 
   test do
     (testpath/"hello.txt").write "hello\n"
-    system "#{bin}/o", "-c", "#{testpath}/hello.txt" # copy the contents of hello.txt to the clipboard
-    system "#{bin}/o", "-p", "#{testpath}/hello2.txt" # paste the contents of the clipboard to hello2.txt
+    system "#{bin}/o", "--copy", "#{testpath}/hello.txt"
+    system "#{bin}/o", "--paste", "#{testpath}/hello2.txt"
     assert_equal (testpath/"hello.txt").read, (testpath/"hello2.txt").read
   end
 end
