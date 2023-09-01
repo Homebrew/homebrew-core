0271876dd959f62babfb1ca5a8ff6a173145cc06
zunda <zunda.dev@gmail.com>
2023-09-01 15:50:44 +0900

    fix name

--- Formula/s/selene-cli.rb
+++ Formula/s/selene-cli.rb
@@ -1,12 +1,12 @@
-class Selene < Formula
+class SeleneCli < Formula
   desc "Formatting technology for Swift source code"
   homepage "https://github.com/zunda-pixel/selene"
-  url "https://github.com/zunda-pixel/selene",
+  url "https://github.com/zunda-pixel/selene.git",
       tag:      "1.1.1",
       revision: "703adbc3fced409da7ecf20ea2b7e162ff6f4986"
   license "Apache-2.0"
   version_scheme 1
-  head "https://github.com/zunda-pixel/selene", branch: "main"
+  head "https://github.com/zunda-pixel/selene.git", branch: "main"
 
   uses_from_macos "swift"
 
