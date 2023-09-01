d85fe68c2aa66e3717af9e92125e658959328cec
zunda <zunda.dev@gmail.com>
2023-09-01 14:53:03 +0900

    add selene-cli

---
+++ Formula/s/selene-cli.rb
@@ -0,0 +1,17 @@
+class Selene < Formula
+  desc "Formatting technology for Swift source code"
+  homepage "https://github.com/zunda-pixel/selene"
+  url "https://github.com/zunda-pixel/selene",
+      tag:      "1.1.1",
+      revision: "703adbc3fced409da7ecf20ea2b7e162ff6f4986"
+  license "Apache-2.0"
+  version_scheme 1
+  head "https://github.com/zunda-pixel/selene", branch: "main"
+
+  uses_from_macos "swift"
+
+  def install
+    system "swift", "build", "--disable-sandbox", "-c", "release"
+    bin.install ".build/release/selene"
+  end
+end
