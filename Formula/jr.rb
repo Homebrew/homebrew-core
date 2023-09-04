0ea06ab6c407b98fe366f126aa834dfa4a303c4b
ram-pi <pierpaolo.rametta@gmail.com>
2023-09-04 19:50:47 +0200

    Bugfixing on JR formula

--- Formula/jr.rb
+++ Formula/jr.rb
@@ -1,26 +1,21 @@
 class Jr < Formula
   desc "CLI program that helps you to create quality random data for your applications"
-  homepage "https://github.com/ugol/jr"
+  homepage "https://jrnd.io/"
   url "https://github.com/ugol/jr/archive/refs/tags/v0.3.3.tar.gz"
   sha256 "fa60365c0ca7b5ff70ef357ff362c7da069aa07a5daa8303f0af04ae75d04f67"
   license "MIT"
   head "https://github.com/ugol/jr.git", branch: "main"
 
-  livecheck do
-    url :stable
-    regex(/^v?(\d+(?:\.\d+)+)$/i)
-  end
-
-  depends_on "go" => [:build]
+  depends_on "go" => :build
 
   def install
     system "make", "all"
     bin.install Dir["build/*"]
-    prefix.install "config"
+    prefix.install "config/jrconfig.json"
     prefix.install "templates"
   end
 
   test do
-    system "#{bin}/jr"
+    assert_match "net_device", shell_output("JR_HOME=$(brew --prefix jr) jr template list").strip
   end
 end
