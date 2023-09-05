f36576e70dd4e10f94489594365527e9c960abfc
Hannes Braun <hannes@hannesbraun.net>
2023-09-05 12:28:56 +0200

    librist 0.2.8
    Also, fix build by defining `_POSIX_C_SOURCE` to `200809L`.

--- Formula/lib/librist.rb
+++ Formula/lib/librist.rb
@@ -1,10 +1,9 @@
 class Librist < Formula
   desc "Reliable Internet Stream Transport (RIST)"
   homepage "https://code.videolan.org/rist/"
-  url "https://code.videolan.org/rist/librist/-/archive/v0.2.7/librist-v0.2.7.tar.gz"
-  sha256 "7e2507fdef7b57c87b461d0f2515771b70699a02c8675b51785a73400b3c53a1"
+  url "https://code.videolan.org/rist/librist/-/archive/v0.2.8/librist-v0.2.8.tar.gz"
+  sha256 "db2dc9dd01f9ba258d728ac6c9d7a62b9fd452053a44007d08a70463870e2dd8"
   license "BSD-2-Clause"
-  revision 4
   head "https://code.videolan.org/rist/librist.git", branch: "master"
 
   livecheck do
@@ -28,6 +27,7 @@ class Librist < Formula
   depends_on "mbedtls"
 
   def install
+    ENV.append_to_cflags "-U_POSIX_C_SOURCE -D_POSIX_C_SOURCE=200809L"
     ENV.append "LDFLAGS", "-Wl,-rpath,#{rpath}"
 
     system "meson", "setup", "--default-library", "both", "-Dfallback_builtin=false", *std_meson_args, "build", "."
