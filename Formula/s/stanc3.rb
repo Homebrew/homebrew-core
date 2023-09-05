b3dab08456626671eab7a6f354da2c2b9b1a0fce
Rui Chen <rui@chenrui.dev>
2023-09-05 09:59:18 -0400

    stanc3 2.33.0

--- Formula/s/stanc3.rb
+++ Formula/s/stanc3.rb
@@ -3,8 +3,8 @@ class Stanc3 < Formula
   homepage "https://github.com/stan-dev/stanc3"
   # git is needed for dune subst
   url "https://github.com/stan-dev/stanc3.git",
-      tag:      "v2.32.2",
-      revision: "bcbf83c52c76018ce4a6cd86233de1601ddf9422"
+      tag:      "v2.33.0",
+      revision: "dc99e6fe107d09da41e8f94633d30b47d0e31d13"
   license "BSD-3-Clause"
 
   bottle do
