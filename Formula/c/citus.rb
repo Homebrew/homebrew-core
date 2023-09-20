3f31a3991fdb705302e4c4f1e20e512706378092
Rui Chen <rui@chenrui.dev>
2023-09-20 17:45:24 -0400

    citus 12.1.0
    citus: use pg 16
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/c/citus.rb
+++ Formula/c/citus.rb
@@ -1,8 +1,8 @@
 class Citus < Formula
   desc "PostgreSQL-based distributed RDBMS"
   homepage "https://www.citusdata.com"
-  url "https://github.com/citusdata/citus/archive/v12.0.0.tar.gz"
-  sha256 "9a6adaecc28e80e03a0523d07ee14c4b848f86f48ed37f84aa8cb98f3489f632"
+  url "https://github.com/citusdata/citus/archive/v12.1.0.tar.gz"
+  sha256 "cc25122ecd5717ac0b14d8cba981265d15d71cd955210971ce6f174eb0036f9a"
   license "AGPL-3.0-only"
   head "https://github.com/citusdata/citus.git", branch: "main"
 
@@ -18,14 +18,14 @@ class Citus < Formula
 
   depends_on "lz4"
   depends_on "openssl@3"
-  depends_on "postgresql@14"
+  depends_on "postgresql@16"
   depends_on "readline"
   depends_on "zstd"
 
   uses_from_macos "curl"
 
   def postgresql
-    Formula["postgresql@14"]
+    Formula["postgresql@16"]
   end
 
   def install
