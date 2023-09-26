5dbc8f17905ca28b54a6743944be638140fd4af9
Selen Taskin <setaskin@Selens-Mac-mini.guest.corp.microsoft.com>
2023-09-26 13:02:03 -0700

    update test

--- Formula/d/devtunnel.rb
+++ Formula/d/devtunnel.rb
@@ -10,6 +10,16 @@ class Devtunnel < Formula
   end
 
   test do
-    system "false"
+    #system "false"
+    resource("testdata") do
+      url "https://tunnelsassetsprod.blob.core.windows.net/cli/1.0.922+fa34179138/osx-x64-devtunnel-zip"
+      sha256 "d9875469d96c563302cb68d1bd6834bde6ba949ace57714a86ecd0108073da1e"
+    end
+    
+    test do
+      resource("testdata").stage do
+        assert_match "OK", shell_output("#{bin}/devtunnel build-devtunnel input.devtunnel")
+      end
+    end
   end
 end
