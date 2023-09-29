97f3893e0e54f3c812a35191b40e8078db80bdfa
Rui Chen <rui@chenrui.dev>
2023-09-29 12:58:30 +0800

    vulkan-tools 1.3.265
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/v/vulkan-tools.rb
+++ Formula/v/vulkan-tools.rb
@@ -1,8 +1,8 @@
 class VulkanTools < Formula
   desc "Vulkan utilities and tools"
   homepage "https://github.com/KhronosGroup/Vulkan-Tools"
-  url "https://github.com/KhronosGroup/Vulkan-Tools/archive/refs/tags/v1.3.263.tar.gz"
-  sha256 "afd5709f54c6d224dd7f2d9aef1fb931b5f275bfd4cc5e265fd47be4898b5277"
+  url "https://github.com/KhronosGroup/Vulkan-Tools/archive/refs/tags/v1.3.265.tar.gz"
+  sha256 "f212a0f7f5b5adc5b64acc060e461a48e0c3cf6a0984a9f5b0621db52d02d133"
   license "Apache-2.0"
   head "https://github.com/KhronosGroup/Vulkan-Tools.git", branch: "main"
 
