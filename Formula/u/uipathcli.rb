7bd19b4dffa768895b746313186b0d2b235bf8af
Constantin Muraru <constantin.muraru@uipath.com>
2023-09-30 16:35:25 +0300

    Implement review

--- Formula/u/uipathcli.rb
+++ Formula/u/uipathcli.rb
@@ -1,37 +1,20 @@
 class Uipathcli < Formula
   desc "Command-line to simplify, script and automate API calls to UiPath services"
   homepage "https://github.com/UiPath/uipathcli"
-  url "https://github.com/UiPath/uipathcli.git",
-      tag:      "v1.0.87",
-      revision: "17a981bebaa7095ff2b3afd3231223fceb69b8ed"
+  url "https://github.com/UiPath/uipathcli/archive/refs/tags/v1.0.87.tar.gz"
+  sha256 "7f512fd2633eee722bb3b7f58c9b2bdaf0e47af8049cfbcbd258b80557ad7602"
   license "MIT"
 
   depends_on "go" => :build
 
   def install
     system "go", "build", *std_go_args(ldflags: "-s -w", output: bin/"uipath")
-
-    mv "definitions", "uipath-definitions"
-    etc.install "uipath-definitions"
-  end
-
-  def caveats
-    <<~EOS
-      You may want to add the following to your .bash_profile:
-        export UIPATH_DEFINITIONS_PATH="#{etc}/uipath-definitions"
-
-      You can enable autocompletion by running the following command:
-        uipath autocomplete enable --shell "bash"
-
-      You can run the interactive config command to finish setting up your CLI:
-        uipath config
-
-      For more information you can read the documentation: https://github.com/UiPath/uipathcli#readme
-    EOS
+    bin.install "definitions"
+    system "#{bin}/uipath", "autocomplete", "enable", "--shell", "bash"
   end
 
   test do
-    ENV["UIPATH_DEFINITIONS_PATH"] = "#{etc}/uipath-definitions"
-    system "#{bin}/uipath", "--help"
+    output = `#{bin}/uipath orchestrator users get 2>&1`
+    assert_match "Missing organization parameter!", output
   end
 end
