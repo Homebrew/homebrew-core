7643902c5650c2b92b1bdac62a7efed265b79090
Constantin Muraru <constantin.muraru@uipath.com>
2023-10-05 13:56:52 +0300

    Use latest version which has the definitions embedded in the executable

--- Formula/u/uipathcli.rb
+++ Formula/u/uipathcli.rb
@@ -1,15 +1,15 @@
 class Uipathcli < Formula
   desc "Command-line to simplify, script and automate API calls to UiPath services"
   homepage "https://github.com/UiPath/uipathcli"
-  url "https://github.com/UiPath/uipathcli/archive/refs/tags/v1.0.87.tar.gz"
-  sha256 "7f512fd2633eee722bb3b7f58c9b2bdaf0e47af8049cfbcbd258b80557ad7602"
+  url "https://github.com/UiPath/uipathcli.git",
+      tag:      "v1.0.90",
+      revision: "66f9370f6c1d92315d7a045d837d74a56e08748b"
   license "MIT"
 
   depends_on "go" => :build
 
   def install
     system "go", "build", *std_go_args(ldflags: "-s -w", output: bin/"uipath")
-    bin.install "definitions"
     system "#{bin}/uipath", "autocomplete", "enable", "--shell", "bash"
   end
 
