c7afb74a4c7e04d9611d8e36bbcec97de7f9c892
Greg Tonoski <111286121+GregTonoski@users.noreply.github.com>
2023-10-05 16:35:55 +0200

    Update bip39-xor.rb with URL with version, shorten test, capitalize letters in class name

--- Formula/b/bip39-xor.rb
+++ Formula/b/bip39-xor.rb
@@ -1,12 +1,12 @@
-class bip39-xor < Formula
+class BIP39-XOR < Formula
   
-  desc "Encrypt or decrypt 12, 15, 18, 21 or 24 BIP39 codewords array (so-called "seed phrase") using exclusive OR (XOR)/Vernam cipher (a.k.a. One Time Pad)."
+  desc "Encrypt or decrypt 12, 15, 18, 21 or 24 BIP39 codewords array (so-called seed phrase) using exclusive OR (XOR)/Vernam cipher (a.k.a. One Time Pad)."
   homepage "https://github.com/GregTonoski/BIP39-XOR"
-  url "https://github.com/GregTonoski/BIP39-XOR/blob/main/BIP39-XOR.bash"
+  url "https://github.com/GregTonoski/BIP39-XOR/releases/download/v2.0.0/BIP39-XOR.bash"
   sha256 "b7f4c585b3ab732e7220fb59c252a645334d0f34069b8ff7c2fe6c95b1968b46"
   license :public_domain
 
   test do
-    assert_match "age age age age age age age age age age age used XOR track turtle shy top rude vanish fuel unique final child camp steak", shell_output("#{bin}/bash -c \"BIP39-XOR.bash time until select then return void float true false case catch depart XOR age age age age age age age age age age age used\")
+    assert_match "*XOR*", shell_output("#{bin}/bash -c \"BIP39-XOR.bash age age age age age age age age age age age used\")
   end
 end
