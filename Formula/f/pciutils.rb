254a4ddc96ad56a7e1ed7d5145d894e18a59721e
Carter Li <CarterLi@users.noreply.github.com>
2023-10-19 10:47:45 +0800

    pciutils 3.10.0 (new formula)
    The PCI Utilities package contains a library for portable access to PCI bus configuration registers and several utilities based on this library.
    
    Pciutils is commonly used in Linux. [fastfetch](https://github.com/CarterLi/homebrew-core/blob/master/Formula/f/fastfetch.rb) uses it to detect GPUs in Linux.

---
+++ Formula/f/pciutils.rb
@@ -0,0 +1,17 @@
+class Pciutils < Formula
+  desc "PCI utilities"
+  homepage "https://github.com/pciutils/pciutils"
+  url "https://github.com/pciutils/pciutils/archive/refs/tags/v3.10.0.tar.gz"
+  sha256 "e579d87f1afe2196db7db648857023f80adb500e8194c4488c8b47f9a238c1c6"
+  license "GPL-2.0-or-later"
+
+  uses_from_macos "zlib"
+
+  def install
+    system "make", "install", "ZLIB=yes", "DNS=yes", "PREFIX=#{prefix}", "MANDIR=#{man}"
+  end
+
+  test do
+    assert_match "lspci version", shell_output("#{bin}/lspci --version")
+  end
+end
