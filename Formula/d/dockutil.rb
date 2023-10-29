090e062e6af2ecc11617f6efa5de2ff9dc47d8a2
Daniel Bayley <daniel.bayley@me.com>
2023-10-29 22:48:35 +0000

    dockutil: undeprecate

--- Formula/d/dockutil.rb
+++ Formula/d/dockutil.rb
@@ -9,10 +9,6 @@ class Dockutil < Formula
     sha256 cellar: :any_skip_relocation, all: "f5f87d9e286c2b294bb157ac9f87baa2720fff044c7a92c0b80b9cb82db8a87e"
   end
 
-  # https://github.com/kcrawford/dockutil/pull/131
-  # https://github.com/Homebrew/homebrew-core/pull/97394
-  deprecate! date: "2023-09-03", because: :does_not_build
-
   depends_on :macos
 
   def install
