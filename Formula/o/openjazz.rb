998350a27ab2fbccdb94217774c4b625cb4f8a36
Sean Molenaar <SMillerDev@users.noreply.github.com>
2023-10-29 18:32:02 +0100

    openjazz: use cmake and clean up formula

--- Formula/o/openjazz.rb
+++ Formula/o/openjazz.rb
@@ -19,8 +19,7 @@ class Openjazz < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "a50f5226a5b83bd7f316b4f1546124fcf1b84cb99a18909a6e9551662a5a17b1"
   end
 
-  depends_on "autoconf" => :build
-  depends_on "automake" => :build
+  depends_on "cmake" => :build
   depends_on "pkg-config" => :build
   depends_on "libmodplug"
   depends_on "sdl12-compat"
@@ -36,19 +35,11 @@ class Openjazz < Formula
     sha256 "ed025415c0bc5ebc3a41e7a070551bdfdfb0b65b5314241152d8bd31f87c22da"
   end
 
-  # MSG_NOSIGNAL is only defined in Linux
-  # https://github.com/AlisterT/openjazz/pull/7
-  patch :DATA
-
   def install
-    # the libmodplug include paths in the source don't include the libmodplug directory
-    ENV.append_to_cflags "-I#{Formula["libmodplug"].opt_include}/libmodplug"
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
 
-    system "autoreconf", "-ivf"
-    system "./configure", "--prefix=#{prefix}",
-                          "--bindir=#{pkgshare}",
-                          "--disable-dependency-tracking"
-    system "make", "install"
 
     # Default game lookup path is the OpenJazz binary's location
     (bin/"OpenJazz").write <<~EOS
@@ -69,20 +60,8 @@ class Openjazz < Formula
         #{pkgshare}
     EOS
   end
-end
 
-__END__
-diff --git a/src/io/network.cpp b/src/io/network.cpp
-index 8af8775..362118e 100644
---- a/src/io/network.cpp
-+++ b/src/io/network.cpp
-@@ -53,6 +53,9 @@
-		#include <errno.h>
-		#include <string.h>
-	#endif
-+ 	#ifdef __APPLE__
-+ 		#define MSG_NOSIGNAL SO_NOSIGPIPE
-+    #endif
- #elif defined USE_SDL_NET
-	#include <arpa/inet.h>
- #endif
+  test do
+    system bin/"OpenJazz", "--version"
+  end
+end
