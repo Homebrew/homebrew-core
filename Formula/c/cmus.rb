3add60ff67e1fc3535bde7990f8a406f3ad89ac3
Rui Chen <rui@chenrui.dev>
2023-11-07 10:46:29 -0500

    cmus: revision bump (faad2 2.11.0)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/c/cmus.rb
+++ Formula/c/cmus.rb
@@ -4,7 +4,7 @@ class Cmus < Formula
   url "https://github.com/cmus/cmus/archive/refs/tags/v2.10.0.tar.gz"
   sha256 "ff40068574810a7de3990f4f69c9c47ef49e37bd31d298d372e8bcdafb973fff"
   license "GPL-2.0-or-later"
-  revision 2
+  revision 3
   head "https://github.com/cmus/cmus.git", branch: "master"
 
   bottle do
