f3e8a034d5e3717716c81a2afff7310fefaf1fd3
Keith Moore <kmoore@clostra.com>
2023-11-11 02:39:41 -0500

    newnode-helper 1.4.2a don't build except on macos

--- Formula/n/newnode-helper.rb
+++ Formula/n/newnode-helper.rb
@@ -6,6 +6,7 @@ class NewnodeHelper < Formula
       revision: "c42a04ded55cfdff857878f4d9234950e530bb00"
   license "GPL-2.0-only"
 
+  depends_on macos: :high_sierra
   depends_on xcode: ["9.3", :build]
   depends_on "automake"
   depends_on "coreutils"
