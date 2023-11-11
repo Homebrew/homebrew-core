2a0d144316df0afd593a9fda8d8c2301ecd4a391
David Lane <git2018@dmlane.net>
2023-11-11 17:49:29 +0100

    crudini 0.9.5

--- Formula/c/crudini.rb
+++ Formula/c/crudini.rb
@@ -13,6 +13,7 @@ class Crudini < Formula
     url "https://files.pythonhosted.org/packages/71/39/171f1c67cd00715f190ba0b100d606d440a28c93c7714febeca8b79af85e/six-1.16.0.tar.gz"
     sha256 "1e61c37477a1626458e36f7b1d82aa5c9b094fa4802892072e49de9c60c4c926"
   end
+
   def install
     virtualenv_install_with_resources
   end
