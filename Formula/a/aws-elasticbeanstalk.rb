0952125788b5a40a28a5ebbd08ae62a4bc6ed66f
Michael Cho <michael@michaelcho.dev>
2023-11-11 22:26:28 -0500

    aws-elasticbeanstalk: migrate to `python@3.12`

--- Formula/a/aws-elasticbeanstalk.rb
+++ Formula/a/aws-elasticbeanstalk.rb
@@ -26,7 +26,7 @@ class AwsElasticbeanstalk < Formula
   depends_on "python-certifi"
   depends_on "python-cryptography"
   depends_on "python-packaging"
-  depends_on "python@3.11"
+  depends_on "python@3.12"
   depends_on "pyyaml"
   depends_on "six"
 
