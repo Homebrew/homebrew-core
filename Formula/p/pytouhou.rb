9e46281cae73b659f218bd8f057221587089b4f2
Michael Cho <michael@michaelcho.dev>
2023-11-15 00:38:13 -0500

    pytouhou: migrate to `python@3.12`

--- Formula/p/pytouhou.rb
+++ Formula/p/pytouhou.rb
@@ -27,7 +27,7 @@ class Pytouhou < Formula
   depends_on "libepoxy"
   depends_on "py3cairo"
   depends_on "pygobject3"
-  depends_on "python@3.11"
+  depends_on "python@3.12"
   depends_on "sdl2"
   depends_on "sdl2_image"
   depends_on "sdl2_mixer"
@@ -38,7 +38,7 @@ class Pytouhou < Formula
   patch :p0, :DATA
 
   def install
-    python = "python3.11"
+    python = "python3.12"
     ENV.prepend_path "PYTHONPATH", Formula["libcython"].opt_libexec/Language::Python.site_packages(python)
 
     # hg can't determine revision number (no .hg on the stage)
