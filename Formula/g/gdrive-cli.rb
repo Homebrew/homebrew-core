af2a495ad204fe801d71aea7ed97f15c721951cc
indianets <vijay@indianets.com>
2023-11-20 23:30:43 +0530

    gdrive-cli 3.9.0 (new formula)

--- Formula/g/gdrive-cli.rb
+++ Formula/g/gdrive-cli.rb
@@ -1,22 +1,23 @@
 class GdriveCli < Formula
   desc "Google Drive CLI Client"
   homepage "https://github.com/glotlabs/gdrive"
-  url "https://github.com/glotlabs/gdrive/releases/download/3.9.0/gdrive_macos-x64.tar.gz"
-  sha256 "348f57f42785e3eedfb933bcf079deba8cf51ac2cf699d29539f33956d025d57"
+  url "https://github.com/glotlabs/gdrive/archive/refs/tags/3.9.0.tar.gz"
+  sha256 "a4476480f0cf759f6a7ac475e06f819cbebfe6bb6f1e0038deff1c02597a275a"
   license "MIT"
   head "https://github.com/glotlabs/gdrive.git", branch: "main"
 
-  bottle do
-    sha256 cellar: :any_skip_relocation, all: "aedaf65cf84ca164aa46149a14df2cab9a3ff9d7679fea4621442170839f3e0b"
-  end
+  depends_on "rust" => :build
 
   conflicts_with "gdrive", because: "gdrive also ships a gdrive binary"
 
   def install
-    bin.install "gdrive"
+    system "cargo", "install", *std_cargo_args
   end
 
   test do
-    assert_match "gdrive 3.9.0", shell_output("#{bin}/gdrive version")
+    assert_match "Usage: gdrive <COMMAND>", shell_output("#{bin}/gdrive")
+    assert_match version.to_s, shell_output("#{bin}/gdrive version")
+    assert_match "Error: No accounts found", shell_output("#{bin}/gdrive account list")
+    assert_match "Error: No account has been selected", shell_output("#{bin}/gdrive files list")
   end
 end
