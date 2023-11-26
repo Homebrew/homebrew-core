eed8cbbbf0d97d9757c4c6255a72b2b38351fe34
Rui Chen <rui@chenrui.dev>
2023-11-26 01:12:10 -0500

    python-soupsieve 2.5 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/p/python-soupsieve.rb
@@ -0,0 +1,29 @@
+class PythonSoupsieve < Formula
+  desc "Modern CSS selector implementation for BeautifulSoup"
+  homepage "https://facelessuser.github.io/soupsieve/"
+  url "https://files.pythonhosted.org/packages/ce/21/952a240de1c196c7e3fbcd4e559681f0419b1280c617db21157a0390717b/soupsieve-2.5.tar.gz"
+  sha256 "5663d5a7b3bfaeee0bc4372e7fc48f9cff4940b3eec54a6451cc5299f1097690"
+  license "MIT"
+
+  depends_on "python-hatchling" => :build
+  depends_on "python@3.11" => [:build, :test]
+  depends_on "python@3.12" => [:build, :test]
+
+  def pythons
+    deps.map(&:to_formula).sort_by(&:version).filter { |f| f.name.start_with?("python@") }
+  end
+
+  def install
+    pythons.each do |python|
+      python_exe = python.opt_libexec/"bin/python"
+      system python_exe, "-m", "pip", "install", *std_pip_args, "."
+    end
+  end
+
+  test do
+    pythons.each do |python|
+      python_exe = python.opt_libexec/"bin/python"
+      system python_exe, "-c", "import soupsieve"
+    end
+  end
+end
