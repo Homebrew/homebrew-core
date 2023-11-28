7b532b375669d123bc2138060c71187b90058977
Patrick Linnane <patrick@linnane.io>
2023-11-28 10:09:16 -0800

    minuit2 6.30.02
    minuit2: remove build patch
    
    minuit2: requires C++17

--- Formula/m/minuit2.rb
+++ Formula/m/minuit2.rb
@@ -1,13 +1,11 @@
 class Minuit2 < Formula
   desc "Physics analysis tool for function minimization"
   homepage "https://root.cern.ch/doc/master/Minuit2Page.html"
-  url "https://root.cern.ch/download/root_v6.28.06.source.tar.gz"
-  sha256 "af3b673b9aca393a5c9ae1bf86eab2672aaf1841b658c5c6e7a30ab93c586533"
+  url "https://root.cern.ch/download/root_v6.30.02.source.tar.gz"
+  sha256 "7965a456d1ad1ee0d5fe4769bf5a8fec291af684ed93db0f3080a9c362435183"
   license "LGPL-2.1-or-later"
   head "https://github.com/root-project/root.git", branch: "master"
-  # Should be removed with new version
-  # https://github.com/root-project/root/commit/ef61c5a6dff06dacff74fc2e15856bbf80b19032
-  # https://github.com/root-project/root/commit/01d07d5b33ee91cd7fd6c6e615cd4c940710c397
+
   livecheck do
     formula "root"
   end
@@ -24,22 +22,17 @@ class Minuit2 < Formula
 
   depends_on "cmake" => :build
 
-  # Fixes missing header MnMatrixfwd.h
-  # Remove in next release
-  patch do
-    url "https://github.com/root-project/root/commit/ef61c5a6dff06dacff74fc2e15856bbf80b19032.patch?full_index=1"
-    sha256 "25a5e3fa2846c83e824bd0a21003658f4c03b6096aa52573179c8aea6228a604"
-  end
+  fails_with gcc: "5" # C++17
 
   def install
     system "cmake", "-S", "math/minuit2", "-B", "build/shared", *std_cmake_args,
-                    "-Dminuit2_standalone=ON", "-DCMAKE_CXX_FLAGS='-std=c++14'", "-DBUILD_SHARED_LIBS=ON",
+                    "-Dminuit2_standalone=ON", "-DCMAKE_CXX_FLAGS='-std=c++17'", "-DBUILD_SHARED_LIBS=ON",
                     "-DCMAKE_INSTALL_RPATH=#{rpath}"
     system "cmake", "--build", "build/shared"
     system "cmake", "--install", "build/shared"
 
     system "cmake", "-S", "math/minuit2", "-B", "build/static", *std_cmake_args,
-                    "-Dminuit2_standalone=ON", "-DCMAKE_CXX_FLAGS='-std=c++14'", "-DBUILD_SHARED_LIBS=OFF"
+                    "-Dminuit2_standalone=ON", "-DCMAKE_CXX_FLAGS='-std=c++17'", "-DBUILD_SHARED_LIBS=OFF"
     system "cmake", "--build", "build/static"
     lib.install Dir["build/static/lib/libMinuit2*.a"]
 
@@ -48,7 +41,7 @@ class Minuit2 < Formula
 
   test do
     cp Dir[pkgshare/"MnTutorial/{Quad1FMain.cxx,Quad1F.h}"], testpath
-    system ENV.cxx, "-std=c++14", "Quad1FMain.cxx", "-o", "test", "-I#{include}/Minuit2", "-L#{lib}", "-lMinuit2"
+    system ENV.cxx, "-std=c++17", "Quad1FMain.cxx", "-o", "test", "-I#{include}/Minuit2", "-L#{lib}", "-lMinuit2"
     assert_match "par0: -8.26907e-11 -1 1", shell_output("./test")
   end
 end
