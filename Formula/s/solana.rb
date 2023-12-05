696e9ed74e3539b919d6bf7c1ab775cfdc9767e6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-12-05 01:03:26 +0000

    solana 1.16.21

--- Formula/s/solana.rb
+++ Formula/s/solana.rb
@@ -1,8 +1,8 @@
 class Solana < Formula
   desc "Web-Scale Blockchain for decentralized apps and marketplaces"
   homepage "https://solana.com"
-  url "https://github.com/solana-labs/solana/archive/refs/tags/v1.16.19.tar.gz"
-  sha256 "4c5b3d42bdfef923c8b17c7edeb83f2a15bceb6264d8c39311651c1c4a9d491c"
+  url "https://github.com/solana-labs/solana/archive/refs/tags/v1.16.21.tar.gz"
+  sha256 "4b81e6c3c1458a17394f2a67d39a5a9af17501eef0f6dce3cff1296e8155b03f"
   license "Apache-2.0"
   version_scheme 1
 
