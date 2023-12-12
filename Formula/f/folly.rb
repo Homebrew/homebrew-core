7566904cfa896b816b31eff863c339d20880befd
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-12-12 11:27:35 -0500

    folly 2023.12.11.00

--- Formula/f/folly.rb
+++ Formula/f/folly.rb
@@ -1,8 +1,8 @@
 class Folly < Formula
   desc "Collection of reusable C++ library artifacts developed at Facebook"
   homepage "https://github.com/facebook/folly"
-  url "https://github.com/facebook/folly/archive/refs/tags/v2023.12.04.00.tar.gz"
-  sha256 "6b13903f058fbb795d3c41124fe9df0da6dc07470348868dedafff3c2b106033"
+  url "https://github.com/facebook/folly/archive/refs/tags/v2023.12.11.00.tar.gz"
+  sha256 "1ff0c0258f8322a818a6e0cd27c0fc965360dc04af308e59349e1c79966190a1"
   license "Apache-2.0"
   head "https://github.com/facebook/folly.git", branch: "main"
 
