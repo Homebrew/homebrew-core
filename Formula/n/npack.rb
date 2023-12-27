cd6a8cb4ef4796f641768e6517bbdef574bad4bf
zhazhazhu <fangyuan.leslie@qq.com>
2023-12-27 20:45:44 +0800

    npack 0.0.8

---
+++ Formula/n/npack.rb
@@ -0,0 +1,19 @@
+class Npack < Formula
+  desc "Use the right package manager (Rust)"
+  homepage "https://github.com/zhazhazhu/ni"
+  url "https://github.com/zhazhazhu/ni/archive/refs/tags/0.0.8.tar.gz"
+  sha256 "2f953a670317860eacf75e3f503e6b6cdc1ed9b1b6638a541006b7af672685e3"
+  license "MIT"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+
+    generate_completions_from_executable(bin/"ni", "completions", "--shell")
+  end
+
+  test do
+    system "#{bin}/ni", "--version"
+  end
+end
