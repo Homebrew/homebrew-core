d8e65fe684a0ce333fc8a3e44ab250f54f6b1fb5
Dustin Rodrigues <dust.rod@gmail.com>
2023-12-27 20:00:46 -0500

    openclonk: remove patches

--- Formula/o/openclonk.rb
+++ Formula/o/openclonk.rb
@@ -13,22 +13,6 @@ class Openclonk < Formula
       depends_on "gtk+3"
       depends_on "libx11"
     end
-
-    # Fix macOS OpenGL initialization. Remove in release after v8.1.
-    patch do
-      url "https://github.com/openclonk/openclonk/commit/1487ff02273975e9a7ea5f3046884c2ebf254333.patch?full_index=1"
-      sha256 "d42821d13bc698435058fbd9135c5e8eb178c835afbc8fca8c6b76bdecbcc563"
-    end
-
-    # Fix Linux build to help find alut.h. Remove in next release.
-    patch do
-      url "https://github.com/openclonk/openclonk/commit/15e58576894f5735af390164eb15e609344f9331.patch?full_index=1"
-      sha256 "af84dc82b85c2b2ae63738badf604745002ddeb2e285fb7911b7ccdf5d4497eb"
-    end
-
-    # Fix build failure with newer GCC because of missing #include <limits>
-    # TODO: submit to upstream GitHub repo
-    patch :DATA
   end
 
   livecheck do
@@ -108,17 +92,3 @@ class Openclonk < Formula
     system bin/"c4group"
   end
 end
-
-__END__
-diff --git a/src/gui/C4ScriptGuiWindow.cpp b/src/gui/C4ScriptGuiWindow.cpp
-index 785e168..1d2c467 100755
---- a/src/gui/C4ScriptGuiWindow.cpp
-+++ b/src/gui/C4ScriptGuiWindow.cpp
-@@ -42,6 +42,7 @@
- #include <C4Viewport.h>
-
- #include <cmath>
-+#include <limits>
-
- // Adds some helpful logs for hunting control & menu based desyncs.
- //#define MenuDebugLogF(...) DebugLogF(__VA_ARGS__)
