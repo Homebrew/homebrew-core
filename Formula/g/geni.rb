28ee3fc242bc77bb0b03da50506ed479e15d0a7c
Emil Privér <emil@priver.dev>
2023-12-28 12:26:55 +0100

    removed v from version

--- Formula/g/geni.rb
+++ Formula/g/geni.rb
@@ -2,7 +2,7 @@ class Geni < Formula
   desc "Database CLI migration tool"
   homepage "https://github.com/emilpriver/geni"
   url "https://github.com/emilpriver/geni/archive/refs/tags/v0.0.1.tar.gz"
-  version "v0.0.1"
+  version "0.0.1"
   sha256 "6ece9e572aaee2c0555b7c5e880fe7a63469397b871d6af8467d59ea868b5351"
   license "MIT"
 
