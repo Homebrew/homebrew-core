c7d6376ef46120518a530e2699e90a1cac47f26f
Mikl Wolfe <wolfemikl@gmail.com>
2023-12-31 09:18:00 -0700

    vulkan-headers 1.3.274

--- Formula/v/vulkan-headers.rb
+++ Formula/v/vulkan-headers.rb
@@ -1,8 +1,8 @@
 class VulkanHeaders < Formula
   desc "Vulkan Header files and API registry"
   homepage "https://github.com/KhronosGroup/Vulkan-Headers"
-  url "https://github.com/KhronosGroup/Vulkan-Headers/archive/refs/tags/v1.3.268.tar.gz"
-  sha256 "d5c59d5fc3ab264006dfea1eb1a11f609ea5dfa8319a5aaca061007828012a78"
+  url "https://github.com/KhronosGroup/Vulkan-Headers/archive/refs/tags/v1.3.274.tar.gz"
+  sha256 "3458dd9049d561d0863069b1dd752cd4a04ca31fc090a58124691d61bff5b62a"
   license "Apache-2.0"
 
   bottle do
