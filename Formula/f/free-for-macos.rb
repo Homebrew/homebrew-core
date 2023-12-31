e4eaabcbab0523f96d17483688a25fbc6eca8eee
dantifer <dantifer@gmail.com>
2023-12-31 12:19:35 +0800

    free-for-macos 1.0
    This formular will add 'free' command line tool for macos, just like 'free' in linux

---
+++ Formula/f/free-for-macos.rb
@@ -0,0 +1,23 @@
+class FreeForMacos < Formula
+  desc "Command-line memory status tool for macOS, similar to free on Linux"
+  homepage "https://github.com/zfdang/free-for-macOS"
+  url "https://github.com/zfdang/free-for-macOS/archive/refs/tags/v1.0.tar.gz"
+  sha256 "1c2935d57c144951bd2d9133519d5482ad5e1fdbece15d2ec336c9704222f859"
+  license "MIT"
+
+  depends_on "cmake" => :build
+
+  def install
+    cflags = %q(-O2 -Wall -std=c99 -D_FREE_VERSION="\"${VER}\"")
+
+    system "make", "CFLAGS=#{cflags}"
+    system "gzip -c free.1 > free.1.gz"
+
+    bin.install "free"
+    man1.install "free.1.gz"
+  end
+
+  test do
+    system "#{bin}/free", "-v"
+  end
+end
