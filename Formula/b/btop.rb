112d20f7f5abc6c181a11c3ddd033e5df3857e3d
Porkepix <porkepix@gmail.com>
2024-01-07 16:46:52 +0100

    btop 1.3.0

--- Formula/b/btop.rb
+++ Formula/b/btop.rb
@@ -1,8 +1,8 @@
 class Btop < Formula
   desc "Resource monitor. C++ version and continuation of bashtop and bpytop"
   homepage "https://github.com/aristocratos/btop"
-  url "https://github.com/aristocratos/btop/archive/refs/tags/v1.2.13.tar.gz"
-  sha256 "668dc4782432564c35ad0d32748f972248cc5c5448c9009faeb3445282920e02"
+  url "https://github.com/aristocratos/btop/archive/refs/tags/v1.3.0.tar.gz"
+  sha256 "375e078ce2091969f0cd14030620bd1a94987451cf7a73859127a786006a32cf"
   license "Apache-2.0"
   head "https://github.com/aristocratos/btop.git", branch: "main"
 
