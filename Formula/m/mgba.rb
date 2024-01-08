b2a6a208cea1e34a4bff21e2fed5f10b83f34774
Porkepix <porkepix@gmail.com>
2024-01-08 14:36:36 +0100

    mgba 0.10.3

--- Formula/m/mgba.rb
+++ Formula/m/mgba.rb
@@ -1,8 +1,8 @@
 class Mgba < Formula
   desc "Game Boy Advance emulator"
   homepage "https://mgba.io/"
-  url "https://github.com/mgba-emu/mgba/archive/refs/tags/0.10.2.tar.gz"
-  sha256 "60afef8fb79ba1f7be565b737bae73c6604a790391c737f291482a7422d675ae"
+  url "https://github.com/mgba-emu/mgba/archive/refs/tags/0.10.3.tar.gz"
+  sha256 "be2cda7de3da8819fdab0c659c5cd4c4b8ca89d9ecddeeeef522db6d31a64143"
   license "MPL-2.0"
   head "https://github.com/mgba-emu/mgba.git", branch: "master"
 
