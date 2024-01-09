b2e505139d528f93d915290d844096de0169c0ed
Timothy Sutton <tim@synthist.net>
2024-01-08 20:25:58 -0500

    fizz 2024.01.08.00

--- Formula/f/fizz.rb
+++ Formula/f/fizz.rb
@@ -1,8 +1,8 @@
 class Fizz < Formula
   desc "C++14 implementation of the TLS-1.3 standard"
   homepage "https://github.com/facebookincubator/fizz"
-  url "https://github.com/facebookincubator/fizz/releases/download/v2023.12.04.00/fizz-v2023.12.04.00.tar.gz"
-  sha256 "5336426e6ce3b90f77eabec1e19637d35505534e74ef46f455b39d50f7bbff49"
+  url "https://github.com/facebookincubator/fizz/releases/download/v2024.01.08.00/fizz-v2024.01.08.00.tar.gz"
+  sha256 "ec2dc1b2238e654e8b40b63157cfe343f44e05a59da53e095aadd5e4ec627144"
   license "BSD-3-Clause"
   head "https://github.com/facebookincubator/fizz.git", branch: "main"
 
