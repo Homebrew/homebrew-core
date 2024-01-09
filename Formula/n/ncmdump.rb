69aca5592f1eb1edba987896b08b1b0c8367f450
TaurusXin <taurusxin@outlook.com>
2024-01-09 10:01:12 +0000

    ncmdump 1.2.1 (new formula)

--- Formula/n/ncmdump.rb
+++ Formula/n/ncmdump.rb
@@ -2,7 +2,7 @@ class Ncmdump < Formula
   desc "Convert Netease Cloud Music ncm files to mp3/flac files"
   homepage "https://github.com/taurusxin/ncmdump"
   url "https://github.com/taurusxin/ncmdump/archive/refs/tags/1.2.1.tar.gz"
-  sha256 "dfb2427acad26f789ed576c19e892fc1b0332e26b55f956a1928dfe42c110b6f"
+  sha256 "a1bd97fd1b46f9ba4ffaac0cf6cf1e920b49bf6ec753870ad0e6e07a72c2de2d"
   license "MIT"
 
   depends_on "taglib"
