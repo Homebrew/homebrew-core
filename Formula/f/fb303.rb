c6d1460b73d18bd786b09162edd5a56bdd70b9a4
Timothy Sutton <tim@synthist.net>
2024-01-08 20:25:38 -0500

    fb303 2024.01.08.00

--- Formula/f/fb303.rb
+++ Formula/f/fb303.rb
@@ -1,8 +1,8 @@
 class Fb303 < Formula
   desc "Thrift functions for querying information from a service"
   homepage "https://github.com/facebook/fb303"
-  url "https://github.com/facebook/fb303/archive/refs/tags/v2023.12.04.00.tar.gz"
-  sha256 "8ecef8c934bef17cded7231a32771a3819166fb46bc5bdb59eb72aea903a0fb6"
+  url "https://github.com/facebook/fb303/archive/refs/tags/v2024.01.08.00.tar.gz"
+  sha256 "b0b70695da62e3df1d2afd8b0d5ef58547960b92110dc3accb06e98c006d7704"
   license "Apache-2.0"
   head "https://github.com/facebook/fb303.git", branch: "main"
 
