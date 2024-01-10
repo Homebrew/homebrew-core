6c5f486f79c8a34211db9a31576ecf343519cad1
Michka Popoff <michkapopoff@gmail.com>
2024-01-10 22:01:37 +0100

    fastqc: fix test failure by compiling from source

--- Formula/f/fastqc.rb
+++ Formula/f/fastqc.rb
@@ -1,9 +1,10 @@
 class Fastqc < Formula
   desc "Quality control tool for high throughput sequence data"
   homepage "https://www.bioinformatics.babraham.ac.uk/projects/fastqc/"
-  url "https://www.bioinformatics.babraham.ac.uk/projects/fastqc/fastqc_v0.12.1.zip"
-  sha256 "5f4dba8780231a25a6b8e11ab2c238601920c9704caa5458d9de559575d58aa7"
+  url "https://github.com/s-andrews/FastQC/archive/refs/tags/v0.12.1.tar.gz"
+  sha256 "cad8bfd11ecf388204c28a62b162dda032aac8ecd08819c49b053aa66613e92b"
   license "GPL-3.0-or-later"
+  revision 1
 
   livecheck do
     url "https://www.bioinformatics.babraham.ac.uk/projects/download.html"
@@ -14,10 +15,19 @@ class Fastqc < Formula
     sha256 cellar: :any_skip_relocation, all: "98115fa6cc5acc2f8ac59e42843802c5e4d1484be2b030edbeafc32ccdac89f6"
   end
 
+  depends_on "ant" => :build
   depends_on "openjdk"
 
+  # https://github.com/s-andrews/FastQC/pull/109
+  # Remove with next release
+  patch do
+    url "https://github.com/s-andrews/FastQC/commit/d4f2e91d67b765867b59cde3c6a9a6c7de34ae37.patch?full_index=1"
+    sha256 "0ccf32805c574e72823d5a22a6d7cf99eeac74fc5779ff6ae695ce5d32b7094a"
+  end
+
   def install
-    libexec.install Dir["*"]
+    system "ant"
+    libexec.install Dir["bin/*"]
     chmod 0755, libexec/"fastqc"
     (bin/"fastqc").write_env_script libexec/"fastqc", JAVA_HOME: Formula["openjdk"].opt_prefix
   end
