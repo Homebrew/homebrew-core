8d1bd7ab5d4dfd68bedb12b645b7d6fc5fcc1600
Greg Guthe <greg.guthe@gmail.com>
2024-01-11 23:08:55 -0500

    llamafile 0.6.1 (new formula)
    Co-authored-by: Sean Molenaar <SMillerDev@users.noreply.github.com>

---
+++ Formula/l/llamafile.rb
@@ -0,0 +1,60 @@
+class Llamafile < Formula
+  desc "Distribute and run LLMs with a single file"
+  homepage "https://github.com/Mozilla-Ocho/llamafile"
+  url "https://github.com/Mozilla-Ocho/llamafile/archive/refs/tags/0.6.1.tar.gz"
+  sha256 "553eea3faecde2f6e52b42ec719c5f2129832f3337b1bc9c5f8e9a449a694f46"
+  license "Apache-2.0"
+  head "https://github.com/Mozilla-Ocho/llamafile.git", branch: "main"
+
+  depends_on "make"
+
+  # Commented out to test perms from clean: https://github.com/Homebrew/homebrew-core/pull/160198#discussion_r1461425767
+  # skip_clean "bin/llamafile",
+  #            "bin/llamafile-convert",
+  #            "bin/llamafile-perplexity",
+  #            "bin/llamafile-quantize",
+  #            "bin/llava-quantize",
+  #            "bin/zipalign"
+
+  resource("mistral.gguf") do
+    url "https://huggingface.co/TheBloke/Mistral-7B-Instruct-v0.1-GGUF/resolve/main/mistral-7b-instruct-v0.1.Q4_K_M.gguf"
+    sha256 "14466f9d658bf4a79f96c3f3f22759707c291cac4e62fea625e80c7d32169991"
+  end
+
+  def install
+    ENV.prepend_path "PATH", pwd
+
+    # Call `make` as `gmake` to use GNU `make` on OSX
+    make = OS.mac? ? "gmake" : "make"
+
+    system make
+    system make, "install", "PREFIX=#{prefix}"
+
+    # Commented out to test default perms. refs: https://github.com/Homebrew/homebrew-core/pull/160198#discussion_r1461424910
+    # %w[
+    #   llamafile
+    #   llamafile-convert
+    #   llamafile-perplexity
+    #   llamafile-quantize
+    #   llava-quantize
+    #   zipalign
+    # ].each do |bin_file|
+    #   chmod "+x", "#{bin}/#{bin_file}"
+    # end
+  end
+
+  test do
+    resource("mistral.gguf").stage do
+      test_cmd = <<~CMD
+        #{bin}/llamafile \
+                   --model mistral-7b-instruct-v0.1.Q4_K_M.gguf \
+                   --cli \
+                   --temp 0.0 \
+                   --n-predict 1 \
+                   --prompt '[INST]Are you working? Be optimistic.[/INST]' \
+                   --grammar 'root ::= "yes" | "no"'
+      CMD
+      assert_match "yes", shell_output(test_cmd)
+    end
+  end
+end
