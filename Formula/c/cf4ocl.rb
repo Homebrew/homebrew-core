88915cb6d86beff74f96d1d60ec0487e1b62d037
Patrick Linnane <patrick@linnane.io>
2024-01-17 15:47:29 -0800

    cf4ocl: update build and test
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

--- Formula/c/cf4ocl.rb
+++ Formula/c/cf4ocl.rb
@@ -18,6 +18,7 @@ class Cf4ocl < Formula
 
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
+  depends_on "gettext"
   depends_on "glib"
 
   on_linux do
@@ -26,53 +27,19 @@ class Cf4ocl < Formula
     depends_on "pocl"
   end
 
-  # Fix build failure on Linux caused by undefined Windows-only constants.
-  # Upstreamed here: https://github.com/nunofachada/cf4ocl/pull/40
-  patch :DATA
-
   def install
-    args = *std_cmake_args
-    args << "-DBUILD_TESTS=OFF"
-    system "cmake", ".", *args
-    system "make", "install"
+    system "cmake", "-S", ".", "-B", "build", "-DBUILD_TESTS=OFF",
+                                              *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
   end
 
   test do
-    system bin/"ccl_devinfo"
+    # OpenCL does not work on ephemeral ARM CI.
+    if Hardware::CPU.virtualized? && Hardware::CPU.arm? && OS.mac?
+      assert_match "OpenCL error -30: Invalid value", shell_output(bin/"ccl_devinfo 2>&1", 1)
+    else
+      assert_match(/TYPE\s+\|\s+[CG]PU/, shell_output(bin/"ccl_devinfo 2>&1"))
+    end
   end
 end
-
-__END__
-diff --git a/src/lib/ccl_event_wrapper.c b/src/lib/ccl_event_wrapper.c
-index 0bfbf8a..0ba8bf9 100644
---- a/src/lib/ccl_event_wrapper.c
-+++ b/src/lib/ccl_event_wrapper.c
-@@ -282,6 +282,7 @@ const char* ccl_event_get_final_name(CCLEvent* evt) {
- 			case CL_COMMAND_GL_FENCE_SYNC_OBJECT_KHR:
- 				final_name = "GL_FENCE_SYNC_OBJECT_KHR";
- 				break;
-+            #if defined(__MSC_VER)
- 			case CL_COMMAND_ACQUIRE_D3D10_OBJECTS_KHR:
- 				final_name = "ACQUIRE_D3D10_OBJECTS_KHR";
- 				break;
-@@ -300,6 +301,7 @@ const char* ccl_event_get_final_name(CCLEvent* evt) {
- 			case CL_COMMAND_RELEASE_D3D11_OBJECTS_KHR:
- 				final_name = "RELEASE_D3D11_OBJECTS_KHR";
- 				break;
-+            #endif
- 			case CL_COMMAND_ACQUIRE_EGL_OBJECTS_KHR:
- 				final_name = "ACQUIRE_EGL_OBJECTS_KHR";
- 				break;
-diff --git a/src/lib/ccl_oclversions.h b/src/lib/ccl_oclversions.h
-index 4e82c9f..598a7e6 100644
---- a/src/lib/ccl_oclversions.h
-+++ b/src/lib/ccl_oclversions.h
-@@ -33,7 +33,7 @@
- 	#include <OpenCL/opencl.h>
- #else
- 	#include <CL/opencl.h>
--	#ifdef CL_VERSION_1_2
-+	#if defined(CL_VERSION_1_2) && defined(__MSC_VER)
- 		#include <CL/cl_dx9_media_sharing.h>
- 	#endif
- #endif
