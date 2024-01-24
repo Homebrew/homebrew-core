f002634c86bb8368f7565672d554bb1245a07ef2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-01-24 15:39:17 +0000

    embulk 0.11.2
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

--- Formula/e/embulk.rb
+++ Formula/e/embulk.rb
@@ -1,11 +1,8 @@
 class Embulk < Formula
   desc "Data transfer between various databases, file formats and services"
   homepage "https://www.embulk.org/"
-  # https://www.embulk.org/articles/2020/03/13/embulk-v0.10.html
-  # v0.10.* is a "development" series, not for your production use.
-  # In your production, keep using v0.9.* stable series.
-  url "https://github.com/embulk/embulk/releases/download/v0.11.0/embulk-0.11.0.jar"
-  sha256 "16664089e2676d5f7fe71c1ccf48b2a24f982b89e4f483e650a15f52a72309c1"
+  url "https://github.com/embulk/embulk/releases/download/v0.11.2/embulk-0.11.2.jar"
+  sha256 "382d2408fa662d736a8e97d387cf5f8b8b1f21d69a9cdd3e1922653b056316b3"
   license "Apache-2.0"
   version_scheme 1
 
