22da59d5bb334cb2e45a0c67faf9f0ee931f4a9c
devantler <nikolaiemildamm@icloud.com>
2024-01-31 02:10:32 +0100

    KSail - A CLI tool for provisioning GitOps enabled K8s clusters in Docker

---
+++ Formula/k/ksail.rb
@@ -0,0 +1,32 @@
+class Ksail < Formula
+  desc "Cli tool for provisioning gitops enabled k8s clusters in docker"
+  homepage "https://github.com/devantler/ksail"
+  url "https://github.com/devantler/ksail/releases/download/v1.2.7/ksail.tar.gz"
+  sha256 "63bb7b256a46dffaec25c0c803f0332f037f44ffa10e94664761b98839d7d66f"
+  license "MIT"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  def install
+    if OS.mac?
+      if Hardware::CPU.arm?
+        bin.install "ksail-darwin-arm64" => "ksail"
+      elsif Hardware::CPU.intel?
+        bin.install "ksail-darwin-amd64" => "ksail"
+      end
+    elsif OS.linux?
+      if Hardware::CPU.arm?
+        bin.install "ksail-linux-arm64" => "ksail"
+      elsif Hardware::CPU.intel?
+        bin.install "ksail-linux-amd64" => "ksail"
+      end
+    end
+  end
+
+  test do
+    system "#{bin}/ksail", "--version"
+  end
+end
