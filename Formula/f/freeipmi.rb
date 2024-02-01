fb661a62837289daaf954460f10e0ffc537a7cdd
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-02-01 00:48:39 +0000

    freeipmi 1.6.14
    freeipmi: remove patch

--- Formula/f/freeipmi.rb
+++ Formula/f/freeipmi.rb
@@ -1,9 +1,9 @@
 class Freeipmi < Formula
   desc "In-band and out-of-band IPMI (v1.5/2.0) software"
   homepage "https://www.gnu.org/software/freeipmi/"
-  url "https://ftp.gnu.org/gnu/freeipmi/freeipmi-1.6.11.tar.gz"
-  mirror "https://ftpmirror.gnu.org/freeipmi/freeipmi-1.6.11.tar.gz"
-  sha256 "65fbd6910fc010457748695414f27c5755b4e8d75734221221f3858c6230a897"
+  url "https://ftp.gnu.org/gnu/freeipmi/freeipmi-1.6.14.tar.gz"
+  mirror "https://ftpmirror.gnu.org/freeipmi/freeipmi-1.6.14.tar.gz"
+  sha256 "1a3dac5c76b7ccc4d4f86aa12b8ef9b212baef7489bf05e899b89abb7e14edb5"
   license "GPL-3.0-or-later"
 
   bottle do
@@ -25,21 +25,9 @@ class Freeipmi < Formula
     depends_on "argp-standalone"
   end
 
-  # Fix -flat_namespace being used on Big Sur and later.
-  patch do
-    url "https://raw.githubusercontent.com/Homebrew/formula-patches/03cf8088210822aa2c1ab544ed58ea04c897d9c4/libtool/configure-pre-0.4.2.418-big_sur.diff"
-    sha256 "83af02f2aa2b746bb7225872cab29a253264be49db0ecebb12f841562d9a2923"
-  end
-
   def install
     # Fix compile with newer Clang
     ENV.append_to_cflags "-Wno-implicit-function-declaration" if DevelopmentTools.clang_build_version >= 1403
-
-    # Hardcode CPP_FOR_BUILD to work around cpp shim issue:
-    # https://github.com/Homebrew/brew/issues/5153
-    inreplace "man/Makefile.in",
-      "$(CPP_FOR_BUILD) -nostdinc -w -C -P -I$(top_srcdir)/man $@.pre $@",
-      "#{ENV.cxx} -E -nostdinc -w -C -P -I$(top_srcdir)/man $@.pre > $@"
 
     system "./configure", "--prefix=#{prefix}"
     system "make", "install"
