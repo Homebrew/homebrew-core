f868d55ac3190a95b76b3b3eb02818613fc90110
William Woodruff <william@yossarian.net>
2024-02-06 10:04:32 +0100

    howdoi: remove `six` dep
    Signed-off-by: William Woodruff <william@yossarian.net>

--- Formula/h/howdoi.rb
+++ Formula/h/howdoi.rb
@@ -29,7 +29,6 @@ class Howdoi < Formula
   depends_on "python-lxml"
   depends_on "python-typing-extensions"
   depends_on "python@3.12"
-  depends_on "six"
 
   resource "appdirs" do
     url "https://files.pythonhosted.org/packages/d7/d8/05696357e0311f5b5c316d7b95f46c669dd9c15aaeecbb48c7d0aeb88c40/appdirs-1.4.4.tar.gz"
