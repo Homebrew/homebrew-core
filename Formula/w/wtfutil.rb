7dc5d5ca0679bc1c68eec74773300329c1904b64
Sean Molenaar <smillerdev@me.com>
2024-02-12 11:38:29 +0100

    wtfutil: test failure case

--- Formula/w/wtfutil.rb
+++ Formula/w/wtfutil.rb
@@ -32,47 +32,9 @@ class Wtfutil < Formula
 
   test do
     testconfig = testpath/"config.yml"
-    testconfig.write <<~EOS
-      wtf:
-        colors:
-          background: "red"
-          border:
-            focusable: "darkslateblue"
-            focused: "orange"
-            normal: "gray"
-          checked: "gray"
-          highlight:
-            fore: "black"
-            back: "green"
-          text: "white"
-          title: "white"
-        grid:
-          # How _wide_ the columns are, in terminal characters. In this case we have
-          # six columns, each of which are 35 characters wide
-          columns: [35, 35, 35, 35, 35, 35]
+    testconfig.write ""
 
-          # How _high_ the rows are, in terminal lines. In this case we have five rows
-          # that support ten line of text, one of three lines, and one of four
-          rows: [10, 10, 10, 10, 10, 3, 4]
-        navigation:
-          shortcuts: true
-        openFileUtil: "open"
-        sigils:
-          checkbox:
-            checked: "x"
-            unchecked: " "
-          paging:
-            normal: "*"
-            selected: "_"
-        term: "xterm-256color"
-    EOS
-
-    begin
-      pid = fork do
-        exec "#{bin}/wtfutil", "--config=#{testconfig}"
-      end
-    ensure
-      Process.kill("HUP", pid)
-    end
+    out = shell_output("#{bin}/wtfutil --config=#{testconfig} 2>&1", 1)
+    assert_match "No modules were defined. Make sure you have at least one properly defined widget", out
   end
 end
