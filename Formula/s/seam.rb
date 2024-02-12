fa857c712a9a76f8a28c493b7622d6d99b84f8c9
Andrii <mastafit1@gmail.com>
2024-02-12 15:01:54 +0100

    Update seam.rb

--- Formula/s/seam.rb
+++ Formula/s/seam.rb
@@ -3,18 +3,20 @@ require "language/node"
 class Seam < Formula
   desc "This utility lets you control Seam resources"
   homepage "https://github.com/seamapi/seam-cli"
-  url "https://github.com/seamapi/seam-cli/archive/refs/tags/0.0.33.tar.gz"
-  sha256 "5a496af613cf30dfa8a0a31bb67be7164321980dde9b4ff1575ae28deac77a56"
+  url "https://registry.npmjs.org/seam-cli/-/seam-cli-0.0.39.tgz"
+  sha256 "af448c0864801fe34466b2cb5c397a7774fcbfe3bbdae23bf046a1cd44fa4e09"
   license "MIT"
 
   depends_on "node"
 
   def install
-    system "npm", "install", *Language::Node.local_npm_install_args
-    system "npm", "run", "build"
-    bin.install "bin/seam.cjs" => "seam"
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
   end
+  
   test do
-    assert_match version.to_s, shell_output("#{bin}/seam --version")
+    system "#{bin}/seam", "config", "set", "fake-server"
+    output = pipe_output("#{bin}/seam health get_health")
+    assert_match 'I’m one with the Force. The Force is with me.', output
   end
 end
