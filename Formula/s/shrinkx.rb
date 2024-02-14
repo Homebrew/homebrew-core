bce136b34c894b39818c26805f82bdd5b0176b05
stormed <88412569+stormedx@users.noreply.github.com>
2024-02-14 00:27:33 -0700

    Added Shrinkx

---
+++ Formula/s/shrinkx.rb
@@ -0,0 +1,20 @@
+class Shrinkx < Formula
+  include Language::Python::Virtualenv
+
+  desc "A Python-based tool for effortlessly compressing media files. "
+  homepage "https://github.com/stormedx/shrinkx"
+  url "https://github.com/stormedx/shrinkx/archive/refs/tags/shrinkx.tar.gz"
+  version "1.1"
+  sha256 "e2d22b9031fb434dd8b37f32f511ad988129b21a40c97fcb7fd5d7d00bb3b2ea"
+
+  depends_on "python@3.9"
+  depends_on "ffmpeg"
+
+  def install
+    virtualenv_install_with_resources
+  end
+
+  test do
+    system "#{bin}/shrinkx", "--version"
+  end
+end
