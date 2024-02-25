5375c70294519a006b021ace0e99efb62f87d14e
Peter Vaiciulis <petervaiciulis@gmail.com>
2024-02-25 18:24:24 -0500

    decanter v0.2.6 (new formula)
    fix issues in syntax

--- Formula/d/decanter.rb
+++ Formula/d/decanter.rb
@@ -3,13 +3,10 @@ class Decanter < Formula
   homepage "https://github.com/p5quared/decanter"
   url "https://github.com/p5quared/decanter/archive/refs/tags/v0.2.6.tar.gz"
   sha256 "c23306d2aa65d7ec740500e2a387079888f067ef5518807dc4a06ae2696aa333"
-  version "0.2.6"
   license "MIT"
 
   depends_on "go" => :build
-
   def install
     system "go", "build"
   end
 end
-
