2f54c91d08d9742e111fc2f89a508b542e0815da
Peter Vaiciulis <petervaiciulis@gmail.com>
2024-02-25 18:03:29 -0500

    decanter v0.2.6 (new formula)
    fix previous missing url

--- Formula/d/decanter.rb
+++ Formula/d/decanter.rb
@@ -1,6 +1,7 @@
 class Decanter < Formula
   desc "Autolab from the CLI; Decan't you see why that's awesome?"
   homepage "https://github.com/p5quared/decanter"
+  url "https://github.com/p5quared/decanter/archive/refs/tags/v0.2.6.tar.gz"
   version "0.2.6"
   license "MIT"
 
