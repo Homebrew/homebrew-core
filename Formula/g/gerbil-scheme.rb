bc402045715a03cc302d6dc6adbe9fd7766c2379
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-02-25 03:35:20 +0000

    gerbil-scheme 0.18.1

--- Formula/g/gerbil-scheme.rb
+++ Formula/g/gerbil-scheme.rb
@@ -1,10 +1,9 @@
 class GerbilScheme < Formula
   desc "Opinionated dialect of Scheme designed for Systems Programming"
   homepage "https://cons.io"
-  url "https://github.com/vyzo/gerbil/archive/refs/tags/v0.17.tar.gz"
-  sha256 "1e81265aba7e9022432649eb26b2e5c85a2bb631a315e4fa840b14cf336b2483"
+  url "https://github.com/vyzo/gerbil/archive/refs/tags/v0.18.1.tar.gz"
+  sha256 "3ce56daa5e8f36f08370b5a4485f54fb6e6e9b892b95d8b9904f70ad17bd4d6e"
   license any_of: ["LGPL-2.1-or-later", "Apache-2.0"]
-  revision 3
 
   livecheck do
     url :stable
