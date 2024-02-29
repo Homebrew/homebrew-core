1913a2dc3e740929ad7c7f6c1323a8421ccc3b12
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-02-29 00:40:22 +0000

    embulk 0.11.2

--- Formula/e/embulk.rb
+++ Formula/e/embulk.rb
@@ -4,8 +4,8 @@ class Embulk < Formula
   # https://www.embulk.org/articles/2020/03/13/embulk-v0.10.html
   # v0.10.* is a "development" series, not for your production use.
   # In your production, keep using v0.9.* stable series.
-  url "https://github.com/embulk/embulk/releases/download/v0.11.0/embulk-0.11.0.jar"
-  sha256 "16664089e2676d5f7fe71c1ccf48b2a24f982b89e4f483e650a15f52a72309c1"
+  url "https://github.com/embulk/embulk/releases/download/v0.11.2/embulk-0.11.2.jar"
+  sha256 "382d2408fa662d736a8e97d387cf5f8b8b1f21d69a9cdd3e1922653b056316b3"
   license "Apache-2.0"
   version_scheme 1
 
@@ -30,7 +30,35 @@ class Embulk < Formula
 
   test do
     system bin/"embulk", "example", "./try1"
-    system bin/"embulk", "guess", "./try1/seed.yml", "-o", "config.yml"
+    # we skip the "embulk guess" command from the documentation because it triggers
+    # a permission request on mac, instead we write the config.yml file ourselves
+    config_file = testpath/"config.yml"
+    config_file.write <<~EOS
+      in:
+        type: file
+        path_prefix: #{testpath}/./try1/csv/sample_
+        decoders:
+        - {type: gzip}
+        parser:
+          charset: UTF-8
+          newline: LF
+          type: csv
+          delimiter: ','
+          quote: '"'
+          escape: '"'
+          null_string: 'NULL'
+          trim_if_not_quoted: false
+          skip_header_lines: 1
+          allow_extra_columns: false
+          allow_optional_columns: false
+          columns:
+          - {name: id, type: long}
+          - {name: account, type: long}
+          - {name: time, type: timestamp, format: '%Y-%m-%d %H:%M:%S'}
+          - {name: purchase, type: timestamp, format: '%Y%m%d'}
+          - {name: comment, type: string}
+      out: {type: stdout}
+    EOS
     system bin/"embulk", "preview", "config.yml"
     system bin/"embulk", "run", "config.yml"
   end
