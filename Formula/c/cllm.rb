d649aa0aff529f8b9bee638606fc7f74b7664919
devleejb <devleejb@gmail.com>
2024-02-29 13:42:31 +0900

    cllm 0.1.1 (new formula)

---
+++ Formula/c/cllm.rb
@@ -0,0 +1,16 @@
+class Cllm < Formula
+    desc "Empower your CLI experience with a command search tool driven by LLM magic!" 
+    homepage "https://github.com/dev-backpack/cllm" 
+    url "https://github.com/dev-backpack/cllm/releases/download/v0.1.1/cllm-x86_64-apple-darwin.tar.gz" 
+    sha256 "bed0845aa5a4f9fc6c5bf3eb8c7d6ef77201c753ce6a4daf74f70e41f1cde47e"
+    version "0.1.1"
+    license "MIT"
+  
+    def install
+      bin.install "cllm"
+    end
+  
+    test do
+      system "#{bin}/cllm", "--help"
+    end
+  end
