8d33289e337ac6385c2a3b25004e8c8e4915891b
Alex Rak <rak.alexei@gmail.com>
2024-03-01 20:05:24 +0200

    updated the formula

--- Formula/m/mac-brightnessctl.rb
+++ Formula/m/mac-brightnessctl.rb
@@ -5,17 +5,15 @@ class MacBrightnessctl < Formula
   sha256 "2a275269326ef2a396d87b5b1dff4779d9a003e9474454a6f0df32e01b0bc4d7"
   license "MIT"
 
-  if OS.mac?
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  on_macos do
     def install
       system "make"
       bin.install "mac-brightnessctl"
     end
-
-    livecheck do
-      url :stable
-      regex(/^v?(\d+(?:\.\d+)+)$/i)
-    end
-  else
-    disable! "This formula is intended for macOS only."
   end
 end
