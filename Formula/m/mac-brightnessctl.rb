5d3121a574ef104c237a5b227cc29b742992859e
Alex Rak <rak.alexei@gmail.com>
2024-03-01 18:48:56 +0200

    updated the formula

--- Formula/m/mac-brightnessctl.rb
+++ Formula/m/mac-brightnessctl.rb
@@ -5,12 +5,16 @@ class MacBrightnessctl < Formula
   sha256 "2a275269326ef2a396d87b5b1dff4779d9a003e9474454a6f0df32e01b0bc4d7"
   license "MIT"
 
-  def install
-    system "make"
-    bin.install "mac-brightnessctl"
-  end
+  if OS.mac?
+    def install
+      system "make"
+      bin.install "mac-brightnessctl"
+    end
 
-  test do
-    assert_match "Usage:", shell_output("#{bin}/mac-brightnessctl --help")
+    test do
+      assert_match "Usage:", shell_output("#{bin}/mac-brightnessctl --help")
+    end
+  else
+    disable! "This formula is intended for macOS only."
   end
 end
