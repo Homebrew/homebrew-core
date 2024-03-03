a466e604d30d697bb12ecaf241d16b08b6ad13c1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-03-03 20:48:59 +0000

    git-cliff: update 2.1.1 bottle.

--- Formula/g/git-cliff.rb
+++ Formula/g/git-cliff.rb
@@ -6,13 +6,13 @@ class GitCliff < Formula
   license all_of: ["Apache-2.0", "MIT"]
 
   bottle do
-    sha256 cellar: :any,                 arm64_sonoma:   "c2f045e053a9c8d4cefbafbd92d14c872186ca3f4b935a4422394936ce28f439"
-    sha256 cellar: :any,                 arm64_ventura:  "728171ea5d52f3376764f26feacfbee014d7ca74d422b30597ec892688404fff"
-    sha256 cellar: :any,                 arm64_monterey: "6d37b2b0dec1dc44d00fc4a2506baca454bb183da55e72b2503f99037f4bece3"
-    sha256 cellar: :any,                 sonoma:         "3cc1cb86f26f079eb12aa861698e57fb9daba4a42c8656d4f7ea3c8549f8e61e"
-    sha256 cellar: :any,                 ventura:        "20becedd2d172d42ee0b4b65f3c9e7602d868bf2b9a77476c2cdad57a85e9499"
-    sha256 cellar: :any,                 monterey:       "cd686a302fa3b91c1af355b2091d944e7952eefad0159fa3ac95a76db8466fb7"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5cc7fb9ab9b41375fac34ce656a14258d6a8fddaf73652f00d26d74ebc686bc4"
+    sha256 cellar: :any,                 arm64_sonoma:   "d9e81ac5b011a56548766465bc31bae07f4cf47f8de008d1708cf0adea224587"
+    sha256 cellar: :any,                 arm64_ventura:  "6381792704010306a949a78c9a1155f5a27f63d5d482a3165620e0649987a3a4"
+    sha256 cellar: :any,                 arm64_monterey: "9899fc654a02cf2a0099ce021b8871e601afc2471b141ac647908335ff99d3dc"
+    sha256 cellar: :any,                 sonoma:         "16582c67f65da303ec053690912f688329a18b99481e68e7c3adf12492cb255a"
+    sha256 cellar: :any,                 ventura:        "03331bc4fd8ed076d5681b8706dba7a0fc71d9c75d60255411cdd8a74532dbcf"
+    sha256 cellar: :any,                 monterey:       "e3c8ceea4fbb83f7028c6729a0b1d7272fc840c68c31881668efcb4c933682af"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9a88b5998e8cc9c95e2c0a04391c90df890c2fce239c95ea13e0d031b6a2973f"
   end
 
   depends_on "pkg-config" => :build
