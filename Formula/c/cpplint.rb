411693131ab567f29fae306fae87d774fb1a91c0
Ruoyu Zhong <zhongruoyu@outlook.com>
2024-03-07 22:21:54 +0800

    cpplint: revision bump (setuptools vendoring)

--- Formula/c/cpplint.rb
+++ Formula/c/cpplint.rb
@@ -6,6 +6,7 @@ class Cpplint < Formula
   url "https://files.pythonhosted.org/packages/18/72/ea0f4035bcf35d8f8df053657d7f3370d56ff4d4e6617021b6544b9958d4/cpplint-1.6.1.tar.gz"
   sha256 "d430ce8f67afc1839340e60daa89e90de08b874bc27149833077bba726dfc13a"
   license "Apache-2.0"
+  revision 1
 
   bottle do
     rebuild 4
