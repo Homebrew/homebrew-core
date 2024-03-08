4623401ac925278ad277ab46f18874a85aaf7bfb
josh rotenberg <joshrotenberg@users.noreply.github.com>
2024-03-08 10:22:47 -0800

    remove redundant version, add test

--- Formula/a/adrs.rb
+++ Formula/a/adrs.rb
@@ -2,7 +2,6 @@ class Adrs < Formula
   desc "Architectural Decision Record tool in Rust"
   homepage "https://joshrotenberg.com/adrs/"
   url "https://github.com/joshrotenberg/adrs/archive/refs/tags/v0.2.1.tar.gz"
-  version "0.2.1"
   sha256 "f2f35d3373734d687104e34c9dfce0496c6610ec024c1f03e7b268362237a5b8"
   license "Apache-2.0"
 
@@ -13,6 +12,7 @@ class Adrs < Formula
   end
 
   test do
-    system "false"
+    system "#{bin}/adrs", "init"
+    assert_predicate testpath/"doc/adr/0001-record-architecture-decisions.md", :exist?
   end
 end
