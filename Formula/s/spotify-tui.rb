2116b739d2d079ae24547cc7a4bf36f2f7902875
John Tyler <johntyler@createdbydata.com>
2024-03-12 13:27:22 +0000

    Updating to openssl 3

--- Formula/s/spotify-tui.rb
+++ Formula/s/spotify-tui.rb
@@ -29,8 +29,6 @@ class SpotifyTui < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "5214a242010de3e25360daf3d1322f353b626e22ca784302032a12bf92a8616c"
   end
 
-  disable! date: "2024-02-12", because: "uses deprecated `openssl@1.1`"
-
   depends_on "rust" => :build
 
   on_linux do
