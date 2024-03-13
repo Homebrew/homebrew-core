ece12a11f57194c7028f08f9577a120d1127fc8e
Thomas Strathmann <thomas@pdp7.org>
2024-03-13 17:48:54 +0100

    soupsieve 2.5 (new formula)

---
+++ Formula/s/soupsieve.rb
@@ -0,0 +1,28 @@
+class Soupsieve < Formula
+  desc "CSS selector implementation for Beautiful Soup"
+  homepage "https://github.com/facelessuser/soupsieve"
+  url "https://files.pythonhosted.org/packages/ce/21/952a240de1c196c7e3fbcd4e559681f0419b1280c617db21157a0390717b/soupsieve-2.5.tar.gz"
+  sha256 "5663d5a7b3bfaeee0bc4372e7fc48f9cff4940b3eec54a6451cc5299f1097690"
+  license "MIT"
+
+  depends_on "python@3.11" => [:build, :test]
+  depends_on "python@3.12" => [:build, :test]
+
+  def pythons
+    deps.map(&:to_formula).sort_by(&:version).filter { |f| f.name.start_with?("python@") }
+  end
+
+  def install
+    pythons.each do |python|
+      python_exe = python.opt_libexec/"bin/python"
+      system python_exe, "-m", "pip", "install", *std_pip_args(build_isolation: true), "."
+    end
+  end
+
+  test do
+    pythons.each do |python|
+      python_exe = python.opt_libexec/"bin/python"
+      system python_exe, "-c", "import soupsieve"
+    end
+  end
+end
