1de85eb2c4e93c6f71f860fa49854291b2c9d99c
Jan Krukowski <jan.krukowski@geneva.com>
2024-03-13 11:03:20 +0100

    fixed tests, fixed formatting, removed arch dependency

--- Formula/w/whisperkit.rb
+++ Formula/w/whisperkit.rb
@@ -1,11 +1,14 @@
 class Whisperkit < Formula
   desc "Swift native on-device speech recognition with Whisper for Apple Silicon"
   homepage "https://github.com/argmaxinc/WhisperKit"
-  url "https://github.com/argmaxinc/WhisperKit.git", tag: "v0.3.3", revision: "bfa357e897dadae0dca6314424bb119149e375d2"
+  url "https://github.com/argmaxinc/WhisperKit.git",
+      tag:      "v0.3.3",
+      revision: "bfa357e897dadae0dca6314424bb119149e375d2"
   license "MIT"
 
   depends_on xcode: ["15.0", :build]
-  depends_on arch: :arm64
+  depends_on "git-lfs"
+  depends_on macos: :ventura
 
   def install
     system "swift", "build", "-c", "release", "--product", "transcribe", "--disable-sandbox"
@@ -13,6 +16,25 @@ class Whisperkit < Formula
   end
 
   test do
-    system "#{bin}/transcribe", "--help"
+    mkdir_p "#{testpath}/Models/whisperkit-coreml"
+    ENV["GIT_LFS_SKIP_SMUDGE"] = "1"
+    system "git", "lfs", "install"
+    system "git",
+        "clone",
+        "https://huggingface.co/argmaxinc/whisperkit-coreml",
+        "#{testpath}/Models/whisperkit-coreml"
+    chdir "#{testpath}/Models/whisperkit-coreml" do
+      system "git", "lfs", "pull", "--include=openai_whisper-tiny/*"
+    end
+    system "#{bin}/transcribe",
+        "--model-path",
+        "#{testpath}/Models/whisperkit-coreml/openai_whisper-tiny",
+        "--audio-path",
+        test_fixtures("test.mp3"),
+        "--audio-encoder-compute-units",
+        "cpuOnly",
+        "--text-decoder-compute-units",
+        "cpuOnly",
+        "--verbose"
   end
 end
