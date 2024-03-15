f8102a66c88f73b0ce02f98a5ed4c2ec3708518c
Anselm Hahn <Anselm.Hahn@gmail.com>
2024-03-15 05:37:38 +0100

    yeoman 5.0.0 (new formula)
    Introduce `yeoman` formula version **5.0.0**, see also: https://yeoman.io
    
    This commit introduces the `yeoman` formula version **5.0.0**. The details include:
    
    - The URL points to the version 5.0.0 of Yo in the npm registry.
    - The SHA256 hash matches the version's hash.
    - The formula depends on Node.
    
    Two tests have been added to check the output of `yo --version`  and `yo doctor` commands.
    The `yeoman` formula is a Homebrew formula for the yo-CLI tool, which is used for running Yeoman generators.

---
+++ Formula/y/yeoman.rb
@@ -0,0 +1,22 @@
+require "language/node"
+
+class Yeoman < Formula
+  desc "CLI tool for running Yeoman generators"
+  homepage "https://yeoman.io"
+  url "https://registry.npmjs.org/yo/-/yo-5.0.0.tgz"
+  sha256 "4395888eda54803a590d20d92b285c4abebd81402908b5becdf9cbc6cbeba65f"
+  license "BSD-2-Clause"
+  head "https://github.com/yeoman/yeoman.git", branch: "main"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/yo --version")
+    assert_match "Everything looks all right!", shell_output("#{bin}/yo doctor")
+  end
+end
