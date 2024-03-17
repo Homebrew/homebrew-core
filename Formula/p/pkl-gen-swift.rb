22a5494ab7e69e47da8fa3bfafc7cba7e53f0c7d
Kenta Enomoto <knt527@gmail.com>
2024-03-17 22:36:37 +0900

    pkl-gen-swift 0.2.3 (new formula)

---
+++ Formula/p/pkl-gen-swift.rb
@@ -0,0 +1,28 @@
+class PklGenSwift < Formula
+  desc "Pkl bindings for the Swift programming language"
+  homepage "https://pkl-lang.org/swift/current/index.html"
+  url "https://github.com/apple/pkl-swift/archive/refs/tags/0.2.3.tar.gz"
+  sha256 "2318ab9f641237a70531652b78fcdcf2e9cb609f96e5386778dd08e14fe847b4"
+  license "Apache-2.0"
+
+  depends_on xcode: ["15.2"]
+  depends_on "pkl"
+
+  uses_from_macos "swift"
+
+  def install
+    system "swift", "build", "--disable-sandbox", "--product", "pkl-gen-swift", "--configuration", "release"
+    bin.install ".build/release/pkl-gen-swift"
+  end
+
+  test do
+    (testpath/"intro.pkl").write <<~EOS
+      name = "Pkl: Configure your Systems in New Ways"
+    EOS
+
+    system "#{bin}/pkl-gen-swift", "#{testpath}/intro.pkl", "-o", "#{testpath}/out/"
+    assert_predicate testpath/"out/intro.pkl.swift", :exist?
+
+    assert_match version.to_s, shell_output("#{bin}/pkl-gen-swift --version")
+  end
+end
