b7841bee2ec32107e6ecbcc82614b60e6b744ba9
Bevan Kay <email@bevankay.me>
2024-03-21 12:59:05 +1100

    cargo-c: revision bump (libgit2 1.8.0)

--- Formula/c/cargo-c.rb
+++ Formula/c/cargo-c.rb
@@ -4,6 +4,7 @@ class CargoC < Formula
   url "https://github.com/lu-zero/cargo-c/archive/refs/tags/v0.9.30.tar.gz"
   sha256 "174cfc3a69263c3e54b95e00c4bd61b13377f7f72d4bf60aa714fd9e7ed3849c"
   license "MIT"
+  revision 1
 
   livecheck do
     url :stable
