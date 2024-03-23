92ac5ebb9b82fdc558b83cac0a63626bc716f0bd
小新 <xinping.wang@hotmail.com>
2024-03-23 11:09:31 -0500

    gemma.cpp 0.1.1

--- Formula/g/gemma.cpp.rb
+++ Formula/g/gemma.cpp.rb
@@ -1,23 +1,21 @@
 class GemmaCpp < Formula
   desc "Lightweight, standalone C++ inference engine for Google's Gemma models"
   homepage "https://github.com/google/gemma.cpp"
-  url "https://github.com/google/gemma.cpp/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "218280867d6f5256d3e3a9a34852e5752acd284538a8e7edaa6b382319e67c5c"
+  url "https://github.com/google/gemma.cpp/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "edc0529563114befcda414f0ae2ca0690b408e661ccfc3b61af360de53c00c4e"
   license "Apache-2.0"
 
   depends_on "cmake" => :build
   depends_on "sentencepiece"
 
   def install
-    system "cmake", "-B", "build", *std_cmake_args
-    cd "build" do
-      system "make", "gemma", "libgemma"
-      bin.install "gemma"
-      lib.install "libgemma.a"
-    end
+    system "cmake", "--preset", "make", *std_cmake_args
+    system "cmake", "--build", "--preset", "make"
+    bin.install "build/gemma"
+    lib.install "build/libgemma.a"
   end
 
   test do
-    system "#{bin}/gemma"
+    assert_match "Example Usage", shell_output("#{bin}/gemma --help 2>&1")
   end
 end
