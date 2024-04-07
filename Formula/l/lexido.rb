533e7e6227df04230baac9ee4c290123f312a3bb
swayz8148 <michealdownard07@gmail.com>
2024-04-07 05:19:52 +0100

    lexido 1.3.1
    lexido: fix styling error and GitHub link

--- Formula/l/lexido.rb
+++ Formula/l/lexido.rb
@@ -1,7 +1,8 @@
 class Lexido < Formula
   desc "Innovative assistant for the command-line"
   homepage "https://github.com/micr0-dev/lexido"
-  url "https://github.com/micr0-dev/lexido.git", tag: "v1.3.1", revision: "36f02d6"
+  url "https://github.com/micr0-dev/lexido.git", tag: "v1.3.1", revision: "36f02d699f2709f0bd51a5f119e0670845b4ad9f"
+  sha256 "8d05016b392fa43a33d6989c1f6e568f0d8b5d8893faf8622d02d36fd2d5e9db"
 
   depends_on "go" => :build
 
