5cdf8f941c47d077cbff37e23a3c40c877359110
AngelaDMerkel <63363428+AngelaDMerkel@users.noreply.github.com>
2024-04-08 17:00:52 +0200

    Update single-file.rb
    Updated version to 2.0.29

--- Formula/s/single-file.rb
+++ Formula/s/single-file.rb
@@ -3,7 +3,7 @@ require "language/node"
 class SingleFile < Formula
   desc "CLI tool for saving a faithful copy of a complete web page in a single HTML file"
   homepage "https://github.com/gildas-lormeau/single-file-cli/blob/master/README.MD"
-  url "https://github.com/gildas-lormeau/single-file-cli/archive/refs/tags/v2.0.14.tar.gz"
+  url "https://github.com/gildas-lormeau/single-file-cli/archive/refs/tags/v2.0.29.tar.gz"
   sha256 "bb12b99dbef208c62cd7733cbbf0930f4d088fc2bcad8fc459142a375c5a9eaa"
   license "AGPL-3.0-or-later"
   
