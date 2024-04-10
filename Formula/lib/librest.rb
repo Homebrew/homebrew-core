32b3aebaae6bb663a8827c07182ab5df2aef51ad
Patrick Linnane <patrick@linnane.io>
2024-04-10 15:20:01 -0700

    librest 0.9.1
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

--- Formula/lib/librest.rb
+++ Formula/lib/librest.rb
@@ -1,10 +1,9 @@
 class Librest < Formula
   desc "Library to access RESTful web services"
   homepage "https://wiki.gnome.org/Projects/Librest"
-  url "https://download.gnome.org/sources/rest/0.8/rest-0.8.1.tar.xz"
-  sha256 "0513aad38e5d3cedd4ae3c551634e3be1b9baaa79775e53b2dba9456f15b01c9"
+  url "https://download.gnome.org/sources/rest/0.9/rest-0.9.1.tar.xz"
+  sha256 "9266a5c10ece383e193dfb7ffb07b509cc1f51521ab8dad76af96ed14212c2e3"
   license all_of: ["LGPL-2.1-or-later", "LGPL-3.0-or-later"]
-  revision 4
 
   # librest doesn't seem to follow the typical GNOME version scheme, so we
   # provide a regex to disable the `Gnome` strategy's version filtering.
@@ -28,22 +27,24 @@ class Librest < Formula
   end
 
   depends_on "gobject-introspection" => :build
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
   depends_on "pkg-config" => :build
   depends_on "glib"
-  depends_on "libsoup@2"
+  depends_on "json-glib"
+  depends_on "libsoup"
+
+  uses_from_macos "libxml2"
 
   def install
-    ENV.prepend_path "PKG_CONFIG_PATH", Formula["libsoup@2"].opt_lib/"pkgconfig"
-    ENV.prepend_path "XDG_DATA_DIRS", Formula["libsoup@2"].opt_share
-    ENV.prepend_path "XDG_DATA_DIRS", HOMEBREW_PREFIX/"share"
-
-    system "./configure", "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}",
-                          "--without-gnome",
-                          "--without-ca-certificates",
-                          "--enable-introspection=yes"
-    system "make", "install"
+    args = %w[
+      -Dexamples=false
+      -Dgtk_doc=false
+      -Dtests=false
+    ]
+    system "meson", "setup", "build", *args, *std_meson_args
+    system "meson", "compile", "-C", "build", "--verbose"
+    system "meson", "install", "-C", "build"
   end
 
   test do
@@ -60,16 +61,16 @@ class Librest < Formula
       }
     EOS
     glib = Formula["glib"]
-    libsoup = Formula["libsoup@2"]
+    libsoup = Formula["libsoup"]
     flags = %W[
-      -I#{libsoup.opt_include}/libsoup-2.4
+      -I#{libsoup.opt_include}/libsoup-3.0
       -I#{glib.opt_include}/glib-2.0
       -I#{glib.opt_lib}/glib-2.0/include
-      -I#{include}/rest-0.7
+      -I#{include}/rest-1.0
       -L#{libsoup.opt_lib}
       -L#{glib.opt_lib}
       -L#{lib}
-      -lrest-0.7
+      -lrest-1.0
       -lgobject-2.0
     ]
     system ENV.cc, "test.c", "-o", "test", *flags
