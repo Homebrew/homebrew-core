8b2eaf7827b67de74fc2c93090f0a8c6ad8996d4
Rui Chen <rui@chenrui.dev>
2024-04-16 13:52:29 -0400

    hickory-dns 0.24.1
    autobump: add hickory-dns
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/h/hickory-dns.rb
+++ Formula/h/hickory-dns.rb
@@ -1,8 +1,8 @@
 class HickoryDns < Formula
   desc "Rust based DNS client, server, and resolver"
   homepage "https://github.com/hickory-dns/hickory-dns"
-  url "https://github.com/hickory-dns/hickory-dns/archive/refs/tags/v0.24.0.tar.gz"
-  sha256 "e5158a8e412876768fc3d171ef4b2c3e0d4e99c1a1d082018b93bcda9fb31334"
+  url "https://github.com/hickory-dns/hickory-dns/archive/refs/tags/v0.24.1.tar.gz"
+  sha256 "3ca31dec004ca99133d4da2e9506ace9da9eb577cfc6d16264f7ebfe387c5a03"
   license any_of: ["Apache-2.0", "MIT"]
   head "https://github.com/hickory-dns/hickory-dns.git", branch: "main"
 
