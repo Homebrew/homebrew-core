f672a24e96a488d7a5d4f46a67f47559ebd6a59d
metaspartan <carsenk@gmail.com>
2024-04-22 13:04:46 -0600

    Update goasitop.rb

--- Formula/g/goasitop.rb
+++ Formula/g/goasitop.rb
@@ -1,3 +1,6 @@
+# typed: true
+# frozen_string_literal: true
+
 class Goasitop < Formula
     desc "Apple Silicon Monitor Top written in Go Lang"
     homepage "https://github.com/context-labs/goasitop"
@@ -15,3 +18,4 @@ class Goasitop < Formula
       system "#{bin}/goasitop", "--version"
     end
   end
+  
