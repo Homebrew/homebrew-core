84425682a2ef14ca819e692b7d3635fcc5007211
Carsen Klock <carsenk@gmail.com>
2024-04-23 21:32:22 -0600

    Update Formula/g/goasitop.rb
    Co-authored-by: Sean Molenaar <SMillerDev@users.noreply.github.com>

--- Formula/g/goasitop.rb
+++ Formula/g/goasitop.rb
@@ -8,6 +8,7 @@ class Goasitop < Formula
   url "https://github.com/context-labs/goasitop/archive/refs/tags/v0.0.7.tar.gz"
   sha256 "a17fb6b7ba87cd055371aa82abef7388cb6e8114cad92479f82eb58f5d9c46dc"
   license "MIT"
+
   depends_on "go" => :build
   depends_on arch: :arm64
   depends_on :macos
