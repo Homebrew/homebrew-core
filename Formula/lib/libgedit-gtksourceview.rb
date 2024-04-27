c9bdd6f72b599fcc5409cf8d70cd78e87c02bc7a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-04-27 00:42:44 +0000

    libgedit-gtksourceview 299.2.1

--- Formula/lib/libgedit-gtksourceview.rb
+++ Formula/lib/libgedit-gtksourceview.rb
@@ -1,8 +1,8 @@
 class LibgeditGtksourceview < Formula
   desc "Text editor widget for code editing"
   homepage "https://gedit-technology.github.io"
-  url "https://github.com/gedit-technology/libgedit-gtksourceview/releases/download/299.0.5/libgedit-gtksourceview-299.0.5.tar.xz"
-  sha256 "4bdac3c6dd885a2af8064a7265618ff8505b2324ab02fb00b0ce55e02978d3d6"
+  url "https://github.com/gedit-technology/libgedit-gtksourceview/releases/download/299.2.1/libgedit-gtksourceview-299.2.1.tar.xz"
+  sha256 "f94ea579636d73b4a783b9ec43d77bc9a43d4b633b3bf9ba9d7a011cadb5cb92"
   license "LGPL-2.1-only"
   head "https://github.com/gedit-technology/libgedit-gtksourceview.git", branch: "main"
 
