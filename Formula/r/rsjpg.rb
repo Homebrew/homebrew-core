ab27f5eed2d173d65d06f7ff43880c7220ac9544
steelPiber <pyh5523@gmail.com>
2024-05-16 23:11:44 +0900

    fix

--- Formula/r/rsjpg.rb
+++ Formula/r/rsjpg.rb
@@ -3,7 +3,7 @@ class Rsjpg < Formula
   homepage "https://github.com/steelPiber/rsjpg"
   url "https://github.com/steelPiber/rsjpg/archive/refs/tags/v0.0.2.tar.gz"
   sha256 "84a661ccc6a39ece8884dc35bdda60be5ccde76d856487580c7641d562d980db"
-  license "MIT"
+  license "Apache-2.0"
 
   depends_on "rust" => :build
 
@@ -11,10 +11,7 @@ class Rsjpg < Formula
     system "cargo", "install", *std_cargo_args
   end
 
-<<<<<<< HEAD
-=======
   test do
     system "#{bin}/rsjpg", "--version"
   end
->>>>>>> 8bf358facee (Fix license and version issues)
 end
