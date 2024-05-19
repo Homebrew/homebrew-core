e336d6561420248cfe3a5b7513eb7ecbb2256b43
Galileo Luna <gluna@getontop.com>
2024-05-19 16:29:37 -0300

    spqr: add new formula with license and test update

--- Formula/s/spqr.rb
+++ Formula/s/spqr.rb
@@ -1,15 +1,16 @@
 class Spqr < Formula
-    desc "A project for creating Go projects using hexagonal architecture easily and simply"
-    homepage "https://github.com/spqr-go/spqr"
-    url "https://github.com/spqr-go/spqr/releases/download/spqr/spqr"
-    sha256 "4848df26b2c33d6f263ecbe841cc8b0e627537b0c281abb3fc2235e6125c7704"
-    version "0.1.0"  
-  
-    def install
-      bin.install "spqr"
-    end
-  
-    test do
-      system "#{bin}/spqr", "--version"
-    end
+  desc "Project for creating Go projects using hexagonal architecture"
+  homepage "https://github.com/spqr-go/spqr"
+  url "https://github.com/spqr-go/spqr/releases/download/spqr/spqr"
+  version "0.1.0"
+  sha256 "4848df26b2c33d6f263ecbe841cc8b0e627537b0c281abb3fc2235e6125c7704"
+  license "MIT"
+
+  def install
+    bin.install "spqr"
   end
+
+  test do
+    system "#{bin}/spqr", "help"
+  end
+end
