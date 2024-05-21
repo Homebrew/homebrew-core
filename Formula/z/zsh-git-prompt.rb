209e4e6b08b19f366c43c92acd59df2608f68ada
herrernst <herr.ernst@gmail.com>
2024-05-21 12:17:10 +0200

    zsh-git-prompt 0.6 (new upstream)
    update zsh-git-prompt; previous upstream was unmaintained, switch to community maintained repo

--- Formula/z/zsh-git-prompt.rb
+++ Formula/z/zsh-git-prompt.rb
@@ -1,16 +1,22 @@
 class ZshGitPrompt < Formula
   desc "Informative git prompt for zsh"
-  homepage "https://github.com/olivierverdier/zsh-git-prompt"
-  url "https://github.com/olivierverdier/zsh-git-prompt/archive/refs/tags/v0.5.tar.gz"
-  sha256 "87e5a908369f402e975426ffd61a8800f1c04c0a293f1d4015a6fb1f4408e77d"
+  homepage "https://github.com/zsh-git-prompt/zsh-git-prompt"
+  url "https://github.com/zsh-git-prompt/zsh-git-prompt/archive/refs/tags/v0.6.tar.gz"
+  sha256 "b28a8249797b92b25e3c3156dc99153548a5b0877d2e6aa20be15caa719dcea2"
   license "MIT"
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "d4fa3836434d56704bd03f88f3e45557cac6e12cb6e17cc251635ecdcbc431eb"
   end
 
+  uses_from_macos "zsh"
+
   def install
-    prefix.install Dir["*.{sh,py}"]
+    prefix.install Dir["*.sh"] # for now, only zsh.sh
+    prefix.install "shell"
+    prefix.install "python"
+    # could also install "haskell" folder? but why
+    # prefix.install "haskell"
   end
 
   def caveats
@@ -21,8 +27,14 @@ class ZshGitPrompt < Formula
   end
 
   test do
-    system "git", "init"
-    zsh_command = ". #{opt_prefix}/zshrc.sh && git_super_status"
-    assert_match "master", shell_output("zsh -c '#{zsh_command}'")
+    system "git", "init", "--initial-branch=main"
+    # commit something, so we have a "clean" state
+    touch "testfile"
+    system "git", "add", "."
+    system "git", "commit", "-m", "testcommit"
+    system "git", "status"
+    # `cd .` triggers directory change, so git vars are updated
+    zsh_command = ". #{opt_prefix}/zshrc.sh && cd . && git_super_status"
+    assert_match "main", shell_output("zsh -c '#{zsh_command}'")
   end
 end
