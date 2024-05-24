d1dd09912bb64ecc2f572a4cf74bd512b07ff903
Nhan Doan <2561973+dnhn@users.noreply.github.com>
2024-05-24 08:38:23 +0700

    opensearch-dashboards 2.14.0

--- Formula/o/opensearch-dashboards.rb
+++ Formula/o/opensearch-dashboards.rb
@@ -4,8 +4,8 @@ class OpensearchDashboards < Formula
   desc "Open source visualization dashboards for OpenSearch"
   homepage "https://opensearch.org/docs/dashboards/index/"
   url "https://github.com/opensearch-project/OpenSearch-Dashboards.git",
-      tag:      "2.11.1",
-      revision: "989d8f41f37cca3275bf3fedc5c2057a717d1d64"
+      tag:      "2.14.0",
+      revision: "1dbbc2a2e911d58f935d289a28cac0488dd77fd7"
   license "Apache-2.0"
 
   bottle do
