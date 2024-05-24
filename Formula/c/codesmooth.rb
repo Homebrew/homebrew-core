4dc1362595f19f488c49b216a30c5f156753827e
Artur Kot <arturkot95@gmail.com>
2024-05-24 22:26:24 +0200

    fix lint errors

--- Formula/c/codesmooth.rb
+++ Formula/c/codesmooth.rb
@@ -1,7 +1,7 @@
 class Codesmooth < Formula
-  desc "Command Line Interface for CodeSmooth projects"
-  version "1.0.0"
+  desc "Command-line interface for CodeSmooth projects"
   homepage "https://codesmooth.dev"
+  version "1.0.0"
   url "https://storage.googleapis.com/codesmooth-resources/public/cli/codesmooth-1.0.0.tar.gz"
   sha256 "e8be488b5f188afa19de35306c31b8393089089d9dea1aac7a9927d6cce74477"
 
