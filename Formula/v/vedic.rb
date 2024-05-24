eb9b192d89b68758d003a06ef24de2c0fc525307
Pt. Prashant tripathi <26687933+PtPrashantTripathi@users.noreply.github.com>
2024-05-24 19:02:20 +0530

    Update vedic.rb
    By : @SumeetSinghJi

--- Formula/v/vedic.rb
+++ Formula/v/vedic.rb
@@ -1,15 +1,34 @@
 class Vedic < Formula
-  desc "Sanskrit programming language"
+  desc "Simple Sanskrit programming language"
   homepage "https://vedic-lang.github.io/"
   url "https://github.com/vedic-lang/vedic/archive/refs/tags/v2.0.6.tar.gz"
   sha256 "5453386fcebfa48a5f3d39bc832d19edaaaa98d8d36841428ff6a20db0dd0151"
   license "MIT"
   head "https://github.com/vedic-lang/vedic.git", branch: "main"
 
+  livecheck do
+    url :stable
+    regex(/release[._-](\d+(?:\.\d+)+)/i)
+    strategy :github_latest
+  end
+
   depends_on "rust" => :build
 
   def install
-    system "cargo", "install", *std_cargo_args(path: "cli")
+    cd "cli" do # use the cargo.toml file within the soure cli folder: ./vedic/cli/cargo.toml
+      system "cargo", "install", *std_cargo_args
+    end
+  end
+
+  def caveats
+    <<~EOS
+      Vedic-lang is a Sanskrit Programming Language
+
+      Documentation here: https://vedic-lang.github.io/
+
+      To get started, run:
+        vedic --help
+    EOS
   end
 
   test do
@@ -17,8 +36,6 @@ class Vedic < Formula
     (testpath/"hello.ved").write <<~EOS
       वद("नमस्ते विश्व!");
     EOS
-
     assert_match "नमस्ते विश्व!", shell_output("#{bin}/vedic hello.ved")
   end
 end
-
