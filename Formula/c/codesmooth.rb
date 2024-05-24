6e2b3accfd02baded4b3bd769e04de8278bde620
Artur Kot <arturkot95@gmail.com>
2024-05-24 22:30:35 +0200

    add license

--- Formula/c/codesmooth.rb
+++ Formula/c/codesmooth.rb
@@ -1,9 +1,10 @@
 class Codesmooth < Formula
-  desc "Command-line interface for CodeSmooth projects"
+  desc "Command Line Interface for CodeSmooth projects"
   homepage "https://codesmooth.dev"
   version "1.0.0"
   url "https://storage.googleapis.com/codesmooth-resources/public/cli/codesmooth-1.0.0.tar.gz"
-  sha256 "e8be488b5f188afa19de35306c31b8393089089d9dea1aac7a9927d6cce74477"
+  sha256 "1d6cc5bf9e04d3aa1d3c89f9661d3c123210c7f0a6f7fc349d4e558b15e98e2b"
+  license "Apache-2.0"
 
   def install
     bin.install "bin/codesmooth-macos" => "codesmooth"
