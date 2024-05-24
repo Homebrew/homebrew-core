f78edbe62b4759099a721de7d829adc413b965e2
Pt. Prashant tripathi <26687933+PtPrashantTripathi@users.noreply.github.com>
2024-05-24 09:34:52 +0530

    Apply suggestions from code review
    Co-authored-by: Rui Chen <rui@chenrui.dev>

--- Formula/v/vedic.rb
+++ Formula/v/vedic.rb
@@ -1,59 +1,24 @@
 class Vedic < Formula
   desc "vedic-lang is a Sanskrit programming language"
-  homepage "https://github.com/vedic-lang/vedic"
-  version "2.0.6"
+  homepage "https://vedic-lang.github.io/"
+  url "https://github.com/vedic-lang/vedic/archive/refs/tags/v2.0.6.tar.gz"
+  sha256 "5453386fcebfa48a5f3d39bc832d19edaaaa98d8d36841428ff6a20db0dd0151"
   license "MIT"
   head "https://github.com/vedic-lang/vedic.git", branch: "main"
 
-  if OS.mac?
-    if Hardware::CPU.arm?
-      url "https://github.com/vedic-lang/vedic/releases/download/v#{version}/vedic-darwin-aarch64.tar.gz"
-      sha256 "bbd827b70e20b8ac1821b5e56d9fa5e7195327ea50a71df912c3088795b1384e"
-    elsif Hardware::CPU.intel?
-      url "https://github.com/vedic-lang/vedic/releases/download/v#{version}/vedic-darwin-x86_64.tar.gz"
-      sha256 "bbd827b70e20b8ac1821b5e56d9fa5e7195327ea50a71df912c3088795b1384e"
-    else
-      # Unsupported platform
-      odie "This platform is not supported."
-    end
-  elsif OS.linux?
-    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/vedic-lang/vedic/releases/download/v#{version}/vedic-linux-gnu-aarch64.tar.xz"
-      sha256 "bbd827b70e20b8ac1821b5e56d9fa5e7195327ea50a71df912c3088795b1384e"
-    elsif Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://github.com/vedic-lang/vedic/releases/download/v#{version}/vedic-linux-gnu-x86_64.tar.xz"
-      sha256 "bbd827b70e20b8ac1821b5e56d9fa5e7195327ea50a71df912c3088795b1384e"
-    elsif Hardware::CPU.intel? && Hardware::CPU.is_32_bit?
-      url "https://github.com/vedic-lang/vedic/releases/download/v#{version}/vedic-linux-gnu-i686.tar.xz"
-      sha256 "bbd827b70e20b8ac1821b5e56d9fa5e7195327ea50a71df912c3088795b1384e"
-    elsif Hardware::CPU.arm? && Hardware::CPU.is_32_bit?
-      url "https://github.com/vedic-lang/vedic/releases/download/v#{version}/vedic-linux-gnueabihf-armv7.tar.xz"
-      sha256 "bbd827b70e20b8ac1821b5e56d9fa5e7195327ea50a71df912c3088795b1384e"
-    elsif Hardware::CPU.intel? && Hardware::CPU.is_64_bit? && OS::Linux::Musl.is_detected?
-      url "https://github.com/vedic-lang/vedic/releases/download/v#{version}/vedic-linux-musl-x86_64.tar.xz"
-      sha256 "bbd827b70e20b8ac1821b5e56d9fa5e7195327ea50a71df912c3088795b1384e"
-    else
-      # Unsupported platform
-      odie "This platform is not supported."
-    end
-  else
-    # Unsupported platform
-    odie "This platform is not supported."
-  end
+  depends_on "rust" => :build
 
   def install
-    bin.install "vedic"  # Install the binary
-  end
-
-  def caveats
-    <<~EOS
-      To get started, run:
-        vedic --help
-    EOS
+    system "cargo", "install", *std_cargo_args(path: "cli")
   end
 
   test do
-    # Simple test to verify the binary is installed
-    assert_match "Usage:", shell_output("#{bin}/vedic --help")
+    # hello world in vedic
+    (testpath/"hello.ved").write <<~EOS
+      वद("नमस्ते विश्व!");
+    EOS
+
+    assert_match "नमस्ते विश्व!", shell_output("#{bin}/vedic hello.ved")
   end
 end
+
