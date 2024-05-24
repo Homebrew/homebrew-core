91606277c0733057fdf304983b3eac2b85b0a5ca
Artur Kot <arturkot95@gmail.com>
2024-05-24 22:21:23 +0200

    update checksum

--- Formula/c/codesmooth.rb
+++ Formula/c/codesmooth.rb
@@ -3,7 +3,7 @@ class Codesmooth < Formula
   version "1.0.0"
   homepage "https://codesmooth.dev"
   url "https://storage.googleapis.com/codesmooth-resources/public/cli/codesmooth-1.0.0.tar.gz"
-  sha256 "b80ede38848488ac78e447c3915f503c0273868ccd66c31b2ceb8e9fa135b15c"
+  sha256 "e8be488b5f188afa19de35306c31b8393089089d9dea1aac7a9927d6cce74477"
 
   def install
     bin.install "bin/codesmooth-macos" => "codesmooth"
