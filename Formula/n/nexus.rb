5581bb11fcee4169653b05ec209c4c381abab44f
ZartX174698 <136252368+ZartX174698@users.noreply.github.com>
2024-06-02 20:06:45 -0500

    nexus 3.68.1-02

--- Formula/n/nexus.rb
+++ Formula/n/nexus.rb
@@ -1,8 +1,8 @@
 class Nexus < Formula
   desc "Repository manager for binary software components"
   homepage "https://www.sonatype.org/"
-  url "https://github.com/sonatype/nexus-public/archive/refs/tags/release-3.38.1-01.tar.gz"
-  sha256 "83b3a39e4d350d9786ce47410607fdd9ec04fca4f8451c0a763d8e22c5639e87"
+  url "https://github.com/sonatype/nexus-public/archive/refs/tags/release-3.68.1-02.tar.gz"
+  sha256 "678173e880e55be8b6e4654ebbc184f2cd4b244f3490ecaecab60c02b2299f55"
   license "EPL-1.0"
 
   # As of writing, upstream is publishing both v2 and v3 releases. The "latest"
