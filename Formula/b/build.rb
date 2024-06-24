659d22a74f73a10c4d49127b3dc0d390fff02622
Branch Vincent <branchevincent@gmail.com>
2024-06-23 18:44:22 -0700

    build: renamed from python-build

--- Formula/b/build.rb
+++ Formula/b/build.rb
@@ -1,4 +1,4 @@
-class PythonBuild < Formula
+class Build < Formula
   include Language::Python::Virtualenv
 
   desc "Simple, correct PEP 517 build frontend"
@@ -7,16 +7,6 @@ class PythonBuild < Formula
   sha256 "526263f4870c26f26c433545579475377b2b7588b6f1eac76a001e873ae3e19d"
   license "MIT"
   head "https://github.com/pypa/build.git", branch: "main"
-
-  bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "8a02b06273633d165aeb7f3a6f9c799c688e95a3e0e6a99dff4447c0d174b49c"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "8a02b06273633d165aeb7f3a6f9c799c688e95a3e0e6a99dff4447c0d174b49c"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "8a02b06273633d165aeb7f3a6f9c799c688e95a3e0e6a99dff4447c0d174b49c"
-    sha256 cellar: :any_skip_relocation, sonoma:         "40ca53f82b413997243774f2adf79eb0baa6900d9ed6db87c4269fa8d43f1908"
-    sha256 cellar: :any_skip_relocation, ventura:        "40ca53f82b413997243774f2adf79eb0baa6900d9ed6db87c4269fa8d43f1908"
-    sha256 cellar: :any_skip_relocation, monterey:       "40ca53f82b413997243774f2adf79eb0baa6900d9ed6db87c4269fa8d43f1908"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7f685d6de8ad4b0d9779ea800b6c3b4a5f7af3caa5d4b8ea8064dff279fb6906"
-  end
 
   depends_on "python@3.12"
 
