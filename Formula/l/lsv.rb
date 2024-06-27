6401f241100412f871d2826abfbd25d991a43fef
Mike Ward <mike@mike-ward.net>
2024-06-27 13:46:14 -0500

    fix name

--- Formula/l/lsv.rb
+++ Formula/l/lsv.rb
@@ -1,7 +1,7 @@
 class Lsv < Formula
   desc "Vlang implementation of ls"
   homepage "https://mike-ward.net/lsv/"
-  url "https://github.com/mike-ward/lsv/releases/download/2024.2/lsv_mac_m1.gz"
+  url "https://github.com/mike-ward/lsv/releases/download/2024.2/lsv_max_m1.gz"
   sha256 "af743cfb64afa597ba62c646d382fdcf82ff0daea9bed8cf80a69879fcff54e6"
   license "MIT"
 
