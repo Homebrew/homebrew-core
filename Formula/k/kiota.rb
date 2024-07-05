1b0b8db97b43ba8522cf82f0bf32e567f71718fc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-07-05 15:35:21 +0000

    kiota 1.16.0

--- Formula/k/kiota.rb
+++ Formula/k/kiota.rb
@@ -1,8 +1,8 @@
 class Kiota < Formula
   desc "OpenAPI based HTTP Client code generator"
   homepage "https://aka.ms/kiota/docs"
-  url "https://github.com/microsoft/kiota/archive/refs/tags/v1.11.1.tar.gz"
-  sha256 "cab39c2bffd20db3c8f31653ba1f0cb9fa3be785f38ff62422be76a34030dee6"
+  url "https://github.com/microsoft/kiota/archive/refs/tags/v1.16.0.tar.gz"
+  sha256 "38f218e29e5ddd9d18df44efdb0645f8095f9d1ecab439e09a881f2d7070c2c3"
   license "MIT"
   head "https://github.com/microsoft/kiota.git", branch: "main"
 
