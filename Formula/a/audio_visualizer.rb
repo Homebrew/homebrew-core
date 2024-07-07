77adfc0f4c9654ff1434dc8d6c6944980411042d
gituser12981u2 <paullwall@protonmail.com>
2024-07-06 21:01:40 -0700

    audio_visualizer: move on_macos above resources

--- Formula/a/audio_visualizer.rb
+++ Formula/a/audio_visualizer.rb
@@ -1,79 +1,78 @@
 class AudioVisualizer < Formula
-    include Language::Python::Virtualenv
+  include Language::Python::Virtualenv
 
-    desc "A janky, yet charming, terminal audio visualizer."
-    homepage "https://github.com/gituser12981u2/audio_visualizer"
-    url "https://github.com/gituser12981u2/audio_visualizer/releases/download/v1.0.0/audio_visualizer-1.0.0.tar.gz"
-    sha256 "7748b515d9aa490a90652bd32ab7f1baec641fbf0f8dd32ba74d79c6f25bf780"
-    license "MIT"
+  desc "Janky, yet charming, terminal audio visualizer"
+  homepage "https://github.com/gituser12981u2/audio_visualizer"
+  url "https://github.com/gituser12981u2/audio_visualizer/releases/download/v1.0.0/audio_visualizer-1.0.0.tar.gz"
+  sha256 "7748b515d9aa490a90652bd32ab7f1baec641fbf0f8dd32ba74d79c6f25bf780"
+  license "MIT"
 
-    depends_on "python@3.12"
-    depends_on "numpy"
-    depends_on "six"
-    depends_on "cmake" => :build
+  option "with-gpu", "Install GPU support via CuPy"
 
-    option "with-gpu", "Install GPU support via CuPy"
+  depends_on "cmake" => :build
+  depends_on "numpy"
+  depends_on "python@3.12"
+  depends_on "six"
 
-    resource "lupa" do
-        url "https://files.pythonhosted.org/packages/b6/13/3a0d2c231ae39bced22f14ded420915be1b88f030bfd2388900d89a74a4b/lupa-2.2.tar.gz"
-        sha256 "665a006bcf8d9aacdfdb953824b929d06a0c55910a662b59be2f157ab4c8924d"
-    end
-    
-    resource "PyAudio" do
-        url "https://files.pythonhosted.org/packages/26/1d/8878c7752febb0f6716a7e1a52cb92ac98871c5aa522cba181878091607c/PyAudio-0.2.14.tar.gz"
-        sha256 "78dfff3879b4994d1f4fc6485646a57755c6ee3c19647a491f790a0895bd2f87"
-    end
-    
-    resource "pynput" do
-        url "https://files.pythonhosted.org/packages/d7/74/a231bca942b1cd9c4bb707788be325a61d26c7998bd25e88dc510d4b55c7/pynput-1.7.6.tar.gz"
-        sha256 "3a5726546da54116b687785d38b1db56997ce1d28e53e8d22fc656d8b92e533c"
+  on_macos do
+    resource "pyobjc-core" do
+      url "https://files.pythonhosted.org/packages/b7/40/a38d78627bd882d86c447db5a195ff307001ae02c1892962c656f2fd6b83/pyobjc_core-10.3.1.tar.gz"
+      sha256 "b204a80ccc070f9ab3f8af423a3a25a6fd787e228508d00c4c30f8ac538ba720"
     end
 
-    resource "cupy" do
-        url "https://files.pythonhosted.org/packages/4a/2b/4f403ddc271ccdb867ccd03482fdc63deca8f23154c2a5ff7b024fa86653/cupy-13.2.0.tar.gz"
-        sha256 "e4dbd2b2ed4159a5cc0c0f98a710a014950eb2c16eeb455e956128f3b3bd0d51"
+    resource "pyobjc-framework-ApplicationServices" do
+      url "https://files.pythonhosted.org/packages/66/a6/3704b63c6e844739e3b7e324d1268fb6f7cb485550267719660779266c60/pyobjc_framework_applicationservices-10.3.1.tar.gz"
+      sha256 "f27cb64aa4d129ce671fd42638c985eb2a56d544214a95fe3214a007eacc4790"
     end
 
-    if OS.mac?
-        resource "pyobjc-core" do
-            url "https://files.pythonhosted.org/packages/b7/40/a38d78627bd882d86c447db5a195ff307001ae02c1892962c656f2fd6b83/pyobjc_core-10.3.1.tar.gz"
-            sha256 "b204a80ccc070f9ab3f8af423a3a25a6fd787e228508d00c4c30f8ac538ba720"
-        end
-      
-        resource "pyobjc-framework-ApplicationServices" do
-            url "https://files.pythonhosted.org/packages/66/a6/3704b63c6e844739e3b7e324d1268fb6f7cb485550267719660779266c60/pyobjc_framework_applicationservices-10.3.1.tar.gz"
-            sha256 "f27cb64aa4d129ce671fd42638c985eb2a56d544214a95fe3214a007eacc4790"
-        end
-      
-        resource "pyobjc-framework-Cocoa" do
-            url "https://files.pythonhosted.org/packages/a7/6c/b62e31e6e00f24e70b62f680e35a0d663ba14ff7601ae591b5d20e251161/pyobjc_framework_cocoa-10.3.1.tar.gz"
-            sha256 "1cf20714daaa986b488fb62d69713049f635c9d41a60c8da97d835710445281a"
-        end
-      
-        resource "pyobjc-framework-CoreText" do
-            url "https://files.pythonhosted.org/packages/9e/9f/d363cb1548808f538d7ae267a9fcb999dfb5693056fdaa5bc93de089cfef/pyobjc_framework_coretext-10.3.1.tar.gz"
-            sha256 "b8fa2d5078ed774431ae64ba886156e319aec0b8c6cc23dabfd86778265b416f"
-        end
-      
-        resource "pyobjc-framework-Quartz" do
-            url "https://files.pythonhosted.org/packages/f7/a2/f488d801197b9b4d28d0b8d85947f9e2c8a6e89c5e6d4a828fc7cccfb57a/pyobjc_framework_quartz-10.3.1.tar.gz"
-            sha256 "b6d7e346d735c9a7f147cd78e6da79eeae416a0b7d3874644c83a23786c6f886"
-        end
+    resource "pyobjc-framework-Cocoa" do
+      url "https://files.pythonhosted.org/packages/a7/6c/b62e31e6e00f24e70b62f680e35a0d663ba14ff7601ae591b5d20e251161/pyobjc_framework_cocoa-10.3.1.tar.gz"
+      sha256 "1cf20714daaa986b488fb62d69713049f635c9d41a60c8da97d835710445281a"
     end
 
-    def install
-        venv = virtualenv_create(libexec, "python3.12", system_site_packages: false)
-        
-        system "echo", "This might take a while."
-        resources.each do |r|
-            if r.name == "cupy" && !build.with?("gpu")
-                next
-            end
-            system "echo", "Installing #{r.name}"
-            venv.pip_install r
-        end
-      
-        venv.pip_install_and_link buildpath
-        system "echo", "Installation completed"
+    resource "pyobjc-framework-CoreText" do
+      url "https://files.pythonhosted.org/packages/9e/9f/d363cb1548808f538d7ae267a9fcb999dfb5693056fdaa5bc93de089cfef/pyobjc_framework_coretext-10.3.1.tar.gz"
+      sha256 "b8fa2d5078ed774431ae64ba886156e319aec0b8c6cc23dabfd86778265b416f"
     end
+
+    resource "pyobjc-framework-Quartz" do
+      url "https://files.pythonhosted.org/packages/f7/a2/f488d801197b9b4d28d0b8d85947f9e2c8a6e89c5e6d4a828fc7cccfb57a/pyobjc_framework_quartz-10.3.1.tar.gz"
+      sha256 "b6d7e346d735c9a7f147cd78e6da79eeae416a0b7d3874644c83a23786c6f886"
+    end
+  end
+
+  resource "lupa" do
+    url "https://files.pythonhosted.org/packages/b6/13/3a0d2c231ae39bced22f14ded420915be1b88f030bfd2388900d89a74a4b/lupa-2.2.tar.gz"
+    sha256 "665a006bcf8d9aacdfdb953824b929d06a0c55910a662b59be2f157ab4c8924d"
+  end
+
+  resource "PyAudio" do
+    url "https://files.pythonhosted.org/packages/26/1d/8878c7752febb0f6716a7e1a52cb92ac98871c5aa522cba181878091607c/PyAudio-0.2.14.tar.gz"
+    sha256 "78dfff3879b4994d1f4fc6485646a57755c6ee3c19647a491f790a0895bd2f87"
+  end
+
+  resource "pynput" do
+    url "https://files.pythonhosted.org/packages/d7/74/a231bca942b1cd9c4bb707788be325a61d26c7998bd25e88dc510d4b55c7/pynput-1.7.6.tar.gz"
+    sha256 "3a5726546da54116b687785d38b1db56997ce1d28e53e8d22fc656d8b92e533c"
+  end
+
+  resource "cupy" do
+    url "https://files.pythonhosted.org/packages/4a/2b/4f403ddc271ccdb867ccd03482fdc63deca8f23154c2a5ff7b024fa86653/cupy-13.2.0.tar.gz"
+    sha256 "e4dbd2b2ed4159a5cc0c0f98a710a014950eb2c16eeb455e956128f3b3bd0d51"
+  end
+
+  def install
+    venv = virtualenv_create(libexec, "python3.12", system_site_packages: false)
+
+    system "echo", "This might take a while."
+    resources.each do |r|
+      next if r.name == "cupy" && !build.with?("gpu")
+
+      system "echo", "Installing #{r.name}"
+      venv.pip_install r
+    end
+
+    venv.pip_install_and_link buildpath
+    system "echo", "Installation completed"
+  end
 end
