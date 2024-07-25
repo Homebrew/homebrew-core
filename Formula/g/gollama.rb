502a2df360a155d21234af7800d24d124f5b6c18
Aleksy Rudy <DanzigerGeist@icloud.com>
2024-07-25 09:50:29 +0200

    gollama 1.20.5

--- Formula/g/gollama.rb
+++ Formula/g/gollama.rb
@@ -18,6 +18,6 @@ class Gollama < Formula
   end
 
   test do
-    assert_match version, shell_output("#{bin}/gollama -v")
+    assert_match version.to_s, shell_output("#{bin}/gollama -v")
   end
 end
