e2fbb6fca8d2e0958f13c04a21744eab41e4aed7
Aleksy Rudy <DanzigerGeist@icloud.com>
2024-07-26 11:11:17 +0200

    added an additional test plus minor changes
    This commit adds the requested additional tests for the functionality of
    the app, plus removes the output file specific directly as suggested by
    the Homebrew maintainers.

--- Formula/g/gollama.rb
+++ Formula/g/gollama.rb
@@ -2,22 +2,23 @@ class Gollama < Formula
   desc "Go manage your Ollama models"
   homepage "https://smcleod.net"
   url "https://github.com/sammcj/gollama/archive/refs/tags/v1.20.4.tar.gz"
-  sha256 "17335e526725cd7952f2ac2b3a9d7f5f556cc0aba24fa14e894e88cfba108773"
+  sha256 "7b29114eefbe0b0c8727cef476c6729b4e5ed246e6f60d2a14119947f7ee5f34"
   license "MIT"
   head "https://github.com/sammcj/gollama.git", branch: "main"
 
   livecheck do
     url :stable
-    strategy :github_latest
+    strategy :github_releases
   end
 
   depends_on "go" => :build
 
   def install
-    system "go", "build", "-mod=readonly", *std_go_args(ldflags: "-s -w -X main.Version=#{version}", output: bin/"gollama")
+    system "go", "build", "-mod=readonly", *std_go_args(ldflags: "-s -w -X main.Version=#{version}")
   end
 
   test do
     assert_match version.to_s, shell_output("#{bin}/gollama -v")
+    assert_match "Search results for: test", shell_output("#{bin}/gollama -s test")
   end
 end
