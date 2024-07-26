3c85a9527542dc41385c333e4f245e0539404d3a
Aleksy Rudy <DanzigerGeist@icloud.com>
2024-07-26 09:32:26 +0200

    version changed to stable 1.20.4

--- Formula/g/gollama.rb
+++ Formula/g/gollama.rb
@@ -1,7 +1,7 @@
 class Gollama < Formula
   desc "Go manage your Ollama models"
   homepage "https://smcleod.net"
-  url "https://github.com/sammcj/gollama/archive/refs/tags/vv1.20.5.tar.gz"
+  url "https://github.com/sammcj/gollama/archive/refs/tags/v1.20.4.tar.gz"
   sha256 "17335e526725cd7952f2ac2b3a9d7f5f556cc0aba24fa14e894e88cfba108773"
   license "MIT"
   head "https://github.com/sammcj/gollama.git", branch: "main"
