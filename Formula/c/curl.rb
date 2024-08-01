d451b62d447d25388452df032612ce7d8e97d74f
redbaron-gt <george.tepnadze@gmail.com>
2024-08-01 22:20:51 +0400

    Update curl.rb to enable c-ares and --dns-servers support

--- Formula/c/curl.rb
+++ Formula/c/curl.rb
@@ -42,6 +42,7 @@ class Curl < Formula
   depends_on "openssl@3"
   depends_on "rtmpdump"
   depends_on "zstd"
+  depends_on "c-ares"
 
   uses_from_macos "krb5"
   uses_from_macos "openldap"
@@ -70,6 +71,7 @@ class Curl < Formula
       --without-libpsl
       --with-zsh-functions-dir=#{zsh_completion}
       --with-fish-functions-dir=#{fish_completion}
+      --enable-ares
     ]
 
     args << if OS.mac?
