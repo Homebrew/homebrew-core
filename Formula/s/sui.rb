1886d34cdeef3604a6de046afebc1c2e0581acad
John Martin <johnjosephmartin@icloud.com>
2024-08-12 16:16:02 -0700

    sui 1.31.1
    Created by https://github.com/mislav/bump-homebrew-formula-action
    
    From release: https://github.com/MystenLabs/sui/actions/runs/10360109256

--- Formula/s/sui.rb
+++ Formula/s/sui.rb
@@ -1,8 +1,8 @@
 class Sui < Formula
   desc "Next-generation smart contract platform powered by the Move programming language"
   homepage "https://sui.io"
-  url "https://github.com/MystenLabs/sui/archive/refs/tags/testnet-v1.30.3.tar.gz"
-  sha256 "60b9eb2ac965a38689a30201e991720e925f5c7f9c1bdc6fc2a929bef80228de"
+  url "https://github.com/MystenLabs/sui/archive/refs/tags/testnet-v1.31.1.tar.gz"
+  sha256 "7f6d156d14f8ec43c2527562e1c2e71a2f3badd2bbabee6abcbecb15ede9815d"
   license "Apache-2.0"
 
   livecheck do
