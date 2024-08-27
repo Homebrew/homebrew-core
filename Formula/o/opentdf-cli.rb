e58e84923b3965487828371ebedee894388586d1
Ryan Schumacher <j.r.schumacher@gmail.com>
2024-08-27 15:18:27 -0500

    rename to opentdf-cli

--- Formula/o/opentdf-cli.rb
+++ Formula/o/opentdf-cli.rb
@@ -21,19 +21,26 @@ class OpentdfCli < Formula
       -X github.com/opentdf/otdfctl/pkg/config.BuildTime=#{time.iso8601}
       -X github.com/opentdf/otdfctl/pkg/config.CommitSha=#{Utils.git_head}
     ].join(" ")
-    system "go", "build", *std_go_args(ldflags:), "-o", bin/"otdfctl"
+    system "go", "build", *std_go_args(ldflags:), "-o", bin/"opentdf-cli"
 
-    generate_completions_from_executable(bin/"otdfctl", "completion", base_name: "otdfctl")
+    generate_completions_from_executable(
+      bin/"opentdf-cli",
+      "completion",
+      base_name: "opentdf-cli",
+    )
   end
 
   test do
-    run_output = shell_output("#{bin}/otdfctl | sed -r \"s/\x1B[[0-9;]*[mK]//g\" 2>&1")
+    run_output = shell_output("#{bin}/opentdf-cli | sed -r \"s/\x1B[[0-9;]*[mK]//g\" 2>&1")
     assert_match "otdfctl - OpenTDF Control Tool", run_output
 
-    version_output = shell_output("#{bin}/otdfctl --version 2>&1")
+    version_output = shell_output("#{bin}/opentdf-cli --version 2>&1")
     assert_match version.to_s, version_output
-
-    profile_output = shell_output("#{bin}/otdfctl policy attributes list --host http://localhost:8080 --with-client-creds '{}' 2>&1")
+    profile_output = shell_output(
+      "#{bin}/opentdf-cli policy attributes list " \
+      "--host http://localhost:8080 " \
+      "--with-client-creds '{}' 2>&1",
+    )
     assert_match "Profile missing credentials", profile_output
   end
 end
