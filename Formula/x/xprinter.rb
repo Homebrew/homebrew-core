dabbcc25bcdf5c2c6fa0c9459b68caf9b081ecfb
Apple Mac mini intel <>
2024-08-30 16:36:17 +0800

    change xprinter.rb

--- Formula/x/xprinter.rb
+++ Formula/x/xprinter.rb
@@ -4,6 +4,8 @@ class Xprinter < Formula
   url "https://github.com/GalaxyCal/homebrew-xprinter/releases/download/v1.0.0/xprinter-1.0.0.tar.xz"
   sha256 "2bacdf67ca39448f2cf78a79f9a58c3ba0c070dd111fb90c4496c28d8572ff95"
 
+  depends_on "macos-specific-dependency"
+
   def install
     bin.install "xprinter"
     bin.install "xprinter-server"
