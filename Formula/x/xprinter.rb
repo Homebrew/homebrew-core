19b47bdaa54138f18e15cab13d98db6488478a68
Apple Mac mini intel <>
2024-08-30 16:57:14 +0800

    change xprinter.rb

--- Formula/x/xprinter.rb
+++ Formula/x/xprinter.rb
@@ -4,7 +4,7 @@ class Xprinter < Formula
   url "https://github.com/GalaxyCal/homebrew-xprinter/releases/download/v1.0.0/xprinter-1.0.0.tar.xz"
   sha256 "2bacdf67ca39448f2cf78a79f9a58c3ba0c070dd111fb90c4496c28d8572ff95"
 
-  depends_on "macos-specific-dependency"
+  depends_on :macos
 
   def install
     on_macos do
