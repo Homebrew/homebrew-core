22520e113b1b5fd3ce71fe48f64d319053a33b59
Kid <44045911+kidonng@users.noreply.github.com>
2024-09-08 23:10:19 +0800

    replicate 0.8.0 (new formula)

---
+++ Formula/r/replicate.rb
@@ -0,0 +1,19 @@
+class Replicate < Formula
+  desc "Run machine learning models in the cloud"
+  homepage "https://github.com/replicate/cli"
+  url "https://github.com/replicate/cli/archive/refs/tags/v0.8.0.tar.gz"
+  sha256 "d61c829ff44ca246cde7e551963cc2ba1b4be91b49c940f78ad1a79e55d1d3d0"
+  license "Apache-2.0"
+  head "https://github.com/replicate/cli.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w -X github.com/replicate/cli/internal.version=#{version}")
+    generate_completions_from_executable(bin/"replicate", "completion")
+  end
+
+  test do
+    assert_match "replicate version #{version}", shell_output("#{bin}/replicate --version")
+  end
+end
