234f0e3674dd736b47b52f59fb9c83def899045c
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2024-09-09 20:42:21 +0000

    gensio 2.8.8

--- Formula/g/gensio.rb
+++ Formula/g/gensio.rb
@@ -1,8 +1,8 @@
 class Gensio < Formula
   desc "Stream I/O Library"
   homepage "https://github.com/cminyard/gensio"
-  url "https://github.com/cminyard/gensio/releases/download/v2.8.6/gensio-2.8.6.tar.gz"
-  sha256 "3220816c23f34a4ccaea91eb8859c56233bcd8b2379b5f0e2b7c50074aa03cfb"
+  url "https://github.com/cminyard/gensio/releases/download/v2.8.8/gensio-2.8.8.tar.gz"
+  sha256 "a5a3e92ee1a706a451e668be30c4cbd6179d2376c1587eb004e882ae872552c0"
   license all_of: ["LGPL-2.1-only", "GPL-2.0-only", "Apache-2.0"]
 
   bottle do
