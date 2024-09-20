1c2adb081a373c5dda40fba537356445f9c05458
MicroProgramer <microprogramer@yahoo.com>
2024-09-20 17:29:27 +0500

    add gitgen formula

--- Formula/g/gitgen.rb
+++ Formula/g/gitgen.rb
@@ -3,10 +3,11 @@ class Gitgen < Formula
   
     desc "A CLI tool for generating git commits to boost your GitHub activity"
     homepage "https://pypi.org/project/gitgen/"
-    url "https://github.com/mubashardev/gitgen/archive/refs/tags/v0.1.1.tar.gz" # Replace with your latest version
-    sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+    url "https://github.com/mubashardev/gitgen/archive/refs/tags/v0.1.3.tar.gz" # Replace with your latest version
+    sha256 "291061dc02dc96ee0c706ec6bc573f10b0d39419b3b30c1ce4444ea5d2ebbb0b"
     license "MIT"
     revision 1
+    version "0.1.3"
     depends_on "python@3.9"
   
     def install
