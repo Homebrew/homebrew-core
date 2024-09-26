a3adeca9d285717fe507805db36119aa9531f321
Molly Stark <mollyc.stark@gmail.com>
2024-09-26 14:59:04 -0700

    Add gpush 2.0.1 (new formula)
    Run linters and tests locally before pushing to a remote git repository

---
+++ Formula/g/gpush.rb
@@ -0,0 +1,57 @@
+class Gpush < Formula
+  desc "Run linters and tests locally before pushing to a remote git repository"
+  homepage "https://github.com/nitidbit/gpush"
+  url "https://github.com/nitidbit/gpush/archive/refs/tags/2.0.1.tar.gz"
+  sha256 "6a484ce64141a57496ddffc56321942be72d4ee61f8c7dd0b0b9b7852df4199f"
+  license "MIT"
+
+  EXECUTABLES = [
+    "gpush_changed_files.rb",
+    "gpush_get_specs.rb",
+    "gpush_run_if_any.rb",
+    "gpush.rb",
+  ].freeze
+
+  OTHER_FILES = [
+    "gpushrc_default.yml",
+  ].freeze
+
+  def install
+    # Logging the start of the installation process
+    ohai "Starting installation of gpush"
+
+    # Ensure libexec directory exists
+    libexec.mkpath
+
+    # Copy all Ruby scripts (*.rb) to the libexec directory
+    ohai "Copying all Ruby scripts to the libexec directory"
+    Dir.glob("src/ruby/*.rb").each do |file|
+      libexec.install file
+    end
+
+    OTHER_FILES.each do |file|
+      libexec.install file
+    end
+
+    # Set execute permissions on the command files only
+    ohai "Making command files executable"
+    EXECUTABLES.each do |file|
+      chmod "+x", libexec/file
+
+      # Create wrapper scripts for each command file
+      bin_name = File.basename(file, ".rb") # Get the name without the .rb extension
+      (bin/bin_name).write <<~EOS
+        #!/bin/bash
+        exec ruby "#{libexec}/#{file}" "$@"
+      EOS
+      chmod "+x", bin/bin_name
+    end
+
+    # Confirming the installation
+    ohai "gpush installation completed"
+  end
+
+  test do
+    system bin/"gpush", "--version"
+  end
+end
