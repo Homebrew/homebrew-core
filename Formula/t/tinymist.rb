258a0bed2469c62da02a898434cd38992febe13c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-09-28 06:51:34 +0000

    tinymist 0.11.23

--- Formula/t/tinymist.rb
+++ Formula/t/tinymist.rb
@@ -1,8 +1,8 @@
 class Tinymist < Formula
   desc "Language server for Typst"
   homepage "https://github.com/Myriad-Dreamin/tinymist"
-  url "https://github.com/Myriad-Dreamin/tinymist/archive/refs/tags/v0.11.22.tar.gz"
-  sha256 "ebb0fde8edca6daed57129a377616344284e1f870e48051ce5401814153d355c"
+  url "https://github.com/Myriad-Dreamin/tinymist/archive/refs/tags/v0.11.23.tar.gz"
+  sha256 "8fd66bad579e645f8974b158421a838f5a2bcce1b1228b3c03f6673cb7a540b9"
   license "Apache-2.0"
   head "https://github.com/Myriad-Dreamin/tinymist.git", branch: "main"
 
