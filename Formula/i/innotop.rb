3f70bf34731c93cd68dba2adcbfa28930d20f57f
Rui Chen <rui@chenrui.dev>
2024-09-27 21:16:39 -0400

    innotop 1.15.1
    autobump: add innotop
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    
    innotop: update deps
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/i/innotop.rb
+++ Formula/i/innotop.rb
@@ -1,10 +1,9 @@
 class Innotop < Formula
   desc "Top clone for MySQL"
   homepage "https://github.com/innotop/innotop/"
-  url "https://github.com/innotop/innotop/archive/refs/tags/v1.13.0.tar.gz"
-  sha256 "6ec91568e32bda3126661523d9917c7fbbd4b9f85db79224c01b2a740727a65c"
+  url "https://github.com/innotop/innotop/archive/refs/tags/v1.15.1.tar.gz"
+  sha256 "8aa53aa54d2a81ff1c62a2fe1095d373778f7a755e150f8196f9702775b3f407"
   license any_of: ["GPL-2.0-only", "Artistic-1.0-Perl"]
-  revision 9
   head "https://github.com/innotop/innotop.git"
 
   bottle do
@@ -37,13 +36,13 @@ class Innotop < Formula
   end
 
   resource "DBI" do
-    url "https://cpan.metacpan.org/authors/id/T/TI/TIMB/DBI-1.643.tar.gz"
-    sha256 "8a2b993db560a2c373c174ee976a51027dd780ec766ae17620c20393d2e836fa"
+    url "https://cpan.metacpan.org/authors/id/H/HM/HMBRAND/DBI-1.645.tgz"
+    sha256 "e38b7a5efee129decda12383cf894963da971ffac303f54cc1b93e40e3cf9921"
   end
 
   resource "DBD::mysql" do
-    url "https://cpan.metacpan.org/authors/id/D/DV/DVEEDEN/DBD-mysql-5.008.tar.gz"
-    sha256 "a2324566883b6538823c263ec8d7849b326414482a108e7650edc0bed55bcd89"
+    url "https://cpan.metacpan.org/authors/id/D/DV/DVEEDEN/DBD-mysql-5.009.tar.gz"
+    sha256 "8552d90dfddee9ef36e7a696da126ee1b42a1a00fbf2c6f3ce43ca2c63a5b952"
   end
 
   resource "Term::ReadKey" do
