dee9bac979d90afa8bd220edaab14d4948e48e51
Rui Chen <rui@chenrui.dev>
2024-09-29 14:43:08 -0400

    rtorrent 0.10.0
    autobump: add rtorrent
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/r/rtorrent.rb
+++ Formula/r/rtorrent.rb
@@ -1,10 +1,9 @@
 class Rtorrent < Formula
   desc "Ncurses BitTorrent client based on libtorrent-rakshasa"
   homepage "https://github.com/rakshasa/rtorrent"
-  url "https://github.com/rakshasa/rtorrent/releases/download/v0.9.8/rtorrent-0.9.8.tar.gz"
-  sha256 "9edf0304bf142215d3bc85a0771446b6a72d0ad8218efbe184b41e4c9c7542af"
+  url "https://github.com/rakshasa/rtorrent/releases/download/v0.10.0/rtorrent-0.10.0.tar.gz"
+  sha256 "cc65bba7abead24151f10af116eca2342b0c320fdff3cb8d604c0af09215d3aa"
   license "GPL-2.0-or-later"
-  revision 3
 
   bottle do
     sha256 cellar: :any,                 arm64_sequoia:  "d73c40d99e7cfbaa067a7c3b405ac28501376977857a68a66a9468122aa1d850"
