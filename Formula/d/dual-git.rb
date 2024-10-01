64e7c17428b89b6792e439f295c1701cbfd9f56d
Halil Sacpapa <halil.bagdadi@epitech.eu>
2024-10-01 16:30:28 +0200

    Adding dual-git formula v0.1

---
+++ Formula/d/dual-git.rb
@@ -0,0 +1,30 @@
+class DualGit < Formula
+  desc "Shell script to manage multiple git account with different ssh key"
+  homepage "https://github.com/HalilSacpapa/dual_git"
+  url "https://github.com/HalilSacpapa/dual_git/archive/refs/heads/main.zip"
+  version "0.1"
+  sha256 "371b009384e504446bf4066167df96394ec6f5ab9feb579f294ea16d433d8620"
+  license "OpenSSL 3.0"
+
+  depends_on "bash"
+
+  def install
+    libexec.install "dual_git.sh" => "dgit"
+    libexec.install "setup_dual_ssh.sh"
+    libexec.install "setup_clone_ssh.sh"
+    libexec.install "manage_repo_origin.sh"
+
+    (bin/"dgit").write_env_script libexec/"dgit", {
+      PATH: "#{libexec}:$PATH"
+    }
+
+    chmod 0755, libexec/"dgit"
+    chmod 0755, libexec/"setup_dual_ssh.sh"
+    chmod 0755, libexec/"setup_clone_ssh.sh"
+    chmod 0755, libexec/"manage_repo_origin.sh"
+  end
+
+  test do
+    # system "#{bin}/dgit", "--help"
+  end
+end
