b0aa339aaea9e8cad2686c37ddd3e08d4e061ba5
Sean Molenaar <SMillerDev@users.noreply.github.com>
2024-10-03 10:40:22 +0200

    Update ortp.rb

--- Formula/o/ortp.rb
+++ Formula/o/ortp.rb
@@ -12,8 +12,8 @@ class Ortp < Formula
     # bctoolbox appears to follow ortp's version. This can be verified at the GitHub mirror:
     # https://github.com/BelledonneCommunications/bctoolbox
     resource "bctoolbox" do
-      url "https://gitlab.linphone.org/BC/public/bctoolbox/-/archive/5.3.84/bctoolbox-5.3.84.tar.bz2"
-      sha256 "1c9ec26a91e74f720b16416a0e9e5e84c8aeb04b3fc490c22a4b2fc10ab6d5e3"
+      url "https://gitlab.linphone.org/BC/public/bctoolbox/-/archive/5.3.88/bctoolbox-5.3.88.tar.bz2"
+      sha256 "21e8916f4efa24117202b97a9f3f88ea436c52e494dbb48fd9ad210cf1359f30"
     end
   end
 
