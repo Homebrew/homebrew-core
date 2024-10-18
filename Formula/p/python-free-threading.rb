a7b0c92e063c8556fc1c90769c485fee3cef31fa
johng <john.griffith243@gmail.com>
2024-10-18 08:18:11 +0100

    fixup

--- Formula/p/python-free-threading.rb
+++ Formula/p/python-free-threading.rb
@@ -1,4 +1,4 @@
-class PythonFreethreading < Formula
+class PythonFreeThreading < Formula
   desc "Interpreted, interactive, object-oriented programming language"
   homepage "https://www.python.org/"
   url "https://www.python.org/ftp/python/3.13.0/Python-3.13.0.tgz"
