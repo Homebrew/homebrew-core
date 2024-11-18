97b11dd630ccdad1da52daad95824059b5d7bd70
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-11-18 21:23:49 +0000

    astgen 3.20.0

--- Formula/a/astgen.rb
+++ Formula/a/astgen.rb
@@ -1,31 +1,37 @@
 class Astgen < Formula
   desc "Generate AST in json format for JS/TS"
   homepage "https://github.com/joernio/astgen"
-  url "https://github.com/joernio/astgen/archive/refs/tags/v3.17.0.tar.gz"
-  sha256 "b989ca182c838e4e0aca56d0ddf55167884f7de7425e8557d1533a4c2f15de11"
+  url "https://github.com/joernio/astgen/archive/refs/tags/v3.20.0.tar.gz"
+  sha256 "73dd16943f0bd10e2d171b7a4b056aa10fa0efd1b0f6f41d19584ec83c22597f"
   license "Apache-2.0"
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "8a3ff787bb98219536e744663035f15f53b7ee2db59ffee934b0a422fc73476a"
   end
 
+  depends_on "yarn" => :build
   depends_on "node"
 
   uses_from_macos "zlib"
 
   def install
-    # Disable custom postinstall script
-    system "npm", "install", *std_npm_args, "--ignore-scripts"
-    bin.install_symlink Dir["#{libexec}/bin/*"]
+    system "yarn"
+    system "yarn", "build"
+    system "yarn", "binary"
+
+    os = OS.mac? ? "macos" : OS.kernel_name.downcase
+    arch = Hardware::CPU.intel? ? "x64" : Hardware::CPU.arch.to_s
+    bin.install "astgen-#{os}-#{arch}" => "astgen"
   end
 
   test do
-    (testpath/"main.js").write <<~JS
+    test_file = testpath/"main.js"
+    test_file.write <<~JS
       console.log("Hello, world!");
     JS
 
     assert_match "Converted AST", shell_output("#{bin}/astgen -t js -i . -o #{testpath}/out")
-    assert_match '"fullName": "main.js"', (testpath/"out/main.js.json").read
-    assert_match '"0": "Console"', (testpath/"out/main.js.typemap").read
+    assert_match "\"fullName\":\"#{test_file}\"", (testpath/"out/main.js.json").read
+    assert_match '"0":"Console"', (testpath/"out/main.js.typemap").read
   end
 end
