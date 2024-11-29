4f7cbe1fd7307a6f6ef68d3e440bd2ff695b8ea2
han-nwin <hannguyen.win@gmail.com>
2024-11-29 14:12:54 -0600

    Update codeflow.rb

--- Formula/codeflow.rb
+++ Formula/codeflow.rb
@@ -1,5 +1,5 @@
 class Codeflow < Formula
-  desc "CodeFlow CLI: A tool for dynamic text-based content display and interaction"
+  desc "Tool for dynamic text-based content display and interaction"
   homepage "https://github.com/han-nwin/codeflow"
   url "https://github.com/han-nwin/codeflow/releases/download/v1.0.0/codeflow-mac.tar.gz"
   sha256 "3a1e00f669b38801adc1f5f4ef13efdd21be3bd40cf9bb3cd9fa2b5ed8f77dea"
@@ -10,6 +10,6 @@ class Codeflow < Formula
   end
 
   test do
-    assert_match "CodeFlow CLI", shell_output("#{bin}/codeflow -version")
+    assert_match "codeflow version 1.0.0", shell_output("#{bin}/codeflow -version")
   end
 end
