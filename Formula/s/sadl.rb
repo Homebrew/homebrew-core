5183516d7ee3c72694fc8731b88bb5db22730e49
Cooper D'Andilly <151992386+CooperDActor-bytes@users.noreply.github.com>
2024-12-06 00:36:11 +1100

    Update sadl.rb

--- Formula/s/sadl.rb
+++ Formula/s/sadl.rb
@@ -1,18 +1,15 @@
 class Sadl < Formula
-  desc "saltyaust downloader in rust"
-  homepage "https://sadl.saltyaus.space/"
+  desc "downloader for a bunch of things"
+  homepage "https://github.com/CooperDActor-bytes/sadl"
   url "https://github.com/CooperDActor-bytes/sadl/archive/v1.0.1.tar.gz"
   sha256 "e0895e2d4ccbd101eb51b0e401679d67297c1056db6dca16713320d980dd6915"
-
   depends_on "rust" => :build
 
   def install
-    system "cargo", "build", "--release"
-    bin.install "target/release/sadl"
-    man1.install "man/sadl.1"
+    system "cargo", "install", "--path", "."
   end
 
   test do
-    system "#{bin}/sadl", "--version"
+    system "#{bin}/sadl", "--help"
   end
 end
