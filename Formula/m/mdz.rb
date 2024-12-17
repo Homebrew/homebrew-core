00f363c1bac812ce36f7041c976151d78513f56a
maxwelbm <maxwelbm@hotmail.com>
2024-12-17 12:23:40 -0300

    mdz 1.31.0 (new formula)
    Add a test to validate the output of the 'mdz configure' command, ensuring it correctly displays client ID, client secret, URL API Auth, and URL API Ledger.
    
    This complements the initial test for the '--version' command.
    
    Repository: https://github.com/LerianStudio/midaz
    Version: 1.31.0
    License: Apache-2.0

--- Formula/m/mdz.rb
+++ Formula/m/mdz.rb
@@ -1,8 +1,8 @@
 class Mdz < Formula
   desc "CLI for the mdz ledger Open Source"
   homepage "https://github.com/LerianStudio/midaz"
-  url "https://github.com/LerianStudio/midaz/archive/refs/tags/v1.30.0.tar.gz"
-  sha256 "be3d5befc9be00119257c63e47051042e594770706e1e3c16e453dc2bc806ca2"
+  url "https://github.com/LerianStudio/midaz/archive/refs/tags/v1.31.0.tar.gz"
+  sha256 "16b0e877091c1ef6fc466e52c4281a9407081459264f3c8bb7917699687224fa"
   license "Apache-2.0 license"
 
   depends_on "go" => :build
@@ -21,6 +21,14 @@ class Mdz < Formula
   end
 
   test do
-    assert_match version.to_s, shell_output("#{bin}/azion --version")
+    assert_match "Mdz CLI #{version}", shell_output("#{bin}/mdz --version")
+
+    output = shell_output("#{bin}/mdz configure --client-id 9670e0ca55a29a466d31 --client-secret dd03f916cacf4a98c6a413d9c38ba102dce436a9 --url-api-auth http://127.0.0.1:8080 --url-api-ledger http://127.0.0.1:3000")
+
+    assert_match "client-id:       9670e0ca55a29a466d31", output
+    assert_match "client-secret:   dd03f916cacf4a98c6a413d9c38ba102dce436a9", output
+    assert_match "url-api-auth:    http://127.0.0.1:8080", output
+    assert_match "url-api-ledger:  http://127.0.0.1:3000", output
+
   end
 end
