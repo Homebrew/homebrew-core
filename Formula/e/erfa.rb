c509707e966017f7597f02d020bd4b650441744e
chenxu <chenxu@mail.ustc.edu.cn>
2024-12-24 09:51:21 +0800

    license
    http://www.iausofa.org/tandc.html

--- Formula/e/erfa.rb
+++ Formula/e/erfa.rb
@@ -3,6 +3,7 @@ class Erfa < Formula
   homepage "https://github.com/liberfa/erfa"
   url "https://github.com/liberfa/erfa/archive/refs/tags/v2.0.1.tar.gz"
   sha256 "d5469fbd0b212b3c7270c1da15c9bd82f37da9218fc89627f98283d27b416cbf"
+  license "SOFA Software License"
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
