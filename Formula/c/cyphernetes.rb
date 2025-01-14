9281c1b43f87ce7cfe7a3fb718ddf39590c41ded
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-14 13:53:00 +0000

    cyphernetes 0.15.2

--- Formula/c/cyphernetes.rb
+++ Formula/c/cyphernetes.rb
@@ -1,8 +1,8 @@
 class Cyphernetes < Formula
   desc "Kubernetes Query Language"
   homepage "https://cyphernet.es"
-  url "https://github.com/AvitalTamir/cyphernetes/archive/refs/tags/v0.15.1.tar.gz"
-  sha256 "304d80ee9c103bdcc0ceaf6337fd95cdb1f68f8fb5b834afe281c0acbe19d08d"
+  url "https://github.com/AvitalTamir/cyphernetes/archive/refs/tags/v0.15.2.tar.gz"
+  sha256 "6f75b0ffd3b479f8c4f52e4a70922894fdc18382e52d960e05047cb2fadbd7c6"
   license "Apache-2.0"
 
   bottle do
