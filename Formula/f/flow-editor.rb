7200f7a0725945c09faadbae102b612aa932801d
plyght <nico.c.jaffer@gmail.com>
2025-01-16 11:43:46 -0500

    flow-editor 0.2.1 (new formula)
    Co-authored-by: Uzair Aftab <48220549+Uzaaft@users.noreply.github.com>
    
    Update Formula/f/flow-editor.rb
    
    Co-authored-by: Uzair Aftab <48220549+Uzaaft@users.noreply.github.com>

--- Formula/f/flow-editor.rb
+++ Formula/f/flow-editor.rb
@@ -1,8 +1,15 @@
 class FlowEditor < Formula
+  version "0.2.1"
   desc "Flow Control: a programmer's text editor"
   homepage "https://github.com/neurocyte/flow"
-  url "https://github.com/neurocyte/flow/archive/refs/tags/#{version}.tar.gz"
-  sha256 "826097db34fe8ed012a0409872b1d46f9aa950949c551faf82a6c3f2b184532d"
+  stable do
+    url "https://github.com/neurocyte/flow/archive/refs/tags/#{version}.tar.gz"
+    sha256 "826097db34fe8ed012a0409872b1d46f9aa950949c551faf82a6c3f2b184532d"
+  end
+
+  head do
+    url "https://github.com/neurocyte/flow.git", branch: "master"
+  end
   license "MIT"
 
   depends_on "zig"
