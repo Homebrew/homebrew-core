94de4333dc36a4eaa94f84304409c8e5c7f73e04
“zheng0116” <2387014174@qq.com>
2025-01-26 16:50:06 +0800

    codstts: fix style issues

--- Formula/c/codstts.rb
+++ Formula/c/codstts.rb
@@ -1,12 +1,12 @@
 class Codstts < Formula
-  desc "A code statistics tool written in Rust that analyzes programming language distribution in projects."
+  desc "Code statistics tool analyzing programming language distribution"
   homepage "https://github.com/zheng0116/codstts"
-  url "https://github.com/zheng0116/codstts/archive/v0.1.0.tar.gz"
+  url "https://github.com/zheng0116/codstts/archive/refs/tags/v0.1.0.tar.gz"
   sha256 "31e6b46b8f43a52a4b036d07b50ea7faa11f562c01582ecda6d861679ca3f3d8"
   license "MIT"
 
   depends_on "rust" => :build
-  
+
   def install
     system "cargo", "install", "--locked", "--root", prefix, "--path", "."
   end
