c9ce4be6723b96fceaa43e80740f0e6ce3f588f0
not-me-for-sure <188757791+not-me-for-sure@users.noreply.github.com>
2025-02-03 22:52:07 +0100

    Update Formula/s/stashapp.rb
    Fix language
    
    Co-authored-by: Mike McQuaid <mike@mikemcquaid.com>

--- Formula/s/stashapp.rb
+++ Formula/s/stashapp.rb
@@ -1,5 +1,5 @@
 class Stashapp < Formula
-  desc "Organizer for your porn"
+  desc "Organizer for your explicit images"
   homepage "https://stashapp.cc"
   url "https://github.com/stashapp/stash/archive/refs/tags/v0.27.2.tar.gz"
   sha256 "23402a61329d3c57f0d161e469b07a14a7672f799ff9d97bf082c1b67ace2dea"
