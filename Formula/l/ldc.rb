11e22e410ea7cf99caa4ff4dbcb956b4db2b4ed5
Carlo Cabrera <github@carlo.cab>
2025-02-04 20:41:36 +0100

    ldc: revision bump (llvm 20.1.1)

--- Formula/l/ldc.rb
+++ Formula/l/ldc.rb
@@ -4,6 +4,7 @@ class Ldc < Formula
   url "https://github.com/ldc-developers/ldc/releases/download/v1.40.0/ldc-1.40.0-src.tar.gz"
   sha256 "80a3ddd7b7292818cdf6c130e55f1246a19b5fce96139a49b45ccf4e2be99e5f"
   license "BSD-3-Clause"
+  revision 1
   head "https://github.com/ldc-developers/ldc.git", branch: "master"
 
   livecheck do
