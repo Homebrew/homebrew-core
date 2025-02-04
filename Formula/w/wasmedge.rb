c81394a8a019219f0f0b53a2f3e5e94505e8988e
Carlo Cabrera <github@carlo.cab>
2025-02-04 20:41:50 +0100

    wasmedge: revision bump (llvm 20.1.1)

--- Formula/w/wasmedge.rb
+++ Formula/w/wasmedge.rb
@@ -4,7 +4,7 @@ class Wasmedge < Formula
   url "https://github.com/WasmEdge/WasmEdge/releases/download/0.14.1/WasmEdge-0.14.1-src.tar.gz"
   sha256 "e5a944975fb949ecda73d6fe80a86507deb2d0a221b2274338807b63758350b4"
   license "Apache-2.0"
-  revision 1
+  revision 2
   head "https://github.com/WasmEdge/WasmEdge.git", branch: "master"
 
   bottle do
