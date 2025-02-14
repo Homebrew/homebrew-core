bda738033e32aa6702b5c93bb55dda92c6807f02
Mike Burton <mb-github@niskala.org>
2025-02-13 19:25:25 -0800

    statesmith 0.17.4 (new formula)

---
+++ Formula/s/statesmith.rb
@@ -0,0 +1,20 @@
+class Statesmith < Formula
+  desc "A state machine code generation tool suitable for bare metal, embedded and more."
+  homepage "https://github.com/StateSmith/StateSmith"
+  url "https://github.com/StateSmith/StateSmith/archive/refs/tags/cli-v0.17.4.tar.gz"
+  sha256 "a4f86dcd9a1e59042f20e134e61f2af87534065ceb0a204f70b20e62eef1ab22"
+  license "Apache-2.0"
+
+  depends_on "dotnet" => :build
+
+  def install
+    chdir "src/StateSmith.Cli" do
+      system "dotnet", "publish", "-c", "Release", "-p:PublishSingleFile=true", "--self-contained", "--framework", "net8.0", "/p:DefineConstants=SS_SINGLE_FILE_APPLICATION"
+      bin.install "./bin/Release/net8.0/osx-arm64/publish/StateSmith.Cli" => "ss.cli"
+    end
+  end
+
+  test do
+    system "#{bin}/ss.cli", "--version"
+  end
+end
