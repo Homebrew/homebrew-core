dca5331de7b9c0d9be97ea99b2398d075ef5f6ee
Jon Caraveo <caraveo@me.com>
2025-02-22 12:32:54 -0700

    aske 0.1.0: add new formula: verbose framework initializer
    A simple tool to initialize python framework.

---
+++ Formula/a/aske.rb
@@ -0,0 +1,34 @@
+class Aske < Formula
+  include Language::Python::Virtualenv
+
+  desc "Platform Architect Development Framework"
+  homepage "https://github.com/caraveo/aske"
+  url "https://github.com/Caraveo/Aske/releases/download/alpha/aske-0.1.0.tar.gz"
+  sha256 "c57ba6c9f56facf351e8632a3a9999d22d8df207844a3e1953538b47c52eb7f1"
+  license "MIT"
+
+  depends_on "python@3.11"
+
+  resource "click" do
+    url "https://files.pythonhosted.org/packages/96/d3/f04c7bfcf5c1862a2a5b845c6b2b360488cf47af55dfa79c98f6a6bf98b5/click-8.1.7.tar.gz"
+    sha256 "ca9853ad459e787e2192211578cc907e7594e294c7ccc834310722b41b9ca6de"
+  end
+
+  resource "python-dotenv" do
+    url "https://files.pythonhosted.org/packages/bc/57/e84d88dfe0aec03b7a2d4327012c1627ab5f03652216c63d49846d7a6c58/python-dotenv-1.0.1.tar.gz"
+    sha256 "e324ee90a023d808f1959c46bcbc04446a10ced277783dc6ee09987c37ec10ca"
+  end
+
+  resource "PyYAML" do
+    url "https://files.pythonhosted.org/packages/cd/e5/af35f7ea75cf72f2cd079c95ee16797de7cd71f29ea7c68ae5ce7be1eda0/PyYAML-6.0.1.tar.gz"
+    sha256 "bfdf460b1736c775f2ba9f6a92bca30bc2095067b8a9d77876d1fad6cc3b4a43"
+  end
+
+  def install
+    virtualenv_install_with_resources
+  end
+
+  test do
+    system bin/"aske", "--version"
+  end
+end 
