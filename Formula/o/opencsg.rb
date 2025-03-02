c6ec7e7aec84bf751242f3bdbdf9fd4912fd2348
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-02 23:11:50 +0000

    opencsg 1.8.0
    opencsg: remove patch
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/o/opencsg.rb
+++ Formula/o/opencsg.rb
@@ -1,8 +1,8 @@
 class Opencsg < Formula
   desc "Constructive solid geometry rendering library"
   homepage "https://www.opencsg.org/"
-  url "https://www.opencsg.org/OpenCSG-1.7.0.tar.gz"
-  sha256 "b892decc81a9e67c2c4d25c5399d576d8f77c3b0c05260606743243c86539df8"
+  url "https://www.opencsg.org/OpenCSG-1.8.0.tar.gz"
+  sha256 "cb2fca02f73d9846566a97cd40863a68143a141aff34c75935be452e52efdb10"
   license "GPL-2.0-or-later"
 
   livecheck do
@@ -18,17 +18,19 @@ class Opencsg < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "b37ea3b9c6749f57cf8b91102f334a161b707a27e9c5d4f939ea946b445207e0"
   end
 
-  depends_on "qt" => :build
+  depends_on "cmake" => :build
   depends_on "glew"
 
-  def install
-    # Disable building examples
-    inreplace "opencsg.pro", "src example", "src"
+  # Upstream pr ref: https://github.com/floriankirsch/OpenCSG/pull/15
+  patch do
+    url "https://github.com/floriankirsch/OpenCSG/commit/2d618a8145388a4fe8da2475725ad9e84a683076.patch?full_index=1"
+    sha256 "1728803d091e44967b6bb788bdf1270d974d08da7e9417b200c40cc44b7b1900"
+  end
 
-    system "qmake", "-r", "INSTALLDIR=#{prefix}",
-                          "INCLUDEPATH+=#{Formula["glew"].opt_include}",
-                          "LIBS+=-L#{Formula["glew"].opt_lib} -lGLEW"
-    system "make", "install"
+  def install
+    system "cmake", "-S", ".", "-B", "build", "-DBUILD_EXAMPLE=OFF", *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
   end
 
   test do
