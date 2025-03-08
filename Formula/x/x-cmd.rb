85e47f362e715e1d6af70338622dfcaaab8b9a4d
lunrenyi <liaoxuanbin@x-cmd.com>
2025-03-08 18:56:55 +0800

    x-cmd: Adjust download source, test, and supplement license

--- Formula/x/x-cmd.rb
+++ Formula/x/x-cmd.rb
@@ -1,26 +1,24 @@
 class XCmd < Formula
   desc "Bootstrap 1000+ command-line tools in seconds"
   homepage "https://x-cmd.com"
-  url "https://raw.githubusercontent.com/x-cmd/release/main/dist/v0.5.5/allinone.tgz"
-  sha256 "362928f94aa56ae5ccd9c651584c35c1207097b730745eba5c6cba0d7fcf1d45"
-  license "AGPL-3.0-only"
+  url "https://github.com/x-cmd/x-cmd/archive/refs/tags/v0.5.5.tar.gz"
+  sha256 "5f954c8791c89df3d231f4e8da0e4fe77acd35a1f7d37db03ac66e49d66d110f"
+  license all_of: ["AGPL-3.0-only", "MIT", "BSD-3-Clause"]
 
   head "https://github.com/x-cmd/x-cmd.git", branch: "X"
 
   def install
-    (prefix/"").install Dir.glob("*", File::FNM_DOTMATCH) - %w[. ..]
-    (prefix/"bin").mkpath
-    cp prefix/"mod/x-cmd/lib/bin/x-cmd", prefix/"bin/x-cmd"
-    replace_path_in_file(prefix/"bin/x-cmd")
+    prefix.install Dir.glob("*")
+    replace_path_in_file(prefix/"mod/x-cmd/lib/bin/x-cmd")
+    bin.install prefix/"mod/x-cmd/lib/bin/x-cmd"
   end
 
   def replace_path_in_file(file)
-    if File.read(file).include?("/opt/homebrew/Cellar/x-cmd/latest")
-      inreplace file, "/opt/homebrew/Cellar/x-cmd/latest", prefix.to_s
-    end
+    inreplace file, "/opt/homebrew/Cellar/x-cmd/latest", prefix.to_s
   end
 
   test do
-    assert_match(/tag:\s*v#{Regexp.escape(version)}/, shell_output("#{bin}/x-cmd version"))
+    assert_match "Welcome to x-cmd", shell_output("#{bin}/x-cmd 2>&1")
+    assert_match "hello", shell_output("#{bin}/x-cmd cowsay hello")
   end
 end
