be0808b09272e1d3721a9cca23f99e5d26c9dc8d
grepsuzette <350354+grepsuzette@users.noreply.github.com>
2025-03-10 13:04:24 +0800

    inc revision

--- Formula/m/moc.rb
+++ Formula/m/moc.rb
@@ -2,7 +2,7 @@ class Moc < Formula
   desc "Terminal-based music player"
   homepage "https://moc.daper.net/"
   license "GPL-2.0-or-later"
-  revision 9
+  revision 10
 
   stable do
     url "https://ftp.daper.net/pub/soft/moc/stable/moc-2.5.2.tar.bz2"
