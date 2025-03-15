7ef33e1a6abad47fec69970295639156096a3b9c
Mischa Spiegelmock <me@mish.dev>
2025-03-15 10:20:37 -0700

    projectm-eval 1.0 (new formula)

---
+++ Formula/p/projectm-eval.rb
@@ -0,0 +1,25 @@
+class ProjectmEval < Formula
+  desc "Milkdrop-compatible music visualizer preset evaluation library"
+  homepage "https://github.com/projectM-visualizer/projectm-eval"
+  url "https://github.com/projectM-visualizer/projectm-eval.git",
+      branch: "master",
+      revision: "ee180a2473c12856fd25dc754bafdab7e843cc7a"
+  version "HEAD-ee180a"
+  license "LGPL-2.1-or-later"
+
+  depends_on "bison" => :build
+  depends_on "flex" => :build
+  depends_on "cmake" => :build
+  depends_on "pkgconf" => [:build, :test]
+
+  def install
+    system "cmake", "-S", ".", "-B", "build", "-DENABLE_PROJECTM_EVAL_INSTALL=ON", *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    assert_predicate lib/"libprojectM_eval.a", :exist?
+    system "pkg-config", "--exists", "projectM-eval"
+  end
+end
