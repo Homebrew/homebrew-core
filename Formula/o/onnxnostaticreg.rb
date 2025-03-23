66ab30841c4eb29dd19c71a3e769a6647a80a096
Chetan Taralekar <chetant@gmail.com>
2025-03-22 22:03:42 -0500

    onnxnostaticreg 1.17.0 (new formula)
    Fixes the onnx dependency for onnxruntime by disabling static
    registration in this package.
    Previously using onnxruntime would throw exceptions on double
    registrations (once from onnx, another from onnxruntime)
    More details:
    https://onnxruntime.ai/docs/build/dependencies.html#use-preinstalled-packages

---
+++ Formula/o/onnxnostaticreg.rb
@@ -0,0 +1,98 @@
+class Onnxnostaticreg < Formula
+  desc "Open standard for machine learning interoperability (built for onnxruntime)"
+  homepage "https://onnx.ai/"
+  url "https://github.com/onnx/onnx/archive/refs/tags/v1.17.0.tar.gz"
+  sha256 "8d5e983c36037003615e5a02d36b18fc286541bf52de1a78f6cf9f32005a820e"
+  license "Apache-2.0"
+
+  depends_on "cmake" => [:build, :test]
+  depends_on "abseil"
+  depends_on "protobuf"
+
+  uses_from_macos "python" => :build
+
+  def install
+    args = %W[
+      -DBUILD_SHARED_LIBS=ON
+      -DCMAKE_INSTALL_RPATH=#{rpath}
+      -DONNX_DISABLE_STATIC_REGISTRATION=ON
+      -DONNX_USE_PROTOBUF_SHARED_LIBS=ON
+      -DPYTHON_EXECUTABLE=#{which("python3")}
+    ]
+
+    system "cmake", "-S", ".", "-B", "build", *args, *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    # https://github.com/onnx/onnx/blob/main/onnx/test/cpp/ir_test.cc
+    (testpath/"test.cpp").write <<~CPP
+      #include <cassert>
+      #include <cctype>
+      #include <memory>
+      #include <string>
+      #include <onnx/common/ir.h>
+      #include <onnx/common/ir_pb_converter.h>
+      using namespace onnx;
+
+      bool IsValidIdentifier(const std::string& name) {
+        if (name.empty()) {
+          return false;
+        }
+        if (!isalpha(name[0]) && name[0] != '_') {
+          return false;
+        }
+        for (size_t i = 1; i < name.size(); ++i) {
+          if (!isalnum(name[i]) && name[i] != '_') {
+            return false;
+          }
+        }
+        return true;
+      }
+
+      int main() {
+        Graph* g = new Graph();
+        g->setName("test");
+        Value* x = g->addInput();
+        x->setUniqueName("x");
+        x->setElemType(TensorProto_DataType_FLOAT);
+        x->setSizes({Dimension("M"), Dimension("N")});
+        Node* node1 = g->create(kNeg, 1);
+        node1->addInput(x);
+        g->appendNode(node1);
+        Value* temp1 = node1->outputs()[0];
+        Node* node2 = g->create(kNeg, 1);
+        node2->addInput(temp1);
+        g->appendNode(node2);
+        Value* y = node2->outputs()[0];
+        g->registerOutput(y);
+
+        ModelProto model;
+        ExportModelProto(&model, std::shared_ptr<Graph>(g));
+
+        for (auto& node : model.graph().node()) {
+          for (auto& name : node.output()) {
+            assert(IsValidIdentifier(name));
+          }
+        }
+        return 0;
+      }
+    CPP
+
+    (testpath/"CMakeLists.txt").write <<~CMAKE
+      cmake_minimum_required(VERSION 3.10)
+      project(test LANGUAGES CXX)
+      find_package(ONNX CONFIG REQUIRED)
+      add_executable(test test.cpp)
+      target_link_libraries(test ONNX::onnx)
+    CMAKE
+
+    ENV.delete "CPATH"
+    args = ["-DCMAKE_FIND_PACKAGE_PREFER_CONFIG=ON"]
+    args << "-DCMAKE_BUILD_RPATH=#{lib};#{HOMEBREW_PREFIX}/lib" if OS.linux?
+    system "cmake", "-S", ".", "-B", "build", *args
+    system "cmake", "--build", "build"
+    system "./build/test"
+  end
+end
