520c81806e966732d6a9e9d3ded3bc150e5e09ce
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-27 14:10:04 +0000

    rio 0.2.11

--- Formula/r/rio.rb
+++ Formula/r/rio.rb
@@ -1,8 +1,8 @@
 class Rio < Formula
   desc "Hardware-accelerated GPU terminal emulator powered by WebGPU"
   homepage "https://raphamorim.io/rio/"
-  url "https://github.com/raphamorim/rio/archive/refs/tags/v0.2.10.tar.gz"
-  sha256 "edf481aaff241f96defaa1a7cdf6d2fa8e835480b6aaa323aa6bf6faefe9c3ee"
+  url "https://github.com/raphamorim/rio/archive/refs/tags/v0.2.11.tar.gz"
+  sha256 "11f9cf3ed783681b208ecc07e37463c915292bc2a39cf5fd53edf76fae27b6b7"
   license "MIT"
   head "https://github.com/raphamorim/rio.git", branch: "main"
 
