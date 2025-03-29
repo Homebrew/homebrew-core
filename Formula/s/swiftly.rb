64e08417b0d3184eb7655bbde6b79da9bd3cef25
Danny Mösch <danny.moesch@icloud.com>
2025-03-29 12:44:27 +0100

    swiftly 1.0.0 (new formula)

---
+++ Formula/s/swiftly.rb
@@ -0,0 +1,29 @@
+class Swiftly < Formula
+  desc "Swift toolchain installer and manager"
+  homepage "https://github.com/swiftlang/swiftly"
+  url "https://github.com/swiftlang/swiftly.git",
+      tag:      "1.0.0",
+      revision: "a9eecca341e6d5047c744a165bfe5bbf239987f5"
+  license "Apache-2.0"
+  head "https://github.com/swiftlang/swiftly.git", branch: "main"
+
+  depends_on macos: :ventura
+  depends_on xcode: "8.0"
+
+  uses_from_macos "swift" => :build, since: :sonoma # swift 5.10+
+  uses_from_macos "zlib"
+
+  def install
+    args = if OS.mac?
+      ["--disable-sandbox"]
+    else
+      ["--static-swift-stdlib"]
+    end
+    system "swift", "build", *args, "--configuration", "release", "--product", "swiftly"
+    bin.install ".build/release/swiftly"
+  end
+
+  test do
+    assert_match "#{bin}/swiftly init", shell_output("#{bin}/swiftly help 2>&1", 1).chomp
+  end
+end
