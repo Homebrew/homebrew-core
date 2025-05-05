283de347297745cf11835b4cc6b9f09a79e4d4f7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-05 11:31:24 +0000

    pympress 1.8.6

--- Formula/p/pympress.rb
+++ Formula/p/pympress.rb
@@ -3,8 +3,8 @@ class Pympress < Formula
 
   desc "Simple and powerful dual-screen PDF reader designed for presentations"
   homepage "https://github.com/Cimbali/pympress/"
-  url "https://files.pythonhosted.org/packages/fb/e2/91827c485aae28d69f0b40c6d366b9f6eb96d8208a98af0345e0ade3fbbd/pympress-1.8.5.tar.gz"
-  sha256 "29bd39115d05f254da993abba42d54a0e9187f4e2ce7c363324b15136c530bf6"
+  url "https://files.pythonhosted.org/packages/87/66/fb9f8f2975740ea8880de293eb16b543965387881c71ca323a00a5d77d8a/pympress-1.8.6.tar.gz"
+  sha256 "243dc5dd225acd13fb6bae680e2de1816d521203b98a9cff588b66f141fffd9a"
   license "GPL-2.0-or-later"
   head "https://github.com/Cimbali/pympress.git", branch: "main"
 
