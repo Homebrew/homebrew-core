b33ba9a7555a578b666251e23b0fcfe22d0cf447
Sam Umbach <samumbach@gmail.com>
2025-05-14 15:17:01 -0400

    mysql-client: remove `-no_warn_duplicate_libraries` linker option when using older `ld`
    - `-no_warn_duplicate_libraries` option was added to Apple's `ld` by ld64-951.9.

--- Formula/m/mysql-client.rb
+++ Formula/m/mysql-client.rb
@@ -38,6 +38,166 @@ class MysqlClient < Formula
 
   on_ventura :or_older do
     depends_on "llvm" => :build
+    # back out commit: Bug#37065301 Silence warnings on macOS
+    patch <<~EOF
+      diff --git a/cmake/component.cmake b/cmake/component.cmake
+      index a77fa0f..e476381 100644
+      --- a/cmake/component.cmake
+      +++ b/cmake/component.cmake
+      @@ -98,10 +98,6 @@ MACRO(MYSQL_ADD_COMPONENT component_arg)
+           # For APPLE: adjust path dependecy for SSL shared libraries.
+           SET_PATH_TO_CUSTOM_SSL_FOR_APPLE(${target})
+
+      -    IF(APPLE)
+      -      TARGET_LINK_OPTIONS(${target} PRIVATE LINKER:-no_warn_duplicate_libraries)
+      -    ENDIF()
+      -
+           IF(WIN32_CLANG AND WITH_ASAN)
+             TARGET_LINK_LIBRARIES(${target}
+               "${ASAN_LIB_DIR}/clang_rt.asan_dll_thunk-x86_64.lib")
+      diff --git a/cmake/libutils.cmake b/cmake/libutils.cmake
+      index 69adcdb..661171b 100644
+      --- a/cmake/libutils.cmake
+      +++ b/cmake/libutils.cmake
+      @@ -358,7 +358,6 @@ MACRO(MERGE_LIBRARIES_SHARED TARGET_ARG)
+
+         IF(APPLE)
+           SET_TARGET_PROPERTIES(${TARGET} PROPERTIES MACOSX_RPATH ON)
+      -    TARGET_LINK_OPTIONS(${TARGET} PRIVATE LINKER:-no_warn_duplicate_libraries)
+         ENDIF()
+
+         TARGET_LINK_OPTIONS(${TARGET} PRIVATE ${export_link_flags})
+      @@ -703,10 +702,6 @@ FUNCTION(ADD_SHARED_LIBRARY TARGET_ARG)
+           ENDIF()
+         ENDIF()
+
+      -  IF(APPLE)
+      -    TARGET_LINK_OPTIONS(${TARGET} PRIVATE LINKER:-no_warn_duplicate_libraries)
+      -  ENDIF()
+      -
+         ADD_OBJDUMP_TARGET(show_${TARGET} "$<TARGET_FILE:${TARGET}>"
+           DEPENDS ${TARGET})
+
+      diff --git a/cmake/mysql_add_executable.cmake b/cmake/mysql_add_executable.cmake
+      index 2a0548b..232e78a 100644
+      --- a/cmake/mysql_add_executable.cmake
+      +++ b/cmake/mysql_add_executable.cmake
+      @@ -212,10 +212,6 @@ FUNCTION(MYSQL_ADD_EXECUTABLE target_arg)
+           MACOS_ADD_DEVELOPER_ENTITLEMENTS(${target})
+         ENDIF()
+
+      -  IF(APPLE)
+      -    TARGET_LINK_OPTIONS(${target} PRIVATE LINKER:-no_warn_duplicate_libraries)
+      -  ENDIF()
+      -
+         IF(WIN32_CLANG AND WITH_ASAN)
+           TARGET_LINK_LIBRARIES(${target}
+             "${ASAN_LIB_DIR}/clang_rt.asan-x86_64.lib"
+      diff --git a/cmake/plugin.cmake b/cmake/plugin.cmake
+      index a7727ef..eac9d74 100644
+      --- a/cmake/plugin.cmake
+      +++ b/cmake/plugin.cmake
+      @@ -327,10 +327,6 @@ MACRO(MYSQL_ADD_PLUGIN plugin_arg)
+             DEPENDS ${target})
+         ENDIF()
+
+      -  IF(BUILD_PLUGIN AND ARG_MODULE_ONLY AND APPLE)
+      -    TARGET_LINK_OPTIONS(${target} PRIVATE LINKER:-no_warn_duplicate_libraries)
+      -  ENDIF()
+      -
+         IF(BUILD_PLUGIN)
+           ADD_DEPENDENCIES(plugin_all ${target})
+           TARGET_COMPILE_FEATURES(${target} PUBLIC cxx_std_20)
+      diff --git a/extra/libcno/CMakeLists.txt b/extra/libcno/CMakeLists.txt
+      index e2513f4..1577bab 100644
+      --- a/extra/libcno/CMakeLists.txt
+      +++ b/extra/libcno/CMakeLists.txt
+      @@ -16,10 +16,6 @@ SET(LIBCNO_GENERATE_DIR "${CMAKE_CURRENT_BINARY_DIR}")
+
+       DISABLE_MISSING_PROFILE_WARNING()
+
+      -IF(APPLE_XCODE)
+      -  STRING_APPEND(CMAKE_C_FLAGS " -Wno-shorten-64-to-32")
+      -ENDIF()
+      -
+       # Following targets were created to mimic behavior of following
+       # files supplied by libcno:
+       #
+      diff --git a/extra/protobuf/protobuf-24.4/cmake/libprotobuf-lite.cmake b/extra/protobuf/protobuf-24.4/cmake/libprotobuf-lite.cmake
+      index f601e7f..b2826db 100644
+      --- a/extra/protobuf/protobuf-24.4/cmake/libprotobuf-lite.cmake
+      +++ b/extra/protobuf/protobuf-24.4/cmake/libprotobuf-lite.cmake
+      @@ -57,11 +57,6 @@ IF(protobuf_BUILD_SHARED_LIBS)
+           RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/library_output_directory
+           )
+
+      -  IF(APPLE)
+      -    TARGET_LINK_OPTIONS(libprotobuf-lite
+      -      PRIVATE LINKER:-no_warn_duplicate_libraries)
+      -  ENDIF()
+      -
+         IF(WIN32)
+           ADD_CUSTOM_COMMAND(TARGET libprotobuf-lite POST_BUILD
+             COMMAND ${CMAKE_COMMAND} -E copy_if_different
+      diff --git a/extra/protobuf/protobuf-24.4/cmake/libprotobuf.cmake b/extra/protobuf/protobuf-24.4/cmake/libprotobuf.cmake
+      index 81428fb..ceb557f 100644
+      --- a/extra/protobuf/protobuf-24.4/cmake/libprotobuf.cmake
+      +++ b/extra/protobuf/protobuf-24.4/cmake/libprotobuf.cmake
+      @@ -61,10 +61,6 @@ IF(protobuf_BUILD_SHARED_LIBS)
+           RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/library_output_directory
+           )
+
+      -  IF(APPLE)
+      -    TARGET_LINK_OPTIONS(libprotobuf PRIVATE LINKER:-no_warn_duplicate_libraries)
+      -  ENDIF()
+      -
+         IF(WIN32)
+           ADD_CUSTOM_COMMAND(TARGET libprotobuf POST_BUILD
+             COMMAND ${CMAKE_COMMAND} -E copy_if_different
+      diff --git a/extra/protobuf/protobuf-24.4/cmake/libprotoc.cmake b/extra/protobuf/protobuf-24.4/cmake/libprotoc.cmake
+      index ef63037..7ba5394 100644
+      --- a/extra/protobuf/protobuf-24.4/cmake/libprotoc.cmake
+      +++ b/extra/protobuf/protobuf-24.4/cmake/libprotoc.cmake
+      @@ -53,9 +53,6 @@ IF(protobuf_BUILD_SHARED_LIBS)
+           DEBUG_POSTFIX ""
+           LIBRARY_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/library_output_directory
+           RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/library_output_directory)
+      -  IF(APPLE)
+      -    TARGET_LINK_OPTIONS(libprotoc PRIVATE LINKER:-no_warn_duplicate_libraries)
+      -  ENDIF()
+         IF(WIN32)
+           ADD_CUSTOM_COMMAND(TARGET libprotoc POST_BUILD
+             COMMAND ${CMAKE_COMMAND} -E copy_if_different
+      diff --git a/extra/protobuf/protobuf-24.4/cmake/protoc.cmake b/extra/protobuf/protobuf-24.4/cmake/protoc.cmake
+      index 215eb21..70f1b60 100644
+      --- a/extra/protobuf/protobuf-24.4/cmake/protoc.cmake
+      +++ b/extra/protobuf/protobuf-24.4/cmake/protoc.cmake
+      @@ -15,10 +15,6 @@ set_target_properties(protoc PROPERTIES
+
+       ################################################################
+
+      -IF(APPLE)
+      -  TARGET_LINK_OPTIONS(protoc PRIVATE LINKER:-no_warn_duplicate_libraries)
+      -ENDIF()
+      -
+       SET_TARGET_PROPERTIES(protoc PROPERTIES
+         RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/runtime_output_directory)
+
+      diff --git a/router/cmake/Plugin.cmake b/router/cmake/Plugin.cmake
+      index ac6b6b2..9b4afc5 100644
+      --- a/router/cmake/Plugin.cmake
+      +++ b/router/cmake/Plugin.cmake
+      @@ -89,9 +89,6 @@ FUNCTION(add_harness_plugin NAME)
+         # .dylib, which we do not want, so we reset it here.
+         ADD_LIBRARY(${NAME} SHARED ${_option_SOURCES})
+         TARGET_COMPILE_FEATURES(${NAME} PUBLIC cxx_std_20)
+      -  IF(APPLE)
+      -    TARGET_LINK_OPTIONS(${NAME} PRIVATE LINKER:-no_warn_duplicate_libraries)
+      -  ENDIF()
+
+         # add plugin to build-all target
+         ADD_DEPENDENCIES(mysqlrouter_all ${NAME})
+    EOF
     fails_with :clang do
       cause <<~EOS
         std::string_view is not fully compatible with the libc++ shipped
@@ -55,6 +215,11 @@ class MysqlClient < Formula
     version "9"
     cause "Requires C++20"
   end
+
+  # Patch out check for Homebrew `boost`.
+  # This should not be necessary when building inside `brew`.
+  # https://github.com/Homebrew/homebrew-test-bot/pull/820
+  patch :DATA
 
   def install
     if OS.linux?
@@ -91,3 +256,41 @@ class MysqlClient < Formula
     assert_match version.to_s, shell_output("#{bin}/mysql --version")
   end
 end
+
+__END__
+diff --git a/CMakeLists.txt b/CMakeLists.txt
+index 438dff720c5..47863c17e23 100644
+--- a/CMakeLists.txt
++++ b/CMakeLists.txt
+@@ -1948,31 +1948,6 @@ MYSQL_CHECK_RAPIDJSON()
+ MYSQL_CHECK_FIDO()
+ MYSQL_CHECK_FIDO_DLLS()
+
+-IF(APPLE)
+-  GET_FILENAME_COMPONENT(HOMEBREW_BASE ${HOMEBREW_HOME} DIRECTORY)
+-  IF(EXISTS ${HOMEBREW_BASE}/include/boost)
+-    FOREACH(SYSTEM_LIB ICU LZ4 PROTOBUF ZSTD FIDO)
+-      IF(WITH_${SYSTEM_LIB} STREQUAL "system")
+-        MESSAGE(FATAL_ERROR
+-          "WITH_${SYSTEM_LIB}=system is not compatible with Homebrew boost\n"
+-          "MySQL depends on ${BOOST_PACKAGE_NAME} with a set of patches.\n"
+-          "Including headers from ${HOMEBREW_BASE}/include "
+-          "will break the build.\n"
+-          "Please use WITH_${SYSTEM_LIB}=bundled\n"
+-          "or do 'brew uninstall boost' or 'brew unlink boost'"
+-          )
+-      ENDIF()
+-    ENDFOREACH()
+-  ENDIF()
+-  # Ensure that we look in /usr/local/include or /opt/homebrew/include
+-  FOREACH(SYSTEM_LIB ICU LZ4 PROTOBUF ZSTD FIDO)
+-    IF(WITH_${SYSTEM_LIB} STREQUAL "system")
+-      INCLUDE_DIRECTORIES(SYSTEM ${HOMEBREW_BASE}/include)
+-      BREAK()
+-    ENDIF()
+-  ENDFOREACH()
+-ENDIF()
+-
+ IF(WITH_AUTHENTICATION_WEBAUTHN OR
+   WITH_AUTHENTICATION_CLIENT_PLUGINS)
+   IF(WITH_FIDO STREQUAL "system" AND
