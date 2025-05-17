7e97c2e986b0dbe0f877dbc8ef147d29a590eae5
Daeho Ro <40587651+daeho-ro@users.noreply.github.com>
2025-05-17 16:33:27 +0900

    mysq@8.4: remove abseil dependency

--- Formula/m/mysql@8.4.rb
+++ Formula/m/mysql@8.4.rb
@@ -6,6 +6,7 @@ class MysqlAT84 < Formula
   url "https://cdn.mysql.com/Downloads/MySQL-8.4/mysql-8.4.5.tar.gz"
   sha256 "53639592a720a719fdfadf2c921b947eac86c06e333202e47667852a5781bd1a"
   license "GPL-2.0-only" => { with: "Universal-FOSS-exception-1.0" }
+  revision 1
 
   livecheck do
     url "https://dev.mysql.com/downloads/mysql/8.4.html?tpl=files&os=src&version=8.4"
@@ -27,7 +28,6 @@ class MysqlAT84 < Formula
   depends_on "bison" => :build
   depends_on "cmake" => :build
   depends_on "pkgconf" => :build
-  depends_on "abseil"
   depends_on "icu4c@77"
   depends_on "lz4"
   depends_on "openssl@3"
