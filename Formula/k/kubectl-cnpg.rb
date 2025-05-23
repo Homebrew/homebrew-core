d848d8887763ac0912d04ff9fec57bb26f75506b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-23 11:05:15 +0000

    kubectl-cnpg 1.25.2

--- Formula/k/kubectl-cnpg.rb
+++ Formula/k/kubectl-cnpg.rb
@@ -2,8 +2,8 @@ class KubectlCnpg < Formula
   desc "CloudNativePG plugin for kubectl"
   homepage "https://cloudnative-pg.io/"
   url "https://github.com/cloudnative-pg/cloudnative-pg.git",
-      tag:      "v1.25.1",
-      revision: "c56e00d462c3899ab305540953ec541dfe0f762a"
+      tag:      "v1.25.2",
+      revision: "2aa4bd487e4bb39bc25ddcacb47e2ae190ff537d"
   license "Apache-2.0"
   head "https://github.com/cloudnative-pg/cloudnative-pg.git", branch: "main"
 
