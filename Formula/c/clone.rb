ca5669b013fb45092c14fe8fe7106645bbfd3fca
Li ZongYing <lizongying@msn.com>
2025-05-24 01:57:28 +0800

    0.1.6

--- Formula/c/clone.rb
+++ Formula/c/clone.rb
@@ -1,23 +1,24 @@
 class Clone < Formula
-  desc 'Clone is a tool for downloading Git repositories. It allows you to download only a specific branch or tag without downloading the entire history, resulting in faster downloads.'
+  desc "Download only a specific branch or tag without downloading the entire history"
   homepage "https://github.com/lizongying/ungit"
-  license "MIT"
   version "0.1.6"
+  license "MIT"
   head "https://github.com/lizongying/ungit.git", branch: "main"
 
- if Hardware::CPU.arm?
+  on_arm do
     url "https://github.com/lizongying/ungit/releases/download/v0.1.6/ungit_aarch64-apple-darwin"
     sha256 "bfc0aa642ee803f324aa4c05112da3158970cc88fe744d7f2deccde8ee0d4b41"
-  else
+  end
+  on_intel do
     url "https://github.com/lizongying/ungit/releases/download/v0.1.6/ungit_x86_64-apple-darwin"
     sha256 "ed92cbd3e9726d2b92d44ad53712df4df8eb219d908590f466290f2db7d3ae16"
   end
 
   def install
     binary_name = if Hardware::CPU.arm?
-          "ungit_aarch64-apple-darwin"
+      "ungit_aarch64-apple-darwin"
     else
-          "ungit_x86_64-apple-darwin"
+      "ungit_x86_64-apple-darwin"
     end
 
     bin.install binary_name => "clone"
