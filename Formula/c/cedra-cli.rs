39065143f9ac056fe83b4f0e28ced08f47c58944
Maksym <87798721+maksym-reznichenko-dev@users.noreply.github.com>
2025-06-09 15:26:26 +0300

    cedra-cli 7.3.1
    Created cedra-cli release.
    
    
    Released CLI tool for the Cedra blockchain

---
+++ Formula/c/cedra-cli.rs
@@ -0,0 +1,17 @@
+class CedraCli < Formula
+  desc "CLI tool for Cedra Labs"
+  homepage "https://cedra.network/"
+  url "https://github.com/cedra-labs/cedra/archive/refs/tags/cedra-cli-v7.3.1.tar.gz"
+  sha256 "904d46a492d47eaaedd3085ae939b9e5fade0d999702cbb826ed295b65f9952e"
+  license "MIT"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args(path: "cli")
+  end
+
+  test do
+    assert_match "cedra", shell_output("#{bin}/cedra --help")
+  end
+end
