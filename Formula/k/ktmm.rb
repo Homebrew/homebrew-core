1235957877a9acf43fbdd5149c3102e4a56c2464
Andrew Odendaal <aoaws@amazon.com>
2025-06-10 02:14:22 +0400

    ktmm 0.5.1 (new formula)
    Keep That Mouse Moving (ktmm) is a utility that prevents system sleep by making periodic mouse movements. It's written in Rust and requires accessibility permissions on macOS to function properly. This formula adds ktmm version 0.5.1 to Homebrew.

--- Formula/k/ktmm.rb
+++ Formula/k/ktmm.rb
@@ -1,18 +1,14 @@
 class Ktmm < Formula
-  desc "Keep That Mouse Moving - Prevents system sleep by making periodic mouse movements"
+  desc "Prevents system sleep by making periodic mouse movements"
   homepage "https://github.com/ao/ktmm"
   url "https://github.com/ao/ktmm/archive/refs/tags/v0.5.1.tar.gz"
-  sha256 "5962df6852d5d1b78ace3248f26d5737fee131dda80f2f8b53bbdf1319e85b76"  # source
+  sha256 "5962df6852d5d1b78ace3248f26d5737fee131dda80f2f8b53bbdf1319e85b76"
   license "MIT"
 
   depends_on "rust" => :build
 
   def install
     system "cargo", "install", *std_cargo_args
-  end
-
-  test do
-    assert_match "ktmm 0.5.1", shell_output("#{bin}/ktmm --version")
   end
 
   def caveats
@@ -23,4 +19,9 @@ class Ktmm < Formula
       and add KTMM to the list of allowed applications.
     EOS
   end
+
+  test do
+    assert_match "ktmm 0.5.1", shell_output("#{bin}/ktmm --version")
+  end
+
 end
