3fee9939bae430fff3b7953148935317461e6401
Maksym <87798721+maksym-reznichenko-dev@users.noreply.github.com>
2025-06-09 13:26:54 +0300

    cedra-cli 7.3.1
    updated sha256 value
    
    Updated requested sha256 hashsum

--- Formula/c/cedra-cli.rb
+++ Formula/c/cedra-cli.rb
@@ -2,7 +2,7 @@ class CedraCli < Formula
   desc "CLI tool for Cedra Labs"
   homepage "https://cedra.network/"
   url "https://github.com/cedra-labs/cedra/archive/refs/tags/cedra-cli-v7.3.1.tar.gz"
-  sha256 "49a50bafaf983e0ea85fdab1364012a4200b1198f7b0e087cf7c88e8b15722e0"
+  sha256 "904d46a492d47eaaedd3085ae939b9e5fade0d999702cbb826ed295b65f9952e"
   license "MIT"
 
   depends_on "rust" => :build
