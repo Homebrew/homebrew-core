b77d793b90698f5dbc90c0ee4b48e64563f7bccc
Stark Christmas <stark@Starks-iMac.local>
2025-06-14 02:56:01 +0800

    fix desc format and restrict build to macOS only

--- Formula/i/ip.rb
+++ Formula/i/ip.rb
@@ -4,16 +4,26 @@ class Ip < Formula
   url "https://github.com/StarkChristmas/ipget/archive/refs/tags/v1.0.0.tar.gz"
   sha256 "0f1e6099eb568f3027db981177a1062c84036171cb13b697c38c992a81339ba0"
   license "MIT"
+  head "https://github.com/StarkChristmas/ipget.git", branch: "main"
 
   depends_on "go" => :build
 
-  def install
-    system "go", "build", *std_go_args, "./main.go"
-  end
+  on_macos do
+    def install
+      system "go", "build", *std_go_args, "."
+    end
 
-  test do
-    output = shell_output("#{bin}/ip 2>&1")
-    assert_match(/\d+\.\d+\.\d+\.\d+/, output)
-    assert_equal 0, $CHILD_STATUS.exitstatus
+    test do
+      output = shell_output("#{bin}/ip 2>&1")
+      assert_match(/\d+\.\d+\.\d+\.\d+/, output)
+      assert_equal 0, $CHILD_STATUS.exitstatus
+    end
+
+    def caveats
+      <<~EOS
+        This formula is only supported on macOS.
+      EOS
+    end
   end
 end
+
