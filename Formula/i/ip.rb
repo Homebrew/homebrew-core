d36bc006c22cf3a22ed897627283834a611181a1
Stark Christmas <stark@Starks-iMac.local>
2025-06-14 01:15:55 +0800

    ip 1.0.0 (new formula)_fix the desc part being too long

--- Formula/i/ip.rb
+++ Formula/i/ip.rb
@@ -1,5 +1,5 @@
 class Ip < Formula
-  desc "Optimize ifconfig output of macos to only display the network interfaces that are activated and have obtained valid IP addresses. The output includes Network Service, Local IPv4 Address, Public IP, and City."
+  desc "ifconfig alternative showing external IP, active interfaces & internal IPv4 only."
   homepage "https://github.com/StarkChristmas/ipget"
   url "https://github.com/StarkChristmas/ipget/archive/refs/tags/v1.0.0.tar.gz"
   sha256 "0f1e6099eb568f3027db981177a1062c84036171cb13b697c38c992a81339ba0"
@@ -12,7 +12,7 @@ class Ip < Formula
   end
 
   test do
-    output = shell_output("#{bin}/ipget 2>&1")
+    output = shell_output("#{bin}/ip 2>&1")
     assert_match /\d+\.\d+\.\d+\.\d+/, output
   end
 end
