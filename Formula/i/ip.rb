9619197ad46dcb0428213cb6ccfb108fc88d509e
Stark Christmas <stark@Starks-iMac.local>
2025-06-14 02:32:16 +0800

    fix desc formatting and test regex

--- Formula/i/ip.rb
+++ Formula/i/ip.rb
@@ -1,5 +1,5 @@
 class Ip < Formula
-  desc "ifconfig alternative showing external IP, active interfaces & internal IPv4 only."
+  desc "Display public and private IPs and city for active network interfaces"
   homepage "https://github.com/StarkChristmas/ipget"
   url "https://github.com/StarkChristmas/ipget/archive/refs/tags/v1.0.0.tar.gz"
   sha256 "0f1e6099eb568f3027db981177a1062c84036171cb13b697c38c992a81339ba0"
@@ -13,6 +13,7 @@ class Ip < Formula
 
   test do
     output = shell_output("#{bin}/ip 2>&1")
-    assert_match /\d+\.\d+\.\d+\.\d+/, output
+    assert_match(/\d+\.\d+\.\d+\.\d+/, output)
+    assert_equal 0, $CHILD_STATUS.exitstatus
   end
 end
