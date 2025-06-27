1c08e15b1764dc202e3310fdab7ea71f77166f62
Evan <evan@MacBook-Pro.local>
2025-06-27 21:10:05 +0800

    sox: upgrade SoX to SoX_NG 14.6.0.1
    SoX was last updated 2015-02-22. SoX_ng project has resumed development with new releases that are
    drop-in backwards-compatible. From the project homepage: https://codeberg.org/sox_ng/sox_ng
    
    The SoX_ng project imports, compares and refines bug fixes and new work from
    the 50-odd software distributions that package SoX and from the plethora of
    forks on github and elsewhere and makes regular releases with a six-monthly
    cadence for each of the micro (bug fixes) and minor (new features) releases.
    A major release (non-backwards-compatible changes) is not planned.
    
    Verified these changes to the formula:
    
    - license: GPL 2.0 as per https://codeberg.org/sox_ng/sox_ng/src/branch/main/COPYING
    - builds cleanly without -Wno-incompatible-function-pointer-types
    - patch beginning "Fix -flat_namespace being used on Big Sur and later" is
      already integrated in SoX_ng.
    - patch beginning "Applies Eric Wong's patch to fix device name length in MacOS"
      does not appear to be required.  coreaudio.c has been significantly changed,
      a different API is used to get device names, and device names appear to be
      retrieved correctly with sox -V -t coreaudio null -n.
    - added dependency on wavpack and verfied SoX_ng builds with wavpack support.

--- Formula/s/sox.rb
+++ Formula/s/sox.rb
@@ -1,22 +1,9 @@
 class Sox < Formula
-  desc "SOund eXchange: universal sound sample translator"
-  homepage "https://sox.sourceforge.net/"
-  url "https://downloads.sourceforge.net/project/sox/sox/14.4.2/sox-14.4.2.tar.gz"
-  sha256 "b45f598643ffbd8e363ff24d61166ccec4836fea6d3888881b8df53e3bb55f6c"
-  license all_of: ["LGPL-2.0-only", "GPL-2.0-only"]
-  revision 6
-
-  no_autobump! because: :requires_manual_review
-
-  bottle do
-    sha256 cellar: :any,                 arm64_sequoia: "dd3179a8e7d8399ef404e8e19e199d7cb2d43bc7472cc6e5932777dd204ea20f"
-    sha256 cellar: :any,                 arm64_sonoma:  "ec5c73d125f2ac73ddde98d2264a298611f2dd819a873e115178f083216064bd"
-    sha256 cellar: :any,                 arm64_ventura: "719011d445046c330686ae5fef7df2561b14c9f966025263bb948b176b528552"
-    sha256 cellar: :any,                 sonoma:        "b9caf7383de463f53526e60b33f07792bcbd89a99d358ed76ed91df4ac580b58"
-    sha256 cellar: :any,                 ventura:       "a582da3eaee0febd63765341ba7e70fabc57895f7595641fe1565dff40929231"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "526bdf965df84741c4e639c24931f3f03e9b19d438caa735192214e1831d809f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3c44da47de6f97f6e1542c5757a01e7aa1dad4ba631d2ae3eb0c24c1bca09a5d"
-  end
+  desc "Sound eXchange NG"
+  homepage "https://codeberg.org/sox_ng/sox_ng"
+  url "https://codeberg.org/sox_ng/sox_ng/releases/download/sox_ng-14.6.0.1/sox_ng-14.6.0.1.tar.gz"
+  sha256 "6370c0fb7208cfa4e52a36c8ec412334f488e45325cd62f93e1c3d4b3da99f17"
+  license "GPL-2.0-only"
 
   depends_on "pkgconf" => :build
   depends_on "flac"
@@ -27,6 +14,7 @@ class Sox < Formula
   depends_on "libvorbis"
   depends_on "mad"
   depends_on "opusfile"
+  depends_on "wavpack"
 
   uses_from_macos "zlib"
 
@@ -34,25 +22,8 @@ class Sox < Formula
     depends_on "alsa-lib"
   end
 
-  # Fix -flat_namespace being used on Big Sur and later.
-  patch do
-    url "https://raw.githubusercontent.com/Homebrew/formula-patches/03cf8088210822aa2c1ab544ed58ea04c897d9c4/libtool/configure-pre-0.4.2.418-big_sur.diff"
-    sha256 "83af02f2aa2b746bb7225872cab29a253264be49db0ecebb12f841562d9a2923"
-  end
-
-  # Applies Eric Wong's patch to fix device name length in MacOS.
-  # This patch has been in a "potential updates" branch since 2016.
-  # There is nothing to indicate when, if ever, it will or will not make it
-  # into the main branch, unfortunately.
-  patch do
-    url "https://80x24.org/sox.git/patch?id=bf2afa54a7dec"
-    sha256 "0cebb3d4c926a2cf0a506d2cd62576c29308baa307df36fddf7c6ae4b48df8e7"
-  end
-
   def install
-    ENV.append_to_cflags "-Wno-incompatible-function-pointer-types" if DevelopmentTools.clang_build_version >= 1500
-
-    args = []
+    args = %w[--enable-replace]
     args << "--with-alsa" if OS.linux?
 
     system "./configure", *args, *std_configure_args
