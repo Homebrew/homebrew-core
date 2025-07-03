9363683546fe0ed324a9143ffcdb19d002d774ca
botantony <antonsm21@gmail.com>
2025-07-03 15:40:12 +0200

    docker-completion 28.3.1

--- Formula/d/docker-completion.rb
+++ Formula/d/docker-completion.rb
@@ -1,8 +1,8 @@
 class DockerCompletion < Formula
   desc "Bash, Zsh and Fish completion for Docker"
   homepage "https://www.docker.com/"
-  url "https://github.com/docker/cli/archive/refs/tags/v28.3.0.tar.gz"
-  sha256 "0ac18927138cd2582e02277d365174a118b962f10084a6bef500a58de705e094"
+  url "https://github.com/docker/cli/archive/refs/tags/v28.3.1.tar.gz"
+  sha256 "ef6808574ea7a4d02ec9715feb8fea419f253fb5b9f68a98020a9e39680baf35"
   license "Apache-2.0"
   head "https://github.com/docker/cli.git", branch: "master"
 
@@ -10,13 +10,11 @@ class DockerCompletion < Formula
     formula "docker"
   end
 
-  no_autobump! because: :requires_manual_review
-
   bottle do
     sha256 cellar: :any_skip_relocation, all: "a19b77d3e86b286ff37b22699eeea211e1d9004a6cc9ea582c76f956af8552cf"
   end
 
-  conflicts_with cask: "docker"
+  conflicts_with cask: "docker-desktop"
 
   # These used to also be provided by the `docker` formula.
   link_overwrite "etc/bash_completion.d/docker"
