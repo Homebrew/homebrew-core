c91520ed4df48314adc84774de917659332ac218
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2025-07-15 05:03:37 -0500

    address style issues

--- Formula/p/preston.rb
+++ Formula/p/preston.rb
@@ -1,5 +1,5 @@
 class Preston < Formula
-  desc "a biodiversity dataset tracker"
+  desc "Biodiversity Dataset Tracker"
   homepage "https://github.com/bio-guoda/preston"
   url "https://github.com/bio-guoda/preston/releases/download/0.11.0/preston.tar.gz"
   sha256 "19d70853f3cc1146f7dccef34dad7525c37bd26e6e9a38d980718fddcc7c7100"
@@ -14,6 +14,6 @@ class Preston < Formula
 
   test do
     output = shell_output("echo 'testing123' | #{bin}/preston track")
-    assert_match "> <http://purl.org/pav/hasVersion> <hash://sha256/d3e54e9d6814999b722fcf9fce64511c16558825fff8d344cb177cafae6e87a3> ", output
+    assert_match "d3e54e9d6814999b722fcf9fce64511c16558825fff8d344cb177cafae6e87a3", output
   end
 end
