ee0472534c787cf31bbb7e389f8adfe911ac0a42
Viktor Szakats <commit@vsz.me>
2025-07-17 14:57:39 +0200

    forgejo 12.0.0 (new formula)

---
+++ Formula/f/forgejo.rb
@@ -0,0 +1,48 @@
+class Forgejo < Formula
+  desc "Self-hosted lightweight software forge"
+  homepage "https://forgejo.org/"
+  url "https://codeberg.org/forgejo/forgejo/releases/download/v12.0.0/forgejo-src-12.0.0.tar.gz"
+  sha256 "d93d873babc2390a1cd327f67cccd736b5e977cb0d6b2baba66e5d919ecae03c"
+  license "GPL-3.0-or-later"
+  head "https://codeberg.org/forgejo/forgejo.git", branch: "forgejo"
+
+  depends_on "go" => :build
+  depends_on "node" => :build
+
+  uses_from_macos "sqlite"
+
+  def install
+    ENV["TAGS"] = "bindata sqlite sqlite_unlock_notify"
+    system "make", "build"
+    bin.install "gitea"
+  end
+
+  service do
+    run [opt_bin/"gitea", "web", "--work-path", var/"forgejo"]
+    keep_alive true
+    log_path var/"log/forgejo.log"
+    error_log_path var/"log/forgejo.log"
+  end
+
+  test do
+    ENV["FORGEJO_WORK_DIR"] = testpath
+    port = free_port
+
+    pid = fork do
+      exec bin/"gitea", "web", "--port", port.to_s, "--install-port", port.to_s
+    end
+    sleep 5
+    sleep 10 if OS.mac? && Hardware::CPU.intel?
+
+    output = shell_output("curl -s http://localhost:#{port}/api/settings/api")
+    assert_match "Go to default page", output
+
+    output = shell_output("curl -s http://localhost:#{port}/")
+    assert_match "Installation - Forgejo: Beyond coding. We Forge.", output
+
+    assert_match version.to_s, shell_output("#{bin}/gitea -v")
+  ensure
+    Process.kill("TERM", pid)
+    Process.wait(pid)
+  end
+end
