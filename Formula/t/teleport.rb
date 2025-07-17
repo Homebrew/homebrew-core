a260e01addd896153fcc304104dd88772d768856
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-17 23:18:26 +0000

    teleport 18.0.2

--- Formula/t/teleport.rb
+++ Formula/t/teleport.rb
@@ -1,8 +1,8 @@
 class Teleport < Formula
   desc "Modern SSH server for teams managing distributed infrastructure"
   homepage "https://goteleport.com/"
-  url "https://github.com/gravitational/teleport/archive/refs/tags/v18.0.0.tar.gz"
-  sha256 "1d88debdf5ed0387bf8a3793001d81165fe999c00e60b74d7dac439b5717574a"
+  url "https://github.com/gravitational/teleport/archive/refs/tags/v18.0.2.tar.gz"
+  sha256 "de20b1141769cc5f90ce9abb7e6de2ef11dcd6efb377c31373e5ddd4740aa835"
   license all_of: ["AGPL-3.0-or-later", "Apache-2.0"]
   head "https://github.com/gravitational/teleport.git", branch: "master"
 
