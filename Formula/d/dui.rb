ab7eef0e6eaf5c8fd3c5d118c88096e45310cfb6
ukhan1219 <113568605+ukhan1219@users.noreply.github.com>
2025-08-01 04:28:03 -0400

    Add dui formula
    - Add Docker management CLI built in Rust
    - Provides GUI-like features in terminal
    - Supports container, image, and network management

---
+++ Formula/d/dui.rb
@@ -0,0 +1,18 @@
+class Dui < Formula
+  desc "An intuitive Docker management CLI built in Rust"
+  homepage "https://github.com/ukhan1219/dui"
+  url "https://github.com/ukhan1219/dui/archive/refs/tags/v4.0.10.tar.gz"
+  sha256 "eaf450e7ffe00f0e81482e9ec5c23afc4aa15ffeac2aa81acc837ac2a7e89e80"
+  license "MIT"
+  head "https://github.com/ukhan1219/dui.git", branch: "main"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    system "#{bin}/dui", "--help"
+  end
+end
