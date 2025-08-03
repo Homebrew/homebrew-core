c490c0916cae735f44241ac3437109c7d5156bda
Tokarak <63452145+Tokarak@users.noreply.github.com>
2025-08-03 18:01:50 +0100

    ghidra: fix style test

--- Formula/g/ghidra.rb
+++ Formula/g/ghidra.rb
@@ -5,7 +5,6 @@ class Ghidra < Formula
   sha256 "8a3f955f04f4a2945afc571a70f1c2140052cdd230fbab99615b1de8480ce4f0"
   license "Apache-2.0"
 
-
   depends_on "gradle" => :build
   depends_on "python@3.13" => :build
   depends_on "openjdk"
@@ -19,7 +18,7 @@ class Ghidra < Formula
 
     libexec.install (buildpath/"build/dist/ghidra_#{version}_PUBLIC").children
     bin.write_env_script libexec/"bin/ghidraRun", Language::Java.overridable_java_home_env("21+")
-
+  end
   test do
     ENV["JAVA_HOME"] = Language::Java.java_home("21+")
     (testpath/"project").mkpath
