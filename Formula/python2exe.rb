232dd5870e40e69dde05fc16f01a6f33b3120dab
Joshua Minguez <planetminguez@gmail.com>
2025-08-22 23:50:24 -0400

    Update python2exe formula to use Python2ExeInC v0.1.0 tarball

--- Formula/python2exe.rb
+++ Formula/python2exe.rb
@@ -1,10 +1,10 @@
 class Python2exe < Formula
   desc "Convert Python scripts into native executables via a generated C wrapper"
-  homepage "https://github.com/planetminguez/homebrew-tools"
-  url "https://github.com/planetminguez/homebrew-tools/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "ef04859472d3d11e2289bc322c7026a241e32d15add2db0524b507eb6fcd5102"
+  homepage "https://github.com/planetminguez/Python2ExeInC"
+  url "https://github.com/planetminguez/Python2ExeInC/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "a71df5fdaedb7f189f59496b58a6cd48421e4952ba6ed54f68cc0d2cd9d6d0ac"
   license "MIT"
-  head "https://github.com/planetminguez/homebrew-tools.git", branch: "main"
+  head "https://github.com/planetminguez/Python2ExeInC.git", branch: "main"
 
   depends_on "python@3.12"
 
@@ -19,7 +19,7 @@ class Python2exe < Formula
       import sys
       print("hello")
       if len(sys.argv) > 1:
-          print(sys.argv[1])
+        print(sys.argv[1])
     PY
 
     system bin/"python2exe", testpath/"hello.py"
@@ -30,4 +30,3 @@ class Python2exe < Formula
     assert_match "world", output
   end
 end
-
