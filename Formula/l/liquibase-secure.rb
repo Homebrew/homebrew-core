7b9eae1fc493841796ab80a7b45b47bb5e4c36b2
Alejandro Alvarez <github@jandroav.net>
2025-08-25 09:38:20 +0200

    liquibase-secure: correct URL for version 4.33.0

--- Formula/l/liquibase-secure.rb
+++ Formula/l/liquibase-secure.rb
@@ -1,7 +1,7 @@
 class LiquibaseSecure < Formula
   desc "Database change management tool (Secure)"
   homepage "https://www.liquibase.org/"
-  url "https://repo.liquibase.com/releases/pro/4.33.0/liquibase-secure-4.33.0.tar.gz"
+  url "https://repo.liquibase.com/releases/pro/4.33.0/liquibase-pro-4.33.0.tar.gz"
   sha256 "689acfcdc97bad0d4c150d1efab9c851e251b398cb3d6326f75e8aafe40ed578"
   license "Apache-2.0"
 
