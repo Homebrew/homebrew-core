bd9767b1a455c872e72ec2bc84faf700c1fb536a
Yuiki Saito <yuikisaito@ysaito.win>
2025-09-06 01:05:55 +0900

    anchor 0.31.1 (new formula)

---
+++ Formula/a/anchor.rb
@@ -0,0 +1,19 @@
+class Anchor < Formula
+  desc "Solana Program Framework"
+  homepage "https://anchor-lang.com"
+  url "https://github.com/solana-foundation/anchor/archive/refs/tags/v0.31.1.tar.gz"
+  sha256 "0c9b1e3e1f14e78cb00271171b1cfac177c7c887814b022196bcbf7e2389e089"
+  license "Apache-2.0"
+
+  depends_on "rust" => :build
+  depends_on "node"
+  depends_on "yarn"
+
+  def install
+    system "cargo", "install", "--no-default-features", *std_cargo_args(path: "cli")
+  end
+
+  test do
+    assert_match "anchor-cli #{version}", shell_output("#{bin}/anchor --version")
+  end
+end
