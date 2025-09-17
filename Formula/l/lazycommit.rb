382af8490a85ef6607cc55e11e53a31a48dc175b
Kartik Labhshetwar <kartik.labhshetwar@gmail.com>
2025-09-18 02:42:44 +0530

    lazycommit 1.0.10 (new formula)

--- Formula/l/lazycommit.rb
+++ Formula/l/lazycommit.rb
@@ -6,7 +6,7 @@ class Lazycommit < Formula
   url "https://registry.npmjs.org/lazycommitt/-/lazycommitt-1.0.10.tgz"
   sha256 "a7f888b386988d4eaf6b070cbab33f0690945b6011e01d1a0005544c0450f363"
   license "Apache-2.0"
-
+ 
   livecheck do
     url :stable
     strategy :npm
