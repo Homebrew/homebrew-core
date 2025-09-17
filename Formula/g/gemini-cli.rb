80d666e10946ccd264b5bf8a8c920706148ccc6c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-17 20:00:42 +0000

    gemini-cli 0.5.1

--- Formula/g/gemini-cli.rb
+++ Formula/g/gemini-cli.rb
@@ -1,8 +1,8 @@
 class GeminiCli < Formula
   desc "Interact with Google Gemini AI models from the command-line"
   homepage "https://github.com/google-gemini/gemini-cli"
-  url "https://registry.npmjs.org/@google/gemini-cli/-/gemini-cli-0.4.1.tgz"
-  sha256 "4264af52ff410840e81092de13efbb00b3ffd5eef5549919c159fad1d392d906"
+  url "https://registry.npmjs.org/@google/gemini-cli/-/gemini-cli-0.5.1.tgz"
+  sha256 "fa2ecfe35d9efbb370902fa3f630c98dd242e5f378871b7db2e302f745767345"
   license "Apache-2.0"
 
   bottle do
