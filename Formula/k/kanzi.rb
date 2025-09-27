01cbb911b4f0c96d193b773731bf744b33ffb43a
Frederic Langlet <flanglet@gmail.com>
2025-09-27 13:19:47 -0400

    Address formula syntax issues

--- Formula/k/kanzi.rb
+++ Formula/k/kanzi.rb
@@ -5,21 +5,17 @@ class Kanzi < Formula
   sha256 "0fdf5979dd3712d8e143a084fa46cf2113058213c5bc02b834371fd6764fc19d"
   license "Apache-2.0"
 
-  bottle do
-    sha256 cellar: :any,                 arm64_sequoia: "6276e6e8448764518f783b0bd28761a47c5ae343e7cc457cb409e844bf24b3d4"
-  end
+  depends_on "cmake" => :build
 
   def install
-    system "make", "install"
-  end
-
-  def uninstall
-    system "make", "uninstall"
+    system "cmake", "-S", "src", "-B", "build", *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
   end
 
   test do
-    srcpath = testpath + "test_kanzi.txt"
-    dstpath = testpath + "test_kanzi.txt.knz"
+    srcpath = testpath/"test_kanzi.txt"
+    dstpath = testpath/"test_kanzi.txt.knz"
 
     srcpath.write "TEST CONTENT"
 
