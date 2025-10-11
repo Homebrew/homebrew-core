d4e984607fe0d85a87bf766a9d97cc701d2f28a5
Roger Ye <roger.ye@sap.com>
2025-10-11 21:10:08 +0800

    cpplint: migrate to python@3.14

--- Formula/c/cpplint.rb
+++ Formula/c/cpplint.rb
@@ -6,12 +6,13 @@ class Cpplint < Formula
   url "https://files.pythonhosted.org/packages/c5/83/47a9e7513ba4d943a9dac2f6752b444377c91880f4f4968799b4f42d89cc/cpplint-2.0.2.tar.gz"
   sha256 "8a5971e4b5490133e425284f0c566c7ade0b959e61018d2c9af3ff7f357ddc57"
   license "Apache-2.0"
+  revision 1
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "e806fee8faa6d3aa1ea85acfe05ddb6b436f60ec95754190bc2b5ad893140218"
   end
 
-  depends_on "python@3.13"
+  depends_on "python@3.14"
 
   def install
     virtualenv_install_with_resources
