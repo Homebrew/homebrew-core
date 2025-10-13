a7a5d3c726a4cce61c48150a7d9c61165ea2525a
Roman Valls Guimera <brainstorm@users.noreply.github.com>
2025-10-13 11:36:20 +1100

    Correct version, sans v

--- Formula/c/copyrite.rb
+++ Formula/c/copyrite.rb
@@ -1,7 +1,7 @@
 class Copyrite < Formula
   desc "CLI tool for efficient checksum and copy operations across object stores"
   homepage "https://github.com/umccr/copyrite"
-  url "https://github.com/umccr/copyrite/archive/refs/tags/v0.2.5.tar.gz"
+  url "https://github.com/umccr/copyrite/archive/refs/tags/0.2.5.tar.gz"
   sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
   license "MIT"
 
