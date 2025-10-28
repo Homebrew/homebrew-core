d06a9e5f4cd48c8f373b165c6a12d83da4b5128a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-28 20:12:39 +0000

    node 25.1.0

--- Formula/n/node.rb
+++ Formula/n/node.rb
@@ -1,10 +1,9 @@
 class Node < Formula
   desc "Platform built on V8 to build network applications"
   homepage "https://nodejs.org/"
-  url "https://nodejs.org/dist/v24.10.0/node-v24.10.0.tar.xz"
-  sha256 "f17e36cb2cc8c34a9215ba57b55ce791b102e293432ed47ad63cbaf15f78678f"
+  url "https://nodejs.org/dist/v25.1.0/node-v25.1.0.tar.xz"
+  sha256 "ee7741190e47402dfc621547ac23d3f58e6463a86878dc1879fb9e8de1ce3226"
   license "MIT"
-  revision 1
   head "https://github.com/nodejs/node.git", branch: "main"
 
   livecheck do
