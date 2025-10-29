2fd9fa3a92e833a5aa0f61c24b443f9d0d445652
Osama Albahrani <54853250+osalbahr@users.noreply.github.com>
2025-10-29 19:49:25 +0300

    redex 2025.09.18

--- Formula/r/redex.rb
+++ Formula/r/redex.rb
@@ -4,41 +4,10 @@ class Redex < Formula
 
   desc "Bytecode optimizer for Android apps"
   homepage "https://fbredex.com/"
+  url "https://github.com/facebook/redex/archive/refs/tags/v2025.09.18.tar.gz"
+  sha256 "49be286761fb89a223a9609d58faa141e584a0c6866bf083d8408357302ee2f8"
   license "MIT"
-  revision 20
   head "https://github.com/facebook/redex.git", branch: "main"
-
-  stable do
-    url "https://github.com/facebook/redex/archive/refs/tags/v2017.10.31.tar.gz"
-    sha256 "18a840e4db0fc51f79e17dfd749b2ffcce65a28e7ef9c2b3c255c5ad89f6fd6f"
-
-    # Fix for automake 1.16.5
-    patch do
-      url "https://github.com/facebook/redex/commit/4696e1882cf88707bf7560a2994a4207a8b7c7a3.patch?full_index=1"
-      sha256 "dccc41146688448ea2d99dd04d4d41fdaf7e174ae1888d3abb10eb2dfa6ed1da"
-    end
-
-    # Apply upstream fixes for GCC 11
-    patch do
-      url "https://github.com/facebook/redex/commit/70a82b873da269e7dd46611c73cfcdf7f84efa1a.patch?full_index=1"
-      sha256 "44ce35ca93922f59fb4d0fd1885d24cce8a08d73b509e1fd2675557948464f1d"
-    end
-    patch do
-      url "https://github.com/facebook/redex/commit/e81dda3f26144a9c94816c12237698ef2addf864.patch?full_index=1"
-      sha256 "523ad3d7841a6716ac973b467be3ea8b6b7e332089f23e4788e1f679fd6f53f5"
-    end
-    patch do
-      url "https://github.com/facebook/redex/commit/253b77159d6783786c8814168d1ff2b783d3a531.patch?full_index=1"
-      sha256 "ed69a6230506704ca4cc7a52418b3af70a6182bd96abdb5874fab02f6b1a7c99"
-    end
-
-    # Fix compilation on High Sierra
-    # Fix boost issue (https://github.com/facebook/redex/pull/564)
-    # Remove for next release
-    patch :DATA
-  end
-
-  no_autobump! because: :requires_manual_review
 
   bottle do
     rebuild 1
@@ -109,36 +78,3 @@ class Redex < Formula
     assert_path_exists testpath/"redex-test-out.apk"
   end
 end
-
-__END__
-diff --git a/libresource/RedexResources.cpp b/libresource/RedexResources.cpp
-index 525601ec..a359f49f 100644
---- a/libresource/RedexResources.cpp
-+++ b/libresource/RedexResources.cpp
-@@ -16,6 +16,7 @@
- #include <map>
- #include <boost/regex.hpp>
- #include <sstream>
-+#include <stack>
- #include <string>
- #include <unordered_set>
- #include <vector>
-diff --git a/libredex/Show.cpp b/libredex/Show.cpp
-index b042070f..5e492e3f 100644
---- a/libredex/Show.cpp
-+++ b/libredex/Show.cpp
-@@ -9,7 +9,14 @@
-
- #include "Show.h"
-
-+#include <boost/version.hpp>
-+// Quoted was accepted into public components as of 1.73. The `detail`
-+// header was removed in 1.74.
-+#if BOOST_VERSION < 107400
- #include <boost/io/detail/quoted_manip.hpp>
-+#else
-+#include <boost/io/quoted.hpp>
-+#endif
- #include <sstream>
-
- #include "ControlFlow.h"
