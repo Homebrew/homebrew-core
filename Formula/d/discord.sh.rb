2d7575e422f924ed5ac0b9a8312d23f4d1560c5a
Sheldon Rupp <hello@sheldon.is>
2025-11-04 01:00:16 +0100

    discord.sh 2.0.1 (new formula)

---
+++ Formula/d/discord.sh.rb
@@ -0,0 +1,21 @@
+class DiscordSh < Formula
+  desc "Write-only command-line Discord webhooks integration"
+  homepage "https://github.com/fieu/discord.sh"
+  url "https://github.com/fieu/discord.sh/archive/refs/tags/v2.0.1.tar.gz"
+  sha256 "49e92bf242f68bee9d739adf77af08ea5ccb58cd0c406a6e25eef42e27a25dcd"
+  license "GPL-3.0-or-later"
+
+  depends_on "bash"
+  depends_on "curl"
+  depends_on "jq"
+
+  def install
+    bin.install "discord.sh"
+  end
+
+  test do
+    assert_match "Usage: discord.sh", shell_output("#{bin}/discord.sh --help 2>&1")
+
+    assert_predicate bin/"discord.sh", :executable?
+  end
+end
