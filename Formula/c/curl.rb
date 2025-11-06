29381dd515e199417efcfc3052b24f47ed04fdf6
Ismael Dönmez <ismael@i10z.com>
2025-11-06 23:37:17 +0100

    curl: Bump revision
    Forgotten during the last commit
    
    Signed-off-by: Ismael Doenmez <ismael@i10z.com>

--- Formula/c/curl.rb
+++ Formula/c/curl.rb
@@ -8,6 +8,7 @@ class Curl < Formula
   mirror "http://fresh-center.net/linux/www/legacy/curl-8.17.0.tar.bz2"
   sha256 "230032528ce5f85594d4f3eace63364c4244ccc3c801b7f8db1982722f2761f4"
   license "curl"
+  revision 1
 
   livecheck do
     url "https://curl.se/download/"
