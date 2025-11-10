dbe6c39175f28c7516e1e3030d9497519118e962
MorganChen <cjfmail@foxmail.com>
2025-11-10 11:36:29 +0800

    chttp 1.0.1 (new formula)

---
+++ Formula/c/chttp.rb
@@ -0,0 +1,39 @@
+class Chttp < Formula
+  desc "Open-source C library for HTTP requests"
+  homepage "https://github.com/json031/chttp"
+  url "https://github.com/json031/chttp/archive/refs/tags/v1.0.1.tar.gz"
+  sha256 "6fd22b6cfad7078180b3fd76f4a2c1970672015786701c90595d91037320e1ba"
+  license "MIT"
+
+  depends_on "pkg-config" => :build
+  depends_on "cjson"
+  depends_on "curl"
+
+  def install
+    ENV.append "CFLAGS", "-I#{Formula["cjson"].opt_include}"
+    ENV.append "LDFLAGS", "-L#{Formula["cjson"].opt_lib}"
+
+    system "make"
+    system "make", "install", "PREFIX=#{prefix}"
+  end
+
+  test do
+
+  (testpath/"test.c").write <<~EOS
+    #include <chttp.h>
+    int main() { return 0; }
+  EOS
+
+  system ENV.cc,
+         "test.c",
+         "-I#{include}",                              # chttp include
+         "-I#{Formula["cjson"].opt_include}",         # cjson include
+         "-L#{lib}",                                  # chttp lib
+         "-L#{Formula["cjson"].opt_lib}",             # cjson lib
+         "-lcjson", "-lchttp", "-lcurl",
+         "-o", "test_chttp"
+
+  system "./test_chttp"
+
+  end
+end
