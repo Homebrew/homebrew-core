79aaf62d6094fbc28be2813f7f52ceb800f46a11
Nolan Lwin <113712250+nolanlwin@users.noreply.github.com>
2025-11-16 18:02:31 -0500

    Update Formula/l/l2m.rb
    Co-authored-by: Anton Melnikov <antonsm21@gmail.com>

--- Formula/l/l2m.rb
+++ Formula/l/l2m.rb
@@ -1,7 +1,7 @@
 class L2m < Formula
   desc "AI-powered legacy code modernization tool"
   homepage "https://github.com/astrio-ai/l2m"
-  url "https://files.pythonhosted.org/packages/source/l/l2m/l2m-0.2.0.tar.gz"
+  url "https://files.pythonhosted.org/packages/03/42/153c7c7f8fdd6ca124d183df7515b5113c77a34c24dd98bdf07bed70b64a/l2m-0.2.0.tar.gz"
   sha256 "ecf3e100bdab0c552303fc56dc66f213043a3ce0de03b8a3005c901c2a8ecbde"
   license "Apache-2.0"
   head "https://github.com/astrio-ai/l2m.git", branch: "main"
