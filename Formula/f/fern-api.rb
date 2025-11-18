545f5eb1be6fb2cc67fbfed030b9457b38a6c534
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-18 17:05:04 +0000

    fern-api 1.8.0

--- Formula/f/fern-api.rb
+++ Formula/f/fern-api.rb
@@ -1,19 +1,16 @@
 class FernApi < Formula
   desc "Stripe-level SDKs and Docs for your API"
   homepage "https://buildwithfern.com/"
-  url "https://registry.npmjs.org/fern-api/-/fern-api-1.5.0.tgz"
-  sha256 "6c74ac698cc2fae70eda61095fd45f7aff6df835a15e77d4eeb7d7bcc33fbb6a"
+  url "https://registry.npmjs.org/fern-api/-/fern-api-1.8.0.tgz"
+  sha256 "e06da8db7e52acfd6c8da7194400a3e141c30d9de09d5643a033bdfd7cd6e43a"
   license "Apache-2.0"
-
-  bottle do
-    sha256 cellar: :any_skip_relocation, all: "eec0b02cf0e949b1d730212f93cd38db847c1cdd2993656bf7510c6425220130"
-  end
 
   depends_on "node"
 
   def install
     # Supress self update notifications
     inreplace "cli.cjs", "await this.nudgeUpgradeIfAvailable()", "await 0"
+
     system "npm", "install", *std_npm_args
     bin.install_symlink libexec.glob("bin/*")
   end
