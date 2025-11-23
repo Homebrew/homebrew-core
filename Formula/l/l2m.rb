977c87e2123e8e296f5934ca070f23e41fea5bf5
naingoolwin <naingoolwin.astrio@gmail.com>
2025-11-22 19:03:16 -0500

    l2m: add version check and update policy

--- Formula/l/l2m.rb
+++ Formula/l/l2m.rb
@@ -1,10 +1,13 @@
 class L2m < Formula
   desc "AI-powered legacy code modernization tool"
   homepage "https://github.com/astrio-ai/l2m"
+  # l2m should only be updated every 10 releases on multiples of 10
   url "https://files.pythonhosted.org/packages/03/42/153c7c7f8fdd6ca124d183df7515b5113c77a34c24dd98bdf07bed70b64a/l2m-0.2.0.tar.gz"
   sha256 "ecf3e100bdab0c552303fc56dc66f213043a3ce0de03b8a3005c901c2a8ecbde"
   license "Apache-2.0"
   head "https://github.com/astrio-ai/l2m.git", branch: "main"
+
+  no_autobump! because: :requires_manual_review
 
   depends_on "python@3.10"
 
@@ -13,6 +16,7 @@ class L2m < Formula
   end
 
   test do
-    system "#{bin}/l2m", "--version"
+    # Test version output
+    assert_match version.to_s, shell_output("#{bin}/l2m --version")
   end
 end
