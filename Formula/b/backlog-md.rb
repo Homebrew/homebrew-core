ab08ddf115587e73b25064442bb0733f4e0148b4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-24 22:53:05 +0000

    backlog-md 1.21.0

--- Formula/b/backlog-md.rb
+++ Formula/b/backlog-md.rb
@@ -1,8 +1,8 @@
 class BacklogMd < Formula
   desc "Markdown‑native Task Manager & Kanban visualizer for any Git repository"
   homepage "https://github.com/MrLesk/Backlog.md"
-  url "https://registry.npmjs.org/backlog.md/-/backlog.md-1.20.1.tgz"
-  sha256 "2802031d9e7d53e4c2c9f2dc175c0ce805969a05c2bf7d40e211cc6783de86da"
+  url "https://registry.npmjs.org/backlog.md/-/backlog.md-1.21.0.tgz"
+  sha256 "204fcfc5ee47203948dc3f295cdd3029513e72019fde4350f6c99d540511449d"
   license "MIT"
 
   bottle do
