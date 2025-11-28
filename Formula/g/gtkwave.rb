7885645ad4bbd960994a2f90830af2f2d36553e8
CJ <sjchin@akribis-sys.com>
2025-11-28 20:26:40 +0800

    gtkwave: address PR review comments
    - Change pkg-config to pkgconf (Copilot AI suggestion)
    - Add flex and gperf as build dependencies (required by meson.build)
    - Add bzip2 and zlib via uses_from_macos (required by meson.build)
    - Add comment explaining why both GTK3 and GTK4 are required
    - Disable update_mime_database during install (not needed for Homebrew)
    - Improve test block: convert VCD to FST using vcd2fst tool instead of
      just checking --version (as requested by @fxcoudert)

--- Formula/g/gtkwave.rb
+++ Formula/g/gtkwave.rb
@@ -6,24 +6,50 @@ class Gtkwave < Formula
   license "GPL-2.0-or-later"
   head "https://github.com/gtkwave/gtkwave.git", branch: "master"
 
+  depends_on "flex" => :build
+  depends_on "gperf" => :build
   depends_on "meson" => :build
   depends_on "ninja" => :build
-  depends_on "pkg-config" => :build
+  depends_on "pkgconf" => :build
   depends_on "glib"
+  # GTKWave requires both GTK3 and GTK4 - the upstream meson.build unconditionally
+  # requires both: gtk+-3.0 for the main application and gtk4 for libgtkwave library.
+  # See: https://github.com/gtkwave/gtkwave/blob/v3.4.0/meson.build
   depends_on "gtk+3"
   depends_on "gtk4"
+
+  uses_from_macos "bzip2"
+  uses_from_macos "zlib"
 
   on_macos do
     depends_on "gtk-mac-integration"
   end
 
   def install
-    system "meson", "setup", "build", *std_meson_args
+    system "meson", "setup", "build", *std_meson_args, "-Dupdate_mime_database=false"
     system "meson", "compile", "-C", "build", "--verbose"
     system "meson", "install", "-C", "build"
   end
 
   test do
-    system bin/"gtkwave", "--version"
+    # Create a minimal VCD file
+    (testpath/"test.vcd").write <<~VCD
+      $timescale 1ns $end
+      $var wire 1 ! clk $end
+      $enddefinitions $end
+      #0
+      0!
+      #10
+      1!
+      #20
+      0!
+    VCD
+
+    # Convert VCD to FST format and verify the output file exists
+    system bin/"vcd2fst", "test.vcd", "test.fst"
+    assert_path_exists testpath/"test.fst"
+
+    # Verify version output
+    assert_match version.to_s, shell_output("#{bin}/gtkwave --version")
   end
 end
