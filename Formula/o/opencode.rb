04a71cd352fcacf33f75a7dce5a60484e28fad1c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-29 02:47:06 +0000

    opencode 1.0.120

--- Formula/o/opencode.rb
+++ Formula/o/opencode.rb
@@ -1,8 +1,8 @@
 class Opencode < Formula
   desc "AI coding agent, built for the terminal"
   homepage "https://opencode.ai"
-  url "https://registry.npmjs.org/opencode-ai/-/opencode-ai-1.0.107.tgz"
-  sha256 "590e4ad65b21b94db31fb091477554e199ca9195cd15b8597dd2f5dcbd8b0eca"
+  url "https://registry.npmjs.org/opencode-ai/-/opencode-ai-1.0.120.tgz"
+  sha256 "cad86b981adea739a9d3ade869ceca16c282c14e7cb37e2a20de989e7f56d346"
   license "MIT"
 
   bottle do
