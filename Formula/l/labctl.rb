1f755dc5c72205c8d7f0d19e5fa3fbf01bdfda5e
raashed <mohammedraashed10@gmail.com>
2025-12-02 17:05:03 +0530

    feat(formula): add Labctl CLI tool integration for Homebrew
    - Introduced Labctl as a new formula for Homebrew.
    - Added support for macOS and Linux architectures (arm64 and amd64).
    - Included installation and testing methods for the Labctl executable.
    - Documented the tool with a description and homepage link.

---
+++ Formula/l/labctl.rb
@@ -0,0 +1,36 @@
+class Labctl < Formula
+    desc "CLI tool for interacting with iximiuz labs and playgrounds"
+    homepage "https://github.com/iximiuz/labctl"
+    license "MIT"
+  
+    version "0.1.50"
+  
+    on_macos do
+      if Hardware::CPU.arm?
+        url "https://github.com/iximiuz/labctl/releases/download/v#{version}/labctl_darwin_arm64.tar.gz"
+        sha256 "6d4209f50f7cc1ae33bc2d492ffb5d2dd9083475dfc7d787c45e08acc2ca5ad1"
+      else
+        url "https://github.com/iximiuz/labctl/releases/download/v#{version}/labctl_darwin_amd64.tar.gz"
+        sha256 "274ea09f6d93a5d0bf35b7767b842d6bbc9c5199ef9fd1dd324ba3c7fa0e1601"
+      end
+    end
+  
+    on_linux do
+      if Hardware::CPU.arm?
+        url "https://github.com/iximiuz/labctl/releases/download/v#{version}/labctl_linux_arm64.tar.gz"
+        sha256 "df136bb19c164e6828fce045114708ee5170e053c756b5a410e1c6c6deeb74da"
+      else
+        url "https://github.com/iximiuz/labctl/releases/download/v#{version}/labctl_linux_amd64.tar.gz"
+        sha256 "34f4f9c3643e37f2fabf26b01620d7f0b7bed34481ad9e1869a017478b356f4b"
+      end
+    end
+  
+    def install
+      bin.install "labctl"
+    end
+  
+    test do
+      output = shell_output("#{bin}/labctl --help")
+      assert_match "labctl", output
+    end
+  end  
