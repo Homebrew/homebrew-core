c69a97aca1842c236e792024cee19900c95f1ef4
Joel Natividad <1980690+jqnatividad@users.noreply.github.com>
2025-12-08 10:02:11 -0500

    qsv 11.0.2 - new expected value for test
    as we added three columns to qsv stats

--- Formula/q/qsv.rb
+++ Formula/q/qsv.rb
@@ -46,9 +46,9 @@ class Qsv < Formula
   test do
     (testpath/"test.csv").write("first header,second header")
     assert_equal <<~EOS, shell_output("#{bin}/qsv stats test.csv")
-      field,type,is_ascii,sum,min,max,range,sort_order,sortiness,min_length,max_length,sum_length,avg_length,stddev_length,variance_length,cv_length,mean,sem,geometric_mean,harmonic_mean,stddev,variance,cv,nullcount,max_precision,sparsity
-      first header,NULL,,,,,,,,,,,,,,,,,,,,,,0,,
-      second header,NULL,,,,,,,,,,,,,,,,,,,,,,0,,
+      field,type,is_ascii,sum,min,max,range,sort_order,sortiness,min_length,max_length,sum_length,avg_length,stddev_length,variance_length,cv_length,mean,sem,geometric_mean,harmonic_mean,stddev,variance,cv,nullcount,n_negative,n_zero,n_positive,max_precision,sparsity
+      first header,NULL,,,,,,,,,,,,,,,,,,,,,,0,,,,,
+      second header,NULL,,,,,,,,,,,,,,,,,,,,,,0,,,,,
     EOS
   end
 end
