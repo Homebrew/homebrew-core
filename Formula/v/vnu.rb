1deb9a89baa94ef6fe2989f65e5c0a6b0375d14e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-11 08:25:58 +0000

    vnu 25.12.11

--- Formula/v/vnu.rb
+++ Formula/v/vnu.rb
@@ -1,8 +1,8 @@
 class Vnu < Formula
   desc "Nu Markup Checker: command-line and server HTML validator"
   homepage "https://validator.github.io/validator/"
-  url "https://registry.npmjs.org/vnu-jar/-/vnu-jar-25.12.9.tgz"
-  sha256 "fb2f7dbd50f6635285e371919836c064252e51ea10d1ecee1509ef1a81a8b606"
+  url "https://registry.npmjs.org/vnu-jar/-/vnu-jar-25.12.11.tgz"
+  sha256 "ec2d96b3b77a36870c1ecd19fa9d37af134eb767894f2d0cad923690070531bb"
   license "MIT"
   version_scheme 1
 
