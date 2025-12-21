f4dea3104c3e7087d92d0fc483eefe548fbc669d
Akshat <79302042+Akczht@users.noreply.github.com>
2025-12-22 02:36:46 +0530

    Update ffmpeg.rb

--- Formula/f/ffmpeg.rb
+++ Formula/f/ffmpeg.rb
@@ -42,8 +42,6 @@ class Ffmpeg < Formula
   depends_on "libvmaf"
   depends_on "libvorbis"
   depends_on "libvpx"
-  depends_on "libx11"
-  depends_on "libxcb"
   depends_on "opencore-amr"
   depends_on "openjpeg"
   depends_on "opus"
@@ -76,6 +74,8 @@ class Ffmpeg < Formula
 
   on_linux do
     depends_on "alsa-lib"
+    depends_on "libx11"
+    depends_on "libxcb"
     depends_on "libxext"
     depends_on "libxv"
   end
