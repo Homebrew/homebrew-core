deda9492b8c73e26ddbaa72e14ff80a7f968997e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-26 23:17:37 +0000

    qxmpp 1.13.0

--- Formula/q/qxmpp.rb
+++ Formula/q/qxmpp.rb
@@ -1,8 +1,8 @@
 class Qxmpp < Formula
   desc "Cross-platform C++ XMPP client and server library"
   homepage "https://invent.kde.org/libraries/qxmpp"
-  url "https://invent.kde.org/libraries/qxmpp/-/archive/v1.11.3/qxmpp-v1.11.3.tar.bz2"
-  sha256 "afa01989d80d06c377b91af82cd951b8bdf568e6f8f76b9446756d99de7f5e29"
+  url "https://invent.kde.org/libraries/qxmpp/-/archive/v1.13.0/qxmpp-v1.13.0.tar.bz2"
+  sha256 "c77709104cf669bafb59ad7d857c751afe9a055fc05b53e5f65e79e88bcd7b6c"
   license "LGPL-2.1-or-later"
 
   bottle do
