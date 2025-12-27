1fe46bc9b47ec935b42c0f15712f6b7ebaf4914f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-27 19:48:48 +0000

    age-plugin-se 0.2.1

--- Formula/a/age-plugin-se.rb
+++ Formula/a/age-plugin-se.rb
@@ -1,8 +1,8 @@
 class AgePluginSe < Formula
   desc "Age plugin for Apple Secure Enclave"
   homepage "https://github.com/remko/age-plugin-se"
-  url "https://github.com/remko/age-plugin-se/archive/refs/tags/v0.1.4.tar.gz"
-  sha256 "52d9b9583783988fbe5e94bbe72089a870d128a2eba197fc09a95c13926fb27a"
+  url "https://github.com/remko/age-plugin-se/archive/refs/tags/v0.2.1.tar.gz"
+  sha256 "a22ba4de99a6463b044894e0d7d26a2c9859be6577e2085b4082481e1ae6e6bc"
   license "MIT"
   head "https://github.com/remko/age-plugin-se.git", branch: "main"
 
