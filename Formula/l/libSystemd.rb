58455d85972d107bd4d5361b996d5a11a68051d7
Trung <trung_bao.d@kbtgvn.tech>
2025-12-30 10:46:11 +0700

    Update fomular

--- Formula/l/libSystemd.rb
+++ Formula/l/libSystemd.rb
@@ -1,6 +1,6 @@
 class Libsystemd < Formula
   include Language::Python::Virtualenv
-
+  
   desc "Port of systemd libsystemd components to macOS"
   homepage "https://github.com/open-sources-port/systemd-port"
   url "https://github.com/open-sources-port/systemd-port/archive/refs/tags/v0.1.1.tar.gz"
@@ -21,16 +21,6 @@ class Libsystemd < Formula
   depends_on "python@3.12" => :build
   depends_on "jinja2-cli" => :build
 
-  # resource "jinja2" do
-  #   url "https://files.pythonhosted.org/packages/source/J/Jinja2/Jinja2-3.2.3.tar.gz"
-  #   sha256 "a2f7f0446a11b60d2f7f650de4f848b04e7f6dc575826c437a2a9274b621e2e4"
-  # end
-
-  # resource "jinja2-cli" do
-  #   url "https://files.pythonhosted.org/packages/source/j/jinja2-cli/jinja2-cli-1.1.1.tar.gz"
-  #   sha256 "f1d8630e9bfbf8f1f16209db5c728ccf781e1c5d0dc8ef1ff1b13f306be5d9e8"
-  # end
-
   def install
     # Use Homebrew compiler toolchain
     ENV["CC"]  = ENV.cc
@@ -49,27 +39,9 @@ class Libsystemd < Formula
 
     cd "macos-homebrew" do
       system "CC=clang", "CXX=clang++", "meson", "setup", "build",
-             "--buildtype=debugoptimized",
-             "--prefix=#{prefix}",
-             "--sysconfdir=#{etc}",
-             "--localstatedir=#{var}"
-            #  "-Ddefault-hierarchy=unified",
-            #  "-Dselinux=false",
-            #  "-Dapparmor=false",
-            #  "-Dima=false",
-            #  "-Dsmack=false",
-            #  "-Dpolkit=false",
-            #  "-Dresolve=false",
-            #  "-Dtimesyncd=false",
-            #  "-Dmachined=false",
-            #  "-Dlogind=false",
-            #  "-Dnetworkd=false",
-            #  "-Dhomed=false",
-            #  "-Dfirstboot=false",
-            #  "-Dldconfig=false"
-
-      system "meson", "compile", "-C", "build"
-      system "meson", "install", "-C", "build"
+          "--buildtype=debugoptimized", "--prefix=#{prefix}", "--sysconfdir=#{etc}",
+          "--localstatedir=#{var}"
+      system "ninja", "-C", "build"
     end
   end
 
