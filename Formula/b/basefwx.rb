f49aec6f2f02f082df9182ca6c5949687db182c6
F1xGOD FWS <f1xgodim@gmail.com>
2026-01-01 05:39:36 -0600

    basefwx 3.5.3 (new formula)

---
+++ Formula/b/basefwx.rb
@@ -0,0 +1,35 @@
+class Basefwx < Formula
+  desc "Hybrid post-quantum and AEAD encryption toolkit for files and media"
+  homepage "https://github.com/F1xGOD/basefwx"
+  url "https://github.com/F1xGOD/basefwx/archive/refs/tags/v3.5.3.tar.gz"
+  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
+  license "GPL-3.0-or-later"
+
+  depends_on "cmake" => :build
+  depends_on "pkg-config" => :build
+  depends_on "ffmpeg"
+  depends_on "openssl@3"
+  depends_on "xz"
+  depends_on "zlib"
+
+  def install
+    args = %W[
+      -DCMAKE_BUILD_TYPE=Release
+      -DBASEFWX_BUILD_CLI=ON
+      -DBASEFWX_REQUIRE_ARGON2=OFF
+      -DBASEFWX_REQUIRE_OQS=OFF
+      -DBASEFWX_REQUIRE_LZMA=ON
+      -DOPENSSL_ROOT_DIR=#{Formula["openssl@3"].opt_prefix}
+    ]
+
+    system "cmake", "-S", "cpp", "-B", "build", *args
+    system "cmake", "--build", "build", "--config", "Release"
+    bin.install "build/basefwx_cpp" => "basefwx"
+  end
+
+  test do
+    encoded = shell_output("#{bin}/basefwx b256-enc brewtest").strip
+    decoded = shell_output("#{bin}/basefwx b256-dec #{encoded}").strip
+    assert_equal "brewtest", decoded
+  end
+end
