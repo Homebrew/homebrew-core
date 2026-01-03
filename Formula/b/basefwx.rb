8276e1f9b7b43f9d095f9acff42dc727d11a04c6
F1xGOD <105874129+F1xGOD@users.noreply.github.com>
2026-01-03 13:26:03 -0800

    Update Formula/b/basefwx.rb
    Co-authored-by: Daeho Ro <40587651+daeho-ro@users.noreply.github.com>

--- Formula/b/basefwx.rb
+++ Formula/b/basefwx.rb
@@ -2,7 +2,7 @@ class Basefwx < Formula
   desc "Hybrid post-quantum and AEAD encryption toolkit for files and media"
   homepage "https://github.com/F1xGOD/basefwx"
   url "https://github.com/F1xGOD/basefwx/archive/refs/tags/v3.5.3.tar.gz"
-  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
+  sha256 "cd24749ac76dccc788dff493bf6b96a58dd7c740167e67cfd7467c13f6e7ec5a"
   license "GPL-3.0-or-later"
 
   depends_on "cmake" => :build
