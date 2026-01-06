ebe7b29479d5021246534a47058aa23a402a8e15
Ivan Uhalin <hi@vanyauhalin.me>
2026-01-06 20:18:11 +0400

    Create gcal.rb

---
+++ Formula/g/gcal.rb
@@ -0,0 +1,7 @@
+class Gcal < Formula
+  desc "Program for calculating and printing calendars"
+  homepage "https://www.gnu.org/software/gcal/"
+  url "https://www.alteholz.dev/gnu/gcal-4.2.0.tar.gz"
+  sha256 "b670e21bf0972c91f18f5b1311521038bd950d96428ab58f509dd4e4a7c5b185"
+  license "GPL-3.0-or-later"
+end
