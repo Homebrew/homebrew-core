48513fbeeb1a4fa7b5245bdc3c2c801669d4cb9c
Phil Beyer <6152278+pjbeyer@users.noreply.github.com>
2026-01-08 09:42:45 -0500

    opencode 1.1.6

--- Formula/o/opencode.rb
+++ Formula/o/opencode.rb
@@ -1,21 +1,12 @@
 class Opencode < Formula
   desc "AI coding agent, built for the terminal"
   homepage "https://opencode.ai"
-  url "https://registry.npmjs.org/opencode-ai/-/opencode-ai-1.0.220.tgz"
-  sha256 "c4bf1790626001477af0c1feca476c53091e351bccfa0585c1e8c47c0bd57b70"
+  url "https://registry.npmjs.org/opencode-ai/-/opencode-ai-1.1.6.tgz"
+  sha256 "36e32f99a0fd70aba56349f4543c1a25c98320b2396a42a1379ba2380a0f8f63"
   license "MIT"
 
   livecheck do
     throttle 10
-  end
-
-  bottle do
-    sha256                               arm64_tahoe:   "9b73883da81dbdb07c59ae54acb6447b3780d1bbd8e2dc1fdd0604ee398ba093"
-    sha256                               arm64_sequoia: "9b73883da81dbdb07c59ae54acb6447b3780d1bbd8e2dc1fdd0604ee398ba093"
-    sha256                               arm64_sonoma:  "9b73883da81dbdb07c59ae54acb6447b3780d1bbd8e2dc1fdd0604ee398ba093"
-    sha256 cellar: :any_skip_relocation, sonoma:        "57c5ed35393c033db9180793affc2a89ab2325a892d4bcb750d6e6e4acf77d6e"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "e5829a5d3479830dbba3e1375b7efec6e9c657dffe87fb02dd8272fc469f4b40"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d525cd1368cddddc44c68d1af96a2d71b9aef09395e0f562c78a3d18f8331b1f"
   end
 
   depends_on "node"
@@ -40,3 +31,4 @@ class Opencode < Formula
     assert_match "opencode", shell_output("#{bin}/opencode models")
   end
 end
+
