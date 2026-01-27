40fbdc3bf02f793af58cf8c6223d5054ba50b498
Chase Willden <chase.willden@gmail.com>
2026-01-27 00:35:02 -0700

    xl: Formatting Fix
    Fix the linting issues (new line and version)

--- Formula/x/xl.rb
+++ Formula/x/xl.rb
@@ -2,8 +2,8 @@ class Xl < Formula
   desc "Fast, terminal-based spreadsheet application written in Rust"
   homepage "https://rustxl.com"
   url "https://github.com/only-using-ai/rustxl/archive/refs/tags/v0.1.2.tar.gz"
-  sha256 "8eb9a86fe86affe314392129d28c06894ae697da9dc8f3138e4796473ff2ae41"
   version "0.1.2"
+  sha256 "8eb9a86fe86affe314392129d28c06894ae697da9dc8f3138e4796473ff2ae41"
   license "MIT"
 
   depends_on "rust" => :build
