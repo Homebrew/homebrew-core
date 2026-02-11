561f39ad3450e4f8ad9915e33bbd370c05f0e8fe
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-11 20:45:14 +0000

    whosthere 0.6.0

--- Formula/w/whosthere.rb
+++ Formula/w/whosthere.rb
@@ -1,8 +1,8 @@
 class Whosthere < Formula
   desc "LAN discovery tool with a modern TUI written in Go"
   homepage "https://github.com/ramonvermeulen/whosthere"
-  url "https://github.com/ramonvermeulen/whosthere/archive/refs/tags/v0.5.1.tar.gz"
-  sha256 "4cac844f7e43bd397b2d3145448aa90a0a11ffee5488e75197b3e082fea86ee1"
+  url "https://github.com/ramonvermeulen/whosthere/archive/refs/tags/v0.6.0.tar.gz"
+  sha256 "d60367a6c51a7a59b2d282e547fced3393c7a6b4918b262aa015caf1211e1c91"
   license "Apache-2.0"
   head "https://github.com/ramonvermeulen/whosthere.git", branch: "main"
 
