513dd3eb6568de22b9acff7815712d361ff50f09
Matthias Cramer <cramer@freestone.net>
2026-02-11 14:17:55 +0100

    better install variant

--- Formula/p/pcapmirror.rb
+++ Formula/p/pcapmirror.rb
@@ -2,7 +2,7 @@ class Pcapmirror < Formula
   desc "Tool for capturing network traffic on remote host using TZSP or ERSPAN"
   homepage "https://git.freestone.net/cramer/pcapmirror"
   url "https://git.freestone.net/cramer/pcapmirror/-/archive/0.6.1/pcapmirror-0.6.1.tar.gz"
-  sha256 "e8f147b6b73865d292d4aed2f9f2a376b25608b71b638146630b3397358c37cb"
+  sha256 "61eab2ebf07101fedd56af5d6d34c9aedb3d0564c8f4c5fcdca3aee966753abf"
   license "BSD-3-Clause"
 
   depends_on "make" => :build
@@ -15,7 +15,7 @@ class Pcapmirror < Formula
   def install
      bin.mkpath
      man8.mkpath
-     system "make", "install"
+     system "make", "install", "BINDIR=#{bin}", "MANDIR=#{man}"
   end
 
   test do
