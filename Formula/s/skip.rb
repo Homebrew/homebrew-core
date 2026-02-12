4df9f9b766c03228e7e1c2773fcf28e061ef0618
Marc Prud'hommeaux <mwp1@cornell.edu>
2026-02-11 23:32:19 -0500

    skip 1.7.1 (new formula)

---
+++ Formula/s/skip.rb
@@ -0,0 +1,38 @@
+class Skip < Formula
+  desc "Tool for building Swift apps for Android"
+  homepage "https://skip.dev"
+  url "https://github.com/skiptools/skipstone.git",
+      tag:      "1.7.1",
+      revision: "6651ffde3d719f8ed92d4a66451c3c9e4c6bba26"
+  license "AGPL-3.0-only"
+  head "https://github.com/skiptools/skipstone.git", branch: "main"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  depends_on xcode: ["15.3", :build]
+  depends_on "gradle"
+  depends_on "openjdk"
+  depends_on "swiftly"
+
+  uses_from_macos "swift" => :build
+
+  def install
+    args = if OS.mac?
+      ["--disable-sandbox"]
+    else
+      ["--static-swift-stdlib"]
+    end
+    system "swift", "build", *args, "-c", "release", "--product", "SkipRunner"
+    bin.install ".build/release/SkipRunner" => "skip"
+    generate_completions_from_executable(bin/"skip", "--generate-completion-script")
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/skip version")
+    system bin/"skip", "welcome"
+    system bin/"skip", "init", "--no-build", "--transpiled-app", "some-app", "SomeApp"
+  end
+end
