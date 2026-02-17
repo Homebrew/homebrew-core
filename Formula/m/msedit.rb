c17737a75ffe3f8158278727626fcbe6ab8771dc
Vlad Yaroslavlev <vladon@vladon.dev>
2026-02-18 00:12:27 +0300

    Add Linux support and update bottle SHA256s

--- Formula/m/msedit.rb
+++ Formula/m/msedit.rb
@@ -6,14 +6,18 @@ class Msedit < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "300de72b71a69bf416777351c3f771cd8f965581ca38933dab0b5f137c99115b"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe: "300de72b71a69bf416777351c3f771cd8f965581ca38933dab0b5f137c99115b"
     sha256 cellar: :any_skip_relocation, arm64_sequoia: "a609fc4363c7917cc775e1d343ca2e9f010dca3fd61d721b15ef26cb5533901e"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a33cf49751e560b1dfbcc926e3b5ff70c01891acbd11cb582dc7f3ce1cb03473"
-    sha256 cellar: :any_skip_relocation, sonoma:        "776afb0bf80f2fbf1629d631cd8eb637c0996e5ca96f5d5e738c9d4f016fa148"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "a33cf49751e560b1dfbcc926e3b5ff70c01891acbd11cb582dc7f3ce1cb03473"
+    sha256 cellar: :any_skip_relocation, sonoma: "776afb0bf80f2fbf1629d631cd8eb637c0996e5ca96f5d5e738c9d4f016fa148"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "06dd6d1e870ad203f2303d327ae201fff386aea4f5c468c384510510b317f414"
   end
 
   depends_on "rust" => :build
-  depends_on :macos # due to test failure on linux
+
+  on_linux do
+    depends_on "pkg-config" => :build
+  end
 
   def install
     ENV["RUSTC_BOOTSTRAP"] = "1"
