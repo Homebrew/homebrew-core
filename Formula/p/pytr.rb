cc9c740d944b6fb92c110cebde7ac95c26dcf606
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-28 08:32:38 +0000

    pytr 0.4.7

--- Formula/p/pytr.rb
+++ Formula/p/pytr.rb
@@ -3,8 +3,8 @@ class Pytr < Formula
 
   desc "Use TradeRepublic in terminal and mass download all documents"
   homepage "https://github.com/pytr-org/pytr"
-  url "https://files.pythonhosted.org/packages/73/ec/792938e5d254b053aff3dcd3c477e0c80fc65a978f39a641afc1073a789f/pytr-0.4.6.tar.gz"
-  sha256 "e77bf0ce3013541c5a6bb644c2616208c4fd64efd85dbe5915f4b5cfd5d11766"
+  url "https://files.pythonhosted.org/packages/04/c0/173ad027a75b3c1b6705ac56c647c78ed9ec99379e7257adfd42ebd8e30f/pytr-0.4.7.tar.gz"
+  sha256 "7b60cbd8fb0f9f623059c42b9a78e751677e1d97726ef834784ff5babd6872e4"
   license "MIT"
 
   bottle do
@@ -17,9 +17,10 @@ class Pytr < Formula
   end
 
   depends_on "certifi" => :no_linkage
+  depends_on "cryptography" => :no_linkage
   depends_on "python@3.14"
 
-  pypi_packages exclude_packages: "certifi"
+  pypi_packages exclude_packages: %w[certifi cryptography]
 
   resource "babel" do
     url "https://files.pythonhosted.org/packages/7d/b2/51899539b6ceeeb420d40ed3cd4b7a40519404f9baf3d4ac99dc413a834b/babel-2.18.0.tar.gz"
@@ -27,8 +28,8 @@ class Pytr < Formula
   end
 
   resource "charset-normalizer" do
-    url "https://files.pythonhosted.org/packages/13/69/33ddede1939fdd074bce5434295f38fae7136463422fe4fd3e0e89b98062/charset_normalizer-3.4.4.tar.gz"
-    sha256 "94537985111c35f28720e43603b8e7b43a6ecfb2ce1d3058bbe955b73404e21a"
+    url "https://files.pythonhosted.org/packages/7b/60/e3bec1881450851b087e301bedc3daa9377a4d45f1c26aa90b0b235e38aa/charset_normalizer-3.4.6.tar.gz"
+    sha256 "1ae6b62897110aa7c79ea2f5dd38d1abca6db663687c0b1ad9aed6f6bae3d9d6"
   end
 
   resource "coloredlogs" do
@@ -36,9 +37,17 @@ class Pytr < Formula
     sha256 "7c991aa71a4577af2f82600d8f8f3a89f936baeaf9b50a9c197da014e5bf16b0"
   end
 
+  resource "curl-cffi" do
+    url "https://files.pythonhosted.org/packages/9b/c9/0067d9a25ed4592b022d4558157fcdb6e123516083700786d38091688767/curl_cffi-0.14.0.tar.gz"
+    sha256 "5ffbc82e59f05008ec08ea432f0e535418823cda44178ee518906a54f27a5f0f"
+
+    # Backport of upstream fix for v0.14.0. upstream pr ref, https://github.com/lexiforest/curl_cffi/pull/697
+    patch :DATA
+  end
+
   resource "ecdsa" do
-    url "https://files.pythonhosted.org/packages/c0/1f/924e3caae75f471eae4b26bd13b698f6af2c44279f67af317439c2f4c46a/ecdsa-0.19.1.tar.gz"
-    sha256 "478cba7b62555866fcb3bb3fe985e06decbdb68ef55713c4e5ab98c57d508e61"
+    url "https://files.pythonhosted.org/packages/25/ca/8de7744cb3bc966c85430ca2d0fcaeea872507c6a4cf6e007f7fe269ed9d/ecdsa-0.19.2.tar.gz"
+    sha256 "62635b0ac1ca2e027f82122b5b81cb706edc38cd91c63dda28e4f3455a2bf930"
   end
 
   resource "humanfriendly" do
@@ -67,8 +76,8 @@ class Pytr < Formula
   end
 
   resource "requests" do
-    url "https://files.pythonhosted.org/packages/c9/74/b3ff8e6c8446842c3f5c837e9c3dfcfe2018ea6ecef224c710c85ef728f4/requests-2.32.5.tar.gz"
-    sha256 "dbba0bac56e100853db0ea71b82b4dfd5fe2bf6d3754a8893c3af500cec7d7cf"
+    url "https://files.pythonhosted.org/packages/34/64/8860370b167a9721e8956ae116825caff829224fbca0ca6e7bf8ddef8430/requests-2.33.0.tar.gz"
+    sha256 "c7ebc5e8b0f21837386ad0e1c8fe8b829fa5f544d8df3b2253bff14ef29d7652"
   end
 
   resource "requests-futures" do
@@ -111,3 +120,120 @@ class Pytr < Formula
     assert_path_exists testpath/"pytr.log"
   end
 end
+
+__END__
+diff --git a/libs.json b/libs.json
+index 5d63db1..03c334d 100644
+--- a/libs.json
++++ b/libs.json
+@@ -30,8 +30,10 @@
+         "system": "Darwin",
+         "machine": "x86_64",
+         "pointer_size": 64,
+-        "libdir": "/Users/runner/work/_temp/install/lib",
++        "libdir": "",
+         "sysname": "macos",
++        "link_type": "static",
++        "obj_name": "libcurl-impersonate.a",
+         "so_name": "libcurl-impersonate.4.dylib",
+         "so_arch": "x86_64"
+     },
+@@ -39,8 +41,10 @@
+         "system": "Darwin",
+         "machine": "arm64",
+         "pointer_size": 64,
+-        "libdir": "/Users/runner/work/_temp/install/lib",
++        "libdir": "",
+         "sysname": "macos",
++        "link_type": "static",
++        "obj_name": "libcurl-impersonate.a",
+         "so_name": "libcurl-impersonate.4.dylib",
+         "so_arch": "arm64"
+     },
+diff --git a/scripts/build.py b/scripts/build.py
+index f40a6f0..ed17fdf 100644
+--- a/scripts/build.py
++++ b/scripts/build.py
+@@ -50,10 +50,12 @@ def detect_arch():
+ 
+ arch = detect_arch()
+ print(f"Using {arch['libdir']} to store libcurl-impersonate")
++obj_name = arch.get("obj_name", arch["so_name"])
++so_arch = arch.get("arch", arch["so_arch"])
+ 
+ 
+ def download_libcurl():
+-    if (Path(arch["libdir"]) / arch["so_name"]).exists():
++    if (Path(arch["libdir"]) / obj_name).exists():
+         print(".so files already downloaded.")
+         return
+ 
+@@ -63,7 +65,7 @@ def download_libcurl():
+     url = (
+         f"https://github.com/lexiforest/curl-impersonate/releases/download/"
+         f"v{__version__}/libcurl-impersonate-v{__version__}"
+-        f".{arch['so_arch']}-{sysname}.tar.gz"
++        f".{so_arch}-{sysname}.tar.gz"
+     )
+ 
+     print(f"Downloading libcurl-impersonate from {url}...")
+@@ -86,6 +88,10 @@ def download_libcurl():
+ def get_curl_archives():
+     print("Files for linking")
+     print(os.listdir(arch["libdir"]))
++    if arch["system"] == "Darwin" and arch.get("link_type") == "static":
++        return [
++            f"{arch['libdir']}/{obj_name}",
++        ]
+     if arch["system"] == "Linux" and arch.get("link_type") == "static":
+         # note that the order of libraries matters
+         # https://stackoverflow.com/a/36581865
+@@ -130,9 +136,11 @@ def get_curl_libraries():
+             "iphlpapi",
+             "cares",
+         ]
+-    elif arch["system"] == "Darwin" or (
+-        arch["system"] == "Linux" and arch.get("link_type") == "dynamic"
+-    ):
++    elif arch["system"] == "Darwin":
++        if arch.get("link_type") == "dynamic":
++            return ["curl-impersonate"]
++        return []
++    elif arch["system"] == "Linux" and arch.get("link_type") == "dynamic":
+         return ["curl-impersonate"]
+     else:
+         return []
+@@ -142,6 +150,15 @@ ffibuilder = FFI()
+ system = platform.system()
+ root_dir = Path(__file__).parent.parent
+ download_libcurl()
++extra_objects = get_curl_archives()
++if system == "Darwin" and arch.get("link_type") == "static":
++    extra_objects = []
++    extra_link_args = [
++        f"-Wl,-force_load,{arch['libdir']}/{obj_name}",
++        "-lc++",
++    ]
++else:
++    extra_link_args = ["-lstdc++"] if system != "Windows" else []
+ 
+ 
+ ffibuilder.set_source(
+@@ -151,7 +168,7 @@ ffibuilder.set_source(
+     """,
+     # FIXME from `curl-impersonate`
+     libraries=get_curl_libraries(),
+-    extra_objects=get_curl_archives(),
++    extra_objects=extra_objects,
+     library_dirs=[arch["libdir"]],
+     source_extension=".c",
+     include_dirs=[
+@@ -165,7 +182,7 @@ ffibuilder.set_source(
+     extra_compile_args=(
+         ["-Wno-implicit-function-declaration"] if system == "Darwin" else []
+     ),
+-    extra_link_args=(["-lstdc++"] if system != "Windows" else []),
++    extra_link_args=extra_link_args,
+ )
+ 
+ with open(root_dir / "ffi/cdef.c") as f:
