9a00b49608e6d3fd1f83555b79960b1192f50fe8
eli <zinger_broiler.6a@icloud.com>
2026-03-29 22:01:29 +0200

    ols dev-2026-03 (new formula)

---
+++ Formula/o/ols.rb
@@ -0,0 +1,53 @@
+class Ols < Formula
+  desc "Language server for The Odin Programming Language"
+  homepage "https://github.com/DanielGavin/ols"
+  url "https://github.com/DanielGavin/ols/archive/refs/tags/dev-2026-03.tar.gz"
+  version "dev-2026-03"
+  sha256 "7c0d9e0312d5dc0d49e1696b98217932838e1b132feb2a68950e6fa7d6d4a2ea"
+  license "MIT"
+
+  depends_on "odin"
+
+  def install
+    system "odin", "build", "src/",
+      "-collection:src=src",
+      "-out:ols",
+      "-microarch:native",
+      "-no-bounds-check",
+      "-o:speed",
+      "-define:VERSION=#{version}"
+
+    libexec.install "ols"
+    pkgshare.install "builtin"
+    (bin/"ols").write_env_script libexec/"ols", OLS_BUILTIN_FOLDER: pkgshare/"builtin"
+  end
+
+  test do
+    json = <<~JSON
+      {
+        "jsonrpc": "2.0",
+        "id": 1,
+        "method": "initialize",
+        "params": {
+          "rootUri": null,
+          "capabilities": {}
+        }
+      }
+    JSON
+
+    input = "Content-Length: #{json.size}\r\n\r\n#{json}"
+
+    output = IO.popen(bin/"ols", "w+") do |pipe|
+      pipe.write(input)
+      pipe.close_write
+      sleep 1
+      result = pipe.read_nonblock(65536)
+      Process.kill("TERM", pipe.pid)
+      result
+    end
+
+    assert_match(/^Content-Length: \d+/i, output)
+    json_dump = output.lines.last.strip
+    assert_equal 1, JSON.parse(json_dump)["id"]
+  end
+end
