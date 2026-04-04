b4e0113a760e930f225ac3cb6b2e898ce4ab8357
Marc Prud'hommeaux <mwp1@cornell.edu>
2026-04-04 15:33:41 -0400

    skip 1.8.3 (new formula)

---
+++ Formula/s/skip.rb
@@ -0,0 +1,48 @@
+class Skip < Formula
+  desc "Tool for building Swift apps for Android"
+  homepage "https://skip.dev"
+  url "https://github.com/skiptools/skipstone.git",
+      tag:      "1.8.3",
+      revision: "11359a52166909de1f150cc3ab825f4d4f18db76"
+  license "AGPL-3.0-only"
+  head "https://github.com/skiptools/skipstone.git", branch: "main"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  depends_on xcode: ["15.3", :build]
+  depends_on "gradle"
+  depends_on "openjdk"
+  depends_on "swiftly"
+
+  uses_from_macos "swift" => :build
+  uses_from_macos "curl"
+  uses_from_macos "libxml2"
+  uses_from_macos "zlib"
+
+  def install
+    if OS.mac?
+      args = ["--disable-sandbox"]
+    else
+      libxml2_lib = Formula["libxml2"].opt_lib
+      args = [
+        "--static-swift-stdlib",
+        "-Xlinker", "-L#{Formula["curl"].opt_lib}",
+        "-Xlinker", "-L#{libxml2_lib}",
+        "-Xlinker", "-L#{HOMEBREW_PREFIX}/lib"
+      ]
+      ENV.prepend_path "LD_LIBRARY_PATH", libxml2_lib
+    end
+    system "swift", "build", *args, "-c", "release", "--product", "SkipRunner"
+    bin.install ".build/release/SkipRunner" => "skip"
+    generate_completions_from_executable(bin/"skip", "--generate-completion-script")
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/skip version")
+    system bin/"skip", "welcome"
+    system bin/"skip", "init", "--no-build", "--transpiled-app", "--appid", "some.app.id", "some-app", "SomeApp"
+  end
+end
