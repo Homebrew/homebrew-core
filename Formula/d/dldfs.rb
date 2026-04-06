672f87dcefecdb7c4bc569bbd44d247978166718
Mark <markgami07310@gmail.com>
2026-04-06 10:27:27 +0530

    Fix test to match actual help output

--- Formula/d/dldfs.rb
+++ Formula/d/dldfs.rb
@@ -13,6 +13,7 @@ class Dldfs < Formula
 
   test do
     output = shell_output("#{bin}/dldfs --help")
-    assert_match "multi-connection", output
+    assert_match "Multiple concurrent connections", output
+    assert_match "Usage:", output
   end
 end
