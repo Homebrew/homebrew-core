b0d3ddd5af8b3f14bf52a4355a105efe0ff7ccff
Mark <markgami07310@gmail.com>
2026-04-06 10:09:03 +0530

    Fix module path and update SHA256

--- Formula/d/dldfs.rb
+++ Formula/d/dldfs.rb
@@ -2,9 +2,11 @@ class Dldfs < Formula
   desc "Blazing-fast multi-connection CLI downloader with IDM-level performance"
   homepage "https://github.com/Mark-0731/homebrew-dldfs"
   url "https://github.com/Mark-0731/homebrew-dldfs/archive/refs/tags/v1.0.2.tar.gz"
-  sha256 "1172f591f367ea582b79cd63f69536b5cca7d017befd3934b4e55166cd382b3b"
+  sha256 "28cf739813494bce85c7b450d86a45ef70babf05f8ccd01027ab3949ed431702"
   license "MIT"
 
+  # Bottles will be built by Homebrew CI after merge
+  # Users won't need Go installed when using bottles
   depends_on "go" => :build
 
   def install
