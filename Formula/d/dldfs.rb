a2a2670106c07512ec2d6d7f19f1a5f07a3fe620
Mark <markgami07310@gmail.com>
2026-04-06 10:18:17 +0530

    Simplify description and test per Homebrew guidelines

--- Formula/d/dldfs.rb
+++ Formula/d/dldfs.rb
@@ -1,12 +1,10 @@
 class Dldfs < Formula
-  desc "Blazing-fast multi-connection CLI downloader with IDM-level performance"
+  desc "Fast multi-connection CLI download manager"
   homepage "https://github.com/Mark-0731/homebrew-dldfs"
   url "https://github.com/Mark-0731/homebrew-dldfs/archive/refs/tags/v1.0.2.tar.gz"
   sha256 "28cf739813494bce85c7b450d86a45ef70babf05f8ccd01027ab3949ed431702"
   license "MIT"
 
-  # Bottles will be built by Homebrew CI after merge
-  # Users won't need Go installed when using bottles
   depends_on "go" => :build
 
   def install
@@ -14,6 +12,7 @@ class Dldfs < Formula
   end
 
   test do
-    assert_match "dldfs", shell_output("#{bin}/dldfs --help")
+    output = shell_output("#{bin}/dldfs --help")
+    assert_match "multi-connection", output
   end
 end
