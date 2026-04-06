953aa8269d5f86aabc3315309765d5e1e407b668
Mark <markgami07310@gmail.com>
2026-04-06 10:37:57 +0530

    Update to use proper project repo (not tap repo)

--- Formula/d/dldfs.rb
+++ Formula/d/dldfs.rb
@@ -1,8 +1,8 @@
 class Dldfs < Formula
   desc "Fast multi-connection CLI download manager"
-  homepage "https://github.com/Mark-0731/homebrew-dldfs"
-  url "https://github.com/Mark-0731/homebrew-dldfs/archive/refs/tags/v1.0.2.tar.gz"
-  sha256 "28cf739813494bce85c7b450d86a45ef70babf05f8ccd01027ab3949ed431702"
+  homepage "https://github.com/Mark-0731/dldfs"
+  url "https://github.com/Mark-0731/dldfs/archive/refs/tags/v1.0.2.tar.gz"
+  sha256 "1a9ce713acab79c89b48e974689d3da2499dedfb8c4abae0a3c0c5132ce29733"
   license "MIT"
 
   depends_on "go" => :build
@@ -14,6 +14,5 @@ class Dldfs < Formula
   test do
     output = shell_output("#{bin}/dldfs --help")
     assert_match "Multiple concurrent connections", output
-    assert_match "Usage:", output
   end
 end
