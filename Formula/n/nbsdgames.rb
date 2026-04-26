24b0d2369cab99cb4bcfae42307bb47c034b5e36
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-26 17:32:40 +0000

    nbsdgames 6

--- Formula/n/nbsdgames.rb
+++ Formula/n/nbsdgames.rb
@@ -1,8 +1,8 @@
 class Nbsdgames < Formula
   desc "Text-based modern games"
   homepage "https://github.com/abakh/nbsdgames"
-  url "https://github.com/abakh/nbsdgames/archive/refs/tags/v5.tar.gz"
-  sha256 "ca81d8b854a7bf9685bbc58aabc1a24cd617cadb7e9ddac64a513d2c8ddb2e6c"
+  url "https://github.com/abakh/nbsdgames/archive/refs/tags/v6.tar.gz"
+  sha256 "be261ad068ade07aeb6c75597fd03c6e3dc865fe4d9851fd95059bb6b929dcf4"
   license :public_domain
   head "https://github.com/abakh/nbsdgames.git", branch: "master"
 
