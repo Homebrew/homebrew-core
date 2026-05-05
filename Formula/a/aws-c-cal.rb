3c2d8e60632b33d6d9dc7962f43b28e8d4abfd71
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-05 11:04:11 +0000

    aws-c-cal: update 0.9.13_1 bottle.

--- Formula/a/aws-c-cal.rb
+++ Formula/a/aws-c-cal.rb
@@ -8,12 +8,12 @@ class AwsCCal < Formula
   compatibility_version 1
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "b4a682ef0ffb80c87cd6cfa4023c66c7ab159deb3f7500d7601014cbe58fb8c0"
-    sha256 cellar: :any,                 arm64_sequoia: "4bce7425760b6ba9e4ab56d8c008a5a885b0d922d978196b16da692b624a8616"
-    sha256 cellar: :any,                 arm64_sonoma:  "e3a1ae1b314af560c06ad0d948c67259ebe7b79afe88a14af9caed5ce1ffaa61"
-    sha256 cellar: :any,                 sonoma:        "4ffd05da94b7ff7f0c8a3479b101a9a964955c111a7c4f4e59653e210de8b38c"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "1332c1328220c4357acdbef60ee792897c7c1e17d3f6ba7892d4406997aaa3ce"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0e801e0e9a5800ce24c4a726e42be9c7a24cf75c8de42d1c70073346a9627a09"
+    sha256 cellar: :any,                 arm64_tahoe:   "e73a86fc73088cee318241acc880741b52a69153e26c774eedfc7115d9a4bc6d"
+    sha256 cellar: :any,                 arm64_sequoia: "3645bc70d5ca122cced6a8f06b8c9def09e32fd30ea9a41ef23c18f2cf9352b1"
+    sha256 cellar: :any,                 arm64_sonoma:  "d864f062b554bc3757358f7b1f062d9bbaff5b803db4e203342424326592cada"
+    sha256 cellar: :any,                 sonoma:        "732928a4952d507a5e2a29b27f485b04d755322efed201b052e6047c5cf5a5dd"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "c7c172608390a7a130d79735abc8c9ccc662f0e12dd5e6fc59a1e8b1960a7cbe"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "fde6efbcb4e6ce3d523eab32425e3e5a66190dd5657fe6eb1320d880a9ef10c4"
   end
 
   depends_on "cmake" => :build
