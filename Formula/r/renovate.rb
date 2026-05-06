a6d811a06e8826d69a9b9fc524fef1a0fefa1afe
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-06 09:40:40 +0000

    renovate 43.166.0

--- Formula/r/renovate.rb
+++ Formula/r/renovate.rb
@@ -2,8 +2,8 @@ class Renovate < Formula
   desc "Automated dependency updates. Flexible so you don't need to be"
   homepage "https://github.com/renovatebot/renovate"
   # TODO: Switch to npm registry URL when https://github.com/renovatebot/renovate/discussions/42965 is fixed
-  url "https://github.com/renovatebot/renovate/archive/refs/tags/43.165.0.tar.gz"
-  sha256 "b8a3731d14cd5c268b97ff0975c151f58de7f2a7035f7a5439163b43ff198afb"
+  url "https://github.com/renovatebot/renovate/archive/refs/tags/43.166.0.tar.gz"
+  sha256 "9cc14aa93063dbe06a1ba3bd49cd9cbe0211c1ea6cdfdff8de049f17e7cee425"
   license "AGPL-3.0-only"
 
   # livecheck needs to surface multiple versions for version throttling but
