2b74078f4e3af64eb660f7d585dbe066e542bac4
Daeho Ro <40587651+daeho-ro@users.noreply.github.com>
2026-05-07 09:36:24 +0900

    libzip: migrate to `openssl@4`

--- Formula/lib/libzip.rb
+++ Formula/lib/libzip.rb
@@ -4,7 +4,7 @@ class Libzip < Formula
   url "https://libzip.org/download/libzip-1.11.4.tar.xz"
   sha256 "8a247f57d1e3e6f6d11413b12a6f28a9d388de110adc0ec608d893180ed7097b"
   license "BSD-3-Clause"
-  revision 1
+  revision 2
   compatibility_version 1
 
   livecheck do
@@ -29,7 +29,7 @@ class Libzip < Formula
   uses_from_macos "bzip2"
 
   on_linux do
-    depends_on "openssl@3"
+    depends_on "openssl@4"
     depends_on "zlib-ng-compat"
   end
 
