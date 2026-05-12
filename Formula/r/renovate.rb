d68121e7ce6012109e0c605b068a96aa0a1ba396
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-12 12:14:22 +0000

    renovate 43.175.0

--- Formula/r/renovate.rb
+++ Formula/r/renovate.rb
@@ -2,8 +2,8 @@ class Renovate < Formula
   desc "Automated dependency updates. Flexible so you don't need to be"
   homepage "https://github.com/renovatebot/renovate"
   # TODO: Switch to npm registry URL when https://github.com/renovatebot/renovate/discussions/42965 is fixed
-  url "https://github.com/renovatebot/renovate/archive/refs/tags/43.173.0.tar.gz"
-  sha256 "ea118c89b54d5d837455739839aa373d997a48101086b8c2e0b46364fcd4904f"
+  url "https://github.com/renovatebot/renovate/archive/refs/tags/43.175.0.tar.gz"
+  sha256 "8be9c31fc4c3cc1144d60106a8d5b3d62f8e4b2934dbf9724e52ce8625f49463"
   license "AGPL-3.0-only"
 
   # livecheck needs to surface multiple versions for version throttling but
@@ -27,6 +27,11 @@ class Renovate < Formula
   uses_from_macos "git", since: :monterey
 
   def install
+    # Pin Ecosystem union member order; z.infer emits it non-deterministically.
+    inreplace "lib/modules/platform/github/schema.ts",
+              "export type Ecosystem = z.infer<typeof Ecosystem>;",
+              "export type Ecosystem = (typeof Ecosystem.options)[number];"
+
     # TODO: switch back to `system "npm", "install", *std_npm_args` when using npm registry URL
     system "npm", "install", *std_npm_args(prefix: false)
     system "npm", "run", "build"
