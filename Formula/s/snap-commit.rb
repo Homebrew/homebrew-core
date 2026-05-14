dc29a7e126350736938497452a26d03ee08b60c3
nxkh4ng <kh4ngnx@gmail.com>
2026-05-14 03:28:18 +0000

    snap-commit 0.4.0 (new formula)

---
+++ Formula/s/snap-commit.rb
@@ -0,0 +1,20 @@
+class SnapCommit < Formula
+  desc "A lightweight CLI tool that helps you create consistent Git commits following the Conventional Commits standard."
+  homepage "https://github.com/nxkh4ng/snap-commit"
+  url "https://github.com/nxkh4ng/snap-commit/archive/refs/tags/v0.4.0.tar.gz"
+  sha256 "d29fad7a42cfd471498500c1b82dbea75c73867a6719365dd3e79bf50b8df56a"
+  license "MIT"
+
+  depends_on "go" => :build
+
+  def install
+    # Binary named "snap" for shorter CLI; formula named "snap-commit" to avoid
+    # namespace conflict with Canonical's snap package
+    ldflags = "-s -w -X github.com/nxkh4ng/snap/cmd.version=#{version}"
+    system "go", "build", "-o", bin/"snap", "-ldflags=#{ldflags}"
+  end
+
+  test do
+    assert_match "snap version #{version}", shell_output("#{bin}/snap --version")
+  end
+end
