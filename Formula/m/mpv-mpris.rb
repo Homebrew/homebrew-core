8f9da4c1524ac6ce6d96df540b4eac02bed438c3
kralupus <tegridy_farmer@disroot.org>
2026-06-05 13:35:41 +0200

    Remove trailing spaces

--- Formula/m/mpv-mpris.rb
+++ Formula/m/mpv-mpris.rb
@@ -1,10 +1,10 @@
 class MpvMpris < Formula
-  desc "MPRIS plugin for MPV "
+  desc "MPRIS plugin for MPV"
   homepage "https://github.com/hoyon/mpv-mpris"
   url "https://github.com/hoyon/mpv-mpris/archive/refs/tags/1.2.tar.gz"
   sha256 "ecdc66f0182a38164b8bdc79502c575df3d2c4453bae5bff225c4e5ce9dbef6c"
   license "MIT"
- 
+
   livecheck do
     url :stable
     strategy :github_latest
