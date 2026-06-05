10136a374daa09d1fd9a8306ba022d17e3c4b64a
Chirag Aggarwal <chiragaggarwal5k@gmail.com>
2026-06-05 15:54:07 +0530

    appwrite 22.0.0

--- Formula/a/appwrite.rb
+++ Formula/a/appwrite.rb
@@ -1,9 +1,15 @@
 class Appwrite < Formula
   desc "Command-line tool for Appwrite"
   homepage "https://appwrite.io"
-  url "https://registry.npmjs.org/appwrite-cli/-/appwrite-cli-21.0.1.tgz"
-  sha256 "af79417d56ecdcc9bad6d7a805c2a967e013d04f38a3a95681b95994135e2d16"
+  url "https://github.com/appwrite/sdk-for-cli/archive/refs/tags/22.0.0.tar.gz"
+  sha256 "aebf5766048c292aa04d72cdde40dc7a9afdf45409f65a62ecca2837795254ac"
   license "BSD-3-Clause"
+  head "https://github.com/appwrite/sdk-for-cli.git", branch: "master"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_tahoe:   "51f537279f972c970a46b2003d254c03a055d768c8459e1cc82753525e85c04c"
@@ -14,21 +20,45 @@ class Appwrite < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "a787b01e9dfe15c0f04b8602a000de3588bc135817203d7dd2846c3b33a149f0"
   end
 
-  depends_on "node"
+  depends_on "bun" => :build
+
+  on_linux do
+    on_arm do
+      depends_on "patchelf" => :build
+    end
+
+    depends_on "icu4c@78"
+  end
 
   def install
-    system "npm", "install", *std_npm_args
-    bin.install_symlink libexec.glob("bin/*")
+    system "bun", "install", "--frozen-lockfile"
 
-    node_modules = libexec/"lib/node_modules/appwrite-cli/node_modules"
-    machos = %w[fsevents/fsevents.node app-path/main]
-    machos.each { |macho| deuniversalize_machos node_modules/macho } if OS.mac?
+    if OS.linux? && Hardware::CPU.arm?
+      system "bun", "build", "cli.ts", "--compile", "--target=bun-linux-arm64", "--outfile", "appwrite"
+
+      libexec.install "appwrite"
+      mkdir_p libexec/"lib"
+
+      icu = Formula["icu4c@78"].opt_lib
+      (libexec/"lib").install_symlink icu/"libicui18n.so.78"
+      (libexec/"lib").install_symlink icu/"libicuuc.so.78"
+      (libexec/"lib").install_symlink icu/"libicudata.so.78"
+
+      system "patchelf", "--set-interpreter", "/lib/ld-linux-aarch64.so.1",
+                         "--set-rpath", "$ORIGIN/lib", libexec/"appwrite"
+
+      bin.install_symlink libexec/"appwrite"
+    else
+      system "bun", "build", "cli.ts", "--compile", "--outfile", "appwrite"
+      bin.install "appwrite"
+    end
+
+    generate_completions_from_executable(bin/"appwrite", "completion")
   end
 
   test do
-    output = shell_output("#{bin}/appwrite client --endpoint http://localhost/v1 2>&1", 1)
-    assert_match "Error: Invalid endpoint", output
-
     assert_match version.to_s, shell_output("#{bin}/appwrite --version")
+    assert_match "#compdef appwrite", shell_output("#{bin}/appwrite completion zsh")
+    assert_match "_appwrite_completion", shell_output("#{bin}/appwrite completion bash")
   end
 end
