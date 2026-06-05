f36690ede2615cec9dc53b985c8f12c16c8ef81a
kralupus <tegridy_farmer@disroot.org>
2026-06-05 13:26:34 +0200

    Change description and move livecheck() up

--- Formula/m/mpv-mpris.rb
+++ Formula/m/mpv-mpris.rb
@@ -1,18 +1,18 @@
 class MpvMpris < Formula
-  desc "MPRIS plugin for MPV media player"
+  desc "MPRIS plugin for MPV "
   homepage "https://github.com/hoyon/mpv-mpris"
   url "https://github.com/hoyon/mpv-mpris/archive/refs/tags/1.2.tar.gz"
   sha256 "ecdc66f0182a38164b8bdc79502c575df3d2c4453bae5bff225c4e5ce9dbef6c"
   license "MIT"
-
-  depends_on "mpv" => :build
-  depends_on "pkg-config" => :build
-  depends_on "glib"
-
+ 
   livecheck do
     url :stable
     strategy :github_latest
   end
+
+  depends_on "pkg-config" => :build
+  depends_on "glib"
+  depends_on "mpv"
 
   def install
     system "make"
@@ -21,7 +21,7 @@ class MpvMpris < Formula
   end
 
   test do
-    assert_predicate lib/"mpv-mpris.so", :exist?
+    assert_path_exists lib/"mpv-mpris.so", :exist?
     assert_match "shared object", shell_output("file #{lib}/mpv-mpris.so")
   end
 end
