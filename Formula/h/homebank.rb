f69a16061cc4eb03da6980a0123d8ab26c6ed2d2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-08 09:34:01 +0000

    homebank 5.10.1

--- Formula/h/homebank.rb
+++ Formula/h/homebank.rb
@@ -1,8 +1,8 @@
 class Homebank < Formula
   desc "Manage your personal accounts at home"
   homepage "https://www.gethomebank.org/en/index.php"
-  url "https://www.gethomebank.org/public/sources/homebank-5.10.0.tar.gz"
-  sha256 "783fd8bb2ebba09713e5d7c183d454f4a4393e828af5763f768b48afabc54386"
+  url "https://www.gethomebank.org/public/sources/homebank-5.10.1.tar.gz"
+  sha256 "67512d3188ea45f92a6f9326e829c142af5ad509306702f3b0457a1ab611d42a"
   license "GPL-2.0-or-later"
 
   livecheck do
