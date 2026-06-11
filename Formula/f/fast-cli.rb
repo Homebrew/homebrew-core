7c26205e9da31fd995e55cb34d6bd15ba90210e2
Rui Chen <rui@chenrui.dev>
2026-06-10 22:56:43 -0400

    fast-cli 5.2.0 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/f/fast-cli.rb
@@ -0,0 +1,27 @@
+class FastCli < Formula
+  desc "Test your download and upload speed using fast.com"
+  homepage "https://github.com/sindresorhus/fast-cli"
+  url "https://registry.npmjs.org/fast-cli/-/fast-cli-5.2.0.tgz"
+  sha256 "05e8cd8259e60631c280efb8e0d8c985aef402c76e8953f234bc4c3028b8fed5"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink libexec.glob("bin/*")
+
+    node_modules = libexec/"lib/node_modules/fast-cli/node_modules"
+
+    # Remove incompatible pre-built `bare-fs`/`bare-os`/`bare-url` binaries
+    os = OS.kernel_name.downcase
+    arch = Hardware::CPU.intel? ? "x64" : Hardware::CPU.arch.to_s
+    node_modules.glob("{bare-fs,bare-os,bare-url}/prebuilds/*")
+                .each { |dir| rm_r(dir) if dir.basename.to_s != "#{os}-#{arch}" }
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/fast --version")
+    assert_match "Could not find Chrome", shell_output("#{bin}/fast --upload 2>&1", 1)
+  end
+end
