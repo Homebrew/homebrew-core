b280672335450c4cbd079e1f4d0bb67ed734b4d6
botantony <antonsm21@gmail.com>
2026-06-26 17:33:18 +0200

    dynaconf 3.3.0
    https://github.com/Homebrew/homebrew-core/actions/runs/28235676243/job/83649864052#step:6:7160
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Formula/d/dynaconf.rb
+++ Formula/d/dynaconf.rb
@@ -3,15 +3,16 @@ class Dynaconf < Formula
 
   desc "Configuration Management for Python"
   homepage "https://www.dynaconf.com/"
-  url "https://files.pythonhosted.org/packages/57/0e/05927cf459e73f8bf9a9277cbea6f2d5b7db8a5cc9dc1e20e7a5fbac1b90/dynaconf-3.2.13.tar.gz"
-  sha256 "d79e0189d97b3f226b8ebb1717e2ce05d1a05cdf6ea05de66d24625fdb5a0cbd"
+  url "https://files.pythonhosted.org/packages/5f/c8/6bf1aa9e219485189f92bf68def0ef46c7292ecf0399757c17754988fb4b/dynaconf-3.3.0.tar.gz"
+  sha256 "923b8a497eda5457d88ec2f30389a564f7f5faaeb83399b027f0a6c50cf4516f"
   license "MIT"
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "a9a33bd12af8a78eac4e1e74fb83ff32a7c28482c4c1dc17cf4db9e5f8fa10fb"
   end
 
-  depends_on "python@3.14"
+  # TODO: switch to 3.14 when this PR is merged: https://github.com/dynaconf/dynaconf/pull/1398
+  depends_on "python@3.13"
 
   def install
     virtualenv_install_with_resources
