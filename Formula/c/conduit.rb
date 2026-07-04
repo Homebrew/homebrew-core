9f2b79157686038c7cec6594971b32748a4d02c0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-04 20:18:14 +0000

    conduit 0.15.0

--- Formula/c/conduit.rb
+++ Formula/c/conduit.rb
@@ -1,8 +1,8 @@
 class Conduit < Formula
   desc "Streams data between data stores. Kafka Connect replacement. No JVM required"
   homepage "https://conduitio.github.io/"
-  url "https://github.com/ConduitIO/conduit/archive/refs/tags/v0.14.0.tar.gz"
-  sha256 "4747b7a8b1cce5a32490ff3bb9c31e0d0cb4a97da75e2d4f9f97a33af9a8c4f1"
+  url "https://github.com/ConduitIO/conduit/archive/refs/tags/v0.15.0.tar.gz"
+  sha256 "c5cfe0b5c5ca56a0afa41be7d2fe433d229100a78c8d88c61e47af97796270ad"
   license "Apache-2.0"
   head "https://github.com/ConduitIO/conduit.git", branch: "main"
 
