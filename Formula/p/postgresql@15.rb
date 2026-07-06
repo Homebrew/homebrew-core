65a0bcba8f2ed3c6ef6b5e3d0dd3a43d5f91a3e1
Carlo Cabrera <github@carlo.cab>
2026-07-06 22:50:42 +0800

    postgresql@15: link sharedir before initdb
    Backport of #291532.

--- Formula/p/postgresql@15.rb
+++ Formula/p/postgresql@15.rb
@@ -117,6 +117,7 @@ class PostgresqlAT15 < Formula
 
   post_install_steps do
     mkdir_p "log"
+    ln_sf "share/postgresql", "share/postgresql@15", source_base: :prefix, target_base: :homebrew_prefix
     # Don't initialize database, it clashes when testing other PostgreSQL versions.
     init_data_dir "postgresql@15", using: :postgresql_initdb
   end
