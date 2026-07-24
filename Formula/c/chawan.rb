63ddc1280f2730519e8e9a0b2839d66d1283c577
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-24 17:46:47 +0000

    chawan: update 0.4.4 bottle.

--- Formula/c/chawan.rb
+++ Formula/c/chawan.rb
@@ -7,12 +7,12 @@ class Chawan < Formula
   head "https://git.sr.ht/~bptato/chawan", branch: "master"
 
   bottle do
-    sha256 cellar: :any, arm64_tahoe:   "bf9417370d67d2be7f92fd31a8d1b112486099144133cefe60a3db9b7a76ce2a"
-    sha256 cellar: :any, arm64_sequoia: "742e196f59b8ec83acd172c57d932b096dc5a1877eb95810b990cbd3103e460e"
-    sha256 cellar: :any, arm64_sonoma:  "3dd82f03461dbb7554b75b4b0888bc2e96edf38d8f198f9e730e9d872b8f8224"
-    sha256 cellar: :any, sonoma:        "246f6d5e965482e2cd3e26c9fb929068cb43bccc9bfe39393dc25782f51e58db"
-    sha256 cellar: :any, arm64_linux:   "ca8e035e0f57e849c9e3d281f3504962dadec1ca38e08a5f31efd98e6ac9c0b2"
-    sha256 cellar: :any, x86_64_linux:  "6ef2d60dd74a5986d969fcca0017b66ff790b123e9a32fa3a8fa0fe3cc98b321"
+    sha256 cellar: :any, arm64_tahoe:   "dfef144aa6e632941c194fcef6041ec76982db39fdc2397278e8a60475fbe66b"
+    sha256 cellar: :any, arm64_sequoia: "51f9388c7e07708388d48b0a823081661c6a5678f49a6051d0d0f6bf024130fc"
+    sha256 cellar: :any, arm64_sonoma:  "fe3b49e760e4b0b82b4473a848554a73839f884322cc60de0ddf0b8703ea1c16"
+    sha256 cellar: :any, sonoma:        "0dc3332b90d0f8811016f213af86af542b6c22d8944757de29faa45b85123dbb"
+    sha256 cellar: :any, arm64_linux:   "40a5438b93cf240a5c751d969618195746762349c05bb7d2445e4ba27e2c37d2"
+    sha256 cellar: :any, x86_64_linux:  "de0c0fe6e6459f251daded1c355c37d1133bdf866f9f7f656f77ee6489b73d36"
   end
 
   depends_on "nim" => :build
