f2d0c7b44eff7bb92372da66cbecc2e6e51341de
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-24 18:00:32 +0000

    just-lsp: update 0.5.0 bottle.

--- Formula/j/just-lsp.rb
+++ Formula/j/just-lsp.rb
@@ -7,12 +7,12 @@ class JustLsp < Formula
   head "https://github.com/terror/just-lsp.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "1c2b9dc154af3535cf7671460f06860976b334648015e531e5b90e6c26dfb54a"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "3a66cc74305583a01d18f534f6fd5630b1a4d7305555f468fa0e7a2aedfe96b7"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "83da24b820f08c14eae0c9b07769efcb78dbc27f096c52140a162479ca162f99"
-    sha256 cellar: :any_skip_relocation, sonoma:        "8f4080122497f38d2fb79c325444fbecb781aa1149aa471fbe7878ace0413d13"
-    sha256 cellar: :any,                 arm64_linux:   "9bcdb201f27b58e067c292bc6bec27a9ebb19711580a8794e1381d35e0026ecc"
-    sha256 cellar: :any,                 x86_64_linux:  "1ba975c213dfe7ab1b42d52847cce947aa5f502949b681ee0ac19eb8305b3c08"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "143fac27a3b2347be9fb9d5d41f650c481c9b4edfc50258589e352ee5591ceee"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "8de14740e97d64dba248f5246297b854d6d27cdbb42c1eeab6916d681206bfad"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "74a10cc9851ce902cbccf1cfa5bfe1f4b6b2d67e3c5ff7e21e2c1ca75eabad7f"
+    sha256 cellar: :any_skip_relocation, sonoma:        "a6fa86a378cfeb024ff163fd31fadc2f82f1c6de6e5e782ff2dc19e8b91758d0"
+    sha256 cellar: :any,                 arm64_linux:   "6265eb256e7638233e8f3b80d6fcc496b9491e1ecdea2c803adbfa801ae83468"
+    sha256 cellar: :any,                 x86_64_linux:  "241ede8da3bcd23dfcbb2e95c5197eec84d479a54b666a92cf018e594d9bfcd8"
   end
 
   depends_on "rust" => :build
