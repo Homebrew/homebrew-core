b4ed1195c2f03094dc4b7dd5b79894499fe97dab
yangzizhoudiwuxuande <yangzizhou2026@outlook.com>
2026-08-08 18:19:14 +0800

    Update zarkdown URL to use releases download link

--- Formula/zarkdown.rb
+++ Formula/zarkdown.rb
@@ -3,7 +3,7 @@ class Zarkdown < Formula
 
   desc "Zarkdown - 键盘友好型纯文本标记语言"
   homepage "https://github.com/yangzizhoudiwuxuande/zarkdown"
-  url "https://github.com/yangzizhoudiwuxuande/zarkdown/archive/refs/tags/v2.0.1.tar.gz"
+  url "https://github.com/yangzizhoudiwuxuande/zarkdown/releases/download/v2.0.1/zarkdown-2.0.1.tar.gz"
   sha256 "a4ae11dcbf3b89ad273ac2755ff4b1d0937c80a23a7de71ad33f27fec14bcef0"
   license "MIT"
 
