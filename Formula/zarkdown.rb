75caa950c22b1d94c70ee952aba1a34ee93132c1
yangzizhoudiwuxuande <yangzizhou2026@outlook.com>
2026-08-08 12:25:10 +0800

    Add Zarkdown formula for Homebrew

---
+++ Formula/zarkdown.rb
@@ -0,0 +1,19 @@
+class Zarkdown < Formula
+  include Language::Python::Virtualenv
+
+  desc "Zarkdown - 键盘友好型纯文本标记语言"
+  homepage "https://github.com/yangzizhoudiwuxuande/zarkdown"
+  url "https://github.com/yangzizhoudiwuxuande/zarkdown/archive/refs/tags/v2.0.1.tar.gz"
+  sha256 "a4ae11dcbf3b89ad273ac2755ff4b1d0937c80a23a7de71ad33f27fec14bcef0"
+  license "MIT"
+
+  depends_on "python@3.14"
+
+  def install
+    virtualenv_install_with_resources
+  end
+
+  test do
+    system "#{bin}/zarkdown", "--version"
+  end
+end
