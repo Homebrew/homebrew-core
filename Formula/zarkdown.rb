f79ccba4fd18d10ba414199b7116224ed5586dc8
yangzizhoudiwuxuande <yangzizhou2026@outlook.com>
2026-08-09 11:45:16 +0800

    Update zarkdown.rb

--- Formula/zarkdown.rb
+++ Formula/zarkdown.rb
@@ -4,7 +4,7 @@ class Zarkdown < Formula
   desc "Zarkdown - 键盘友好型纯文本标记语言"
   homepage "https://github.com/yangzizhoudiwuxuande/zarkdown"
   url "https://github.com/yangzizhoudiwuxuande/zarkdown/releases/download/v2.0.1/zarkdown-2.0.1.tar.gz"
-  sha256 "a4ae11dcbf3b89ad273ac2755ff4b1d0937c80a23a7de71ad33f27fec14bcef0"
+  sha256 "963d118f63883567eedb150d6207884fffe315f94e3abb420500a7bfc5a7f4e4"
   license "MIT"
 
   depends_on "python@3.14"
