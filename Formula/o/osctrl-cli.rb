456106eaded93c17f45982ca543751cc4018ae05
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-24 09:00:47 +0000

    osctrl-cli 0.5.7

--- Formula/o/osctrl-cli.rb
+++ Formula/o/osctrl-cli.rb
@@ -1,8 +1,8 @@
 class OsctrlCli < Formula
   desc "Fast and efficient osquery management"
   homepage "https://osctrl.net"
-  url "https://github.com/jmpsec/osctrl/archive/refs/tags/v0.5.6.tar.gz"
-  sha256 "2c0fb1f771495fbba1a21c41de4c6a26a6be010b0fa04554ca6e757b8d70df36"
+  url "https://github.com/jmpsec/osctrl/archive/refs/tags/v0.5.7.tar.gz"
+  sha256 "7da3eb1d5cd95a9aa4e0e1b4fc8fb832280eac94c185814a32ebe6143fc9deec"
   license "MIT"
   head "https://github.com/jmpsec/osctrl.git", branch: "develop"
 
