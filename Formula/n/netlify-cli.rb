a0eab3c06f7e46b03c532741a24598a31245b2a3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-27 12:18:29 +0000

    netlify-cli 27.3.1

--- Formula/n/netlify-cli.rb
+++ Formula/n/netlify-cli.rb
@@ -1,8 +1,8 @@
 class NetlifyCli < Formula
   desc "Netlify command-line tool"
   homepage "https://www.netlify.com/docs/cli"
-  url "https://registry.npmjs.org/netlify-cli/-/netlify-cli-27.3.0.tgz"
-  sha256 "252b9af41c6646ec7a6e730aa95a35521d2feadc4d6660c2685bf7b9f4f9a6f4"
+  url "https://registry.npmjs.org/netlify-cli/-/netlify-cli-27.3.1.tgz"
+  sha256 "a695f7d58112e37eae4c40034d24458e9d2fefeff76c5b0e32737d2d349c774e"
   license "MIT"
 
   bottle do
@@ -36,8 +36,8 @@ class NetlifyCli < Formula
   end
 
   resource "node-gyp" do
-    url "https://registry.npmjs.org/node-gyp/-/node-gyp-13.0.1.tgz"
-    sha256 "455327cde805c299d5a16603419e106853db5b9257dfb85e44eb7f4ec4d99de5"
+    url "https://registry.npmjs.org/node-gyp/-/node-gyp-13.0.2.tgz"
+    sha256 "1b1524d914331bd01312729e31a828192d53af84e113dacb6e36afabb6c21a6d"
   end
 
   def install
