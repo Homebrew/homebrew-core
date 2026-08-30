9869f93bfbcb9166d6cbb2bb1a999cb3aa9cce01
Vijay-macbook-pro <oponite@gmail.com>
2026-08-30 18:30:38 -0400

    lana 1.1.1

---
+++ Formula/l/lana.rb
@@ -0,0 +1,21 @@
+class Lana < Formula
+  desc "Verified register-based language with explicit uncertainty"
+  homepage "https://github.com/oponite/lana"
+  url "https://github.com/oponite/lana/releases/download/v1.1.1/lana-1.1.1-source.tar.gz"
+  sha256 "680a67b46378aa145175bb480cd4ddc4af10837454b41b8135cbf446e8a916f3"
+  license "Apache-2.0"
+
+  depends_on "cmake" => :build
+
+  def install
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    system "cmake", "--build", "build", "--parallel"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    assert_match "Lana #{version} (LABC v1,", shell_output("#{bin}/lana version")
+    system bin/"lana", "new", "hello-lana"
+    system bin/"lana", "run", "hello-lana"
+  end
+end
