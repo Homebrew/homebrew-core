d337dd23af5dee924e31b79b29a37ef2376f573d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-09 02:11:04 +0000

    edencommon 2026.09.07.00

--- Formula/e/edencommon.rb
+++ Formula/e/edencommon.rb
@@ -1,10 +1,9 @@
 class Edencommon < Formula
   desc "Shared library for Watchman and Eden projects"
   homepage "https://github.com/facebookexperimental/edencommon"
-  url "https://github.com/facebookexperimental/edencommon/archive/refs/tags/v2026.07.27.00.tar.gz"
-  sha256 "7be895a8d9a6eb88523488142503ce9b14d097b518967dd50b9c4cf7675c8080"
+  url "https://github.com/facebookexperimental/edencommon/archive/refs/tags/v2026.09.07.00.tar.gz"
+  sha256 "5179fa12f1836d60eca4b9fd536d7126a50618b489483f58b47e065610781b19"
   license "MIT"
-  revision 1
   compatibility_version 1
   head "https://github.com/facebookexperimental/edencommon.git", branch: "main"
 
