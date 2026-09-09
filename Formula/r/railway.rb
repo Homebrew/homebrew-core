044219b989b3c10f6dbf670bf78f30ede47bca77
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-09 02:43:10 +0000

    railway 5.50.0

--- Formula/r/railway.rb
+++ Formula/r/railway.rb
@@ -1,8 +1,8 @@
 class Railway < Formula
   desc "Develop and deploy code with zero configuration"
   homepage "https://railway.com/"
-  url "https://github.com/railwayapp/cli/archive/refs/tags/v5.49.6.tar.gz"
-  sha256 "3b980270c5d48d86c855bf0b02938a0675e7deb36aa5f5631253581a22531c74"
+  url "https://github.com/railwayapp/cli/archive/refs/tags/v5.50.0.tar.gz"
+  sha256 "0ebd315b9f72ceb825f244742d5b5fa4abaa3e9d465a6c860d1c4d2f121fe4a8"
   license "MIT"
   head "https://github.com/railwayapp/cli.git", branch: "master"
 
