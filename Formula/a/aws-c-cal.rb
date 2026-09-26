1c1da4f3f6962dbdac08b01cbf891ffefe8903f0
Michael Cho <michael@michaelcho.dev>
2026-09-26 17:10:07 -0400

    aws-c-cal: revision bump (migrate to `openssl@4`)

--- Formula/a/aws-c-cal.rb
+++ Formula/a/aws-c-cal.rb
@@ -4,6 +4,7 @@ class AwsCCal < Formula
   url "https://github.com/awslabs/aws-c-cal/archive/refs/tags/v1.0.0.tar.gz"
   sha256 "9c6d424d206dd7822aa44fa39ce31575dcbaa83133620abdac8e56e4cea9667c"
   license "Apache-2.0"
+  revision 1
   compatibility_version 2
 
   bottle do
@@ -17,7 +18,7 @@ class AwsCCal < Formula
 
   depends_on "cmake" => :build
   depends_on "aws-c-common"
-  depends_on "openssl@3"
+  depends_on "openssl@4"
 
   deny_network_access!
 
